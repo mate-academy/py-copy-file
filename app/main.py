0a5adec88c35fc2cd0ba89e83a8b0b19a809e8e4
Nazar-kob <kobrinnazar05@mail.com>
2022-02-02 15:34:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str):
+    command = command.split(' ')
+    if command[1] != command[2]:
+        with open(command[1], 'r') as file:
+            with open(command[2], 'w') as file_copy:
+                file_copy.write(file.read())
