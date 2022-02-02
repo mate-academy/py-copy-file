c31b62fe81b9f42de0ca6b593ad8ce5d0f7157e4
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-02-02 15:35:52 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str):
+    sep_command = command.split()
+    file = sep_command[1]
+    new_file = sep_command[2]
+
+    if file != new_file:
+        with open(file, "r") as f:
+            with open(new_file, "w") as f2:
+                f2.write(str(f.read()))
+
+
+if __name__ == '__main__':
+    copy_file("cp file.txt file1.txt")
