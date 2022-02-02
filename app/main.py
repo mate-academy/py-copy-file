4c511f09419bb4bc0d4fdcbc721c75640e23e39f
alix makarova <alix.makarova@gmail.com>
2022-02-02 17:10:03 +0200

    Changed file names

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str):
     split_command = command.split()
     if split_command[1] != split_command[2]:
-        with open(split_command[1], 'r') as f1:
-            lines = f1.readlines()
-            with open(split_command[2], "w") as f2:
+        with open(split_command[1], 'r') as source:
+            lines = source.readlines()
+            with open(split_command[2], "w") as destination:
                 for line in lines:
-                    f2.write(line)
+                    destination.write(line)
                 print(f"File {split_command[1]} copied successfully!")
