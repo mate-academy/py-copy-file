5a44b2752362cf5551b7034977a7474328683872
Vlad Tsvigun <vl.tsv@outlook.com>
2022-02-02 16:45:16 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str):
+    new_command = command.split()
+    file = new_command[1]
+    new_file = new_command[2]
+
+    if file != new_file:
+        shutil.copy(file, new_file)
