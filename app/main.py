ed570ab411e629ec290e9ccd2d9e9f5b05589522
Denys Lipin <lipin.transstream@gmail.com>
2022-02-02 16:26:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str):
+    main_command, old_file, new_file = command.split()
+    if main_command != "cp" or old_file == new_file:
+        exit()
+    with open(old_file, "r") as file_read:
+        with open(new_file, "w") as file_write:
+            file_write.writelines(file_read.readlines())
+
+
+if __name__ == "__main__":
+    copy_file(input("Enter command: "))
