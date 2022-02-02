042ae0c81127f79c87de88a3e0c2d864f2efcb14
nikon4uk <nikonchukmax4@gmail.com>
2022-02-02 20:14:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def copy_file(command):
+    operator, file, new_file = command.split()
+    if operator != "cp":
+        return
+    if file == new_file:
+        return
+
+    with open(file, 'r') as f:
+        with open(new_file, 'w') as f2:
+            for line in f:
+                f2.write(line)
+
+
+def main():
+    copy_file("cp test1.txt test1.txt")
+
+
+if __name__ == "__main__":
+    main()
