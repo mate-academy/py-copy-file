ef706f952792631b367fb0ecffcdbe4e5773da8c
Rybak Dmytro <codingclaud@gmail.com>
2022-02-02 21:24:39 +0200

    'Rename_variables'

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ def copy_file(command: str):
     if command_list[1] == command_list[2]:
         return
 
-    with open(command_list[1], "r") as first_file:
-        first_file = first_file.read()
+    with open(command_list[1], "r") as source:
+        source = source.read()
 
-        with open(command_list[2], "w") as second_file:
-            second_file.write(first_file)
+        with open(command_list[2], "w") as destination:
+            destination.write(source)
 
 
 copy_file("cp file.txt file2.txt")
