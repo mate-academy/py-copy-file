094845a0472572a3d600d549d8cf32fe3813cce6
TJgkOsVP <sasha06052001@gmail.com>
2022-02-02 20:30:29 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def copy_file(command):
-    file1, file2 = command.split(' ')[1], command.split(' ')[2]
-    with open(file1, "r") as f1:
-        with open(file2, "w") as f2:
-            f2.write(f1.read())
+    with open(command.split(' ')[1], "r") as source:
+        with open(command.split(' ')[2], "w") as destination:
+            destination.write(source.read())
