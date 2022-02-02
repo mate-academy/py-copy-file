aeeabf8df812c03288c80cb7869fa1594d12effa
TJgkOsVP <sasha06052001@gmail.com>
2022-02-02 17:06:10 +0200

    'solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,4 @@
-# write your code here
+def copy_file(command):
+    with open(command.split(' ')[1], "r") as f1:
+        with open(command.split(' ')[2], "w") as f2:
+            f2.write(f1.read())
