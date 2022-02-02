99a2e882969c312432385bea4b108651931949dc
alix makarova <alix.makarova@gmail.com>
2022-02-02 15:08:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str):
+    split_command = command.split()
+    if split_command[1] != split_command[2]:
+        with open(split_command[1], 'r') as f1:
+            lines = f1.readlines()
+            with open(split_command[2], "w") as f2:
+                for line in lines:
+                    f2.write(line)
+                print(f"File {split_command[1]} copied successfully!")
