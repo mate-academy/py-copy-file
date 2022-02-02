3a14415a82e4693e2263036091250350a73443ad
Kalyta Valerii <deptovka@gmail.com>
2022-02-02 17:40:22 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command):
+    data = command.split()
+    if data[0] == 'cp' and data[1] != data[2]:
+        with open(data[1], 'r') as source:
+            with open(data[2], 'w') as result:
+                result.write(str(source.read()))
+
+if __name__ == "__main__":
+    copy_file("cp file.txt file1.txt")
