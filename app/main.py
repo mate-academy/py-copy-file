7f16f6aebb714cc077077be4d45950312ab70b8c
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-02 16:13:39 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command):
+    ls = command.split()
+    if ls[1] != ls[2]:
+        with open(f"{ls[1]}", "w") as f:
+            f.write("something")
+        with open(f"{ls[1]}", "r") as f1:
+            content = f1.read()
+            with open(ls[2], "w") as f2:
+                f2.write(content)
+
+
+copy_file("cp file.txt new_file.txt")
