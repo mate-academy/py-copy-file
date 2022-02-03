461eb71fc31a7980c1e56c2a69a33c4824462751
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-03 07:17:43 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def copy_file(command):
     ls = command.split()
     if ls[1] != ls[2]:
-        with open(f"{ls[1]}", "w") as f:
-            f.write("something")
         with open(f"{ls[1]}", "r") as f1:
             content = f1.read()
             with open(ls[2], "w") as f2:
