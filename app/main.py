dc7cb7ec63ef22ac1ae010fa3e88c737ab7edfa8
Dmitry Ivanov <karward94@gmail.com>
2022-02-03 18:39:49 +0200

    Fix: Read all file; file_lines → file_content; file → first_file, file2 → second_file.

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ def copy_file(command: str):
     if command_list[0] != "cp":
         print("Enter correct command (cp)")
     elif command_list[1] != command_list[2]:
-        with open(command_list[1], "r") as file:
-            file_lines = file.readlines()
-            with open(command_list[2], "w") as file2:
-                file2.write("".join(file_lines))
+        with open(command_list[1], "r") as first_file:
+            file_content = first_file.read()
+            with open(command_list[2], "w") as second_file:
+                second_file.write(file_content)
 
 
 if __name__ == '__main__':
