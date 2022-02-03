72c3dab5c999f554c3568dc0459dced5a0c8394d
Zikkolion <nmadaraosnova835@gmail.com>
2022-02-03 17:35:28 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command):
+    command = command.split(' ')
+    if command[1] != command[2]:
+        with open(f'{command[1]}', 'r') as f:
+            with open(f'{command[2]}', 'w') as fc:
+                fc.write(f.read())
+
+
+if __name__ == '__main__':
+    copy_file('cp file.txt new_file.txt')
