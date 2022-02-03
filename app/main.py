0756e65b470e36b324157cd4d13014611f685ee7
Anita Posledova <anitaposliedova@gmail.com>
2022-02-03 17:27:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ def copy_file(command):
     new_file = command.split()[2]
 
     if old_file != new_file:
-        with open(old_file, "r") as f:
-            with open(new_file, "w") as f2:
-                f2.write(f.read())
+        with open(old_file, "r") as source:
+            with open(new_file, "w") as destination:
+                destination.write(source.read())
