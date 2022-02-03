e6a437e1e693b2cc366ddb7fcdf04e22c303723f
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-03 10:12:30 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command):
     user_command = command.split()
     if user_command[0] == "cp" and user_command[1] != user_command[2]:
-        with open(f"{user_command[1]}", "r") as fin:
+        with open(user_command[1], "r") as fin:
             with open(user_command[2], "w") as fout:
                 for line in fin:
                     fout.write(line)
