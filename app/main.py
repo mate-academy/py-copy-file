3f2ac4f2c87d5019b350604d1765748a077c9e81
Vlad Tsvigun <vl.tsv@outlook.com>
2022-02-03 15:33:45 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -7,4 +7,6 @@ def copy_file(command: str):
     new_file = new_command[2]
 
     if file != new_file:
-        shutil.copy(file, new_file)
+        with open(file, "r") as origin:
+            with open(new_file, "w") as copy:
+                copy.write(str(origin.read()))
