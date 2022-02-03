b36aeb61fd1065baba916f8f2699bd55f8a1b6d7
TJgkOsVP <sasha06052001@gmail.com>
2022-02-03 15:30:57 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 def copy_file(command):
     with open(command.split(' ')[1], "r") as source:
         with open(command.split(' ')[2], "w") as destination:
-            destination.write(source.read())
+            if command.split(' ')[1] != command.split(' ')[2]:
+                destination.write(source.read())
