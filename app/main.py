7a98f51c948f2873b71620ce72bcc8ae925c4b43
Dmitry Ivanov <karward94@gmail.com>
2022-02-04 11:36:44 +0200

    Fix: first_file → source, second_file → destination.

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ def copy_file(command: str):
     if command_list[0] != "cp":
         print("Enter correct command (cp)")
     elif command_list[1] != command_list[2]:
-        with open(command_list[1], "r") as first_file:
-            file_content = first_file.read()
-            with open(command_list[2], "w") as second_file:
-                second_file.write(file_content)
+        with open(command_list[1], "r") as source:
+            file_content = source.read()
+            with open(command_list[2], "w") as destination:
+                destination.write(file_content)
 
 
 if __name__ == '__main__':
