7e19d021b1051753903ff2911deae1f4da935b0d
Marko Khodan <khodanmarko@gmail.com>
2022-05-24 19:02:32 +0300

    requesr fixed

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command):
     files = command.split(" ")[1::]
     old_file = files[0]
     new_file = files[1]
-    if old_file != new_file and command == f"co {old_file} {new_file}":
+    if old_file != new_file and command == f"cp {old_file} {new_file}":
         with open(old_file, "r") as old_file:
             with open(new_file, "w") as new_file:
                 new_file.write(old_file.read())
