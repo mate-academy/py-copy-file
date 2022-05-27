dbed7828388ef4b0ff9ec9b9c29d4cbeefa49c9b
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-27 11:33:34 +0300

    Remove function call

--- app/main.py
+++ app/main.py
@@ -5,6 +5,3 @@ def copy_file(command: str):
         with open(file_names[1], "r") as f:
             with open(file_names[2], "w") as f2:
                 f2.write(f.read())
-
-
-copy_file("cp test.txt test-copy.txt")
