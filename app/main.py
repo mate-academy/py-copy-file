6cc1164068a8e69abc3faffe4665384fe5560730
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-27 10:40:44 +0300

    Fix remarks

--- app/main.py
+++ app/main.py
@@ -1,14 +1,10 @@
-import re
-
-
 def copy_file(command: str):
-    file_names = re.findall(r"(\w+[-_.,]?\w+.txt)", command)
+    file_names = command.split(" ")
 
-    if file_names[0] != file_names[1]:
-        with open(file_names[0], "r") as f:
-            with open(file_names[1], "w") as f2:
+    if file_names[1] != file_names[2]:
+        with open(file_names[1], "r") as f:
+            with open(file_names[2], "w") as f2:
                 f2.write(f.read())
-                print("File copy is created successes")
 
 
 copy_file("cp test.txt test-copy.txt")
