e7772621bc7dac9681dff8175d28769ca9810d3d
Flashmobber <ufinaid@gmail.com>
2022-05-28 20:12:52 +0300

    All done.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str):
+    parsed = command.split()
+
+    if parsed[0] == "cp" and parsed[1] != parsed[2]:
+
+        with open(parsed[1], "r") as f1:
+            with open(parsed[2], "w") as f2:
+                f2.write(f1.read())
+                f2.close()
+            f1.close()
