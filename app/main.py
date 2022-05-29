cd550503141287b96336c428fb91354f28e96842
Max Sukhaniuk <maksym.sukhaniuk@gmail.com>
2022-05-29 21:04:48 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command):
+    command = command.split()
+    if command[0] == "sp":
+        if command[1] != command[2]:
+            with open(command[1], "r") as old_file:
+                with open(command[2], "w") as new_file:
+                    new_file.write(old_file.read())
