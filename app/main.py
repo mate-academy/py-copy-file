3bda28942f55d461d584bf7b8ded28f0b6c6263a
Serhii Leonenko <leonenko3149@gmail.com>
2022-05-29 14:18:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str):
+    command_ls = command.split()
+
+    assert command_ls[0] == "cp", f"Command:'{command_ls[0]}' is not found"
+
+    if command_ls[1] != command_ls[2]:
+        with open(command_ls[1], "r") as f:
+            with open(command_ls[2], "w") as f2:
+                f2.write(f.read())
+
+                print(f"File:{command_ls[1]} copied!")
