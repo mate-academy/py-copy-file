071ee55dcdc3904aaf31d75c892b8e71565288dd
Magus6784 <100879423+Magus6784@users.noreply.github.com>
2022-05-30 20:33:02 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str):
     command = command.split()
-    if command[0] == "sp" and command[1] != command[2]:
+    if command[0] == "cp" and command[1] != command[2]:
         with open(command[1], "r") as old_file:
             with open(command[2], "w") as new_file:
                 new_file.write(old_file.read())
