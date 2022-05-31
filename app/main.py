eb565fe10898448017b1186f3c986189f1100781
Олег Белый <belochard@gmail.com>
2022-05-31 17:14:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str):
+    cmdList = command.split()
+    if cmdList[1] == cmdList[2]:
+        print("Same file")
+    else:
+        with open(cmdList[1], "r") as fin, open(cmdList[2], "w+") as fout:
+            fout.write(fin.read())
+
+
+copy_file("cp file.txt file2.txt")
