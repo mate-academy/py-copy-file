86430ddfc004b59a364477ae33627aae642b0a4b
Elena <lenasharabura@gmail.com>
2022-05-31 15:29:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command):
+    src = command.split()[1]
+    dst = command.split()[2]
+    with open(src, "r") as file_in, open(dst, "w") as file_out:
+        file_out.writelines(file_in.read())
