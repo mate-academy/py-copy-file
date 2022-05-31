4fbb79abe1a6481ebdebca49ec9a1752c73894c3
Ed Orlovskyy <fedorov12816@gmail.com>
2022-05-31 17:02:37 +0300

    CopySolution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str):
+    origin_file = command.split()[1]
+    file_copy = command.split()[2]
+
+    if origin_file != file_copy:
+        with open(origin_file, "r") as f:
+            with open(file_copy, "w") as c:
+                c.write(f.read())
