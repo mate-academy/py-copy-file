69fdd20b8399c4b0a36dd3f0f28a2083b2bdedf9
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-05-31 13:08:08 +0300

    'copy-file_'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str):
     if command.split()[1] != command.split()[2]:
-        with open(command.split()[1], "r") as file_in, \
-                open(command.split()[2], "w") as file_out:
-            file_out.write(file_in.read())
+        with open(command.split()[1], "r") as file, \
+                open(command.split()[2], "w") as new_file:
+            new_file.write(file.read())
