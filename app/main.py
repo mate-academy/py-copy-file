8c54e8013ff407394c1ff8a42c2474a17b5ab384
Yurnerroo <yura.zdanevych@gmail.com>
2022-05-31 17:16:16 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str):
+    command_spl = command.split()
+    if command_spl[0] == "cp":
+        if command_spl[1] != command_spl[2]:
+            with open(command_spl[1], "r") as file1:
+                file1 = file1.read()
+            with open(command_spl[2], "w") as file2:
+                file2.write(file1)
