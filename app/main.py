8bdfcb9598df768ca4f690c8232ed13dafab838c
Yaroslav Havryliv <ghavryliv@gmail.com>
2022-05-31 15:54:34 +0300

    final solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str):
     file_names = command.split()
 
-    with open(file_names[1], "r") as file_in, \
-            open(file_names[2], "w") as file_out:
-        data = file_in.read()
-        file_out.write(data)
+    if file_names[1] != file_names[2]:
+        with open(file_names[1], "r") as file_in:
+            with open(file_names[2], "w") as file_out:
+                data = file_in.read()
+                file_out.write(data)
