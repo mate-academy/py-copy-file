9f82f7917bb3d1a32fca4665fa292ec6c87ae95a
Roman Tsybulia <104589999+RomaTsybulia@users.noreply.github.com>
2022-05-31 16:41:13 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str):
-    command_list = list(command.split())
+    command_list = command.split()
     if command_list[0] == "cp" and command_list[1] != command_list[2]:
-        with open(f"{command_list[1]}", "r") as old_file:
-            with open(f"{command_list[2]}", "w") as new_file:
+        with open(command_list[1], "r") as old_file:
+            with open(command_list[2], "w") as new_file:
                 new_file.write(old_file.read())
