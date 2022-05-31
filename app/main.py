63d1cf643a9388ff83d9956aabb323cd7906e0d7
Katya Vasylenko <kettyblack20@gmail.com>
2022-05-31 19:18:25 +0300

    Fixed Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str):
-    check_list = command.split(" ")
+    check_list = command.split()
     if check_list[1] != check_list[2]:
-        with open(f"{check_list[1]}", "r") as file_in:
+        with open(check_list[1], "r") as file_in:
             lines = file_in.readlines()
 
-        with open(f"{check_list[2]}", "w") as file_out:
+        with open(check_list[2], "w") as file_out:
             for line in lines:
                 file_out.write(line)
