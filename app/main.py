1fd80fa0410e4664ad5a13a9d459666dee8acce1
Serhii Leonenko <leonenko3149@gmail.com>
2022-05-31 14:12:37 +0300

    Used descriptive file names

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 def copy_file(command: str):
     command_ls = command.split()
+    command_name = command_ls[0]
+    current_file = command_ls[1]
+    new_file = command_ls[2]
 
-    assert command_ls[0] == "cp", f"Command:'{command_ls[0]}' is not found"
+    assert command_name == "cp", f"Command:'{command_name}' is not found"
 
-    if command_ls[1] != command_ls[2]:
-        with open(command_ls[1], "r") as f:
-            with open(command_ls[2], "w") as f2:
+    if current_file != new_file:
+        with open(current_file, "r") as f:
+            with open(new_file, "w") as f2:
                 f2.write(f.read())
 
-                print(f"File:{command_ls[1]} copied!")
+                print(f"File:{current_file} copied!")
