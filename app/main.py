a57beec2f212128794b97324eae9d998831b90fe
Igor Cegelnyk <il.cegelnik@gmail.com>
2022-06-01 18:22:52 +0300

    third solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command):
     command_list = command.split(" ")
-    if command_list[1] != command_list[2]:
+    if command_list[1] != command_list[2] and command_list[0] == "cp":
         with open(f"{command_list[1]}", "r") as file_in, \
                 open(f"{command_list[2]}", "w") as file_out:
             file_out.write(f"{file_in.read()}")
