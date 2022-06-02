19d213a835304a6801382512dc09f935e19617c6
Evgeniy Tretiak <103074600+Evgeniy994@users.noreply.github.com>
2022-06-02 15:46:21 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str):
-    names = command.split('')
+    names = command.split()
 
     if names[1] != names[2]:
         with open(names[1], "r") as f_in, open(names[2], "w") as f_out:
