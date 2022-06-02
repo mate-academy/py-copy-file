e2496bdfac7cd10040a93241611c39316ae89daf
Vasyl Smutok <23.smutok@gmail.com>
2022-06-02 15:10:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ def copy_file(command: str):
     if cmd[0] == "cp" and cmd[1] != cmd[2]:
         with open(cmd[1], "r") as file_in, open(cmd[2], "w") as file_out:
             file_out.write(file_in.read())
-        print("Successful")
+        print("Successful!")
