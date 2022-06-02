5956bc1b7e540ea1e703c46ea5df71efa2f8373a
Artem Toporchan <gydratvar@gmail.com>
2022-06-02 20:12:12 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # If you're reviewing this code - thank you <3
 
 def copy_file(command: str):
-    command_list = list(command.split(" "))
+    command_list = command.split()
     if command_list[0] == "cp" and command_list[1] != command_list[2]:
         with open(command_list[1], "r") as file_in, \
                 open(command_list[2], "w") as file_out:
