27f118920811f9003edaecf2def86b25038d74cb
ILIAS <nossudak@gmail.com>
2022-06-02 16:28:44 +0300

    Removed comment and removed 0 from return

--- app/main.py
+++ app/main.py
@@ -2,10 +2,8 @@ def copy_file(command: str):
     old_file_name = command.split()[1]
     new_file_name = command.split()[2]
     if old_file_name == new_file_name:
-        return 0
+        return
     with open(old_file_name, "r") as file:
         content = file.readlines()
     with open(new_file_name, "w") as file:
         file.writelines(content)
-
-# copy_file("cp file.txt new_file.txt")
