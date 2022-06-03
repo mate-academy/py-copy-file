15ec393e11dfbff34054650419e0936e2e7e6a8d
Denis <sairustv228@gmail.com>
2022-06-03 16:29:56 +0300

    v1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str):
+    list_of_commands = command.split()
+    start = list_of_commands[1]
+    end = list_of_commands[2]
+
+    if start != end:
+        with open(start, "r") as start_f:
+            with open(end, "w") as end_f:
+                end_f.write(start_f.read())
