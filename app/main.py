c9c888b51f123bfc6706efbce3b01b297870efd3
Artem Kharchenko <darten360@gmail.com>
2022-06-06 22:06:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str):
     lst = command.split(" ")
     if lst[0] == "cp" and lst[1] != lst[2]:
-        with open(f"{lst[1]}", "r") as file_in, open(f"{lst[2]}", "r") as file_out:
+        with open(f"{lst[1]}", "r") as file_in, open(f"{lst[2]}", "w") as file_out:
             file_out.write(file_in.read())
