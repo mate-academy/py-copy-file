2acfa59923a3aa8b463d6f6dc810a3d72df20de8
Anton Lasko <anton.lasko1993@gmail.com>
2022-06-08 17:06:11 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command):
-    with open(command[3:12], 'r') as first, open(command[12:], 'a') as second:
-        if first != second:
-            for line in first:
-                second.write(line)
+    with open(command[3:12], 'r') as old, open(command[12:], 'a') as new:
+        if old != new:
+            for line in old:
+                new.write(line)
