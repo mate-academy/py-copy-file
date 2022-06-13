d58d3630d64702ea90bd48189d0ba48c45718ae5
Oleg Fadeev <61756426+drivemax26@users.noreply.github.com>
2022-06-13 12:33:59 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,4 +3,3 @@ def copy_file(command: str):
     if file[1] != file[2] and file[0] == "cp":
         with open(file[1], "r") as file1, open(file[2], "w") as file2:
             file2.write(file1.read())
- 
