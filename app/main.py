02ae412c3ad88a4aa8ac0bd6af1f62333be2e087
Oleg Fadeev <61756426+drivemax26@users.noreply.github.com>
2022-06-13 12:31:10 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-def copy_file(command):
+def copy_file(command: str):
     file = command.split()
-    if file[1] != file[2]:
-        with open(file[1], "r") as file1:
-            with open(file[2], "w") as file2:
-                file2.write(file1.read())
+    if file[1] != file[2] and file[0] == "cp":
+        with open(file[1], "r") as file1, open(file[2], "w") as file2:
+            file2.write(file1.read())
+ 
