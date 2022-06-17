2638ddf2ea3bdc09087f95f21d69e01042d96f01
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-18 01:34:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+from os import remove
+
+
+def copy_file(command):
+    if command.split()[1] != command.split()[2]:
+        old_file = open(command.split()[1], 'r')
+        new_file = open(command.split()[2], "w")
+        new_file.write(old_file.read())
+        old_file.close()
+        remove(command.split()[1])
+        new_file.close()
+    return "Files names is identical"
