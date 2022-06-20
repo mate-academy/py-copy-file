f55942318498a917c1bec121c34847304b101606
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-20 13:51:08 +0300

    Refactored with used context manager

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ from os import remove
 
 def copy_file(command):
     if command.split()[1] != command.split()[2]:
-        old_file = open(command.split()[1], 'r')
-        new_file = open(command.split()[2], "w")
-        new_file.write(old_file.read())
-        old_file.close()
-        remove(command.split()[1])
-        new_file.close()
+        with open(command.split()[1], 'r') as old_file:
+            with open(command.split()[2], "w") as new_file:
+                new_file.write(old_file.read())
+                old_file.close()
+                remove(command.split()[1])
+                new_file.close()
     return "Files names is identical"
