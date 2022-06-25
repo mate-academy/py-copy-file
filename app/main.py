62ad2bde78a6e407a802907854360ed052279461
SenchaPencha <SenchaPencha@mail.ru>
2022-06-25 15:56:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str):
+    file1 = command.split()[1]
+    file2 = command.split()[2]
+    if file1 == file2:
+        return
+    else:
+        with open(file1, "r") as file_in, open(file2, "w") as file_out:
+            if file_in.read() == file_out.read():
+                return
+            else:
+                file_out.write(file_in.read())
