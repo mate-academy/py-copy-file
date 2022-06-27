6bb5170925c11405e3b6aa78faabbb47083ec79d
SenchaPencha <SenchaPencha@mail.ru>
2022-06-27 16:09:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str):
-    file1 = command.split()[1]
-    file2 = command.split()[2]
-    if file1 != file2:
-        with open(file1, "r") as file_in, open(file2, "w") as file_out:
-            if file_in.read() == file_out.read():
-                return
-            else:
-                file_out.write(file_in.read())
+    com = command.split()[0]
+    if com == "cp":
+        file1 = command.split()[1]
+        file2 = command.split()[2]
+        if file1 != file2:
+            with open(file1, "r") as file_in, open(file2, "w") as file_out:
+                if not  file_in.read() == file_out.read():
+                    file_out.write(file_in.read())
