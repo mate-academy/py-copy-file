d13b7bfbfd9436fb518e20f48ed302f068048651
dstarkaiev <dstarkaiev@gmail.com>
2022-07-15 16:11:45 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 def copy_file(command):
+    prefix = command.split(" ")[0]
     old_file = command.split(" ")[1]
     new_file = command.split(" ")[2]
 
-    if old_file == new_file:
+    if old_file == new_file or not prefix == "cp":
         return
     try:
         with open(old_file, "r") as old, open(new_file, "w") as new:
