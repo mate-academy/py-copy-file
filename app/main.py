87f47bdbfa979c10392b9de6bc2df6659b7ad2d0
dstarkaiev <dstarkaiev@gmail.com>
2022-07-15 15:57:00 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command):
+    old_file = command.split(" ")[1]
+    new_file = command.split(" ")[2]
+
+    if old_file == new_file:
+        return
+    try:
+        with open(old_file, "r") as old, open(new_file, "w") as new:
+            new.write(old.read())
+    except FileNotFoundError:
+        print("Mentioned file does not exist")
