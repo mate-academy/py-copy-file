b7aff1676e7e5cd05cdc3927081b60fcad5c9862
muhactive <muhactive@gmail.com>
2022-07-16 20:01:35 +0300

    cp

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command):
     copy_list = list(command)
-    if copy_list[1] != copy_list[2]:
+    if copy_list[1] != copy_list[2] and copy_list[0] == "cp":
         with open(copy_list[1], "r") as file_in, \
                 open(copy_list[2], "w") as file_out:
             file_out.write(file_in.read())
     else:
-        return open(copy_list[1]).read() == open(copy_list[2]).read()
+        return 0
