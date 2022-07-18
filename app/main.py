51404cd0dd7a447e87b8cf0509e2097523d67959
Denis Pidlubniy <denispodlubnyj@gmail.com>
2022-07-18 18:17:45 +0300

    check cp

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command):
-    file_in = command.split()[1]
-    file_out = command.split()[2]
-    if file_in != file_out:
-        with open(file_in, "r") as file_in, open(file_out, "w") as file_out:
+    file_1 = command.split()[1]
+    file_2 = command.split()[2]
+    if file_1 != file_2 and command.split()[0] == 'cp':
+        with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
             file_out.write(file_in.read())
