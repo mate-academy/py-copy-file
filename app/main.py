97a73fe406e384e904f0aa281c75accc5aefcec7
Vladyslav Peresta <perestavlad@gmail.com>
2022-07-24 16:27:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str):
+    command_list = command.split(" ")
+    copy_from = command_list[1]
+    copy_to = command_list[2]
+
+    if copy_from != copy_to:
+        with open(copy_from, mode="r") as read_file, \
+                open(copy_to, mode="w") as written_file:
+            for line in read_file:
+                written_file.write(line)
