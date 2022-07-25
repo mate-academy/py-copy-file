0a85ca0f3a2ebf5f82a12885b738f88ce6856e6e
Vladyslav Peresta <perestavlad@gmail.com>
2022-07-25 12:21:31 +0300

    Additional condition

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 def copy_file(command: str):
     command_list = command.split(" ")
+    perform_command = command_list[0]
     copy_from = command_list[1]
     copy_to = command_list[2]
 
-    if copy_from != copy_to:
+    if perform_command == "cp" and copy_from != copy_to:
         with open(copy_from, mode="r") as read_file, \
                 open(copy_to, mode="w") as written_file:
             for line in read_file:
