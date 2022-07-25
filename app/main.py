ed3f03a84c8d28ebe8129521ee6a947ca8bb7ece
Andriy Demeshko <ande777complaint@gmail.com>
2022-07-25 11:53:08 +0300

    Solution edited

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command):
     file_names = command.split()
-    print(file_names)
+    if file_names[1] == file_names[2]:
+        return
     with open(file_names[1], "r") as file_out, \
             open(file_names[2], "w") as file_in:
         context = file_out.read()
