c77d0ddcf02c890dc1456a020a7e09b3c24e43d7
stoveprofi <stoveprofi@gmail.com>
2022-08-02 19:31:03 +0100

    Remake

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-import os
-
-
 def copy_file(command):
-    os.system(command)
+    s_list = command.split(" ")
+    file_in = s_list[1]
+    file_out = s_list[2]
+    with open(file_in, "r") as f_in, open(file_out, "w") as f_out:
+        cont = f_in.read()
+        f_out.write(cont)
