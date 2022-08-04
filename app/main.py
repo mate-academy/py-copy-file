5072578d2a4bd330440675e43681f8255290b60e
Dima Povkh <dimapovkh@gmail.com>
2022-08-04 12:14:40 +0200

    copy_file_v1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+
+def copy_file(command: str):
+    file = command.split()[1]
+    new_file = command.split()[2]
+    if file != new_file and command.split()[0] == "cp":
+        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+            # shutil.copyfile()
+            file_out.write(file_in.read())
