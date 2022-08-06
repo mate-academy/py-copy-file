0652b6d7be8bbf02fad46b04c9398a06fa5ad907
Timur <timur.hike@gmail.com>
2022-08-06 23:02:30 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command):
+    result = command.split(" ")
+    old_file = result[1]
+    new_file = result[2]
+    if old_file == new_file:
+        return
+    with open(old_file, "r") as old, open(new_file, "w") as new:
+        text = old.read()
+        new.write(text)
