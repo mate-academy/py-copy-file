060119bae77293606e74302546aa58ec4a72d978
muhactive <muhactive@gmail.com>
2022-08-07 15:26:49 +0300

    delete else

--- app/main.py
+++ app/main.py
@@ -4,5 +4,4 @@ def copy_file(command):
         with open(copy_list[1], "r") as file_in, \
                 open(copy_list[2], "w") as file_out:
             file_out.write(file_in.read())
-    else:
-        return
+    return
