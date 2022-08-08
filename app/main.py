104c66b82cbce82cc204fc369214ce32734cb0a8
yaroslav-demchenko <dastex1411@gmail.com>
2022-08-08 14:47:01 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str):
     file_data_list = command.split(" ")
     old_file = file_data_list[1]
     new_file = file_data_list[2]
-    if old_file != new_file:
+    if old_file != new_file and file_data_list[0] == "cp":
         with open(old_file, "r") as file_out, open(new_file, "w") as file_in:
             for line in file_out:
                 file_in.write(line)
