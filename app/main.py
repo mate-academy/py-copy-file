b4c8cd0338904a46f2474e3b9109a0d032a5544c
AnDy <ansmbox@gmail.com>
2022-08-09 21:54:21 +0300

    Fix command line validation

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str):
-    file_name_in, file_name_out = command.split()[1:]
+    file_operation, file_name_in, file_name_out = command.split()
 
-    if file_name_in and file_name_out and file_name_in != file_name_out:
+    if file_operation == "cp" and file_name_in != file_name_out:
         with open(file_name_in, "r") as file_in,\
                 open(file_name_out, "w") as file_out:
             file_out.write(file_in.read())
