9baef7a6fea36bb970dc46d5951708e3c1a27a80
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2022-08-12 13:26:47 +0300

    Added command check

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str):
+    copy_command = command.split(" ")[0]
     file1 = command.split(" ")[1]
     file2 = command.split(" ")[2]
-    if file1 != file2:
+    if file1 != file2 and copy_command == "cp":
         with open(file1, "r") as file_in, open(file2, "w") as file_out:
             file_out.write(file_in.read())
