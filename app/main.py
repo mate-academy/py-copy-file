286c9d25d3302ac14260929a065d1bfde3b808f6
Viktor Beniukh <superbigbenjamin@gmail.com>
2022-08-18 15:36:15 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str):
 
     command = command.split()
 
-    if command[1] != command[2]:
+    if command[1] != command[2] and command[0] == "cp":
         with open(command[1], "r") as file_in, \
                 open(command[2], "w") as file_out:
 
