d204207a6bbd9401318faaa7c03fb0cc412b8b4e
Volodymyr Panov <vpanov25@gmail.com>
2022-08-19 20:16:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str):
+    names = command.split()
+    original_name = names[1]
+    file_copy = names[2]
+    if original_name == file_copy:
+        pass
+    with open(original_name, "r") as file_in, \
+            open(file_copy, "w") as file_out:
+        [file_out.write(line) for line in file_in]
