0cf5ebcb3a3f2b3c5f8f09238a2ad0318a21b16c
Volodymyr Panov <vpanov25@gmail.com>
2022-08-22 16:31:39 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str):
     names = command.split()
-    original_name = names[1]
-    file_copy = names[2]
-    if original_name == file_copy:
+    original_name = names[-2]
+    file_copy = names[-1]
+    if original_name == file_copy or "cp" not in command:
         pass
     with open(original_name, "r") as file_in, \
             open(file_copy, "w") as file_out:
