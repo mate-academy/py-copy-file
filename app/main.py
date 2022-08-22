c2dd6142ebc6dae94b1e92ab4a19100b66c84f35
Volodymyr Panov <vpanov25@gmail.com>
2022-08-22 16:50:18 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -3,9 +3,7 @@ def copy_file(command: str):
     cp = names[0]
     original_name = names[-2]
     file_copy = names[-1]
-    if original_name == file_copy or cp != "cp":
-        pass
-    else:
+    if original_name != file_copy or cp == "cp":
         with open(original_name, "r") as file_in, \
                 open(file_copy, "w") as file_out:
             [file_out.write(line) for line in file_in]
