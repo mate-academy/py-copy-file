e265a484bc263d794b18c6c68d589b1ea22c33b8
Anastasia Martyniuk <anastasia.20.12.1995@gmail.com>
2022-08-23 19:11:08 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ def copy_file(command: str):
     list_command = command.split()
     source = list_command[1]
     destination = list_command[2]
-    if source != destination:
+    if source != destination and list_command[0] == "cp":
         shutil.copyfile(source, destination)
