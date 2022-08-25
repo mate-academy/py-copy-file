ec00d2ae81bc9e2af3b9cae81056be98e85a5eef
Oleg Usenko <Oleg911Usenko@gmail.com>
2022-08-25 16:13:51 +0300

    Complete task v05

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str):
-    temp = command.split()
-    if temp[-1] != temp[-2]:
-        with open(f"{temp[-2]}", "r") as file_in, \
-                open(f"{temp[-1]}", "w") as file_out:
+    str_list_command = command.split()
+    if str_list_command[-1] != str_list_command[-2]:
+        with open(f"{str_list_command[-2]}", "r") as file_in, \
+                open(f"{str_list_command[-1]}", "w") as file_out:
             file_out.write(f"{file_in.read()}")
