b92df56e4f66d7eeb04183cde4a0ab01dc6b1dcf
Diliaver Yusupov <diliaver795@gmail.com>
2022-08-26 20:49:57 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(command):
+    command = input()
     list = command.split()
     if list[1] != list[2]:
-        with open(f"{list[1]}", "r") as firstfile, \
-                open(f"{list[2]}", "a") as secondfile:
-            for line in firstfile:
-                secondfile.write(line)
-    else:
-        pass
+        with open(f"{list[1]}", "r") as file_to_copy, \
+                open(f"{list[2]}", "a") as result_file:
+            for line in file_to_copy:
+                result_file.write(line)
