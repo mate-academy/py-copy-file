6755fa75f4134a424a364ada0311050b83de23e2
Vlad Magdenko <vladearth7@gmail.com>
2022-08-28 12:27:42 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+from shutil import copyfileobj
+
+
+def copy_file(command):
+    data = command.split()
+    if data[0] == 'cp':
+        with open(f"{data[1]}.txt", "r") as file_in, \
+             open(f"{data[2]}.txt", "w") as file_out:
+            copyfileobj(file_in, file_out)
