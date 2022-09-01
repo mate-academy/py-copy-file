8c7ac16b71fea18ab32e13407868fe9eae42603d
Paul Starodub <amorallex@gmail.com>
2022-09-01 10:35:48 +0300

    Task

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(command: str):
     list_args = command.split()
-    print(list_args)
-    if list_args[1] != list_args[2]:
+    if list_args[0] == "cp" and list_args[1] != list_args[2]:
         with open(f"{list_args[1]}", "r") as file_in,\
                 open(f"{list_args[2]}", "w") as file_out:
             c = file_in.read()
