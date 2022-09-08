1b13e3e6e8432c1f5e507484789f4aeee99237ed
Foggy-Cat <rosky.yaroslav@gmail.com>
2022-09-08 22:07:14 +0300

    prov

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str):
+    new_com = command.split()
+    if new_com[1] == new_com[2]:
+        return
+    with open(new_com[1], "r") as one, open(new_com[2], "w") as two:
+        two.write(one.read())
