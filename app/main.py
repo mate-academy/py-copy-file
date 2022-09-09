b28833b4807906c30de7cccf42d70774612953f4
Foggy-Cat <rosky.yaroslav@gmail.com>
2022-09-09 23:20:35 +0300

    Sol for check command

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def copy_file(command: str):
-    new_com = command.split()
+    new_com = command.strip().split()
+    if new_com[0] != "cp" and len(new_com) != 3:
+        return "Wrong command"
     if new_com[1] == new_com[2]:
         return
     with open(new_com[1], "r") as one, open(new_com[2], "w") as two:
