3a41c9d44fcd20a9aab9b5e10934e8c14933aa7f
Oleksandr Panasiuk <provizor.307@gmail.com>
2022-09-12 14:36:44 +0300

    CP check

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str):
     file = command.split(" ")
-    if file[1] != file[2]:
-        with open(file[1], "r") as file_in, open(file[2], "w") as file_out:
-            for line in file_in:
-                file_out.write(line)
+    if file[0] == "cp":
+        if file[1] != file[2]:
+            with open(file[1], "r") as file_in, open(file[2], "w") as file_out:
+                for line in file_in:
+                    file_out.write(line)
