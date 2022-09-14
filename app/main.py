10835eff69069f7a2b39310e2cebab3e808a03f6
Vlad Milkov <vladmilkov@gmail.com>
2022-09-14 17:03:17 +0300

    Added check for input command. Added error handling for wrong command string

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 def copy_file(command: str):
-    commands = command.split()
-    file, new_file = commands[1], commands[2]
+    try:
+        input_command, file, new_file = command.split()
+    except ValueError:
+        print("Incorrect command")
+        return
 
     if file == new_file:
         return
 
-    with open(file, "r") as file_in, open(new_file, "w") as file_out:
-        file_out.write(file_in.read())
+    if input_command == "cp":
+        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
