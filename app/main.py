d84f2de9f51e468845ca6b85a13def0828dafe41
Davyd Kryvko <davidkrivko@gmail.com>
2022-09-18 10:45:45 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(files: str):
+    files = files.split()
+    if files[1] == files[0]:
+        return
+    with open(files[0], "r") as file_in, open(files[1], "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
