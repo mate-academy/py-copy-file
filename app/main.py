91949af9120cf13c311261a49d11174db606bf83
Kyryll Katash <kyryllkatash@gmail.com>
2022-09-19 18:34:58 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(str_of_files: str):
     files = str_of_files.split()
-    if files[0] == files[1]:
+    if files[1] == files[2]:
         return
     else:
-        with open(files[0], "r") as file_in, open(files[1], "w") as file_out:
+        with open(files[1], "r") as file_in, open(files[2], "w") as file_out:
             content = file_in.read()
             file_out.write(content)
