bd134f34fcf79127d8c8d11c2235a50af0c8ad52
Ivan Antonishak <jus1stored@gmail.com>
2022-09-21 21:45:22 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    original_file, copy = command.split(" ")[1:]
+    if original_file != copy:
+        # os.popen("cp original_file copy") for Linux
+        os.popen(f"copy {original_file} {copy}")
