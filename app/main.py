e644bd865aeb8311bee3c4282367dd39bf9278f7
Diana <daria.blessed18@gmail.com>
2022-09-22 00:03:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command):
+    file_to_copy = command.split()[1]
+    new_file = command.split()[2]
+    if file_to_copy == new_file:
+        return
+    with open(file_to_copy, "r") as file_one, open(new_file, "a") as file_two:
+        for line in file_one:
+            file_two.write(line)
