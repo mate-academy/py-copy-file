35608c1e68d2eb482d4cb3f29610b11a590f61dc
Soroka Igor <copokaigor@gmail.com>
2022-09-22 14:32:00 +0300

    solution1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(command: str):
     new_file = command.split(" ")
     new_file.remove(new_file[0])
-    print(new_file)
     with open(new_file[1], "r") as file_in, open(new_file[2], "w") as file_out:
         for info in file_in:
             file_out.write(info)
