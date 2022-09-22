3acc268c7300b7e61172e19f3eb7cab204519abc
mltdll <ilya.vyplavin@gmail.com>
2022-09-22 18:44:15 +0300

    Remove checks

--- app/main.py
+++ app/main.py
@@ -1,19 +1,9 @@
 def copy_file(command):
-    command = command.split()
+    name_in, name_out = command.split()[1:]
 
-    match command:
-        case ["cp", name_in, name_out]:
-            if name_in == name_out:
-                return
+    if name_in == name_out:
+        return
 
-            with (open(name_in, "r") as file_in,
-                  open(name_out, "w") as file_out):
-                for line in file_in:
-                    file_out.write(line)
-
-        case ["cp", *arguments]:
-            raise ValueError(f"Invalid syntax: command cp takes "
-                             f"2 arguments, {len(arguments)} given.")
-
-        case _:
-            raise ValueError(f"Invalid command: {command[0]}")
+    with open(name_in, "r") as file_in, open(name_out, "w") as file_out:
+        for line in file_in:
+            file_out.write(line)
