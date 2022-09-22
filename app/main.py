f72afd13995cbea52f63e81ba2637156f122699a
mltdll <ilya.vyplavin@gmail.com>
2022-09-22 18:38:18 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def copy_file(command):
+    command = command.split()
+
+    match command:
+        case ["cp", name_in, name_out]:
+            if name_in == name_out:
+                return
+
+            with (open(name_in, "r") as file_in,
+                  open(name_out, "w") as file_out):
+                for line in file_in:
+                    file_out.write(line)
+
+        case ["cp", *arguments]:
+            raise ValueError(f"Invalid syntax: command cp takes "
+                             f"2 arguments, {len(arguments)} given.")
+
+        case _:
+            raise ValueError(f"Invalid command: {command[0]}")
