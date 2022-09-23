2fc66a9328b9daee313951caf4758c0ab06c0aab
Maks Danylchuk <maxitosm00@gmail.com>
2022-09-24 02:28:06 +0300

    Ready

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str):
+    names = command.split(" ")
+    with open(f"{names[1]}", "r") as file, open(f"{names[2]}", "w") as f_copy:
+        if names[1] != names[2]:
+            f_copy.write(file.read())
