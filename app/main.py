2ed0dfda0fe4deb14bac34aed06c4ccd3a69d8d1
Artem <Artemvyhovanec@gmail.com>
2022-09-25 15:09:45 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command):
+    command = command.split()
+    if command[1] == command[2]:
+        return
+    with open(command[1], "r") as f, open(command[2], "w") as ff:
+        ff.write(f.read())
