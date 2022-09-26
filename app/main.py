66093eeeacf018083e2d87d7e93000a50950deb9
Stas Okhrym <s.okhrym@gmail.com>
2022-09-26 12:40:38 +0300

    Fixed file naming

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copyfile_example(command: str) -> None:
-    command_name, file_in, file_out = command.split()
+    command_name, file_name, file_copy_name = command.split()
 
-    if file_in == file_out:
+    if file_name == file_copy_name:
         return
 
-    with open(file_in, 'r') as source, open(file_out, 'w') as destination:
-        destination.write(source.read())
+    with open(file_name, 'r') as source, open(file_copy_name, 'w') as copy:
+        copy.write(source.read())
