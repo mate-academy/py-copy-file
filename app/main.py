fcc6b02e5465b9fbf79d894d36d809fbf339091c
Stas Okhrym <s.okhrym@gmail.com>
2022-09-26 12:42:58 +0300

    Typos removed

--- app/main.py
+++ app/main.py
@@ -4,5 +4,5 @@ def copyfile_example(command: str) -> None:
     if file_name == file_copy_name:
         return
 
-    with open(file_name, 'r') as source, open(file_copy_name, 'w') as copy:
+    with open(file_name, "r") as source, open(file_copy_name, "w") as copy:
         copy.write(source.read())
