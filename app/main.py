0a74d39b29cf73dafd98201042dc186ded051194
Artem Vykhovanets <80789163+AVyha@users.noreply.github.com>
2022-09-26 09:39:52 +0300

    rename arguments

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    if command[1] == command[2]:
+    old_file_name = command[1]
+    new_file_name = command[2]
+    if old_file_name == new_file_name:
         return
-    with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+    with (open(old_file_name, "r") as file_in,
+          open(new_file_name, "w") as file_out):
         file_out.write(file_in.read())
