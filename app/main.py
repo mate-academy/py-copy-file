a12155a47a74d7f3870202da039297817a8f398d
Alex Parkhomenko <105493988+wQuelS@users.noreply.github.com>
2022-09-27 08:55:01 +0000

    fixed to plural:)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
-    command = command.split()
+    commands = command.split()
 
-    if command[1] == command[2]:
+    if commands[1] == commands[2]:
         return
 
-    with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+    with open(commands[1], "r") as file_in, open(commands[2], "w") as file_out:
         file_out.write(file_in.read())
