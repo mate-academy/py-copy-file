224c1e8f1155e274485d87299aa43a88a437f1d4
Zakhar Podporinov <zp162162@gmail.com>
2022-09-28 23:44:11 +0300

    Corrected

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def copy_file(command: str) -> None:
     command = command.split()
     if command[0] == 'cp' and command[1] != command[2]:
-        with open(command[2], 'r') as copied_file:
-            content = copied_file.read()
-        with open(command[2], 'w') as file:
-            file.write(content)
+        with open(command[2], 'r') as copied, open(command[2], 'w') as file:
+            file.write(copied.read())
