44475166dc79bede57165b8ae8cf5a921bbe2555
Oleksandr Mamchur <irex1988@gmail.com>
2022-09-29 14:52:05 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command):
+    a = command.split(" ")
+    if a[1] != a[2]:
+        with open(f"{a[1]}", "r") as file, open(f"{a[2]}", "w") as file_copy:
+            content = file.read()
+            file_copy.write(content)
