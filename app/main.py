99b799df6e036ee9ab66009139e13937579b8e57
Yura <pazyn404@gamil.com>
2022-09-29 23:48:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command_line: str) -> None:
+    _, source_name, destination_name = command_line.split(" ")
+
+    with (open(source_name, "r") as source,
+          open(destination_name, "w") as destination):
+
+        destination.writelines(source.readlines())
