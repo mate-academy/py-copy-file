3e36772ed3b2b0a2f1e60c1658a1b3208f72da8f
Zakhar Podporinov <zp162162@gmail.com>
2022-09-29 22:52:49 +0300

    Correct quotes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    if command[0] == 'cp' and command[1] != command[2]:
-        with open(command[2], 'r') as copied, open(command[2], 'w') as file:
+    if command[0] == "cp" and command[1] != command[2]:
+        with open(command[2], "r") as copied, open(command[2], "w") as file:
             file.write(copied.read())
