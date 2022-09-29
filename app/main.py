ecdf020f21a7f66ef006cac02562caaab5eba62f
Oleksandr Mamchur <irex1988@gmail.com>
2022-09-29 18:11:15 +0300

    'Fixed_mistakes_part2'

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ def copy_file(command):
     file2 = string[2]
     if string[0] == "cp":
         if file1 != file2:
-            with open(f"{file1}", "r") as file, open(f"{file2}", "w") as file_copy:
+            with open(f"{file1}", "r") as file,\
+                    open(f"{file2}", "w") as file_copy:
                 content = file.read()
                 file_copy.write(content)
