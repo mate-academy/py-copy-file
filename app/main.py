545cd05bfffe974459607ad7ca588168b0b4c6a9
Oleksandr Murynskyi <95870462+Aleksandro777@users.noreply.github.com>
2022-09-29 19:54:55 +0300

    Update main.py
    "cp" check added

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 def copy_file(command: str) -> None:
     command = command.split()
 
-    if command[1] == command[2]:
-        return
+    if command[0] == "cp" and command[1] != command[2]:
 
-    with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
-        file_out.write(file_in.read())
+        with open(command[1], "r") \
+                as file_in, open(command[2], "w") \
+                as file_out:
+            file_out.write(file_in.read())
