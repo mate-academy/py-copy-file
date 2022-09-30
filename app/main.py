d2e0fe14ea9d3fd206045b9e78c57e9fd6e2f6ed
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-09-30 18:08:46 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None | str:
+    command = command.split()
+    original = command[1]
+    copy_original = command[2]
+
+    if command[0] != "cp":
+        return "Error: unknown command"
+    if original != copy_original:
+        return
+
+    with open(original, "r") as file_in, open(copy_original, "w") as file_out:
+        file_out.write(file_in.read())
