a289c31f59496926b6fbfbd66c802e04cce873eb
Yulia Bozhieva <bozhieva93@gmail.com>
2022-09-30 10:32:11 -0400

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-# write your code here
 def copy_file(command):
-    if not command.split()[1] == command.split()[2]:
-        with open(command.split()[1], "r") as file_in, \
-                open(command.split()[2], "w") as file_out:
-            content = file_in.read()
-            file_out.write(content)
+    if command.split()[1] == command.split()[2] \
+            or command.split()[0] != "cp":
+        return
+    with open(command.split()[1], "r") as file_in, \
+            open(command.split()[2], "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
