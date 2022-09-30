d8eaee395f9f4c85cd8d90f2178f31630bb9c488
Yura <pazyn404@gamil.com>
2022-09-30 14:15:41 +0300

    Fixed copy file in itself

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command_line: str) -> None:
     _, source_name, destination_name = command_line.split(" ")
 
-    with (open(source_name, "r") as source,
-          open(destination_name, "w") as destination):
+    if source_name != destination_name:
+        with (open(source_name, "r") as source,
+              open(destination_name, "w") as destination):
 
-        destination.writelines(source.readlines())
+            destination.writelines(source.readlines())
