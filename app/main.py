eea8aa1ade521e7c28aa8bdc3d0faa0dbfbe7ab6
HStakhiv <gsstahiv@gmail.com>
2022-10-01 16:30:06 +0300

    New message and logic

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(line: str):
     command, old_file, new_file = line.split(" ")
 
-    if old_file == new_file:
-        return "Your file names are the same!"
     if command != "cp":
+        return "Wrong command"
+    if old_file == new_file:
         return
 
     with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
