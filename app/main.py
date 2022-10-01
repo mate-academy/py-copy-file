065cd75275775390530452ce9c4c84edace0792c
HStakhiv <gsstahiv@gmail.com>
2022-10-01 16:27:36 +0300

    Empty return

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-def copy_file(line: str) -> str:
+def copy_file(line: str):
     command, old_file, new_file = line.split(" ")
 
-    if command != "cp" or old_file == new_file:
+    if old_file == new_file:
         return "Incorrect command or your file names are the same!"
+    if command != "cp":
+        return
 
     with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
         file_out.write(file_in.read())
