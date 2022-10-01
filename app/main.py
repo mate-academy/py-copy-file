1599d77aafdd4652455d582801c9546ebb93a954
Roman Kolmakov <cerne313@gmail.com>
2022-10-01 15:15:07 +0300

    solve the task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str):
+    com_list = command.split
+    old_name = com_list[1]
+    new_name = com_list[2]
+
+    if com_list[0] != 'cp':
+        return
+    if old_name == new_name:
+        return
+
+    with open(old_name, 'r') as original, open(new_name, 'w') as copied:
+        copied.write(original.read())
