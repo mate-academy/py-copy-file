661f28b2edae540ea6f5be83bbcb25a53d1e6752
HStakhiv <gsstahiv@gmail.com>
2022-10-01 16:22:19 +0300

    Fixed logic

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-def copy_file(command: str) -> None:
-    old_file, new_file = command.split(" ")[1:]
+def copy_file(line: str) -> str:
+    command, old_file, new_file = line.split(" ")[1:]
 
-    if old_file != new_file:
-        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-            file_out.write(file_in.read())
+    if command != "cp" or old_file == new_file:
+        return "Incorrect command or your file names are the same!"
+
+    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
