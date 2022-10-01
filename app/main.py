637293ec182cca07437bcefa38fb5bd55d667c45
HStakhiv <gsstahiv@gmail.com>
2022-10-01 16:32:31 +0300

    Empty return. No messages

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 def copy_file(line: str):
     command, old_file, new_file = line.split(" ")
 
-    if command != "cp":
-        return "Wrong command"
-    if old_file == new_file:
+    if command != "cp" or old_file == new_file:
         return
 
     with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
