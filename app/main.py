cbe9932db7a06f41a3a9787bb4b7fb33b01a7fb5
Katrin Lazarenko <katrin903@gmail.com>
2022-10-02 19:08:19 +0300

    Fixed faild flake8 test

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
     list_of_commands = command.split()
-    if list_of_commands[1] != list_of_commands[2] and list_of_commands[0] == "cp":
+    if list_of_commands[1] != list_of_commands[2] and \
+            list_of_commands[0] == "cp":
         with open(list_of_commands[1], "r") as origin_file,\
                 open(list_of_commands[2], "w") as new_file:
             new_file.write(origin_file.read())
