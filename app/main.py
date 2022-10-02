6b294481ffb142b254ed1d00af97b21aac71fcd6
Alexander <alexzubkoff123@gmail.com>
2022-10-02 16:00:28 +0300

    Added checking if source file exists

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
+import os.path
+
+
 def copy_file(command: str) -> None:
     if command.split()[0] == "cp":
         if command.split()[1] != command.split()[2]:
-            with open(command.split()[1], "r") as file_in,\
-                    open(command.split()[2], "w") as file_out:
-                file_out.write(file_in.read())
+            if os.path.exists(command.split()[1]):
+                with open(command.split()[1], "r") as file_in,\
+                        open(command.split()[2], "w") as file_out:
+                    file_out.write(file_in.read())
         else:
             return
