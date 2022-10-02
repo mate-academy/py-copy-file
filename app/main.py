454e84833f536e8241a37e092d74e4bc8f0bfd7c
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-10-02 17:13:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str):
+    lst_separated = command.split(" ")
+    if lst_separated[1] != lst_separated[2]:
+        with open(lst_separated[1], "r") as first_file, \
+                open(lst_separated[2], "a") as second_file:
+            for line in first_file:
+                second_file.write(line)
