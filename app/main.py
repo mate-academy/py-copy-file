df89a81e3a2d63cfe47ec2bac9f1aa0686eb89b5
Sergii Franchuk <Franchuksergei@gmail.com>
2022-10-02 20:00:45 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    if command[0] == "cp" and command[1] != command[2]:
+        with open(command[1]) as f, open(command[2], "w") as copy:
+            copy.write(f.read())
