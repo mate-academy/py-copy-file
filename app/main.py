6c9d82d232d1a84b476918589930f332f178d1bc
Katrin Lazarenko <katrin903@gmail.com>
2022-10-02 18:57:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    list_of_commands = command.split()
+    if list_of_commands[1] != list_of_commands[2] and list_of_commands == "cp":
+        with open(list_of_commands[1], "r") as origin_file,\
+                open(list_of_commands[2], "w") as new_file:
+            new_file.write(origin_file.read())
