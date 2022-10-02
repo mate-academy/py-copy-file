e957e251d61ce9bca69a93677084f415a72f1c6a
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-10-02 18:01:47 +0300

    Solution with all conditions

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str):
     lst_separated = command.split(" ")
-    if lst_separated[1] != lst_separated[2]:
+    if lst_separated[0] == "cp" and lst_separated[1] != lst_separated[2]:
         with open(lst_separated[1], "r") as first_file, \
                 open(lst_separated[2], "a") as second_file:
             for line in first_file:
