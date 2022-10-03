7655910732597b6a57532c62818a5033cceed6ba
Dmytro Skrypka <2346677tyu@gmail.com>
2022-10-03 12:51:40 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp, file, file_copy = command.split(",")
+
+    if file == file_copy or cp != "cp":
+        return
+
+    with open(file, "r+") as line:
+        line.write(line.read())
