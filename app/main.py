0236a15a46898cc2aaac6bb3e3d969c151eb6cd8
Dmytro Skrypka <2346677tyu@gmail.com>
2022-10-03 13:48:20 +0300

    Solution changed the conditional operator number two

--- app/main.py
+++ app/main.py
@@ -2,5 +2,5 @@ def copy_file(command: str) -> None:
     cp, file, file_copy = command.split(" ")
 
     if file == file_copy or cp != "cp":
-        with open(file, "r+") as line:
-            line.write(line.read())
+        with open(file, "r") as line, open(file, "w") as copy:
+            copy.write(line.read())
