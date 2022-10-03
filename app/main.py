402e06db70c6dd683f4f7eb5ba17fcf0de229235
Dima Ovcharenko <kukuku381@gmail.com>
2022-10-03 12:33:06 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_name, file, new_file = command.split()
+
+    if file == new_file:
+        return
+
+    with open(file, "r") as main, open(new_file, "w") as copy:
+        copy.write(main.read())
