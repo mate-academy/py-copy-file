bca73be24752b650c97833075f73f782adf0af81
Adrian Kuzhel <adrian.kujel@gmail.com>
2022-10-03 22:35:45 +0300

    Fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy_file(command: str) -> None:
     cmd_list = command.split(' ')
     if cmd_list[1] != cmd_list[2] and cmd_list[0] == "cp":
-        with open(cmd_list[1], "r") as file_in, open(cmd_list[2], "w") as file_out:
-            file_out.write(file_in.read())
-
+        with open(cmd_list[1], "r") as f_in, open(cmd_list[2], "w") as f_out:
+            f_out.write(f_in.read())
