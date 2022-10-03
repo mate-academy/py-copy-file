0920311fec77adb69cd8b85b7c3de66b71ac7a76
Pavlo Kapitanets <aixpavlik"gmail.com>
2022-10-03 17:44:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    new_command = command.split()
+    if new_command[1] == new_command[2] or new_command[0] != "cp":
+        print("Invalid input")
+        return
+    with (open(new_command[1], "r") as file_in,
+          open(new_command[2], "w") as file_out):
+        lines = file_in.readlines()
+        file_out.writelines(lines)
