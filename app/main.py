62cb3f1e5e644a8cbe6b04d5cf5640b2d5a626f1
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-10-03 14:05:57 +0300

    Solution without if in main logic

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str):
     lst_separated = command.split(" ")
-    if lst_separated[0] == "cp" and lst_separated[1] != lst_separated[2]:
-        with open(lst_separated[1], "r") as first_file, \
-                open(lst_separated[2], "a") as second_file:
-            for line in first_file:
-                second_file.write(line)
+    if lst_separated[0] != "cp" or lst_separated[1] == lst_separated[2]:
+        return
+
+    with open(lst_separated[1], "r") as f, open(lst_separated[2], "a") as s:
+        for line in f:
+            s.write(line)
