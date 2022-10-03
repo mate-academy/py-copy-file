05519033843f42d0399f2f75f52d136537f15ab1
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-10-03 09:37:23 +0300

    PEP8 fixed

--- app/main.py
+++ app/main.py
@@ -3,7 +3,3 @@ def copy_file(command: str) -> None:
     if cmd[0] == "cp" and cmd[1] != cmd[2]:
         with open(cmd[1], "r") as source, open(cmd[2], "w") as copy:
             copy.write(source.read())
-
-
-
-
