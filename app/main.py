ef0f51615e58655059d47d65cf1e1b74ff9d09e9
Nikyxa <kolisova.veronika81@gmail.com>
2022-10-03 17:04:28 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str):
+    list_commands = command.split()
+    command = list_commands[0]
+    first_file = list_commands[1]
+    new_file = list_commands[2]
+
+    if first_file == new_file and command != "cp":
+        return None
+
+    with open(first_file, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
+    return "Complete!"
