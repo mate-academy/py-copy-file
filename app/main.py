f8d653b7fff77aaf2b2103890c1e368f5142593e
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-10-03 16:05:42 +0200

    Add check cp

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     command = command.split()
 
-    if command[1] == command[2]:
+    if command[0] != "cp" or command[1] == command[2]:
         return
 
     with open(command[1], "r") as file, open(command[2], "a") as copy:
