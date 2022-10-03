f2677bb7ac34c9188fa739f25369dd74a164856b
graandda <graanddadays@gmail.com>
2022-10-03 13:48:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ def copy_file(command: str):
     old_file = cmd_list[1]
     new_file = cmd_list[2]
 
-    if old_file != new_file and cmd == "cp":
+    if old_file == new_file and cmd != "cp":
+        return print("Something wrong!")
+    else:
         with open(old_file, "r") as file, open(new_file, "w") as new_file:
             new_file.write(file.read())
-    else:
-        return
