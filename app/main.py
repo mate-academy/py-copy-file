0a6b32c14fa1e320317cfb454fcf63ddad4c56e6
Sergey <servizah@gmail.com>
2022-10-03 17:39:25 +0300

    break-lines changed and output symplified

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(command: str) -> None:
     command_list = command.split()
-    if command_list[1] == command_list[2]:
+    if command_list[1] == command_list[2] or command_list[0] != "cp":
         return
     else:
-        with open(command_list[1], "r") as file_in,\
-                open(command_list[2], "w") as file_out:
-            lines = file_in.readlines()
-            file_out.writelines(lines)
+        with open(command_list[1], "r") as file_in:
+            with open(command_list[2], "w") as file_out:
+                file_out.writelines(file_in.readlines())
