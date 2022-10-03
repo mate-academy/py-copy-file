39e0940fa99a166df5fcdad7c3e6b9e1d6feab88
Alexander <alexzubkoff123@gmail.com>
2022-10-03 21:11:35 +0300

    Remove extra else

--- app/main.py
+++ app/main.py
@@ -8,5 +8,3 @@ def copy_file(command: str) -> None:
                 with open(command.split()[1], "r") as file_in,\
                         open(command.split()[2], "w") as file_out:
                     file_out.write(file_in.read())
-        else:
-            return
