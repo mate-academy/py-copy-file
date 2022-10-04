f3b1831bd0445b6753905b43519d842b8ea21b22
Dima Ovcharenko <kukuku381@gmail.com>
2022-10-04 12:56:05 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     command_name, file, new_file = command.split()
 
-    if file == new_file:
+    if command_name != "cp" or file == new_file:
         return
 
     with open(file, "r") as main, open(new_file, "w") as copy:
