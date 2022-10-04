d3e445c1333b2624807c05afd330d584fcc8e839
Yevhenii Davydenko <akazhulich@gmail.com>
2022-10-04 11:51:11 +0300

    add command check

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ def copy_file(command: str):
     sep_cm = command.split(" ")
     if sep_cm[1] == sep_cm[2]:
         return
-    with (open(f"{sep_cm[1]}", "r") as file_in,
-          open(f"{sep_cm[2]}", "w") as file_out):
-        file_out.writelines(file_in.readlines())
+    if sep_cm[0] == "cp":
+        with (open(f"{sep_cm[1]}", "r") as file_in,
+              open(f"{sep_cm[2]}", "w") as file_out):
+            file_out.writelines(file_in.readlines())
