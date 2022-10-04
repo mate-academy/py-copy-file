1e38f8eda3013346877c9911b2031a8558076854
Dmytro Petliovanyi <petliovany@gmail.com>
2022-10-04 19:18:27 +0300

    Solution4

--- app/main.py
+++ app/main.py
@@ -10,5 +10,6 @@ def copy_file(command: str) -> str | None:
     if file_to_copy == copy_of_file:
         return f'file with name "{file_to_copy}" already exists'
 
-    with open(file_to_copy, "r") as file_original, open(copy_of_file, "w") as file_copy:
+    with open(file_to_copy, "r") as file_original, \
+            open(copy_of_file, "w") as file_copy:
         file_copy.write(file_original.read())
