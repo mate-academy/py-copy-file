df05dce7dc817c24ea37e044e4c498b22c7c9c68
Mariia Movchan <mariia.movchan.m@gmail.com>
2022-10-04 17:49:00 +0300

    Added condition for command, removed split() duplication

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
-    file = command.split()[1]
-    new_file = command.split()[2]
-    if file != new_file:
-        with open(file, "r") as file_in, open(new_file, "w") as file_out:
-            file_out.write(file_in.read())
+    command, file, new_file = command.split()
+
+    if file == new_file or command != "cp":
+        return
+
+    with open(file, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
