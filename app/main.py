3c9e9f057b870bc401f89ea23e4af29632bd28aa
Maks Danylchuk <maxitosm00@gmail.com>
2022-10-04 17:19:51 +0300

    rdy

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 def copy_file(command: str):
     if command:
         names = command.split(" ")
-        with open(f"{names[1]}", "r") as file, open(f"{names[2]}", "w") as f_copy:
+        with open(f"{names[1]}", "r") as file, \
+                open(f"{names[2]}", "w") as f_copy:
             if names[1] != names[2]:
                 f_copy.write(file.read())
+    else:
+        return "command entered incorrectly"
