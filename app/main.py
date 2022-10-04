5a153be289ed01b3342a60d16faae49b4d3a6530
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-10-04 13:41:10 +0300

    After refactor

--- app/main.py
+++ app/main.py
@@ -1,14 +1,10 @@
-# write your code here
 def copy_file(command: str) -> None:
     command_list = command.split(" ")
 
-    if command_list[1] == command_list[2]:
+    if command_list[0] != "cp" and command_list[1] != command_list[2]:
         return
-    else:
 
-        old_file = command_list[1]
-        new_file = command_list[2]
-
-        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-            string_file = file_in.read()
-            file_out.write(string_file)
+    with open(command_list[1], "r") as file_in, open(command_list[2],
+                                                     "w") as file_out:
+        string_file = file_in.read()
+        file_out.write(string_file)
