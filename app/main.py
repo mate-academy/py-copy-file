0a9ed9b553eee4048e15a03c25bf9f2c748adabb
illia riaboi <riaboy94@gmail.com>
2022-10-04 13:15:12 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
-    text = command.split()
-    if text[0] != "cp" or text[1] == text[2]:
+    cmd = command.split()
+    if cmd[0] != "cp" or cmd[1] == cmd[2]:
         return
 
-    with open(text[1], "r") as file_in, open(text[2], "w") as file_out:
+    with open(cmd[1], "r") as file_in, open(cmd[2], "w") as file_out:
         for line in file_in:
             file_out.write(line)
