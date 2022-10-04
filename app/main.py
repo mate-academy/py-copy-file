7e3a3f74ba35825752cf649b35019ed21c536e3f
Bohdan2001007 <bogdan1991007@gmail.com>
2022-10-04 13:33:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command):
-    file = command.split()
+    file = command.split(" ")
 
     command = file[0]
     old_file = file[1]
     new_file = file[2]
 
     if old_file != new_file and command == "ср":
-        with open('old_file', "r") as file_in, open('new_file', "w") as file_out:
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             file_out.write(file_in.read())
