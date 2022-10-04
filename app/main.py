451e00a84ec664628a7f0bb6d0e59183f16b4765
Vasyl Poremchuk <nleveryone@gmail.com>
2022-10-04 14:41:52 +0300

    Checks and variable names have been changed

--- app/main.py
+++ app/main.py
@@ -1,15 +1,8 @@
-from re import match
-
-
 def copy_file(command: str) -> None:
-    command_check = match(r"cp .*\.txt .*\.txt", command)
+    copy_command, file1, file2 = command.split()
 
-    if command_check:
-        first_file_name = command.split()[1]
-        second_file_name = command.split()[2]
-
-        if first_file_name != second_file_name:
-            with (open(first_file_name, "r") as first_file,
-                  open(second_file_name, "w") as second_file):
-                for line in first_file:
-                    second_file.write(line)
+    if copy_command == "cp":
+        if file1 != file2:
+            with (open(file1, "r") as input_file,
+                  open(file2, "w") as output_file):
+                output_file.write(input_file.read())
