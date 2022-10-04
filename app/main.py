bce22e2e4fa3e5661975b033b727153bf70ffb69
Vitalii Shevchuk <shevch.nl@gmail.com>
2022-10-04 18:29:13 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
     command = command.split()
     if command[0] == "cp" and command[1] != command[2]:
-        with open(command[1], "r") as source_file,\
-                open(command[2], "w") as result_file:
+        with (
+                open(command[1], "r") as source_file,
+                open(command[2], "w") as result_file
+        ):
             result_file.write(source_file.read())
