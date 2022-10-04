e9365d876df9af216c6711d5a0e03ff6c7f6e955
Nailia Mamiedova <nailia.mamiedova.work@gmail.com>
2022-10-04 19:15:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str):
+    names = command.split()
+    file = names[1]
+    file_copy = names[2]
+    with open(file, "r") as file_in, open(file_copy, "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
