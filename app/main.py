8745ea1be712f2a2f33e0b8f358f43e43340dad4
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-10-04 14:28:00 +0300

    if was fixed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 def copy_file(command: str) -> None:
     command_list = command.split(" ")
 
-    if command_list[0] != "cp" and command_list[1] != command_list[2]:
+    if command_list[0] != "cp":
+        return
+
+    if command_list[1] == command_list[2]:
         return
 
     with open(command_list[1], "r") as file_in, open(command_list[2],
