0848dc5cb9735b73270f2e6cf7e3da8438b280dc
Nikyxa <113314502+Nikyxa@users.noreply.github.com>
2022-10-04 15:41:21 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str):
-    list_commands = command.split()
-    command = list_commands[0]
-    first_file = list_commands[1]
-    new_file = list_commands[2]
+    files = command.split()
+    command = files[0]
+    first_file = files[1]
+    new_file = files[2]
 
-    if first_file == new_file and command != "cp":
+    if first_file == new_file or command != "cp":
         return None
 
     with open(first_file, "r") as file_in, open(new_file, "w") as file_out:
