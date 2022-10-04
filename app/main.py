a6edabb21d54dbdca9a4cdd256ef0beb27c96fb7
illia riaboi <riaboy94@gmail.com>
2022-10-04 09:39:02 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command):
     name_in, name_out = command.split()[1:]
 
-    if name_in == name_out:
+    if command != "cp" or name_in == name_out:
         return
 
     with open(name_in, "r") as file_in, open(name_out, "w") as file_out:
