8e03d9bfa707a9048dbc843a8dd3f8e964d151f5
Vlad Hrytsai <sunrises2929@gmail.com>
2022-10-05 13:15:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(copy_files: str):
+    cp_file = copy_files.split(" ")
+    if cp_file[1] != cp_file[2]:
+        with open(cp_file[1], "r") as file_in,\
+                open(cp_file[2], "w") as file_out:
+            file_out.write(file_in.read())
