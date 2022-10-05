40af2bcce74e332c719d9cfe6a3709d456e5fdb9
Nailia Mamiedova <nailia.mamiedova.work@gmail.com>
2022-10-06 00:15:27 +0300

    Solution_v3

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-def copy_file():
-    command = input()
+def copy_file(command: str):
     names = command.split()
+    cd_command = names[0]
     file = names[1]
     file_copy = names[2]
-    with open(file, "r") as file_in, open(file_copy, "w") as file_out:
-        content = file_in.read()
-        file_out.write(content)
+    if file != file_copy and cd_command == "cd":
+        with open(file, "r") as file_in, open(file_copy, "w") as file_out:
+            content = file_in.read()
+            file_out.write(content)
