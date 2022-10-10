608bb7a478241f47ae600c15b91bc7f011741791
IvanGLS <gladiolus0707@gmail.com>
2022-10-10 16:30:04 +0300

    'Done'

--- app/main.py
+++ app/main.py
@@ -2,4 +2,10 @@ import os
 
 
 def copy_file(command: str) -> None:
-    os.popen(command)
+    command_string = command.split()
+    checker = os.path.isfile(command_string[1])
+    if checker and command_string[1] != command_string[2]:
+        with open(command_string[1], "r") as file_for_copy:
+            text_from_file = file_for_copy.read()
+        with open(command_string[2], "w") as copied_file:
+            copied_file.write(text_from_file)
