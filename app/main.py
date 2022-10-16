8d00be81906929f78b1ace842879d57e423b2e3c
Nataliia <amirammina@gmail.com>
2022-10-16 16:23:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+    with open(command[1], "r") as file_from, open(command[2], "a") as file_to:
+        for line in file_from.readlines():
+            file_to.write(line)
