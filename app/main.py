41f3e281ba2c920a462d49ac538c0bc5d27f0d97
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-10-17 11:17:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> list:
+    result = command.split()
+    with open(result[1], "r") as file_in, open(result[2], "w") as file_out:
+        if result[1] == result[2]:
+            return []
+        result = file_in.read()
+        file_out.write(result)
+
+
+copy_file("cp file.txt file2.txt")
