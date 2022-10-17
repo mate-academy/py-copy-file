721bbe9d0b12a3d78c13cc775585451a68b0806d
Nataliia <amirammina@gmail.com>
2022-10-17 11:53:57 +0300

    Checked if the input command was correct

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 def copy_file(command: str) -> None:
     command = command.split(" ")
+
+    if command[0] != "cp":
+        raise NameError("Cannot recognize the command")
+
     with open(command[1], "r") as file_from, open(command[2], "a") as file_to:
         for line in file_from.readlines():
             file_to.write(line)
