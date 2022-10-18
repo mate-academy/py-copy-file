0bca5db87e3c62a03f7f74c0da3cce6baa9d7723
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-10-18 16:04:40 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 def copy_file(command: str) -> None:
+    cp_name = command.split()[0]
     first_file_name = command.split()[1]
     second_file_name = command.split()[2]
-    if first_file_name != second_file_name:
-        with open(first_file_name, "r") as file_in, \
-             open(second_file_name, "w") as file_out:
-            result = file_in.read()
-            file_out.write(result)
+    if cp_name == "cp":
+        if first_file_name != second_file_name:
+            with open(first_file_name, "r") as file_in, \
+                 open(second_file_name, "w") as file_out:
+                result = file_in.read()
+                file_out.write(result)
