75c2b5910958de372f69945f9740749e09dd3db2
Taras Zelinskyi <taras.sv89@gmail.com>
2022-10-19 15:09:56 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 def copy_file(command: str) -> None:
     words = command.split()
-    print(words[1])
-    print(words[2])
     if words[0] == "cp" and words[1] != words[2]:
         with open(words[1], "r") as file_in, open(words[2], "w") as file_out:
             context = file_in.read()
             file_out.write(context)
-    else:
-        pass
