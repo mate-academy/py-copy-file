39c9fce13749086afa78dbe890898ff66911cf3c
Anton Chumak <Chumak7377@gmail.com>
2022-10-24 11:40:21 +0300

    added check the input command

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ def copy_file(command: str) -> None:
     name_file = input("Enter file name: ")
     new_name_file = input("Enter a new file name: ")
 
-    if not name_file == new_name_file:
+    if name_file != new_name_file and command == "cp":
         with open(f"{name_file}.txt", "w") as file_in, \
                 open(f"{new_name_file}.txt", "w") as file_out:
             file_in.write(f"{command} {name_file}.txt {new_name_file}.txt")
