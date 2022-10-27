2edeeb7824aa2c6503e5e3fca7bc14c8e019db22
Oleksandr Kolomiiets <jumanje82@gmail.com>
2022-10-27 23:10:27 +0300

    'Solution_#1'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    separate = command.split()
+    if separate[1] != separate[2]:
+        with open(separate[1], "r") as file_in,\
+                open(separate[2], "w") as file_out:
+            file_new = file_in.read()
+            file_out.write(file_new)
