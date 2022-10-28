63804ffa807a16cc8a7adac5c2dc112cc66555f4
Oleksandr Kolomiiets <jumanje82@gmail.com>
2022-10-28 17:32:54 +0300

    'Solution_#2'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 def copy_file(command: str) -> None:
     separate = command.split()
-    if separate[1] != separate[2]:
-        with open(separate[1], "r") as file_in,\
-                open(separate[2], "w") as file_out:
-            file_new = file_in.read()
-            file_out.write(file_new)
+    if separate[1] == separate[2]:
+        return
+    with open(separate[1], "r") as file_in, open(separate[2], "w") as file_out:
+        file_new = file_in.read()
+        file_out.write(file_new)
+
+
+if __name__ == "__main__":
+    copy_file("cp file.txt file-copy.txt")
