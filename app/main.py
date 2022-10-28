5e9d8e49ff71d6a5e976f91345a035b59b922c4b
Oleksandr Kolomiiets <jumanje82@gmail.com>
2022-10-28 17:44:52 +0300

    'maybe_so'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
-    separate = command.split()
-    if separate[1] == separate[2]:
-        return
-    with open(separate[1], "r") as file_in, open(separate[2], "w") as file_out:
-        file_new = file_in.read()
-        file_out.write(file_new)
+    separate = command.split(" ")
+    if separate[0] == "cp" and separate[1] != separate[2]:
+        with open(separate[1], "r") as file_in,\
+                open(separate[2], "w") as file_out:
+            file_new = file_in.read()
+            file_out.write(file_new)
 
 
 if __name__ == "__main__":
