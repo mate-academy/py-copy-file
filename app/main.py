d4578f53f4ec66409a69581145a118b3534d68e6
Dede <specialized8393@gmail.com>
2022-10-30 13:45:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str):
+    if not "cp" in command:
+        print("Does nothing")
+        return
+    if command.split()[1] == command.split()[2]:
+        print("Does nothing")
+        return
+    file_1, file_2 = command.split()[1], command.split()[2]
+
+    with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
+        file_out.write(file_in.read())
+
+
+copy_file("cp file.txt new_file.txt")
