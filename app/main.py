84032d89fc5792eb80dacc1cc36a07005647276a
Marina Bidenko <bidenko.bmw@gmail.com>
2022-11-01 19:09:25 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    file, new_file = command.split()[1:]
+
+    if file == new_file:
+        print("Nothing changed")
+        return
+    else:
+        with open(file, "r") as inp, open(new_file, "w") as out:
+            out.write(inp.read())
+
