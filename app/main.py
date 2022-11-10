efb7b19f06b99463a1812dd4d9d02ce0d02a9265
Nazar Dmytrenko <nazarkadm@gmail.com>
2022-11-10 10:44:30 +0200

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ def copy_file(command: str) -> None:
 
     if cp != "cp" or command.split()[1] == command.split()[2]:
         print("Does nothing")
+        return
 
     with open(command.split()[1], "r") as file_in,\
             open(command.split()[2], "w") as file_out:
