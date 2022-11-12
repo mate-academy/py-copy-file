2bb1e091fef1478894ca99caf498b2373ed560ff
Igor <igornev83@gmail.com>
2022-11-12 23:34:34 +0200

    Without test

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_list = command.split()
+    if file_list[0] != "cp":
+        print("Incorrect command")
+    if file_list[1] == file_list[2]:
+        pass
+    with open(file_list[1], "w+") as file_in, \
+         open(file_list[2], "w") as file_out:
+        save_data = file_in.read()
+        file_out.write(save_data)
