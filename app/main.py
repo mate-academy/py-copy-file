6b93dbbc550c20746be33d917e009fc5a3d6a38a
Igor <igornev83@gmail.com>
2022-11-14 16:30:39 +0200

    Included check of command in program run

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
     file_list = command.split()
-    if file_list[0] != "cp":
-        print("Incorrect command")
-    if file_list[1] != file_list[2]:
-        with open(file_list[1], "w+") as file_in, \
-             open(file_list[2], "w") as file_out:
-            save_data = file_in.read()
-            file_out.write(save_data)
+    if file_list[0] == "cp":
+        if file_list[1] != file_list[2]:
+            with open(file_list[1], "w+") as file_in, \
+                 open(file_list[2], "w") as file_out:
+                save_data = file_in.read()
+                file_out.write(save_data)
+    print("Incorrect command")
