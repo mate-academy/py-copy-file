22d757dcc2a234e4206f95849ed5bc0b52b24a3d
Ihor Ivashov <dm-i@i.ua>
2022-11-14 12:00:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    ls = command.split()
+    if len(ls) == 3:
+        with open(ls[1], "r") as file_in, open(ls[2], "w") as file_out:
+            file_out.writelines(file_in.readlines())
