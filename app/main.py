22730ef8b5d99eb12a29909eba3864e8fe8b3e94
Ihor Ivashov <dm-i@i.ua>
2022-11-14 14:22:34 +0200

    Fixed condition

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 def copy_file(command: str) -> None:
-    ls = command.split()
-    if len(ls) == 3:
-        with open(ls[1], "r") as file_in, open(ls[2], "w") as file_out:
+    command_ls = command.split()
+    if len(command_ls) == 3 and \
+       command_ls[0] == "cp" and \
+       command_ls[1] != command_ls[2]:
+        with open(command_ls[1], "r") as file_in, \
+             open(command_ls[2], "w") as file_out:
             file_out.writelines(file_in.readlines())
