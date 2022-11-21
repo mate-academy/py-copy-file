c4706ff8962648003d683f38144eb2e6ec110171
Ivan Mayor <maramorosh2@gmail.com>
2022-11-21 17:04:28 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    splited_command = command.split()
+
+    if splited_command[0] == "cp" and splited_command[1] != splited_command[2]:
+        with open(splited_command[1], "r") as \
+                old_file, open(splited_command[2], "w") as new_file:
+            read_file = old_file.read()
+            new_file.write(read_file)
+
+    elif command[1] == command[2]:
+        raise Exception("Names are equal!")
