54b75bdc9102fc2e04c2cb48e5a831ac49fd016f
Ed Olentsevych <ed.olentsevych@gmail.com>
2022-11-22 19:11:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    if command[1] == command[2]:
+        return
+    with open(command[1], "r") as source_file, \
+            open(command[2], "w") as file_for_copy:
+        for line in source_file:
+            file_for_copy.write(line)
