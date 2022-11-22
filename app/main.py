93761fc4271d4ec7a62b4a2892671e27d87d7250
Ivan Mayor <maramorosh2@gmail.com>
2022-11-22 15:32:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
-def copy_file(command: str) -> None:
-    splited_command = command.split()
+class EqualNamesError(Exception):
+    pass
 
-    if splited_command[0] == "cp" and splited_command[1] != splited_command[2]:
-        with open(splited_command[1], "r") as \
-                old_file, open(splited_command[2], "w") as new_file:
+
+def copy_file(command: str) -> None:
+    command, file, new_file = command.split()
+
+    if file == new_file:
+        raise EqualNamesError("File names have not to be equal!!!")
+
+    elif command == "cp" and file != new_file:
+        with open(file, "r") as \
+                old_file, open(new_file, "w") as new_file:
             read_file = old_file.read()
             new_file.write(read_file)
-
-    elif command[1] == command[2]:
-        raise Exception("Names are equal!")
