f4b5707e21f980678666436089db7d6f43cefd55
Ed Olentsevych <ed.olentsevych@gmail.com>
2022-11-23 19:54:07 +0200

    Updated solution: added check of cp

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    if command[1] == command[2]:
+    if command[0] != "cp" or command[1] == command[2]:
+        print("error_0")
         return
     with open(command[1], "r") as source_file, \
             open(command[2], "w") as file_for_copy:
