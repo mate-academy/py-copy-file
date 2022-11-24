8bb90e821108aeafac0c77c88c700c1756dbb7a1
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2022-11-24 12:00:05 +0100

    Solution with command checking

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 def copy_file(command: str) -> None:
     command_list = command.split()
+    command_name = command_list[0]
     file_1 = command_list[1]
     file_2 = command_list[2]
 
-    if file_1 != file_2:
+    if file_1 != file_2 and command_name == "cp":
         with open(file_1, "r") as file_out, open(file_2, "a") as file_in:
             for copy_line in file_out.readlines():
                 file_in.write(f"{copy_line}\n")
