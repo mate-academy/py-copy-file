d7b9c3dc9a34ae053ed834a0379f3d7e94798246
Daria Hannochenko <soloha7965@gmail.com>
2022-12-01 13:53:40 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    names = command.split()
+    if names[1] == names[2]:
+        print("you can not create files with simular names")
+    else:
+        with open(names[1], "r") as file_in, open(names[2], "w") as file_out:
+            file_out.write(file_in.read())
