da68db8c830fe338807a4f39b8df2a7f48c8e3dd
Dmytro Bleichyk <dima.badiuk@gmail.com>
2022-12-02 19:49:11 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(line: str) -> None:
+    f_names = line.split(" ")
+    print(f_names)
+    if (f_names[0] == "cp") and (f_names[1] != f_names[2]):
+        with open(f_names[1], "r") as file_in, \
+                open(f_names[2], "w") as file_out:
+            file_out.write(file_in.read())
