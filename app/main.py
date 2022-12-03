489f08137669e3845acd31fe1d889d2245c10c79
Dmytro Bleichyk <dima.badiuk@gmail.com>
2022-12-03 22:44:39 +0200

    Variables unpacked

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy_file(line: str) -> None:
     com, file1, file2 = line.split()
     if (com == "cp") and (file1 != file2):
-        with open(file1, "r") as file_in, \
-                open(file2, "w") as file_out:
+        with open(file1, "r") as file_in, open(file2, "w") as file_out:
             file_out.write(file_in.read())
