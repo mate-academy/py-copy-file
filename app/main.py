5a5490e9e9802deded72e46a404f8a8a1afd36a4
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2022-12-06 22:08:05 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    command = command.split()
+    with open(command[1], "r"), open(command[2], "a"):
+        if f"{command[1]}" != f"{command[2]}" and command[0] == "cp":
+            shutil.copyfile(f"{command[1]}", f"{command[2]}")
