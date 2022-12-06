94a9b0a4c0e2f16c42a27fd3d5c92d87335e935b
Artem Motrunich <motrunich2013@gmail.com>
2022-12-06 17:04:05 +0100

    Solution_v2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_file(command: str) -> None:
-    com, old_file, new_file = command.split()
+    split_command = command.split()
+    old_file, new_file = split_command[1], split_command[2]
     if old_file != new_file:
         with open(old_file) as old, open(new_file, "w") as new:
             reader = old.read()
