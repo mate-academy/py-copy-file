f632e734866e48d30c2af6806882e5261d9598c0
Dmytro Bleichyk <dima.badiuk@gmail.com>
2022-12-07 13:50:31 +0200

    Used full names for variables

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(line: str) -> None:
-    com, file1, file2 = line.split()
-    if (com == "cp") and (file1 != file2):
-        with open(file1, "r") as file_in, open(file2, "w") as file_out:
+    command, old_file, new_file = line.split()
+    if (command == "cp") and (old_file != new_file):
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             file_out.write(file_in.read())
