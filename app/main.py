f2e4c0c706b9ff3933266d1d4f7ab61817829e5b
Ivan Berezyanskiy <65457374+Vanya2389@users.noreply.github.com>
2022-12-08 09:54:38 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
-        if command[0] != "cp" or command[1] == command[2]:
+    if command[0] != "cp" or command[1] == command[2]:
+        with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
             file_out.write(file_in.read())
