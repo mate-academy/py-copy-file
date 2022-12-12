79d3b92cf3a722472a1fc9abafd8ae144f9e4968
Artem Motrunich <motrunich2013@gmail.com>
2022-12-12 08:44:01 +0100

    Solution_v3(deleted "print")

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy_file(command: str) -> None:
     old_file, new_file = command.split()[1], command.split()[2]
-    print(old_file, new_file)
     if old_file != new_file:
         with open(old_file) as old, open(new_file, "w") as new:
             reader = old.read()
