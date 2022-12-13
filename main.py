302c10c56e15715bfca725c74f4446268b8e2a2d
MilManIT <104576473+MilManIT@users.noreply.github.com>
2022-12-13 21:32:03 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# write your code here
+def copy_file(command: str) -> None:
+
+    first_name, second_name = command.split()[1], command.split()[2]
+
+    if first_name != second_name:
+
+        with open(first_name, "r") as file_in, \
+                open(second_name, "w") as file_out:
+            file_out.write(file_in.read())
