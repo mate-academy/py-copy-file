d8b762edae4646eb906d6e3ef7fa3e595a7b471a
Tetiana Kozakova <kleotan901@gmail.com>
2022-12-14 20:03:09 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+import shutil
+
+
+def copy_file(string: str) -> None:
+    file_name = string.split(" ")[1]
+    file_name_copy = string.split(" ")[2]
+    with open(file_name, "r") as file_in, \
+            open(file_name_copy, "w") as file_out:
+        shutil.copyfileobj(file_in, file_out)
