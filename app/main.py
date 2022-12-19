b74ab329d2749bcc3738e6bbe61bb588c5824b43
NVMBR13 <108105455+NVMBR13@users.noreply.github.com>
2022-12-19 19:35:30 +0200

    devd

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cmnd = command.split()
+    if cmnd[1] == cmnd[2]:
+        return
+    with open(cmnd[1], "r") as file_in, open(cmnd[2], "w") as file_out:
+        file_out.write(file_in.read())
