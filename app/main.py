6a84db523e7d0ae940da2b1be2fd1894c07c900a
NVMBR13 <108105455+NVMBR13@users.noreply.github.com>
2022-12-19 20:55:59 +0200

    dev1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    if command[1] == command[2]:
+    if command[1] == command[2] or command[0] != "cp":
         return
     with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
         file_out.write(file_in.read())
