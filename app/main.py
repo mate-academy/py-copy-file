80dd6639fe873021e6720fe4cca3f613bc741a4b
Haishuk Pavlo <xomas@ukr.net>
2022-12-22 18:56:47 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = (command[3:-1] + "t").split(" ")
+    name_to_search = split_command[0]
+    name_to_copy = split_command[1]
+    if name_to_copy == name_to_search:
+        return
+    with open(f"{name_to_search}", "r") as source:
+        with open(f"{name_to_copy}", "w") as receiver:
+            receiver.write(source.read())
