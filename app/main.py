e845462558dace366b9381671e6a9ad01dc0a020
Haishuk Pavlo <xomas@ukr.net>
2022-12-24 00:48:55 +0200

    redone

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 def copy_file(command: str) -> None:
-    split_command = (command[3:-1] + "t").split(" ")
-    name_to_search = split_command[0]
-    name_to_copy = split_command[1]
+    split_command = command.split(" ")
+    if command[0] != "cp":
+        raise NameError("The command does not exists")
+    name_to_search = split_command[1]
+    name_to_copy = split_command[2]
     if name_to_copy == name_to_search:
         return
     with open(f"{name_to_search}", "r") as source:
