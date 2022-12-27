cb23400764c0f5604c54c15c312e5a1f40d25343
oleg-potichnyi <potichnyi.oleg@gmail.com>
2022-12-27 12:52:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    sours_fl, new_fl = command.split()[0], command.split()[2]
+    if sours_fl != new_fl:
+        with open(sours_fl, "r") as file_in, open(new_fl, "w") as file_out:
+            file_out.write(file_in.read())
