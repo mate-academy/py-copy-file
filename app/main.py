4e16452324ea0f569809f3d410407d5bd031d30c
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-12-27 22:28:12 +0200

    fixed_split

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_type, source, destination = command.split()
+    if command_type == "cp" and source != destination:
+        with open(source, "r") as file_in, open(destination, "w") as file_out:
+            file_out.write(file_in.read())
