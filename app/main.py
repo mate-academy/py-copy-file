9e331b386f3cc2059ce9cb66523715e12a4f43a1
oleg-potichnyi <potichnyi.oleg@gmail.com>
2022-12-27 18:58:15 +0200

    Unpacking done

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_file(command: str) -> None:
-    sours_fl, new_fl = command.split()[0], command.split()[2]
+    cp, sours_fl, new_fl = \
+        command.split()[0], command.split()[1], command.split()[2]
     if sours_fl != new_fl:
         with open(sours_fl, "r") as file_in, open(new_fl, "w") as file_out:
             file_out.write(file_in.read())
