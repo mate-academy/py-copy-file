a7b3269fa53ce7aaa6f82c8b58fde61d103bf02c
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2022-12-28 15:06:28 +0200

    Solution_2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 def copy_file(command: str) -> None:
-    command_list = command.split()
-    with open(f"{command_list[1]}", "r") as file_in,\
-            open(f"{command_list[2]}", "w") as file_out:
-        if command_list[1] != command_list[2]:
+    _, src, dst = command.split()
+    with open(f"{src}", "r") as file_in, \
+            open(f"{dst}", "w") as file_out:
+        if src != dst:
             file_out.write(file_in.read())
+
+
+copy_file("cp file.txt file.txt")
