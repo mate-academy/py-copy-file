19859f19da848356f60a9effe9ff8ec6acffb138
oleg-potichnyi <potichnyi.oleg@gmail.com>
2022-12-29 16:36:10 +0200

    Made corrections

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
     cp, sours_fl, new_fl = command.split()
-    if sours_fl != new_fl:
+    if sours_fl != new_fl and "cp":
         with open(sours_fl, "r") as file_in, open(new_fl, "w") as file_out:
             file_out.write(file_in.read())
