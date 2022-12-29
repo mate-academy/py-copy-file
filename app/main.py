c77d7f17d734cb6722daea46362a232fcaf6b76e
Alexander Tsvirko <115401096+AlexanderTsvirko@users.noreply.github.com>
2022-12-29 16:38:17 +0100

    Update app/main.py
    Co-authored-by: Ivan Ramyk <ivan.ramyk@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    file_in_name, file_out_name = command.lstrip("cp").split()
+    command_type, file_in_name, file_out_name = command.split()
     if file_in_name != file_out_name:
         with open(file_in_name, "r") as file_in, \
                 open(file_out_name, "w") as file_out:
