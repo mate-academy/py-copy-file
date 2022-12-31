d52817470cebea8ca094eaa4435995c2e7c26394
Vitalii Panochko <vpapplic@gmail.com>
2022-12-31 11:02:40 +0200

    remove except

--- app/main.py
+++ app/main.py
@@ -4,11 +4,8 @@ class CommandNotFound(Exception):
 
 def copy_file(command: str) -> None:
     elements_of_command = command.split()
-    try:
-        if elements_of_command[0] != "cp":
-            raise CommandNotFound
-    except CommandNotFound("You must enter a valid command"):
-        raise
+    if elements_of_command[0] != "cp":
+        raise CommandNotFound("You must enter a valid command")
 
     with open(elements_of_command[1], "r") as file_in, \
             open(elements_of_command[2], "w") as file_out:
