786067a86072023c2ad3382c6674d651983a5b06
Anna Kabatova <kabatova.anna@ukr.net>
2023-01-01 19:22:09 +0200

    2nd try solution (copy file)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(command: str) -> None:
     command_ls = command.split()
-    file_old = command_ls[1]
-    file_new = command_ls[2]
-    if file_old != file_new:
+    file_old, file_new = command_ls[1], command_ls[2]
+    if file_old != file_new and command_ls[0] == "cp":
         with open(file_old, "r") as file_in, open(file_new, "w") as file_out:
             file_out.write(file_in.read())
