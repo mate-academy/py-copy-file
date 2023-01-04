d81ae384d786e903874aad9b772dff8a69192743
IhorPokr <pokroveckijigor@gmail.com>
2023-01-04 15:42:25 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 def copy_file(command: str) -> None:
     cp, file, file_copy = command.split()
-    if cp != "cp":
+    if cp == "cp" and file != file_copy:
         return
-    if file == file_copy:
-        return
-    with open(file, "r") as file_in, open(file, "w") as file_out:
+    with open(file, "r") as file_in, open(file_copy, "w") as file_out:
         for line in file_in:
             file_out.write(line)
