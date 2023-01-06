2b7659be458b245f321e91aea0a4b6058f402afd
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-01-06 21:14:33 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    name_list = command.split(" ")
+    if name_list[1] != name_list[2]:
+        with open(name_list[1], "r") as file_get:
+            info = file_get.read()
+        with open(name_list[2], "w") as file_write:
+            file_write.write(info)
