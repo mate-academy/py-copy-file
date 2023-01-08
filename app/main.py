b3d10836bc7ba196591d51309d14fe37ebbd328f
Yuliia Stopkyna <styopkinayuliya@gmail.com>
2023-01-08 14:39:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
+    short_command = command.split()[0]
     original = command.split()[1]
     copy = command.split()[2]
-    if original == copy:
+    if short_command != "cp" or original == copy:
         return
     with open(original, "r") as original, open(copy, "w") as copy:
         copy.write(original.read())
