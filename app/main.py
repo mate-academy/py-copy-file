070001f212be29d6567c009cf80565cc670a0f2b
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-01-12 18:29:37 +0200

    change writing to file

--- app/main.py
+++ app/main.py
@@ -4,5 +4,4 @@ def copy_file(command: str) -> None:
     if file1_name != file2_name and cmd == "cp":
         with open(f"{file1_name}", "r") as file_in, \
                 open(f"{file2_name}", "a") as file_out:
-            for line in file_in:
-                file_out.write(line)
+            file_out.writelines(file_in.readlines())
