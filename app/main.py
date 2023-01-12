6bd38d98a3621a5c9476678617c3e1951a9c883c
Serhii Skivka <skivkas.v@gmail.com>
2023-01-12 18:11:25 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 # write your code here
 def copy_file(command: str) -> None:
-    from_command = command.split()
-    file_name = from_command[1]
-    new_file_name = from_command[2]
-    if file_name != new_file_name:
-        with open(file_name, "r") as file_in, \
-                open(new_file_name, "w") as file_out:
-            content = file_in.read()
-            file_out.write(content)
+    cp, file, file_copy = command.split()
+    if cp != "cp":
+        return
+    if file != file_copy:
+        with open(file, "r") as file_in, \
+                open(file_copy, "w") as file_out:
+            for line in file_in:
+                file_out.write(line)
