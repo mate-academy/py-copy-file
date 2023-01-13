bf08aa4f0c36b6c876916be3a2fc0f1f99c4eb53
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-01-13 19:13:54 +0200

    Fix variables and condition

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(command: str) -> None:
-    origin_file = command.split()[1]
-    file_copy = command.split()[2]
-    if origin_file != copy_file:
-        with open(origin_file, "r") as origin, open(file_copy, "w") as copy_f:
-            for line in origin:
-                copy_f.write(line)
+    cp, origin, file_copy = command.split()
+    if origin != file_copy:
+        with open(origin, "r") as file, open(file_copy, "w") as new_file:
+            for line in file:
+                new_file.write(line)
