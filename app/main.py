edf5c6d999dd860845da70a5dbaf92190a93cd2b
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-01-13 18:30:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ def copy_file(command: str) -> None:
     cmd, file1_name, file2_name = command.split()
 
     if file1_name != file2_name and cmd == "cp":
-        with open(f"{file1_name}", "r") as file_in, \
-                open(f"{file2_name}", "a") as file_out:
+        with (
+            open(file1_name, "r") as file_in, open(file2_name, "w") as file_out
+        ):
             file_out.writelines(file_in.readlines())
