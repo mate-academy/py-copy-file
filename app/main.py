416d55544c1577c81e24a03416bfc6955f44e593
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-01-13 22:18:18 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     command_name, original_file, new_file = command.split()
-    if command_name != "cp" and original_file != new_file:
+    if command_name == "cp" and original_file != new_file:
         with open(original_file, "r") as file_get, \
                 open(new_file, "w") as file_write:
             info = file_get.read()
