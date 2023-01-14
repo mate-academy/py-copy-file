1fb95be9e58cbb8f2f9e577de81de8dfe282832a
drapail <mv.dibrov@gmail.com>
2023-01-14 14:39:24 +0100

    implemented solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
 # write your code here
+def copy_file(command: str) -> None:
+    command_index = 0
+    source_file_index = 1
+    target_file_index = 2
+
+    if command.split()[command_index] != "cp":
+        return
+
+    source_file = command.split()[source_file_index]
+    target_file = command.split()[target_file_index]
+
+    if source_file == target_file:
+        return
+
+    with open(source_file, "r") as source, open(target_file, "w") as target:
+        target.write(source.read())
