ddd39491556423d54761ca89332410d6dcbc2252
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-01-15 18:07:06 +0200

    Check command correctness

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_file(command: str) -> None:
     cp, origin, file_copy = command.split()
-    if origin != file_copy:
-        with open(origin, "r") as file, open(file_copy, "w") as new_file:
-            new_file.write(file.read())
+    if cp == "cp":
+        if origin != file_copy:
+            with open(origin, "r") as file, open(file_copy, "w") as new_file:
+                new_file.write(file.read())
