ffa70efc1d07f28c8094634fe9f0e7c1ae7c1325
yura.vataschuk <yura.vataschuk@gmail.com>
2023-01-16 20:38:20 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 def copy_file(command: str) -> None:
-    file_in = command.split(" ")[1]
-    file_out = command.split(" ")[2]
-    with open(file_in, "r") as file_to_copy, \
-            open(file_out, "a+") as copy_of_file:
-        lines = file_to_copy.readlines()
-        for line in lines:
-            print(line.strip())
-            copy_of_file.write(line)
+    input_command, file_in, file_out = command.split()
+    if command == "cp":
+        with (
+            open(file_in, "r") as file_to_copy,
+            open(file_out, "a+") as copy_of_file
+        ):
+            lines = file_to_copy.readlines()
+            for line in lines:
+                copy_of_file.write(line)
