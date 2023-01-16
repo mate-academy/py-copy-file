7a64a140983794e7d8b49e112a91eb504d15c4fe
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-01-16 16:49:41 +0200

    Rewrite condition

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy_file(command: str) -> None:
     cp, origin, file_copy = command.split()
-    if cp == "cp":
-        if origin != file_copy:
-            with open(origin, "r") as file, open(file_copy, "w") as new_file:
-                new_file.write(file.read())
+    if cp == "cp" and origin != file_copy:
+        with open(origin, "r") as file, open(file_copy, "w") as new_file:
+            new_file.write(file.read())
