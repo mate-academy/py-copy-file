7263be8082cbeaad23108fcf960b8ce954a1b931
Dmytro Prokofiev <forbuffa@gmail.com>
2023-01-17 21:54:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp, current_file, copied_file = command.split()
+    with open(f"{current_file}.txt", "r") as file_in, \
+            open(f"{copied_file}.txt", "w") as file_out:
+        file_out.write(file_in.read())
