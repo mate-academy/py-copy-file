d8501e8b85b3b6be7f05f75a5fd8b1b0fd9ee764
yura.vataschuk <yura.vataschuk@gmail.com>
2023-01-18 13:42:57 +0200

    added file names check

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     input_command, file_in, file_out = command.split()
-    if command == "cp":
+    if command == "cp" and file_in != file_out:
         with (
             open(file_in, "r") as file_to_copy,
             open(file_out, "a+") as copy_of_file
