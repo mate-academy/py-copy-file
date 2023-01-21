76139bde815bb0997b0ac483705bf0b255366a74
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-01-21 22:20:11 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_names = command[3:].split(" ")
+    if file_names[0] != file_names[1]:
+        with open(file_names[0],
+                  "r") as file_in, open(file_names[1], "w") as file_out:
+            file_out.write(file_in.read())
