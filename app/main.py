a8be52f22530476307c08a5055e6f2ceec8b532b
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-01-22 16:22:16 +0100

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
+import os
+
+
 def copy_file(command: str) -> None:
-    file_names = command[3:].split(" ")
-    if file_names[0] != file_names[1]:
-        with open(file_names[0],
-                  "r") as file_in, open(file_names[1], "w") as file_out:
+    try:
+        cp, current_file, copied_file = command.split()
+    except ValueError:
+        raise ValueError("Command is incorrect")
+    if cp != "cp" or os.path.exists(current_file) is False:
+        raise ValueError("Command is incorrect")
+    if current_file != copied_file:
+        with open(current_file,
+                  "r") as file_in, open(copied_file, "w") as file_out:
             file_out.write(file_in.read())
