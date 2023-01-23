80b2ef327bb5b1aaff07d56927164319748c1a00
sergk <srx92@protonmail.com>
2023-01-23 21:31:21 +0200

    fix v.3

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
-    cmd, source, target = command.split(" ")
-    if cmd == "cp" and len(command.split(" ")) == 3 \
-            and source != target:
-        with (
-            open(source, "r") as file1,
-            open(target, "w") as file2
-        ):
-            file2.write(file1.read())
+    if len(command.split(" ")) == 3:
+        cmd, source, target = command.split(" ")
+        if cmd == "cp" and source != target:
+            with (
+                open(source, "r") as src_file,
+                open(target, "w") as tg_file
+            ):
+                tg_file.write(src_file.read())
