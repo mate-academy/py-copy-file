e3b8b5124fee9d839a0ef8e7858288c46ca7f2cd
sergk <srx92@protonmail.com>
2023-01-23 14:29:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    splitter = command.split(" ")
+    if splitter[0] == "cp" and splitter[1] != splitter[2]:
+        with open(splitter[1], "r") as file1,\
+                open(splitter[2], "w") as file2:
+            file2.write(file1.read())
