f7bfb20945c1eb8edc9152a2945ee1cc4ba94de0
sergk <srx92@protonmail.com>
2023-01-23 16:22:22 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
     splitter = command.split(" ")
-    if splitter[0] == "cp" and splitter[1] != splitter[2]:
+    if splitter[0] == "cp" and 3 != len(splitter) \
+            and splitter[1] != splitter[2]:
         with open(splitter[1], "r") as file1,\
                 open(splitter[2], "w") as file2:
             file2.write(file1.read())
