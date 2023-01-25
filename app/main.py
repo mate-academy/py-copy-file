f58f3bfed61c1bb1eec4c66f7bc4fffee6e754be
Olena Liubynetska <olena.liuby@gmail.com>
2023-01-25 12:54:41 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
 # write your code here
+def copy_file(command: str) -> None:
+    try:
+        command_cp, file_name, file_copy_name = command.split()
+    except ValueError:
+        raise ValueError(
+            "Check if the command has the format: cp file.txt file_copy.txt"
+        )
+    if command_cp != "cp":
+        raise ValueError("The command must start with 'cp'")
+    if file_name == file_copy_name:
+        raise ValueError("The file copy must have different name")
+    with open(file_name, "r") as file_in, \
+            open(file_copy_name, "w") as file_out:
+        file_out.write(file_in.read())
