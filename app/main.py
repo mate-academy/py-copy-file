7360cadbdb3d35ec81ca18651bff739f40fbea5a
sergk <srx92@protonmail.com>
2023-01-25 13:24:30 +0200

    suppose, final fix

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 def copy_file(command: str) -> None:
     splitted = command.split()
     if len(splitted) == 3:
-        if splitted[0] == "cp" and splitted[1] != splitted[2]:
+        cmd, source, target = splitted
+        if cmd == "cp" and source != target:
             with (
-                open(splitted[1], "r") as source_file,
-                open(splitted[2], "w") as target_file
+                open(source, "r") as src_file,
+                open(target, "w") as tg_file
             ):
-                target_file.write(source_file.read())
+                tg_file.write(src_file.read())
