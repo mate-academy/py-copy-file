9fd80a1290f7222b4494f855186dc2177dd73bf9
Paul Maslov <maslovp67@gmail.com>
2023-01-26 01:12:26 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    result = command.split(" ")
+    if result[1] == result[2]:
+        return
+    with open(result[1], "r") as file_in, open(result[2], "w") as file_out:
+        file_out.write(file_in.read())
