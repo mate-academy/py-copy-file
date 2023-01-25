883c522c2a36abef04105ba7855e046bed0e5b35
Olena Liubynetska <olena.liuby@gmail.com>
2023-01-25 12:37:14 +0000

    Flake8 fix

--- app/main.py
+++ app/main.py
@@ -6,5 +6,6 @@ def copy_file(command: str) -> None:
     if command_cp != "cp":
         return None
     if file_name != file_copy_name:
-        with open(file_name, "r") as file_in, open(file_copy_name, "w") as file_out:
+        with open(file_name, "r") as file_in, \
+                open(file_copy_name, "w") as file_out:
             file_out.write(file_in.read())
