9469e6b685ec1f9c14ca1fe03c5255bbb140036e
Taras Savchyn <savik1992@gmail.com>
2023-01-26 14:38:49 +0200

    add block 'if'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 def copy_file(command: str) -> None:
     old_name, new_name = command.split()[1:]
-    with open(old_name, "r") as old_file, open(new_name, "w") as new_file:
-        new_file.write(old_file.read())
+    if old_name != new_name:
+        with open(old_name, "r") as old_file, open(new_name, "w") as new_file:
+            new_file.write(old_file.read())
