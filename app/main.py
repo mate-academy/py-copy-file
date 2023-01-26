837a44968c5392e79248f32cfcb2ec5b8598731d
Taras Savchyn <savik1992@gmail.com>
2023-01-26 08:48:52 +0200

    in one line

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 def copy_file(command: str) -> None:
     old_file_name, new_file_name = command.split()[1:]
-    with open(old_file_name, "r") as old_file:
-        context = old_file.read()
-    with open(new_file_name, "w") as new_file:
-        new_file.write(context)
+    with open(old_file_name, "r") as old_file, open(new_file_name, "w") as new_file:
+        new_file.write(old_file.read())
