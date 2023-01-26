faee7156084111c9e492adf95b14ef069f1fc59b
Taras Savchyn <savik1992@gmail.com>
2023-01-26 08:41:40 +0200

    add type anotation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def copy_file(command: str):
+def copy_file(command: str) -> None:
     old_file_name, new_file_name = command.split()[1:]
     with open(old_file_name, "r") as old_file:
         context = old_file.read()
