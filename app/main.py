e4933991aa9612bbf34b38e015a6f154fff23a09
Olena Liubynetska <olena.liuby@gmail.com>
2023-01-26 14:48:45 +0000

    Fixed variables naming & code style

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
-# write your code here
 def copy_file(command: str) -> None:
     try:
-        command_cp, file_name, file_copy_name = command.split()
+        input_command, input_file_name, output_file_name = command.split()
     except ValueError:
         raise ValueError(
             "Check if the command has the format: cp file.txt file_copy.txt"
         )
-    if command_cp != "cp":
+    if input_command != "cp":
         raise ValueError("The command must start with 'cp'")
-    if file_name == file_copy_name:
+    if input_file_name == output_file_name:
         raise ValueError("The file copy must have different name")
-    with open(file_name, "r") as file_in, \
-            open(file_copy_name, "w") as file_out:
+    with (
+        open(input_file_name, "r") as file_in,
+        open(output_file_name, "w") as file_out
+    ):
         file_out.write(file_in.read())
