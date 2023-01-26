025a85b5b918466b595eef7d75a3469f08d2a8d2
Taras Savchyn <savik1992@gmail.com>
2023-01-26 08:51:05 +0200

    rename variable

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 def copy_file(command: str) -> None:
-    old_file_name, new_file_name = command.split()[1:]
-    with open(old_file_name, "r") as old_file, open(new_file_name, "w") as new_file:
+    old_name, new_name = command.split()[1:]
+    with open(old_name, "r") as old_file, open(new_name, "w") as new_file:
         new_file.write(old_file.read())
