e06d0bc661c1a99e31f51b36f01d731d01db474d
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-01-28 16:15:40 +0200

    add command verification

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 import shutil
 
 
+class CommandTypeError(Exception):
+    pass
+
+
 def copy_file(command: str) -> None:
     command, input_file, output_file = command.split(" ")
+    if command != "cp":
+        raise CommandTypeError("Check input command")
     shutil.copyfile(input_file, output_file)
