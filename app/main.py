78ca37fe2518d5edbcb1a530601f7c2cb9d9e199
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-01-28 12:36:45 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    command, input_file, output_file = command.split(" ")
+    shutil.copyfile(input_file, output_file)
