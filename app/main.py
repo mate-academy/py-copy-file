51e82aff3ca6ec124d443cf435f7a4f880e3c8cd
Andii Rashevskyi <phoenixerazer@gmail.com>
2023-01-29 21:01:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 def copy_file(command: str) -> None:
-    cp, original_f, copy_f = command.split()
-    if cp == "cp" and original_f != copy_f:
-        with open(original_f, "r") as file_in, open(copy_f, "w") as file_out:
-            file_out.write(file_in.read())
+    if len(command.split()) != 3:
+        return print("Command length is not equal to 3")
+
+    action, original_file, new_file = command.split()
+    if action != "cp" or original_file == new_file:
+        return print("Command entered incorrectly")
+
+    with open(original_file, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
