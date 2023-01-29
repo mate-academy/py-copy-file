86846375a574a369443c0a4fd244f8bd954053e6
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-01-29 12:08:42 +0200

    fix style

--- app/main.py
+++ app/main.py
@@ -6,5 +6,6 @@ def copy_file(command: str) -> None:
     command = command.split()
     if len(command) != 3 or command[0] != "cp":
         raise CommandError("Check input command")
-    with open(command[1], "r") as input_file, open(command[2], "w") as output_file:
+    with (open(command[1], "r") as input_file,
+          open(command[2], "w") as output_file):
         output_file.write(input_file.read())
