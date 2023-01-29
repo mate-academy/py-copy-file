07c89c65476f730d9cb5bba26078037fd3ef5ac4
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-01-29 11:55:39 +0200

    rework solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
-import shutil
-
-
 class CommandError(Exception):
     pass
 
 
 def copy_file(command: str) -> None:
-    try:
-        command, input_file, output_file = command.split(" ")
-    except ValueError:
-        raise CommandError("Check command arguments")
-    if command != "cp":
+    command = command.split()
+    if len(command) != 3 or command[0] != "cp":
         raise CommandError("Check input command")
-    shutil.copyfile(input_file, output_file)
+    with open(command[1], "r") as input_file, open(command[2], "w") as output_file:
+        output_file.write(input_file.read())
