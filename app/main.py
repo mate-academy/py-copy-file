773999bf808df3cb8c0f954b4dd18f012d179066
Taras Savchyn <savik1992@gmail.com>
2023-01-30 09:37:00 +0200

    added a check

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_file(command: str) -> None:
     command, old_name, new_name = command.split()
-    if old_name != new_name and command == "cp":
+    if (old_name != new_name and command == "cp"
+            and "." in old_name and "." in new_name):
         with open(old_name, "r") as old_file, open(new_name, "w") as new_file:
             new_file.write(old_file.read())
