ed1b95e648baac0919df32bb2137356f82e45acf
Illia Smyrnov <ilushasm@gmail.com>
2023-01-31 23:13:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    comm_list = command.split(" ")
+
+    if comm_list[0].strip() != "cp" \
+            or comm_list[1].strip() == comm_list[2].strip():
+        return
+
+    with open(comm_list[1], "r") as file_old, open(comm_list[2], "w") \
+            as file_new:
+        file_new.write(file_old.read())
