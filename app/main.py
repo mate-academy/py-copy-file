65641631d3c9dedff1e911c9f48a942f1bdf7704
Illia Smyrnov <ilushasm@gmail.com>
2023-02-01 17:35:46 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ def copy_file(command: str) -> None:
     comm_list = command.split(" ")
 
     if comm_list[0].strip() != "cp" \
-            or comm_list[1].strip() == comm_list[2].strip():
+            or comm_list[1].strip() == comm_list[2].strip() \
+            or len(comm_list) < 3:
         return
 
     with open(comm_list[1], "r") as file_old, open(comm_list[2], "w") \
