3ce5d495b419478f9eaeb9d53154073502ce20c9
Vitalii <fahivchik1@gmail.com>
2023-02-02 09:23:07 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    command = command.split(" ")
+    if command[1] == command[2]:
+        print("Does nothing")
+        return
+
+    with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+        data = file_in.read()
+        file_out.write(data)
