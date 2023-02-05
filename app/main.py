3130b0dbca9c9d9a283b52a7ebd5fe970e216421
Andriy <andriykrav@gmail.com>
2023-02-05 20:00:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> str:
+    file_1, file_2 = command.split()[1], command.split()[2]
+    with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
+        if file_in == file_out:
+            pass
+        return shutil.copyfile(file_1, file_2)
+
+
+# copy_file("cp file1.txt file2.txt")
