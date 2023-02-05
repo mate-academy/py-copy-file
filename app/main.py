e047315f5ea02f2fee54b77488eb2fee4e17d97e
Sergii Bushinskiy <116838006+Sergey-Bushinskiy@users.noreply.github.com>
2023-02-05 19:13:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ def copy_file(command: str) -> None:
     copy_command = command[0]
     source_file = command[1]
     target_file = command[2]
-    if len(command) == 3 and \
-            ("cp" in copy_command and source_file != target_file):
-        with open(f"{source_file}", "r") as file_in, \
-                open(f"{target_file}", "w") as file_out:
-            file_out.write(file_in.read())
+    if len(command) == 3:
+        if "cp" in copy_command and source_file != target_file:
+            with open(f"{source_file}", "r") as file_in:
+                with open(f"{target_file}", "w") as file_out:
+                    file_out.write(file_in.read())
