564bf1288e9ebabbb079eb811192ea82c87cc7be
Andriy <andriykrav@gmail.com>
2023-02-06 20:58:44 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
-import shutil
+def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        raise TypeError(f"the command should have 3 arguments,"
+                        f" got {len(command.split())} instead")
+    cp_command = command.split()[0]
+    if "cp" not in command.split():
+        raise TypeError(f"the command should have 'cp' keyword, "
+                        f"got '{cp_command}' instead")
+    source_f, result_f = command.split()[1], \
+        command.split()[2]
+    if source_f == result_f:
+        raise TypeError(f"{source_f} and {result_f} are the same file")
+    else:
+        with open(source_f,"r") as source, open(result_f, "w") as result:
+            for line in source:
+                result.write(line)
 
-
-def copy_file(command: str) -> str:
-    file_1, file_2 = command.split()[1], command.split()[2]
-    with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
-        if file_in == file_out:
-            pass
-        return shutil.copyfile(file_1, file_2)
-
-
-# copy_file("cp file1.txt file2.txt")
