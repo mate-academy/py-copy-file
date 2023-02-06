88a73a694bc6bacab13a8cada7b7c5c3643f1855
Vitalii <fahivchik1@gmail.com>
2023-02-06 14:10:17 +0200

    'Solution4'

--- app/main.py
+++ app/main.py
@@ -4,9 +4,13 @@ def copy_file(command: str) -> None:
         return "wrong command"
 
     command, old_file, new_file = command.split(" ")
+
     if old_file == new_file:
         return
 
-    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-        data = file_in.read()
-        file_out.write(data)
+    if old_file.endswith(".txt") and new_file.endswith(".txt"):
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            data = file_in.read()
+            file_out.write(data)
+    else:
+        return "wrong command"
