ed53ea1ded2dec9b8a4ccf1d7096cae7d21c1588
Vyacheslav Kungin <acheros4@gmail.com>
2023-02-06 14:19:13 +0200

    copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    text = command.split()
+    if text[1] == text[2]:
+        return
+
+    with (
+        open(text[1], "r") as file_in,
+        open(text[2], "w") as file_copy
+    ):
+        file_copy.write(file_in.read())
