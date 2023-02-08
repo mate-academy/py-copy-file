e775928482e566df2e04a7969c5c91f05b6384d8
Andriy <andriykrav@gmail.com>
2023-02-08 17:32:54 +0200

    Solution4

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 def copy_file(command: str) -> None:
+    cmd, source_f, result_f = command.split()
     if len(command.split()) != 3:
         raise ValueError(f"the command should have 3 arguments,"
                          f" got {len(command.split())} instead")
-    if "cp" not in command.split():
+    if cmd != "cp":
         raise TypeError("the command should have 'cp' keyword, "
-                        "got '%s' instead" % command.split()[0])
-    source_f, result_f = command.split()[1], \
-        command.split()[2]
+                        "got '%s' instead" % cmd)
     if source_f == result_f:
         raise TypeError(f"{source_f} and {result_f} are the same file")
     with open(source_f, "r") as source, open(result_f, "w") as result:
