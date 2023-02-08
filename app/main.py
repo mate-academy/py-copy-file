9188ff6e2d435e6f2b651c6f98e93291daa42306
Vitalii <fahivchik1@gmail.com>
2023-02-08 11:20:10 +0200

    'Solution6'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
     """Copies the contents of one file to another"""
 
-    # checking for correct command structure
-    if command.count(" ") > 2 or command.count(" ") < 2:
+    # we check the command structure
+    if len(command.split()) != 3:
         return "wrong command"
 
     # separate the command and file names
@@ -10,14 +10,12 @@ def copy_file(command: str) -> None:
 
     # check that the file names do not match
     if old_file == new_file:
-        return
+        return "the file names do match"
 
-    # check whether the file names and commands are specified correctly
-    if old_file.endswith(".txt") \
-            and new_file.endswith(".txt") \
-            and command == "cp":
+    # checking if the command name is correct
+    if command == "cp":
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             data = file_in.read()
             file_out.write(data)
     else:
-        return "wrong command"
+        return f"unknown command '{command}'"
