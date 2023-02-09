69826f116dfbb1abeb7e19506d217902fbd06d23
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-02-09 23:31:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    copied_data = None
-    with open(command[1], "r") as file_to_copy:
-        copied_data = file_to_copy.read()
-    with open(command[2], "w") as new_file:
-        new_file.write(copied_data)
+    with (
+        open(command[1], "r") as file_to_copy,
+        open(command[2], "w") as new_file
+    ):
+        if file_to_copy.name != new_file.name:
+            new_file.write(file_to_copy.read())
