69fe5d6d0cc1b95c675ddc75e55970c5577566a5
Zoriana Dvulit <zdvulit@ukr.net>
2023-02-10 22:21:23 +0200

    New New Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     _, copied_file, file_to_copy = command.split()
     if copied_file != file_to_copy:
-        with open(copied_file, "r") as file_in, \
-                open(file_to_copy, "w") as file_out:
+        with (open(copied_file, "r") as file_in,
+              open(file_to_copy, "w") as file_out):
             file_out.write(file_in.read())
