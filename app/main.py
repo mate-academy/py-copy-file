02f8770fc107d7ba82e52036bc0b5eb435827cd9
Viacheslav Shcherbak <misternictof@gmail.com>
2023-02-12 09:26:16 +0200

    Fix

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@
 def copy_file(command: str) -> None:
     command_type, source, destination = command.split()
     if command_type == "cp":
-        with open(source, "r") as file_in, \
-                open(destination, "w") as file_out:
+        with (
+                open(source, "r") as file_in,
+                open(destination, "w") as file_out
+        ):
             result = file_in.read()
             file_out.write(result)
