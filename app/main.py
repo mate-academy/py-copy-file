95c9398ab74ca12b4f52f17bee8f4b2525b01657
Dmytro <omelchenko.dm@gmail.com>
2023-02-12 18:28:33 +0200

    copy_file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
     cp, current_file, copied_file = command.split()
     if cp == "cp" and copied_file != current_file:
-        with open(f"{current_file}", "r") as file_in, \
-             open(f"{copied_file}", "w") as file_out:
+        with (
+            open(f"{current_file}", "r") as file_in,
+            open(f"{copied_file}", "w") as file_out
+        ):
             file_out.write(file_in.read())
