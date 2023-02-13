72d728164339d2f4908f5ba9c9e297ae4b1d1e2f
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-13 21:26:27 +0200

    make one action

--- app/main.py
+++ app/main.py
@@ -1,13 +1,7 @@
 def copy_file(command_line: str) -> None:
     command, old_file, new_file = command_line.split()
-    content = ""
 
-    if old_file == new_file:
-        return
-
-    if command == "cp":
+    if command == "cp" and old_file != new_file:
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-            # collect all text from old file to variable content
-            content += "".join(file_in.readlines())
-            file_out.writelines(content)
+            file_out.write(file_in.read())
             print(f"File {old_file} has been copied to the file {new_file}")
