3eb85116e9a33858a19c1835d9b97d49051dba0d
Vitalii <fahivchik1@gmail.com>
2023-02-13 14:51:35 +0200

    'Solution7'

--- app/main.py
+++ app/main.py
@@ -1,18 +1,12 @@
 def copy_file(command: str) -> None:
-    """Copies the contents of one file to another"""
-
-    # we check the command structure
     if len(command.split()) != 3:
         return "wrong command"
 
-    # separate the command and file names
     command, old_file, new_file = command.split(" ")
 
-    # check that the file names do not match
     if old_file == new_file:
         return "the file names do match"
 
-    # checking if the command name is correct
     if command == "cp":
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             data = file_in.read()
