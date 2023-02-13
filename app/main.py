59ecdb0d3e5c7cd0f99e7937f731150cf6dd52c0
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-13 20:16:18 +0200

    some fixes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 def copy_file(command_line: str) -> None:
-    command, old_file, new_file = command_line.split(" ")
+    command, old_file, new_file = command_line.split()
     content = ""
+
+    if old_file == new_file:
+        return
+
     if command == "cp":
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             # collect all text from old file to variable content
