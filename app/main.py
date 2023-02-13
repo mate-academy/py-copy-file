b7b885b5187b328e29236c40efa25c5b85b78869
bohdanlysuchenko <lysachenko111@ukr.net>
2023-02-13 21:51:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    ls_file = command.split()
+    if ls_file[1] != ls_file[2] and ls_file[0] == "cp":
+        with open(ls_file[1], "r") as old_file, open(ls_file[2], "w") as copy:
+            copy.write(old_file.read())
