4e7a15114f99f22e7709c0a6348e6b4ae667162c
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-13 13:35:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command_line: str) -> None:
+    command, old_file, new_file = command_line.split(" ")
+    content = ""
+    if command == "cp":
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            # collect all text from old file to variable content
+            content += "".join(file_in.readlines())
+            file_out.writelines(content)
+            print(f"File {old_file} has been copied to the file {new_file}")
