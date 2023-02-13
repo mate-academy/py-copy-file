cfc004f756b672728273b0ea2aa1ddaedc52d70b
dmytro <manjustice1998@gmail.com>
2023-02-13 12:54:24 +0200

    Changed cd to cp

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
     command, file_source, file_copy = command.split()
-    if command == "cd" and file_copy != file_source:
+    if command == "cp" and file_copy != file_source:
         with open(file_source, "r") as source, open(file_copy, "w") as copy:
             copy.write(source.read())
