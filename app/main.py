3a367f77e714fba5bfb2cca3bb92eb83f42baeba
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-02-14 21:53:01 +0200

    unpack items with one line, change variable names

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(command: str) -> None:
-    data = command.split()
-    if data[0] == "cp":
-        file_1 = data[1]
-        file_2 = data[2]
-        with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
-            if file_1 != file_2:
+    content = command.split()
+    command, old_file, new_file = content[0], content[1], content[2]
+    if command == "cp":
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            if old_file != new_file:
                 content_to_copy = file_in.read()
                 file_out.write(content_to_copy)
