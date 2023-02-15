61e735b8645124d090a5faf967abd324f1333390
David Gluschenko <davidglusch@gmail.com>
2023-02-15 16:37:15 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if command.split()[1] == command.split()[2]:
+        return None
+
+    with (
+        open(command.split()[1], "r") as origin,
+        open(command.split()[2], "w") as copy
+    ):
+        copy.write(origin.read())
