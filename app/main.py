db87d61339e22e0b7240493f75f4c38df6bdaea7
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-15 21:15:36 +0200

    break line fix

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ def copy_file(command: str) -> None:
     if input_command != "cp" or file_name_to_copy == new_file_name:
         return
 
-    with open(file_name_to_copy, "r") as file_in,\
-            open(new_file_name, "w") as file_out:
+    with (open(file_name_to_copy, "r") as file_in,
+            open(new_file_name, "w") as file_out):
         read_content = file_in.read()
         file_out.write(read_content)
