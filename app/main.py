64846a9d8e516bdde1d506f1d5328e77c84470a9
Sashkoooo <o.denysenko@hotmail.com>
2023-02-15 21:56:36 +0200

    Solution v1.0

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    inner_command = command.split()
+    if inner_command[0] == "cp" and inner_command[1] != inner_command[2]:
+        with open(f"{inner_command[1]}", "w") as file_1:
+            shutil.copyfile(f"{file_1.name}", f"{inner_command[2]}")
