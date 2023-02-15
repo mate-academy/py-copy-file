e252c508c481d2164ad09d1e610d05a3a8e2d6f7
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-15 20:34:00 +0200

    readme hotwfix 2

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ def copy_file(command: str) -> None:
         raise AttributeError(f"Name of copy command "
                              f"should be 'cp', not {copy}")
 
+    if file_name == new_file_name:
+        return
+
     with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
         temp = file_in.read()
         file_out.write(temp)
