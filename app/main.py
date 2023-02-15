3db2422c9e69d8eda4e2d774329d1fa65722426d
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-15 19:30:29 +0200

    Fixed condition

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     input_command, file_name_to_copy, new_file_name = command.split()
-    if input_command != "cp":
+    if input_command != "cp" or file_name_to_copy == new_file_name:
         return
 
     with open(file_name_to_copy, "r") as file_in,\
