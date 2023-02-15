96f6cf67c3e1037a34ee9f7fb757561d123c65d8
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-15 20:17:53 +0200

    need suggestions

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_name, new_file_name = command.split()[1:]
+    with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
+        temp = file_in.read()
+        file_out.write(temp)
+        file_in.close()
+        file_out.close()
