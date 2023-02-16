a65af62b3e1b7ee707a021242b97a91bd67eccb3
Maksym Pyshniak <pyshniakm91@gmail.com>
2023-02-16 18:04:08 +0200

    update solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 def copy_file(command: str) -> None:
+
+    # Split command line into three parts
     cmd, file_name, copy_file_name = command.split()
-    if file_name == copy_file_name or cmd != "cp":
-        return
+
+    # Raise exception for invalid input
+    if file_name == copy_file_name:
+        raise ValueError("File names should be different")
+    if cmd != "cp":
+        raise ValueError(f"Wrong command, {cmd} != 'cp'")
+
+    # Make copy file
     with open(file_name) as file, open(copy_file_name, "w") as new_file:
         new_file.write(file.read())
