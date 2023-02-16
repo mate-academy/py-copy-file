bd99685504d11b5bb29e6c7b88fb00fd717a2865
Roman <parom95@ukr.net>
2023-02-16 13:06:50 +0200

    Copy File

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(command: str) -> None:
-    if len(command.split()) == 3:
-        _, current_file, future_file = command.split()
-        if current_file != future_file:
-            with (
-                open(f"{current_file}", "r") as file,
-                open(f"{future_file}", "w") as file_copy
-            ):
-                file_copy.write(file.read())
+    cm, current_file, future_file = command.split()
+    if cm == "cp" and current_file != future_file:
+        with (
+            open(f"{current_file}", "r") as file,
+            open(f"{future_file}", "w") as file_copy
+        ):
+            file_copy.write(file.read())
