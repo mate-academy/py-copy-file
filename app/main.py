ba05087adb2b81cd5470d10056ba7d196c4d9dca
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-16 15:39:39 +0200

    united function

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ def copy_file(command: str) -> None:
         return
 
     with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
-        temp = file_in.read()
-        file_out.write(temp)
+        file_out.write(file_in.read())
         file_in.close()
         file_out.close()
