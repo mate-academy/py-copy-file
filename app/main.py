6a50a7de2d34e715b0e1aa617070474a71967e13
Mykhailo Benzel <mishabenzel1997@gmail.com>
2023-02-16 12:54:20 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 def copy_file(command: str) -> None:
     command = command.split()
     if command[1] != command[2]:
-        with open(command[1]) as file1, open(command[2], "w") as file2:
-            file2.write(file1.read())
+        with open(command[1]) as file1:
+            file_empty = len(file1.read())
+            if file_empty != 0:
+                with open(command[2], "w") as file2:
+                    file2.write(file1.read())
