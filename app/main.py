ef0c173e008011702b8d8acc6700cfd89d66b1c1
Natalie Goriela <natalie.goriela@gmail.com>
2023-02-16 15:26:56 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str) -> None:
 
     command = command.split(" ")
 
-    if command[1] == command[2]:
+    if command[0] != "cp" or command[1] == command[2]:
         return
 
     with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
