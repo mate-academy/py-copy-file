5672e82db9b48043dfb603afda3fe623972464fb
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-02-16 13:12:42 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+    if len(command) != 3 or command[0] != "cd" or command[1] == command[2]:
+        return None
+    with open(command[1], "r") as original, open(command[2], "w") as copy:
+        copy.write(original.read())
