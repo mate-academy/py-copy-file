14da1167f02e3172a7aaa89c69099e0708f18de1
Andrii Kozlovskyi <0503424511@ukr.net>
2023-02-16 11:59:44 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, old_name, new_name = command.split()
+    if command == "cp" and old_name != new_name:
+        with open(old_name, "r") as old, open(new_name, "w") as new:
+            new.write(old.read())
