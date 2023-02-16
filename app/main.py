637eeec71b20670ebf9a937457a180222bd08b17
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-02-16 20:36:55 +0200

    optimize check function

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
     content = command.split()
     command, old_file, new_file = content[0], content[1], content[2]
-    if command == "cp":
+    if command == "cp" and old_file != new_file:
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-            if old_file != new_file:
-                content_to_copy = file_in.read()
-                file_out.write(content_to_copy)
+            content_to_copy = file_in.read()
+            file_out.write(content_to_copy)
