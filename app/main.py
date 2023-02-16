fdf3ed8591017d7111454e24e4150bfee8a696a6
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-02-16 20:16:12 +0100

    removed try-except Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
 def copy_file(command: str) -> None:
-    try:
-        command, file_original, file_copy = command.split()
-        if command != "cd":
-            raise ValueError
-    except ValueError:
-        print("Wrong command!")
-        return None
+    command, file_original, file_copy = command.split()
+    if command != "cd":
+        raise ValueError("Wrong command!")
     if file_original == file_copy:
-        print("Files must have different names.")
-        return None
+        return
     with open(file_original, "r") as original, open(file_copy, "w") as copy:
         copy.write(original.read())
