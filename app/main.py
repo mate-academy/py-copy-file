612678312142e7834cccc68b7e17f21b225a5b5b
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-02-16 16:17:23 +0100

    replaced if Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 def copy_file(command: str) -> None:
     try:
         command, file_original, file_copy = command.split()
-        if command != "cd" or file_original == file_copy:
+        if command != "cd":
             raise ValueError
     except ValueError:
         print("Wrong command!")
+        return None
+    if file_original == file_copy:
+        print("Files must have different names.")
         return None
     with open(file_original, "r") as original, open(file_copy, "w") as copy:
         copy.write(original.read())
