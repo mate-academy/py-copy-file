cb9d4b9f6be42690702ad675416666c461e16d5d
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-02-17 15:51:30 +0100

    removed return Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
-    command, file_original, file_copy = command.split()
+    command, file_initial, file_copy = command.split()
     if command != "cd":
         raise ValueError("Wrong command!")
-    if file_original == file_copy:
-        return
-    with open(file_original, "r") as original, open(file_copy, "w") as copy:
-        copy.write(original.read())
+    if not file_initial == file_copy:
+        with open(file_initial, "r") as original, open(file_copy, "w") as copy:
+            copy.write(original.read())
