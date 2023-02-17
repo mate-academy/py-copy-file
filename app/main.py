b8e2e02882718d0b7e3f147472025194db94651e
Maksym Pyshniak <pyshniakm91@gmail.com>
2023-02-17 11:53:27 +0200

    update solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 def copy_file(command: str) -> None:
 
     # Split command line into three parts
+    if len(command.split()) != 3:
+        raise ValueError("Wrong command")
     cmd, file_name, copy_file_name = command.split()
+
 
     # Do nothing when user is trying to copy file to file with the same name
     if file_name == copy_file_name:
