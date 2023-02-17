c8c2470da5aa305034e9471fbf1b1a1bc41aab3f
kuznetsb <kuznetsbog@gmail.com>
2023-02-17 15:26:28 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import re
+
+
+class IncorrectCommand(Exception):
+    """Error if command has incorrect format"""
+
+
+def copy_file(command: str) -> None:
+    command_format = r"cp \w+.txt \w+.txt"
+    if not re.match(command_format, command):
+        raise IncorrectCommand(f"{command} has incorrect format")
+    copied_file, new_file = command.split()[1], command.split()[2]
+    if copied_file != new_file:
+        with open(copied_file, "r") as old, open(new_file, "w") as new:
+            new.write(old.read())
