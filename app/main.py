067a9fbd5b7af828b6722d46c93ce3e879b4b7ec
David Gluschenko <davidglusch@gmail.com>
2023-02-17 17:33:18 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 def copy_file(command: str) -> None:
-    cp, origin, copy = command.split()
+    command_copy, origin, copy = command.split()
 
-    if origin == copy or cp == "cp":
-        return None
+    if origin != copy and command_copy == "cp":
 
-    with (
-        open(origin, "r") as origin,
-        open(copy, "w") as copy
-    ):
-        copy.write(origin.read())
+        with (
+            open(origin, "r") as origin,
+            open(copy, "w") as copy
+        ):
+            copy.write(origin.read())
