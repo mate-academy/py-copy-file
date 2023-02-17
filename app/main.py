3171064a411665278092f5924a0b3a70fa19da26
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-17 16:59:17 +0200

    delete prints

--- app/main.py
+++ app/main.py
@@ -4,4 +4,3 @@ def copy_file(command: str) -> None:
     if command == "cp" and old_file != new_file:
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             file_out.write(file_in.read())
-            print(f"File {old_file} has been copied to the file {new_file}")
