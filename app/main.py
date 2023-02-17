f48c6a1ed67dfa8ff492ed7cd5f81daeae47e82c
Maksym Nikityuk <maksym71417@gmail.com>
2023-02-17 18:57:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str):
+    res = command.split(" ")
+    if res[0] == "cp":
+        with open(res[1], "r") as source:
+            data = source.read()
+
+        with open(res[2], "w") as target:
+            target.write(data)
