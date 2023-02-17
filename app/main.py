ad175f60f71a9bac10269f4159d28525330b0632
Andrii Kozlovskyi <0503424511@ukr.net>
2023-02-17 12:21:14 +0000

    Solution_v3

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 def copy_file(command: str) -> None:
-    command, copied_file, file_to_copy = command.split()
-    if command == "cp" and copied_file != file_to_copy:
-        with (open(copied_file, "r") as file_r,
-                open(file_to_copy, "w") as file_w):
+    interim = command.split()
+    command_file = interim[0]
+    copied_file = interim[1]
+    file_to_copy = interim[2]
+    if command_file == "cp" and copied_file != file_to_copy:
+        with (
+            open(copied_file, "r") as file_r,
+                open(file_to_copy, "w") as file_w
+        ):
             file_w.write(file_r.read())
