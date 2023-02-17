ae6fea177b7d653527583a851ceade907ecb259f
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-17 18:23:56 +0200

    refactor

--- app/main.py
+++ app/main.py
@@ -1,13 +1,9 @@
 def copy_file(command: str) -> None:
-    copy, file_name, new_file_name = command.split()
-    if copy != "cp":
-        raise AttributeError(f"Name of copy command "
-                             f"should be 'cp', not {copy}")
+    command, old_file, new_file = command.split()
+    if command != "cp":
+        raise ValueError(f"Name of copy command "
+                         f"should not be {command}")
 
-    if file_name == new_file_name:
-        return
-
-    with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
-        file_out.write(file_in.read())
-        file_in.close()
-        file_out.close()
+    if old_file != new_file:
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
