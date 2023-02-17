6a746cf28b6bf7cd8ef6a17d51a72d5fbd5890c8
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-17 17:23:46 +0200

    fixed if and write method

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 def copy_file(command: str) -> None:
     input_command, file_name_to_copy, new_file_name = command.split()
-    if input_command != "cp" or file_name_to_copy == new_file_name:
-        return
 
-    with (open(file_name_to_copy, "r") as file_in,
-            open(new_file_name, "w") as file_out):
-        read_content = file_in.read()
-        file_out.write(read_content)
+    if input_command == "cp" and file_name_to_copy != new_file_name:
+        with (open(file_name_to_copy, "r") as file_in,
+                open(new_file_name, "w") as file_out):
+            file_out.write(file_in.read())
