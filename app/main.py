70eecc068945c1bd93f699c1734ae67f16867618
Sashkoooo <o.denysenko@hotmail.com>
2023-02-17 15:16:10 +0200

    Solution v3.3

--- app/main.py
+++ app/main.py
@@ -3,8 +3,11 @@ def copy_file(command: str) -> None:
         command, file_to_copy, new_file = command.split()
 
         if command == "cp" and file_to_copy != new_file:
-            with open(file_to_copy, "r") as file_to_copy,\
-                    open(new_file, "w") as new_file:
+            with (
+                open(file_to_copy, "r") as file_to_copy,
+                open(new_file, "w") as new_file
+
+            ):
                 new_file.write(file_to_copy.read())
 
     except FileNotFoundError:
