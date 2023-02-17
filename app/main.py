d56f616ea537d5e808312b7ac6ffe357e18f38a8
usrofgh <mailforgimail@gmail.com>
2023-02-17 22:59:25 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        cd, old_file, new_file = command.split()
+    except ValueError:
+        raise ValueError("Write a command correctly. "
+                         "Example: cp file1.txt file2.txt")
+    if old_file == new_file:
+        return
+
+    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
