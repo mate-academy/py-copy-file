0b96a8fa5b31e13ea7b6502e4c5db785c7652770
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-17 20:37:50 +0200

    3 commit

--- app/main.py
+++ app/main.py
@@ -2,5 +2,4 @@ def copy_file(command: str) -> None:
     cp_command, file, file_copy = command.split()
     if file != file_copy and cp_command == "cp":
         with open(file, "r") as file_in, open(file_copy, "w") as file_out:
-            data = file_in.read()
-            file_out.write(data)
+            file_out.write(file_in.read())
