66484e45ca6a76df748011f0edeb38ed54a892ce
Eugene <eugene.lot77@gmail.com>
2023-02-17 17:49:07 +0200

    Solution changed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(command: str) -> None:
-    file_org = command.split()[1]
-    file_copy = command.split()[2]
+    cmd, file_org, file_copy = command.split()
 
-    if command.startswith("cp") and file_org != file_copy:
+    if cmd == "cp" and file_org != file_copy:
         with open(file_org, "r") as file_out, open(file_copy, "w") as file_in:
             file_in.write(file_out.read())
