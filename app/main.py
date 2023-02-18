114aeee1b783f80284ba1494e5667495114f29e4
bohdanlysuchenko <lysachenko111@ukr.net>
2023-02-18 11:46:18 +0200

    Changes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    command, file, directory = command.split()
-    if file != directory and command == "cp":
-        with open(file, "r") as old_file, open(directory, "w") as copy:
+    command, file, new_file = command.split()
+    if file != new_file and command == "cp":
+        with open(file, "r") as old_file, open(new_file, "w") as copy:
             copy.write(old_file.read())
