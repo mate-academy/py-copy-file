78ef733d26d9da1fe568c4dd50105d08d9b2da16
Natalie Goriela <natalie.goriela@gmail.com>
2023-02-18 10:33:26 +0200

    'Solution'

---
+++ app/error.py
@@ -0,0 +1,10 @@
+class SameNameError(Exception):
+    pass
+
+
+class WrongCommandError(Exception):
+    pass
+
+
+class ArgumentsError(Exception):
+    pass
--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
+from app.error import WrongCommandError, SameNameError, ArgumentsError
+
+
 def copy_file(command: str) -> None:
 
     command = command.split(" ")
 
-    if command[0] != "cp" or command[1] == command[2]:
-        return
+    if command[0] != "cp":
+        raise WrongCommandError("Incorrect command")
+
+    if command[1] == command[2]:
+        raise SameNameError("The copy should be renamed")
+
+    if len(command) != 3:
+        raise ArgumentsError(
+            "'Command', 'file name' and 'new file' name should be provided"
+        )
 
     with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
         file_out.write(file_in.read())
