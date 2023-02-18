4fbfa5fa391d1eb7e6e6b5531c9d10828da7f902
Ivan Hutsuliak <ivan.guculyak@gmail.com>
2023-02-18 20:28:06 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    arguments = [word for word in command.split()]
+    if arguments[1] == arguments[2]:
+        return
+    with open(arguments[1], "r") as file, open(arguments[2], "w") as new_file:
+        new_file.write(file.read())
