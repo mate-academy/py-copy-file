f290893c735f807c136584a31348a2e456cb50d7
Dmytry <donchykd@gmail.com>
2023-02-18 16:20:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,6 +4,5 @@ def copy_file(command: str) -> None:
         return 0
     if temp[1] == temp[2]:
         return 0
-    with open(f"{temp[1]}", "r") as file_in, open(f"{temp[2]}",
-                                                  "w") as file_out:
-        file_out.write(file_in.read())
+    with open(f"{temp[1]}", "r") as source, open(f"{temp[2]}", "w") as copy:
+        copy.write(source.read())
