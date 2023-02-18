7e56d72220f9caf580aebabe3dabd68acbd6560d
Sergii Machulin <machulinsv@gmail.com>
2023-02-18 16:09:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_words = command.split()
+    if (
+            len(command_words) == 3
+            and command_words[0] == "cp"
+            and command_words[1] != command_words[2]
+    ):
+        with (
+            open(command_words[1], "r") as file_in,
+            open(command_words[2], "w") as file_out
+        ):
+            file_out.write(file_in.read())
