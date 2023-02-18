17766a1510874525ae46688d77a4addf667bf56c
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-02-18 23:30:17 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+from shutil import copyfile
+
+
+def copy_file(command: str) -> None:
+    execution_command, curr_file, new_file = command.split(" ")
+
+    if execution_command == "cp" and curr_file != new_file:
+        copyfile(curr_file, new_file)
