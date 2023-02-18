e5f2f714f6b81c44657c8e79d1eea524108a9e17
Yura Hudzovskiy <50211819+yura-hudzovskyi@users.noreply.github.com>
2023-02-18 12:53:18 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> any:
+    file_name = command.split()
+
+    if file_name[0] == "cp":
+        with open(
+                file_name[1],
+                "r"
+        ) as file, open(
+            file_name[2],
+            "w"
+        ) as file_copy:
+            file_copy.write(file.read())
