57cbffb142a764014cd1a1384b77c85b36f84a46
Max Bilokur <bilmaxim@gmail.com>
2023-02-18 15:54:02 +0200

    variable name fix

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    com, old_name, new_name = command.split()
-    if com == "cp" and old_name != new_name:
+    operation, old_name, new_name = command.split()
+    if operation == "cp" and old_name != new_name:
         with open(old_name, "r") as reader, open(new_name, "w") as writer:
             writer.write(reader.read())
