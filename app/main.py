59df7ae6a1221a9b45cf6f4aa723d9d996f7ecea
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-18 17:09:19 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    # cp file.txt file-copy.txt
+    if len(command.split()) != 3 or command[:3] != "cp ":
+        return 0
+    name_f_in = command.split()[1]
+    name_f_out = command.split()[2]
+    if name_f_in == name_f_out:
+        return 0
+    with open(name_f_in, "r") as file_in, open(name_f_out, "w") as file_out:
+        file_out.write(file_in.read())
