45de000775d07f1a14a266e4d5ce67c07bf72df8
usrofgh <mailforgimail@gmail.com>
2023-02-18 11:57:01 +0200

    condition checking was changed

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 def copy_file(command: str) -> None:
     try:
-        cd, old_file, new_file = command.split()
+        cp, old_file, new_file = command.split()
+        if cp != "cp" or old_file == new_file:
+            raise ValueError
     except ValueError:
         raise ValueError("Write a command correctly. "
                          "Example: cp file1.txt file2.txt")
-    if old_file == new_file:
-        return
 
     with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
         file_out.write(file_in.read())
+
+
+copy_file("cp file1.txt file2.txt")
