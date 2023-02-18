05048ac6bf91d6431e1e14a6871ed69879450838
Yura Hudzovskiy <50211819+yura-hudzovskyi@users.noreply.github.com>
2023-02-18 19:03:21 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 def copy_file(command: str) -> None:
     file_name = command.split()
 
-    if file_name[0] == "cp":
-        with open(
-                file_name[1],
-                "r"
-        ) as file, open(
-            file_name[2],
-            "w"
-        ) as file_copy:
-            file_copy.write(file.read())
+    if file_name[1] != file_name[2]:
+        if file_name[0] == "cp":
+            with open(
+                    file_name[1],
+                    "r"
+            ) as file, open(
+                file_name[2],
+                "w"
+            ) as file_copy:
+                file_copy.write(file.read())
