190d4490d57e3b881315d6c4fc986fd12dbf2b99
kuznetsb <kuznetsbog@gmail.com>
2023-02-18 13:18:51 +0200

    Changed command split

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def copy_file(command: str) -> None:
     command_format = r"cp \w+.txt \w+.txt"
     if not re.match(command_format, command):
         raise IncorrectCommand(f"{command} has incorrect format")
-    copied_file, new_file = command.split()[1], command.split()[2]
+    _, copied_file, new_file = command.split()
     if copied_file != new_file:
         with open(copied_file, "r") as old, open(new_file, "w") as new:
             new.write(old.read())
