891f2699cd951ccb7b08113e99be91cceff69ee6
Ruslan <noezyboy1@gmail.com>
2023-02-19 13:57:49 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    components = command.split()
+    if len(components) != 3:
+        raise ValueError("Invalid command format. "
+                         "Usage: cp <source_file> <destination_file>")
+
+    source_file = components[1]
+    destination_file = components[2]
+
+    if source_file == destination_file:
+        return
+
+    if not os.path.exists(source_file):
+        raise ValueError(f"Source file '{source_file}' does not exist")
+
+    with open(source_file, "rb") as fsrc, open(destination_file, "wb") as fdst:
+        fdst.write(fsrc.read())
