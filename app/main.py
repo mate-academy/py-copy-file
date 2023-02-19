14f5e4e45a1b0b87b945df83e61547daaa50c07a
Yulia <Mala>
2023-02-19 20:25:44 +0200

    one cond instead of two

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def copy_file(command: str) -> None:
     command, old_file, new_file = command.split()
-    if old_file == new_file:
-        return
-    if command == "cp":
+    if old_file != new_file and command == "cp":
         with (open(f"../{old_file}", "r") as file_in,
               open(f"../{new_file}", "w") as file_out):
             file_out.write(file_in.read())
