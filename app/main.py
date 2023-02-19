36c0982d0770836fcc35d10c9933f1c594a6a202
Anna Boichenko <ann.boychenko199@gmail.com>
2023-02-19 10:16:30 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    temp = command.split(" ")
+    old_file = temp[1]
+    new_file = temp[2]
+    with open(old_file, "r") as old:
+        with open(new_file, "w") as new:
+            new.write(old.read())
