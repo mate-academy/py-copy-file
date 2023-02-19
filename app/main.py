bb2399fd60130fdf10817eb42ffe8debde536037
Dmytry <donchykd@gmail.com>
2023-02-19 13:47:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def copy_file(command: str) -> None:
     temp = command.split(" ")
-    if temp[0] != "cp":
-        return 0
-    if temp[1] == temp[2]:
+    if temp[0] != "cp" or temp[1] == temp[2]:
         return 0
     with open(f"{temp[1]}", "r") as source, open(f"{temp[2]}", "w") as copy:
         copy.write(source.read())
