a1dabef1ae4161d9c5775d0daebc65213ef48621
Ivan Hutsuliak <ivan.guculyak@gmail.com>
2023-02-19 10:45:52 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 def copy_file(command: str) -> None:
-    cp_command, current_file, future_file = command.split()
-    if current_file == future_file and cp_command == "cp":
-        return
-    with open(current_file, "r") as file, open(future_file, "w") as new_file:
-        new_file.write(file.read())
+    try:
+        cp_command, current_file, future_file = command.split()
+        if current_file == future_file or cp_command != "cp":
+            return
+        with open(
+                current_file, "r"
+        ) as file, open(
+            future_file, "w"
+        ) as new_file:
+            new_file.write(file.read())
+    except AttributeError:
+        print("Command should look like `cp file.txt file-copy.txt`.")
