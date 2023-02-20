cd1dd36abe7257a4b218bab555eddeb2aeb69d76
Anatolii Pozniak <a13101990a@gmail.com>
2023-02-20 21:08:39 +0200

    cmd -> command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    cmd, old_file, new_file = command.split()
-    if cmd == "cp" and old_file != new_file:
+    command, old_file, new_file = command.split()
+    if command == "cp" and old_file != new_file:
         with (
             open(old_file, "r") as old_file,
             open(new_file, "w") as new_file
