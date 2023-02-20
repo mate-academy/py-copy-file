4fbd3228a89313c978df602b97d854b1b032ca76
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-02-20 12:29:45 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     original_file = command.split()[1]
     copy_file = command.split()[2]
-    if original_file != copy_file:
+    if original_file[0] == "cp" and original_file != copy_file:
         content = open(original_file, "r")
         with open(f"{copy_file}", "w") as source:
             source.write(content.read())
