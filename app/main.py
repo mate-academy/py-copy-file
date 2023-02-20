5c4bb8a4865ca61af713bb595f2920871a1fb0a0
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-02-20 12:06:17 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    files = command.split()
+    if open(files[1]) != open(files[2]) and files[0] == "cp":
+        with open(files[1], "r") as file_1, open(files[2], "w") as file_2:
+            df = file_1.readline()
+            for line in df:
+                file_2.write(line)
