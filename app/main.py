bfe76965ae3e2ea38058f1c20ef0a9810baddfbd
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-20 15:20:23 +0200

    without excessive split

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
-def copy_file(command: str) -> None:
-    if len(command.split()) != 3 or command[:3] != "cp ":
-        return 0
-    name_f_in = command.split()[1]
-    name_f_out = command.split()[2]
+def copy_file(command: str) -> None | str:
+    short_command, name_f_in, name_f_out = command.split()
+    if short_command != "cp":
+        return "Write correct command"
     if name_f_in == name_f_out:
-        return 0
+        return "the name of the second file must not be the same as the first"
     with open(name_f_in, "r") as file_in, open(name_f_out, "w") as file_out:
         file_out.write(file_in.read())
