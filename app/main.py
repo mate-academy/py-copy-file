2c49f9fdd98e504759be6b4e1418a7b6d203041a
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-02-20 07:08:54 +0200

    add check command == 3

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 def copy_file(command: str) -> None:
     content = command.split()
-    command, old_file, new_file = content[0], content[1], content[2]
-    if command == "cp" and old_file != new_file:
-        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-            content_to_copy = file_in.read()
-            file_out.write(content_to_copy)
+    if len(content) == 3:
+        command, old_file, new_file = content[0], content[1], content[2]
+        if command == "cp" and old_file != new_file:
+            with open(old_file, "r") as file_in, \
+                    open(new_file, "w") as file_out:
+                content_to_copy = file_in.read()
+                file_out.write(content_to_copy)
+
+
+copy_file("text.txt new_text.txt")
