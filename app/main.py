b323f9c4e9c7326c2f4e43cb862dbfad5a520679
Sergii Machulin <machulinsv@gmail.com>
2023-02-20 17:58:17 +0200

    Remove try/except

--- app/main.py
+++ app/main.py
@@ -1,16 +1,8 @@
 def copy_file(command: str) -> None:
-    try:
-        cp_command, file_in_name, file_out_name = command.split()
-        if cp_command != "cp" or file_in_name == file_out_name:
-            raise ValueError
+    cp_command, file_in_name, file_out_name = command.split()
+    if cp_command == "cp" and file_in_name != file_out_name:
         with (
             open(file_in_name, "r") as file_in,
             open(file_out_name, "w") as file_out
         ):
             file_out.write(file_in.read())
-    except ValueError:
-        print(
-            "Your command must consist of: keyword 'cp',"
-            " file name to copy and new file name, "
-            "separated by spaces"
-        )
