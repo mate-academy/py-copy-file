4769ef8d4f2b4e3d57071c6c3661255a563baa45
Roman Rega <romanrega1994@gmail.com>
2023-02-20 16:46:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(comand: str) -> None:
+    names = comand.split(" ")
+    if names[1] == names[2]:
+        print("Does nothing")
+    if names[1] != names[2]:
+        with open(names[1], "r") as file_in, open(names[2], "w") as file_out:
+            for i in file_in:
+                file_out.write(i)
