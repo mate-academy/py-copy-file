14d2247cfc0e8348cebbf5d02b2cbadb345c47e9
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-20 17:01:04 +0200

    full name variables

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
-    short_command, name_f_in, name_f_out = command.split()
+    short_command, name_file_in, name_file_out = command.split()
 
-    if short_command == "cp" and name_f_in != name_f_out:
+    if short_command == "cp" and name_file_in != name_file_out:
         with (
-            open(name_f_in, "r") as file_in,
-            open(name_f_out, "w") as file_out
+            open(name_file_in, "r") as file_in,
+            open(name_file_out, "w") as file_out
         ):
             file_out.write(file_in.read())
