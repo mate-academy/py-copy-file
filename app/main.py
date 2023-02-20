bd40f6b8d748bbb67b4fbce60851486fba25d137
Bohdan Kovalenko <bog7807@gmail.com>
2023-02-20 20:10:53 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 def copy_file(command: str) -> None:
-    command = command.split()
-    with (
-        open(command[1], "r") as file_to_copy,
-        open(command[2], "w") as new_file
-    ):
-        if file_to_copy.name != new_file.name and command[0] == "cp":
+    command, to_copy, new = command.split()
+    if to_copy != new and command == "cp":
+        with (
+            open(to_copy, "r") as file_to_copy,
+            open(new, "w") as new_file
+        ):
             new_file.write(file_to_copy.read())
-        else:
-            print("Error: same file name.")
