da73e65efe67ee095209dffff5e05c65a5016df9
Anna Boichenko <ann.boychenko199@gmail.com>
2023-02-20 18:55:34 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
-    temp = command.split(" ")
-    old_file = temp[1]
-    new_file = temp[2]
-    if old_file == new_file:
-        exit()
-    with open(old_file, "r") as old:
-        with open(new_file, "w") as new:
-            new.write(old.read())
+    if command.startswith("cp"):
+        temp = command.split()
+        old_file, new_file = temp[1], temp[2]
+        if temp[1] != temp[2]:
+            with open(old_file, "r") as file_in, open(
+                new_file, "w"
+            ) as file_out:
+                file_out.write(file_in.read())
