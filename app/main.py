b66500374b8c67ff917dca58e5beee5aed7c070d
Ada Kravchenko <ada.t.krav@gmail.com>
2023-02-20 17:36:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(file_names: str) -> None:
     command, original, copy = file_names.split()
-    if command != "cp":
-        exit(f"Command '{command}' was not found")
     if original == copy:
         exit(0)
-    with open(original, "r") as original, open(copy, "w") as copy:
-        lines = original.readlines()
-        copy.writelines(lines)
+    if command == "cp":
+        with open(original, "r") as original, open(copy, "w") as copy:
+            lines = original.readlines()
+            copy.writelines(lines)
