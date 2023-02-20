04c4667c39dafaf25af4fe5eb3711595032201bc
Maksym Nikityuk <maksym71417@gmail.com>
2023-02-20 18:04:13 +0200

    S

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def copy_file(command: str) -> None:
-    command, file, new_file = command.split("")
-    if command == "cp":
+    command, file, new_file = command.split()
+    if command == "cp" and file != new_file:
         with open(file, "r") as source, open(new_file, "w") as target:
             data = source.read()
             target.write(data)
-    if file == new_file:
-        return
