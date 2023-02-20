a7a2f9ae8c9191c96efa69d823ef96f40c551ca1
Ruslan <noezyboy1@gmail.com>
2023-02-20 16:00:58 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,21 +2,17 @@ import os
 
 
 def copy_file(command: str) -> None:
-    components = command.split()
-    if len(components) != 3 or components[0] != "cp":
+    command, source_file, destination_file = command.split()
+
+    if command != "cp":
         raise ValueError("Invalid command format. "
                          "Usage: cp <source_file> <destination_file>")
 
-    source_file = components[1]
-    destination_file = components[2]
-
     if source_file == destination_file:
-        return
+        if not os.path.exists(source_file):
+            raise ValueError(f"Source file '{source_file}' does not exist")
 
-    if not os.path.exists(source_file):
-        raise ValueError(f"Source file '{source_file}' does not exist")
-
-    with open(source_file, "rb") as sourcefile, \
-            open(destination_file, "wb") as destin_file:
-        destin_file.write(sourcefile.read())
-
+    with open(source_file, "rb") as sourcefile, (
+            open(destination_file, "wb")
+    ) as destinationfile:
+        destinationfile.write(sourcefile.read())
