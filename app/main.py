8ce58154a4f1da72cf606dba208cc20a7a0cca0a
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-02-20 20:42:10 +0200

    optimize with statement

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ def copy_file(command: str) -> None:
     if len(content) == 3:
         command, old_file, new_file = content[0], content[1], content[2]
         if command == "cp" and old_file != new_file:
-            with open(old_file, "r") as file_in, \
-                    open(new_file, "w") as file_out:
+            with (
+                open(old_file, "r") as file_in,
+                open(new_file, "w") as file_out
+            ):
                 content_to_copy = file_in.read()
                 file_out.write(content_to_copy)
