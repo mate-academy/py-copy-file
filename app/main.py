82943906c402b727921694d60f5813cf0625ec8f
oksana-feshchenko <foksanv@gmail.com>
2023-02-20 11:56:10 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def copy_file(command: str) -> None:
-    cp, first, second = command.split(" ")
-    if first != second and cp == "cp":
-        with open(first, "r") as file_in, open(second, "w") as file_out:
+    my_command, existed_file, file_to_create = command.split()
+    if existed_file != file_to_create and my_command == "cp":
+        with open(existed_file, "r") as file_in, open(
+            file_to_create, "w"
+        ) as file_out:
             file_out.write(file_in.read())
