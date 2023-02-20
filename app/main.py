9adc840d3b7f7f8cfc94c76402b1f95866f10e19
oithegod <serejathegod@gmail.com>
2023-02-20 18:08:16 +0200

    Fix

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 def copy_file(command: str) -> None:
     command, source_file, result_file = command.split()
 
-    if source_file == result_file or command != "cp":
-        return
-
-    with (open(source_file, "r") as source,
-          open(result_file, "w") as result):
-        result.write(source.read())
+    if not source_file == result_file and command == "cp":
+        with (open(source_file, "r") as source,
+              open(result_file, "w") as result):
+            result.write(source.read())
