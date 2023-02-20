ea18267e9523089740a58a1735404d48797edf08
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-02-20 19:57:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    task, first_file, second_file = command.split(" ")
-    if task == "cp" and first_file != second_file:
+    command_name, first_file, second_file = command.split()
+    if command_name == "cp" and first_file != second_file:
         with open(first_file, "r") as first, open(second_file, "w") as second:
             second.write(first.read())
