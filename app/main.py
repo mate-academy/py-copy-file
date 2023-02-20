bebe285c3b5c29b7b7ca6f608f0c8d838d8cdd2e
Maksym Nikityuk <maksym71417@gmail.com>
2023-02-20 18:10:10 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,4 @@ def copy_file(command: str) -> None:
     command, file, new_file = command.split()
     if command == "cp" and file != new_file:
         with open(file, "r") as source, open(new_file, "w") as target:
-            data = source.read()
-            target.write(data)
+            target.write(source.read())
