bd4f90d62384a4867cd91d39866bcfd5b76daf81
name <ksyusha0kubay@gmail.com>
2023-02-20 16:44:44 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> any:
+    first_file = command.split()[1]
+    second_file = command.split()[2]
+
+    if first_file.split(".")[0] == second_file.split(".")[0]:
+        return "Does nothing"
+
+    with open(first_file, "r") as first_f, open(second_file, "a") as second_f:
+        for line in first_f:
+            second_f.write(line)
