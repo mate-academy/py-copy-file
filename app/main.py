f35c0deebaffbad861930f95e249135d59ae88d4
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-20 15:36:37 +0200

    minus one (if)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-def copy_file(command: str) -> None | str:
+def copy_file(command: str) -> None:
     short_command, name_f_in, name_f_out = command.split()
-    if short_command != "cp":
-        return "Write correct command"
-    if name_f_in == name_f_out:
-        return "the name of the second file must not be the same as the first"
-    with open(name_f_in, "r") as file_in, open(name_f_out, "w") as file_out:
-        file_out.write(file_in.read())
+
+    if short_command == "cp" and name_f_in != name_f_out:
+        with (
+            open(name_f_in, "r") as file_in,
+            open(name_f_out, "w") as file_out
+        ):
+            file_out.write(file_in.read())
