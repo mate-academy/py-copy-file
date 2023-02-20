e2d5ff3d4b3949fd0c7c5d0493b5efda84a95bcd
Mykola Savchyn <kola.savchyn@gmail.com>
2023-02-20 14:31:47 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    commands = command.split()
+    old_file = commands[1]
+    new_file = commands[2]
+    if old_file == new_file:
+        return
+    with open(old_file, "r") as source:
+        content = source.read()
+
+    with open(new_file, "w") as destination:
+        destination.write(content)
