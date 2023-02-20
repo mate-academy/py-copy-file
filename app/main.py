f35305a6e1ac0a5a3675199aada0237954e8e14f
Ostap <anastasiia@Anastasiias-Air.Dlink>
2023-02-20 15:51:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    temp_list = command.split()
+    if temp_list[0] != "cp" or (
+            not temp_list[1].endswith(".txt")
+            or not temp_list[2].endswith(".txt")
+    ):
+        print("Invalid command format! "
+              "Please, follow this ex: 'cp file.txt file_copy.txt'")
+        return
+    if temp_list[1] != temp_list[2]:
+        with open(temp_list[1], "r") as file_in, open(
+                temp_list[2], "w") as file_out:
+            temp = file_in.read()
+            file_out.write(temp)
