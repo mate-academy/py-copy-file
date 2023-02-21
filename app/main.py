a0a9810a98366b094087f2a1de2613b87e8fdf6a
Misha Vovchok <mivvaw@gmail.com>
2023-02-21 17:20:53 +0200

    sol

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy_file(command: str) -> None:
     copy_command, original_file, duplicate_file = command.split()
-    if original_file == duplicate_file:
-        return
-    with open(original_file, "r") as file_in, open(duplicate_file, "w") as file_out:
-        file_out.write(file_in.read())
+    if original_file != duplicate_file:
+        with open(original_file, "r") as file_in, open(duplicate_file, "w") as file_out:
+            file_out.write(file_in.read())
