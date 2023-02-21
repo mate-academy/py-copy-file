98cda125978287424f569ca0ba5313720cd93198
ramsty <ramsty27@gmail.com>
2023-02-21 16:05:45 +0200

    corrections

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    _, file, new_file = command.split()
+    cp, file, new_file = command.split()
 
-    if file != new_file:
+    if file != new_file and cp == "cp":
         with open(file, "r") as file_in, open(new_file, "w") as file_out:
             file_out.write(file_in.read())
