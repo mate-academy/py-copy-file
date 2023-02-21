229dc21c29f3e6e67b8ca8b5cf61418e19d6a5a5
ramsty <ramsty27@gmail.com>
2023-02-21 11:39:33 +0200

    Name changed

--- app/main.py
+++ app/main.py
@@ -2,5 +2,5 @@ def copy_file(command: str) -> None:
     _, file, new_file = command.split()
 
     if file != new_file:
-        with open(file, "r") as f_in, open(new_file, "w") as f_out:
-            f_out.write(f_in.read())
+        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
