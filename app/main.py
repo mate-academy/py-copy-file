2316cac7d9eeb53ee93c2135d1319f73fef0562e
Ada Kravchenko <ada.t.krav@gmail.com>
2023-02-21 07:38:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 def copy_file(file_names: str) -> None:
     command, original, copy = file_names.split()
-    if original == copy:
-        exit(0)
-    if command == "cp":
+    if command == "cp" and original != copy:
         with open(original, "r") as original, open(copy, "w") as copy:
-            lines = original.readlines()
-            copy.writelines(lines)
+            copy.writelines(original.readlines())
