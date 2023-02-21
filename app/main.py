d0bed8522a444c5b233b869c952377aa3b416014
usrofgh <mailforgimail@gmail.com>
2023-02-21 21:18:23 +0200

    update

--- app/main.py
+++ app/main.py
@@ -1,14 +1,8 @@
 def copy_file(command: str) -> None:
-    try:
-        cp, old_file, new_file = command.split()
-        if cp != "cp" or old_file == new_file:
-            raise ValueError
-    except ValueError:
+    cp_command, old_file, new_file = command.split()
+    if cp_command != "cp" or old_file == new_file:
         raise ValueError("Write a command correctly. "
                          "Example: cp file1.txt file2.txt")
 
     with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
         file_out.write(file_in.read())
-
-
-copy_file("cp file1.txt file2.txt")
