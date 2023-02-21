bd6a1b73d62472ffb44e4453834d3b2bd8af53a0
MikhailLyvak <michael.lyvak@gmail.com>
2023-02-21 12:28:20 +0200

    Name changes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    cmd, old_file, new_file = command.split(" ")
-    if cmd == "cp" and old_file != new_file:
+    command, old_file, new_file = command.split()
+    if command == "cp" and old_file != new_file:
         with open(old_file, "r") as file_out, open(new_file, "w") as file_in:
             file_in.write(file_out.read())
