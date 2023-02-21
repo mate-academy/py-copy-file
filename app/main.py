94da0bf9468ccf49889fbc11a2d6d3cfaffdddb4
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-02-21 14:40:31 +0200

    unpacking variables in one line added condition

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
-    file_names = command.split()
+    command_name, first_file, second_file = command.split()
 
-    first, second = file_names[1], file_names[2]
-
-    if first == second:
-        with open(first, "r") as file_in, open(second, "w") as file_out:
+    if first_file != second_file and command_name == "cp":
+        with open(first_file, "r") as file_in, open(
+            second_file, "w"
+        ) as file_out:
             file_out.write(file_in.read())
