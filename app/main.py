b3cc0fda3ddba27406ed21198851340fa9eebfe7
Andy <andy77andy@ukr.net>
2023-02-21 21:35:00 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, olf_file, new_file = command.split()
+    if olf_file != new_file:
+        with open(new_file, "w") as f_1, open(olf_file, "r") as f_2:
+            f_1.write(f_2.read())
