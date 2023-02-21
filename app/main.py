5316d8c03f82af62f875a1a5e0ef6fe1f39929ac
Ostap <anastasiia@Anastasiias-Air.Dlink>
2023-02-21 17:34:45 +0200

    change conditions and remove redundant things

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
 def copy_file(command: str) -> None:
     cp, filename, copy_filename = command.split()
-    if cp != "cp" or (
-            not filename.endswith(".txt")
-            or not copy_filename.endswith(".txt")
-    ):
-        print("Invalid command format! "
-              "Please, follow this ex: 'cp file.txt file_copy.txt'")
-        return
-
-    with open(filename, "r") as file_in, open(
-            copy_filename, "w") as file_out:
-        temp = file_in.read()
-        file_out.write(temp)
+    if cp != "cp" and filename != copy_filename:
+        with open(filename, "r") as file_in, open(
+                copy_filename, "w") as file_out:
+            temp = file_in.read()
+            file_out.write(temp)
