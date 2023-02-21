c12cfedbf7aa0d7ce3a6bea1b3fc9b7563fef523
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-02-21 18:09:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 def copy_file(command: str) -> None:
-    if "cp" not in command:
-        return
-    files = command.split(" ")
-    copied = files[1]
-    file_to_copy = files[2]
-    if copied == file_to_copy:
+    _, copied, file_to_copy = command.split(" ")
+    if copied == file_to_copy or _ != "cp":
         return
     with open(copied, "r") as file_in, open(file_to_copy, "w") as file_out:
         for line in file_in:
