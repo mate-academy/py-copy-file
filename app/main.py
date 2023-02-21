732dfda41906ca996a43b684027a3aebd7b01273
Artur Tytomyr <art.tytomyr@gmail.com>
2023-02-21 13:56:06 +0200

    revised

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(string: str) -> None:
     command, file_name, new_file_name = string.split()
     if command == "cp" and file_name != new_file_name:
-        with(open(f"{file_name}", "r") as file,
+        with (open(f"{file_name}", "r") as file,
              open(f"{new_file_name}", "w") as new_file):
             new_file.write(file.read())
