011171ef3c1139bd5e9660f67e490fe7fe8fe1db
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-02-21 16:35:44 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -2,5 +2,4 @@ def copy_file(command: str) -> None:
     files = command.split()
     if files[1] != files[2] and files[0] == "cp":
         with open(files[1], "r") as file_1, open(files[2], "w") as file_2:
-            read_file = file_1.readlines()
-            file_2.writelines(read_file)
+            file_2.write(file_1.read())
