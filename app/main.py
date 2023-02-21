649ae7b8b41778ea56d956cf4a9952eb0d8cea88
Ostap <anastasiia@Anastasiias-Air.Dlink>
2023-02-21 12:41:38 +0200

    fixed as per comments

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 def copy_file(command: str) -> None:
-    temp_list = command.split()
-    if temp_list[0] != "cp" or (
-            not temp_list[1].endswith(".txt")
-            or not temp_list[2].endswith(".txt")
+    cp, filename, copy_filename = command.split()
+    if cp != "cp" or (
+            not filename.endswith(".txt")
+            or not copy_filename.endswith(".txt")
     ):
         print("Invalid command format! "
               "Please, follow this ex: 'cp file.txt file_copy.txt'")
         return
-    if temp_list[1] != temp_list[2]:
-        with open(temp_list[1], "r") as file_in, open(
-                temp_list[2], "w") as file_out:
-            temp = file_in.read()
-            file_out.write(temp)
+
+    with open(filename, "r") as file_in, open(
+            copy_filename, "w") as file_out:
+        temp = file_in.read()
+        file_out.write(temp)
