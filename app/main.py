12f2998f848b96e2ab9f39c1847bbca80cc545b0
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-02-21 13:25:41 +0200

    Added command check

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+
 def copy_file(command: str) -> None:
+    command_name = command.split()[0]
     base_filename = command.split()[1]
     new_filename = command.split()[2]
 
-    if base_filename != new_filename:
+    if command_name == "cp" and base_filename != new_filename:
         with (open(base_filename, "r") as base_file,
               open(new_filename, "w") as new_file):
             new_file.write(base_file.read())
