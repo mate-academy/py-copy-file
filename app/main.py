292a2a2ee3d6285aae0ef4114c332fff9b0eac5a
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-02-21 13:33:32 +0200

    Edited solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 
 def copy_file(command: str) -> None:
-    command_name = command.split()[0]
-    base_filename = command.split()[1]
-    new_filename = command.split()[2]
+    command_name, base_filename, new_filename = command.split()
 
     if command_name == "cp" and base_filename != new_filename:
         with (open(base_filename, "r") as base_file,
