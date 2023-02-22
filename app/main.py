08b3ef74c71389784baeccadd28236573fb9fc2a
Ostap <anastasiia@Anastasiias-Air.Dlink>
2023-02-22 11:26:47 +0200

    delete all redundand things

--- app/main.py
+++ app/main.py
@@ -3,5 +3,4 @@ def copy_file(command: str) -> None:
     if cp != "cp" and filename != copy_filename:
         with open(filename, "r") as file_in, open(
                 copy_filename, "w") as file_out:
-            temp = file_in.read()
-            file_out.write(temp)
+            file_out.write(file_in.read())
