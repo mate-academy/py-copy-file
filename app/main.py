1ba582689e2507e4e69e6de7256d285bfdcc9f13
Misha Vovchok <mivvaw@gmail.com>
2023-02-22 14:35:45 +0200

    add cp comand

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
     copy_command, original, copy = command.split()
-    if original != copy:
+    if copy_command == "cp" and original != copy:
         with open(original, "r") as file_in, open(copy, "w") as copy:
             copy.writelines(file_in.readlines())
