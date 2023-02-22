08f04350c68a8303aef5a6db941a5872e0997c72
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-02-21 20:19:08 -0600

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str):
+    file_names = command.split()
+    if file_names[1] != file_names[2]:
+        with open(file_names[1], "r") as file_old, open(file_names[2], "w") as file_new:
+            file_new.write(file_old.read())
