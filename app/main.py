4216f08849b044cff27d4e89db03ed917a118fad
Andrii Kozlovskyi <0503424511@ukr.net>
2023-02-22 09:29:30 +0000

    Solution_v5

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 def copy_file(command: str) -> None:
     if len(command) != 3:
         raise ValueError("Invalid command value")
-    interim, command_file, copied_file = command
+    interim, command_file, copied_file = command.split()
     file_to_copy = interim[2]
     if command_file == "cp" and copied_file != file_to_copy:
         with (
             open(copied_file, "r") as file_r,
-                open(file_to_copy, "w") as file_w
+            open(file_to_copy, "w") as file_w
         ):
             file_w.write(file_r.read())
