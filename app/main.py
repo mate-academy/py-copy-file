7b83d2079f42bdced93ff1601bb9a37d6ac1355e
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-02-22 11:23:26 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 def copy_file(command: str) -> None:
     file_names = command.split()
+
+    if file_names[0] != "cp":
+        raise ValueError(f"{file_names[0]} is not a copying command!")
+    if len(file_names) != 3:
+        raise ValueError(f"Expected two values for cp, got {len(file_names)}")
     if file_names[1] != file_names[2]:
-        with (open(file_names[1], "r") as file_old,
-                open(file_names[2], "w") as file_new):
-            file_new.write(file_old.read())
+        return None
+
+    with (open(file_names[1], "r") as file_old,
+            open(file_names[2], "w") as file_new):
+        file_new.write(file_old.read())
+
