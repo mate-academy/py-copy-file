24c4a7352a9441ceb0a285accd1a2d6a7a4931d1
Andy <andy77andy@ukr.net>
2023-02-22 23:53:24 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def copy_file(command: str) -> None:
-    command, olf_file, new_file = command.split()
-    if olf_file != new_file:
-        with open(new_file, "w") as f_1, open(olf_file, "r") as f_2:
-            f_1.write(f_2.read())
+    if command.count(" ") == 2:
+        command, old_file, new_file = command.split()
+        if old_file != new_file:
+            with open(new_file, "w") as f_1, open(old_file, "r") as f_2:
+                f_1.write(f_2.read())
+git commit -am 'Solution'
