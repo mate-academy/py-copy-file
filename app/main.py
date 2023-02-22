03e923955707decc7b1a9fb8a3fbd12ece99e01b
Misha Vovchok <mivvaw@gmail.com>
2023-02-22 13:59:22 +0200

    sol

--- app/main.py
+++ app/main.py
@@ -2,4 +2,4 @@ def copy_file(command: str) -> None:
     copy_command, original, copy = command.split()
     if original != copy:
         with open(original, "r") as file_in, open(copy, "w") as copy:
-            copy.write(file_in.read())
+            copy.writelines(file_in.readlines())
