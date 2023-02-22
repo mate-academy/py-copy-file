1ea3d0c3872076195a3f6027f699c78dcbbf20fe
Ostap <anastasiia@Anastasiias-Air.Dlink>
2023-02-22 15:42:03 +0200

    change var name and fix identation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    cp, filename, copy_filename = command.split()
-    if cp != "cp" and filename != copy_filename:
-        with open(filename, "r") as file_in, open(
-                copy_filename, "w") as file_out:
+    command, filename, copy_filename = command.split()
+    if command == "cp" and filename != copy_filename:
+        with (open(filename, "r") as file_in,
+              open(copy_filename, "w") as file_out):
             file_out.write(file_in.read())
