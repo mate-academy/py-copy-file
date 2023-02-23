6c2f80c34f4f5f4381daf6cf8f1a8af4603b5c7e
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-02-23 21:10:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_name, file, file_copy = command.split(" ")
+    if file != file_copy and command_name == "cd":
+        with (open(file, "r") as file_in,
+              open(file_copy, "w") as file_out):
+            file_out.write(file_in.read())
