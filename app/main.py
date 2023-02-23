d5bf735983933f11b9ccfe3e62676590256612e6
Andy <andy77andy@ukr.net>
2023-02-23 21:31:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
     if command.count(" ") == 2:
-        command, old_file, new_file = command.split()
-        if old_file != new_file:
-            with open(new_file, "w") as f_1, open(old_file, "r") as f_2:
-                f_1.write(f_2.read())
+        command, source_file_path, destination_file_path = command.split()
+        if source_file_path != destination_file_path:
+            with open(destination_file_path, "w") as source_file, open(source_file_path, "r") as destination_file:
+                source_file.write(destination_file.read())
+
