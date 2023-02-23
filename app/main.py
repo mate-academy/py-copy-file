5707f0fc2846179fc7bcf2e3b7785c272347aabf
Aleksandr <lotrikys@gmail.com>
2023-02-23 13:57:40 +0200

    Fix command validate

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 def copy_file(command: str) -> None:
-    command_name, original_file, new_file = command.split()
+    if len(command.split()) == 3:
+        command_name, original_file, new_file = command.split()
+    else:
+        raise Exception("Command should be 'cp file.txt new_file.txt'")
 
     if command_name == "cp" and original_file != new_file:
-        with (open(original_file, "r") as file_in,
-              open(new_file, "w") as file_out):
+        with (
+            open(original_file, "r") as file_in,
+            open(new_file, "w") as file_out
+        ):
             file_out.write(file_in.read())
