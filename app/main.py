2f329a8684c8c4451eba06bacec692fa34fbb401
Zenon <zenion@ukr.net>
2023-02-23 20:01:13 +0200

    Fix

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
     name_of_file = command.split()
-    if name_of_file[1] == name_of_file[2] or name_of_file[1][-4:] != ".txt":
+    if name_of_file[0] != "cp" or name_of_file[1] == name_of_file[2]:
         return
 
     from shutil import copyfile
 
-    copyfile("first.txt', 'second.txt")
+    copyfile(name_of_file[1], name_of_file[2])
