2cb0453c993d53e7f3de05057a4279a682cc5a0a
Zenon <zenion@ukr.net>
2023-02-23 21:05:16 +0200

    Fix import

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
+from shutil import copyfile
+
+
 def copy_file(command: str) -> None:
     name_of_file = command.split()
-    if name_of_file[0] != "cp" or name_of_file[1] == name_of_file[2]:
-        return
-
-    from shutil import copyfile
-
-    copyfile(name_of_file[1], name_of_file[2])
+    if not (name_of_file[0] != "cp" or name_of_file[1] == name_of_file[2]):
+        copyfile(name_of_file[1], name_of_file[2])
