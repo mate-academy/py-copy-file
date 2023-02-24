82a13c433da1a4daaf41a0bcad972292920be8d6
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-02-24 14:18:49 +0200

    new new solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 def copy_file(command: str) -> None:
     parts = command.split(" ")
     if len(parts) == 3:
-        command_name, file, file_copy = parts
-        if file != file_copy and command_name == "cd":
-            with (open(file, "r") as file_in,
-                  open(file_copy, "w") as file_out):
-                file_out.write(file_in.read())
+        command_name, source_file_path, destination_file_path = parts
+        if source_file_path != destination_file_path and command_name == "cd":
+            with (
+                open(source_file_path, "r") as source_file,
+                open(destination_file_path, "w") as destination_file
+            ):
+                destination_file.write(source_file.read())
