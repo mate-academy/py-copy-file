f82f56894c7555cdc8547e160ef49111c2d6ec71
Roman28101 <r.kovalenko28101@gmail.com>
2023-02-25 02:04:04 +0800

    Solution fixed

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 def copy_file(command: str) -> None:
-    split = command.split(" ")
-    if split[1] != split[2] and split[0] == "cp":
-        try:
-            with (open(split[1], "r") as file_in,
-                  open(split[2], "w") as file_out
-                  ):
-                for line in file_in:
-                    file_out.write(line)
-        except FileNotFoundError:
-            print("No such file in directory")
+    sep_command = command.split(" ")
+    if len(sep_command) == 3:
+        if sep_command[1] != sep_command[2] and sep_command[0] == "cp":
+            try:
+                with (
+                    open(sep_command[1], "r") as file_in,
+                    open(sep_command[2], "w") as file_out
+                ):
+                    file_out.write(file_in.read())
+            except FileNotFoundError:
+                print("No such file in directory")
 
 
 copy_file(input("Enter your command: "))
