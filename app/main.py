1fe5a25ffbf139d4c994099c9248592ac0265f74
ZaikaDaria <corplinebusiness@gmail.com>
2023-02-26 01:56:10 +0200

    Add solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    line = command.split()
+    if line[0] != "cp" or line[1] == line[2]:
+        pass
+    else:
+        with open(line[1], "r") as file_in, open(line[2], "w") as file_out:
+            file_out.write(file_in.read())
+
+
+copy_file("cp file.txt new_file.txt")
