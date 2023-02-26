eaaeb7940c82ee4c9f04a216b4ec4a6bf80d8072
WalDragnir <akgk530@gmail.com>
2023-02-26 02:01:39 +0200

    Fixed command validation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 def copy_file(command: str) -> None:
     split_command = command.split()
+    if len(split_command) != 3:
+        raise ValueError("The command must be 'cp file.txt new_file.txt'")
+    if split_command[0] != "cp":
+        raise ValueError("The command should be 'cp'")
     source_file = split_command[1]
     new_file = split_command[2]
     if source_file != new_file:
