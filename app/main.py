c0853f928ba5895ca00d9c2c7023655a6fbe8055
ZaikaDaria <corplinebusiness@gmail.com>
2023-02-27 19:12:16 +0200

    Made changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def copy_file(command: str) -> None:
     line = command.split()
-    if line[0] != "cp" or line[1] == line[2]:
-        pass
-    else:
+    if len(line) != 3 or line[0] != "cp" or line[1] == line[2]:
         with open(line[1], "r") as file_in, open(line[2], "w") as file_out:
             file_out.write(file_in.read())
