fe21815716ef323fd3059d9de9ffb1b36bd1dd29
ZaikaDaria <corplinebusiness@gmail.com>
2023-02-28 20:37:59 +0200

    Fix wrong checkings

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
     line = command.split()
-    if len(line) != 3 or line[0] != "cp" or line[1] == line[2]:
+    if len(line) == 3 or line[0] == "cp" or line[1] != line[2]:
         with open(line[1], "r") as file_in, open(line[2], "w") as file_out:
             file_out.write(file_in.read())
