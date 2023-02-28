6d61a3ce7460e515dabad95808c966690c8c2d1a
ZaikaDaria <corplinebusiness@gmail.com>
2023-02-28 22:52:21 +0200

    Implement unpacking + flake8

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_file(command: str) -> None:
     cmd, filename, new_filename = command.split()
     if len(command.split()) == 3 or cmd == "cp" or filename != new_filename:
-        with open(filename, "r") as file_in, open(new_filename, "w") as file_out:
+        with open(filename, "r") as file_in, \
+             open(new_filename, "w") as file_out:
             file_out.write(file_in.read())
