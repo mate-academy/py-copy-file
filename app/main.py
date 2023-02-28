8253039a63a0eb8c5d4bc91c73e2876dd456dddd
Maksym_Sylantiev <maxym2s@yahoo.com>
2023-02-28 21:47:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 def copy_file(command: str) -> None:
-    if len(command) == 3:
-        cp, current_file_name, copied_file_name = command.split()
+    try:
+        input_command, current_file_name, copied_file_name = command.split()
 
-        if cp == "cp" and current_file_name != copied_file_name:
+        if input_command == "cp" and current_file_name != copied_file_name:
             with (
                 open(f"{current_file_name}.txt", "r") as file_to_copy,
                 open(f"{copied_file_name}.txt", "w") as file_result
             ):
                 file_result.write(file_to_copy.read())
+    except ValueError:
+        return
