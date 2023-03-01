12d8ae12d44fe18bca75eafb5d6d066fba778e5c
ZaikaDaria <corplinebusiness@gmail.com>
2023-03-01 13:42:10 +0200

    Move len(command.split()) before unpacking

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 def copy_file(command: str) -> None:
-    cmd, filename, new_filename = command.split()
-    if len(command.split()) == 3 or cmd == "cp" or filename != new_filename:
-        with open(filename, "r") as file_in, \
-             open(new_filename, "w") as file_out:
-            file_out.write(file_in.read())
+    if len(command.split()) == 3:
+        cmd, filename, new_filename = command.split()
+        if cmd == "cp" or filename != new_filename:
+            with (
+                open(filename, "r") as file_in,
+                open(new_filename, "w") as file_out
+            ):
+                file_out.write(file_in.read())
