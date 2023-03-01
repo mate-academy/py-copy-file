95a195905b54da8bf076406f35984d0306cfce22
Andrew <6594071@gmail.com>
2023-03-01 18:49:55 +0200

    Solution_3

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 def copy_file(command: str) -> None:
     if len(command.split()) == 3:
-        input_command, source_file_path, destination_file_path \
-            = command.split()
+        (
+            input_command,
+            source_file_path,
+            destination_file_path
+        ) = command.split()
         if input_command == "cp" and source_file_path != destination_file_path:
-            with open(source_file_path, "r") as original_file, \
-                    open(destination_file_path, "w") as file_copy:
+            with open(source_file_path, "r") as original_file, open(
+                    destination_file_path, "w"
+            ) as file_copy:
                 file_copy.write(original_file.read())
