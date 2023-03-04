25844403ad2aa190d2f567e7fff8b45383aa8e09
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-03-04 15:46:01 +0200

    Solution_

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    cp, file_1, file_2 = [word for word in command.split()]
+    cp, file_1, file_2 = command.split()
 
     if cp == "cp" and file_1 != file_2:
         with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
