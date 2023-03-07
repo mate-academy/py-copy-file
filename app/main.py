c4e97f22c6e461c8ea6ed37f2ebb36357e8fd104
Vladimir Byshevets <texnadzor.info@gmail.com>
2023-03-07 22:15:18 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command):
+
+    str_line = command.split()
+    if str_line[1] != str_line[2]:
+        with open(str_line[1], "r") as \
+                file_in, open(str_line[2], "w") as file_out:
+            txt = file_in.read()
+            file_out.write(txt)
+        file_in.close()
+        file_out.close()
