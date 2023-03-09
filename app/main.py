5775d0890302f3a5f9d73e56d7b227f8bb4c146f
Kirill Melanich <kirillmelanich@gmail.com>
2023-03-09 17:53:02 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    result = command.split()
+    if result[1] == result[2]:
+        print("Same file")
+    else:
+        with open(result[1], "r") as fin, open(result[2], "w+") as fout:
+            fout.write(fin.read())
+
+    copy_file("cp file.txt file2.txt")
