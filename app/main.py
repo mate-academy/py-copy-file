70eae395002da2d9d4f750d0c195234d5b24925f
Svitlana Savikhina <savihina111@gmail.com>
2023-03-10 19:33:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_names = command.split()
+    if file_names[1] != file_names[2]:
+        with (open(file_names[1], "r") as file1,
+              open(file_names[2], "w") as file2):
+            file2.write(str(file1))
