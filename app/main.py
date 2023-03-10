2b1572f8397c7f9ac1c35bb640ec4add090f85c8
Svitlana Savikhina <savihina111@gmail.com>
2023-03-10 19:35:13 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ def copy_file(command: str) -> None:
     if file_names[1] != file_names[2]:
         with (open(file_names[1], "r") as file1,
               open(file_names[2], "w") as file2):
-            file2.write(str(file1))
+            file2.write(file1.read())
