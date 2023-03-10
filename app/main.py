86511f492bfa1197ce84fdb5adcae79c2e8b0d22
Oleksandr <@gmail.com>
2023-03-10 13:38:18 +0200

    'Fixed_v1'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 def copy_file(command: str) -> None:
-    command, file, new_file = command.split()
-    if command == "cp" and file != new_file:
-        with (open(file, "r") as file_in,
-              open(new_file, "w") as file_out):
+    command, file_name, new_file_name = command.split()
+
+    if len(command.split()) == 3 and command == "cp" and file_name != new_file_name:
+        with (
+            open(file_name, "r") as file_in,
+            open(new_file_name, "w") as file_out
+        ):
             file_out.write(file_in.read())
