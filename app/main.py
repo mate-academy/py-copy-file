c871fcc98a603b34b8a87852a0af0f96ce132c08
kostya-kononenko <kkononenko3@gmail.com>
2023-03-11 12:15:30 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
     command, copied_file, file_to_copy = command.split()
-    if copied_file != file_to_copy:
-        with open(copied_file, "r") as file_in, \
-                open(file_to_copy, "w") as file_out:
-            file_out.write(file_in.read() + "\n")
+    if len(command.split()) == 3:
+        if copied_file != file_to_copy:
+            with open(copied_file, "r") as file_in, \
+                    open(file_to_copy, "w") as file_out:
+                file_out.write(file_in.read() + "\n")
