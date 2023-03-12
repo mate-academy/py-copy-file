7c99db38138befd48b5a06681078cb47538b13f1
Alex Isaev <lesisv@gmail.com>
2023-03-12 10:19:21 +0200

    Solution v3

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def copy_file(command: str) -> None:
     file_out_name = string_list[2]
     if len(string_list) == 3 and instruct == "cp":
         if file_in_name != file_out_name:
-            with open(file_in_name, "r") as file_in, \
-                    open(file_out_name, "w") as file_out:
+            with (open(file_in_name, "r") as file_in,
+                  open(file_out_name, "w") as file_out):
                 file_in = file_in.read()
                 file_out.write(file_in)
