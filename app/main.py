72d81e90589d9e8b72f8067a496e60e00d1fd4e6
Vlad Haponov <vladislav4i4eg@gmail.com>
2023-03-13 14:18:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    f_name = command.split()
+    if f_name[1] != f_name[2]:
+        with open(f_name[1], "r") as file_in, open(f_name[2], "w") as file_out:
+            file_out.write(file_in.read())
+
+
+if __name__ == "__main__":
+    copy_file("cp test1.txt test2.txt")
