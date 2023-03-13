75ea33fe0e26d193637ef37aa15faf8cb15c5ea3
Vlad Haponov <vladislav4i4eg@gmail.com>
2023-03-13 18:15:37 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
     if len(command.split()) == 3:
         command, copied_file, file_to_copy = command.split()
-        if command == "cp":
-            if copied_file != file_to_copy:
-                with open(copied_file, "r") as copied_file,\
-                        open(file_to_copy, "w") as file_to_copy:
-                    file_to_copy.write(copied_file.read())
+        if command == "cp" and copied_file != file_to_copy:
+            with (open(copied_file, "r") as copied_file,
+                  open(file_to_copy, "w") as file_to_copy):
+                file_to_copy.write(copied_file.read())
