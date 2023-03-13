68084ed0fc294259a1fad0f63c89a64cb594b688
Vlad Haponov <vladislav4i4eg@gmail.com>
2023-03-13 17:45:06 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(command: str) -> None:
-    f_name = command.split()
-    if f_name[1] != f_name[2]:
-        with open(f_name[1], "r") as file_in, open(f_name[2], "w") as file_out:
-            file_out.write(file_in.read())
-
-
-if __name__ == "__main__":
-    copy_file("cp test1.txt test2.txt")
+    if len(command.split()) == 3:
+        command, copied_file, file_to_copy = command.split()
+        if command == "cp":
+            if copied_file != file_to_copy:
+                with open(copied_file, "r") as copied_file,\
+                        open(file_to_copy, "w") as file_to_copy:
+                    file_to_copy.write(copied_file.read())
