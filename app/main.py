45f62f1777e4f2b98e8366bfc57d864018f0e4bd
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-18 12:41:49 +0200

    Create command_list var, use parentheses in with operator

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 def copy_file(command: str) -> None:
-    file_name = command.split()[1]
-    file_copy_name = command.split()[2]
+    command_list = command.split()
+    file_name = command_list[1]
+    file_copy_name = command_list[2]
 
     if file_name == file_copy_name:
         return
 
-    with open(file_copy_name, "r") as file_in, \
-            open(file_name, "w") as file_out:
+    with (open(file_copy_name, "r") as file_in,
+            open(file_name, "w") as file_out):
         file_in.write(file_out.read())
