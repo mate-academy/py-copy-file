2117c6baab9742670fdac96d5beb3a4bbfc8734e
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-18 08:27:39 +0200

    Finish task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_name = command.split()[1]
+    file_copy_name = command.split()[2]
+
+    if file_name == file_copy_name:
+        return
+
+    with open(file_copy_name, "r") as file_in, \
+            open(file_name, "w") as file_out:
+        file_in.write(file_out.read())
