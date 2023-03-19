bc9423416e919543a115c378eaeb25fdc53df443
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-19 09:58:48 +0200

    Add code validation, fix logic

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 def copy_file(command: str) -> None:
     command_list = command.split()
-    file_name = command_list[1]
-    file_copy_name = command_list[2]
 
-    if file_name == file_copy_name:
+    if (
+            len(command_list) != 3
+            or command_list[0] != "cp"
+            or command_list[1][-3:] != "txt"
+            or command_list[2][-3:] != "txt"
+    ):
+        print("The command must be in the following format:")
+        print("cp file.txt file-copy.txt")
         return
 
-    with (open(file_copy_name, "r") as file_in,
-            open(file_name, "w") as file_out):
-        file_in.write(file_out.read())
+    source_name = command_list[1]
+    copy_name = command_list[2]
+
+    if source_name == copy_name:
+        print("Name of original file and it's copy must be different")
+        return
+
+    with (open(source_name, "r") as source,
+            open(copy_name, "w") as copy):
+        copy.write(source.read())
