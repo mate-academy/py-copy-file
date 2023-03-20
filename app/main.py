b91a75828bfa07af5a5d58cf0b9b17a6340a31c2
Iryna Ishchenko <irynaira@gmail.com>
2023-03-20 18:08:56 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+from shutil import copy
+
+
+def copy_file(command: str) -> None:
+    _, filename1, filename2 = command.split()
+    if filename1 != filename2:
+        copy(filename1, filename2)
