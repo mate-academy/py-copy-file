525be362c0e05c21fbb1d7aea11fee2e8858405e
iishchenko <irynaira@gmail.com>
2023-03-22 12:35:37 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from shutil import copy
-
-
 def copy_file(command: str) -> None:
-    _, filename1, filename2 = command.split()
-    if filename1 != filename2:
-        copy(filename1, filename2)
+    if len(command.split()) == 3:
+        command, file_original, file_copy = command.split()
+        if command == "cp" and file_original != file_copy:
+            with (
+                open(file_original, "r") as read,
+                open(file_copy, "w") as write
+            ):
+                write.write(read.read())
+                
