e6c77049ea078db6c2ba9bdd51f4c154951ba386
Anastasiia <nastyashch1601@gmail.com>
2023-03-23 15:48:19 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cmd, file_original, file_copy = command.split(" ")
+    if cmd == "cp" and file_original != file_copy:
+        with open(file_original, "r") as file_in, open(
+            file_copy, "w"
+        ) as file_out:
+            for line in file_in:
+                file_out.write(line)
