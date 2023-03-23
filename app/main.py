dd04d375421bfc0025ca1435c73ca620aab8be4b
Igor Starodub <igorastar@gmail.com>
2023-03-23 11:09:04 +0200

    Solo2

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 def copy_file(command: str) -> None:
     file_names = command.split()
-    if len(file_names) != 3 or file_names[0] != "cp" or file_names[1] == file_names[2]:
+    if len(file_names) != 3\
+            or file_names[0] != "cp"\
+            or file_names[1] == file_names[2]:
         return
     try:
-        with open(file_names[1], "r") as file_in, open(file_names[2], "w") as file_out:
+        with open(file_names[1], "r") as file_in,\
+                open(file_names[2], "w") as file_out:
             file_out.write(file_in.read())
     except OSError:
         print("File does not exist")
