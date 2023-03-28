a41cb1f26de4e9da25adf7c4c75153b8c69a47e8
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-03-28 23:02:00 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
-    parameters = command.split(" ")
+    parameters = command.split()
 
-    if len(parameters) == 3 \
-            and parameters[0] == "cp" \
-            and parameters[1] != parameters[2]:
+    if (len(parameters) == 3
+            and parameters[0] == "cp"
+            and parameters[1] != parameters[2]):
         new_file = parameters[2]
         old_file = parameters[1]
 
