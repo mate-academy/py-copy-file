edf14593235e878d8bef6ba3fdca45b6fd30463d
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-03-29 14:54:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def copy_file(command: str) -> None:
-    parameters = command.split()
+    parameters = command.split(" ")
+    old_file = parameters[1]
+    new_file = parameters[2]
 
-    if (len(parameters) == 3
+    if (
+            len(parameters) == 3
             and parameters[0] == "cp"
-            and parameters[1] != parameters[2]):
-        new_file = parameters[2]
-        old_file = parameters[1]
-
-        with (open(old_file, "r") as file_in,
-              open(new_file, "w") as file_out):
+            and old_file != new_file
+    ):
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             file_out.write(file_in.read())
