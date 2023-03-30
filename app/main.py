f261c3dc05fe4cd8345563763c009c263a1ac45f
Alexandr <olexandrzhezheria@gmail.com>
2023-03-30 03:53:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(cp: str) -> None:
+    tokens = cp.split()
+    if len(tokens) != 3:
+        raise ValueError("Invalid command")
+    source_file, dest_file = tokens[1], tokens[2]
+    if source_file == dest_file:
+        return
+    with open(source_file, "r") as file_in, open(dest_file, "w") as file_out:
+        file_out.write(file_in.read())
