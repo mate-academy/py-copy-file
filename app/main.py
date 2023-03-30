03c0c0e01334ecf53261f634c645b64faeb9d194
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-03-30 17:49:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 def copy_file(command: str) -> None:
-    parameter, old_file, new_file = command.split()
+    if len(command.split()) == 3:
+        parameter, old_file, new_file = command.split()
 
-    if (parameter == "cp"
-            and old_file != new_file):
-        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-            file_out.write(file_in.read())
-    else:
-        raise ValueError("Parameter must be 'cp'")
+        if (parameter == "cp"
+                and old_file != new_file):
+            with (open(old_file, "r") as file_in,
+                  open(new_file, "w") as file_out):
+                file_out.write(file_in.read())
+        else:
+            raise ValueError("Parameter must be 'cp'")
