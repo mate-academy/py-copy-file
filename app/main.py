e95d8b2a8ae50becc443da1ba97f02c9ce6b7cba
Halyna Petrova <1208petrova@gmail.com>
2023-04-01 11:23:01 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
-    new_file = command.split(" ")
-    if new_file[1] != new_file[2]:
-        with open(new_file[1], "r") as file_in, \
-                open(new_file[2], "w") as file_out:
-            file_out.write(file_in.read())
+    if len(command.split()) == 3:
+        new_file = command.split()
+        if command[0] == "cp" and new_file[1] != new_file[2]:
+            with (open(new_file[1], "r") as file_in,
+                  open(new_file[2], "w") as file_out):
+                file_out.write(file_in.read())
