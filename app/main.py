1bf740de9a86540df70d5cf64d928a9d2e3e583e
Dmytro Shvets <dirolius@gmail.com>
2023-04-02 21:46:46 +0300

    copy file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
-    old_file_name = command.split()[1]
-    new_file_name = command.split()[2]
+    splitted_command = command.split()
+    old_file_name = splitted_command[1]
+    new_file_name = splitted_command[2]
     if old_file_name != new_file_name:
         with (open(old_file_name, "r") as file_in,
               open(new_file_name, "w") as file_out):
