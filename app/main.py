bf716dae59aad2f6a20221324557e2a15498d2c8
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-03 22:03:16 +0300

    added check to parameters count

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    sp_com = command.split()
-    if sp_com[0] == "cp" and sp_com[1] != sp_com[2]:
+    sp_com = command.split(" ")
+    if len(sp_com) == 3 and sp_com[0] == "cp" and sp_com[1] != sp_com[2]:
         with open(sp_com[1], "r") as file_in, open(sp_com[2], "w") as file_out:
             file_out.write(file_in.read())
