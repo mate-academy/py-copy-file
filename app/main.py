5bb382368c969c99ac5e281b0aa20a169cd8cec5
Sasha Semenenko <semenenko233249@gmail.com>
2023-04-04 17:20:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(command: str) -> None:
-    if len(command.split()) != 3:
-        raise ValueError("The command must have 3 arguments")
-    command_cp, file_name, file_to_copy = command.split()
-    if file_name == file_to_copy:
-        raise TypeError(f"{file_name} are the same name {file_to_copy}")
-    if command_cp == "cp":
-        with open(file_name, "r") as parent_file, open(file_to_copy, "w") as file_copy:
-            file_copy.write(parent_file.read())
+    if len(command.split()) == 3:
+        command_cp, file_name, file_to_copy = command.split()
+        if file_name == file_to_copy:
+            raise TypeError(f"{file_name} are the same name {file_to_copy}")
+        if command_cp == "cp":
+            with open(file_name, "r") as parent_file, open(file_to_copy, "w") as file_copy:
+                file_copy.write(parent_file.read())
