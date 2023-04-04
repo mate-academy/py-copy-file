039cc848874bf057562ebc71f60f0132752a6ef9
Sasha Semenenko <semenenko233249@gmail.com>
2023-04-04 15:56:40 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_cp, file_name, file_to_copy = command.split()
+    if file_name == file_to_copy:
+        raise TypeError(f"{file_name} are the same name {file_to_copy}")
+    if command_cp == "cp" and file_name != file_to_copy:
+        with open("file.txt", "r") as parent_file, \
+                open("new_file.txt", "w") as file_copy:
+            recent_file = parent_file.read()
+            file_copy.write(recent_file)
