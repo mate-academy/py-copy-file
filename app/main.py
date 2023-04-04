e2d84659d29b228215e26bb8a375a4e9f48bff6a
Sasha Semenenko <semenenko233249@gmail.com>
2023-04-04 18:40:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
-    if len(command.split()) == 3:
-        command_cp, file_name, file_to_copy = command.split()
-        if file_name == file_to_copy:
-            raise TypeError(f"{file_name} are the same name {file_to_copy}")
-        if command_cp == "cp":
-            with open(file_name, "r") as parent_file:
-                with open(file_to_copy, "w") as file_copy:
-                    file_copy.write(parent_file.read())
+    update_command = command.split()
+    if len(update_command) == 3:
+        if update_command[1] == update_command[2]:
+            raise TypeError(f"{update_command[1]} are the same name {update_command[2]}")
+        if update_command[0] == "cp":
+            with open(update_command[1], "r") as parent_file, \
+                    open(update_command[2], "w") as file_copy:
+                file_copy.write(parent_file.read())
