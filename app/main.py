b62734c430c6005d99e1f7ea027aaa52ca1572f8
Mykola <nickkozlov90@gmail.com>
2023-04-04 12:18:24 +0300

    corrections applied

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 def copy_file(command: str) -> None:
     files_names = command.split()
-    if files_names[1] == files_names[2]:
+    file_to_copy = files_names[1]
+    new_file = files_names[2]
+    if file_to_copy == new_file:
         return
-    with open(f"{files_names[1]}", "r") as file_in, \
-            open(f"{files_names[2]}", "w") as file_out:
+    with (open(f"{file_to_copy}", "r") as file_in,
+          open(f"{new_file}", "w") as file_out):
         content = file_in.read()
         file_out.write(content)
