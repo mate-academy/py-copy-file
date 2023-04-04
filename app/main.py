9b3862961cd3bfcf349f24991a9982aa8668e088
Sasha Semenenko <semenenko233249@gmail.com>
2023-04-04 18:45:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ def copy_file(command: str) -> None:
     update_command = command.split()
     if len(update_command) == 3:
         if update_command[1] == update_command[2]:
-            raise TypeError(f"{update_command[1]} are the same name {update_command[2]}")
+            raise TypeError(
+                f"{update_command[1]} are the same name {update_command[2]}"
+            )
         if update_command[0] == "cp":
             with open(update_command[1], "r") as parent_file, \
                     open(update_command[2], "w") as file_copy:
