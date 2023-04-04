a8f58ba2d15135a2c1cf0c61107c2cd38fe59863
Sasha Semenenko <semenenko233249@gmail.com>
2023-04-04 17:44:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,5 +4,6 @@ def copy_file(command: str) -> None:
         if file_name == file_to_copy:
             raise TypeError(f"{file_name} are the same name {file_to_copy}")
         if command_cp == "cp":
-            with open(file_name, "r") as parent_file, open(file_to_copy, "w") as file_copy:
-                file_copy.write(parent_file.read())
+            with open(file_name, "r") as parent_file:
+                with open(file_to_copy, "w") as file_copy:
+                    file_copy.write(parent_file.read())
