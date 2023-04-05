b0b37f83a68f2f5c216f722bc332e3f6bfceb5fa
Bohdan Marko <goldenbohdanm@gmail.com>
2023-04-05 17:52:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        raise ValueError("Missed some arguments.")
+    cp, file_in, file_out = command.split()
+    if cp != "cp":
+        raise ValueError("Incorrect command.")
+    if file_in == file_out:
+        raise ValueError("Incorrect FileIn and FileOut.")
+    with open(file_in, "r") as f_in, open(file_out, "w") as f_out:
+        for line in f_in:
+            f_out.write(line)
