741caaa0502ec9f350a2f2ddaa64c8548177e9fe
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-05 20:57:42 +0300

    corected code solutions with decorator

--- app/main.py
+++ app/main.py
@@ -7,7 +7,11 @@ def copy_checks(func: Callable) -> Any:
         command_name = words[0]
         name_to_copy = words[1]
         new_file_name = words[2]
-        if len(words) != 3 or command_name != "cp" or name_to_copy == new_file_name:
+        if (
+                len(words) != 3
+                or command_name != "cp"
+                or name_to_copy == new_file_name
+        ):
             return None
         return func(command)
     return wrapper
@@ -17,5 +21,8 @@ def copy_checks(func: Callable) -> Any:
 def copy_file(command: str) -> None:
     name_to_copy = command.split(" ")[1]
     new_file_name = command.split(" ")[2]
-    with open(name_to_copy, "r") as file_in, open(new_file_name, "w") as file_out:
+    with (
+        open(name_to_copy, "r") as file_in,
+        open(new_file_name, "w") as file_out
+    ):
         file_out.write(file_in.read())
