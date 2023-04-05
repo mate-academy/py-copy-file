68cb06e8522816a2f52cb5b5fef3b8daed0948c5
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-05 21:23:49 +0300

    shorted solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,9 @@
 def copy_file(command: str) -> None:
-    words = command.split(" ")
-    command_name = words[0]
-    name_to_copy = words[1]
-    new_file_name = words[2]
-    if (
-            len(words) == 3
-            and command_name == "cp"
-            and name_to_copy != new_file_name
-    ):
-        with (
-            open(name_to_copy, "r") as file_in,
-            open(new_file_name, "w") as file_out
-        ):
-            file_out.write(file_in.read())
+    if len(command.split()) == 3:
+        command_name, name_to_copy, new_file_name = command.split()
+        if command_name == "cp" and name_to_copy != new_file_name:
+            with (
+                open(name_to_copy, "r") as file_in,
+                open(new_file_name, "w") as file_out
+            ):
+                file_out.write(file_in.read())
