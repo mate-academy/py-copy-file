13f786c22701d51c599f7e00935710a85e28a187
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-05 21:04:04 +0300

    shorted solution with more detailed var names

--- app/main.py
+++ app/main.py
@@ -4,13 +4,12 @@ def copy_file(command: str) -> None:
     name_to_copy = words[1]
     new_file_name = words[2]
     if (
-            len(words) != 3
-            or command_name != "cp"
-            or name_to_copy == new_file_name
+            len(words) == 3
+            and command_name == "cp"
+            and name_to_copy != new_file_name
     ):
-        return None
-    with (
-        open(name_to_copy, "r") as file_in,
-        open(new_file_name, "w") as file_out
-    ):
-        file_out.write(file_in.read())
+        with (
+            open(name_to_copy, "r") as file_in,
+            open(new_file_name, "w") as file_out
+        ):
+            file_out.write(file_in.read())
