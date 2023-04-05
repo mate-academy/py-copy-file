85be39655d8f061ef4bbcb734b38f48fb127fcb8
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-05 21:00:35 +0300

    solution with more detailed var names

--- app/main.py
+++ app/main.py
@@ -1,26 +1,14 @@
-from typing import Callable, Any
-
-
-def copy_checks(func: Callable) -> Any:
-    def wrapper(command: str) -> Any:
-        words = command.split(" ")
-        command_name = words[0]
-        name_to_copy = words[1]
-        new_file_name = words[2]
-        if (
-                len(words) != 3
-                or command_name != "cp"
-                or name_to_copy == new_file_name
-        ):
-            return None
-        return func(command)
-    return wrapper
-
-
-@copy_checks
 def copy_file(command: str) -> None:
-    name_to_copy = command.split(" ")[1]
-    new_file_name = command.split(" ")[2]
+    words = command.split(" ")
+    command_name = words[0]
+    name_to_copy = words[1]
+    new_file_name = words[2]
+    if (
+            len(words) != 3
+            or command_name != "cp"
+            or name_to_copy == new_file_name
+    ):
+        return None
     with (
         open(name_to_copy, "r") as file_in,
         open(new_file_name, "w") as file_out
