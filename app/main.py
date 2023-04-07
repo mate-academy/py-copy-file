238c35bd3a0ba674f5fa5002ec9456ea86934c46
“Ruslan <callogan217@gmail.com>
2023-04-07 18:31:22 +0300

    Fourth commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 # write your code here
 
 def copy_file(command: str) -> None:
-    if len(command.split()) == 3 and "cp" in command:
-        _, source_path, destination_path = command.split()
+    comparison_list = command.split()
+    if len(comparison_list) == 3 and "cp" in command:
+        _, source_path, destination_path = comparison_list
     if source_path != destination_path:
         with open(source_path, "r") as source_file, \
                 open(destination_path, "w") as destination_file:
