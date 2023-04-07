5c5c1fc4fe501678c725d6857bf60c87fc44a569
“Ruslan <callogan217@gmail.com>
2023-04-07 17:35:32 +0300

    Second commit

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
 # write your code here
 
-
 def copy_file(command: str) -> None:
-    _, src_file, dest_file = command.split()
-    if src_file != dest_file:
-        with open(src_file, "r") as file_in, open(dest_file, "w") as file_out:
-            file_out.write(file_in.read())
-
-
-print(copy_file("cp file.txt new_file.txt"))
+    if len(command.split()) == 3 and "cp" in command:
+        _, source_path, destination_path = command.split()
+    if source_path != destination_path:
+        with open(source_path, "r") as source_file, open(destination_path, "w") as destination_file:
+            destination_file.write(source_file.read())
