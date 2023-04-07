142e982c09556fc8857505b12d13ac5042100c8a
“Ruslan <callogan217@gmail.com>
2023-04-07 14:24:41 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
 # write your code here
+
+
+def copy_file(command: str) -> None:
+    _, src_file, dest_file = command.split()
+    if src_file != dest_file:
+        with open(src_file, "r") as file_in, open(dest_file, "w") as file_out:
+            file_out.write(file_in.read())
+
+
+print(copy_file("cp file.txt new_file.txt"))
