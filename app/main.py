d58c191bce010ad6dcebae16bfadc94a5693a08a
Volodymyr <elohimwz@gmail.com>
2023-04-09 11:25:00 +0300

    'Solution#copyFile'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    part_of_command = command.split(" ")
+    if part_of_command[0] == "cp":
+        if part_of_command[1] == part_of_command[2]:
+            return
+        with (
+            open(part_of_command[1], "r") as file_in,
+            open(part_of_command[2], "w") as file_out
+        ):
+            copy = file_in.read()
+            file_out.write(copy)
