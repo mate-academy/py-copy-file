8a0b35a36df09b7d794ff4be6e9dcacf2b359985
Dmytry Bondariev <bondarievmd@gmail.com>
2023-04-10 14:40:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    listed_command = command.split()
+    first_file = listed_command[1]
+    second_file = listed_command[2]
+    if first_file != second_file:
+        with open(first_file, "r") as f, \
+                open(second_file, "w") as s:
+            text_to_copy = str(f.read())
+            s.write(text_to_copy)
+
+
+if __name__ == "__main__":
+    copy_file("cp text.txt new_file.txt")
