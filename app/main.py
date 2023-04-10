f41c1d248fafddfce32fa73930a0ac77e787356a
Dmytry Bondariev <bondarievmd@gmail.com>
2023-04-10 15:25:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,12 +2,11 @@ def copy_file(command: str) -> None:
     listed_command = command.split()
     first_file = listed_command[1]
     second_file = listed_command[2]
-    if first_file != second_file:
-        with open(first_file, "r") as f, \
-                open(second_file, "w") as s:
-            text_to_copy = str(f.read())
-            s.write(text_to_copy)
+    if first_file != second_file and len(listed_command) == 3:
+        with open(first_file, "r") as old_file, \
+                open(second_file, "w") as new_file:
+            text_to_copy = str(old_file.read())
+            new_file.write(text_to_copy)
 
 
-if __name__ == "__main__":
-    copy_file("cp text.txt new_file.txt")
+copy_file("cp text.txt new_file1.txt")
