a25b18263bfd0e8575f0a23c1fa09c51668a4d36
Oleksandr Yanchuk <sashajanchuck13@gmail.com>
2023-04-10 14:24:28 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_split = command.split()
+    old_file = command_split[1]
+    new_file = command_split[2]
+    if len(command_split) != 3 or command_split[0] != "cp":
+        return
+    if old_file == new_file:
+        return
+    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
