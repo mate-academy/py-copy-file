2ccd61b3049a08a96bf6547ba0cf24d355959f49
Oleksandr Yanchuk <sashajanchuck13@gmail.com>
2023-04-10 14:47:03 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     command_split = command.split()
-    if len(command_split) != 3 or command_split[0] != "cp":
+    if len(command_split) != 3:
         return
     command, source_path, destination_path = command_split
     if command != "cp" or source_path == destination_path:
