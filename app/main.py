54c6c8e0aad518899a9e948f218de1b98c2f40b5
Oleksandr Yanchuk <sashajanchuck13@gmail.com>
2023-04-10 14:44:33 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 def copy_file(command: str) -> None:
     command_split = command.split()
-    old_file = command_split[1]
-    new_file = command_split[2]
     if len(command_split) != 3 or command_split[0] != "cp":
         return
-    if old_file == new_file:
+    command, source_path, destination_path = command_split
+    if command != "cp" or source_path == destination_path:
         return
-    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+    with \
+            open(source_path, "r") as file_in, \
+            open(destination_path, "w") as file_out:
         file_out.write(file_in.read())
