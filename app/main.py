35b76a7c529198d62d5746373bacf0a29bdb2448
Tatasha <NatashaLeonidovna319@gmail.com>
2023-04-14 00:01:58 +0300

    'changes2'

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 def copy_file(command: str) -> None:
-    cmd = command.split(" ")
-    if cmd[1] == cmd[2]:
+    command_line = command.split(" ")
+    file_1 = command_line[1]
+    file_2 = command_line[2]
+    if file_1 == file_2:
         pass
 
-    f1 = open(cmd[1], "r")
-    f2 = open(cmd[2], "w")
-    date = f1.read()
-    f2.write(str(date))
-    f1.close()
-    f2.close()
+    with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
+        date = file_in.read()
+        file_out.write(str(date))
