6f376b26f036d57042762cf6b116e2429b242eae
Sasha Semenenko <semenenko233249@gmail.com>
2023-04-15 18:57:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,9 +2,8 @@ def copy_file(command: str) -> None:
     update_command = command.split()
     if len(update_command) == 3:
         command_flag, current_file, file_copy = update_command
-
         if current_file == file_copy:
-            raise TypeError(f"{current_file} are the same name {file_copy}")
+            return
         if command_flag == "cp":
             with open(current_file, "r") as parent_file, \
                     open(file_copy, "w") as file_copy:
