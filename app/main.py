219848ab2fbc024dbf3f549f73027b6c3c72c225
Volodymyr Borovets <vborovets0@gmail.com>
2023-04-15 20:49:28 +0300

    rename files, add exception if len !=3

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-def copy_file(command: str) -> None:
-    com, first_file, second_file = command.split()
-    if first_file == second_file or com != "cp":
+def copy_file(command_line: str) -> None:
+    info = command_line.split()
+    if len(info) != 3:
         return
-    with open(first_file, "r") as file_in, open(second_file, "w") as file_out:
+    command, old_file, new_file = info
+    if old_file == new_file or command != "cp":
+        return
+    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
         file_out.write(file_in.read())
-
-
-copy_file("cp file.txt new_file.txt")
