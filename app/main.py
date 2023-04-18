820c9e9ff01f727e623a1e1343d50fcfc07df210
Tatasha <NatashaLeonidovna319@gmail.com>
2023-04-18 15:59:05 +0300

    'changes3'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 def copy_file(command: str) -> None:
     command_line = command.split(" ")
-    file_1 = command_line[1]
-    file_2 = command_line[2]
-    if file_1 == file_2:
-        pass
-
-    with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
-        date = file_in.read()
-        file_out.write(str(date))
+    command, file_1, file_2 = command_line
+    if file_1 != file_2 and command == "cp":
+        with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
+            date = file_in.read()
+            file_out.write(str(date))
