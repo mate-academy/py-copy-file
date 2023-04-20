e9b22dd28c65a57bb959e87573db71975bbc5067
Tatasha <NatashaLeonidovna319@gmail.com>
2023-04-20 17:16:55 +0300

    'changes4'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 def copy_file(command: str) -> None:
-    command_line = command.split(" ")
-    command, file_1, file_2 = command_line
-    if file_1 != file_2 and command == "cp":
-        with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
-            date = file_in.read()
-            file_out.write(str(date))
+    command_line = command.split()
+    if len(command_line) == 3:
+        command, source_path, copy_path = command_line
+        if source_path != copy_path and command == "cp":
+            with open(source_path, "r") as source, \
+                    open(copy_path, "w") as copy:
+                date = source.read()
+                copy.write(str(date))
