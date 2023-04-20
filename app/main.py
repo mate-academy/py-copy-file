fbe3d39ce8d6d71cedead101e0336f73ec6ad95f
Dmytro Kucherenko <dimakucherenko91@gmail.com>
2023-04-20 17:29:53 +0300

    'SolutionNew'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 def copy_file(command: str) -> None:
     new_command = command.split()
-    if new_command[1] != new_command[2]:
+    if (
+            new_command[1] != new_command[2]
+            and len(command.split()) == 3
+            and command.split()[0] == "cp"
+    ):
         with (
             open(new_command[1], "r") as file_in,
             open(new_command[2], "w") as file_out
