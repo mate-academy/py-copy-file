33bfad687587ce752dd0e1b56af502c1de97d01f
Dmytro Kucherenko <dimakucherenko91@gmail.com>
2023-04-20 16:24:06 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    new_command = command.split()
+    if new_command[1] != new_command[2]:
+        with (
+            open(new_command[1], "r") as file_in,
+            open(new_command[2], "a") as file_out
+        ):
+            file_out.write(file_in.read())
