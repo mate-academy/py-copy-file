d11912f275b695c4625fde90e3b58a8c7e8e2398
Dmytro Kucherenko <dimakucherenko91@gmail.com>
2023-04-20 16:25:07 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ def copy_file(command: str) -> None:
     if new_command[1] != new_command[2]:
         with (
             open(new_command[1], "r") as file_in,
-            open(new_command[2], "a") as file_out
+            open(new_command[2], "w") as file_out
         ):
             file_out.write(file_in.read())
