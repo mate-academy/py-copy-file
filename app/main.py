79f4da32c65053240962f2c67957683e7dbfef6b
Maksym Filipchuk <maxfiliya@gmail.com>
2023-04-23 00:31:48 +0200

    Fixed 2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(command: str) -> None:
-    command_type = command.split()[0]
-    file_in = command.split()[1]
-    file_out = command.split()[2]
+    command = command.split()
+    command_type, file_in, file_out = command
     if command_type == "cp" and file_in != file_out:
         with open(file_in, "r") as file_orig, open(file_out, "w") as file_copy:
             file_copy.write(file_orig.read())
