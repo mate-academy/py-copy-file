990feca877fc6b87e1390fdb13c4e5ca2f1dcc17
Illya Svyshcho <iill44iikk@gmail.com>
2023-04-24 19:36:40 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -6,13 +6,7 @@ def copy_file(command: str) -> None:
             len(command.split()) != 3 or\
             command.split()[0] != "cp":
         return
-    with open(names[1], "r") as f_r, open(names[2], "w") as f_w:
-        arr_data = f_r.readlines()
-        arr_data_checked = []
-        for i in arr_data:
-            arr_data_checked.append(i.strip("\n"))
-        for line in arr_data_checked:
-            f_w.write(line + "\n")
-
-
-copy_file("cp test1.txt file8.txt")
+    with open(names[1], "r") as file_read, open(names[2], "w") as file_write:
+        arr_data = file_read.readlines()
+        for line in arr_data:
+            file_write.write(line + "\n")
