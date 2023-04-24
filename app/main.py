8695d3028d1e11e597aedfe5e1aab236babe11ea
Illya Svyshcho <iill44iikk@gmail.com>
2023-04-24 17:54:14 +0300

    Solution of copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+
+
+def copy_file(command: str) -> None:
+    names = command.split()
+    if names[1] == names[2] or\
+            len(command.split()) != 3 or\
+            command.split()[0] != "cp":
+        return
+    with open(names[1], "r") as f_r, open(names[2], "w") as f_w:
+        arr_data = f_r.readlines()
+        arr_data_checked = []
+        for i in arr_data:
+            arr_data_checked.append(i.strip("\n"))
+        for line in arr_data_checked:
+            f_w.write(line + "\n")
+
+
+copy_file("cp test1.txt file8.txt")
