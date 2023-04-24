bd8ac9df562a5853d25a63e1aa8b11f1c50b6a61
Illya Svyshcho <iill44iikk@gmail.com>
2023-04-24 20:08:39 +0300

    Fixed 2

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@
 
 def copy_file(command: str) -> None:
     names = command.split()
-    if names[1] == names[2] or\
-            len(command.split()) != 3 or\
-            command.split()[0] != "cp":
+    if len(command.split()) != 3 \
+            or command.split()[0] != "cp" \
+            or names[1] == names[2]:
         return
     with open(names[1], "r") as file_read, open(names[2], "w") as file_write:
         arr_data = file_read.readlines()
