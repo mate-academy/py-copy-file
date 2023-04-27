ffa7ae79c8e7f29a0b5cefbf24614404a5a8c407
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-04-27 12:13:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        raise ValueError("command should have 3 arguments")
+    src_file_path = command[1]
+    dst_file_path = command[2]
+    if src_file_path == dst_file_path:
+        raise TypeError
+    with open(src_file_path, "r") as file_in,\
+            open(dst_file_path, "w") as file_out:
+        file_in.write(file_out.read())
