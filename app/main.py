515109f6aedfc5a6389bf0fa22a1ae2e6dda1ef7
chipa <mezismezis@gmail.com>
2023-04-30 19:42:47 +0300

    done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    old_file, new_file = command.split(" ")[1:]
+    if old_file != new_file:
+        with open(old_file, "r") as old, open(new_file, "w") as new:
+            new.write(old.read())
