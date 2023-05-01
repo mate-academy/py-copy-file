0fea86991397e3dd95bf0d6938987e12b6c2977f
chipa <mezismezis@gmail.com>
2023-05-01 15:19:31 +0300

    checked if command correct(is there 'cp' in)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    old_file, new_file = command.split(" ")[1:]
-    if old_file != new_file:
+    cp, old_file, new_file = command.split(" ")
+    if cp == "cp" and old_file != new_file:
         with open(old_file, "r") as old, open(new_file, "w") as new:
             new.write(old.read())
