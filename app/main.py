985340c4e537b4261689b3aed07f29adc109099d
chipa <mezismezis@gmail.com>
2023-05-01 16:23:41 +0300

    maybe like this?

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
-def copy_file(command: str) -> None:
-    cp, old_file, new_file = command.split(" ")
-    if cp == "cp" and old_file != new_file:
-        with open(old_file, "r") as old, open(new_file, "w") as new:
-            new.write(old.read())
+def copy_file(command: str) -> None or str:
+    try:
+        cp, old_file, new_file = command.split(" ")
+    except ValueError:
+        return "invalid command"
+
+    if cp != "cp":
+        return "unknown command"
+
+    if old_file != new_file:
+        with open(old_file, "r") as copy_from, open(new_file, "w") as copy_to:
+            copy_to.write(copy_from.read())
