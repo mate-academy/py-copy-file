d67081da37914ec13ce1e2840368a73ce7686680
MaksymUK <clear>
2023-05-01 22:52:30 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
 # write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split(" ")
+    if len(split_command) < 3 or split_command[1] == split_command[2]:
+        return
+    else:
+        with open(
+                split_command[1], "r"
+        ) as file_in, open(
+            split_command[2], "w"
+        ) as file_out:
+            file_content = file_in.read()
+            file_out.write(file_content)
