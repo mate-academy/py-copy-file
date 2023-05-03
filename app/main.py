036c3bfd0d5e9fef298f4a371a063fc5d5e0c1c8
Alim Selemetov <alimselemetow@gmail.com>
2023-05-03 20:44:13 +0300

    use unpacking instead using indexes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 def copy_file(command: str) -> None:
     new_list = command.split()
-    file_needs_copy = new_list[1]
-    new_copy_of_file = new_list[2]
-    if file_needs_copy == new_copy_of_file:
-        return
-    with (
-        open(file_needs_copy, "r") as file_1,
-        open(new_copy_of_file, "w") as file_2
-    ):
-        file_2.write(file_1.read())
+    if "cp" in command:
+        abr_command, file_needs_copy, new_copy_of_file = new_list
+        if file_needs_copy == new_copy_of_file:
+            return
+        with (
+            open(file_needs_copy, "r") as file_1,
+            open(new_copy_of_file, "w") as file_2
+        ):
+            file_2.write(file_1.read())
