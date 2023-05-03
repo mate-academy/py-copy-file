71219361a1b8329721bfaefb783d7aad0b7b887e
Alim Selemetov <alimselemetow@gmail.com>
2023-05-03 18:20:51 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    new_list = command.split()
+    if new_list[1] == new_list[2]:
+        return
+    with (
+        open(new_list[1], "r") as file_need_to_copy,
+        open(new_list[2], "w") as copied_file
+    ):
+        copied_file.write(file_need_to_copy.read())
