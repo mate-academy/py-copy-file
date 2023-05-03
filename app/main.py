3380cdf11f184a737d4b2c2a1c705e84db5815ec
MaksymUK <clear>
2023-05-03 22:33:31 +0100

    Changes

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 def copy_file(command: str) -> str:
-    cp_command, file_in_name, file_out_name = command.split()
+    split_command = command.split()
+    cp_command, file_in_name, file_out_name = split_command
 
     if (
-            len(command.split()) < 3
+            len(split_command) < 3
             or file_in_name == file_out_name
             or cp_command != "cp"
     ):
         return "Something has gone wrong. Please check command entry"
-    with open(
-            file_in_name, "r"
-    ) as file_in, open(
-        file_out_name, "w"
-    ) as file_out:
+
+    with open(file_in_name, "r") as file_in,\
+            open(file_out_name, "w") as file_out:
         file_content = file_in.read()
         file_out.write(file_content)
