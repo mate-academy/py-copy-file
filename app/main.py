69a7acd8f144e6cc173aef0c68d8101a6b00369b
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-05-05 20:52:00 +0300

    Using unpacking

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 def copy_file(command: str) -> None:
-    text = command.split(" ")
-    if "cp" not in text[0] or len(text) != 3:
+    text = command.split()
+    cp, from_file, to_file = text
+    if "cp" not in cp or len(text) != 3:
         print("Invalid command. Usage: cp {from_file} {to_file}")
         return
-    from_file = text[1]
-    to_file = text[2]
     if from_file == to_file:
         return
     with open(from_file, "r") as file_in, open(to_file, "w") as file_out:
