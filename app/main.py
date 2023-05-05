ab4cde88ac15c76039c3ee5c4aef9d6b8564bce4
Vlad Tetruk <vladteteruk01@gmail.com>
2023-05-05 21:50:04 +0300

    add some checks

--- app/main.py
+++ app/main.py
@@ -1,4 +1,18 @@
+def checking_command(command: str) -> None:
+    if len(command.split()) != 3:
+        raise ValueError("Too many / Not enough values!")
+    if command[0] != "cp":
+        raise ValueError(f"The program can "
+                         f"copy only (use 'cp', not '{command[0]}')!")
+    if "." not in command[1] or "." not in command[2]:
+        raise ValueError("Specify the extension (*.txt)")
+    for word in command[1].split() + command[2].split():
+        if not word:
+            raise ValueError("Check that the values are correct")
+
+
 def copy_file(command: str) -> None:
+    checking_command(command)
     first_file = command.split()[1]
     second_file = command.split()[2]
     if first_file != second_file:
