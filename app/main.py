809233ab034bd3c01089422ed3141ce036fc3951
Vlad Tetruk <vladteteruk01@gmail.com>
2023-05-05 21:07:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,6 @@ def copy_file(command: str) -> None:
     first_file = command.split()[1]
     second_file = command.split()[2]
     if first_file != second_file:
-        with open(first_file) as copied_file, open(second_file, "w") as pasted_file:
+        with (open(first_file) as copied_file,
+                open(second_file, "w") as pasted_file):
             pasted_file.write(copied_file.read())
