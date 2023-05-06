6dfcc6211a235b6f2957c29fdfb5331dbd6749e5
Munkoor <eee1lvlboss@gmail.com>
2023-05-06 14:36:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
 def copy_file(command: str) -> None:
-    _, input_file, output_file = command.split()
-    if input_file != output_file:
-        with open(input_file, "rb") as f_in, open(output_file, "wb") as f_out:
-            f_out.write(f_in.read())
+    split_command = command.split()
+
+    if len(split_command) != 3:
+        raise ValueError
+    if split_command[0] != "cp":
+        raise ValueError
+    if split_command[1] == split_command[2]:
+        raise ValueError
+
+    _, input_file, output_file = split_command
+    with open(input_file, "rb") as f_in, open(output_file, "wb") as f_out:
+        f_out.write(f_in.read())
