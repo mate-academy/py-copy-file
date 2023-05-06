ef219bea75407cbc7ce9347f35cb3d04158a91d2
Munkoor <eee1lvlboss@gmail.com>
2023-05-06 23:09:10 +0300

    fixed a code

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 def copy_file(command: str) -> None:
     split_command = command.split()
+    cmd, input_path, output_path = split_command
 
     if len(split_command) != 3:
-        raise ValueError
-    if split_command[0] != "cp":
-        raise ValueError
-    if split_command[1] == split_command[2]:
+        raise ValueError(
+            "Your command is wrong, try this format: 'cp file1.txt file2.txt'")
+    if cmd != "cp":
+        raise ValueError(
+            "Your command is wrong, try this format: 'cp file1.txt file2.txt'")
+    if input_path == output_path:
         return
-
-    _, input_file, output_file = split_command
-    with open(input_file, "rb") as f_in, open(output_file, "wb") as f_out:
+    with open(input_path, "rb") as f_in, open(output_path, "wb") as f_out:
         f_out.write(f_in.read())
