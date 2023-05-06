2afffd75fe2e056e736f9044174aa8ac437df01e
MaksymUK <clear>
2023-05-06 09:09:29 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -5,13 +5,12 @@ def copy_file(command: str) -> str:
         return "It looks like some of the required entries are missing"
     cp_command, file_in_name, file_out_name = split_command
 
-    if (
-            file_in_name == file_out_name
-            or cp_command != "cp"
-    ):
+    if file_in_name == file_out_name or cp_command != "cp":
         return "Something has gone wrong. Please check command entry"
 
-    with open(file_in_name, "r") as file_in:
-        with open(file_out_name, "w") as file_out:
-            file_content = file_in.read()
-            file_out.write(file_content)
+    with (
+        open(file_in_name, "r") as file_in,
+        open(file_out_name, "w") as file_out,
+    ):
+        file_content = file_in.read()
+        file_out.write(file_content)
