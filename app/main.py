d3a71fa50acf33c293c37606a375615e74a340f8
Denys Belei <denysbelei96>
2023-05-06 18:37:13 +0100

    "solution"

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+
+    _, src_file, dst_file = command.split()
+
+    if not os.path.exists(src_file):
+        print(f"Error: {src_file} does not exist")
+        return
+    if os.path.exists(dst_file) and src_file == dst_file:
+        return
+    with open(src_file, "r") as f_src, open(dst_file, "w") as f_dst:
+        f_dst.write(f_src.read())
