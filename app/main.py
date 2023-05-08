0b9b0324182459feba3c6e90f03f535338f8e0be
Roman Fedorchuk <slasherroman@gmail.com>
2023-05-08 18:13:16 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split()
+    source_name = command_list[1]
+    copied_file = command_list[2]
+    if len(command_list) == 3:
+        if source_name != copied_file:
+            with open(
+                    source_name, "r"
+            ) as source, open(
+                    copied_file, "w"
+            ) as new_file:
+                for line in source:
+                    new_file.write(line)
