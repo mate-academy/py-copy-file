e0ea7c801c9bf164b78e041c644606b9610a7c4c
Dmitry <dimior1992@gmail.com>
2023-05-08 11:29:35 +0300

    fix2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 def copy_file(command: str) -> None:
-    command_line, start, end = command.split()
-    if start == end and command_line != "cp":
+    if len(command.split()) != 3:
         return
+
+    command_line, start, end = command.split()
+    if start == end or command_line != "cp":
+        return
+
     with open(start, "r") as f_start, open(end, "w") as f_end:
         f_end.write(f_start.read())
