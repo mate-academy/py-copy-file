1a66b53d81ca777fc9f02f7338ae1f543699aaaa
Dmitry <dimior1992@gmail.com>
2023-05-08 09:49:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    _, start, end = command.split()
+    if start == end:
+        return
+    with open(start, "r") as f_start, open(end, "w") as f_end:
+        f_end.write(f_start.read())
