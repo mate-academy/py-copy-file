5bb65a4e1ea7bc5db353b09e3b8d992a95e204e8
Roman Fedorchuk <slasherroman@gmail.com>
2023-05-09 16:58:54 +0300

    formatting

--- app/main.py
+++ app/main.py
@@ -3,9 +3,8 @@ def copy_file(command: str) -> None:
     if len(command_list) == 3 and command_list[0] == "cp":
         source_name, copied_file = command_list[1:]
         if source_name != copied_file:
-            with open(
-                    source_name, "r"
-            ) as source, open(
-                copied_file, "w"
-            ) as new_file:
+            with (
+                open(source_name, "r") as source,
+                open(copied_file, "w") as new_file
+            ):
                 new_file.write(source.read())
