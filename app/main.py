181b987d017432ab0ed843533604353ca0d8a316
Roman Fedorchuk <slasherroman@gmail.com>
2023-05-09 16:52:58 +0300

    unpacking

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
     command_list = command.split()
     if len(command_list) == 3 and command_list[0] == "cp":
-        source_name = command_list[1]
-        copied_file = command_list[2]
+        source_name, copied_file = command_list[1:]
         if source_name != copied_file:
             with open(
                     source_name, "r"
