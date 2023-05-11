fb862ce53b1a02e85b8e8b87f81a705df72f195d
Anton Piddubnyi <antonpiddubnyii@gmail.com>
2023-05-11 12:06:07 -0600

    Made code more readable

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 def copy_file(command: str) -> None:
     split_command = command.split()
-
-    if split_command[1] == split_command[2]:
-        pass
-    else:
-        with open(
-                f"{split_command[1]}", "r"
-        ) as initial_file, \
-                open(
-                    f"{split_command[2]}", "w"
-        ) as copied_file:
-            copied_file.write(initial_file.read() + "\n")
+    if (len(split_command) == 3
+            and split_command[0] == "cp"
+            and split_command[1] != split_command[2]):
+        with (
+            open(f"{split_command[1]}", "r") as initial_file,
+            open(f"{split_command[2]}", "w") as copied_file
+        ):
+            copied_file.write(initial_file.read())
