ff5cecdd25092fa9a348cf0d8f88b8f0e242fc4b
admin <admin@Yaroslavs-MacBook-Pro.local>
2023-05-12 18:16:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ def copy_file(command: str) -> None:
     split_command = command.split()
     if len(split_command) != 3 or split_command[0] != "cp":
         return
-    print(split_command)  # перевірка на наявність 3 аргументів в split_command
     original_file = split_command[1]
     new_file = split_command[2]
     if original_file == new_file:
