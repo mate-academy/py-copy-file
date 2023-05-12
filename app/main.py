90b498083675bca2983c9c399c1389eb0e3ed833
admin <admin@Yaroslavs-MacBook-Pro.local>
2023-05-12 17:29:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 def copy_file(command: str) -> None:
-    if "cp" not in command:  # перевірка на наявність команди cp в command
-        return
     split_command = command.split()
-    print(split_command)  # перевірка на наявність 3 аргументів в split_command
-    file_1 = split_command[1]
-    file_2 = split_command[2]
-    if file_1 == file_2:
+    if split_command[0] != "cp":
         return
-    with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
-        for line in file_in:
-            file_out.write(line)
+    print(split_command)  # перевірка на наявність 3 аргументів в split_command
+    original_file = split_command[1]
+    new_file = split_command[2]
+    if original_file == new_file:
+        return
+    with open(original_file, "r") as file_in, open(new_file, "w") as file_out:
+        file_in.write(file_out.read())
