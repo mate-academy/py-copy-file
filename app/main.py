ef76dac478c06990be2591dc0f7ab9ca9b8a4dd1
OleksandrLeliukh <alexander.leluh@gmail.com>
2023-05-13 18:43:56 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split()
+    if len(split_command) != 3 or split_command[0] != "cp":
+        return print("Wrong command!")
+    if split_command[1] == split_command[2]:
+        return
+    with open(f"{split_command[1]}", "r") as file_in:
+        content = file_in.read()
+    with open(f"{split_command[2]}", "w") as file_out:
+        file_out.write(str(content))
