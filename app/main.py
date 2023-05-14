216eacefb2d449f812e993eed604e2dfe96449fe
OleksandrLeliukh <alexander.leluh@gmail.com>
2023-05-14 17:09:14 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ def copy_file(command: str) -> None:
         return print("Wrong command!")
     file_in_name = command_split[1]
     file_out_name = command_split[2]
-    if file_out_name == file_out_name:
+    if file_in_name == file_out_name:
         return
-    with (open(f"{file_in_name}", "r") as file_in,
-          open(f"{file_out_name}", "w") as file_out):
+    with (open(file_in_name, "r") as file_in,
+          open(file_out_name, "w") as file_out):
         file_out.write(file_in.read())
