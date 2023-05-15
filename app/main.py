437db8980dbab43a804024362e87ba6ad2c489b9
Bohdan11Dii <bodyabrovdiy@gmail.com>
2023-05-15 14:41:52 +0300

    Copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    line_command = command.split(" ")
+
+    if (len(line_command) == 3
+            and line_command[0] == "cp"
+            and line_command[1] != line_command[2]):
+
+        with (open(line_command[1], "r") as firstfile, 
+              open(line_command[2], "w") as secondfile):
+            secondfile.write(firstfile.read())
+    else:
+        print("Some wrong with copy")
+    
