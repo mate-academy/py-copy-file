10634e2bd2f97c386a775f8199478be8fc3cf3ab
Oksana Makieieva <oksana.zhornyk@gmail.com>
2023-05-17 17:14:43 +0200

    py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
-def copy_file(file_in: str, file_out: str) -> None:
-    with open(file_in, "r") as file_in, open(file_out, "w") as file_out:
-        file_out.write(file_in.read())
+def copy_file(command: str) -> None:
+    file_in = command.split(" ")[1]
+    file_out = command.split(" ")[2]
+    if file_in != file_out:
+        with open(file_in, "r") as file_in, open(file_out, "w") as file_out:
+            file_out.write(file_in.read())
