a9a0b5d774c2b35a0fb7eda43ef2d82cf3362f2d
o.makieieva <oksana.zhornyk@gmail.com>
2023-05-17 21:08:01 +0200

    py-copy-file-v3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
     elements = command.split()
-    if elements[0] == "cp" and elements[1] != elements[1]:
+    if elements[0] == "cp" and elements[1] != elements[2]:
         with open(elements[1], "r") as f_in, open(elements[2], "w") as f_out:
             f_out.write(f_in.read())
