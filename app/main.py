fa9f70bb4e1056e49bab03309c2e91887e4a0e64
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-05-18 20:07:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    commands = command.split()
+    if len(commands) < 3 or len(commands) > 3:
+        raise ValueError("Value isn't valid. It should contain "
+                         "command 'cp' and names of 2 files")
+    if commands[0] == "cp" and (
+        commands[1] != commands[2]
+    ):
+        with (
+            open(f"{commands[1]}", "r") as source_file,
+            open(f"{commands[2]}", "w") as copy_file
+        ):
+            content = source_file.read()
+            copy_file.write(f"{content}")
