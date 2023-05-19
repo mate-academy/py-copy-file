049a9f3c252aa0dadf595d0e806286ae2c8e73c9
Volodymyr Bohomol <8773927@gmail.com>
2023-05-19 22:56:35 +0300

    Implemented function `copy_file`

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split(" ")) != 3:
+        return
+    in_name, out_name = command.split(" ")[1:]
+    if in_name == out_name:
+        return
+    with open(in_name, "r") as file_in, open(out_name, "w") as file_out:
+        file_out.write(file_in.read())
