4e5191edc83f0d983107f33fa7f91bebcec62469
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-05-19 20:09:08 +0300

    Use raise exception in wrong incoming data cases

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ def copy_file(command: str) -> None:
     parts_of_command = command.split()
 
     if parts_of_command[0] != "cp" or len(parts_of_command) != 3:
-        print("You entered the wrong command!")
-        return
+        raise Exception("You entered the wrong command!")
 
     file_name = parts_of_command[1]
     new_file_name = parts_of_command[2]
 
     if file_name == new_file_name:
-        print("You try to copy the file to a file with the same name!")
-        return
+        raise Exception(
+            "You try to copy the file to a file with the same name!"
+        )
 
     with (
         open(file_name, "r") as file_in,
