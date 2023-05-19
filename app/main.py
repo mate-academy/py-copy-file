17120c9fdf1d4ffd1d9ae931d83e5fca1a342967
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-05-19 21:09:24 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    x = command.split()
+    comm = x[0]
+    first_file = x[1]
+    second_file = x[2]
+    if first_file != second_file:
+        with open(first_file, "r") as first,\
+            open(second_file, "w") as second:
+            y = str(first.read())
+            second.write(y)
