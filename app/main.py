00ab2bd5dc785492c999b45c09a78ed3226f1077
Ivan <ivan.korshunov.1997@gmail.com>
2023-05-19 14:37:23 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 def copy_file(command: str) -> None:
-    file_name1, file_name2 = command.split()[1:]
+    command = command.split()
+    if len(command) != 3:
+        return
+    file_name1, file_name2 = command[1:]
     if file_name1 == file_name2:
         return
     with open(file_name1, "r") as file_in, open(file_name2, "w") as file_out:
