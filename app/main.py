289b5f30869304d900ae94585992da13ab805b5e
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-05-20 21:50:25 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ def copy_file(command: str) -> None:
     part = command.split()
     if len(command.split()) != 3:
         raise ValueError("command should have 3 arguments")
+    if part[0] != "cp":
+        raise ValueError("The command should be 'cp'")
     first_file = part[1]
     second_file = part[2]
     if first_file != second_file:
