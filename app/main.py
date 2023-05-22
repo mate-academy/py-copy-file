e79457f0b09b4969deb77c84bf6049285e6fb9bf
Arina Oltarzhevska <arinaoltarzhevska.ua@gmail.com>
2023-05-22 13:31:35 +0200

    update 1.0

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str) -> None:
     commands = command.split()
     if len(commands) != 3:
         raise ValueError("command should have 3 arguments")
-    if commands[0] != "cd":
+    if commands[0] != "cp":
         raise ValueError("The command should be 'cp'")
     if commands[1] != commands[2]:
         with (open(commands[1], "r") as first_file,
