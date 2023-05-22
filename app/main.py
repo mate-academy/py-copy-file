03072e1735520f23dcb890e8bca3a72c69721a3d
Arina Oltarzhevska <arinaoltarzhevska.ua@gmail.com>
2023-05-22 13:17:31 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    commands = command.split()
+    if len(commands) != 3:
+        raise ValueError("command should have 3 arguments")
+    if commands[0] != "cd":
+        raise ValueError("The command should be 'cp'")
+    if commands[1] != commands[2]:
+        with (open(commands[1], "r") as first_file,
+              open(commands[2], "w") as second_file
+              ):
+            first_content = str(first_file.read())
+            second_file.write(first_content)
