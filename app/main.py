5f33128cdcd2632ea049dc136b979ca3baad9f41
laskarj <koks.adekvat@gmail.com>
2023-05-23 15:34:53 +0200

    Task solution.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp, source, replication = command.split()
+    if cp == "cp" and source != replication:
+        with open(source, "r") as file, open(replication, "w") as copy:
+            copy.write(file.read())
