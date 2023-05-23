5686208ce25c098ba6a16efcb6063603ab5d34de
laskarj <koks.adekvat@gmail.com>
2023-05-23 15:49:56 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_file(command: str) -> None:
-    cp, source, replication = command.split()
-    if cp == "cp" and source != replication:
-        with open(source, "r") as file, open(replication, "w") as copy:
-            copy.write(file.read())
+    if len(command.split()) == 3:
+        cp, source, replication = command.split()
+        if cp == "cp" and source != replication:
+            with open(source, "r") as file, open(replication, "w") as copy:
+                copy.write(file.read())
