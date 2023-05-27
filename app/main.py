26f5ba281c8639b377ed8160155924f067b3b3eb
Vitaliy Perehrest <vit.perehrest@gmail.com>
2023-05-27 15:11:35 +0300

    copy_file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    _, src_file, dest_file = command.split()
+    if src_file == dest_file:
+        return
+
+    with open(src_file, "r") as src, open(dest_file, "w") as dest:
+        shutil.copyfileobj(src, dest)
+
+
+copy_file("cp file.txt file.txt")
