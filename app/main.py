4b7a0ca6b5c08c61ebab165b00225e24b21af3ce
Andrii Kuzmenkov <kuzmenkov.andr@gmail.com>
2023-05-29 17:57:16 +0300

    Refactored3

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
     try:
-        cmd, source_file, target_file = command.split()
+        command, source_file, target_file = command.split()
     except ValueError:
         print("Command must have 2 arguments")
         return
-    if cmd != "cp" or source_file == target_file:
+    if command != "cp" or source_file == target_file:
         return
 
     with open(source_file, "r") as source, open(target_file, "w") as target:
