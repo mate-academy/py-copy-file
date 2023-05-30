f46a77956e078a2d358500badc89a6a134b76385
Soloveyk1n <swain.volt@gmail.com>
2023-05-30 11:48:59 +0300

    solution with changed len() of task

--- app/main.py
+++ app/main.py
@@ -2,15 +2,16 @@ def copy_file(command: str) -> None:
     command = command.split()
     source_file = command[1]
     destination_file = command[2]
-    if len(command) != 2:
-        raise IndexError("list index out of range")
-    if source_file == destination_file:
-        return
-    if len(command) == 2:
-        with (open(source_file, "r") as file_in,
+    if command[0] == "cp":
+        if len(command) != 3:
+            raise IndexError("list index out of range")
+        if source_file == destination_file:
+            return
+        if len(command) == 3:
+            with (open(source_file, "r") as file_in,
                 open(destination_file, "w") as file_out):
-            file_out.write(file_in.read())
+                    file_out.write(file_in.read())
 
 
 if __name__ == "__main__":
-    copy_file(command=__name__)
+    copy_file(command = __name__)
