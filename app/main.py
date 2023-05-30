c802ece9aae367738071ff26a061ccdf1221adf0
Soloveyk1n <swain.volt@gmail.com>
2023-05-30 14:07:43 +0300

    fixes and changes

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 def copy_file(command: str) -> None:
     command = command.split()
+    if len(command) != 3:
+        raise IndexError("list index out of range")
     source_file = command[1]
     destination_file = command[2]
+    if source_file == destination_file:
+        return
     if command[0] == "cp":
-        if len(command) != 3:
-            raise IndexError("list index out of range")
-        if source_file == destination_file:
-            return
-        if len(command) == 3:
-            with (open(source_file, "r") as file_in,
-                    open(destination_file, "w") as file_out):
-                file_out.write(file_in.read())
+        with (open(source_file, "r") as file_in,
+                open(destination_file, "w") as file_out):
+            file_out.write(file_in.read())
 
 
 if __name__ == "__main__":
