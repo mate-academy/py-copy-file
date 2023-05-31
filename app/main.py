8f10271a7e48b7140127753fd48276bfd4f95f57
Dmytro Kolodii <dimakolodiy.ua@gmail.com>
2023-05-31 15:27:23 +0300

    fix names

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
     command_list = command.split()
     if len(command_list) == 3:
-        cp = command_list[0]
-        src_file = command_list[1]
-        dst_file = command_list[2]
+        cmd = command_list[0]
+        src_path = command_list[1]
+        dst_path = command_list[2]
 
-        if src_file != dst_file and cp == "cp":
-            with open(src_file, "r") as file1, open(dst_file, "w") as fin_file:
+        if src_path != dst_path and cmd == "cp":
+            with open(src_path, "r") as file1, open(dst_path, "w") as fin_file:
                 fin_file.write(file1.read())
