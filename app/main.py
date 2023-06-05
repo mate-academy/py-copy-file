f3a0cf15f76df5db5acb4dfbe8663b5f97d3c946
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-06-05 16:55:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def copy_file(command: str) -> None:
         raise ValueError("Command must have 'cp' keyword and 2 arguments")
 
     if command == "cp" and source_file != target_file:
-        with open(source_file, "r") as source,\
-             open(target_file, "w") as target:
+        with (
+                open(source_file, "r") as source,
+                open(target_file, "w") as target
+        ):
             target.write(source.read())
