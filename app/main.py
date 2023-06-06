f00d313da5bf4ea3d8f58212364e47c21a9632e7
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-06 14:02:51 +0300

    Copy file function implemented

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+from shutil import copy
+
+
+def copy_file(command: str) -> None:
+    comm_data = command.split()
+    if comm_data[-1] == comm_data[-2]:
+        return
+    copy(comm_data[-2], comm_data[-1])
