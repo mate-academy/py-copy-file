29e5f00b5d23ae45b26e7d4517a936c337a2c6c8
Denys Raboshchuk <kontroler444@gmail.com>
2023-06-06 15:50:56 +0300

    Done!

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    list_of_command = command.split()
+    if list_of_command[1] != list_of_command[2] and list_of_command[0] == "cp":
+        with open(list_of_command[1], "r") as source_file, \
+                open(list_of_command[2], "w") as destination_file:
+            content = source_file.read()
+            destination_file.write(content)
