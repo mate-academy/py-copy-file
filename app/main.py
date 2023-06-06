91bbde61ce0ff1188efdd55de9a7a87c06bac05e
Artem Putsenko <artempucenko3@gmail.com>
2023-06-06 23:32:44 +0300

    '2

--- app/main.py
+++ app/main.py
@@ -2,8 +2,7 @@ def copy_file(command: str) -> None:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "cp":
         raise ValueError
-    file1 = parts[1]
-    file2 = parts[2]
+    file1, file2 = parts[1:3]
 
     if file1 == file2:
         return
