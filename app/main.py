c1c2ebc8b45542551a05c6dbc704c98c6939f071
Artem Putsenko <artempucenko3@gmail.com>
2023-06-06 19:42:42 +0300

    '1'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        raise ValueError
+    file1 = parts[1]
+    file2 = parts[2]
+
+    if file1 == file2:
+        return
+
+    with open(file1, "r") as file_in, open(file2, "w") as file_out:
+        file_out.write(file_in.read())
