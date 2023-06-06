b85c8c9c567f7ca8f216e6321d026eaa93092075
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-06 14:07:21 +0300

    command len check added

--- app/main.py
+++ app/main.py
@@ -3,6 +3,5 @@ from shutil import copy
 
 def copy_file(command: str) -> None:
     comm_data = command.split()
-    if comm_data[-1] == comm_data[-2]:
-        return
-    copy(comm_data[-2], comm_data[-1])
+    if len(comm_data) == 3 and comm_data[-1] != comm_data[-2]:
+        copy(comm_data[-2], comm_data[-1])
