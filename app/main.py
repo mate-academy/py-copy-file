7647b2f11ce9721f5cd453f299735906ff6cc6b2
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-07 17:22:41 +0300

    Complete rewrite without using indexes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
 from shutil import copy
+from os.path import exists
+
+
+class ArgumentException(Exception):
+    pass
 
 
 def copy_file(command: str) -> None:
-    comm_data = command.split()
-    if len(comm_data) == 3 and comm_data[-1] != comm_data[-2]:
-        copy(comm_data[-2], comm_data[-1])
+    try:
+        cmd, source, destination = command.split()
+        if cmd != "cp":
+            raise ArgumentException("Unknown command")
+        if source == destination:
+            raise ArgumentException("Destination must differ from the source")
+        if not exists(source):
+            raise FileNotFoundError(f"Unable to find the "
+                                    f"source file at '{source}'")
+    except ValueError:
+        print("You have to provide 3 parameters in the command string")
+    except ArgumentException as er:
+        print(f"Invalid arguments: {er}")
+    except FileNotFoundError as er:
+        print(er)
+    except Exception as er:
+        print(f"Unexpected error: {er}")
+    else:
+        copy(source, destination)
