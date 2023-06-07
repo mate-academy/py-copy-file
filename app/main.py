57619519e14e90474a1490495d7508ef6c8f2a87
Denys Raboshchuk <kontroler444@gmail.com>
2023-06-07 18:26:53 +0300

    Validate command length, used unpacking. I can't use () to split lines. Can you explain how?

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 def copy_file(command: str) -> None:
-    list_of_command = command.split()
-    if list_of_command[1] != list_of_command[2] and list_of_command[0] == "cp":
-        with open(list_of_command[1], "r") as source_file, \
-                open(list_of_command[2], "w") as destination_file:
-            content = source_file.read()
-            destination_file.write(content)
+    if len(command) == 2:
+        current_command, source_file, destination_file = command.split()
+        if source_file != destination_file:
+            with open(source_file) as source_file, \
+                    open(destination_file, "w") as destination_file:
+
+                content = source_file.read()
+                destination_file.write(content)
