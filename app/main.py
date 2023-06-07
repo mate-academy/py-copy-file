162911b3849cfd4db84be37263ed5d85226ca209
Nikita <melniknikita2002@icloud.com>
2023-06-07 22:49:38 +0300

    task

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def copy_file(command: str) -> None:
-    command = command.split(" ")
-    file = command[1]
-    copy_file = command[2]
-    if file == copy_file:
-        return
-    with open(file, "r") as original, open(copy_file, "w") as copy:
+    command_parts = command.split(" ")
+    file_name = command_parts[1]
+    copy_file_name = command_parts[2]
+    with open(file_name, "r") as original, open(copy_file_name, "w") as copy:
         copy.write(original.read())
