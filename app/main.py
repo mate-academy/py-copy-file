fe3a817857a59fbdf97302695dfdac2412e3638a
Nikita <melniknikita2002@icloud.com>
2023-06-08 20:06:54 +0300

    task

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
     command_parts = command.split(" ")
-    file_name = command_parts[1]
-    copy_file_name = command_parts[2]
     if command_parts[0] != "cp" or len(command_parts) != 3:
         raise ValueError("Command is incorrect")
+    file_name = command_parts[1]
+    copy_file_name = command_parts[2]
     if file_name == copy_file_name:
         return
     with open(file_name, "r") as original, open(copy_file_name, "w") as copy:
