dc5d74ca64d9ed7643556d7767c9a00157f34b66
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-08 13:23:44 +0300

    Moving from .net-style ArgumentException to pythonic ValueError

--- app/main.py
+++ app/main.py
@@ -2,27 +2,18 @@ from shutil import copy
 from os.path import exists
 
 
-class ArgumentException(Exception):
-    pass
-
-
 def copy_file(command: str) -> None:
     try:
         cmd, source, destination = command.split()
         if cmd != "cp":
-            raise ArgumentException("Unknown command")
+            raise ValueError("Unknown command")
         if source == destination:
-            raise ArgumentException("Destination must differ from the source")
+            raise ValueError("Destination must differ from the source")
         if not exists(source):
             raise FileNotFoundError(f"Unable to find the "
                                     f"source file at '{source}'")
-    except ValueError:
-        print("You have to provide 3 parameters in the command string")
-    except ArgumentException as er:
-        print(f"Invalid arguments: {er}")
-    except FileNotFoundError as er:
-        print(er)
+        copy(source, destination)
+    except (ValueError, FileNotFoundError) as er:
+        print(f"Error: {er}")
     except Exception as er:
         print(f"Unexpected error: {er}")
-    else:
-        copy(source, destination)
