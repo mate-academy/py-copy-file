790bbd707af496052bb4d9c24ff17a4edfa90ef7
Nikita <melniknikita2002@icloud.com>
2023-06-08 13:09:44 +0300

    task

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ def copy_file(command: str) -> None:
     file_name = command_parts[1]
     copy_file_name = command_parts[2]
     if command_parts[0] != "cp":
-        raise ValueError("There is not \"cp\" command")
+        raise ValueError('There is not "cp" command')
     if file_name == copy_file_name:
         return
     with open(file_name, "r") as original, open(copy_file_name, "w") as copy:
