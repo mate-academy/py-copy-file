a9b6cdd8cd88a268742dcd8573c5a6e4f70f0346
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-06-09 13:27:18 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,21 +1,25 @@
-from os import path
-import sys
+from os import path, getcwd
 
 
 def copy_file(command: str) -> str or None:
-    cmd, main_file, new_file = command.split()
-    source = path.join(sys.path[0], main_file)
-    destination = path.join(sys.path[0], new_file)
+
     try:
+        cmd, main_file, new_file = command.split()
+        source = fr"{getcwd()}\{main_file}"
+
         if cmd != "cp":
             raise ValueError("Wrong command! You should use `cp`.")
         if main_file == new_file:
             raise ValueError("Cannot copy file to itself!")
+        if not (main_file.endswith(".txt") and new_file.endswith(".txt")):
+            raise ValueError("Files are in wrong format!")
+        if not path.exists(source):
+            raise FileNotFoundError("Source file doesn't exist!")
 
-    # we can use copy() from shutil, but I've made it in more "vanilla" way
-        with open(source, "r") as file_in, open(destination, "w") as file_out:
+        with open(source, "r") as file_in, open(new_file, "w") as file_out:
             file_out.write(file_in.read())
-    except ValueError as e:
-        print(f"ValueError: {e}")
-    else:
+
         print("File copied successfully!")
+
+    except (ValueError, FileNotFoundError) as e:
+        print(f"{type(e).__name__}: {e}")
