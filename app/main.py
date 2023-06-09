eda18fc2f531e8be0c36cc71c876173b5c8fbe3e
Denys Raboshchuk <kontroler444@gmail.com>
2023-06-09 20:30:51 +0300

    Fixed issues

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
     current_command, source_file, destination_file = command.split()
     if len(current_command) == 2 and source_file != destination_file:
-        with open(source_file) as source_file, \
-                open(destination_file, "w") as destination_file:
-            content = source_file.read()
-            destination_file.write(content)
+        with (
+                open(source_file) as source_file,
+                open(destination_file, "w") as destination_file
+                ):
+            destination_file.write(source_file.read())
