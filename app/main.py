9e78e2fdf932f691e66261ed78b786cab8617949
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-06-09 17:07:16 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,22 +1,19 @@
-from os import path, getcwd
+from os import path
 
 
 def copy_file(command: str) -> str or None:
 
     try:
         cmd, main_file, new_file = command.split()
-        source = fr"{getcwd()}\{main_file}"
 
         if cmd != "cp":
             raise ValueError("Wrong command! You should use `cp`.")
         if main_file == new_file:
             raise ValueError("Cannot copy file to itself!")
-        if not (main_file.endswith(".txt") and new_file.endswith(".txt")):
-            raise ValueError("Files are in wrong format!")
-        if not path.exists(source):
+        if not path.exists(main_file):
             raise FileNotFoundError("Source file doesn't exist!")
 
-        with open(source, "r") as file_in, open(new_file, "w") as file_out:
+        with open(main_file, "r") as file_in, open(new_file, "w") as file_out:
             file_out.write(file_in.read())
 
         print("File copied successfully!")
