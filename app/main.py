0a260762890aba120b5a25697619487193222905
Danylo Tiutiushkin <48014331+danylott@users.noreply.github.com>
2023-06-09 12:50:56 +0300

    Delete tests directory

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
-# write your code here
+import shutil
+
+
+def copy_file(str1: str) -> tuple:
+    src_path = str1.rsplit(" ")[1]
+    dst_path = str1.rsplit(" ")[2]
+    if src_path != dst_path:
+        shutil.copy(src_path, dst_path)
+        print("Copied")
+        if open(src_path).read() == open(dst_path).read():
+            print(True)
+            try:
+                with open(src_path, "r") as src, open(dst_path, "w") as dst:
+                    pass
+                if src.closed and dst.closed:
+                    print("Файли закрито")
+                else:
+                    print("Файли не закрито")
+
+            except IOError:
+                print("Помилка відкриття/закриття файлів")
+        else:
+            print("файли не однакові")
+            print(False)
+    else:
+        print("файли однакові")
+        return
+
+
+copy_file("Copied file.txt txtа89.txt")
