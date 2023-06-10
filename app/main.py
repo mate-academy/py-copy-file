81aae6d2bb02f97682d660cf032c868c51f15247
Tarasidze <dtar84@ukr.net>
2023-06-10 17:07:36 +0300

    Fixed Solution 1.01

--- app/main.py
+++ app/main.py
@@ -11,7 +11,12 @@ def copy_file(command: str) -> None:
     if file_name_in == file_name_out or cmd != "cp":
         return
 
-    with (open(file_name_in, "r") as file_in,
+    try:
+        open(file_name_in)
+    except (FileExistsError, PermissionError) as e:
+        raise f"Error {e}. The file does not exist or can not be read"
+
+    with (open(file_name_in) as file_in,
           open(file_name_out, "w") as file_out):
         file_content = file_in.read()
         file_out.write(file_content)
