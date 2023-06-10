7c0c3a6f996e7dafc664cbd1aceb462a6a0d1069
Anton Shpakovych <shpakovych18@gmail.com>
2023-06-10 15:55:12 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> bool:
+    if len(command.split(" ")) != 3:
+        return False
+
+    file, new_file = command.split(" ")[1:]
+
+    if file != new_file:
+        try:
+            with open(file) as file, open(new_file, "a+") as new_file:
+                for line in file:
+                    new_file.write(line)
+        except (FileNotFoundError, PermissionError):
+            return False
