b001af2d20813d8743cf80dc62171d2ff57d12f0
Oleksii Kolii <igravkompik@gmail.com>
2023-06-10 17:16:37 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+class CommandError(Exception):
+    pass
+
+
+class PathError(Exception):
+    pass
+
+
+def copy_file(command: str) -> None:
+    try:
+        command = command.split()
+        if command[0] == "cp":
+            if command[1] != command[2]:
+                with (open(command[1], "r") as source,
+                      open(command[2], "w") as new_file):
+                    new_file.write(source.read())
+                print("Copying done!")
+                return
+
+            raise PathError(f"File path {command[1]} and "
+                            f"'{command[2]}' can not be the same!")
+
+        raise CommandError(f"Unknown command: {command[0]}\n"
+                           "Did you mean 'cp'?")
+
+    except (CommandError, PathError) as e:
+        print(e)
+    except FileNotFoundError as e:
+        print(e)
