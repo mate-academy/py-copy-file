08f7de928c2619ebe9d5c9baa067d7410b1d826d
Oleksii Kolii <igravkompik@gmail.com>
2023-06-10 17:56:30 +0300

    added check for command length

--- app/main.py
+++ app/main.py
@@ -9,10 +9,12 @@ class PathError(Exception):
 def copy_file(command: str) -> None:
     try:
         command = command.split()
+        if len(command) != 3:
+            raise CommandError("Unsupported format of command")
         if command[0] == "cp":
             if command[1] != command[2]:
                 with (open(command[1], "r") as source,
-                      open(command[2], "w") as new_file):
+                    open(command[2], "w") as new_file):
                     new_file.write(source.read())
                 print("Copying done!")
                 return
