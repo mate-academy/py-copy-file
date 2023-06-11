9ba117dbf008172a6ff237e0f37289c12a41d089
Vlad <vladislav.zhyla@gmail.com>
2023-06-12 00:43:11 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_ls = command.split()
+    try:
+        with open(command_ls[1], "r") as file_in, open(command_ls[2], "x") as file_out:
+            file_out.write(file_in.read())
+    except FileExistsError:
+        pass
