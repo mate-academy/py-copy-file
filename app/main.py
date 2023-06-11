a6b23553efba0a4bc8b7cc9b1f2b668ba6c0c54c
Anton Komarov <anton_komarov_qa@ukr.net>
2023-06-11 12:14:57 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if not command.startswith("cp ") or len(command.split(" ")) != 3:
+        return
+    old_file_name = command.split(" ")[1]
+    new_file_name = command.split(" ")[2]
+    if old_file_name == new_file_name:
+        return
+    with (open(old_file_name, "r") as old_file,
+          open(new_file_name, "w") as new_file):
+        new_file.write(old_file.read())
