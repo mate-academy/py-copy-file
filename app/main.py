6cf037f6f9a204a735e09891f54c29283caed4d5
maccarets <maccarets@gmail.com>
2023-06-12 02:39:18 +0300

    done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    if len(command) != 3:
+        raise ValueError("Command shold have three arguments")
+    if command[0] != "cp":
+        raise ValueError("It is not cp command")
+    if command[1] != command[2]:
+        with (open(command[1], "r") as source,
+              open(command[2], "w") as copy):
+            copy.write(source.read())
