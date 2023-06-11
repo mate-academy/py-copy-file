2bb4f6f6575e2bef618b2706834749420ec69780
Oleksii Kolii <igravkompik@gmail.com>
2023-06-11 16:33:37 +0300

    deleted FilePath exception error

--- app/main.py
+++ app/main.py
@@ -2,15 +2,12 @@ class CommandError(Exception):
     pass
 
 
-class PathError(Exception):
-    pass
-
-
 def copy_file(command: str) -> None:
     try:
         command = command.split()
         if len(command) != 3:
             raise CommandError("Unsupported format of command")
+
         if command[0] == "cp":
             if command[1] != command[2]:
                 with (open(command[1], "r") as source,
@@ -19,13 +16,10 @@ def copy_file(command: str) -> None:
                 print("Copying done!")
                 return
 
-            raise PathError(f"File path {command[1]} and "
-                            f"'{command[2]}' can not be the same!")
-
         raise CommandError(f"Unknown command: {command[0]}\n"
                            "Did you mean 'cp'?")
 
-    except (CommandError, PathError) as e:
+    except CommandError as e:
         print(e)
     except FileNotFoundError as e:
         print(e)
