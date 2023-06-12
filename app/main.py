971e702262131b55ee4761891337756e7073f3ea
Oleksii Kolii <igravkompik@gmail.com>
2023-06-12 16:49:02 +0300

    replaced try block and splitted checks for same name/command

--- app/main.py
+++ app/main.py
@@ -3,20 +3,24 @@ class CommandError(Exception):
 
 
 def copy_file(command: str) -> None:
-    try:
-        command = command.split()
-        if len(command) != 3:
-            raise CommandError("Unsupported format of command")
+    command = command.split()
+    if len(command) != 3:
+        raise CommandError("Unsupported format of command")
 
-        cmd, file_name, new_file_name = command
-        if cmd == "cp" and file_name != new_file_name:
-            with (open(file_name, "r") as source,
-                    open(new_file_name, "w") as new_file):
-                new_file.write(source.read())
-            print("Copying done!")
+    cmd, file_name, new_file_name = command
+    if cmd == "cp":
+        if file_name != new_file_name:
+            try:
+                with (open(file_name, "r") as source,
+                        open(new_file_name, "w") as new_file):
+                    new_file.write(source.read())
+                print("Copying done!")
+            except FileNotFoundError as e:
+                print(e)
         else:
-            raise CommandError(f"Unknown command: {cmd}\n"
-                               "Did you mean 'cp'?")
+            print("File with the same name cannot be copied!")
+    else:
+        raise CommandError(f"Unknown command: {cmd}\n"
+                            "Did you mean 'cp'?")
 
-    except FileNotFoundError as e:
-        print(e)
+
