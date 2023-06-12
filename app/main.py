c1adb378075219ddd6cce9ae636c4d51eab3732d
Oleksii Kolii <igravkompik@gmail.com>
2023-06-12 14:43:52 +0300

    improved readability removed catch on command err exception

--- app/main.py
+++ app/main.py
@@ -7,19 +7,16 @@ def copy_file(command: str) -> None:
         command = command.split()
         if len(command) != 3:
             raise CommandError("Unsupported format of command")
-
-        if command[0] == "cp":
-            if command[1] != command[2]:
-                with (open(command[1], "r") as source,
-                      open(command[2], "w") as new_file):
-                    new_file.write(source.read())
-                print("Copying done!")
-                return
+        
+        cmd, file_name, new_file_name = command
+        if cmd == "cp" and file_name != new_file_name:
+            with (open(file_name, "r") as source,
+                    open(new_file_name, "w") as new_file):
+                new_file.write(source.read())
+            print("Copying done!")
 
         raise CommandError(f"Unknown command: {command[0]}\n"
                            "Did you mean 'cp'?")
 
-    except CommandError as e:
-        print(e)
     except FileNotFoundError as e:
         print(e)
