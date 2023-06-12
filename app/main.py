514ab2fb3d8f7a53f2f487b2d7cec6dfdfd22db9
Yaryk Konoplyov <kannabis252@gmail.com>
2023-06-12 15:36:25 +0300

    Check for copy optimized

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
+    if len(command.split(" ")) < 3:
+        return
+
     command_list = command.split(" ")
     original_file = command_list[1]
     file_to_copy = command_list[2]
-
-    if original_file == file_to_copy:
-        return
 
     with open(original_file, "r") as orig, open(file_to_copy, "a") as copy:
         for line in orig.readlines():
