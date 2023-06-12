48245993bec00d85f35e27e8e55c403c5fd57bc9
Oleksii Kolii <igravkompik@gmail.com>
2023-06-12 18:02:17 +0300

    united two checks in one

--- app/main.py
+++ app/main.py
@@ -4,19 +4,15 @@ class CommandError(Exception):
 
 def copy_file(command: str) -> None:
     command = command.split()
-    if len(command) != 3:
+    if len(command) != 3 and command[0] == "cp":
         raise CommandError("Unsupported format of command")
 
-    cmd, file_name, new_file_name = command
-    if cmd == "cp":
-        if file_name != new_file_name:
-            try:
-                with (open(file_name, "r") as source,
-                        open(new_file_name, "w") as new_file):
-                    new_file.write(source.read())
-                print("Copying done!")
-            except FileNotFoundError as e:
-                print(e)
-    else:
-        raise CommandError(f"Unknown command: {cmd}\n"
-                           "Did you mean 'cp'?")
+    file_name, new_file_name = command[1:]
+    if file_name != new_file_name:
+        try:
+            with (open(file_name, "r") as source,
+                    open(new_file_name, "w") as new_file):
+                new_file.write(source.read())
+            print("Copying done!")
+        except FileNotFoundError as e:
+            print(e)
