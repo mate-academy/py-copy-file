876247c0003f6d4136d6627013f5753aa2e22f62
Nazariy <mac@Nazariys-MacBook-Pro.local>
2023-06-12 15:40:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_part = command.split()
+    cp, source, target = command_part
+    if source == target:
+        print("Source and target are the same")
+
+    with open(source, "r") as file_in, open(target, "w") as file_out:
+        file_out.write(file_in.read())
