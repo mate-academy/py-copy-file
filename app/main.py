bde8a5072758e0a8acca73b81e215560c389bdba
Alex Yatsun <starcrafter4444@gmail.com>
2023-06-12 14:49:11 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    coped_file = command.split()[1]
+    new_file = command.split()[2]
+
+    if coped_file == new_file:
+        return
+
+    with (open(coped_file, "r") as c_file,
+          open(new_file, "w") as n_file):
+        n_file.write("".join(c_file.readlines()))
