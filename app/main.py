7d1d2be9d26dde8c8ccb4d5625574c404fdec383
yakhlamova <yanaakhlamova@gmail.com>
2023-06-12 16:40:30 +0300

    'Solution_update'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy(command: str) -> None:
     cmd, file_name, new_file_name = command.split()
-    if file_name == new_file_name or cmd != "cp":
-        print("Does nothing")
-    with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
-        file_out.write(file_in.read())
+    if file_name != new_file_name or cmd == "cp":
+        with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
+            file_out.write(file_in.read())
