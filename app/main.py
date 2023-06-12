8572af0cd0e4f52260e565873378e1e484d6783f
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-06-12 19:07:06 +0300

    Used unpacking

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    command = command.split()
-    if command[1] != command[2]:
-        with open(command[1], "r") as origin, open(command[2], "w") as copy:
+    command, origin, copy = command.split()
+    if origin != copy:
+        with open(origin, "r") as origin, open(copy, "w") as copy:
             lines = origin.readlines()
             copy.writelines(lines)
