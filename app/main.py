c177969948974a47f0786fd0e69529a114fe91e9
YOUR_NAME <YOUR_EMAIL>
2023-06-12 19:46:55 +0300

    Copy file (add check com == "cp", fix file_out.write(file_in.read()))

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 def copy_file(command: str) -> None:
     com, first_file, second_file = command.split()
-    if first_file != second_file:
+
+    if first_file != second_file and com == "cp":
         with (open(first_file, "r") as file_in,
               open(second_file, "w") as file_out):
-            data = file_in.read()
-            file_out.write(data)
+            file_out.write(file_in.read())
             print(f"The data was successfully copied from the "
                   f"{first_file} to a {second_file}")
-    print("The files have the same names. Copy error!")
+    print("Copy error! The command should be 'cp' "
+          "and the files should have different names!")
