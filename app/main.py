416caf4b901a61663a0209e9932d28fda79a79b9
Denys Seliverstov <catsperr322@gmail.com>
2023-06-12 15:06:45 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    target_file, new_file = command.split()[1:]
+    if target_file == new_file:
+        return
+
+    with open(target_file, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
+
+
+if __name__ == "__main__":
+    copy_file(input(
+        "Write a command with arguments "
+        "[target_file, new_file] :"
+    ))
