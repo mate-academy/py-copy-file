d453978cfc55c9b67da50d554058a5853b35f697
Eugene Polunin <altahoma@gmail.com>
2023-06-12 15:08:09 +0300

    Simplified copying, increased variety of file types

--- app/main.py
+++ app/main.py
@@ -2,6 +2,5 @@ def copy_file(command: str) -> None:
     source, target = command.split()[1:]
 
     if source != target:
-        with open(source) as file_in, open(target, "w") as file_out:
-            for line in file_in:
-                file_out.write(line)
+        with open(source, "rb") as file_in, open(target, "wb") as file_out:
+            file_out.write(file_in.read())
