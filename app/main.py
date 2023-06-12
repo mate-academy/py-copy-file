98f6af617b08fc9049fc7590409b695151aafa1e
Erik <erik.agayan1@gmail.com>
2023-06-12 13:56:43 +0300

    Solution #3

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 def copy_file(command: str) -> None:
-    cp, first_file, second_file = command.split(" ")
+    cp, first_file, second_file = command.split()
 
     if first_file != second_file:
-        with open(first_file, "r") as old_file, \
-                open(second_file, "w") as new_file:
-            content = old_file.read()
-            new_file.write(content)
-    else:
-        return
+        with open(first_file, "r") as old, open(second_file, "w") as new:
+            content = old.read()
+            new.write(content)
