634b303af4e064eef4dc9140a76d7a99d86d2d16
Eugene Polunin <altahoma@gmail.com>
2023-06-12 17:10:50 +0300

    Added a check for command incorrectness

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    source, target = command.split()[1:]
+    statement, source, target = command.split()
 
-    if source != target:
+    if source != target and statement == "cp":
         with open(source, "rb") as file_in, open(target, "wb") as file_out:
             file_out.write(file_in.read())
