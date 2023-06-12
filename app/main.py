9ffb0a7ee90ae37c8a41514c84f18943751c2231
Anton Komarov <anton_komarov_qa@ukr.net>
2023-06-12 17:11:56 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,16 +1,14 @@
 def copy_file(command: str) -> None | str:
-    try:
-        cmd_name, old_file_name, new_file_name = command.split()
-        if cmd_name != "cp":
-            raise ValueError("Entered incorrect command!!!")
-        if old_file_name != new_file_name:
-            try:
-                with (open(old_file_name, "r") as old_file,
-                      open(new_file_name, "w") as new_file):
-                    new_file.write(old_file.read())
-            except FileNotFoundError:
-                return f"There is no file with name {old_file_name}"
-            except PermissionError:
-                return f"You don't have permission to file {old_file_name}"
-    except ValueError as error:
-        print(error)
+    cmd_name, old_file_name, new_file_name = command.split()
+    if cmd_name != "cp":
+        return "Entered incorrect command!!!"
+    if old_file_name != new_file_name:
+        try:
+            with (open(old_file_name, "r") as old_file,
+                  open(new_file_name, "w") as new_file):
+                new_file.write(old_file.read())
+        except FileNotFoundError:
+            return f"There is no file with name {old_file_name}"
+        except PermissionError:
+            return f"You don't have permission to file {old_file_name}"
+
