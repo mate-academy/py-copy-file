340fb038593db24417dc4c0b7763c9e314918d15
Yaryk Konoplyov <kannabis252@gmail.com>
2023-06-12 15:34:55 +0300

    Check for copy name added

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ def copy_file(command: str) -> None:
     original_file = command_list[1]
     file_to_copy = command_list[2]
 
+    if original_file == file_to_copy:
+        return
+
     with open(original_file, "r") as orig, open(file_to_copy, "a") as copy:
         for line in orig.readlines():
             copy.write(line)
