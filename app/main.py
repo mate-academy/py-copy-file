f92f551f0e002d5b6ce265ae38793bc70e15ed95
Anton Shpakovych <shpakovych18@gmail.com>
2023-06-12 13:21:43 +0300

    fixed changing function parametr

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     try:
-        command, file, new_file = command.split()
-        if file != new_file and command == "cp":
+        current_command, file, new_file = command.split()
+        if file != new_file and current_command == "cp":
             with open(file) as file, open(new_file, "a+") as new_file:
                 new_file.write(file.read())
     except (ValueError, FileNotFoundError, PermissionError):
