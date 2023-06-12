0a7fa9261341e7056bffcd59018f8628baac38c6
Mariia Horbova <gorbova21@gmail.com>
2023-06-12 11:27:40 +0300

    changed according to PR

--- app/main.py
+++ app/main.py
@@ -1,20 +1,39 @@
 import os
 
 
+class ErrorWithString(Exception):
+    pass
+
+
+class CopyToSelf(Exception):
+    pass
+
+
+class FileNotFound(Exception):
+    pass
+
+
 def copy_file(command: str) -> None:
-    parts = command.split()
-    source_file = parts[1]
-    destination_file = parts[2]
+    try:
+        parts = command.split()
+        source_file = parts[1]
+        destination_file = parts[2]
 
-    if len(parts) != 3 or parts[0] != "cp":
-        return
+        if len(parts) != 3 or parts[0] != "cp":
+            raise ErrorWithString
 
-    if source_file == destination_file:
-        return
+        if source_file == destination_file:
+            raise CopyToSelf
 
-    if not os.path.isfile(source_file):
-        return
+        if not os.path.isfile(source_file):
+            raise FileNotFound
 
-    with (open(source_file, "r") as file_in,
-          open(destination_file, "w") as file_out):
-        file_out.write(file_in.read())
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
+            file_out.write(file_in.read())
+    except ErrorWithString:
+        print("Given string is not executable")
+    except CopyToSelf:
+        print("Trying to copy file to the same file")
+    except FileNotFound:
+        print("Not found file in given directory")
