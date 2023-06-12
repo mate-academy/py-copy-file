e19d4d17a64da18c0565a4eb33b44cc46c3f80dd
yakhlamova <yanaakhlamova@gmail.com>
2023-06-12 13:20:29 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy(command: str) -> None:
+    command = command.split()
+    if command[1] == command[2] or command[0] != "cp":
+        return
+    with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+        for line in file_in:
+            file_out.write(line)
