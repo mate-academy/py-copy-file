dfb78afade2ad078cf55b5098679359a9ed631a0
Anton Shpakovych <shpakovych18@gmail.com>
2023-06-12 13:19:08 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
-def copy_file(command: str) -> bool:
+def copy_file(command: str) -> None:
     try:
-        command, file, new_file = command.split(" ")
+        command, file, new_file = command.split()
         if file != new_file and command == "cp":
             with open(file) as file, open(new_file, "a+") as new_file:
                 new_file.write(file.read())
-        else:
-            return False
     except (ValueError, FileNotFoundError, PermissionError):
-        return False
-    else:
-        return True
+        raise
