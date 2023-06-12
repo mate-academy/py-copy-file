7527dfaeba159ba9f43c5451b42963dcefd4b3eb
Yaryk Konoplyov <kannabis252@gmail.com>
2023-06-12 15:33:21 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split(" ")
+    original_file = command_list[1]
+    file_to_copy = command_list[2]
+
+    with open(original_file, "r") as orig, open(file_to_copy, "a") as copy:
+        for line in orig.readlines():
+            copy.write(line)
