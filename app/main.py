9d25bfa614f32f9dceea24823b1cd6df2e76ed55
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-06-12 15:08:13 +0300

    My solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    if command[1] != command[2]:
+        with open(command[1], "r") as origin, open(command[2], "w") as copy:
+            lines = origin.readlines()
+            copy.writelines(lines)
