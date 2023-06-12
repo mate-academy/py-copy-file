f234e922bdc782380f94d0abe39f35910ea97d76
Tarasidze <dtar84@ukr.net>
2023-06-12 20:59:51 +0300

    Solution 1.07

--- app/main.py
+++ app/main.py
@@ -5,9 +5,6 @@ def copy_file(command: str) -> None:
         _, file_name_in, file_name_out = command_list
 
         if file_name_in != file_name_out:
-            try:
-                with (open(file_name_in) as file_in,
-                      open(file_name_out, "w") as file_out):
-                    file_out.write(file_in.read())
-            except (FileNotFoundError, PermissionError) as e:
-                return str(e)
+            with (open(file_name_in) as file_in,
+                  open(file_name_out, "w") as file_out):
+                file_out.write(file_in.read())
