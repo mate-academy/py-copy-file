62094acef893a20e400c609b6a6b13fd05c29011
Alex Yatsun <starcrafter4444@gmail.com>
2023-06-12 18:01:59 +0300

    Improvement

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 def copy_file(command: str) -> None:
-    coped_file = command.split()[1]
+    copied_file = command.split()[1]
     new_file = command.split()[2]
 
-    if coped_file == new_file:
-        return
-
-    with (open(coped_file, "r") as c_file,
-          open(new_file, "w") as n_file):
-        n_file.write("".join(c_file.readlines()))
+    if copied_file != new_file:
+        with (open(copied_file, "r") as c_file,
+              open(new_file, "w") as n_file):
+            n_file.write(c_file.read())
