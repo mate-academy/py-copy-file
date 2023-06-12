d2cf75fdfded8832db0ba7f10a0d51d864aef458
yakhlamova <yanaakhlamova@gmail.com>
2023-06-12 18:59:30 +0300

    'Solution_update_changes'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy(command: str) -> None:
-    cmd, current_file, future = command.split()
-    if current_file != future or cmd == "cp":
-        with open(current_file, "r") as file_in, open(future, "w") as file_out:
+    cmd, current_file, future_file = command.split()
+    if current_file != future_file or cmd == "cp":
+        with (open(current_file, "r") as file_in,
+              open(future_file, "w") as file_out):
             file_out.write(file_in.read())
