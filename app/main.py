68995c34c899ec97e17ef4c3656e49455f21e933
Eugene Polunin <altahoma@gmail.com>
2023-06-12 14:09:10 +0300

    Implemented an app for copying file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    source, target = command.split()[1:]
+
+    if source != target:
+        with open(source) as file_in, open(target, "w") as file_out:
+            for line in file_in:
+                file_out.write(line)
