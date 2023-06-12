a8445737af2a59eee74a406e30952e35fc2ac4c8
YOUR_NAME <YOUR_EMAIL>
2023-06-12 14:12:16 +0300

    Copy file (add prints)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 def copy_file(command: str) -> None:
-    com, first_file, second_file = command.split(" ")
+    com, first_file, second_file = command.split("")
     if first_file == second_file:
+        print("The files have the same names. Copy error!")
         return
     with open(first_file, "r") as file_in, open(second_file, "w") as file_out:
         data = file_in.read()
         file_out.write(data)
+        print(f"The data was successfully copied from the "
+              f"{first_file} to a {second_file}")
