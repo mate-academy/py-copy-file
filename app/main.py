8aaf62d2aa7da255d5f051bebaa1d128ae9d700a
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-06-13 16:08:52 +0300

    Checking if command is correct

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 def copy_file(command: str) -> None:
     command, origin, copy = command.split()
+    if command != "cp" or ".txt" in (origin, copy):
+        print("Wrong command")
+        return
     if origin != copy:
         with open(origin, "r") as origin, open(copy, "w") as copy:
             lines = origin.readlines()
