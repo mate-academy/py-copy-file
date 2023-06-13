d770ebc433eb3abc283ce56181068833700d19fd
Denys Seliverstov <catsperr322@gmail.com>
2023-06-13 13:30:23 +0300

    Fixed...probably

--- app/main.py
+++ app/main.py
@@ -2,11 +2,6 @@ def copy_file(command: str) -> None:
     cmd, target_file, new_file = command.split()
     if cmd != "cp":
         raise ValueError("Command not found.")
-    if not target_file:
-        raise FileNotFoundError("File is not found."
-                                f"No such file or directory '{target_file}'")
-    if not new_file:
-        raise ValueError("Name of copy_file is not found")
     if target_file != new_file:
         with (open(target_file, "r") as file_in,
               open(new_file, "w") as file_out):
