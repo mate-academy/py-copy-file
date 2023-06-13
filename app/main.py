bd87b2abc380316d40f08b63c27acb5f87ee0fa3
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-06-13 17:59:58 +0300

    Deleted return)

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 def copy_file(command: str) -> None:
     command, origin, copy = command.split()
-    if command != "cp" or ".txt" in (origin, copy):
-        print("Wrong command")
-        return
-    if origin != copy:
-        with open(origin, "r") as origin, open(copy, "w") as copy:
-            lines = origin.readlines()
-            copy.writelines(lines)
+    if command == "cp" and ".txt" in (origin, copy):
+        if origin != copy:
+            with open(origin, "r") as origin, open(copy, "w") as copy:
+                lines = origin.readlines()
+                copy.writelines(lines)
