2afda04e028a63eaa8e0618b8175947b3035dccd
Yaryk Konoplyov <kannabis252@gmail.com>
2023-06-13 13:38:02 +0300

    Blank return removed

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 def copy_file(command: str) -> None:
     command, original_file, file_to_copy = command.split()
 
-    if (len(command.split()) < 3
+    if not (len(command.split()) < 3
             or original_file == file_to_copy
             or command != "cp"):
-        return
 
-    with open(original_file, "r") as orig, open(file_to_copy, "a") as copy:
-        copy.write(orig.read())
+        with open(original_file, "r") as orig, open(file_to_copy, "a") as copy:
+            copy.write(orig.read())
