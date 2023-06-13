1eb77d9d68c977acf71b443464303fd2fa4b6695
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-06-13 14:57:15 +0300

    New solution according to task condition

--- app/main.py
+++ app/main.py
@@ -1,25 +1,7 @@
 def copy_file(command: str) -> None:
-    try:
-        cmd, old_file, new_file = command.split()
-    except ValueError:
-        print("You put incorrect data")
-    else:
-        try:
-            if old_file == new_file:
-                raise ValueError(f"You can't copy {old_file}"
-                                 f" with the same name!")
-            if cmd != "cp":
-                raise ValueError(f"This command {cmd} unavailable."
-                                 f" You can just copy file!")
-            if not (old_file.endswith(".txt") and new_file.endswith(".txt")):
-                raise ValueError("File's format is wrong."
-                                 " It must be just '.txt'")
+    cmd, old_file, new_file = command.split()
+    if old_file == new_file:
+        return
 
-            with (open(old_file, "r") as file_in,
-                  open(new_file, "w") as file_out):
-                file_out.write(file_in.read())
-        except ValueError as ve:
-            print(f"{ve}")
-        except FileNotFoundError:
-            print(f"File {file_in} doesn't exist"
-                  f" or situated in another directory")
+    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
