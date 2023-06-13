de6a483f7ab110b61b3695a625ecab139bbe19e9
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-06-13 17:12:34 +0300

    Fixed reverse condition

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def copy_file(command: str) -> None:
     cmd, old_file, new_file = command.split()
-    if old_file == new_file:
-        return
-
-    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-        file_out.write(file_in.read())
+    if old_file != new_file:
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
