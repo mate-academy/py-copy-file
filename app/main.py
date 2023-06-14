d5d713b1a668df36e6d45215021f7c44a0c32ee9
Mariia Horbova <gorbova21@gmail.com>
2023-06-14 12:26:43 +0300

    changed according to PR

--- app/main.py
+++ app/main.py
@@ -5,9 +5,9 @@ def copy_file(command: str) -> None:
     cp_command, source_file, destination_file = command.split()
 
     if (
-            source_file != destination_file and
-            os.path.isfile(source_file) and
-            cp_command == "cp"
+            source_file != destination_file
+            and os.path.isfile(source_file)
+            and cp_command == "cp"
     ):
         with (open(source_file, "r") as file_in,
               open(destination_file, "w") as file_out):
