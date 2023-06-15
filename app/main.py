f1d3fefb69dd48043429c691414186519c5b1f75
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-15 17:11:52 +0300

    improvement of split, unpacking

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def copy_file(command: str) -> None:
-    command_list = command.split(" ")
-    if (len(command_list) == 3
-            and command_list[1] != command_list[2]
-            and command_list[0] == "cp"):
-        with (open(command_list[1], "r") as file_in,
-              open(command_list[2], "w") as file_out):
+    cp, file_name, new_file_name = command.split()
+    if file_name != new_file_name and cp == "cp":
+        with (open(file_name, "r") as file_in,
+              open(new_file_name, "w") as file_out):
             file_out.write(file_in.read())
