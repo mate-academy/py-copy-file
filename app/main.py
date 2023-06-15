57c0f9957ee81643ed2613f44265149cf6a37630
Vlad <vladislav.zhyla@gmail.com>
2023-06-15 17:59:16 +0300

    cp check optimized

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 def copy_file(command: str) -> None:
     cmd, file_in, file_out = command.split()
-    if not cmd == "cp":
-        return
-    try:
-        with (open(file_in, "r") as file_in,
-              open(file_out, "x") as file_out):
-            file_out.write(file_in.read())
-    except FileExistsError:
-        print("File is already exist")
+    if cmd == "cp":
+        try:
+            with (open(file_in, "r") as file_in,
+                  open(file_out, "x") as file_out):
+                file_out.write(file_in.read())
+        except FileExistsError:
+            print("File is already exist")
