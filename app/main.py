91b82f337b1c7756d6e418dc41821767865f0a7a
Anton Hryhorenko <grygorenko23@gmail.com>
2023-06-15 21:42:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    words = command.split()
-    if words[0] == "cp" and words[1] != words[2]:
-        with open(words[1], "r") as file_in, open(words[2], "w") as file_out:
+    mode, s_file, d_file = command.split()
+    if mode == "cp" and s_file != d_file:
+        with open(s_file, "r") as file_in, open(d_file, "w") as file_out:
             file_out.write(file_in.read())
