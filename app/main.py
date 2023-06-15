82e0655259e82a231d50fd755def40c057229684
Nazariy <mac@Nazariys-MacBook-Pro.local>
2023-06-15 13:12:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def copy_file(command: str) -> None:
     cp, source, target = command.split()
-    if source == target:
-        return
-
-    with open(source, "r") as file_in, open(target, "w") as file_out:
-        file_out.write(file_in.read())
+    if source != target:
+        with open(source, "r") as file_in, open(target, "w") as file_out:
+            file_out.write(file_in.read())
