e7de4e8378aefb711cc03570a31186bd0501ec9f
Alona <alyonkabagritskaya@gmail.com>
2023-06-15 16:45:37 +0300

    $ git commit -am

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
     files = command.split()
-    if files[0] == "cp" and files[1] != files[2]:
+    if len(files) == 3 and files[0] == "cp" and files[1] != files[2]:
         with open(files[1], "r") as file_in, open(files[2], "w") as file_out:
             file_out.write(file_in.read())
