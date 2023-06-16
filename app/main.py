c14bd57fbeea988c587942331beab12bd51b1453
Anton Hryhorenko <grygorenko23@gmail.com>
2023-06-16 17:00:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ def copy_file(command: str) -> None:
             "Invalid command. "
             "Expected three parts: mode, source file, destination file."
         )
-    mode, source_file, destination_file = command.split()
-    if mode == "cp" and source_file != destination_file:
-        with open(source_file, "r") as file_in, \
-                open(destination_file, "w") as file_out:
+    if command.split()[0] == "cp" and command.split()[1] != command.split()[2]:
+        with (open(command.split()[1], "r") as file_in,
+              open(command.split()[2], "w") as file_out):
             file_out.write(file_in.read())
