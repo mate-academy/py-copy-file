5f84f27812fc536cd686c129415372d62bbb5fed
Anton Hryhorenko <grygorenko23@gmail.com>
2023-06-16 18:09:27 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 def copy_file(command: str) -> None:
-    if len(command.split()) != 3:
+    words = command.split()
+    if len(words) != 3:
         raise ValueError(
             "Invalid command. "
             "Expected three parts: mode, source file, destination file."
         )
-    if command.split()[0] == "cp" and command.split()[1] != command.split()[2]:
-        with (open(command.split()[1], "r") as file_in,
-              open(command.split()[2], "w") as file_out):
+    mode = words[0]
+    source_file = words[1]
+    destination_file = words[2]
+    if mode == "cp" and source_file != destination_file:
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
             file_out.write(file_in.read())
