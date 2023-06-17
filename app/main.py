de25b8ef1e3e10653f5fe6d387786ed5d97b40c2
Denys Gordiichuk <dd111320@gmail.com>
2023-06-17 17:47:10 +0300

    finish

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(command: str) -> None:
     command = command.split()
     if command[1] != command[2]:
-        with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+        with (open(command[1], "r") as file_in,
+              open(command[2], "w") as file_out):
             file_out.write(file_in.read())
-
-
