0109c1ea500a3ba6ce2f85b1a7d353c7d5562b00
Denys Gordiichuk <dd111320@gmail.com>
2023-06-17 16:59:58 +0300

    finish

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    words = command.split(" ")
+    if words[1] != words[2]:
+        with open(words[1], "r") as file_in, open(words[2], "w") as file_out:
+            file_out.write(file_in.read())
+
+
+copy_file("cp fl.txt fl_copy.txt")
+copy_file("cp fl_copy.txt fl_copy1.txt")
+
+print(
+    open("fl.txt").read() == open("fl_copy.txt").read()
+)
