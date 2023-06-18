1ebc4ecf81ed08f06c8c2db4ff738b86efde73bc
Rostyslav <rostyslav.furman@gmail.com>
2023-06-18 20:06:15 +0300

    my_commit_2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
+    _, source_file, res_file = command.split()
     if not command.startswith("cp"):
         raise Exception("this interface support only 'cp' command")
-    ls_with_name = command.split()[1:]
-    if ls_with_name[0] != ls_with_name[1]:
-        with open(ls_with_name[0], "r") as file1,\
-                open(ls_with_name[1], "w") as file2:
+    if source_file != res_file:
+        with open(source_file, "r") as file1, open(res_file, "w") as file2:
             file2.write(file1.read())
