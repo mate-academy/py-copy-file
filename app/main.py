8110cb3090b3b1a182799a1cb978eb350b682546
Vlad Dukh <vladdukh15@gmail.com>
2023-06-19 22:36:26 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -8,15 +8,10 @@ def copy_file(command: str) -> None:
             raise ValueError(
                 "Wrong command, you should use 'cp' command to copy file"
             )
-        if file_to_copy == new_file:
-            raise ValueError(
-                "Wrong name file, you can't copy file to itself"
-            )
         if not path.exists(file_to_copy):
             raise FileNotFoundError("File doesn't exists")
 
-        with open(file_to_copy, "r") as file_in, \
-                open(new_file, "w") as file_out:
+        with open(file_to_copy, "r") as file_in, open(new_file, "w") as file_out:
             file_out.write(file_in.read())
     except (ValueError, FileNotFoundError) as e:
         print(f"{type(e).__name__}: {e}")
