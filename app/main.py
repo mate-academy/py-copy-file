79c0ebc1c234867a892fc47dd52583da191b5fdb
Vlad Dukh <vladdukh15@gmail.com>
2023-06-20 17:20:49 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -11,7 +11,8 @@ def copy_file(command: str) -> None:
         if not path.exists(file_to_copy):
             raise FileNotFoundError("File doesn't exists")
 
-        with open(file_to_copy, "r") as file_in, open(new_file, "w") as file_out:
+        with open(file_to_copy, "r") as file_in, \
+                open(new_file, "w") as file_out:
             file_out.write(file_in.read())
     except (ValueError, FileNotFoundError) as e:
         print(f"{type(e).__name__}: {e}")
