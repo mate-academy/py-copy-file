58791f2151a43b39fd71700a1d8062ed7969932b
Jorjio <Jorjio22@gmail.com>
2023-06-21 15:16:53 +0000

    'Removed'

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ def copy_file(command: str) -> None:
 
     if sour_file == des_file or copy_file_command != "cp":
         return
-    else:
-        print(f"{copy_file_command} {sour_file} {des_file}")
-        with open(sour_file, "r") as file_in, open(des_file, "w") as file_out:
-            file_out.write(file_in.read())
+
+    with open(sour_file, "r") as file_in, open(des_file, "w") as file_out:
+        file_out.write(file_in.read())
