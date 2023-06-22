b95c6effcff7b3496d80b7798a28047a1829c765
Michael <manchester6913@gmail.com>
2023-06-22 07:56:46 -0400

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    list_words = command.split()
+    if list_words[1] != list_words[2]:
+        with open(list_words[1], "r") as file_in, \
+                open(list_words[2], "w") as file_out:
+            content_file = file_in.read()
+            file_out.write(content_file)
