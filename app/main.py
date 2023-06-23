aa0631fb1c4c83e0bdc44eb10de80c8cc2ae2004
Nazar Lytvyniuk <nazarltvnk@icloud.com>
2023-06-23 21:15:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if command:
+        split_command = command.split(" ")
+
+        if len(split_command) == 3 and split_command[0] == "cp":
+            file_name1 = split_command[1]
+            file_name2 = split_command[2]
+
+            if file_name1 == file_name2:
+                return
+
+            with open(file_name1, "r") as f1, open(file_name2, "w") as f2:
+                for line in f1.readlines():
+                    f2.write(line)
