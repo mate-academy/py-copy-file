ab43da753af37ba7f71eb3eda4ccd96219862c56
Michael <manchester6913@gmail.com>
2023-06-23 07:06:35 -0400

    Solution_3

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 def copy_file(command: str) -> None:
-    command = command.split()
-    if len(command) == 3:
-        if command[0] == "cp" and command[1] != command[2]:
-            with open(command[1], "r") as file_in, \
-                    open(command[2], "w") as file_out:
-                content_file = file_in.read()
-                file_out.write(content_file)
+    command_list = command.split()
+    if len(command_list) == 3:
+        copy_command, file_in_name, file_out_name = command_list
+        if copy_command == "cp" and file_in_name != file_out_name:
+            with (open(file_in_name, "r") as file_in,
+                  open(file_out_name, "w") as file_out):
+                file_out.write(file_in.read())
+
+
+file = open("file.txt", "w")
+file.write("17:00 14/09/2022 value error")
+file.close()
+copy_file("cp file.txt new_file.txt")
