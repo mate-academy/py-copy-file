76a003275a750d9b30241b7a977153fd95bf82bb
Nazar Lytvyniuk <nazarltvnk@icloud.com>
2023-06-24 13:18:04 +0200

    Fix code

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 def copy_file(command: str) -> None:
     if command:
-        split_command = command.split(" ")
+        split_command = command.split()
+        file_name1 = split_command[1]
+        file_name2 = split_command[2]
 
-        if len(split_command) == 3 and split_command[0] == "cp":
-            file_name1 = split_command[1]
-            file_name2 = split_command[2]
+        if (
+                len(split_command) == 3
+                and split_command[0] == "cp"
+                and file_name1 != file_name2
+        ):
 
-            if file_name1 == file_name2:
-                return
-
-            with open(file_name1, "r") as f1, open(file_name2, "w") as f2:
-                for line in f1.readlines():
-                    f2.write(line)
+            with (
+                open(file_name1, "r") as origin_file,
+                open(file_name2, "w") as duplicate_file
+            ):
+                content_from_origin = origin_file.read()
+                duplicate_file.write(content_from_origin)
