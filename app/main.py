93b3390a1cdc8c2457badf1bac20935477100eaa
stepband <vladshvik@gmail.com>
2023-06-26 22:05:59 +0300

    Implemented validation of command length and check is firs command "cp"

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     command_parts = command.split()
-    if len(command_parts) != 3 or command_parts[0] != 'cp':
+    if len(command_parts) != 3 or command_parts[0] != "cp":
         return
 
     _, source_file, dest_file = command_parts
