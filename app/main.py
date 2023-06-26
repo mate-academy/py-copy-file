7a2f4812711c178ebc5cc6b3583dd695c7724066
stepband <vladshvik@gmail.com>
2023-06-26 20:38:51 +0300

    Function works with copying file content. Tested with .txt.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    _, source_file, dest_file = command.split()
+    if source_file == dest_file:
+        return
+    with (open(source_file, "r") as file_in,
+          open(dest_file, "w") as file_out):
+        file_out.write(file_in.read())
