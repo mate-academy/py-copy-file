2d379c11176bc8e2ef8ba4eafc97bf55299b780f
Alex Matiash <matiash.jr@gmail.com>
2023-06-29 22:50:12 +0300

    'Solution3'

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ def copy_file(command: str) -> None:
     if len(command) != 3:
         raise IndexError("Your command is wrong")
     cmd, source_file, destination_file = command
-    if source_file == destination_file or cmd == "cp":
-        return
     if command[0] == "cp":
         with (
             open(source_file, "r") as source,
