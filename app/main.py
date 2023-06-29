6dd6e7c6342fc709714fb6a0fbb68c70afdf8389
Alex Matiash <matiash.jr@gmail.com>
2023-06-29 13:48:22 +0300

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ def copy_file(command: str) -> None:
     command = command.split()
     if len(command) != 3:
         raise IndexError("list index out of range")
-    source_file = command[1]
-    destination_file = command[2]
-    if source_file == destination_file:
+    cmd, source_file, destination_file = command
+    if source_file == destination_file or cmd == "cp":
         return
     if command[0] == "cp":
-        with (open(source_file, "r") as f,
-                open(destination_file, "w") as s):
-            s.write(f.read())
+        with (
+            open(source_file, "r") as source,
+            open(destination_file, "w") as destination
+        ):
+            destination.write(source.read())
 
 
 copy_file("cp file.txt new_file.txt")
