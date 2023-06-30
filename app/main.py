f0fa7176ac7df05fbc942437cfc8ed6a3b2b78f3
Alek Ilchenko <yaichenko87@gmail.com>
2023-06-30 09:40:50 +0100

    Update

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(command: str) -> None:
-    command = command.split(" ")
+    command = command.split()
     command_order = command[0]
     origin_file = command[1]
     copy_file = command[2]
-    if origin_file == copy_file or command_order != "cp":
-        return None
-    with open(origin_file, "r") as file_in, open(copy_file, "w") as file_out:
-        file_out.write(file_in.read())
+    if origin_file != copy_file and command_order == "cp":
+        with open(origin_file, "r") as file_in, open(copy_file, "w") as file_out:
+            file_out.write(file_in.read())
