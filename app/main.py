c9b9208d2e48743106a08f4ba2f91a60a891eff8
Andrew Sulym <a.sulymm@gmail.com>
2023-06-30 18:47:44 +0100

    Fix

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 def copy_file(command: str) -> None:
-    command = command.split()
-    command_info = [0]
-    file_current = command[1]
-    file_copy = command[2]
+    command_info, file_current, file_copy = command.split()
 
     if file_current != file_copy and command_info == "cp":
-        with open(file_current, "r") as file_in, \
-             open(file_copy, "w") as file_out:
+        with (
+             open(file_current, "r") as file_in,
+             open(file_copy, "w") as file_out
+             ):
             file_out.write(file_in.read())
