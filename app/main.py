995ab7a9f4ae2190679014caf56c49307cad8904
Alek Ilchenko <yaichenko87@gmail.com>
2023-06-30 09:46:08 +0100

    Fix flake8 error

--- app/main.py
+++ app/main.py
@@ -4,5 +4,6 @@ def copy_file(command: str) -> None:
     origin_file = command[1]
     copy_file = command[2]
     if origin_file != copy_file and command_order == "cp":
-        with open(origin_file, "r") as file_in, open(copy_file, "w") as file_out:
+        with open(origin_file, "r") as file_in,\
+                open(copy_file, "w") as file_out:
             file_out.write(file_in.read())
