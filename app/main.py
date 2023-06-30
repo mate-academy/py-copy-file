47436ad5b7ae2560bb524d15e1c0a28ff4646117
Kateryna Semesenko <semesenkoekaterina@gmail.com>
2023-06-30 22:31:59 +0300

    Аdded check

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ import os
 
 
 def copy_file(command: str) -> None:
+
     command_list = command.split(" ")
-
-    command_name = command_list[0]
-    src_file = command_list[1]
-    dest_file = command_list[2]
-
-    if (command_name == "cp"
-            and src_file != dest_file
-            and os.path.exists(src_file)):
-        with (open(command_list[1], "r") as file_in,
-              open(command_list[2], "w") as file_out):
-            file_out.write(file_in.read())
+    if len(command_list) == 3:
+        command_name = command_list[0]
+        src_file = command_list[1]
+        dest_file = command_list[2]
+        if (command_name == "cp"
+                and src_file != dest_file
+                and os.path.exists(src_file)):
+            with (open(src_file, "r") as file_in,
+                  open(dest_file, "w") as file_out):
+                file_out.write(file_in.read())
