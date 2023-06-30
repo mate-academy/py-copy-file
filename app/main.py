f362bd6b2b235c2a770d8e65a3d88ae115d9280b
Kateryna Semesenko <semesenkoekaterina@gmail.com>
2023-06-30 14:20:31 +0300

    Implemented py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    command_list = command.split(" ")
+    if command_list[0] == "cp" and command_list[1] != command_list[2] and os.path.exists(command_list[1]):
+        with (open(command_list[1], "r") as file_in,
+              open(command_list[2], "w") as file_out):
+            text = file_in.read()
+            file_out.write(text)
