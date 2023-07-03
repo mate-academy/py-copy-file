54b0bdd772ade6e1426aa969b8694629051d24e1
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2023-07-03 21:15:40 +0300

    Changes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
-    command = command.split(" ")
+    command = command.split()
     source_file_name = command[1]
     new_file_name = command[2]
 
     if source_file_name != new_file_name:
-        with open(source_file_name, "r") as source_file, \
-                open(new_file_name, "w") as new_file:
+        with (open(source_file_name, "r") as source_file,
+              open(new_file_name, "w") as new_file):
             new_file.write(source_file.read())
