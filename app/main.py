d24e7e26c047ac0ee91c11b79af50c2877cdcdd4
FadieievaA <fadieieva.arina@gmail.com>
2023-07-04 14:59:35 +0300

    copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cmd_list = command.split()
+    if cmd_list[1] != cmd_list[2]:
+        with open(cmd_list[1], "r") as file_in, \
+                open(cmd_list[2], "w") as file_out:
+            file_out.write(file_in.read())
