7505d26b7766fdbf2403d833884bfc36f2069e0f
Volodymyr Yerokhov <kavbian@gmail.com>
2023-07-05 21:44:24 +0300

    'flake8'

--- app/main.py
+++ app/main.py
@@ -9,5 +9,6 @@ def copy_file(command: str) -> None:
     if in_file_name == out_file_name:
         return
 
-    with open(in_file_name, "r") as file_in, open(out_file_name, "w") as file_out:
+    with open(in_file_name, "r") as file_in, \
+            open(out_file_name, "w") as file_out:
         file_out.write(file_in.read())
