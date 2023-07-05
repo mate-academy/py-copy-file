4b71d68cf67c0d968f2a9745fdff9b73542df34b
Volodymyr Yerokhov <kavbian@gmail.com>
2023-07-05 22:06:24 +0300

    'check_variable_name_()'

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 def copy_file(command: str) -> None:
-    words = command.split()
-    if len(words) >= 3:
-        in_file_name = words[1]
-        out_file_name = words[2]
+    separate_command = command.split()
+    if len(separate_command) >= 3 and separate_command[0] == "cp":
+        in_file_name = separate_command[1]
+        out_file_name = separate_command[2]
     else:
         return "Incorrect command!"
 
     if in_file_name == out_file_name:
         return
 
-    with open(in_file_name, "r") as file_in, \
-            open(out_file_name, "w") as file_out:
+    with (open(in_file_name, "r") as file_in,
+            open(out_file_name, "w") as file_out):
         file_out.write(file_in.read())
