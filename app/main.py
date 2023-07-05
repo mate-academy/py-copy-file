f535c480163532f6ebc6b266740ebfc66427aede
Volodymyr Yerokhov <kavbian@gmail.com>
2023-07-05 18:53:16 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    words = command.split()
+    file1 = words[1]
+    file2 = words[2]
+
+    if file1 == file2:
+        return None
+
+    with open(file1, "r") as f_in, open(file2, "w") as f_out:
+        f_out.write(f_in.read())
