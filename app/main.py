0175acfbde7bc1755fa427546ba1773ccee9f9a7
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-07-06 13:55:42 +0300

    cp_like_Linux

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cmd_1 = command.split()
+    if cmd_1[1] != cmd_1[2]:
+        with open(cmd_1[1], "r") as file_in, open(cmd_1[2], "w") as file_out:
+            file_out.write(file_in.read())
