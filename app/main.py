e0b7bf11e3d1411094ff70745e8ec9427fced2d5
Maksym Hovorunenko <govorunenko@gmail.com>
2023-07-07 11:06:16 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    with open(command.split()[1] + ".txt", "r") as text_file,\
+            open(command.split()[2], "w") as copy_text_file:
+        content = text_file.read()
+        copy_text_file.write(content)
