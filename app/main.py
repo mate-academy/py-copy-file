95f927d360481a65bc98c1241068cf617c3d7b55
Olexandr <maxbvling23432@gmail.com>
2023-07-07 10:32:26 +0300

    Merge remote-tracking branch 'origin/master'

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 def copy_file(argument: str) -> tuple:
-    if len(argument.rsplit(" ")) == 3:
-        cp, first_file, second_file = argument.split()
+    if len(argument.rsplit(", ")) == 3:
+        cp, first_file, second_file = argument.split(", ")
         if first_file != second_file and cp == "Copied":
             with open(first_file, "rb") as first, (
                     open(second_file, "wb")
             ) as dst:
                 dst.write(first.read())
-                print("Copied")
             if open(first_file).read() == open(second_file).read():
+                print("Copied")
                 return
+
+argument1 = "Copied, txt.txt, ff929.txt"
+copy_file(argument1)
