c31db96abeff8ef5af9fb6543a21391226d8902a
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-07-07 17:12:27 +0300

    cd_like_Linux

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ def copy_file(command: str) -> None:
     name_of_old_file = cmd_1[1]
     name_of_new_file = cmd_1[2]
     if name_of_old_file != name_of_new_file:
-        with (open(name_of_old_file, "r") as file_in,
-              open(name_of_new_file, "w") as file_out
-        ):
+        with open(name_of_old_file, "r") as file_in,\
+             open(name_of_new_file, "w") as file_out:
             file_out.write(file_in.read())
