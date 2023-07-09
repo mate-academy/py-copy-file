b9dfb8cca9be5440b11116771b33093dd82efdb8
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-07-09 13:35:54 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, file_name, copy_file_name = command.split(" ")
+
+    if command == "cp" and file_name != copy_file_name:
+        with (
+            open(file_name, "r") as file_in,
+            open(copy_file_name, "w") as file_out
+        ):
+            content = file_in.read()
+            file_out.write(content)
