f5fb99f9b5411d132d675eee71c149ad2d77f631
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-07-10 08:43:26 +0300

    Implemented Copy-file task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    elements = command.split()
+    if len(elements) == 3:
+        cp_command, initial_file, copied_file = elements
+        if cp_command == "cp" and initial_file != copied_file:
+            with (
+                open(initial_file, "r") as file_in,
+                open(copied_file, "w") as file_out
+            ):
+                if os.path.exists(initial_file):
+                    file_out.write(file_in.read())
