6331216cdb059e3d90a5dfe5279b812925dc911d
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2023-07-11 16:26:13 +0300

    - added brackets

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
-    list_of_arguments_in_command = command.split(" ")
+    list_of_arguments_in_command = command.split()
 
     file_to_copy = list_of_arguments_in_command[1]
     file_copy = list_of_arguments_in_command[2]
 
     if file_to_copy != file_copy:
-        with open(file_to_copy, "r") as file_in, \
-             open(file_copy, "w") as file_out:
+        with (open(file_to_copy, "r") as file_in,
+             open(file_copy, "w") as file_out):
             file_out.write(file_in.read())
