da0b3e4baec352b92d600a534aff969e76f81dc9
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2023-07-12 18:54:30 +0200

    Corrected solution

--- app/main.py
+++ app/main.py
@@ -3,5 +3,7 @@ def copy_file(command: str) -> None:
     file_in_name = file_split[1]
     file_out_name = file_split[2]
     if file_in_name != file_out_name:
-        with open(file_in_name, "r") as file_in, open(file_out_name, "w") as file_out:
+        with open(
+                file_in_name, "r") as file_in, open(
+                file_out_name, "w") as file_out:
             file_out.write(file_in.read())
