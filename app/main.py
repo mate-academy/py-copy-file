137efc17eb3b3760681bf8bddf28702641bd6cd8
Yuliia <verbikyullya@gmail.com>
2023-07-15 15:55:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_name = command.split()[1]
+    new_file_name = command.split()[2]
+    file_content = ""
+    if file_name == new_file_name:
+        return
+    with open(file_name, "r") as f:
+        file_content = f.read()
+    with open(new_file_name, "w") as f:
+        f.write(file_content)
