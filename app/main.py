9efe5815ee5743c747ff99f9fea976d2b213d993
Tanya Burdenko <burdenko.tetyana@gmail.com>
2023-07-17 14:06:18 +0300

    v6

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    if len(command.split()) == 3 and command.split()[0] == "cp":
-        original, copy = command.split()[1], command.split()[2]
-        if original != copy:
+    if len(command.split()) == 3:
+        cmd, original, copy = command.split()
+        if cmd == "cp" and original != copy:
             with open(original, "r") as file_in, open(copy, "w") as file_out:
                 file_out.write(file_in.read())
