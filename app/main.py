41878d3c638f05805381bbb1c00c7df29b15b4b0
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-07-20 22:38:23 +0300

    added requested changes

--- app/main.py
+++ app/main.py
@@ -1,16 +1,7 @@
 def copy_file(command: str) -> None:
-    command_name, file_name, new_name = command.split(" ")
-    if (command_name == "cp"
-            and new_name != file_name):
-        with open(f"{file_name}") as file:
-            content = file.read()
-        with open(f"{new_name}", "w") as new_file:
-            new_file.write(content)
-
-
-if __name__ == "__main__":
-    file_one = "file.txt"
-    file_two = "new_file.txt"
-    copy_file(f"cp {file_one} {file_two}")
-    with open(file_one) as one, open(file_two) as two:
-        print(one.read() == two.read())
+    split = command.split()
+    if (len(split) == 3
+            and split[0] == "cp"
+            and split[1] != split[2]):
+        with open(split[1]) as content, open(split[2], "w") as new_file:
+            new_file.write(content.read())
