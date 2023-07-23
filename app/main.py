6581912839d6ef230ffa49931a9616e538694ca6
Marina <marishka28023@gmail.com>
2023-07-23 17:52:56 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    command_parts = command.split()
+
+    if len(command_parts) != 3:
+        raise ValueError("Invalid command format")
+
+    _, source_file, destination_file = command_parts
+
+    if source_file == destination_file:
+        return
+
+    with open(source_file, "r") as file_in, \
+            open(destination_file, "w") as file_out:
+        shutil.copyfileobj(file_in, file_out)
