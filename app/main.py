3951e4aceabbdbea083128e6b087ba3be2ebdc73
Marina <marishka28023@gmail.com>
2023-07-24 14:30:10 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -1,17 +1,8 @@
-import shutil
-
-
 def copy_file(command: str) -> None:
     command_parts = command.split()
-
-    if len(command_parts) != 3:
-        raise ValueError("Invalid command format")
-
-    _, source_file, destination_file = command_parts
-
-    if source_file == destination_file:
-        return
-
-    with open(source_file, "r") as file_in, \
-            open(destination_file, "w") as file_out:
-        shutil.copyfileobj(file_in, file_out)
+    if command_parts[1] == command_parts[2]:
+        pass
+    if command_parts[0] == "cp":
+        with open(command_parts[1], "r") as file_in,\
+                open(command_parts[2], "w") as file_out:
+            file_out.write(file_in.read())
