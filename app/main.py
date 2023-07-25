4fb10e686c01147cf9037117e2dcefd04cb075fb
Marina <marishka28023@gmail.com>
2023-07-25 18:38:31 +0200

    'Solution4'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def copy_file(command: str) -> None:
     command_parts = command.split()
+    if len(command_parts) != 3:
+        raise ValueError("Invalid command format")
     if command_parts[1] == command_parts[2]:
         pass
     if command_parts[0] == "cp":
