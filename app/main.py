02877a45cc6d1732ea947d843e940f709818c5e6
Illya Zhmurskiy <thebrawler6@gmail.com>
2023-07-26 22:14:08 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,10 +4,9 @@ def copy_file(command: str) -> None:
     if len(parts) < 3 or parts[0] != "copy" or parts[1] == parts[2]:
         return
 
-    source_file = parts[1]
-    destination_file = parts[2]
+    _, source_file, destination_file = parts
 
-    with open(source_file, "r") as file_in,\
-            open(destination_file, "w") as file_out:
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
         content = file_in.read()
         file_out.write(content)
