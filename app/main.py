ce30fde570f43c89e89dcbc588ff81bc9b5d7f8a
H1kki <vetalkuz102@gmail.com>
2023-07-28 21:52:00 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_parts = command.split()
+    _, source_file, destination_file = command_parts
+
+    if source_file == destination_file:
+        return
+    with open(
+            source_file,
+            "r"
+    ) as file_in, open(
+        destination_file,
+        "w"
+    ) as file_out:
+        file_out.write(file_in.read())
+
+
+copy_file("cp file.txt file1.txt")
