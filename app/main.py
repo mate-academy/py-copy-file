804572467c45c7b66e8f08080ddd0e6d529157ce
Vladyslav Shepilov <shepsterya@gmail.com>
2023-07-30 17:06:43 +0300

    resloved

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    source, destination = command.split()[1:]
+    if source == destination:
+        return
+    with open(source, "r") as source_file,\
+         open(destination, "w") as destination_file:
+        destination_file.write(source_file.read())
