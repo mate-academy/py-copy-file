dca721c14ef9be98aa6b73029d2da07c8cb0264c
Dima Khrystenko <hrystanko.dmytro@outlook.com>
2023-08-02 18:06:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split()) == 3:
+        cmd = command.split()[0]
+        file_in = command.split()[1]
+        file_out = command.split()[2]
+
+        if cmd == "cp" and file_in != file_out:
+            try:
+                with open(file_in, "r") as source, open(file_out, "w") as destination:
+                    content = source.read()
+                    destination.write(content)
+            except FileNotFoundError:
+                print("The source file was not found in working directory")
