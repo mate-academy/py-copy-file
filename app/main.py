ae4594893a291e888ec275e82b9633d53a1ab511
struhanets <posseydon87@gmail.com>
2023-08-02 22:49:28 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 def copy_file(command: str) -> None:
 
     parts = command.split()
-    cmd, file_name, file_copy_name = parts
-    if cmd == "cp" and file_name != file_copy_name:
-        with open(file_name, "r") as content_out, \
-                open(file_copy_name, "w") as content_in:
+    if len(parts) == 3:
+        cmd, file_name, file_copy_name = parts
+        if cmd == "cp" and file_name != file_copy_name:
+            with open(file_name, "r") as content_out, \
+                    open(file_copy_name, "w") as content_in:
 
-            copy_text = content_out.read()
-            content_in.write(copy_text)
-    else:
-        print("Does nothing")
+                copy_text = content_out.read()
+                content_in.write(copy_text)
+        else:
+            print("Does nothing")
