a5947619450efa8d336b0f90deb861a09f607250
struhanets <posseydon87@gmail.com>
2023-08-02 09:45:50 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 def copy_file(command: str) -> None:
-    todolist = command.split(" ")
-    file_name = todolist[1]
-    file_copy_name = todolist[2]
-    with open(file_name, "w") as some_info:
-        some_info.write(input())
-    if file_name != file_copy_name:
-        with open(file_name, "r") as content_out, \
-                open(file_copy_name, "w") as content_in:
-            copy_text = content_out.read()
-            content_in.write(copy_text)
-    else:
-        print("Does nothing")
+    if "cp" in command:
+        todolist = command.split()
+        *other, file_name, file_copy_name = todolist
+
+        if file_name != file_copy_name:
+            with open(file_name, "r") as content_out, \
+                    open(file_copy_name, "w") as content_in:
+
+                copy_text = content_out.read()
+                content_in.write(copy_text)
+        else:
+            print("Does nothing")
