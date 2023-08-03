4fb61512154f976a5d3b0ae5a3b195f0ac6fdee0
Anastasia <anastasiiya.p@gmail.com>
2023-08-03 16:51:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    splitted = command.split()
+
+    if splitted[0] == "cp" and splitted[1] != splitted[2]:
+        with (open(splitted[1], "r") as file_in,
+              open(splitted[2], "a") as file_out):
+            for line in file_in:
+                file_out.write(line)
