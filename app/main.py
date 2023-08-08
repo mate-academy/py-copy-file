e6dc23c61e58fcc1b43d4ab83a4d5025ccb3c518
Bogdan Dovgan <dovganbv94@gmail.com>
2023-08-08 11:52:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ def copy_file(command: str) -> None:
         print("Enter the correct command.")
         return
     _, file_copy, file_paste = command
-    if file_copy == file_paste:
-        return
-    with open(file_copy, "r") as file_in, open(file_paste, "w") as file_out:
-        file_out.write(file_in.read())
+    if file_copy != file_paste and _ == "cp":
+        with (open(file_copy, "r") as file_in,
+              open(file_paste, "w") as file_out):
+            file_out.write(file_in.read())
