95f5f343f9a9ef4244b6bfbff12cab2e8d424e0f
Bogdan Dovgan <dovganbv94@gmail.com>
2023-08-08 13:51:41 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ def copy_file(command: str) -> None:
     if len(command) != 3:
         print("Enter the correct command.")
         return
-    _, file_copy, file_paste = command
-    if file_copy != file_paste and _ == "cp":
+    cmd, file_copy, file_paste = command
+    if file_copy != file_paste and cmd == "cp":
         with (open(file_copy, "r") as file_in,
               open(file_paste, "w") as file_out):
             file_out.write(file_in.read())
