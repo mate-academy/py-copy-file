244548eacaa9cee3326b2faca0c14c8f796d8092
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-08-08 22:32:34 +0200

    Loading and writing into file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    separate = command.split()
+    old_file = separate[1]
+    new_file = separate[2]
+    if separate[0] == "cp" and old_file != new_file:
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            for each_line in file_in:
+                file_out.write(each_line)
