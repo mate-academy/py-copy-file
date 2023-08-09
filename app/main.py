d2e9b03adff412f9d94a13dd65e4dfc3b30e03a9
Andrii Ishchenko <iscenkoa@gmail.com>
2023-08-09 22:20:11 +0300

    I believе шin it

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(command: str) -> None:
-    command_in_list = command.split()
-    if (len(command_in_list) == 3
-            and command_in_list[1] != command_in_list[2]
-            and command_in_list[0] == "cp"):
+    if len(command.split()) == 3:
+        command_name, file_name, copy_file_name = command.split()
+        if file_name != copy_file_name and command_name == "cp":
 
-        with (open(command_in_list[1], "r") as file_in,
-              open(command_in_list[2], "w") as file_out):
-            file_out.write(file_in.read())
+            with (open(file_name, "r") as file_in,
+                  open(copy_file_name, "w") as file_out):
+                file_out.write(file_in.read())
