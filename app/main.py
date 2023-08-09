b8ad263d7d61fa38088f9f8e04717c9c219d6ace
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-08-09 12:26:12 +0200

    Refactored1

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
     separate = command.split()
+    if len(separate) != 3:
+        raise ValueError("Incorrect command line.")
     old_file = separate[1]
     new_file = separate[2]
-    if len(separate) == 3 and separate[0] == "cp" and old_file != new_file:
+    if separate[0] == "cp" and old_file != new_file:
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             for each_line in file_in:
                 file_out.write(each_line)
-    else:
-        raise ValueError("Incorrect command line.")
