c5afe951b59558f69fe4f47eec2d235ee8a8c7aa
Ihor Bachuk <igor.bachuk04@gmail.com>
2023-08-10 21:02:24 +0300

    solution update update

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    command = command.split(" ")
-    if len(command) == 3 and command[1] != command[2]:
+    command = command.split()
+    if len(command) == 3 and command[1] == "cp" and command[1] != command[2]:
         with (open(command[1], "r") as file_in,
                 open(command[2], "w") as file_out):
             file_out.write(str(file_in.read()))
