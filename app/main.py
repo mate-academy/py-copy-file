602eb3bd28f957dd202c315ad3b5762fb1b6d762
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-08-12 16:48:26 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    args = command.split(" ")
+    com = args[0]
+    file_copy = args[1]
+    new_file = args[2]
+
+    if com == "cp" and file_copy != new_file:
+        with open(file_copy, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
