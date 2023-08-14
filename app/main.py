5baad12e23a63307d7a7958386b41498e36e0fbe
Alex Pelevin <ya.pelevin1999@gmail.com>
2023-08-14 12:51:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+def copy_file(command: str) -> None:
+    arguments = command.split(" ")
+
+    if len(arguments) > 0 and arguments[0] == "cp":
+        if len(arguments) < 2:
+            print("Missing file operand")
+
+        if len(arguments) < 3:
+            print(f"Missing destination file operand after '{arguments[1]}'")
+
+        if arguments[1] == arguments[2]:
+            print(f"'{arguments[1]}' and '{arguments[2]}' are the same file")
+
+        try:
+            with open(arguments[1], "r") as file_in:
+                text = file_in.read()
+
+            with open(arguments[2], "w") as file_out:
+                file_out.write(text)
+
+        except FileNotFoundError as e:
+            print(e)
+        else:
+            print("Copy successfully")
+    else:
+        print("Command not found")
+
+
+copy_file("cp text1.txt text.txt")
