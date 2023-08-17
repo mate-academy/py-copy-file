a11c9708aebbb337cecb56e64248b39cbfd1d99c
Oleksandr Suligan <suligan.alexander@gmail.com>
2023-08-17 17:44:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    files_names = command.split(" ")
+    first_file, second_file = files_names[1], files_names[2]
+
+    if first_file == second_file:
+        return
+
+    with open(first_file, "r") as original, open(second_file, "w") as copied:
+        copied.write(original.read())
+
+
+copy_file("cp file.txt new_file.txt")
