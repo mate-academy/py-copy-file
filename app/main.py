9ab7d77b5b471e2b2fdda766d67c410522b9e239
Oleksandr Suligan <suligan.alexander@gmail.com>
2023-08-18 17:20:24 +0300

    Solution: fixed len args check

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(command: str) -> None:
     files_names = command.split()
-    first_file, second_file = files_names[1], files_names[2]
-
     if (len(files_names) == 3 and files_names[0] == "cp"
             and files_names[1] != files_names[2]):
+        first_file, second_file = files_names[1], files_names[2]
         with (open(first_file, "r") as original,
               open(second_file, "w") as copied):
             copied.write(original.read())
