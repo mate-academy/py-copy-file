980dbc24747848fd20dda8e1ed3b12f485557754
Oleksandr Suligan <suligan.alexander@gmail.com>
2023-08-18 18:42:22 +0300

    Solution: renamed var 'files_names' to 'arguments'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
-    files_names = command.split()
-    if (len(files_names) == 3 and files_names[0] == "cp"
-            and files_names[1] != files_names[2]):
-        first_file, second_file = files_names[1], files_names[2]
+    arguments = command.split()
+    if (len(arguments) == 3 and arguments[0] == "cp"
+            and arguments[1] != arguments[2]):
+        first_file, second_file = arguments[1], arguments[2]
         with (open(first_file, "r") as original,
               open(second_file, "w") as copied):
             copied.write(original.read())
