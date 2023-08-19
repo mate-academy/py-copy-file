150b69a76a244ec2180598c14520eb6e30038452
Mikhail <tiunovmixs@gmail.com>
2023-08-19 19:43:34 +0300

    Remove "else"

--- app/main.py
+++ app/main.py
@@ -4,6 +4,5 @@ def copy_file(command: str) -> None:
     name2 = parts[2]
     if name1 == name2:
         return
-    else:
-        with open(name1, "r") as file_in, open(name2, "w") as file_out:
-            file_out.write(file_in.read())
+    with open(name1, "r") as file_in, open(name2, "w") as file_out:
+        file_out.write(file_in.read())
