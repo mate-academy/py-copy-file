91f3abca69672c136b1f813ddbebbc8ac47c17ca
Mikhail <tiunovmixs@gmail.com>
2023-08-19 18:08:59 +0300

    Implemented copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    name1 = parts[1]
+    name2 = parts[2]
+    if name1 == name2:
+        print("Incorrectly")
+    else:
+        with open(name1, "r") as file_in, open(name2, "w") as file_out:
+            file_out.write(file_in.read())
