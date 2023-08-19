488cddc9a5c9b14cc7560e16060c7ee440fb49cd
Mikhail <tiunovmixs@gmail.com>
2023-08-19 19:24:23 +0300

    Change print to return

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ def copy_file(command: str) -> None:
     name1 = parts[1]
     name2 = parts[2]
     if name1 == name2:
-        print("Incorrectly")
+        return
     else:
         with open(name1, "r") as file_in, open(name2, "w") as file_out:
             file_out.write(file_in.read())
