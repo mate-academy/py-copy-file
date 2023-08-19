8c051aa6d56b76a1712ddc5cb40b5a22a957695e
Olha Davydova <o.chookity@gmail.com>
2023-08-19 09:50:22 +0300

    Implement copy_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+
+    file_in_name = command[1]
+    file_out_name = command[2]
+
+    if file_in_name != file_out_name:
+        with (open(file_in_name, "r") as file_in,
+              open(file_out_name, "w") as file_out):
+            file_out.write(file_in.read())
