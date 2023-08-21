c21a694099345d3c310e5db5792adb6fdd34feae
Taras Borshch <tarasborshch4@gmail.com>
2023-08-21 17:23:10 +0300

    Completed py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    keys = command.split(" ")
+    try:
+        file_name = keys[1]
+        copy_name = keys[2]
+    except IndexError:
+        return
+
+    if file_name == copy_name:
+        return
+
+    with open(file_name, "r") as old_file, open(copy_name, "w") as file_copy:
+        file_content = old_file.read()
+        file_copy.write(file_content)
