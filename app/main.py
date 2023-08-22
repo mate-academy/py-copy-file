b90553fc538bdde7031d5a1382457b6c2186eeec
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-08-22 21:05:18 +0300

    Implemented copy_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    splitted_command = command.split()
+
+    if splitted_command[0] != "cp":
+        return
+
+    source_file = splitted_command[1]
+    destination_file = splitted_command[2]
+
+    if source_file != destination_file:
+        with (
+            open(source_file, "r") as file_in,
+            open(destination_file, "w") as file_out,
+        ):
+            for line in file_in.readlines():
+                file_out.write(line)
