bef0f4f64d26918a44acc39bea557b413773b5ce
Yeva <yevabrookman@gmail.com>
2023-08-22 20:24:04 +0300

    Added unpacking and checking command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 def copy_file(command: str) -> None:
-    files = tuple(command.split()[1:])
+    try:
+        command, original_file, copy_file = command.split()
+    except ValueError:
+        return
 
-    if files[0] != files[1]:
-        with open(files[0], "r") as file_in, open(files[1], "w") as file_out:
+    if command == "cp" and original_file != copy_file:
+        with open(original_file, "r") as file_in, open(
+            copy_file, "w"
+        ) as file_out:
             file_out.write(file_in.read())
