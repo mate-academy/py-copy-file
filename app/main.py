70ce1d2c0fa32f652d00bc950cc2d30597fa7cee
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-08-22 16:05:28 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> str:
+    parts_of_command = command.split()
+    if parts_of_command[1] == parts_of_command[2]:
+        return command
+
+    with open(parts_of_command[1], "r") as copy_from:
+        with open(parts_of_command[2], "w") as copy_to:
+            copy_to.write(copy_from.read())
