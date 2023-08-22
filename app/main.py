eb0cae473b0c43c3c742415973b461ebeb4f9c48
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-08-22 19:24:41 +0200

    update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-def copy_file(command: str) -> str:
-    parts_of_command = command.split()
-    if parts_of_command[1] == parts_of_command[2]:
-        return command
-
-    with open(parts_of_command[1], "r") as copy_from:
-        with open(parts_of_command[2], "w") as copy_to:
+def copy_file(command_string: str) -> str:
+    command, original_file, copy_file = command_string.split()
+    if command == "cp" and original_file != copy_file:
+        with (
+            open(original_file, "r") as copy_from,
+            open(copy_file, "w") as copy_to
+        ):
             copy_to.write(copy_from.read())
