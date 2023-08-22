dca12739047aad3e5a136dd83f5d2fb3f17d77f0
oreny <orenycdota@gmail.com>
2023-08-22 18:37:09 +0300

    Task solved

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    list_from_command = command.split(" ")
+    if len(list_from_command) != 3:
+        return
+    if list_from_command[0] != "cp":
+        return
+    if list_from_command[1] == list_from_command[2]:
+        return
+    with open(list_from_command[1], "r") as file_in, open(
+        list_from_command[2], "w"
+    ) as file_out:
+        file_out.write(file_in.read())
