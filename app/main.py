84213cdd7b582183854e904dd23f3e1f1b41e0bb
CaCtUs1k <denischernish19012004@gmail.com>
2023-08-22 15:27:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        print("Invalid command format. Use: cp source_file target_file")
+        return
+
+    source_file_name = parts[1]
+    target_file_name = parts[2]
+
+    if source_file_name == target_file_name:
+        print("Source and target file names are the same. "
+              "Copy operation is not allowed.")
+        return
+
+    try:
+        with open(source_file_name, "r") as source, \
+             open(target_file_name, "w") as target:
+            target.write(source.read())
+        print(f"File '{source_file_name}' has "
+              f"been copied to '{target_file_name}'.")
+    except FileNotFoundError:
+        print(f"Source file '{source_file_name}' not found.")
