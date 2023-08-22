731e45646e7bc140aacc47d062dfd0f62967c5e5
Kristian Yendrek <kristianendreck4@gmail.com>
2023-08-22 12:08:17 +0300

    Implemented copy_file.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        cp_command, old_name, new_name = command.split()
+    except ValueError:
+        raise Exception("Enter correct command!")
+
+    if old_name != new_name and cp_command == "cp":
+        with open(old_name, "r") as file_in, open(new_name, "w") as file_out:
+            file_content = file_in.readlines()
+            file_out.writelines(file_content)
