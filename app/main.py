90837537d7e3504ece7683c39b50a843cfb45325
Kateryna Zadorozhna <136272476+katryana@users.noreply.github.com>
2023-08-22 20:14:09 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
     current_command, file_in_name, file_out_name = command.split()
     if current_command == "cp" and file_in_name != file_out_name:
-        with open(file_in_name, "r") as file_in, open(
-                file_out_name, "w"
-        ) as file_out:
+        with (
+            open(file_in_name, "r") as file_in,
+            open(file_out_name, "w") as file_out
+        ):
             file_out.write(file_in.read())
