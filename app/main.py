d826a003b9c14f0dce282ee74a2bf037a29e68ba
oreny <orenycdota@gmail.com>
2023-08-23 00:06:20 +0300

    Applied resolved blocks

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 def copy_file(command: str) -> None:
-    list_from_command = command.split(" ")
+    list_from_command = command.split()
     if len(list_from_command) != 3:
         return
-    if list_from_command[0] != "cp":
+    command, original_file, copy_of_file = command.split()
+    if command != "cp":
         return
-    if list_from_command[1] == list_from_command[2]:
+    if original_file == copy_of_file:
         return
-    with open(list_from_command[1], "r") as file_in, open(
-        list_from_command[2], "w"
+    with open(original_file, "r") as file_in, open(
+        copy_of_file, "w"
     ) as file_out:
         file_out.write(file_in.read())
