0c9a0fff95fbb8fc53af93ad630b8a5f29f05e64
Pavlo Harbuz <lgsilay1991@gmail.com>
2023-08-22 13:15:05 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ def copy_file(command: str) -> None:
     if command_list[1] == command_list[2]:
         return
 
-    with open(command_list[1], "r") as file_in,\
-            open(command_list[2], "w") as file_out:
+    with (
+        open(command_list[1], "r") as file_in,
+        open(command_list[2], "w") as file_out
+    ):
         file_out.write(file_in.read())
