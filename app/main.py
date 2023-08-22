104a6fccba22dca66cac7dfc6539335531fff108
Yaroslav Skrypchenko <yarik2300@gmail.com>
2023-08-22 14:48:53 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+    sourse_file = command[1]
+    copied_file = command[2]
+    if sourse_file != copied_file:
+        with open(sourse_file, "r") as sourse:
+            with open(copy_file, "w") as copy:
+                copy.write(sourse.read())
