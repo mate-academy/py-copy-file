c3bb3c5a63f9809923ace95e304de7f2f600dde9
Inna Isareva <inna8110302@gmail.com>
2023-08-22 19:59:18 +0300

    py-copy-file v2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
-    command_list = command.split(" ")
-    if command_list[0] == "cp" and command_list[1] != command_list[2]:
-        with (open(command_list[1], "r") as file_in,
-              open(command_list[2], "w") as file_out):
+    command, original_file, copy_file = command.split()
+    if command == "cp" and original_file != copy_file:
+        with (open(original_file, "r") as file_in,
+              open(copy_file, "w") as file_out):
             content = file_in.read()
             file_out.write(content)
