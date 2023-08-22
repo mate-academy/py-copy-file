2cefd2ebdfc3763ef326f2f6f503a641aedbf569
Max Katkalov <makskatkalov@gmail.com>
2023-08-22 19:55:59 +0300

    with rewritten

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ def copy_file(command: str) -> None:
         if command == "cp" and (
             current_file != new_file and os.path.exists(current_file)
         ):
-            with open(current_file, "r") as file_in, open(
-                new_file, "w"
-            ) as file_out:
+            with (
+                open(current_file, "r") as file_in,
+                open(new_file, "w") as file_out
+            ):
                 file_out.write(file_in.read())
