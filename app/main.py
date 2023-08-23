70dbe1c95d0c5e7d7bf86c8a78570f7d6fd0a552
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-08-23 19:33:04 +0300

    Made unpacking easier to read and replaced loop-writing for one-line writing

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
 def copy_file(command: str) -> None:
-    splitted_command = command.split()
+    instruction, source_file, destination_file = command.split()
 
-    if splitted_command[0] != "cp":
+    if instruction != "cp":
         return
-
-    source_file = splitted_command[1]
-    destination_file = splitted_command[2]
 
     if source_file != destination_file:
         with (
             open(source_file, "r") as file_in,
             open(destination_file, "w") as file_out,
         ):
-            for line in file_in.readlines():
-                file_out.write(line)
+            file_out.write(file_in.read())
