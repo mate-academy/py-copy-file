2f6895f9ada6544b17c7945dfc723e92fcf22668
Ostap <ostapbojko04@gmail.com>
2023-08-23 13:01:57 +0300

    py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,20 +1,11 @@
-import time
-from datetime import datetime
+def copy_file(command: str) -> str:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        return "Invalid command. Please provide a valid 'cp' command."
 
+    if parts[1] == parts[2]:
+        return
 
-def create_log_file() -> str:
-    current_time = datetime.now()
-    filename = f"app-{current_time.hour:02d}_{current_time.minute:02d}" \
-               f"_{current_time.second:02d}.log"
-    timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")
-
-    with open(filename, "w") as file:
-        file.write(timestamp)
-
-    print(f"Created file: {filename}")
-    print(f"Timestamp: {timestamp}\n")
-
-
-while True:
-    create_log_file()
-    time.sleep(1)
+    with open(parts[1], "r") as file1, open(parts[2], "w") as file2:
+        new_file = file1.read()
+        file2.write(new_file)
