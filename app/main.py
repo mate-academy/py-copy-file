f1c18b5595918a8ea052b0954945bf0a1b06a207
Oleh Chernous <chernous.oleh@gmail.com>
2023-08-23 18:32:48 +0200

    unpacked values to descriptive variables

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
-    command = command.split()
+    command, old_file, file_copy = command.split()
 
-    if command[1] == command[2] and command[0] == "cp":
+    if old_file == file_copy and command == "cp":
         return
 
-    with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+    with open(old_file, "r") as file_in, open(file_copy, "w") as file_out:
         content = file_in.read()
         file_out.write(content)
