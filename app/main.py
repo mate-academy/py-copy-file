8f394f704e63847c1e64822533c2f8eba8d76ee8
WoyDiam <freedom.channel2003@gmail.com>
2023-08-23 16:38:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
 def copy_file(command: str) -> None:
     parts = command.split()
-    if len(parts) <= 4 and parts[0] != "cp":
+    if len(parts) != 3 and parts[0] != "cp":
         print("Invalid command format")
         return
 
-    s_file = parts[1]
-    d_file = parts[2]
-
-    if s_file == d_file:
+    if parts[1] == parts[2]:
         print("Source file and destination file names are the same")
         return
 
     try:
-        with open(s_file, "r") as file_in, open(d_file, "w") as file_out:
+        with open(parts[1], "r") as file_in, open(parts[2], "w") as file_out:
             file_out.write(file_in.read())
             print("File copied successfully")
     except FileNotFoundError:
