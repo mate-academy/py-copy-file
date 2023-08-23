79c2b4f9c69287c2bff19d233d8cc86a9668b203
ivan <ivan@isba.local>
2023-08-23 16:46:43 +0300

    Fix

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ class InvalidCommandError(Exception):
 
 def copy_file(command: str) -> None:
     command_list = command.split()
-    if command_list[0] != "cp":
-        raise InvalidCommandError("Гnknown command.")
+    if command_list[0] != "cp" or len(command_list) != 3:
+        raise InvalidCommandError("Unknown command.")
     if command_list[1] != command_list[2]:
         with (
             open(command_list[1], "r") as source_file,
