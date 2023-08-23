e763d13028d6a07e89f47909c6f01370089b82f8
ivan <ivan@isba.local>
2023-08-23 16:19:59 +0300

    Solition

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+class InvalidCommandError(Exception):
+    ...
+
+
+def copy_file(command: str) -> None:
+    command_list = command.split()
+    if command_list[0] != "cp":
+        raise InvalidCommandError("Гnknown command.")
+    if command_list[1] != command_list[2]:
+        with (
+            open(command_list[1], "r") as source_file,
+            open(command_list[2], "w") as new_file
+        ):
+            new_file.write(source_file.read())
