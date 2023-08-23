9079a78ce4060076d871bc1c342e50b3e65dc7c5
Ostap <ostapbojko04@gmail.com>
2023-08-23 16:49:43 +0300

    py-copy-file1

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-def copy_file(command: str) -> str:
+def copy_file(command: str, target_file: str, source_file: str) -> str:
     parts = command.split()
-    if len(parts) != 3 or parts[0] != "cp":
+    if len(parts) != 3 or parts != "cp":
         return "Invalid command. Please provide a valid 'cp' command."
 
-    if parts[1] == parts[2]:
+    if source_file[1] == target_file[2]:
         return
 
-    with open(parts[1], "r") as file1, open(parts[2], "w") as file2:
+    with open(source_file, "r") as file1, open(target_file, "w") as file2:
         new_file = file1.read()
         file2.write(new_file)
