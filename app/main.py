513c1e6117fb39aef2c5e2d929b594171493b917
Vasya Turok <turokvasil0@gmail.com>
2023-08-24 16:22:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
+class InvalidCommandFormat(Exception):
+    pass
+
 
 def copy_file(command: str) -> None:
-    command = command.split(" ")
+    command = command.split()
+
+    if command[0] != "cp":
+        raise InvalidCommandFormat("Please provide a command like "
+                                   "'cp file.txt file-copy.txt'")
+
     if command[1] != command[2]:
         with (open(command[1], "r") as file_in,
               open(command[2], "w") as file_out):
             content = file_in.read()
             file_out.write(content)
             print("OK")
-
-
-copy_file("cp file.txt file_copy.txt")
