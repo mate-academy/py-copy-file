1f7b39eb1d558af62164f631b8a721042546bd92
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-08-25 16:34:10 +0300

    py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
-def copy_file(command: str) -> bool:
+def copy_file(command: str) -> None:
     command_split = command.split()
     if command_split[0] == "cp":
         if command_split[1] == "file.txt":
-            with open("file.txt", "r") as file:
-                content = file.read()
-
-            if command_split[2] == "new_file.txt":
-                with open("new_file.txt", "w") as file:
-                    file.write(content)
-
-    return open("file.txt").read() == open("new_file.txt").read()
+            with open("file.txt", "r") as file_in, open("new_file.txt", "w") as file_out:
+                content = file_in.read()
+                if command_split[2] == "new_file.txt":
+                    file_out.write(content)
