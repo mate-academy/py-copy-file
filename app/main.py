98d32ef0b7cc34e0f3a543dfb71e3d7471458a3b
Dania Minyaylo <dendan202@gmail.com>
2023-08-26 21:38:39 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 import shutil
 import os
 
-def copy_file(source_file, target_file):
-    if source_file == target_file:
-        return  # Do nothing
 
-    # Check if the source file exists
+def copy_file(source_file: str, target_file: str) -> None:
+
+    if source_file == target_file:
+        return
+
     if not os.path.isfile(source_file):
         print(f"Source file '{source_file}' does not exist.")
         return
 
-    # Copy the source file to the target file
     shutil.copyfile(source_file, target_file)
 
-# Test cases
-copy_file("D:/All_For_Programming/Projects/py-copy-file/app/file.txt", "D:/All_For_Programming/Projects/py-copy-file/app/new_file.txt")
+
+copy_file("D:/All_For_Programming/Projects/py-copy-file/app/file.txt",
+          "D:/All_For_Programming/Projects/py-copy-file/app/new_file.txt")
