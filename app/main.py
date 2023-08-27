e4cc80089cd0735ec370eb4a855f5bcec4c4b3da
Vasyl Dmytruk <vasyldmytruk104@gmail.com>
2023-08-27 13:33:16 +0300

    1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        cp_command, previous_name, new_name = command.split()
+    except ValueError:
+        raise Exception("Enter correct command!")
+
+    if previous_name != new_name and cp_command == "cp":
+        with open(previous_name, "r") as file_in, \
+                open(new_name, "w") as file_out:
+            file_content = file_in.readlines()
+            file_out.writelines(file_content)
