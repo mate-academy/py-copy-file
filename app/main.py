8a40c62be173bf11357c92dee9118ab4c311aecc
Dania Minyaylo <dendan202@gmail.com>
2023-08-27 15:40:07 +0300

    Solution_Final

--- app/main.py
+++ app/main.py
@@ -1,18 +1,25 @@
 import shutil
-import os
-
 
 def copy_file(source_file: str, target_file: str) -> None:
+    """
+    Copy a file from the source to the target location.
 
-    if source_file == target_file:
-        return
+    Args:
+        source_file (str): The path to the source file.
+        target_file (str): The path to the target file.
 
-    if not os.path.isfile(source_file):
+    Returns:
+        None
+    """
+    try:
+        shutil.copy(source_file, target_file)
+        print(f"File '{source_file}' copied to '{target_file}' successfully.")
+    except FileNotFoundError:
         print(f"Source file '{source_file}' does not exist.")
-        return
+    except IsADirectoryError:
+        print(f"'{source_file}' is a directory, not a file.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    shutil.copyfile(source_file, target_file)
 
-
-copy_file("D:/All_For_Programming/Projects/py-copy-file/app/file.txt",
-          "D:/All_For_Programming/Projects/py-copy-file/app/new_file.txt")
+copy_file("file.txt", "file-copy.txt")
