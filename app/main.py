f2580c4389ac47ca463d88e6a3732805b78a2934
Vasyl Dmytruk <vasyldmytruk104@gmail.com>
2023-08-28 17:35:13 +0300

    2

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ def copy_file(command: str) -> None:
         raise Exception("Enter correct command!")
 
     if previous_name != new_name and cp_command == "cp":
-        with open(previous_name, "r") as file_in, \
-                open(new_name, "w") as file_out:
+        with (
+            open(previous_name, "r") as file_in,
+            open(new_name, "w") as file_out
+        ):
             file_content = file_in.readlines()
             file_out.writelines(file_content)
