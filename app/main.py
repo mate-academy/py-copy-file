d0c650ca1bca5c120109ea000922d11b17db41fd
oreny <orenycdota@gmail.com>
2023-08-28 17:55:20 +0300

    United 2 conditions into one

--- app/main.py
+++ app/main.py
@@ -3,9 +3,7 @@ def copy_file(command: str) -> None:
         command, original_file, copy_of_file = command.split()
     except ValueError:
         return
-    if command != "cp":
-        return
-    if original_file == copy_of_file:
+    if command != "cp" or original_file == copy_of_file:
         return
     with (
         open(original_file, "r") as file_in,
