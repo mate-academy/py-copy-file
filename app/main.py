cca3858db2499caa2f1eff2f75a5b05339784e8b
Oleh Bohatov <obohatov@gmail.com>
2023-08-28 14:01:09 +0200

    Solution with fmt

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
     command, original_file, new_file = command.split()
     if command == "cp" and original_file != new_file:
-        with open(original_file, "r") as file_in, \
-             open(new_file, "w") as file_out:
+        with (open(original_file, "r") as file_in,
+              open(new_file, "w") as file_out
+              ):
             content = file_in.read()
             file_out.write(content)
