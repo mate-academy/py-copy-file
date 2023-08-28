7d3facd980877cdc183b01eafcc3793c9b96431b
Taras Borshch <tarasborshch4@gmail.com>
2023-08-28 13:57:34 +0300

    formatted nextlines

--- app/main.py
+++ app/main.py
@@ -3,10 +3,8 @@ def copy_file(command: str) -> None:
         key, file_name, copy_name = command.split()
     except IndexError:
         return
-
     if file_name == copy_name or key != "cp":
         return
-
     with open(file_name, "r") as old_file, open(copy_name, "w") as file_copy:
         file_content = old_file.read()
         file_copy.write(file_content)
