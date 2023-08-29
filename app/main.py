cfd2ac6f5e2a4c1831ab87918be39539789fda29
Valeriia Tymchenko <maksimova.tymchenko@gmail.com>
2023-08-29 16:46:43 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    _, file, new_file = command.split()
+
+    if file != new_file:
+        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+            content = file_in.read()
+            file_out.write(content)
