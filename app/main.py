5273c5b69f6c928ed0273f17cd847109c70c9133
Valeriia Tymchenko <maksimova.tymchenko@gmail.com>
2023-08-29 18:13:23 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 def copy_file(command: str) -> None:
-    _, file, new_file = command.split()
+    in_command = command.split()
+
+    if len(in_command) != 3:
+        raise SyntaxError("Wrong format of command")
+
+    _, file, new_file = in_command
 
     if file != new_file:
         with open(file, "r") as file_in, open(new_file, "w") as file_out:
