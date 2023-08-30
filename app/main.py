e8b61148a863457d709569a632b30edc3684dc67
Alla Kuksa <alla.kuksa92@gmail.com>
2023-08-30 12:36:59 +0300

    Solutiin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    cd, file_name, new_file = command.split(" ")
+
+    if file_name == new_file:
+        return
+    else:
+        with open(file_name, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
