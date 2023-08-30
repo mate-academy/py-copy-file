7cf606ca066a0f30e08289b7710f1496d5765918
Alla Kuksa <alla.kuksa92@gmail.com>
2023-08-30 13:14:12 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 def copy_file(command: str) -> None:
 
-    cd, file_name, new_file = command.split(" ")
+    if len(command) == 3:
+        cd, file_name, new_file = command.split()
 
-    if file_name == new_file:
-        return
-    else:
+        if file_name == new_file:
+            return
+
         with open(file_name, "r") as file_in, open(new_file, "w") as file_out:
             file_out.write(file_in.read())
