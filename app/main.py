aa60c5e0e209ab5589bdfff7680c91c18a0244ad
Roman Sachavskyi <roman.sachavskyi.ki.2019@lpnu.ua>
2023-09-03 11:56:58 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(filename: str) -> None:
+    file_for_copy = filename.split(" ")[1]
+    new_file = filename.split(" ")[2]
+    with open(file_for_copy, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
