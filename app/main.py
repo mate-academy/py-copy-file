cab6e65ec66e8ad4f5d379c279a15b62ea1d6fe4
Даня Пономаренко <you@example.com>
2023-09-05 13:51:21 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,6 @@
 def copy_file(command: str) -> None:
-    list_command = list(command)
-    word = []
-    for index, bykva in enumerate(list_command):
-        if index >= 3:
-            if bykva == " ":
-                old_name = "".join(word)
-                word = []
-            else:
-                word.append(bykva)
-    new_name = "".join(word)
-    if new_name != old_name:
-        with open(new_name, "w") as new_file, open(old_name, "r") as old_file:
+    commands = command.split(" ")
+    if commands[2] != commands[1]:
+        with (open(commands[2], "w") as new_file,
+              open(commands[1], "r") as old_file):
             new_file.write(old_file.read())
