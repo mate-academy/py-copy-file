7bb77c18a08b0d44db1be10d489986658ef3441e
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-09-07 20:19:09 +0100

    copy file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
     files_list = command.split(" ")
-    if files_list[1] != files_list[2]:
-        with (open(files_list[1], "r") as file_in,
-              open(files_list[2], "w") as file_out):
-            file_out.write(file_in.read())
+    if len(files_list) == 3:
+        if files_list[1] != files_list[2]:
+            with (open(files_list[1], "r") as file_in,
+                  open(files_list[2], "w") as file_out):
+                file_out.write(file_in.read())
