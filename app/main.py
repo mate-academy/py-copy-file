f2761aa27381fa89e7fa691e2890312513a1e5bd
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-08 20:08:52 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,28 @@
 import os
 
 
-def copy_file(command):
+def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) != 3:
-        print("Invalid command. Usage: cp source_file destination_file")
+        print("Invalid command. "
+              "Usage: cp source_file destination_file")
         return
 
     _, source_file, destination_file = parts
 
     if source_file == destination_file:
-        print("Source and destination files have the same name. Doing nothing.")
+        print("Source and destination files "
+              "have the same name. Doing nothing.")
         return
 
     if not os.path.exists(source_file):
         print(f"Source file '{source_file}' does not exist.")
         return
 
-    with open(source_file, "rb") as file_in, open(destination_file, "wb") as file_out:
+    with open(source_file, "rb") as file_in, \
+            open(destination_file, "wb") as file_out:
         file_out.write(file_in.read())
 
-    print(f"File {source_file}' copied to '{destination_file}' successfully.")
+    print(f"File {source_file}' copied to "
+          f"'{destination_file}' successfully.")
