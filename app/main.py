a3927c89c3e76e147734f7d4246eb32fba4e52b8
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-09-08 15:29:03 +0300

    task completed (2)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
+class CustomError(Exception):
+    pass
+
+
+class NameCommandError(CustomError):
+    """Command name is not cp"""
+
+
+class CommandError(CustomError):
+    """The error of missing three elements in the team"""
+
+
 def copy_file(command: str) -> None:
+    if command[:2] != "cp":
+        raise NameCommandError
     command = command.split(" ")
 
-    if command[1] == command[2]:
-        pass
+    if len(command) != 3:
+        if command[1] == command[2]:
+            pass
 
     with open(command[1], "r") as data_file:
         data = data_file.read()
