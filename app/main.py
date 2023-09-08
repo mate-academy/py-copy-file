958bb0682661da18c19126d718b24d0d2b6a9352
Vitalii Zhabskii <vitalik15031996@gmail.com>
2023-09-08 20:06:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+
+
+def copy_file(command):
+    parts = command.split()
+
+    if len(parts) != 3:
+        print("Invalid command. Usage: cp source_file destination_file")
+        return
+
+    _, source_file, destination_file = parts
+
+    if source_file == destination_file:
+        print("Source and destination files have the same name. Doing nothing.")
+        return
+
+    if not os.path.exists(source_file):
+        print(f"Source file '{source_file}' does not exist.")
+        return
+
+    with open(source_file, "rb") as file_in, open(destination_file, "wb") as file_out:
+        file_out.write(file_in.read())
+
+    print(f"File {source_file}' copied to '{destination_file}' successfully.")
