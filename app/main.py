9182847a9bfc96f937b3f38fb3208659b24af591
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-09-08 18:50:07 +0300

    task completed (4)

--- app/main.py
+++ app/main.py
@@ -21,7 +21,6 @@ def copy_file(command: str) -> None:
     if command[1] == command[2]:
         pass
 
-    with open(command[1], "r") as data_file:
+    with open(command[1], "r") as data_file, open(command[2], "w") as new_file:
         data = data_file.read()
-        with open(command[2], "w") as new_file:
-            new_file.write(data)
+        new_file.write(data)
