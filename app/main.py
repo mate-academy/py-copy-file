bf044a5934e0b44f73e00a2462d90bbb85ccab80
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-09-08 18:16:27 +0300

    task completed (3)

--- app/main.py
+++ app/main.py
@@ -16,8 +16,10 @@ def copy_file(command: str) -> None:
     command = command.split(" ")
 
     if len(command) != 3:
-        if command[1] == command[2]:
-            pass
+        raise CommandError
+
+    if command[1] == command[2]:
+        pass
 
     with open(command[1], "r") as data_file:
         data = data_file.read()
