9031a3e96f73cec4d570c669d0f9145017830965
Artem <hartem472@gmail.com>
2023-09-09 16:49:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
-    split_command = command.split()
-    with open(split_command[1], "r") as file_in,\
-         open(split_command[2], "w") as file_out:
-        for line in file_in:
-            file_out.write(line)
+    if command.startswith("cp"):
+        split_command = command.split()
+        with open(split_command[1], "r") as file_in,\
+             open(split_command[2], "w") as file_out:
+            for line in file_in:
+                file_out.write(line)
