d82879c86fbfee2214b076296b295caa40c4dabe
Dmytro Beschastnyi <d.beschastnyi@icloud.com>
2023-09-10 19:23:54 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    com, orig_file, copyed_file = command.split()
+
+    if com == "cp" and orig_file != copyed_file:
+        with open(orig_file, "r") as file_in,\
+                open(copyed_file, "w") as file_out:
+            for line in file_in:
+                file_out.write(line)
