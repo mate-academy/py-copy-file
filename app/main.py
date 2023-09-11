46c18da7edb45b698f889b3027f07a63d80287c7
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-11 18:10:09 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,11 +5,12 @@ def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) != 3:
-        print("Invalid command. "
-              "Usage: cp source_file destination_file")
-        return
-
+    raise Exception("Invalid command. Usage: cp source_file destination_file")
+else:
     _, source_file, destination_file = parts
+    # Now you have the source_file and destination_file variables
+    print("Source File:", source_file)
+    print("Destination File:", destination_file)
 
     if source_file == destination_file:
         print("Source and destination files "
@@ -20,9 +21,10 @@ def copy_file(command: str) -> None:
         print(f"Source file '{source_file}' does not exist.")
         return
 
-    with open(source_file, "rb") as file_in, \
-            open(destination_file, "wb") as file_out:
-        file_out.write(file_in.read())
+    with open(source_file, "rb") as file_in, (
+        open(destination_file, "wb") as file_out):
+    file_out.write(file_in.read())
+
 
     print(f"File {source_file}' copied to "
           f"'{destination_file}' successfully.")
