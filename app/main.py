0f6a496413f6db22bfd5424aa8b0b49cd0c92dbf
Maksim Stahovskij <maksss196@gmail.com>
2023-09-11 12:35:20 +0300

    py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> str:
+    old_file, new_file = command.split()[1:]
+    if old_file == new_file:
+        return f"The file '{old_file}' already exists with the same name"
+
+    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
+
+    return f"cp {old_file} new_{new_file}"
