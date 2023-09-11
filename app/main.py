1a70711705e6f57d4ba71278bc74f3cbf68cf9fc
Dmytro Beschastnyi <d.beschastnyi@icloud.com>
2023-09-11 20:12:45 +0200

    solution grand finale

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ def copy_file(command: str) -> None:
 
     if len(command) == 3:
         com, orig_file, copyed_file = command.split()
+
         if com == "cp" and orig_file != copyed_file:
             with (
                 open(orig_file, "r") as file_in,
                 open(copyed_file, "w") as file_out
             ):
-                for line in file_in:
-                    file_out.write(line)
+                file_out.write(file_in.read())
