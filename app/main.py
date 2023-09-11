bcb0f97001239daf3225ee21333ba456ce26cb6b
Dmytro Beschastnyi <d.beschastnyi@icloud.com>
2023-09-11 20:05:17 +0200

    all fixed, probably2

--- app/main.py
+++ app/main.py
@@ -1,19 +1,11 @@
 def copy_file(command: str) -> None:
-    com = ""
-    orig_file = ""
-    copyed_file = ""
 
-    kyes = command.split()
-
-    if len(kyes) == 3:
-        com = kyes[0]
-        orig_file = kyes[1]
-        copyed_file = kyes[2]
-
-    if com == "cp" and orig_file != copyed_file:
-        with (
-            open(orig_file, "r") as file_in,
-            open(copyed_file, "w") as file_out
-        ):
-            for line in file_in:
-                file_out.write(line)
+    if len(command) == 3:
+        com, orig_file, copyed_file = command.split()
+        if com == "cp" and orig_file != copyed_file:
+            with (
+                open(orig_file, "r") as file_in,
+                open(copyed_file, "w") as file_out
+            ):
+                for line in file_in:
+                    file_out.write(line)
