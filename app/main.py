b5b8dbea4a7a0cf0f3faea9c84188a2277287f06
Misha Shushman <endernagashushman@gmail.com>
2023-09-14 19:46:01 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(comand: str) -> None:
+    file1, file2 = comand.split()[1], comand.split()[2]
+    if not file1 == file2:
+        with open(file1, "r") as f1, open(file2, "w") as f2:
+            f2.write(f1.read())
