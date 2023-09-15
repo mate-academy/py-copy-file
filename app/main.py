27ab1724fe4a6cac0957087210caad3175729183
Nadiia <nadiia.developer777@gmail.com>
2023-09-15 19:24:23 +0300

    Solution-py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split(" ")
+    print(parts)
+    source_path = parts[1]
+    destination_path = parts[2]
+
+    if source_path == destination_path:
+        return
+
+    with open(source_path, "r") as file_in, open(destination_path, "w") as file_out:
+        for line in file_in:
+            file_out.write(line)
+
+
+if __name__ == "__main__":
+    copy_file("cp file.txt new_file.txt")
