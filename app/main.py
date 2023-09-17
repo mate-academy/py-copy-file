3e5c0cb336fdd39e704a281ee19066baf779e2b5
Andriy_Shumskyy <andshumskyy@gmail.com>
2023-09-17 11:32:39 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str):
+    data = command.split(" ")
+    if data[1] == data[2]:
+        return
+    else:
+        with open(data[1], "r") as file_in, open(data[2], "w") as file_out:
+            file_data = file_in.readlines()
+            for row in file_data:
+                file_out.write(row)
+
+
+print('try copy the same')
+copy_file("cp file.txt file.txt")
+print('copy different')
+copy_file("cp file.txt new_file.txt")
+check = open("file.txt").read() == open("new_file.txt").read()  # True
+print(f'check {check}')
