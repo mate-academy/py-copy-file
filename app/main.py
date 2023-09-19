752b8709160f14a818b61f45a7420a56a8665af5
Maksym Pavliuchenko <maksym.pavliuchenko@scalors.com>
2023-09-19 19:52:53 +0300

    fix indentation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 def copy_file(command: str) -> None:
     command_split = command.split(" ")
-    if (len(command_split) == 3
+    if (
+            len(command_split) == 3
             and command_split[0] == "cp"
-            and command_split[1] != command_split[2]):
+            and command_split[1] != command_split[2]
+    ):
         with open(command_split[2], "w") as file_in, \
                 open(command_split[1], "r") as file_out:
             file_in.writelines(file_out.readlines())
