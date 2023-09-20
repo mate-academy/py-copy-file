49a5780d44c597cd5f778969954611048cd6d440
Anton <anton380969658858@gmail.com>
2023-09-20 13:48:01 +0300

    added cp validation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     files = command.split(" ")
-    if files[1] == files[2]:
+    if files[1] == files[2] or files[0] != "cp":
         return
     with open(files[1], "r") as file_in, open(files[2], "w") as file_out:
         file_out.write(file_in.read())
