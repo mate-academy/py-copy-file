a1f8d1d45a9779b94e14f62e7307b6e8a9ce742f
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-09-21 13:43:41 +0300

    fix for 3 elem.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 def copy_file(command: str) -> None:
+    if len(command.split(" ")) != 3:
+        return None
     cp, source, new = command.split(" ")
     if cp != "cp" or source == new:
         return None
