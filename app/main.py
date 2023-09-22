94d9b60ce081e4a3562a58a8e4b4cac4bb06fab0
Hlova Oleh <hlovaoleh0@gmail.com>
2023-09-22 19:31:36 +0300

    implemented py-copy-file task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    content = command.split()
+    if content[1] != content[2]:
+        with open(content[1], "rb") as src_file, \
+                open(content[2], "wb") as dest_file:
+            shutil.copyfileobj(src_file, dest_file)
