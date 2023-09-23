5e08f749a00eeddf15b910009ea2df7fa5cc3866
Hlova Oleh <hlovaoleh0@gmail.com>
2023-09-23 17:52:59 +0300

    suggested changes have been implemented

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-import shutil
-
-
 def copy_file(command: str) -> None:
     content = command.split()
-    if content[1] != content[2]:
-        with open(content[1], "rb") as src_file, \
-                open(content[2], "wb") as dest_file:
-            shutil.copyfileobj(src_file, dest_file)
+    if content[0] == "cp" and content[1] != content[2]:
+        with open(content[1], "rb") as src_file, (
+                open(content[2], "wb")
+        ) as dest_file:
+            while True:
+                data = src_file.read()
+                if not data:
+                    break
+                dest_file.write(data)
