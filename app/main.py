d2c9da0dddd18746ee57e45450d74b412b17471b
Dmytro Tatarinov <nsd7night@gmail.com>
2023-09-25 21:47:51 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class CommandError(Exception):
 def check_command(func: callable) -> callable:
     def wrapper(command: str) -> callable:
         if command.split()[0] != "cp" or len(command.split()) < 3:
-            raise CommandError("Please enter valid command")
+            raise CommandError("Please enter valid command!")
         return func(command)
 
     return wrapper
