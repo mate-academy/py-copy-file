452946ca1b9ff20687a801046233970424ddeb3c
valikdeb <73042683+valikdeb@users.noreply.github.com>
2023-09-26 23:41:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    name1 = command.split()[1]
+    name2 = command.split()[2]
+    with open(name1) as f1, open(name2, "w") as f2:
+        f2.write(f1.read())
