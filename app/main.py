cf8c03e2f6fe2442092a9dfa2fb538b833577f3a
Roman Sachavskyi <roman.sachavskyi.ki.2019@lpnu.ua>
2023-09-26 12:50:52 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,6 @@ def copy_file(filename: str) -> None:
     file_for_copy = filename.split(" ")[1]
     new_file = filename.split(" ")[2]
     if file_for_copy != new_file:
-        with open(file_for_copy, "r") as file_in, open(new_file, "w") as file_out:
+        with open(file_for_copy, "r") as file_in, \
+             open(new_file, "w") as file_out:
             file_out.write(file_in.read())
