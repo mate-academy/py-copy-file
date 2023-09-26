965c1cb0f2002469ea446d8f3cfac2971d9419e1
Philip <filllick13@gmail.com>
2023-09-26 22:21:03 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+from shutil import copyfile
+
+
+def copy_file(command: str) -> None:
+    original_copy_names = command[2:].split()
+    if original_copy_names[0] != original_copy_names[1]:
+        copyfile(original_copy_names[0], original_copy_names[1])
