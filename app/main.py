c151600b6ea93916cba61a1f99ddc98ef89100f2
valikdeb <73042683+valikdeb@users.noreply.github.com>
2023-09-27 13:35:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 def copy_file(command: str) -> None:
-    source_file_name, new_file_name = command.split()[1:3]
+    source_file_name, new_file_name = command.split()[1:]
     with open(source_file_name) as file, open(new_file_name, "w") as new_file:
         new_file.write(file.read())
