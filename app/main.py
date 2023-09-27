12e2c7df51965d77e003c402cb1fd52dc669ed39
valikdeb <73042683+valikdeb@users.noreply.github.com>
2023-09-27 13:43:41 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,19 @@
+class NameCommandError(Exception):
+    """Command name is not cp"""
+
+
+class CommandError(Exception):
+    """The error of missing three elements in the team"""
+
+
 def copy_file(command: str) -> None:
-    source_file_name, new_file_name = command.split()[1:]
+    if len(command.split()) != 3:
+        raise CommandError
+
+    name_command, source_file_name, new_file_name = command.split()
+
+    if name_command != "cp":
+        raise NameCommandError
+
     with open(source_file_name) as file, open(new_file_name, "w") as new_file:
         new_file.write(file.read())
