f9bf80f22a511ac4ca249c4f00ba01069bc2d1b3
Vadym Tiron <tironvadim1@gmail.com>
2023-09-27 19:31:30 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+from shutil import copy
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3:
+        return
+
+    cp, source_file, destination_file = parts
+    if source_file == destination_file:
+        return
+
+    with open(source_file, "r") as source, open(destination_file, "w") as destination:
+        copyfileobj(source, destination)
