0b4fdbbf06216951948bc9e01ba3e548b50cbde3
Vadym Tiron <tironvadim1@gmail.com>
2023-09-27 21:57:07 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ from shutil import copyfileobj
 
 def copy_file(command: str) -> None:
     parts = command.split()
-    if len(parts) == 3 and parts[1] != parts[2]:
-        with (open(parts[1], "r") as source,
-              open(parts[2], "w") as destination):
+    if len(parts) == 3 and parts[0] == "cp" and parts[1] != parts[2]:
+        source_filename, destination_filename = parts[1], parts[2]
+
+        with (open(source_filename, "r") as source,
+              open(destination_filename, "w") as destination):
             copyfileobj(source, destination)
