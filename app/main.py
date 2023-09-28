9384832ffe21e8ca29c30bbedb3a720e9e5eb902
Philip Plokhikh <filllick13@gmail.com>
2023-09-28 15:41:45 +0300

    Changed_\_to_()

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ def copy_file(command: str) -> None:
     if command_data[0] != "cp" or len(command_data) != 3:
         return
 
-    with open(command_data[1], "r") as file_in, \
-            open(command_data[2], "w") as file_out:
+    with (open(command_data[1], "r") as file_in,
+          open(command_data[2], "w") as file_out):
         file_out.writelines(file_in.readlines())
