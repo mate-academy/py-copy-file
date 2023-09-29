3894dddad89354c882298b2675a21bdbdb463ec2
ypyskun <eugene2piskun@gmail.com>
2023-09-29 19:32:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        cmd, source_file, second_file = command.split(maxsplit=3)
+    except ValueError:
+        raise
+    if command == "cp" and source_file != second_file:
+        with(
+            open(source_file, "r") as source,
+            open(second_file, "w") as second
+        ):
+            second.write(source.read())
