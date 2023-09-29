794fddb48dc91bbb07a47e53a34d62c6c402000f
Alex <moleksii@gmail.com>
2023-09-29 13:02:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    separate = command.split()
+    old_file = separate[1]
+    new_file = separate[2]
+    if old_file == new_file:
+        return
+    with open(old_file, "r") as f, open(new_file, "w") as new_f:
+        data = f.readlines()
+        for row in data:
+            new_f.write(row)
