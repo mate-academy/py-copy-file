df15b95bf3b4e370636d8b2a9601ed94adf9135f
Alex <moleksii@gmail.com>
2023-10-02 12:26:26 +0300

    Fixed Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 def copy_file(command: str) -> None:
     separate = command.split()
-    old_file = separate[1]
-    new_file = separate[2]
+    if len(separate) == 3:
+        if separate[0] == "cp":
+            old_file = separate[1]
+            new_file = separate[2]
     if old_file == new_file:
         return
-    with open(old_file, "r") as f, open(new_file, "w") as new_f:
-        data = f.readlines()
+    with open(old_file, "r") as old_f, open(new_file, "w") as new_f:
+        data = old_f.readlines()
         for row in data:
             new_f.write(row)
