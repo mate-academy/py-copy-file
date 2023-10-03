f9c5859a6e532d4c4dcc6777ce6f82a2e1d4658e
asdadaversa <peikqudakeus@gmail.com>
2023-10-03 15:53:51 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,6 +4,5 @@ def copy_file(command: str) -> None:
     if len(command) == 3 and command[0] == "cp":
         if command[1] == command[2]:
             pass
-        if command[0] == "cp" and len(command) == 3:
-            with open(command[1], "r") as f_in, open(command[2], "w") as f_out:
-                f_out.write(f_in.read())
+        with open(command[1], "r") as f_in, open(command[2], "w") as f_out:
+            f_out.write(f_in.read())
