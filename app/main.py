c5a3d0e812d29a6e266f1ff3d629bfe875baa5bb
Serhii Berdianskyi <zeferot94@gmail.com>
2023-10-04 11:26:40 +0300

    Implemented copy_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> str:
+    command_list = command.split()
+    if command_list[1] != command_list[2]:
+        with open(command_list[1], "r") as file_in, open(command_list[2], "w") as file_out:
+            for line in file_in:
+                file_out.write(line)
