f1e5f60fc94ac3c1ec1b4dd72bf02209372fb201
Serhii Berdianskyi <zeferot94@gmail.com>
2023-10-04 11:28:24 +0300

    fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> str:
     command_list = command.split()
     if command_list[1] != command_list[2]:
-        with open(command_list[1], "r") as file_in, open(command_list[2], "w") as file_out:
+        with (open(command_list[1], "r") as file_in,
+              open(command_list[2], "w") as file_out):
             for line in file_in:
                 file_out.write(line)
