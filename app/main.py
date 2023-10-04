0fb2a3104e41f0703f7310d16af504af3299b73f
Vladyslav Sabo <lebwa1337@gmail.com>
2023-10-04 22:11:11 +0300

    Solution4

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
-    comm, file_1, file_2 = command.split()
-    check_for_valid_command = comm == "cp" and len(command.split()) == 3
-    if file_1 != file_2 and check_for_valid_command:
-        with (open(file_1, "r") as file_in,
-              open(file_2, "w") as file_out):
-            file_out.write(file_in.read())
+    if len(command.split()) == 3:
+        command_, source_file, destination_file = command.split()
+        if source_file != destination_file and command_ == "cp":
+            with (open(source_file, "r") as file_in,
+                  open(destination_file, "w") as file_out):
+                file_out.write(file_in.read())
