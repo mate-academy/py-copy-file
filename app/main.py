c833ddecbde591f534ad29f734b4367d9256a002
Serhii Berdianskyi <zeferot94@gmail.com>
2023-10-04 11:31:00 +0300

    removed return annotation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def copy_file(command: str) -> str:
+def copy_file(command: str) -> None:
     command_list = command.split()
     if command_list[1] != command_list[2]:
         with (open(command_list[1], "r") as file_in,
