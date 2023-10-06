1f7ab8882fa320b7c6f0d82ae2471cea763048e4
Depaton <you@example.com>
2023-10-06 15:26:56 +0300

    Changes again

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
-    if len(command.split()) == 3 and command.split()[0] == "cp":
-        action, file, new_file = command.split()
-        if file != new_file:
+    command = command.split()
+    if len(command) == 3:
+        action, file, new_file = command
+        if action == "cp" and file != new_file:
             with (open(f"{file}", "r") as file_in,
                   open(f"{new_file}", "w") as file_out):
                 file_out.write(file_in.read())
