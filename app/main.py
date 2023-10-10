0e1012e07ef550103da55ebd2007a488f32bd606
SerhiiBudko <140949363+SerhiiBudko@users.noreply.github.com>
2023-10-10 17:38:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,23 +1,19 @@
-def copy_file(command: str) -> None:
-    # command = cp file.txt file-cope.txt
-
-    file_name = command.split()
-    if len(file_name) != 3:
-        return "Command has exactly 3 parts!"
-
-    _, main_file, destination_file = file_name
-
-    if main_file == destination_file:
-        return "Files are the same!"
-
+def copy_file(command: str) -> str:
     try:
-        with (
-            open(main_file, "r") as file_in,
-            open(destination_file, "w") as file_out
-        ):
+        parts = command.split()
+
+        if len(parts) != 3:
+            raise ValueError("Invalid command format")
+
+        _, source_file, destination_file = parts
+
+        if source_file == destination_file:
+            print("Files are the same!")
+            return
+
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
             file_out.write(file_in.read())
-        return "Files were copied successfully!"
-    except FileNotFoundError as e:
-        print(f"Error: {e}")
+            print(f"File {source_file} copied to {destination_file}")
     except Exception as e:
-        print(f"Error: {e}")
+        raise e
