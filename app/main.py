42e27497cadef1ae3402fca0b626fdb4adbdb54d
Vyacheslav Vasylenko <vvg04021968@gmail.com>
2023-10-11 11:01:08 +0300

    'Solution_copy-file'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cmd = command.split()[0]
+    source = command.split()[1]
+    destination = command.split()[2]
+    if source != destination and cmd == "cp":
+        with open(source, "r") as file_in, open(destination, "w") as file_out:
+            file_out.write(file_in.read())
