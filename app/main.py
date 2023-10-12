4cf53895a3f8157079ef2c8d728a2307c25286ea
Dmytro Prokhach <dprokhach@gmail.com>
2023-10-12 18:21:16 +0300

    replaced '\' to '()'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     time_result = command.split(" ")
     if time_result[0] == "cp" and len(time_result) == 3:
-        with open(time_result[1], "r") as current_file,\
-                open(time_result[2], "w+") as new:
+        with (open(time_result[1], "r") as current_file,
+              open(time_result[2], "w+") as new):
             content = current_file.read()
             new.write(content)
