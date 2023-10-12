2417b042e9e756d25363e114b239a0ffb9e680d4
Dmytro Prokhach <dprokhach@gmail.com>
2023-10-12 18:08:18 +0300

    added 1 condition

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
     time_result = command.split(" ")
-    with open(time_result[1], "r") as current_file,\
-            open(time_result[2], "w+") as new:
-        content = current_file.read()
-        new.write(content)
+    if time_result[0] is "cp" and len(time_result) == 3:
+        with open(time_result[1], "r") as current_file,\
+                open(time_result[2], "w+") as new:
+            content = current_file.read()
+            new.write(content)
