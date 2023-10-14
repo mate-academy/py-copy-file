1efc84e1a8d2418a2edb92cbd563a3ba41f480e7
TROT <seotrotpol@gmail.com>
2023-10-14 16:34:00 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    vul = command.split()
+    if vul[1] == vul[2] and vul[0] == "cp":
+        return
+    with open(vul[1], "r") as file_in, open(vul[2], "w") as file_out:
+        file_out.write(file_in.read())
