9c532e1fee2f166869a87ef04ccc9f79afd4c7bc
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-10-15 22:39:38 +0300

    Added a comment length check

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 def copy_file(command: str) -> None:
-    cp, input_file, output_file = command.split(" ")
-    if cp != cp or input_file == output_file:
-        return
-    with open(input_file, "r") as file_in, open(output_file, "w") as file_out:
-        file_out.write(file_in.read())
+    command = command.split(" ")
+    if len(command) == 3:
+        function, input_file, output_file = command
+        if function != function or input_file == output_file:
+            return
+        with open(input_file, "r") as file_in, open(output_file, "w") as file_out:
+            file_out.write(file_in.read())
+    return
