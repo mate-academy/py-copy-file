a8d530dc789e655712cae169b38576277dcf688a
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-16 12:56:02 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    path_origin = command[1]
+    path_copy = command[2]
+
+    if path_origin != path_copy:
+        with open(path_origin, "r") as file_in, open(path_copy, "w") as file_out:
+            file_out.write(file_in.read())
