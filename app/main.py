720295d676308ede824559c0c6b3d1923e5d304d
Andrey Britov <abritov82@gmail.com>
2023-10-16 16:18:17 +0300

    Solusion_2

--- app/main.py
+++ app/main.py
@@ -8,8 +8,3 @@ def copy_file(command: str) -> Any:
         return
     with open(res_list[1], "r") as file_in, open(res_list[2], "w") as file_out:
         file_out.write(file_in.read())
-
-
-copy_file("cp file.txt file.txt")
-# copy_file("cp file.txt new_file.txt")
-# print(open("file.txt").read() == open("new_file.txt").read())
