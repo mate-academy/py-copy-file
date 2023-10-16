86c62b420ee44d3715c33c3d51fee040d0c6e488
Andrey Britov <abritov82@gmail.com>
2023-10-16 16:13:24 +0300

    Solusion_1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+from typing import Any
+
+
+def copy_file(command: str) -> Any:
+
+    res_list = command.split(" ")
+    if res_list[1] == res_list[2]:
+        return
+    with open(res_list[1], "r") as file_in, open(res_list[2], "w") as file_out:
+        file_out.write(file_in.read())
+
+
+copy_file("cp file.txt file.txt")
+# copy_file("cp file.txt new_file.txt")
+# print(open("file.txt").read() == open("new_file.txt").read())
