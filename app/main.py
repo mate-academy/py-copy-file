b45031b2765356fbbde62b5796f9ef379b319755
naiv <nazarivankiv1@gmail.com>
2023-10-16 11:57:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    commands = command.split()
+
+    if commands[0] != commands[1]:
+        with (open(commands[0], "r") as file_in,
+              open(commands[1], "w") as file_out):
+            file_out.writelines(file_in.readlines())
