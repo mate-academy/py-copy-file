8aec74847ec463dd0dac3a540c34ff62fd6cf0e9
Andrey Britov <abritov82@gmail.com>
2023-10-16 16:48:32 +0300

    Solusion_4

--- app/main.py
+++ app/main.py
@@ -7,5 +7,6 @@ def copy_file(command: str) -> Any:
     if res_list[1] == res_list[2]:
         return
     if len(res_list) == 3 and res_list[0] == "cp":
-        with open(res_list[1], "r") as file_in, open(res_list[2], "w") as file_out:
+        with (open(res_list[1], "r") as file_in,
+              open(res_list[2], "w") as file_out):
             file_out.write(file_in.read())
