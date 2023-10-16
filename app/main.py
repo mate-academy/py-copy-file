0ee04fe482543741665acbb77575b67013c2ae37
Lytillis <chemurchem@gmail.com>
2023-10-16 20:57:22 +0300

    Copy File solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    if command[1] == command[2]:
+        return
+    with open(command[1], "r") as file_in, open(command[2], "a") as file_out:
+        for line in file_in:
+            file_out.write(line)
+
+
+if __name__ == "__main__":
+    copy_file("cp text.txt text.txt")
