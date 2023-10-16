235c5207c419c5f65e24509d82d2e86ff58d38ed
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-10-16 13:45:55 +0300

    Fix error

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str) -> None:
     command = command.split(" ")
     if len(command) == 3:
         function, input_file, output_file = command
-        if function != function or input_file == output_file:
+        if function != "cp" or input_file == output_file:
             return
         with (open(input_file, "r") as file_in,
               open(output_file, "w") as file_out):
