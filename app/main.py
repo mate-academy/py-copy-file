4b53102506d549d1350e67717274c538b2092cf8
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-16 13:05:48 +0300

    delete return

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
-def copy_file(command: str) -> str:
+def copy_file(command: str) -> None:
     command, path_origin, path_copy = command.split()
 
     if path_origin != path_copy and command == "cp":
         with (open(path_origin, "r") as file_in,
               open(path_copy, "w") as file_out):
             file_out.write(file_in.read())
-
-    else:
-        return "Does nothing"
