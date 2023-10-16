4133d301ca7550df1563f6d5156fba3139e4bfb7
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-10-16 14:06:31 +0300

    change var name

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
-    command = command.split(" ")
-    if len(command) == 3:
-        function, input_file, output_file = command
-        if function != "cp" or input_file == output_file:
+    parts = command.split(" ")
+    if len(parts) == 3:
+        command, input_file, output_file = parts
+        if command != "cp" or input_file == output_file:
             return
         with (open(input_file, "r") as file_in,
               open(output_file, "w") as file_out):
