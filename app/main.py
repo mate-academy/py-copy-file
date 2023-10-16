05bb4fffb18f131958c8ad6be7352acff7da4e89
Kostia <kostia3507@gmail.com>
2023-10-16 16:32:13 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+    path_in = command[1]
+    path_out = command[2]
+    if path_in != path_out:
+        with open(path_in, "r") as file_in, open(path_out, "w") as file_out:
+            file_in.write(file_out.read())
