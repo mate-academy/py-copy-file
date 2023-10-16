ca7937e325004b3e41156302183102d8cc6cb91c
mykolamateichuk <mateichukmykola@gmail.com>
2023-10-16 18:13:19 +0300

    Solution_v3

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
-    cp, file_name_to_copy, new_file_name = command.split()
+    command_alias, file_name_to_copy, new_file_name = command.split()
 
-    if cp == "cp" and file_name_to_copy != new_file_name:
+    if command_alias == "cp" and file_name_to_copy != new_file_name:
         with (open(file_name_to_copy) as file_to_copy,
              open(new_file_name, "w") as new_file):
-            new_file.writelines(file_to_copy.readlines())
+            new_file.write(file_to_copy.read())
