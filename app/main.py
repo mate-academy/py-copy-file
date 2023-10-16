8032d4ac610554c068a2553f39b0d8ca7dbfe692
Andrey Britov <abritov82@gmail.com>
2023-10-16 16:39:04 +0300

    Solusion_3

--- app/main.py
+++ app/main.py
@@ -3,8 +3,9 @@ from typing import Any
 
 def copy_file(command: str) -> Any:
 
-    res_list = command.split(" ")
+    res_list = command.split()
     if res_list[1] == res_list[2]:
         return
-    with open(res_list[1], "r") as file_in, open(res_list[2], "w") as file_out:
-        file_out.write(file_in.read())
+    if len(res_list) == 3 and res_list[0] == "cp":
+        with open(res_list[1], "r") as file_in, open(res_list[2], "w") as file_out:
+            file_out.write(file_in.read())
