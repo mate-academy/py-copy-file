def06f22f0be4dc14ca2af8ec11657ca89cf2f30
Rost <rv4@ukr.net>
2023-10-16 19:41:21 +0300

    py-copy-file Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_tuple = tuple(command.split(" "))
+    if ((len(command_tuple) != 3
+        or command_tuple[1] == command_tuple[2])
+            or command_tuple[0] != "cp"):
+        return
+    with (open(command_tuple[1], "r") as file_in,
+          open(command_tuple[2], "w") as file_out):
+        file_out.write(file_in.read())
