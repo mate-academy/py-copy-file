e0d7b5e65c7d516ff5094b7462e4a7110b4eace0
mykolamateichuk <mateichukmykola@gmail.com>
2023-10-16 12:47:12 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_name_to_copy = command.split(" ")[1]
+    new_file_name = command.split(" ")[2]
+
+    if file_name_to_copy == new_file_name:
+        return
+
+    with open(file_name_to_copy) as file_to_copy,\
+         open(new_file_name, "w") as new_file:
+        new_file.writelines(file_to_copy.readlines())
