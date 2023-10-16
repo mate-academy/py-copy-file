128b4ee6e346dd5bbcc280333ff977ebcd63655e
Nazar Ivankiv <83316497+Naz-iv@users.noreply.github.com>
2023-10-16 12:08:08 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     commands = command.split()
 
-    if commands[0] != commands[1]:
-        with (open(commands[0], "r") as file_in,
-              open(commands[1], "w") as file_out):
+    if commands[1] != commands[2]:
+        with (open(commands[1], "r") as file_in,
+              open(commands[2], "w") as file_out):
             file_out.writelines(file_in.readlines())
