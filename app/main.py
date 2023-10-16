a52af58ae808b4e74d37128fc1577278bf3025c9
Ihor Prochnickiy <2prochigor@gmail.com>
2023-10-16 18:24:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,10 @@
 def copy_file(command: str) -> None:
     command_name, file, file_to_copy = command.split()
-    if command_name != "cp":
-        print("This command not exist")
-        return
-    try:
-        with (open(file, "r") as current_file,
-              open(file_to_copy, "x") as new_file):
-            new_file.write(current_file.read())
-    except FileNotFoundError:
-        print(f"There is not file with name {file}")
-        return
-    except FileExistsError:
-        print("This file already exist")
+    if command_name == "cp" and file != file_to_copy:
+        try:
+            with (open(file, "r") as current_file,
+                  open(file_to_copy, "w") as new_file):
+                new_file.write(current_file.read())
+        except FileNotFoundError as found_error:
+            print(found_error)
+            return
