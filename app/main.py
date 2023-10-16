4de48f9d6cc95c461ba75e963df97c0597ac8157
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-16 13:02:03 +0300

    cp

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
-def copy_file(command: str) -> None:
-    command = command.split()
-    path_origin = command[1]
-    path_copy = command[2]
+def copy_file(command: str) -> str:
+    command_part = command.split()
+    command = command_part[0]
+    path_origin = command_part[1]
+    path_copy = command_part[2]
 
-    if path_origin != path_copy:
+    if path_origin != path_copy and command == "cp":
         with (open(path_origin, "r") as file_in,
               open(path_copy, "w") as file_out):
             file_out.write(file_in.read())
+
+    else:
+        return "Does nothing"
