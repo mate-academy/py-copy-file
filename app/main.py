1c0297863467bd414748d70a85f822a7f987655d
D1st3f <dima.purig@gmail.com>
2023-10-17 15:38:07 +0300

    Few fix

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def copy_file(command: str) -> None:
-    command_list = command.split()
-    path_in = command[1]
-    path_out = command[2]
-    if command_list[0] == "cp" and path_in != path_out:
+    command, path_in, path_out = command.split()
+    if command == "cp" and path_in != path_out:
         with (open(path_in, "r") as file_in,
               open(path_out, "w") as file_out):
             file_out.write(file_in.read())
