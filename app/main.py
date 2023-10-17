0f5ce11959886378247f2ff89e5c6cfd052f3f1d
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-17 17:37:06 +0300

    Check command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     command, old_file, new_file = command.split()
-    if old_file != new_file:
+    if old_file != new_file and command == "cp":
         with (open(old_file, "r") as old_file,
               open(new_file, "w") as new_file):
             content = old_file.read()
