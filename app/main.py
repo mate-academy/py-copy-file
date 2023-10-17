c51ffebc738b75bf640b07d00fcd00d97c2d3703
Ihor Prochnickiy <2prochigor@gmail.com>
2023-10-17 08:27:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
+from os import path
+
+
 def copy_file(command: str) -> None:
     command_name, file, file_to_copy = command.split()
+    if not path.exists("file.txt"):
+        print(f"file {file} do not exist")
     if command_name == "cp" and file != file_to_copy:
-        try:
-            with (open(file, "r") as current_file,
-                  open(file_to_copy, "w") as new_file):
-                new_file.write(current_file.read())
-        except FileNotFoundError as found_error:
-            print(found_error)
-            return
+        with (open(file, "r") as current_file,
+                open(file_to_copy, "w") as new_file):
+            new_file.write(current_file.read())
