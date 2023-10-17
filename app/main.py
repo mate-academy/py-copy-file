28e04e1e99c731a4938b99a42596330c6d2fd6f5
Serhii <martinenko.sergiy@gmail.com>
2023-10-17 21:42:02 +0300

    useless slices

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     act, first_file, second_file = command.split()
     if act == "cp":
-        if first_file[1] != second_file[2]:
+        if first_file != second_file:
             with (open(first_file, "r") as file_in,
                   open(second_file, "w") as file_out):
                 file_out.write(file_in.read())
