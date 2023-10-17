fa840dd90de204d7d3b4bc8f0c38740408278829
Ihor Prochnickiy <2prochigor@gmail.com>
2023-10-17 09:05:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ def copy_file(command: str) -> None:
     command_name, file, file_to_copy = command.split()
     if not path.exists("file.txt"):
         print(f"file {file} do not exist")
+        return
     if command_name == "cp" and file != file_to_copy:
         with (open(file, "r") as current_file,
                 open(file_to_copy, "w") as new_file):
