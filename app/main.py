1b9a2be02a0bc58e522f1cf705aed20315433367
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-17 16:59:37 +0300

    My solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,7 @@
 def copy_file(command: str) -> None:
     cp_command = command.split()
-    if validate_cp_command(cp_command):
+    if cp_command[1] != cp_command[2]:
         with (open(cp_command[1], "r") as file,
               open(cp_command[2], "w") as new_file):
             content = file.read()
             new_file.write(content)
-
-
-def validate_cp_command(cp_command: list) -> bool:
-    if cp_command[1] != cp_command[2]:
-        return True
