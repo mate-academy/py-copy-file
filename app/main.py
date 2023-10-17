c3ee93c85375c900b37d7c1855459026e174ee24
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-10-17 14:02:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split("")
+    command_name, source_file, copied_file = command_list
+
+    if len(command_list) != 3:
+        return
+
+    if command_name != "cp":
+        return
+
+    if source_file != copied_file:
+        with (open(source_file, "r") as file,
+              open(copied_file, "w") as copied_file):
+            content = file.read()
+            copied_file.write(content)
