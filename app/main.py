33f4607268516304a8f74ff0bca3e5083d7e6292
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-17 17:21:28 +0300

    copy_file was corrected

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
-    cp_command = command.split()
-    if cp_command[1] != cp_command[2]:
-        with (open(cp_command[1], "r") as file,
-              open(cp_command[2], "w") as new_file):
-            content = file.read()
+    command, old_file, new_file = command.split()
+    if old_file != new_file:
+        with (open(old_file, "r") as old_file,
+              open(new_file, "w") as new_file):
+            content = old_file.read()
             new_file.write(content)
