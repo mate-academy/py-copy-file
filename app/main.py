5d138b88950e924d3011c2c909cacdb412697418
Kostia <kostia3507@gmail.com>
2023-10-17 14:58:58 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def copy_file(command: str) -> None:
-    command = command.split(" ")
-    path_in = command[1]
-    path_out = command[2]
+    command, path_in, path_out = command.split()
     if path_in != path_out:
         with open(path_in, "r") as file_in, open(path_out, "w") as file_out:
             file_in.write(file_out.read())
