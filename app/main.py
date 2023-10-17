9896d601007802a039e83e9b6e84e0728a339a81
Vladyslav Pidborskyi <pidborskii17@gmail.com>
2023-10-18 01:30:53 +0300

    Solution update

--- app/main.py
+++ app/main.py
@@ -1,13 +1,6 @@
 def copy_file(command: str) -> None:
-    parts = command.split()
-    if len(parts) == 3:
-        _, filename, new_filename = parts
-        if filename != new_filename:
-            try:
-                with (open(filename, "rb") as source,
-                      open(new_filename, "wb") as destination):
-                    destination.write(source.read())
-            except FileNotFoundError:
-                print(f"File not found: {filename}")
-            except Exception as e:
-                print(f"An error occurred: {e}")
+    command, filename, new_filename = command.split()
+    if command == "cp" and filename != new_filename:
+        with (open(filename, "rb") as source,
+              open(new_filename, "wb") as destination):
+            destination.write(source.read())
