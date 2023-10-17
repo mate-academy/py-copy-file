159cccff49bc9a78cd2657605fa3b6932ce51b87
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-10-17 20:12:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,17 +1,7 @@
 def copy_file(command: str) -> None:
-    parts = command.split()
-
-    if len(parts) != 3 or parts[0] != "cp":
-        print("Invalid command format")
+    command, old_file, new_file = command.split()
+    if old_file == new_file or command != "cp":
         return
-
-    file_name = parts[1]
-    new_file_name = parts[2]
-
-    if file_name == new_file_name:
-        print("Source and destination files have the same name")
-        return
-
-    with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
+    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
         text = file_in.read()
         file_out.write(text)
