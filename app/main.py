bf4c46fba114dae7e02aa6e1ca5eaaa1ee8b7a96
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-17 16:23:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp_command = command.split()
+    if validate_cp_command(cp_command):
+        with (open(cp_command[1], "r") as file,
+              open(cp_command[2], "w") as new_file):
+            content = file.read()
+            new_file.write(content)
+
+
+def validate_cp_command(cp_command: list) -> bool:
+    if cp_command[0] != "cp":
+        return False
+    if cp_command[1] == cp_command[2]:
+        return False
+    return True
