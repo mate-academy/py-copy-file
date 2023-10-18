5361313d6916713da685218988500d98164d9d56
Lytillis <chemurchem@gmail.com>
2023-10-18 18:47:24 +0300

    Minor changes

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ def copy_file(command: str) -> None:
     cp, initial_file, new_file = command.split()
     if initial_file == new_file:
         return
-    with (open(initial_file, "r") as file_in,
-          open(new_file, "w") as file_out):
-        file_out.write(file_in)
+    if cp == "cp":
+        with (open(initial_file, "r") as file_in,
+            open(new_file, "w") as file_out):
+            file_out.write(file_in)
 
 
 if __name__ == "__main__":
