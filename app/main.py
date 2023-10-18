5fd66b4f42e940fb54277638dc570c58e5c4343d
Rostyslav Chornyi <145897105+RVChornyy@users.noreply.github.com>
2023-10-18 23:36:05 +0300

    refactored

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 def copy_file(command: str) -> None:
-    command_tuple = tuple(command.split(" "))
-    if ((len(command_tuple) != 3
-        or command_tuple[1] == command_tuple[2])
-            or command_tuple[0] != "cp"):
-        return
-    with (open(command_tuple[1], "r") as file_in,
-          open(command_tuple[2], "w") as file_out):
-        file_out.write(file_in.read())
+    operation, old_file, new_file = command.split(" ")
+    if operation == "cp" and old_file != new_file:
+        with (open(old_file, "r") as file_in,
+              open(new_file, "w") as file_out):
+            file_out.write(file_in.read())
