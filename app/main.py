60fac5e65d10d5875eb3ae2e4957ed3004e45739
Olena Lisova <lisovausa@gmail.com>
2023-10-18 18:07:28 -0400

    Done copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command_str: str) -> None:
+    command = command_str.split(" ")
+    if command[0] == "cp" and command[1] != command[2]:
+        with open(command[1]) as file_out, open(command[2], "w") as file_in:
+            file_in.write(file_out.read())
