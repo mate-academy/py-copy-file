2c31941fe7c872225b6ef53a088c96d312acb7c3
Serhii <martinenko.sergiy@gmail.com>
2023-10-18 17:02:13 +0300

    one line conditions

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(command: str) -> None:
     act, first_file, second_file = command.split()
-    if act == "cp":
-        if first_file != second_file:
-            with (open(first_file, "r") as file_in,
-                  open(second_file, "w") as file_out):
-                file_out.write(file_in.read())
+    if act == "cp" and first_file != second_file:
+        with (open(first_file, "r") as file_in,
+                open(second_file, "w") as file_out):
+            file_out.write(file_in.read())
