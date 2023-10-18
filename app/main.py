886177d0fece0d35d4a0489906939d8cdf1134c4
Severyn Krok <sevkrok@gmail.com>
2023-10-18 12:49:38 +0300

    Implement py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 def copy_file(command: str) -> None:
     _, file_name, new_file_name = command.split()
     if file_name != new_file_name:
-        with open(file_name) as file_in, open(new_file_name, "w") as file_out:
+        with (
+            open(file_name) as file_in,
+            open(new_file_name, "w") as file_out
+        ):
             file_out.write(file_in.read())
