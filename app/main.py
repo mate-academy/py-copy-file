167d0278b4aa202b167d96580d0214c96ab96050
Andrii Silenko <andrewformicr@gmail.com>
2023-10-18 21:38:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+class InvalidCommandError(Exception):
+    pass
+
+
+def copy_file(command: str) -> None:
+    command_attributes = command.split()
+    if command_attributes[0] != "cp":
+        raise InvalidCommandError
+
+    with open(command_attributes[1], "r") as file_in,\
+         open(command_attributes[2], "w") as file_out:
+        if file_in == file_out:
+            return
+        file_out.write(file_in.read())
