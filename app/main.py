c8adc9db92fbc98dbddc28e56e37c6b5a90b3565
Severyn Krok <sevkrok@gmail.com>
2023-10-20 14:15:52 +0300

    Updating for checking is command == `cp`

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    _, file_name, new_file_name = command.split()
-    if file_name != new_file_name:
+    command, file_name, new_file_name = command.split()
+    if file_name != new_file_name and command == "cp":
         with (
             open(file_name) as file_in,
             open(new_file_name, "w") as file_out
