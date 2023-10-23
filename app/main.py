c7017fe5f3a0c775d27f25aabbb0cba1e0cec000
Your_Name Your_Surname <your@email>
2023-10-23 16:02:34 +0300

    fix bag, but I don't understand how unpack this list_command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     list_command = command.split()
-    if list_command[1] == list_command[2] and list_command[0] == "cp":
+    if list_command[1] == list_command[2] or list_command[0] != "cp":
         return
     with (open(list_command[1], "r") as file_in,
           open(list_command[2], "w") as file_out):
