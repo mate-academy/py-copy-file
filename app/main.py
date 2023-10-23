9e86cbc6a38fc25af77ab2ca5f83e88410d88849
Your_Name Your_Surname <your@email>
2023-10-23 16:23:57 +0300

    add unpack

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
-    list_command = command.split()
-    if list_command[1] == list_command[2] or list_command[0] != "cp":
+    old_file = command.split()
+    com, old_file, new_file = old_file
+    if old_file[1] == new_file[2] or com[0] != "cp":
         return
-    with (open(list_command[1], "r") as file_in,
-          open(list_command[2], "w") as file_out):
+    with (open(old_file[1], "r") as file_in,
+          open(new_file[2], "w") as file_out):
         file_out.writelines(file_in.read())
