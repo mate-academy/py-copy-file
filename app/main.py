273e552bde24fbc09357c4ecd6cb1f265aed9d23
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-10-25 00:38:55 +0300

    Created file copy program.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp, file, new_file = command.split()
+
+    if(
+        cp == "cp"
+        and file.endswith(".txt") and len(file.split(".")[0]) > 0
+        and new_file.endswith(".txt") and len(new_file.split(".")[0]) > 0
+        and file != new_file
+    ):
+        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
