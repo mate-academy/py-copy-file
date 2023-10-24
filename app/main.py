df5eb317da3c8761fc4cf8885f8c07eecd04a8f5
AbsoluteProgramist <108784501+maksimpro001@users.noreply.github.com>
2023-10-24 23:26:07 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def copy_file(command: str) -> None:
     text = command.split(" ")
-    if text[0] != "cp":
+    if text[0] != "cp" or text[1] == text[2]:
         return
-    if text[1] == text[2]:
-        return
-    with open(text[1], "r") as f1, open(text[2], "w") as f2:
-        f2.write(f1.read())
+    with open(text[1], "r") as file1, open(text[2], "w") as file2:
+        file2.write(file1.read())
