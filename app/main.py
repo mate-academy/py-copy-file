77965e627b4641a4c1fea63e17b6d4f79a3c8056
Danylo Sheremeta <danylo3002@gmail.com>
2023-10-25 20:57:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    old_file = command.split()[1]
+    new_file = command.split()[2]
+    if command.split()[0] != "cp" or old_file == new_file:
+        return
+
+    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+        old_file_text = file_in.read()
+        file_out.write(old_file_text)
