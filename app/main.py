b7e752816e91c0748eec6333c1b73176f32674a5
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-10-25 20:27:10 +0300

    Removed redundant checks, renamed variables.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 def copy_file(command: str) -> None:
-    cp, file, new_file = command.split()
+    args = command.split()
 
-    if (
-            cp == "cp"
-            and file.endswith(".txt") and len(file.split(".")[0]) > 0
-            and new_file.endswith(".txt") and len(new_file.split(".")[0]) > 0
-            and file != new_file
-    ):
-        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+    if len(args) != 3 or args[1] == args[2]:
+        return
+
+    cp, path, new_path = args
+
+    try:
+        with open(path, "r") as file_in, open(new_path, "w") as file_out:
             file_out.write(file_in.read())
+
+    except FileNotFoundError:
+        print(print(f"File \"{path}\" not found."))
