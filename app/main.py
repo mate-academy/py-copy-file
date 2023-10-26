f137a5ead5a95fb861fbe3bb722290a987a7d5fc
Seniv Volodymyr <01156240@pw.edu.pl>
2023-10-26 15:51:55 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+class Error(Exception):
+    pass
+
+
+class FileNameError(Error):
+    pass
+
+
+class CommandError(Error):
+    pass
+
+
+def copy_file(command: str, statring_file_name: str, final_file_name: str) -> None:
+    if command != "cp":
+        raise CommandError("You privided the wrong command")
+    if statring_file_name == final_file_name:
+        raise FileNameError("File names can not be equal")
+    content_to_copy = " "
+    with open(statring_file_name, "r") as file:
+        content_to_copy = file.read()
+    with open(final_file_name, "w") as file:
+        file.write(content_to_copy)
