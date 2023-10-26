fcdde9a3c99bde3d11d111359f7d0377756e214c
Alexandr K. <alex329657@gmail.com>
2023-10-26 17:58:57 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+from os import path
+
+
+def copy_file(command: str) -> None:
+    com, filename_in, filename_out = command.split()
+    if (com == "cp" and filename_in != filename_out
+            and path.exists(filename_in)):
+        with (open(filename_in, "r") as file_in,
+              open(filename_out, "w") as file_out):
+            file_out.write(file_in.read())
