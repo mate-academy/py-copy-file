25f73d950a29e9d9e5a9ebad3dfc4da0309aeab2
unknown <temakot254@gmail.com>
2023-10-28 21:02:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_parts = command.split()
+
+    if len(command_parts) != 3:
+        return
+
+    source_file, destination_file, action = command_parts
+
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
+        file_out.write(file_in.read())
+
