5aad823e000416a4d806c208db3902f0d5f983d3
Vlad Krit <kritvladislav98@gmail.com>
2023-10-28 21:22:21 +0300

    solution_py-copy-file_1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
 # write your code here
+def copy_file(cmd: str) -> None:
+    cmd_list = cmd.split()
+    existing_file = cmd_list[1]
+    new_file = cmd_list[2]
+    with open(existing_file, "r") as file_in, open(new_file, "w") as file_out:
+
+        if existing_file == new_file:
+            return
+
+        for line in file_in:
+            file_out.write(line)
