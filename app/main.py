484d39f27c719aa8c2a0ef79491aa34ce1ad2f97
Oleksiy Rozehan <rozeganalexey@gmail.com>
2023-10-28 16:38:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
-    # Split the command into parts
     command_parts = command.split()
 
-    if command_parts[0] != "cp" or command_parts[1] == command_parts[2]:
+    command, source, destination = command_parts
+    if command != "cp" or source == destination:
         return
 
     with (open(command_parts[1], "r") as file_in,
