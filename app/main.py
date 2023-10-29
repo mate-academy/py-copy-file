8ba804f7dfa5753ec755a74e4f724547c00536a0
Vlad Krit <kritvladislav98@gmail.com>
2023-10-29 13:07:01 +0300

    solution_py-copy-file_2

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 # write your code here
 def copy_file(cmd: str) -> None:
     cmd_list = cmd.split()
-    existing_file = cmd_list[1]
-    new_file = cmd_list[2]
+    if len(cmd_list) == 3:
+        _, existing_file, new_file = cmd_list
+    else:
+        print("cmd_list should have a length of 3.")
+
     with open(existing_file, "r") as file_in, open(new_file, "w") as file_out:
 
         if existing_file == new_file:
-            return
-
-        for line in file_in:
-            file_out.write(line)
+            try:
+                with open(source_filename, 'r') as file_in, open(destination_filename, 'w') as file_out:
+                    file_out.write(file_in.read())
+                return True
+            except FileNotFoundError:
+                print("File not found.")
+                return False
