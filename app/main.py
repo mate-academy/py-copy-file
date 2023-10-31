cb04d1aa14e0b58140e5c05a5406bc8aa1d54b08
makukh <makukh@gmail.com>
2023-10-31 23:55:54 +0200

    Solution for Windows copy

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    sep_phrases = command.split()
+    old_filename = sep_phrases[1]
+    new_filename = sep_phrases[2]
+    if sep_phrases[0] == "copy" and old_filename != new_filename:
+
+        with (
+            open(old_filename, "r") as file_in,
+            open(new_filename, "w") as file_out
+        ):
+
+            file_out.write(
+                file_in.read()
+            )
