9b8f8121483aee92b92eed8f77193599e46ad96d
Oleksiy Rozehan <rozeganalexey@gmail.com>
2023-11-01 10:59:24 +0100

    New new solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 def copy_file(string: str) -> None:
     names = string.split(" ")
-    if names[0] == "cp":
-        if names[1] != names[2]:
-            with open(names[1], "r") as first_file:
+    if len(names) >= 3 and names[0] == "cp":
+        source_file, dest_file = names[1], names[2]
+
+        if source_file != dest_file:
+            with open(source_file, "r") as first_file:
                 content = first_file.read()
-            with open(names[2], "w") as second_file:
+            with open(dest_file, "w") as second_file:
                 second_file.write(content)
