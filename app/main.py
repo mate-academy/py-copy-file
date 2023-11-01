2b3bd922c5f8e0006a88bcec3e2eb6392106f4ea
Yuriy Makukh <106692775+makukh@users.noreply.github.com>
2023-11-01 12:30:42 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str) -> None:
     sep_phrases = command.split()
     old_filename = sep_phrases[1]
     new_filename = sep_phrases[2]
-    if sep_phrases[0] == "copy" and old_filename != new_filename:
+    if sep_phrases[0] == "cp" and old_filename != new_filename:
 
         with (
             open(old_filename, "r") as file_in,
