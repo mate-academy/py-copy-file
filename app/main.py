896b86f70548680f3c1ac14889e5f12f261b80b6
Hryhoriy <hryhoriy.bilous@gmail.com>
2023-11-01 23:52:41 +0200

    Solution amen Halilua

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
     command_list = command.split()
-    if command_list[1] == command_list[2]:
-        pass
-    elif command_list[0] == "cp" and len(command_list) == 3:
+    if command_list[0] == "cp" and len(command_list) == 3:
+        if command_list[1] == command_list[2]:
+            return
         with (
             open(command_list[1], "r") as copy,
             open(command_list[2], "w") as paste
