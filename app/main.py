d4d56c4a79da1583b254b592961408eac486acaa
makukh <makukh@gmail.com>
2023-11-01 22:43:15 +0200

    one more syntax correction...

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ def copy_file(command: str) -> None:
     new_filename = parts[2]
 
     if len(parts) != 3:
-        print(f"Invalid command format. Follow this structure:"
-              f"cp filename.txt copied_filename.txt")
+        print("Invalid command format. Follow this structure:"
+              "cp filename.txt copied_filename.txt")
         return
 
     if parts[0] == "cp" and old_filename != new_filename:
