8d7d90dbb40cac526ea596e5b45f1004c49acd60
Sergii Pryima <spryima@gmail.com>
2023-11-01 16:22:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file1, file2 = command.split()[1:]
+    if file1 != file2:
+        with open(file1, "r") as source, open(file2, "w") as destination:
+            destination.write(source.read())
