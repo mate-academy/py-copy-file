cd043b4a05f6cd69783f640d4cc8933cd559340d
Sergii Pryima <spryima@gmail.com>
2023-11-01 16:59:12 +0200

    check input data added

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def copy_file(command: str) -> None:
-    file1, file2 = command.split()[1:]
-    if file1 != file2:
-        with open(file1, "r") as source, open(file2, "w") as destination:
-            destination.write(source.read())
+    parts = command.split()
+    if len(parts) == 3:
+        command, file1, file2 = parts
+        if file1 != file2 and command == "cp":
+            with open(file1, "r") as source, open(file2, "w") as destination:
+                destination.write(source.read())
