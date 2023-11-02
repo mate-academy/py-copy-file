c9139c0d615f764b38c90044c803c28700bed602
Seniv Volodymyr <01156240@pw.edu.pl>
2023-11-02 12:51:11 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -11,10 +11,6 @@ class CommandError(Error):
 
 
 def copy_file(command: str) -> None:
-    first_arg, second_arg, third_arg = command.split()
-    if first_arg != "cp":
-        raise CommandError("You privided the wrong command")
-    if second_arg == third_arg:
-        raise FileNameError("File names can not be equal")
+    _, second_arg, third_arg = command.split()
     with open(second_arg, "r") as source, open(third_arg, "w") as destination:
         destination.write(source.read())
