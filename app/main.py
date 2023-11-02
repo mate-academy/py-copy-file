dc0a184d9e0a5098b08aeda509a2a7a50a0f3d26
Seniv Volodymyr <01156240@pw.edu.pl>
2023-11-02 12:53:44 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,3 @@
-class Error(Exception):
-    pass
-
-
-class FileNameError(Error):
-    pass
-
-
-class CommandError(Error):
-    pass
-
-
 def copy_file(command: str) -> None:
     _, second_arg, third_arg = command.split()
     with open(second_arg, "r") as source, open(third_arg, "w") as destination:
