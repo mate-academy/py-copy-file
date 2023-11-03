096780e8c07f30ad4f059ee6752d443a69e4673b
Roman Shupak <romanshupak808@gmail.com>
2023-11-03 22:45:53 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    components = command.split()
+    file_in, file_out = components[1], components[2]
+    if components[1] == components[2]:
+        print("No action to be taken")
+        return
+
+    with (
+        open(file_in, "r") as source_file,
+        open(file_out, "w") as destination_file
+    ):
+
+        destination_file.write(source_file.read())
