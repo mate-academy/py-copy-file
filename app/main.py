4e213db3254dfc2a3df8bd0be761e120005ae7bd
unknown <temakot254@gmail.com>
2023-11-04 13:43:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ def copy_file(command: str) -> None:
     command_parts = command.split()
 
     if len(command_parts) != 3:
-        return
+        raise ValueError("Command must consist of three parts: action,"
+                         " source filename, and destination filename")
 
     action, source_filename, destination_filename = command_parts
     if action != "cp" or source_filename == destination_filename:
-        raise "You have an error"
+        raise ValueError("The action must be 'cp'")
 
     with (open(source_filename, "r") as source_file,
           open(destination_filename, "r") as destination_file):
