9403b1698d025c9c75c07cd6edb2acbb690913e2
Andrii Malovanyi <malovaniy.an@gmail.com>
2023-11-06 15:12:57 +0200

    fix checking block

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 def cope_file(command: str) -> None:
     command_list = command.split(" ")
-    if len(command_list) == 3:
-        with (open(command_list[1], "r") as old,
-              open(command_list[2], "w") as copy):
+    if (
+            len(command_list) == 3
+            and command_list[0] == "cp"
+            and command_list[1] != command_list[2]
+    ):
+        with (
+            open(command_list[1], "r") as old,
+            open(command_list[2], "w") as copy
+        ):
             copy.write(old.read())
