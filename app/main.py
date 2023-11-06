4057afd1581cbd81f401ad3ec0c6e600e0164b16
Andrii Malovanyi <malovaniy.an@gmail.com>
2023-11-06 14:43:36 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def cope_file(command: str) -> None:
+    command_list = command.split(" ")
+    if len(command_list) == 3:
+        with (open(command_list[1], "r") as old,
+              open(command_list[2], "w") as copy):
+            copy.write(old.read())
