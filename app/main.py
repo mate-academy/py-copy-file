55722cce5c65cabbdddb6588401d38a6ea9f3b62
Roman Shupak <romanshupak808@gmail.com>
2023-11-06 20:03:07 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 def copy_file(command: str) -> None:
     components = command.split()
-    file_in, file_out = components[1], components[2]
+    if len(components) == 3 and components[0] == "cp":
+        file_in, file_out = components[1], components[2]
+    else:
+        print("Invalid command")
+        return
+
     if components[1] == components[2]:
         print("No action to be taken")
         return
@@ -9,5 +14,8 @@ def copy_file(command: str) -> None:
         open(file_in, "r") as source_file,
         open(file_out, "w") as destination_file
     ):
-
         destination_file.write(source_file.read())
+
+
+if __name__ == '__main__':
+    copy_file("cp file.txt file-copy.txt")
