511e2bb0b30b546b03aaba779f5d5e6e544473a7
Oleksiy Rozehan <rozeganalexey@gmail.com>
2023-11-06 14:13:49 +0100

    Solution without doing twice same things

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(string: str) -> None:
     names = string.split()
 
-    if len(names) >= 3 and names[0] == "cp":
+    if len(names) == 3 and names[0] == "cp":
         command, source_file, dest_file = names
-        if source_file != dest_file and command == "cp":
+        if source_file != dest_file:
             with open(source_file, "r") as source_file:
                 content = source_file.read()
                 with open(dest_file, "w") as destination_file:
