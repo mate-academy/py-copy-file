fb6d964a96769581242c20eb7970e31b6c7d54c7
Denis Dymerlii <dum.denya15@gmail.com>
2023-11-07 19:00:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 def copy_file(command: str) -> None:
-    files_names = command.split()
-
-    cp_command = files_names[0]
-    first_file = files_names[1]
-    second_file = files_names[2]
-
-    if len(files_names) < 3 or first_file == second_file or cp_command != "cp":
+    command_content = command.split()
+    if len(command_content) != 3:
         return
 
-    with (open(files_names[1], "r") as main_file,
-          open(files_names[2], "w") as copied_file):
+    cp_command, first_file, second_file = command_content
+
+    if first_file == second_file or cp_command != "cp":
+        return
+
+    with (open(first_file, "r") as main_file,
+          open(second_file, "w") as copied_file):
         copied_file.writelines(line for line in main_file)
