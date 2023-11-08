e48b2d856a15cb55bdd93e11c51bd7f607099aae
Vitalii Taran <vitaliy.taran.1993@gmail.com>
2023-11-08 13:41:45 +0200

    Solution_2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
 
     sep_command = command.split()
-    if not (len(sep_command) == 3):
+    if len(sep_command) != 3:
         return
     cop, file_name, new_file_name = sep_command
-    if file_name == new_file_name:
+    if file_name == new_file_name or cop != "cp":
         return
     try:
         with (open(file_name, "r") as file_in,
