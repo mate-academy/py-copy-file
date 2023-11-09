4cf20428f9c8229db61de33610db8c277c49bcef
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2023-11-09 13:29:01 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 def copy_file(command: str) -> None:
     parts = command.split()
 
-    if len(parts) == 3 and parts[1] != parts[2]:
+    if parts[1] == "cp" and len(parts) == 3 and parts[1] != parts[2]:
+        source_file, destination_file = parts[1], parts[2]
 
-        with (open(parts[1], "r") as file_in,
-              open(parts[2], "w") as file_out):
+        with (
+            open(source_file, "r") as file_in,
+            open(destination_file, "w") as file_out
+        ):
             file_out.write(file_in.read())
