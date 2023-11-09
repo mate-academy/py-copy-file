587b23a635585545082e85ea0ca7791bed4f9e4b
Vlad Krit <kritvladislav98@gmail.com>
2023-11-09 12:19:22 +0200

    solution_py-copy-file_3

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 # write your code here
 def copy_file(cmd: str) -> None:
     cmd_list = cmd.split()
+
     if len(cmd_list) == 3 and cmd_list[0] == "cp":
         _, existing_file, new_file = cmd_list
-    if existing_file != new_file:
-        with (
-            open(existing_file, "r") as file_in,
-            open(new_file, "w") as file_out
-        ):
-            file_out.write(file_in.read())
+
+        if existing_file != new_file:
+
+            with open(existing_file, "r") as file_in:
+                with open(new_file, "w") as file_out:
+
+                        file_out.write(file_in.read())
