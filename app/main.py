2083375f6013cb1aa69f6c754d1cb52e587e9a6e
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2023-11-09 15:25:11 +0200

    Fixed5

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
 def copy_file(command: str) -> None:
     parts = command.split()
-    what_command = parts[0]
+    command = parts[0]
     source_file, destination_file = parts[1], parts[2]
+    if len(parts) == 3:
+        if command == "cp" and source_file != destination_file:
 
-    if (
-            what_command == "cp"
-            and len(parts) == 3
-            and source_file != destination_file
-    ):
-
-        with (
-            open(source_file, "r") as file_in,
-            open(destination_file, "w") as file_out
-        ):
-            file_out.write(file_in.read())
+            with (
+                open(source_file, "r") as file_in,
+                open(destination_file, "w") as file_out
+            ):
+                file_out.write(file_in.read())
