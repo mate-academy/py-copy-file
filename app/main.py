366ffb3e99a678bed7c3649267b300259482e245
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2023-11-09 13:00:28 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) == 3 and parts[1] != parts[2]:
+
+        with (open(parts[1], "r") as file_in,
+              open(parts[2], "w") as file_out):
+            file_out.write(file_in.read())
