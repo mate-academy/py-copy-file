511fede050dd1161c6dfbab003a1c4f16b2ef7ec
Vishnevskyi Dmytro <vishneman@gmail.com>
2023-11-09 13:17:12 +0200

    fixed flake8

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(files_name: str) -> None:
-    command_line = files_name.split(" ")
+    command_line = files_name.split()
     if len(command_line) == 3 and command_line[0] == "cp":
         command, name_old_file, name_new_file = command_line
         if name_old_file != name_new_file:
