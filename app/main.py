5cd9ac44678d8859a3bc1e8ece106e2e2fc6ee2c
Kyryl Zhelizko <kyryl.zhelizko@gmail.com>
2023-11-10 11:44:06 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 def copy_file(command: str) -> None:
     if not isinstance(command, str):
         raise TypeError("Command must be str type")
-    if len(command.split()) != 3 or command.split()[0] != "cp":
+    command_split = command.split()
+    if len(command_split) != 3 or command_split[0] != "cp":
         raise ValueError(
             "Command should be like this:"
             " cp file.txt file-copy.txt"
         )
 
-    first_file_name = command.split()[1]
-    second_file_name = command.split()[2]
+    first_file_name = command_split[1]
+    second_file_name = command_split[2]
     if first_file_name != second_file_name:
         with (
             open(first_file_name, "r") as first_file,
