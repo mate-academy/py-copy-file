263b1834f4f178374b72e2b9ecb7306a00edb0cc
Anatolii Hrechaniuk <genezisium@gmail.com>
2023-11-10 16:35:51 +0200

    checking list_command has 3 arguments

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 def copy_file(command: str) -> None:
     list_command = command.split()
-    source_file, destination_file = list_command[1], list_command[2]
-    if source_file != destination_file:
-        with (
-            open(source_file, "r") as file_in,
-            open(destination_file, "w") as file_out
-        ):
-            file_out.write(file_in.read())
+    if len(list_command) == 3:
+        source_file, destination_file = list_command[1], list_command[2]
+        if source_file != destination_file:
+            with (
+                open(source_file, "r") as file_in,
+                open(destination_file, "w") as file_out
+            ):
+                file_out.write(file_in.read())
