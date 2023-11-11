84889d9f44e3f85b9f0898752cbeb649a8f19ecf
Denysov Viacheslav <wdenisov113@gmail.com>
2023-11-11 10:24:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    files_names = command.split(" ")
+    if files_names[1] != files_names[2]:
+        with open(files_names[1], "r") as file_to_copy, \
+                open(files_names[2], "w") as new_file:
+            file_content = file_to_copy.readlines()
+            new_file.write("".join(file_content))
+
