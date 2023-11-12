2e9c0b7a0d0af931cf9fc667faef0719f1e64a25
Alex <oleksa.shcherbakov@gmail.com>
2023-11-12 20:43:58 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def copy_file(command: str) -> None:
-    temp_arr = command.split()
-    if len(temp_arr) == 3 and temp_arr[1] != temp_arr[2]:
-        with (open(temp_arr[1], "r") as source,
-              open(temp_arr[2], "w") as copied_file
-              ):
+    command_keyword, initial_filename, copied_filename = command.split()
+    if command_keyword == "cp" and initial_filename != copied_filename:
+        with (open(initial_filename, "r") as source, (copied_filename, "w") as copied_file):
             copied_file.write(source.read())
