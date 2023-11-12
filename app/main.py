7e98ca4b8292fa0476873aec421307d4b323d811
Alex <oleksa.shcherbakov@gmail.com>
2023-11-12 20:45:04 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_file(command: str) -> None:
     command_keyword, initial_filename, copied_filename = command.split()
     if command_keyword == "cp" and initial_filename != copied_filename:
-        with (open(initial_filename, "r") as source, (copied_filename, "w") as copied_file):
+        with (open(initial_filename, "r") as source,
+              (copied_filename, "w") as copied_file):
             copied_file.write(source.read())
