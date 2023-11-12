6e125196a294a3dd46e43be0aa18eac42a5f0896
Alex <oleksa.shcherbakov@gmail.com>
2023-11-12 15:29:45 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy_file(command: str) -> None:
-    temp_arr = command.split(" ")
+    temp_arr = command.split()
     if len(temp_arr) == 3 and temp_arr[1] != temp_arr[2]:
-        with open(temp_arr[1], "r") as source:
-            with open(temp_arr[2], "w") as copied_file:
-                copied_file.write(source.read())
+        with (open(temp_arr[1], "r") as source, open(temp_arr[2], "w") as copied_file):
+            copied_file.write(source.read())
