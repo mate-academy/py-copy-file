0c7c841bc457c8d89f32257c4d535713e279138b
Yuliia Valko <yuliia.valko1@gmail.com>
2023-11-12 20:12:48 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    all_data = command.split(" ")
+    with open(all_data[1], "r") as file_in, open(all_data[2], "w") as file_out:
+        if all_data[1] != all_data[2]:
+            content = file_in.read()
+            file_out.write(content)
