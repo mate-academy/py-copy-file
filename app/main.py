e63c41c64440630b9fc9d1b21a2e5b0d340ff4b3
Yuliia Valko <yuliia.valko1@gmail.com>
2023-11-13 17:49:11 +0200

    Solution_2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 def copy_file(command: str) -> None:
-    all_data = command.split(" ")
-    with open(all_data[1], "r") as file_in, open(all_data[2], "w") as file_out:
-        if all_data[1] != all_data[2]:
-            content = file_in.read()
-            file_out.write(content)
+    all_data = command.split()
+
+    if all_data[0] != "cp" or all_data[1] == all_data[2] or len(all_data) != 3:
+        return
+
+    file_to_copy = all_data[1]
+    new_file = all_data[2]
+
+    with open(file_to_copy, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
