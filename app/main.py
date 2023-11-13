aa9b832bb3bbf7e8cfddea7907958c47ba4f21d6
Yuliia Valko <yuliia.valko1@gmail.com>
2023-11-13 20:54:14 +0200

    Solution_2

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 def copy_file(command: str) -> None:
-    all_data = command.split()
+    user_input = command.split()
 
-    if all_data[0] != "cp" or all_data[1] == all_data[2] or len(all_data) != 3:
-        return
+    if (len(user_input) != 3
+            or user_input[0] != "cp"
+            or user_input[1] == user_input[2]):
+        raise Exception("Please enter correct data.")
 
-    file_to_copy = all_data[1]
-    new_file = all_data[2]
+    _, file_to_copy, new_file = user_input
 
     with open(file_to_copy, "r") as file_in, open(new_file, "w") as file_out:
         file_out.write(file_in.read())
