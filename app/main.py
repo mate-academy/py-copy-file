028a42b04500a886d5f09002be530a1b32a09429
Denysov Viacheslav <wdenisov113@gmail.com>
2023-11-14 15:35:51 +0200

    Final Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 def copy_file(command: str) -> None | str:
     file_names = command.split()
 
-    if len(file_names) != 3 or file_names[1] == file_names[2]:
+    if (
+            len(file_names) != 3
+            or file_names[1] == file_names[2]
+            or file_names[0] != "cp"
+    ):
         return "invalid format"
 
     old_file, new_file = file_names[1], file_names[2]
 
-    # Copy the file content from source to destination
     with open(old_file, "r") as file_to_copy, open(new_file, "w") as new_file:
         file_content = file_to_copy.readlines()
         new_file.write("".join(file_content))
