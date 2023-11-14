38b85e53b19645db98eb6edc2aad93b2ffe38a31
Denysov Viacheslav <wdenisov113@gmail.com>
2023-11-14 15:17:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-def copy_file(command: str) -> None:
-    files_names = command.split(" ")
-    if files_names[1] != files_names[2]:
-        with open(files_names[1], "r") as file_to_copy, \
-                open(files_names[2], "w") as new_file:
-            file_content = file_to_copy.readlines()
-            new_file.write("".join(file_content))
+def copy_file(command: str) -> None | str:
+    file_names = command.split()
+
+    if len(file_names) != 3 or file_names[1] == file_names[2]:
+        return "invalid format"
+
+    old_file, new_file = file_names[1], file_names[2]
+
+    # Copy the file content from source to destination
+    with open(old_file, "r") as file_to_copy, open(new_file, "w") as new_file:
+        file_content = file_to_copy.readlines()
+        new_file.write("".join(file_content))
