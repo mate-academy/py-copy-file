e07e54f0c35fba895351672b604fc9470def7897
Denysov Viacheslav <wdenisov113@gmail.com>
2023-11-15 14:19:06 +0200

    I hope this is it

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 def copy_file(command: str) -> None:
     command, old_file, new_file = command.split()
-    if (
-            old_file == new_file
-            or command != "cp"
-    ):
+    if old_file == new_file or command != "cp":
         return
 
     with open(old_file, "r") as file_to_copy, open(new_file, "w") as new_file:
-        file_content = file_to_copy.read()
-        new_file.write("".join(file_content))
+        new_file.write(file_to_copy.read())
