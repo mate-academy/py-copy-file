01d6b2b25f813f25c079bdfdd4ef84e6992e30cd
Volodymyr Ivanchuk <Volodymyr_Ivanchuk@jabil.com>
2023-11-16 13:04:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
     parts = command.split()
     if (
-        parts[0] == "cp" and
-        parts[1] != parts[2] and
-        len(parts) == 3
+        parts[0] == "cp"
+        and parts[1] != parts[2]
+        and len(parts) == 3
     ):
         with (
             open(parts[2], "w") as new_file,
