9c2aa3b9679bf61162db17efb4e5c044a86e9324
Oleksandr Matviichuk <oleksandr.matviychuk@gmail.com>
2023-11-17 17:08:56 +0200

    Solution3

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     file_names = command.split()
-    if (file_names[1] != file_names[2]
-            and len(file_names) == 3
+    if (len(file_names) == 3
+            and file_names[1] != file_names[2]
             and file_names[0] == "cp"):
         with (open(file_names[1], "r") as file_in,
               open(file_names[2], "w") as file_out):
