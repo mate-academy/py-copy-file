0c44c9c5207c25c56b7556d0e07f866dfd1c1d1d
Marina <marina.ua13@gmail.com>
2023-11-17 20:59:57 +0200

    changed

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
 def copy_file(command: str) -> None:
-    with open("file.txt", "w") as f:
-        f.write("Hello world!")
+    parts = command.split()
 
-        parts = command.split()
+    if len(parts) == 3 and parts[0] == "cp":
+        source_file, destination_file = parts[1], parts[2]
 
-        if len(parts) == 3 and parts[0] == "cp":
-            source_file, destination_file = parts[1], parts[2]
-
-            if source_file != destination_file:
-                with (
-                    open(source_file, "r") as file_in,
-                    open(destination_file, "w") as file_out
-                ):
-                    file_out.write(file_in.read())
-                print(f"File {source_file} "
-                      f"copied to {destination_file}")
+        if source_file != destination_file:
+            with (
+                open(source_file, "r") as file_in,
+                open(destination_file, "w") as file_out
+            ):
+                file_out.write(file_in.read())
+            print(f"File {source_file} "
+                  f"copied to {destination_file}")
