7901fcd297fa69a0320b8eb26f02f1c268080e7c
Marina <marina.ua13@gmail.com>
2023-11-17 18:10:28 +0200

    changed

--- app/main.py
+++ app/main.py
@@ -8,28 +8,10 @@ def copy_file(command: str) -> None:
             source_file, destination_file = parts[1], parts[2]
 
             if source_file != destination_file:
-                try:
-                    with open(source_file, "r") as file_in, \
-                            open(destination_file, "w") as file_out:
-                        file_out.write(file_in.read())
-                    print(f'File "{source_file}" '
-                          f'copied to "{destination_file}"')
-                except FileNotFoundError:
-                    print(f"Source file {source_file} does not exist")
-
-                else:
-                    print(
-                        "Source and destination file names are the same. "
-                        "Doing nothing."
-                    )
-            else:
-                print(
-                    "Invalid command format. "
-                    "Use 'cp source_file destination_file.'"
-                )
-
-
-copy_file("cp file.txt file.txt")
-
-copy_file("cp file.txt new_file.txt")
-print(open("file.txt").read() == open("new_file.txt").read())
+                with (
+                    open(source_file, "r") as file_in,
+                    open(destination_file, "w") as file_out
+                ):
+                    file_out.write(file_in.read())
+                print(f"File {source_file} "
+                      f"copied to {destination_file}")
