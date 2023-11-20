e812f40225e8b3650132785d42a21f7f1106afb2
lyaskov <lyaskov1@gmail.com>
2023-11-20 14:42:21 +0200

    implement code review feedback

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    command, old_file, new_file = command.split(" ")
+    command, old_file, new_file = command.split()
     if command == "cp" and old_file != new_file:
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             copy_content = file_in.read()
