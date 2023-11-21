cef64e88370b2d115b55d8d5349a3d3cc5a1de2b
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2023-11-21 19:48:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+
+def copy_file(command: str) -> None:
+    file = command.split()
+    if not file[0].startswith("cp"):
+        return
+    if file[1] != file[2]:
+        with open(file[1], "r") as origin_file, open(file[2], "w") as cp_file:
+            cp_file.write(origin_file.read())
