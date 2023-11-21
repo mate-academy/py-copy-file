45773bf49faf6cfeba625de91e21490514e1881d
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2023-11-21 19:53:36 +0200

    fix variable name

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
-
 def copy_file(command: str) -> None:
-    file = command.split()
-    if not file[0].startswith("cp"):
+    part = command.split()
+    if not part[0].startswith("cp"):
         return
-    if file[1] != file[2]:
-        with open(file[1], "r") as origin_file, open(file[2], "w") as cp_file:
+    if part[1] != part[2]:
+        with open(part[1], "r") as origin_file, open(part[2], "w") as cp_file:
             cp_file.write(origin_file.read())
