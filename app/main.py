a28adb4abdbfa36f1ad24e4d43059debc887954f
Serhii <serhii.domik@gmail.com>
2023-11-22 14:49:18 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    commands = command.split()
+    source_file = commands[1]
+    destination_file = commands[2]
+
+    if source_file != copy_file:
+        with (open(source_file, "r") as first_file,
+              open(destination_file , "w") as second_file):
+            content = first_file.read()
+            second_file.write(content)
