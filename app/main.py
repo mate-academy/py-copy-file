f3c8aff02ee9807799f85a8fd2105d80060ae61a
Marinel444 <380978907444m@gmail.com>
2023-11-22 17:14:58 +0200

    add '\'

--- app/main.py
+++ app/main.py
@@ -3,5 +3,6 @@ def copy_file(command: str) -> None:
         command, file, new_file = command.split(" ")
         if command == "cp":
             if file != new_file:
-                with open(file, "r") as file_in, open(new_file, "w") as file_out:
+                with open(file, "r") as file_in, \
+                        open(new_file, "w") as file_out:
                     file_out.write(file_in.read())
