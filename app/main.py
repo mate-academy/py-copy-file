7ecc8a77059f299da665ec7c79c1a73b1690bd14
Serhii <serhii.domik@gmail.com>
2023-11-22 15:22:45 +0200

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 def copy_file(command: str) -> None:
-    commands = command.split()
-    source_file = commands[1]
-    destination_file = commands[2]
 
-    if source_file != copy_file:
-        with (open(source_file, "r") as first_file,
-              open(destination_file , "w") as second_file):
-            content = first_file.read()
-            second_file.write(content)
+    commands = command.split()
+    if len(commands) == 3 and commands[0] == "cp":
+        source_file = commands[1]
+        destination_file = commands[2]
+
+        if source_file != copy_file:
+            with (open(source_file, "r") as first_file,
+                  open(destination_file , "w") as second_file):
+                content = first_file.read()
+                second_file.write(content)
