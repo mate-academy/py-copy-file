58a36c18eb10e50e82ba9f343e8c67c59269a120
Marinel444 <380978907444m@gmail.com>
2023-11-22 20:03:41 +0200

    Rename command_keyword

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    command, file, new_file = command.split(" ")
-    if command == "cp":
+    command_keyword, file, new_file = command.split(" ")
+    if command_keyword == "cp":
         if file != new_file:
             with open(file, "r") as file_in, open(new_file, "w") as file_out:
                 file_out.write(file_in.read())
