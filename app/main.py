5059dd5419aab33832fbc7b6aea874217333ab71
MykolaMazhula <mykm3ua@gmail.com>
2023-11-23 18:44:21 +0100

    Copy-File-Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    arguments = command.split()
+    if (command.startswith("cp ")
+            and len(arguments) == 3
+            and arguments[1] != arguments[2]):
+        file1 = open(arguments[1], "r")
+        file2 = open(arguments[2], "w")
+        file2.write(file1.read())
+        file1.close()
+        file2.close()
