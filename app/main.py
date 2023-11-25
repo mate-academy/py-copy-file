4a784842fb1f95bf4b5e9b5255adb60d5108ed7f
MykolaMazhula <mykm3ua@gmail.com>
2023-11-25 16:59:36 +0100

    edited 3.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(command: str) -> None:
     command_elements = command.split()
-    _command = command_elements[0]
-    if len(command_elements) != 3 or _command != "cp":
+    if len(command_elements) != 3 or command_elements[0] != "cp":
         raise ValueError('"command" must be a string with command "cp", '
                          "file name to copy and new file name, "
                          "separated by spaces.")
