d2a1be9d84d43cd297fb879835e040552428e630
Maksym <tarasyukmaxim@gmail.com>
2023-11-25 13:20:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str):
+    command_list = command.split()
+
+    source_file, destination_file = command_list[1], command_list[2]
+
+    if source_file != destination_file:
+        with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
+            file_out.write(file_in.read())
