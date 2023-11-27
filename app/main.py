e2a0a2b922f459d90daa82e79c9bcbffb9c987f2
Vlad <mr.darmstadtium@gmail.com>
2023-11-27 18:32:57 +0100

    wrote all code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    command_split = command.split()
+    action = command_split[0]
+    file_original = command_split[1]
+    file_for_copy = command_split[2]
+
+    if file_original != file_for_copy:
+        with (
+            open(file_original, "r") as original,
+            open(file_for_copy, "a") as for_copy
+        ):
+            all_content = original.read()
+            for_copy.write(all_content)
