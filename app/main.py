62a24b2fdb65b56a7d11304a68d19b21b86c1ef2
Denys Kibalenko <dennieking87@gmail.com>
2023-11-27 19:57:01 +0100

    Solution fix command extra tests

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 def copy_file(command: str) -> None:
     split_command = command.split(" ")
-    if split_command[1] != split_command[2]:
-        with (open(split_command[1], "r") as file_in,
-              open(split_command[2], "w") as file_out):
-            content = file_in.read()
-            file_out.write(content)
+    if len(split_command) == 3:
+        if (split_command[0] == "cp"
+                and split_command[1] != split_command[2]):
+            with (open(split_command[1], "r") as file_in,
+                  open(split_command[2], "w") as file_out):
+                content = file_in.read()
+                file_out.write(content)
