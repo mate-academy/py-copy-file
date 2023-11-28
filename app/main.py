81cfacb5b3396b9d13002a58605c06d8d67e5c15
Oleksandra <aleksandra@regdesk.co>
2023-11-28 18:22:52 +0200

    'fixed'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 def copy_file(command: str) -> None:
-    files = command.split(" ")[1:]
-    curr_file = files[0]
-    new_file = files[1]
+    command_, current_file, new_file = command.split()
 
-    if curr_file != new_file:
-        with open(curr_file, "r") as file_in, open(new_file, "w") as file_out:
+    if current_file != new_file and command_ == "cp":
+        with (
+            open(current_file, "r") as file_in,
+            open(new_file, "w") as file_out
+        ):
             file_out.write(file_in.read())
