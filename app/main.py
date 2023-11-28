2fe282867229d4705c6f4c8866eaa6d68db19965
Ryshtun Denys <rishtund@gmail.com>
2023-11-28 14:34:00 +0200

    ADone

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, file_name, new_file_name = command.split(" ")
+
+    if file_name != new_file_name:
+        try:
+            with open(file_name, "r") as old, open(new_file_name, "w") as new:
+                new.write(old.read())
+
+        except FileNotFoundError:
+            print("File not found")
