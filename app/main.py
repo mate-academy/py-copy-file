96c31a56c74183e5db584926d12c75a2424b8e37
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2023-11-28 10:08:19 +0100

    Add checking for command "cp"

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 def copy_file(command: str) -> None:
 
     command_split = command.split()
+    
+    if len(command_split) == 3:
+        file_command = command_split[0]
+        original = command_split[1]
+        copy = command_split[2]
 
-    file_original = command_split[1]
-    file_for_copy = command_split[2]
-
-    if file_original != file_for_copy:
-        with (
-            open(file_original, "r") as original,
-            open(file_for_copy, "a") as for_copy
-        ):
-            all_content = original.read()
-            for_copy.write(all_content)
+        if (original != copy) and (file_command == "cp"):
+            with (
+                open(original, "r") as original,
+                open(copy, "a") as for_copy
+            ):
+                all_content = original.read()
+                for_copy.write(all_content)
