ce78d3509b0e63d3e2c6d8bae8c4b10e38e54f6b
xboris <boris.tikhonov.21@gmail.com>
2023-11-28 19:34:41 +0200

    'solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, file_name, file_copy = command.split()
+    if command != "cp" or file_name == file_copy:
+        pass
+    with open(file_name, "r") as file_in, open(file_copy, "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
