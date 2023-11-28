ec79e2ed6826a9b5c88dfa1e32f27768338fc29a
Vladyslav Moiseienko <moiseenkov651@gmail.com>
2023-11-28 13:08:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        print("Invalid command")
+        return
+    source, destination = parts[1], parts[2]
+    if source == destination:
+        return
+    with open(source, "r") as file_from, open(destination, "w") as file_to:
+        file_to.write(file_from.read())
