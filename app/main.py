d82b9f8642fc4f9e184afdc22e42b9266ceb8e5b
Anton Konyk <antonkonyk.com>
2023-11-29 13:05:28 +0100

    Solution_29112023

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ def copy_file(command: str) -> None:
     if (parts[0] == "cp"
        and parts[1] != parts[2]
        and len(parts) == 3):
+
         with (open(parts[1], "r") as file_in,
              open(parts[2], "w") as file_out):
             file_out.write(file_in.read())
