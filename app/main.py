2c702cdfc7af809b2a774d14de91da1c426d6253
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-12-01 22:27:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    list_of_tokens = command.split()
+    if list_of_tokens[1] == list_of_tokens[2]:
+        return None
+    with open(list_of_tokens[1], "r") as file_in, \
+            open(list_of_tokens[2], "w") as file_out:
+        file_out.write(file_in.read())
