4efba8f0251355dfccce221fb886f0a177775753
eLQeR <131271125+eLQeR@users.noreply.github.com>
2023-12-03 17:24:56 +0200

    Solution fully

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     if command.startswith("cp"):
         files = command.split()[1:]
-        if len(files) > 1:
+        if len(set(files)) > 1:
             with (open(files[0], "r") as file_to_copy,
                   open(files[1], "w") as file_copy):
                 file_copy.write(file_to_copy.read())
