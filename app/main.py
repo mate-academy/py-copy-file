9c623c99d3eb6663a48eb1b59b204dce71b9bd9c
Nataliia Povashevych <interchanal@ukr.net>
2023-12-04 20:30:32 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    user_command, user_file, copy_user_file = command.split(" ")
+    if user_command == "cp":
+        with (open(user_file, "r") as file_in,
+              open(copy_user_file, "w") as file_out):
+            if user_file != copy_user_file:
+                file_out.write(file_in.read())
