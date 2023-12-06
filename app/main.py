5b67461e1bad08020c938b892c7a364b1e416af4
Mykhailo Slavsky <slavskij2017@gmail.com>
2023-12-07 00:33:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(copy_command: str) -> None:
+    old_name = copy_command.split()[1]
+    new_name = copy_command.split()[2]
+    if old_name == new_name:
+        return
+    with open(new_name, "r") as file_in, open(old_name, "w") as file_out:
+        for string in file_in:
+            file_out.write(string)
