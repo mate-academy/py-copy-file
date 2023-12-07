b42947e1de422592e9975dc89ed889f6c5c1d2cd
Mykhailo Slavsky <slavskij2017@gmail.com>
2023-12-07 17:22:58 +0200

    Fix Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def copy_file(copy_command: str) -> None:
-    copy_commands = copy_command.split()
-    old_name = copy_commands[1]
-    new_name = copy_commands[2]
+    command, old_name, new_name = copy_command.split()
     if old_name == new_name:
         return
     with open(new_name, "r") as file_in, open(old_name, "w") as file_out:
