d89662284fe709f43de33356193ea8e0246461a1
P-is-here <pasha.nasonovhtc@gmail.com>
2023-12-07 23:34:55 +0200

    refactored to one if statement

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 def copy_file(command: str) -> None:
     tokens = command.split()
-
-    if tokens[1] == tokens[2]:
-        return None
-
-    if tokens[0] != "cp" and len(tokens) != 3:
+    if tokens[0] != "cp" and len(tokens) != 3 and tokens[1] == tokens[2]:
         with open(tokens[1], "r") as file_in, \
                 open(tokens[2], "w") as file_out:
             file_out.write(file_in.read())
