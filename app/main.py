fa4fc92e01e6b83b60b273178bedf9b34dc5869b
Юрий <you@exampleabc1234567892777@gmail.com>
2023-12-08 14:16:03 +0200

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -2,15 +2,8 @@ def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) == 3 and parts[1] != parts[2]:
-        source_file = parts[1]
-        destination_file = parts[2]
+        source_file, destination_file = parts[1], parts[2]
 
         with (open(source_file, "r") as file_in,
               open(destination_file, "w") as file_out):
             file_out.write(file_in.read())
-
-
-copy_file("cp file.txt file.txt")
-
-copy_file("cp file.txt new_file.txt")
-print(open("file.txt").read() == open("new_file.txt").read())
