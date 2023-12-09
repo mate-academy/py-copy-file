4024c38cc5b525f0dc7bbe37f95ed44506379d71
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2023-12-09 21:10:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 def copy_file(command: str) -> None:
-    command = command[3:].split(" ")
-    f_in = str(command[0])
-    f_out = str(command[1])
+    command = command.split(" ")
+    try:
+        main_command = str(command[0])
+        if main_command == "cp":
+            f_in = str(command[1])
+            f_out = str(command[2])
+    except IndexError:
+        pass
 
     with open(f_in, "r") as file_in, open(f_out, "w") as file_out:
         if f_in != f_out:
