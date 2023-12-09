d22b217439ba83aa55043d964b6753d9be1be714
Volodymyr Kovalenko <380974261314kv@gmail.com>
2023-12-09 18:42:49 +0200

    implemented  copy_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    lst = command.split()
+    if lst[0] == "cp":
+        if lst[1] != lst[2]:
+            with open(lst[1], "r") as file_in, open(lst[2], "w") as file_out:
+                file_out.write(file_in.read())
