f8e354c5c5c14809b36e3f97a8b504d7efa13f71
Bohdan11Dii <bodyabrovdiy@gmail.com>
2023-12-09 13:13:07 +0200

    Rename on source and destination

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ def copy_file(command: str) -> None:
             and line_command[0] == "cp"
             and line_command[1] != line_command[2]):
 
-        with (open(line_command[1], "r") as file_in, 
-              open(line_command[2], "w") as file_out):
-            file_out.write(file_in.read())
-    
+        with (open(line_command[1], "r") as source,
+              open(line_command[2], "w") as destination):
+            destination.write(source.read())
+    else:
+        print("Some wrong with copy")
