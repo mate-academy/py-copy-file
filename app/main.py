ada95c748ea0f9c9ee6e2c3286a7652d7f09a20f
Volodymyr Kovalenko <380974261314kv@gmail.com>
2023-12-10 18:39:22 +0200

    add try/except checking

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 def copy_file(command: str) -> None:
-    if len(command.split()) == 3:
-        operation, file_1, file_2 = command.split()
-    if operation == "cp" and file_1 != file_2:
-        with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
-            file_out.write(file_in.read())
+    parts = command.split()
+    if len(parts) == 3 and parts[0] == "cp":
+        source_file, destination_file = parts[1], parts[2]
+        if source_file != destination_file:
+            try:
+                with open(source_file, "r") as file_in:
+                    content = file_in.read()
+
+                with open(destination_file, "w") as file_out:
+                    file_out.write(content)
+                print(f"File '{source_file}' copied to '{destination_file}'")
+            except FileNotFoundError:
+                print(f"Error: File '{source_file}' not found.")
+            except Exception as e:
+                print(f"Error: {e}")
+        else:
+            print("Error: Source and destination file names are the same.")
+    else:
+        print("""Error: Invalid command format.
+            Please use 'cp source_file destination_file'.""")
