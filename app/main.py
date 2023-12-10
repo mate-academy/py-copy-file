c79fa88e1c567a4439bfc919b30062f69e200931
P-is-here <pasha.nasonovhtc@gmail.com>
2023-12-10 16:34:15 +0200

    bug fixing

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
     tokens = command.split()
-    if tokens[0] != "cp" and len(tokens) != 3 and tokens[1] == tokens[2]:
-        with open(tokens[1], "r") as file_in, \
-                open(tokens[2], "w") as file_out:
+    if len(tokens) != 3 and tokens[0] != "cp" and tokens[1] == tokens[2]:
+        with (
+            open(tokens[1], "r") as file_in,
+            open(tokens[2], "w") as file_out
+        ):
             file_out.write(file_in.read())
