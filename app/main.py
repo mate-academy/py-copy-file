799495053128e5a9c1d97349d7537feee52c6b32
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-12-10 20:43:52 +0200

    != to ==

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     tokens = command.split()
-    if len(tokens) != 3 and tokens[0] != "cp" and tokens[1] == tokens[2]:
+    if len(tokens) == 3 and tokens[0] != "cp" and tokens[1] == tokens[2]:
         with (
             open(tokens[1], "r") as file_in,
             open(tokens[2], "w") as file_out
