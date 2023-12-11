8f6acf2187365fdb099351574a3f41b0b1212976
Volodymyr Kovalenko <380974261314kv@gmail.com>
2023-12-11 11:41:38 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,21 +1,22 @@
 def copy_file(command: str) -> None:
-    parts = command.split()
-    if len(parts) == 3 and parts[0] == "cp":
-        source_file, destination_file = parts[1], parts[2]
-        if source_file != destination_file:
-            try:
-                with open(source_file, "r") as file_in:
-                    content = file_in.read()
-
-                with open(destination_file, "w") as file_out:
-                    file_out.write(content)
-                print(f"File '{source_file}' copied to '{destination_file}'")
-            except FileNotFoundError:
-                print(f"Error: File '{source_file}' not found.")
-            except Exception as e:
-                print(f"Error: {e}")
+    if len(command.split()) == 3:
+        operation, source_file, destination_file = command.split()
+        if operation == "cp":
+            if source_file != destination_file:
+                try:
+                    with open(source_file, "r") as file_in,\
+                         open(destination_file, "w") as file_out:
+                        file_out.write(file_in.read())
+                    print(f"File '{source_file}'"
+                          f"copied to '{destination_file}'")
+                except FileNotFoundError:
+                    print(f"Error: File '{source_file}' not found.")
+                except Exception as e:
+                    print(f"Error: {e}")
+            else:
+                print("Error: Source and destination file names are the same.")
         else:
-            print("Error: Source and destination file names are the same.")
+            print("Error. Operation should be 'cp'")
     else:
-        print("""Error: Invalid command format.
-            Please use 'cp source_file destination_file'.""")
+        print("Error: Invalid command format."
+              "Please use 'cp source_file destination_file'.")
