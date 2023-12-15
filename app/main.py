f8d2a59431debd9acf9d805f4b358249dbd524c8
Bohdan Horbachov <bgbro.inc@gmail.com>
2023-12-15 22:27:58 +0200

    added copy_file func

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, fname_to_copy, new_fname = command.split(" ")
+    if fname_to_copy == new_fname:
+        pass
+
+    with open(fname_to_copy, "r") as file_in, open(new_fname, "w") as file_out:
+        file_out.write(file_in.read())
