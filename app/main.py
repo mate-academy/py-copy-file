4aa461a3902f89fa9bdcccd533db80ccb067dcff
artem.puhach <artem.puhach@cyberpollice.gov.ua>
2023-12-16 16:16:21 +0200

    fix: checked command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    command, file_1, file_2 = command.split(" ")
-    if file_1 == file_2:
+    cp_command, file_1, file_2 = command.split(" ")
+    if file_1 == file_2 or cp_command != "cp":
         pass
     else:
         with open(file_1, "r") as source, open(file_2, "a") as target:
