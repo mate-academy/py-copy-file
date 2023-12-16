2a177cc3232a16ab2b8c6a6767f256724960da80
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-12-16 22:29:00 +0200

    fix: after review

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def get_full_path(file_name: str) -> str:
 
 def copy_file(command: str) -> None:
     arguments = command.split(" ")
-    if (arguments[1] != arguments[2]):
+    if (len(arguments) == 3 and arguments[0] == "cp" and arguments[1] != arguments[2]):
         with (open(get_full_path(arguments[1]), "r")
               as file_in, open(get_full_path(arguments[2]), "w") as file_out):
             if (file_in.name != file_out.name):
