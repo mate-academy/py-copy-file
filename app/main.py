4dae0126ebe6d5771faaeeedac5de87f456974f7
artem.puhach <artem.puhach@cyberpollice.gov.ua>
2023-12-16 13:16:14 +0200

    ready!

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, file_1, file_2 = command.split(" ")
+    if file_1 == file_2:
+        pass
+    else:
+        with open(file_1, "r") as source, open(file_2, "a") as target:
+            for line in source:
+                target.write(line)
