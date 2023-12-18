4c83cc8f57de1e0058e4a26eec79cb073f3e7a22
Bohdan Horbachov <bgbro.inc@gmail.com>
2023-12-18 21:07:52 +0200

    fixed errors

--- app/main.py
+++ app/main.py
@@ -1,14 +1,11 @@
 def copy_file(command: str) -> None:
-    splitted_arguments = command.split(" ")
+    splitted_arguments = command.split()
     if len(splitted_arguments) != 3:
-        raise ValueError("Incorrect comand provided")
-    command, fname_to_copy, new_fname = command.split(" ")
+        raise ValueError("Incorrect command provided")
+    command, fname_to_copy, new_fname = splitted_arguments
 
-    if not command == "cp":
-        raise ValueError("Incorrect comand provided")
-
-    if fname_to_copy == new_fname:
-        raise ValueError("Incorrect comand provided")
+    if command != "cp" or fname_to_copy == new_fname:
+        raise ValueError("Incorrect command provided")
 
     with open(fname_to_copy, "r") as file_in, open(new_fname, "w") as file_out:
         file_out.write(file_in.read())
