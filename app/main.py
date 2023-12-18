a071ee0deb6cffc5133899533830c27d507d7886
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2023-12-18 13:20:31 +0200

    fixed: created custom exceptions, checked conditions for command

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
+class FileCopyError(Exception):
+    """Raise when my source and target file are the same"""
+
+
+class WrongCommandError(Exception):
+    """Raise when user written wrong command name"""
+
+
+class WrongArgumentsNumberError(Exception):
+    """Raise when user command consists not from 3 elements"""
+
+
 def copy_file(command: str) -> None:
     cp_command, file_1, file_2 = command.split(" ")
-    if file_1 == file_2 or cp_command != "cp":
-        pass
+    if file_1 == file_2:
+        raise FileCopyError("Source and target file are the same!")
+    if cp_command != "cp":
+        raise WrongCommandError("You written wrong command, "
+                                "if you wanna copy file, use cp command")
+    if len(command.split(" ")) != 3:
+        raise WrongArgumentsNumberError("Your copy command "
+                                        "should have 3 arguments!")
     else:
         with open(file_1, "r") as source, open(file_2, "a") as target:
-            for line in source:
-                target.write(line)
+            target.write(source.read())
