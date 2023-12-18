13d2e8f9d466f2fc4897561aed2abe29930c6e68
Bohdan Horbachov <135819754+BGBroth@users.noreply.github.com>
2023-12-18 12:11:03 +0200

    added raises and len check

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 def copy_file(command: str) -> None:
+    splitted_arguments = command.split(" ")
+    if len(splitted_arguments) != 3:
+        raise ValueError("Incorrect comand provided")
     command, fname_to_copy, new_fname = command.split(" ")
 
     if not command == "cp":
-        pass
+        raise ValueError("Incorrect comand provided")
 
     if fname_to_copy == new_fname:
-        pass
+        raise ValueError("Incorrect comand provided")
 
     with open(fname_to_copy, "r") as file_in, open(new_fname, "w") as file_out:
         file_out.write(file_in.read())
