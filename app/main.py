eaa83a7a2d1cf5c1982a769b4033a424b59152f6
Bohdan Horbachov <bgbro.inc@gmail.com>
2023-12-18 21:51:59 +0200

    fixed flake8 error

--- app/main.py
+++ app/main.py
@@ -7,5 +7,6 @@ def copy_file(command: str) -> None:
     if command != "cp" or file_name_to_copy == new_file_name:
         raise ValueError("Incorrect command provided")
 
-    with open(file_name_to_copy, "r") as file_in, open(new_file_name, "w") as file_out:
+    with open(file_name_to_copy, "r") as file_in, \
+            open(new_file_name, "w") as file_out:
         file_out.write(file_in.read())
