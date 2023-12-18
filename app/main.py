2851986601471fee60aa4adc276075183408f5fd
Bohdan Horbachov <bgbro.inc@gmail.com>
2023-12-18 21:49:10 +0200

    replaced fname with file_name and new_fname with new_file_name

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ def copy_file(command: str) -> None:
     splitted_arguments = command.split()
     if len(splitted_arguments) != 3:
         raise ValueError("Incorrect command provided")
-    command, fname_to_copy, new_fname = splitted_arguments
+    command, file_name_to_copy, new_file_name = splitted_arguments
 
-    if command != "cp" or fname_to_copy == new_fname:
+    if command != "cp" or file_name_to_copy == new_file_name:
         raise ValueError("Incorrect command provided")
 
-    with open(fname_to_copy, "r") as file_in, open(new_fname, "w") as file_out:
+    with open(file_name_to_copy, "r") as file_in, open(new_file_name, "w") as file_out:
         file_out.write(file_in.read())
