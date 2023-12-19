bc2360b7207ebd4b58442fa0f94672dabaff1b94
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2023-12-19 20:20:45 +0100

    added length check of splitted command, py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    cp, file_to_copy_name, new_file_name = command
+    if len(command) == 3:
+        cp, file_to_copy_name, new_file_name = command
 
-    if cp == "cp" and not file_to_copy_name == new_file_name:
-        with (
-            open(file_to_copy_name, "r") as file_in,
-            open(new_file_name, "w") as file_out
-        ):
-            text_to_copy = file_in.read()
-            file_out.write(text_to_copy)
+        if cp == "cp" and not file_to_copy_name == new_file_name:
+            with (
+                open(file_to_copy_name, "r") as file_in,
+                open(new_file_name, "w") as file_out
+            ):
+                text_to_copy = file_in.read()
+                file_out.write(text_to_copy)
