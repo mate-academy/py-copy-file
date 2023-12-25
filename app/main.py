24edb3d174062a4960a8013dc1e5b7231543be71
Maxim Voytkevichus <m4k51m15k4m@gmail.com>
2023-12-25 15:11:47 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -2,14 +2,9 @@ def copy_file(command: str) -> None:
     """ Copy content from one file to another
     if given file names are different. """
 
-    _, copy, paste = command.split()
+    mode, copy, paste = command.split()
 
-    if copy != paste:
+    if copy != paste and mode == "cp":
         with (open(copy) as from_file,
               open(paste, "w") as paste_file):
             paste_file.write(from_file.read())
-
-
-if __name__ == "__main__":
-    copy_file("cp test.txt test1.txt")
-    print(open("test.txt").read() == open("test1.txt").read())
