1804eea74b45b409192bfdafd37953d96ac1df58
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2023-12-30 15:22:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def copy_file(command: str) -> None:
     cd, source_name, copy_name, *_ = command.split()
-    if cd != "cp":
-        return
-    if source_name == copy_name:
+    if source_name == copy_name or cd != "cp":
         return
     with (open(source_name, "r") as source,
           open(copy_name, "w") as result):
