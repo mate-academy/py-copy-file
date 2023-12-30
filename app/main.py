df26eba390c1aa408951fdd130e9faf547f04c75
Misha Purh <mishapurh@Mishas-MacBook-Air.local>
2023-12-30 10:31:41 -0700

    Solution

--- app/main.py
+++ app/main.py
@@ -3,13 +3,11 @@ import shutil
 
 def copy_file(command: str) -> None:
     command_file = command.split()
+
+    if len(command_file) != 3 or command_file[0] != "cp":
+        print("Wrong command!")
+
     src_f = command_file[1]
     dest_f = command_file[2]
     if src_f != dest_f:
         shutil.copy2(src_f, dest_f)
-    else:
-        return
-
-
-if __name__ == "__main__":
-    copy_file("cp file.txt file-copy.txt")
