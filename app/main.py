8793596fc9ddcd88e5c00af8336c14c4ce877ac8
Misha Purh <mishapurh@Mishas-MacBook-Air.local>
2023-12-30 00:41:10 -0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import shutil
+def copy_file(command: str) -> None:
+    command_file = command.split()
+    src_f = command_file[1]
+    dest_f = command_file[2]
+    if src_f != dest_f:
+        shutil.copy2(src_f, dest_f)
+    else:
+        return
+
+
+
+if __name__ == "__main__":
+    copy_file("cp file.txt file-copy.txt")
