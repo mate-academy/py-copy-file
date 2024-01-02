6cef32fdbb4325e4e8259ee3dac5f133d2d6d15a
Valeriia Danyliuk <bladeryz@gmail.com>
2024-01-03 01:50:05 +0200

    Implemented "copy_file" function. However, I haven't found any tests in the forked directory for some reason...

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    key_elements = command.split()
+
+    if key_elements[0] != "cp":
+        raise ValueError("Invalid command. Maybe you meant 'cp...'?")
+    if key_elements[1] == key_elements[2]:
+        raise ValueError("Can't create a new file with the same name.")
+
+    with (open(f"{key_elements[1]}", "r") as original_file,
+          open(f"{key_elements[2]}", "w") as copied_file):
+        copied_file.write(original_file.read())
