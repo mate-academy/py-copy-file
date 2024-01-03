74485d59448ded4aead66f2ce620f22c62316707
Valeriia Danyliuk <bladeryz@gmail.com>
2024-01-03 13:18:40 +0200

    Edited the name and provided a check for the arguments.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ def copy_file(command: str) -> None:
     command_arguments = command.split()
 
     if len(command_arguments) != 3:
-        raise ValueError("Invalid command. The right format is 'cp [original_file] [copied_file]'.")
+        raise ValueError("Invalid command. The right format is"
+                         " 'cp [original_file] [copied_file]'.")
     if command_arguments[0] != "cp":
         raise ValueError("Invalid command. Maybe you meant 'cp...'?")
     if command_arguments[1] == command_arguments[2]:
