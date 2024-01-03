c4ccf393a7e70ef61fc12acb84e24bf8946284c8
Valeriia Danyliuk <bladeryz@gmail.com>
2024-01-03 13:40:26 +0200

    Used unpacking to make the code clear and concise.

--- app/main.py
+++ app/main.py
@@ -3,12 +3,16 @@ def copy_file(command: str) -> None:
 
     if len(command_arguments) != 3:
         raise ValueError("Invalid command. The right format is"
-                         " 'cp [original_file] [copied_file]'.")
-    if command_arguments[0] != "cp":
-        raise ValueError("Invalid command. Maybe you meant 'cp...'?")
-    if command_arguments[1] == command_arguments[2]:
+                         " 'cp [source_file] [destination_file]'.")
+
+    command, source_file, destination_file = command_arguments
+
+    if command != "cp":
+        raise ValueError("Invalid command. Maybe you meant 'cp ...'?")
+
+    if source_file == destination_file:
         raise ValueError("Can't create a new file with the same name.")
 
-    with (open(f"{command_arguments[1]}", "r") as original_file,
-          open(f"{command_arguments[2]}", "w") as copied_file):
+    with (open(f"{source_file}", "r") as original_file,
+          open(f"{destination_file}", "w") as copied_file):
         copied_file.write(original_file.read())
