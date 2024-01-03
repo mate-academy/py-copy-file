8394f17fdf097708f9d80c5a3ef936845d140426
Valeriia Danyliuk <bladeryz@gmail.com>
2024-01-03 13:16:39 +0200

    Fixed the name of the value, also provided a check whether command has three arguments

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 def copy_file(command: str) -> None:
-    key_elements = command.split()
+    command_arguments = command.split()
 
-    if key_elements[0] != "cp":
+    if len(command_arguments) != 3:
+        raise ValueError("Invalid command. The right format is 'cp [original_file] [copied_file]'.")
+    if command_arguments[0] != "cp":
         raise ValueError("Invalid command. Maybe you meant 'cp...'?")
-    if key_elements[1] == key_elements[2]:
+    if command_arguments[1] == command_arguments[2]:
         raise ValueError("Can't create a new file with the same name.")
 
-    with (open(f"{key_elements[1]}", "r") as original_file,
-          open(f"{key_elements[2]}", "w") as copied_file):
+    with (open(f"{command_arguments[1]}", "r") as original_file,
+          open(f"{command_arguments[2]}", "w") as copied_file):
         copied_file.write(original_file.read())
