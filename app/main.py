245d85dee68eb3c62ffc8412231ecda24ca36f2a
Yura Push <youdeimos1@gmail.com>
2024-01-04 17:19:41 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp, file1, file2 = command.split()
+
+    if file1 == file2:
+        pass
+
+    with open(file1, "r") as file_in, open(file2, "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
+
+
+copy_file("cp file.txt new_file.txt")
