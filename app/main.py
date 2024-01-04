08ec3cf4d7e6537907bc0e17b6d0d1c680e21eab
Andrii Zubchenko <dissom1987@gmail.com>
2024-01-04 11:02:58 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -2,12 +2,9 @@ def copy_file(command: str) -> None:
     way = command.split()
 
     if len(way) == 3 and way[1] != way[2]:
-        try:
-            with (open(way[1], "r") as file_in,
-                open(way[2], "w") as file_out):
-                file_out.write(file_in.read())
-        except Exception as a:
-            print("Error copying file", a)
+        with (open(way[1], "r") as file_in,
+              open(way[2], "w") as file_out):
+            file_out.write(file_in.read())
 
 
 if __name__ == "__main__":
