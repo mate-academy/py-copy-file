1a32edb9667fe85a9e392eca04d06413c5f3771e
Andrii Zubchenko <dissom1987@gmail.com>
2024-01-04 11:04:35 +0200

    last changes

--- app/main.py
+++ app/main.py
@@ -5,8 +5,3 @@ def copy_file(command: str) -> None:
         with (open(way[1], "r") as file_in,
               open(way[2], "w") as file_out):
             file_out.write(file_in.read())
-
-
-if __name__ == "__main__":
-    copy_file("cp file.txt new_file.txt")
-    # open("file.txt").read() == open("new_file.txt").read()
