d42e06b25b24a27683a286ec6ec2c680f3434716
Yura Push <youdeimos1@gmail.com>
2024-01-05 19:43:59 +0200

    -added check command

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     cp, file1, file2 = command.split()
 
-    if file1 == file2:
+    if file1 == file2 and not command.startswith(cp):
         pass
 
     with open(file1, "r") as file_in, open(file2, "w") as file_out:
