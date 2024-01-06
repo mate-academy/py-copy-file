2fa92697885cad010bf343728e3b40ba6bde2239
Yura Push <youdeimos1@gmail.com>
2024-01-06 14:39:09 +0200

    -removed files, call to function -added check if command is correct

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def copy_file(command: str) -> None:
+    if len(command.split()) < 3:
+        pass
+
     cp, file1, file2 = command.split()
 
-    if file1 == file2 and not command.startswith(cp):
+    if file1 == file2 or not command.startswith(cp):
         pass
 
     with open(file1, "r") as file_in, open(file2, "w") as file_out:
         content = file_in.read()
         file_out.write(content)
-
-
-copy_file("cp file.txt new_file.txt")
