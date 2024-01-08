238d83f9f63319974a25780c0e336a071a85e877
Veronika <veronikaboeva935@gmail.com>
2024-01-08 21:59:32 +0100

    Check

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split()
+    if split_command[0] == "cp":
+        if split_command[1] != split_command[2]:
+            with (open(split_command[1], "r") as file1,
+                  open(split_command[2], "w") as file2):
+                file2.write(file1.read())
