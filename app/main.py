f201a8a3e4c5ec64b2850c5158786b704891b108
Denys Sidorov <zondayttop1ggg@gmail.com>
2024-01-08 22:35:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
+import shutil
+
+
 def copy_file(command: str) -> None:
     command_split = command.split()
-    file_name_1 = command_split[1]
-    file_name_2 = command_split[2]
-
-    if file_name_1 != file_name_2:
-        with (
-            open(file_name_1, "r") as file_in,
-            open(file_name_2, "w") as file_out
-        ):
-            file_out.write(file_in.read())
+    if len(command_split) == 3 and command_split[0] == "cp":
+        file_name_1, file_name_2 = command_split[1], command_split[2]
+        if file_name_1 != file_name_2:
+            shutil.copyfile(file_name_1, file_name_2)
+    else:
+        print("Invalid command. Usage: cp <source_file> <destination_file>")
