4eabe9a0687c81aab66ce773faad8d55173737ab
Artur Poltser <polcerartur@gmail.com>
2024-01-08 09:57:19 +0200

    Fixed unpacking

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
-    part = command.split()
+    cp_command, file_name, new_file_name = command.split()
 
-    if len(part) != 3 or part[0] != "cp" or part[1] == part[2]:
+    if cp_command != "cp" or file_name == new_file_name:
         return
 
-    with open(part[1], "r") as file_in, open(part[2], "w") as file_out:
+    with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
         file_out.write(file_in.read())
