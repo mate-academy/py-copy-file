635817a82876edf1d733f80bbac5b264b35c0c1e
Vladyslav Pliashechnyk <vlad.pliashechnyk@gmail.com>
2024-01-08 19:33:57 +0200

    py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, current_file_name, new_file_name = command.split()
+    if current_file_name != new_file_name and command == "cp":
+        with (
+            open(current_file_name, "r") as current_file,
+            open(new_file_name, "w") as new_file
+        ):
+            new_file.write(current_file.read())
