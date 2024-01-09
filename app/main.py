fe0a39ce9da905c273a1684c007b2768b2409c3a
Veronika <veronikaboeva935@gmail.com>
2024-01-09 20:55:16 +0100

    Solution 09.01.2024

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     parts = command.split()
-    if len(parts) == 3 and parts[1] != parts[2]:
-        with (open(parts[1], "r") as file1,
-              open(parts[2], "w") as file2):
-            file2.write(file1.read())
+    if len(parts) == 3 and parts[1] != parts[2] and parts[0] == "cp":
+        with (open(parts[1], "r") as source,
+              open(parts[2], "w") as destination):
+            destination.write(source.read())
