2aaf64669bba66c236ebf900789622f39ab01dd4
el chibuk <chebukin404@gmail.com>
2024-01-09 17:44:43 +0200

    optimized

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 def copy_file(command: str) -> None:
-    command_parts = command.split()
-    orig_name = command_parts[1]
-    copy_name = command_parts[2]
+    cp, orig_name, copy_name = command.split()
 
     if orig_name != copy_name:
         with (open(orig_name, "r") as orig_file,
               open(copy_name, "w") as copy_file):
-            for line in orig_file:
-                copy_file.write(line)
+            copy_file.write(orig_file.read())
