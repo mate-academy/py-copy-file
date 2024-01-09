4df3403d1d50253ea86e7a8fa2e03284a444e56f
el chibuk <chebukin404@gmail.com>
2024-01-09 17:40:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_parts = command.split()
+    orig_name = command_parts[1]
+    copy_name = command_parts[2]
+
+    if orig_name != copy_name:
+        with (open(orig_name, "r") as orig_file,
+              open(copy_name, "w") as copy_file):
+            for line in orig_file:
+                copy_file.write(line)
