34a15cf8a28f179e64f7e2f081cf4a765683f0be
el chibuk <chebukin404@gmail.com>
2024-01-10 12:10:03 +0200

    add one more check statement

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     cp, orig_name, copy_name = command.split()
 
-    if orig_name != copy_name:
+    if orig_name != copy_name and cp == "cp":
         with (open(orig_name, "r") as orig_file,
               open(copy_name, "w") as copy_file):
             copy_file.write(orig_file.read())
