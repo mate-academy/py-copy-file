683a24f7c8f607ee977e296a668f4c3827cc9428
Dariia Shostak <davorobii@gmail.com>
2024-01-10 19:19:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
-    ls_command = command.split()
-    command_name, file_name, new_file_name = (
-        ls_command[0], ls_command[1], ls_command[2])
-    if file_name != new_file_name and command_name == "cp":
-        with (open(file_name, "r") as source,
-              open(new_file_name, "w") as new_file):
-            new_file.write(source.read())
+    parts = command.split()
+    if len(parts) == 3:
+        command_name, file_name, new_file_name = parts
+        if file_name != new_file_name and command_name == "cp":
+            with (open(file_name, "r") as source,
+                  open(new_file_name, "w") as new_file):
+                new_file.write(source.read())
