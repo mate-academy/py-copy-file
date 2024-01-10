6974b2eda3eab0d069a83fd61b2cff1217dace65
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-01-10 21:26:40 +0100

    fix task

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     parts = command.split()
     if len(parts) == 3:
-        if parts[1] != parts[2] and parts[0] == "cp":
+        if len(parts) == 3 and parts[1] != parts[2] and parts[0] == "cp":
             with (open(parts[1], "r") as file_in,
                   open(parts[2], "w") as file_out):
                 file_content = file_in.read()
