4c7847c02f62b9c0873180ec0672ee6bd293ca0a
Dariia Shostak <davorobii@gmail.com>
2024-01-10 16:32:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    ls_command = command.split(" ")
+    if ls_command[1] != ls_command[2] and ls_command[0] == "cp":
+        with (open(ls_command[1], "r") as source,
+              open(ls_command[2], "w") as new_file):
+            new_file.write(source.read())
