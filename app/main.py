a429fdf4c6f5d505d70ba8a23bf0b41866d8cc35
el chibuk <chebukin404@gmail.com>
2024-01-10 18:41:55 +0200

    fixed variable name

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
-    cp, orig_name, copy_name = command.split()
+    command, orig_name, copy_name = command.split()
 
-    if orig_name != copy_name and cp == "cp":
+    if orig_name != copy_name and command == "cp":
         with (open(orig_name, "r") as orig_file,
               open(copy_name, "w") as copy_file):
             copy_file.write(orig_file.read())
