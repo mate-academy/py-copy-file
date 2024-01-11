4e0308b109e1951b885da3aa77291080a826a891
Max <bidun.max@gmail.com>
2024-01-11 14:48:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(text: str) -> None:
+    if len(text.split()) == 3:
+        text = text.split()
+        if text[0] == "cp" and text[1] != text[2]:
+            with open(text[1], "r") as file_in, open(text[2], "w") as file_out:
+                file_out.write(file_in.read())
