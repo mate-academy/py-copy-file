b844fc6aa4a1fe0157c71073e2fc642af2e55f0a
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-01-11 16:02:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    operation, source_file_name, new_file_name = command.split()
+    if operation != "cp":
+        return
+    with open(source_file_name, "r") as source:
+        with open(new_file_name, "w") as new_file:
+            new_file.writelines(source.read())
