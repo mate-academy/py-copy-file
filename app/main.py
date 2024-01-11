4326e9be8dd75c72008a704ccfde00ee4c67e3ad
Kate Chernyavska <ccarts.kpi@gmail.com>
2024-01-11 12:03:02 +0200

    formatted long line

--- app/main.py
+++ app/main.py
@@ -3,5 +3,6 @@ def copy_file(command: str) -> None:
         cp, file_from, file_to = command.split(" ")
 
         if file_from != file_to and cp == "cp":
-            with open(file_to, "r") as file_in, open(file_from, "w") as file_out:
+            with (open(file_to, "r") as file_in,
+                  open(file_from, "w") as file_out):
                 file_out.write(file_in.read())
