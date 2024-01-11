842e867dadbb3b8ba1717d84ab45ec4ecc374937
Kate Chernyavska <ccarts.kpi@gmail.com>
2024-01-11 12:00:44 +0200

    added checking if the line has 3 word

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
-    cp, file_from, file_to = command.split(" ")
+    if len(command) == 3:
+        cp, file_from, file_to = command.split(" ")
 
-    if file_from != file_to and cp == "cp":
-        with open(file_to, "r") as file_in, open(file_from, "w") as file_out:
-            file_out.write(file_in.read())
+        if file_from != file_to and cp == "cp":
+            with open(file_to, "r") as file_in, open(file_from, "w") as file_out:
+                file_out.write(file_in.read())
