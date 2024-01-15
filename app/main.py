0f9ecc643d66e3f2a6a6cd3e99f54878ec48416d
xrkxdd <nazar.mc28@gmail.com>
2024-01-15 09:24:29 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 def copy_file(command: str) -> None:
-    command, old_file, new_file = command.split()
+    cp_command, source_file, destination_file = command.split()
 
-    if old_file == new_file or command != "cp":
-        return
-
-    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-        file_out.write(file_in.read())
+    if cp_command == "cp" and source_file != destination_file:
+        with (open(source_file, "r") as file_in, open(destination_file, "w")
+                as file_out):
+            file_out.write(file_in.read())
 
 
 if __name__ == "__main__":
