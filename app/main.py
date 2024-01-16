cc3e36ad4f92146ae3cfcf3fe8e0fd418559f007
Arsen Rudnytskyy <acikz.33@google.com>
2024-01-16 11:37:45 +0100

    Fix unpacking values and command condition

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def copy_file(command: str) -> None:
-    files = command.split(" ")
-    old_file = files[1]
-    new_file = files[2]
+    input_command, old_file, new_file = command.split()
 
-    if old_file != new_file:
+    if old_file != new_file and input_command == "cp":
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             new_file = file_out.write(file_in.read())
