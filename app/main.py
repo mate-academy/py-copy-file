f2d77ad9fdfbca5cbb8bd8e086943a2e27e7860d
Arsen Rudnytskyy <acikz.33@google.com>
2024-01-16 15:41:01 +0100

    Check command.split length

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 def copy_file(command: str) -> None:
-    input_command, old_file, new_file = command.split()
+    files = command.split()
+    if len(files) != 3:
+        raise Exception("The command line must have 3 parameters")
+    input_command, old_file, new_file = files
 
     if old_file != new_file and input_command == "cp":
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
