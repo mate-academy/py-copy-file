7cbb6170d6e9e30576ebceadd598b79b9f63965a
MaxymChyncha <max.chyncha@gmail.com>
2024-01-17 17:09:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_str_into_list = command.split()
+    old_file, new_file = command_str_into_list[1], command_str_into_list[2]
+    if old_file != new_file:
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
