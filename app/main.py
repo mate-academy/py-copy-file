03cf9cf9405b799ac5932e4c749be53c46e48bfa
angelzagniy <angelzagniy@gmail.com>
2024-01-17 23:07:12 +0200

    make lines shorter

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 def copy_file(command: str) -> None:
     file_names = command[3:].split()
-    if command[0:2] == "cp" and command.count(" ") == 2 and file_names[0] != file_names[1]:
-        with open(file_names[0], "r") as file_in, open(file_names[1], "w") as file_out:
+    if (
+            command[0:2] == "cp"
+            and command.count(" ") == 2
+            and file_names[0] != file_names[1]
+    ):
+        with (
+            open(file_names[0], "r") as file_in,
+            open(file_names[1], "w") as file_out
+        ):
             file_out.write(file_in.read())
