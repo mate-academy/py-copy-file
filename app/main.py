1a4bedded4d6ca32bb718b9dfd9383c9125e4cea
angelzagniy <angelzagniy@gmail.com>
2024-01-17 23:04:48 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_names = command[3:].split()
+    if command[0:2] == "cp" and command.count(" ") == 2 and file_names[0] != file_names[1]:
+        with open(file_names[0], "r") as file_in, open(file_names[1], "w") as file_out:
+            file_out.write(file_in.read())
