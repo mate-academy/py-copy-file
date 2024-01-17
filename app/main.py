501189f1268bbb5d748d6178c5efd40be2c3a09b
Julia Okhrymenko <Ohrimenko15@gmail.com>
2024-01-17 16:11:03 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    parts_of_command = command.split()
+    if len(parts_of_command) != 3 or parts_of_command[0] != "cp":
+        return
+
+    first_file, second_file = parts_of_command[1], parts_of_command[2]
+
+    if first_file == second_file:
+        return
+
+    with open(first_file, "r") as file_in, open(second_file, "w") as file_out:
+        file_out.write(file_in.read())
