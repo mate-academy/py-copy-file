06960fbefa21e7e63715dad074f673ab4e922b68
angelzagniy <angelzagniy@gmail.com>
2024-01-18 21:32:41 +0200

    use unpacking instead of indexing for command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     if len(command) > 3:
-        file_names = command[3:].split()
+        _, *file_names = command.split()
         if (
                 command[0:2] == "cp"
                 and command.count(" ") == 2
