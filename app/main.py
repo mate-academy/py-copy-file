76d4e3e360eadae5f0f07a04d33dccd1c3502705
MaxymChyncha <max.chyncha@gmail.com>
2024-01-18 17:12:49 +0200

    Added checking for linux_command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy_file(command: str) -> None:
-    command_str_into_list = command.split()
-    old_file, new_file = command_str_into_list[1], command_str_into_list[2]
-    if old_file != new_file:
+    linux_command, old_file, new_file = command.split()
+    if old_file != new_file and linux_command == "cp":
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             file_out.write(file_in.read())
