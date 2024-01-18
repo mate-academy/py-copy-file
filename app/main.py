1dc17e97fd3abed7c1368b7ad38ee6da74f680aa
yes <yes@yess-MacBook-Pro.local>
2024-01-18 04:37:02 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import os
+import sys
+
+
+def copy_file(command: str) -> None:
+    os.chdir(os.path.dirname(sys.argv[0]))
+    items = command.split(" ")
+    reference_name, copy_name = items[1], items[2]
+
+    if reference_name == copy_name:
+        return
+    if copy_name in os.listdir(os.getcwd()):
+        return
+    else:
+        open(copy_name, "x").write(open(reference_name, "r").read())
