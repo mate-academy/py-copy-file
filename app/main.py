9bc6d7ecf4b24d28770b3e5b9b35eee3a5e90309
IvanGuculyak <ivan.guculyak@gmail.com>
2024-01-19 19:16:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_vile(command: str) -> None:
+    text = command.split()
+    if text[0] == "cp" and text[1] != text[2]:
+        with (
+            open(text[1], "r") as source,
+            open(text[2], "w") as new_file
+        ):
+            new_file.write(source.read())
