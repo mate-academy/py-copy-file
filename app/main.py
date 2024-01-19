21740fc14ef946d79094eb3dd49ad93bdb4284bd
Ant <u123@ua.fm>
2024-01-19 14:15:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,10 +2,6 @@ def copy_file(cp_cmd: str) -> None:
     arg = cp_cmd.split(" ")
     if arg[0] != "cp" or arg[1] == arg[2]:
         return
-    with open(arg[1], "r") as fin, open(arg[2], "w") as fout:
-        content = fin.read()
-        fout.write(content)
-
-
-# =====
-copy_file("cp main.py example.tst")
+    with open(arg[1], "r") as file_in, open(arg[2], "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
