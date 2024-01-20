2d11fba4c63565073b62d5cc1bc3e85e8a9bb8ba
Maksym Nimych <maksimnimitch@gmail.com>
2024-01-20 17:22:51 +0200

    Add check & informative error

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
+class CopyFileError(Exception):
+    pass
+
+
 def copy_file(command: str) -> None:
-    instruction, f_in_name, f_out_name = command.split()
+    command_split = command.split()
+    if len(command_split) != 3:
+        raise CopyFileError("Invalid command input")
+
+    instruction, f_in_name, f_out_name = command_split
     if instruction != "cp" or f_in_name == f_out_name:
-        return
+        raise CopyFileError("Invalid command or file name")
 
     with open(f_in_name, "r") as file_in, open(f_out_name, "w") as file_out:
         file_out.write(file_in.read())
