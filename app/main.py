1da54f45e53d8ab58d92c22d425a60a1236cf4a7
Maksym Nimych <maksimnimitch@gmail.com>
2024-01-20 15:14:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    instruction, f_in_name, f_out_name = command.split()
+    if instruction != "cp" or f_in_name == f_out_name:
+        return
+
+    with open(f_in_name, "r") as file_in, open(f_out_name, "w") as file_out:
+        file_out.write(file_in.read())
