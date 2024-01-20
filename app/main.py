f9bc31c03219070e9d0249ed238d168fb3908daf
barabarinov <barabarinov@gmail.com>
2024-01-20 19:54:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) == 3 and parts[0] == "cp":
+        source_file, copied_file = parts[1], parts[2]
+
+        if source_file != copied_file:
+            with (
+                open(source_file, "r") as read_file,
+                open(copied_file, "w") as write_file,
+            ):
+                write_file.write(read_file.read())
+
+
+if __name__ == "__main__":
+    copy_file("cp test.txt new_test.txt")
