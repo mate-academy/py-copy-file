b55cf436eba16f8b82258e9487dfa6c45e65a5f0
Mariia Movchan <mariia.sergeevna.k@gmail.com>
2024-01-22 18:55:19 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str):
+    args = command.split()
+    if len(args) == 3 and args[0] == "cp" and args[1] != args[2]:
+        with open(args[1], "r") as file_in, open(args[2], "w") as file_out:
+            file_out.write(file_in.read())
