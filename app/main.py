d250c889570f7dfa2033b0d9bd45b1736277d3d8
Bohdan Cherniavskyi <b.cherniavskyi@gmail.com>
2024-01-23 00:08:33 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+class ErrorCopyFile(Exception):
+    pass
+
+
+def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        raise ErrorCopyFile("Error file copy")
+
+    file_from = command.split()[1]
+    file_to = command.split()[2]
+
+    with open(file_from, "r") as file_in, open(file_to, "w") as file_out:
+        file_out.write(file_in.read())
