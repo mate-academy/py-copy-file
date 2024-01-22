56de936f5ed12e67f1bf744a46447731224fc2c5
barabarinov <barabarinov@gmail.com>
2024-01-22 14:43:45 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 def copy_file(command: str) -> None:
-    parts = command.split()
+    _, source_file, copied_file = command.split()
 
-    if len(parts) == 3 and parts[0] == "cp":
-        source_file, copied_file = parts[1], parts[2]
-
-        if source_file != copied_file:
-            with (
-                open(source_file, "r") as read_file,
-                open(copied_file, "w") as write_file,
-            ):
-                write_file.write(read_file.read())
+    if source_file != copied_file:
+        with (
+            open(source_file) as read_file,
+            open(copied_file, "w") as write_file,
+        ):
+            write_file.write(read_file.read())
 
 
 if __name__ == "__main__":
