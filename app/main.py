ee33ca076871101153fdb3d2fc2879fdb23f4677
barabarinov <barabarinov@gmail.com>
2024-01-22 17:15:18 +0200

    fixed variable

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 def copy_file(command: str) -> None:
-    _, source_file, copied_file = command.split()
+    operation, source_file, copied_file = command.split()
 
-    if source_file != copied_file:
-        with (
-            open(source_file) as read_file,
-            open(copied_file, "w") as write_file,
-        ):
-            write_file.write(read_file.read())
+    if len(command) == 3 and operation == "cp":
+        if source_file != copied_file:
+            with (
+                open(source_file) as read_file,
+                open(copied_file, "w") as write_file,
+            ):
+                write_file.write(read_file.read())
 
 
 if __name__ == "__main__":
