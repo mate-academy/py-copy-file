63ae588f1e0f6a61a0dd6aa3dd963153541bd1ad
Mariia Movchan <mariia.sergeevna.k@gmail.com>
2024-01-22 18:57:05 +0100

    Add type annotation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def copy_file(command: str):
+def copy_file(command: str) -> None:
     args = command.split()
     if len(args) == 3 and args[0] == "cp" and args[1] != args[2]:
         with open(args[1], "r") as file_in, open(args[2], "w") as file_out:
