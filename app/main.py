3964cb9b23aa4a1fbf4d4783f300bbbaa74c4137
Mykola <mykola22dubyna@gmail.com>
2024-01-23 19:04:06 +0200

    fix some truble

--- app/main.py
+++ app/main.py
@@ -2,5 +2,5 @@ def copy_file(command: str) -> None:
     command = command.split()
     if command[1] == command[2] or command[0] != "cp":
         return
-    with open(command[1], "r") as source, open(command[1], "w") as source_copy:
-        source_copy.writelines(source.readlines())
+    with open(command[1], "r") as source, open(command[2], "w") as source_copy:
+        source_copy.write(source.read())
