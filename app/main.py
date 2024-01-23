65f695c74628e4cb5195744015d66e889374efe6
yulias97 <yulkastetsko@gmail.com>
2024-01-23 20:34:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    filenames = command.split()
+    source_filename = filenames[1]
+    destination_filename = filenames[2]
+    if source_filename == destination_filename:
+        return
+    with open(source_filename, "r") as file_in, \
+            open(destination_filename, "w") as file_out:
+        shutil.copyfileobj(file_in, file_out)
