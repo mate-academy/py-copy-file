285ac89d58f004094adb98158f1ad18ee7bb7004
Mykola <mykola22dubyna@gmail.com>
2024-01-23 18:48:11 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    if command[1] == command[2] or command[0] != "cp":
+        return
+    with open(command[1], "r") as source, open(command[1], "w") as source_copy:
+        source_copy.writelines(source.readlines())
