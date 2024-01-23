dd9dd4d9a32977906e371a36841fe141b963ef09
Artem Chernyshov <achernyshovv1@gmail.com>
2024-01-23 15:54:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str):
+    split_command = command.split()
+    first_part = split_command[1]
+    second_part = split_command[2]
+
+    if first_part == second_part:
+        return
+
+    with open(first_part, "r") as file_in, open(second_part, "w") as file_out:
+        file_out.write(file_in.read())
