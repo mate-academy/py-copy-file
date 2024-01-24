ff386348024de684c44fcf4ba75efab3ba227a2b
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-01-24 12:45:42 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    components = command.split()
+    old_file = components[1]
+    file_copy = components[2]
+    if old_file == file_copy:
+        return
+
+    with open(old_file, "r") as file_in, open(file_copy, "w") as file_out:
+        file_out.write(file_in.read())
+        return
