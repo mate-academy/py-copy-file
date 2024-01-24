3066535ae330b17d265c15ef62682c24974698d3
Yevhen <evgeniy.playid@icloud.com>
2024-01-24 19:14:45 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
-# write your code here
 def copy_file(command: str) -> None:
-    command = command.split()
-    file_name = command[1]
-    new_file_name = command[2]
-    if file_name != new_file_name:
+    cp, file_name, new_file_name = command.split()
+    if cp == "cp" and file_name != new_file_name:
         with (
             open(file_name, "r") as file_in,
             open(new_file_name, "w") as file_out
