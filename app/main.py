b1ed3b4472f39928b45bb54dd007b19354411197
Kyrylo Storozhenko <busterakel@gmail.com>
2024-01-24 19:21:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 def copy_file(command: str) -> None:
-    command_parts = command.split()
-    cp_command, source, destination_file = (command_parts[0],
-                                            command_parts[1],
-                                            command_parts[2])
+    cp_command, source, destination_file = command.split()
     if cp_command != "cp" or source == destination_file:
         print("Command is not 'cp' or source "
               "and destination files are the same.")
@@ -11,3 +8,6 @@ def copy_file(command: str) -> None:
     with open(source, "r") as file_in, open(destination_file, "w") as file_out:
         file_out.write(file_in.read())
     print(f"Copied contents from {source} to {destination_file}")
+
+
+copy_file("cp file.txt new_file.txt")
