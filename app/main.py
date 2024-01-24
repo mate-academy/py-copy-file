92cc71d2ced76ebdf1ccd48c8d4ccaa2aa642aec
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-01-24 14:22:39 +0200

    'Add_check'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 def copy_file(command: str) -> None:
     components = command.split()
-    old_file = components[1]
-    file_copy = components[2]
-    if old_file == file_copy:
-        return
-
-    with open(old_file, "r") as file_in, open(file_copy, "w") as file_out:
-        file_out.write(file_in.read())
-        return
+    if len(components) == 3:
+        cmd, source_path, destination_path = components
+        if source_path != destination_path and cmd == "cp":
+            with open(source_path, "r") as file_in, \
+                    open(destination_path, "w") as file_out:
+                file_out.write(file_in.read())
+    return
