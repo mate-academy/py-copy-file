3705d9140ae669d4b1149fa302c3a193912edb74
Yevhen <evgeniy.playid@icloud.com>
2024-01-24 15:02:30 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
 # write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    file_name = command[1]
+    new_file_name = command[2]
+    if file_name != new_file_name:
+        with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
+            content = file_in.read()
+            file_out.write(content)
