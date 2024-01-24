d4e37764b558d8d75ead4db33a21122494e27930
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-01-24 18:09:18 +0200

    Sol

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+
+    if (command[1] != command[2]) and command[0] == "cp":
+        with (open(command[1], "r") as old_file, open(command[2], "w") as new_file):
+            content = old_file.read()
+            new_file.write(content)
