abe1878370fc6a76c338a88e003716897a5f2be8
Nataliia Korzh <clarioruberia@gmail.com>
2024-01-24 16:17:38 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ def copy_file() -> None:
     command = input("Enter command: ")
     parts = command.split()
 
+    if len(parts) != 3 or parts[0] != "cp":
+        raise ValueError("Error: Invalid command format. "
+                         "Please use 'cp file copy_file'")
+
     with (open(parts[1], "r") as file_origin,
           open(parts[2], "w") as file_copy):
 
@@ -11,13 +15,7 @@ def copy_file() -> None:
         while True:
 
             if parts[1] == parts[2]:
-                print("Copy cannot have the same name!")
-                break
-
-            if len(parts) != 3 or parts[0] != "cp":
-                print("Error: Invalid command format. "
-                      "Please use 'cp file copy_file'")
-                return
+                raise NameError("Copy cannot have the same name!")
 
             file_copy.write(text)
 
