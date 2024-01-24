3249b248ad6e8669ca90ab543d5afad5b0e0b25f
Tymofii Skopenko <youmustdeth@gmail.com>
2024-01-24 19:18:06 +0200

    change oper

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def copy_file(command: str) -> None:
-    oper, input_file, output_file = command.split()
-    if input_file != output_file and oper == "cp":
+    operator, input_file, output_file = command.split()
+    if input_file != output_file and operator == "cp":
         try:
 
             with (open(input_file, "r") as file_in,
                   open(output_file, "w") as file_out):
                 file_out.write(file_in.read())
         except FileNotFoundError:
-            print(f"File {file_in} not found.")
+            print(f"File {input_file} not found.")
         except Exception as error:
             print(f"An error occurred: {error}")
