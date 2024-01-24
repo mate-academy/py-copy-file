ddff631cd191b903cae9f7b397e742caaa1a6a47
yulias97 <yulkastetsko@gmail.com>
2024-01-24 12:47:16 +0200

    Fix solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy_file(command: str) -> None:
     cp, origin_name, copy_name = command.split()
-    if cp != "cp" or origin_name == copy_name:
-        return
-    with open(origin_name, "r") as file_in, open(copy_name, "w") as file_out:
-        file_out.write(file_in.read())
+    if cp == "cp" and origin_name != copy_name:
+        with open(origin_name, "r") as file_in, open(copy_name, "w") as file_out:
+            file_out.write(file_in.read())
