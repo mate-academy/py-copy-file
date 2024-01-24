5ebbddc1f0e2788be9e48532865b3a9a8dca3956
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-01-24 12:10:41 +0200

    Changed cp into command, added check for input command

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 def copy_file(command: str) -> None:
     try:
-        cp, main_file, for_copy_file = command.split()
+        command, main_file, for_copy_file = command.split()
     except ValueError:
         return
-
-    if main_file == for_copy_file:
+    if command != "cp" or main_file == for_copy_file:
         return
 
     with open(main_file, "r") as source, open(for_copy_file, "w") as copy:
