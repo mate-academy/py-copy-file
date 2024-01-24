c9dbe218ecfec9c6fa22b2021252fa2c4b7e38f3
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-01-24 12:42:47 +0200

    Removed return, changed cp check

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ def copy_file(command: str) -> None:
         command, main_file, for_copy_file = command.split()
     except ValueError:
         return
-    if command != "cp" or main_file == for_copy_file:
-        return
 
-    with open(main_file, "r") as source, open(for_copy_file, "w") as copy:
-        copy.write(source.read())
+    if command == "cp" and main_file != for_copy_file:
+        with open(main_file, "r") as source, open(for_copy_file, "w") as copy:
+            copy.write(source.read())
