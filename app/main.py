c0d65d186a6beced938d856b399bc127b1399111
unknown <artem.sapiens@gmail.com>
2024-01-24 18:02:11 +0200

    'Fix'

--- app/main.py
+++ app/main.py
@@ -8,6 +8,6 @@ def copy_file(command: str) -> None:
         if source_file == destination_file:
             return
 
-        with open(source_file, "r") as file_in:
-            with open(destination_file, "w") as file_out:
-                file_out.write(file_in.read())
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
+            file_out.write(file_in.read())
