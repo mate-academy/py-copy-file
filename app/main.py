18e1753ba2086b648cdd51c12cfbac16f2913008
Maksym Operchuk <operlast529@gmail.com>
2024-01-24 14:54:14 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 def copy_file(command: str) -> None:
-
     action, file_name, new_file_name = command.split(" ")
 
-    if file_name != new_file_name and action == "cd":
-        with (
-            open(file_name, "r") as file_in,
-            open(new_file_name, "w") as file_out
-        ):
-            file_content = file_in.readlines()
+    if file_name == new_file_name and action != "cd":
+        return
 
-            for line in file_content:
-                file_out.write(line)
+    with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
+        file_content = file_in.readlines()
+        for line in file_content:
+            file_out.write(line)
