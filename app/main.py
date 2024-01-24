deadf83e470a793ba1e2a0a622f8077708f8112d
Maksym Operchuk <operlast529@gmail.com>
2024-01-24 14:43:00 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str):
+    action, file_name, new_file_name = command.split(" ")
+
+    if file_name == new_file_name and action != "cd":
+        return None
+
+    with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
+        file_content = file_in.readlines()
+        for line in file_content:
+            file_out.write(line)
