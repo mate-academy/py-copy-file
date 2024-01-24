1ae28b078ac390404415e87110d5a4f8b0d94564
Ihor Bondarenko <igorb6766@gmail.com>
2024-01-24 17:45:42 +0200

    implemented copy_file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp, file_name, new_file_name = command.split()
+    if cp != "cp":
+        raise ValueError(f"Command not found: {cp}")
+    if file_name == new_file_name:
+        return None
+
+    with (open(file_name, "r") as file_in,
+          open(new_file_name, "w") as file_out):
+        file_out.writelines(file_in.readlines())
