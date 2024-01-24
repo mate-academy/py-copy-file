89a02df8c3180581c0057813c5847c1d23a7c31b
Maksym Operchuk <operlast529@gmail.com>
2024-01-24 14:56:47 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,6 +5,4 @@ def copy_file(command: str) -> None:
         return
 
     with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
-        file_content = file_in.readlines()
-        for line in file_content:
-            file_out.write(line)
+        file_out.write(file_in.read())
