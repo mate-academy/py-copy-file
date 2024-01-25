91f70e7181d086dff2e21fa15ab29acc0028df36
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-01-25 15:19:39 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,7 @@
-def copy_file(command: str) -> None:
-    linux_command, source_name, new_file = command.split(" ")
-    if (
-            linux_command != "cp" or not (
-            source_name.endswith(".txt")
-            or new_file.endswith(".txt")
-            )):
-
-        raise ValueError("Check your command and try again")
+def copy_file(full_command: str) -> None:
+    command, source_name, new_file = full_command.split(" ")
     if source_name == new_file:
-        raise ValueError("Names should not have the same name")
-    try:
-        source = open(source_name, "r")
-    except (FileNotFoundError, IOError):
-        print("File not found")
-    else:
-        with source as file_in, open(new_file, "w") as file_out:
-            file_out.write(file_in.read())
-        print(f"File {source_name} copied to {new_file} successfully")
+        return None
+    source = open(source_name, "r")
+    with source as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
