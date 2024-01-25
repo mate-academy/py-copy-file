3d4333ca19c9f2e606dfaec967e1238f3fd34f02
Kyrylo Storozhenko <busterakel@gmail.com>
2024-01-25 10:24:03 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,6 +7,3 @@ def copy_file(command: str) -> None:
     with open(source, "r") as file_in, open(destination_file, "w") as file_out:
         file_out.write(file_in.read())
     print(f"Copied contents from {source} to {destination_file}")
-
-
-copy_file("cp file.txt new_file.txt")
