b15ec5ac6ffbd61e54fb268c7fb8ed9929de150e
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-01-25 20:13:48 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(full_command: str) -> None:
     command, source_name, new_file = full_command.split(" ")
     if source_name == new_file:
-        return None
+        return
     source = open(source_name, "r")
     with source as file_in, open(new_file, "w") as file_out:
         file_out.write(file_in.read())
