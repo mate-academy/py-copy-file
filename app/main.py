0a869dadc5f993928c782e1145b8f3d44850fb65
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-01-27 11:34:00 +0200

    'Dell_return'

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ def copy_file(command: str) -> None:
     if len(components) == 3:
         cmd, source_path, destination_path = components
         if source_path != destination_path and cmd == "cp":
-            with open(source_path, "r") as file_in, \
-                    open(destination_path, "w") as file_out:
+            with (open(source_path, "r") as file_in,
+                  open(destination_path, "w") as file_out):
                 file_out.write(file_in.read())
-    return
