52b2cbf64d9fea754b56682d82bba883774bb5c3
Olha Rozdorozhna <olga.rozdorozhna@gmail.com>
2024-01-29 17:31:56 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    args = command.split()
+    source_file, destination_file = args[1], args[2]
+
+    if source_file == destination_file:
+        print("Source and destination files are the same.")
+    try:
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
+            file_out.write(file_in.read())
+    finally:
+        file_in.close()
+        file_out.close()
