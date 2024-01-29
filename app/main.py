b7ce7d2d8a2075e638d701a481cba11623ea62d6
Olha Rozdorozhna <olga.rozdorozhna@gmail.com>
2024-01-29 19:17:17 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 def copy_file(command: str) -> None:
     args = command.split()
-    source_file, destination_file = args[1], args[2]
-
+    if args[0] != "cp":
+        print("Command must start with 'cp'.")
+    command_name, source_file, destination_file, *_ = args
     if source_file == destination_file:
         print("Source and destination files are the same.")
-    try:
-        with (open(source_file, "r") as file_in,
-              open(destination_file, "w") as file_out):
-            file_out.write(file_in.read())
-    finally:
-        file_in.close()
-        file_out.close()
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
+        file_out.write(file_in.read())
