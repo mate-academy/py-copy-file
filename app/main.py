f04e0a7cfa51fe9d84e25ac47f45e7f46d0d4c4c
Maksym Operchuk <operlast529@gmail.com>
2024-01-29 09:28:48 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
-    action, file_name, new_file_name = command.split(" ")
+    action, file_name, new_file_name = command.split()
 
-    if file_name != new_file_name or action == "cd":
+    if file_name != new_file_name or action == "cp":
         with (
             open(file_name, "r") as file_in,
             open(new_file_name, "w") as file_out
