da6c7380175ae83e6f8c01cc43b3d1ed72a2928e
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-01-29 09:31:11 +0200

    Update main.py
    1) add checking command - cp
    2) deleted " " (default parameter)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(full_command: str) -> None:
-    command, source_name, new_file = full_command.split(" ")
-    if source_name == new_file:
-        return
-    source = open(source_name, "r")
-    with source as file_in, open(new_file, "w") as file_out:
-        file_out.write(file_in.read())
+    command, source_name, new_file = full_command.split()
+    if command == "cp":
+        if source_name == new_file:
+            return
+        source = open(source_name, "r")
+        with source as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
