ccf30741eb8089993f977435c7676713867afd34
Vyacheslav Malchyk <k2.mvo98@gmail.com>
2024-01-30 20:57:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split()
+    if command_list[1] != command_list[2]:
+        with (open(command_list[1], "r") as file_in,
+              open(command_list[2], "a") as file_out):
+            for line in file_in:
+                file_out.write(line)
