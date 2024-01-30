fcfeec3f0404a7ddae1989e7e7b38755e49c2baa
AntonSkyrda <mr.skyrda@gmail.com>
2024-01-30 14:06:47 +0200

    fix issues

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     parts = command.split()
 
-    if len(parts) != 3:
+    if not command.startswith("cp "):
         print("Invalid command."
               "Please provide a command in the format: "
               "cp source_file destination_file")
@@ -10,19 +10,13 @@ def copy_file(command: str) -> None:
     _, source_file, destination_file = parts
 
     if source_file == destination_file:
-        print("Source and destination file names are the same."
-              "Nothing will be done.")
-        return
+        raise Exception("Source and destination file names are the same. "
+                        "Nothing will be done.")
 
-    try:
-        with (open(source_file, "r") as file_in,
-              open(destination_file, "w") as file_out):
-            file_content = file_in.read()
-            file_out.write(file_content)
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
+        file_content = file_in.read()
+        file_out.write(file_content)
 
         print(f"File '{source_file}' copied to "
               f"'{destination_file}' successfully.")
-    except FileNotFoundError:
-        print(f"Error: File '{source_file}' not found.")
-    except Exception as error:
-        print(f"Error: {error}")
