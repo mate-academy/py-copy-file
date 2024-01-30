090b63aad313ac42929ea6f2c614f77efde2e895
volry <v.v.ryzhuk@gmail.com>
2024-01-30 07:17:46 +0200

    fixed errors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import shutil
 
 
-def copy_file(command_line):
+def copy_file(command_line: str) -> None:
     parts = command_line.split()
-    
+
     if len(parts) == 3 and parts[0] == "cp":
         source = parts[1]
         dest = parts[2]
@@ -13,8 +13,8 @@ def copy_file(command_line):
             return
 
         try:
-            with open(source,"r") as file_in, open(dest, "w") as file_out:
-                shutil.copyfileobj(file_in, file_out):
+            with open(source, "r") as file_in, open(dest, "w") as file_out:
+                shutil.copyfileobj(file_in, file_out)
             print(f"File {source} coppied to destination")
         except FileNotFoundError:
             print(f"File {source} not exists")
