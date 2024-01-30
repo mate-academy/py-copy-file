12a2621211d64ebc03c42c6400bca2d16ad5b581
volry <v.v.ryzhuk@gmail.com>
2024-01-30 07:12:21 +0200

    ready to push

--- app/main.py
+++ app/main.py
@@ -1,2 +1,22 @@
-def copy_file():
-    pass
+import shutil
+
+
+def copy_file(command_line):
+    parts = command_line.split()
+    
+    if len(parts) == 3 and parts[0] == "cp":
+        source = parts[1]
+        dest = parts[2]
+
+        if source == dest:
+            print("Do nothing")
+            return
+
+        try:
+            with open(source,"r") as file_in, open(dest, "w") as file_out:
+                shutil.copyfileobj(file_in, file_out):
+            print(f"File {source} coppied to destination")
+        except FileNotFoundError:
+            print(f"File {source} not exists")
+    else:
+        print("invalid command")
