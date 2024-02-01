f22b4de48d2d0ee5d91433c7951a7da9ba80a33c
cthusq <cthusq.qusevi@gmail.com>
2024-02-01 23:56:22 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 def copy_file(command: str) -> None:
     name = command.split(" ")
+    copies_file = name[1]
+    result_file = name[2]
 
     if len(name) == 3 and name[0] == "cp" and not name[1] == name[2]:
+        with (open(copies_file, "r") as file_in,
+              open(result_file, "w") as file_out):
+            file_out.write(file_in.read())
 
-        with open(name[1], "r") as file_in:
-            text = file_in.read()
-
-        with open(name[2], "w") as file_out:
-            file_out.write(text)
+        # with open(name[1], "r") as file_in:
+        #     text = file_in.read()
+        #
+        # with open(name[2], "w") as file_out:
+        #     file_out.write(text)
