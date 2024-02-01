9ff23288165398a739da905a3b11b9050d537469
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-02-01 19:43:38 +0000

    len of command is checked

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
-    cp_command, source_name, destination_name = command.split()
-    if source_name != destination_name and cp_command == "cp":
-        with (open(source_name, "r") as source,
-              open(destination_name, "w") as destination):
-            destination.write(source.read())
+    commands_list = command.split()
+    if len(commands_list) == 3:
+        cp_command, source_name, destination_name = commands_list
+        if source_name != destination_name and cp_command == "cp":
+            with (open(source_name, "r") as source,
+                  open(destination_name, "w") as destination):
+                destination.write(source.read())
