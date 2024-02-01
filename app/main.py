af092519f690dd0a9cc1b39435622aebf1b78b71
sergey <38cerg12021979@jmal.com>
2024-02-01 13:18:51 +0200

    01_02_24_02

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def copy_file(command: str) -> None:
     line = command.split()
-    if line[0] == "cp" and not line[1] == line[2]:
+    if len(line) == 3 and line[0] == "cp" and not line[1] == line[2]:
         with open(line[1], "r") as file_in, open(line[2], "w") as file_out:
             file_out.write(file_in.read())
+
+
