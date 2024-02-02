f022d11a199dea7cdaa07f28c8480eed72f9724d
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-02-02 13:21:13 +0200

    implement-py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,32 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split(" ")
+
+    if len(parts) != 3 or parts[0] != "cp":
+        print("Invalid command. Usage: cp original_file copied_file")
+        return
+
+    original_file, copied_file = parts[1], parts[2]
+
+    if original_file == copied_file:
+        print("Original and copied file names"
+              " are the same. Nothing to copy.")
+        return
+
+    try:
+        with (open(original_file, "r") as file_in,
+              open(copied_file, "w") as file_out):
+            file_out.write(file_in.read())
+
+        print(f"File \"{original_file}\" copied to"
+              f" \"{copied_file}\" successfully.")
+
+    except FileNotFoundError:
+        print(f"Error: \"{original_file}\" not found.")
+
+
+# example of usage
+copy_file("cp test.txt new_file.txt")
+if open("test.txt").read() == open("new_file.txt").read():
+    print(True)
+else:
+    print(False)
