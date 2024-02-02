28ac33b8c5fb7e4f60bf564404bba2736a49836d
Damodar <alaso.se@gmail.com>
2024-02-02 11:30:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    words_of_command = command.split(" ")
+    if words_of_command[1] != words_of_command[2]:
+        with (open(words_of_command[1], "r") as file_in,
+              open(words_of_command[2], "w") as file_out):
+            file_out.write(file_in.read())
