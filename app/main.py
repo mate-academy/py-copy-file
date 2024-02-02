4a27fa3344237d81713118de0a4cc8d4ed34f949
Eugene Protsenko <guitaaarpro@gmail.com>
2024-02-02 19:02:07 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -8,14 +8,15 @@ class ArgumentsOutOfRange(Exception):
 
 def copy_file(command: str) -> None | str:
     sep_file = command.split(" ")
-
-    if sep_file[1] == sep_file[2]:
-        return "Does nothing"
-    if sep_file[0] != "cp":
-        raise NoCpInCommand
     if len(sep_file) != 3:
         raise ArgumentsOutOfRange
+    if sep_file[0] != "cp":
+        raise NoCpInCommand
+    if sep_file[1] == sep_file[2]:
+        return "Does nothing"
 
     with open(sep_file[1], "r") as file_in, open(sep_file[2], "w") as file_out:
         file_content = file_in.read()
         file_out.write(file_content)
+
+copy_file("cp text.txt")
