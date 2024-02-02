2f7701ec6d2d57852fb85d39fef696827d826fad
Damodar <alaso.se@gmail.com>
2024-02-02 13:18:18 +0200

    added checks

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
     words_of_command = command.split(" ")
-    if words_of_command[1] != words_of_command[2]:
+    if (len(words_of_command) == 3
+            and words_of_command[0] == "cp"
+            and words_of_command[1] != words_of_command[2]):
         with (open(words_of_command[1], "r") as file_in,
               open(words_of_command[2], "w") as file_out):
             file_out.write(file_in.read())
