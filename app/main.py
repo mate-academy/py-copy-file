1e0f9068c3027a7640ae8fab5013503a39119932
cthusq <cthusq.qusevi@gmail.com>
2024-02-02 16:58:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,9 +7,3 @@ def copy_file(command: str) -> None:
         with (open(copies_file, "r") as file_in,
               open(result_file, "w") as file_out):
             file_out.write(file_in.read())
-
-        # with open(name[1], "r") as file_in:
-        #     text = file_in.read()
-        #
-        # with open(name[2], "w") as file_out:
-        #     file_out.write(text)
