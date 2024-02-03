0ae130c3929ea2f8ec98dcec211432dbb75adada
mkuchuman <mkuchuman1@gmail.com>
2024-02-03 18:12:54 +0100

    renamed veriebles

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
-    if len(command.split(" ")) == 3:
-        command, file, copy = command.split(" ")
-    if command == "cp" and file != copy:
-        with open(file, "r") as old_file, open(copy, "w") as new_file:
+    if len(command.split(" ")) != 3:
+        return
+    command, source_path, destination_path = command.split(" ")
+    if command == "cp" and source_path != destination_path:
+        with (open(source_path, "r") as old_file,
+              open(destination_path, "w") as new_file):
             new_file.write(old_file.read())
