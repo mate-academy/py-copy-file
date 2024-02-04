0d97983c803d048d924361e1fc59a39068787dce
voronvasia <voronvasia@gmail.com>
2024-02-04 14:52:48 +0200

    bugs fixed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
     parts = command.split()
-    if (parts[0] != "cp"
-            or parts[1] == parts[2]
-            or len(parts) != 3):
+    if (len(parts) != 3
+            or parts[0] != "cp"
+            or parts[1] == parts[2]):
         return
     with (open(parts[1], "r") as file_in,
           open(parts[2], "w") as file_out):
