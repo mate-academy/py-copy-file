a97751058f5440ed1a32a0804b3bfb603d68ed28
Alex Grytsai <a.grytsai1987@gmail.com>
2024-02-04 16:23:40 +0200

    1. Fixed all comments 2. Add try/except if copied file not found

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 def copy_file(command: str) -> None:
-    commands_list = command.split()
-    copy_command = commands_list[0]
-    old_file = commands_list[1]
-    new_file = commands_list[2]
+    command_list = command.split()
+    if len(command_list) != 3:
+        raise ValueError(f"Command must have 3 elements, but got: "
+                         f"{len(command_list)}")
+    copy_command, old_file, new_file = command_list
     if copy_command != "cp":
         raise NameError(f"{copy_command} is not a valid command.")
     if old_file == new_file:
         raise ValueError(f"File {old_file} and {new_file} have same name")
-    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-        for line in file_in:
-            file_out.write(line)
+    try:
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        raise ValueError(f"File {old_file} not found.")
