fc171527b208abb3951ef2f77f7b1829be2891da
voronvasia <voronvasia@gmail.com>
2024-02-04 09:36:42 +0200

    task completed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split()
+    if command_list[0] != "cp":
+        return
+    if command_list[1] == command_list[2]:
+        return
+    with (open(command_list[1], "r") as file_in,
+          open(command_list[2], "w") as file_out):
+        file_in_read = "".join(file_in.readlines())
+        file_out.write(str(file_in_read))
