aa7a45bec40192a3885fee2c8c370e90e931b284
voronvasia <voronvasia@gmail.com>
2024-02-04 14:22:52 +0200

    task refactored

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
-    command_list = command.split()
-    if command_list[0] != "cp":
+    parts = command.split()
+    if (parts[0] != "cp"
+            or parts[1] == parts[2]
+            or len(parts) != 3):
         return
-    if command_list[1] == command_list[2]:
-        return
-    with (open(command_list[1], "r") as file_in,
-          open(command_list[2], "w") as file_out):
-        file_in_read = "".join(file_in.readlines())
+    with (open(parts[1], "r") as file_in,
+          open(parts[2], "w") as file_out):
+        file_in_read = "".join(file_in.read())
         file_out.write(str(file_in_read))
