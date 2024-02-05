b2032998ba65404c8d8dd30c98b54c0e084da7dc
voidmain69 <94644605+voidmain69@users.noreply.github.com>
2024-02-05 18:49:35 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,11 +5,11 @@ def copy_file(command: str) -> None:
     if (len(components) == 3 and command_ == "cp"
         and source_file != destination_file):
 
-            try:
-
-                with (open(source_file, "r") as file_in,
-                      open(destination_file, "w") as file_out):
-
-                    file_out.write(file_in.read())
-            except FileNotFoundError as e:
-                print(e)
+        try:
+    
+            with (open(source_file, "r") as file_in,
+                  open(destination_file, "w") as file_out):
+    
+                file_out.write(file_in.read())
+        except FileNotFoundError as e:
+            print(e)
