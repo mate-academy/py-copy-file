83667cf6384b94d48ef8004b11c282373af370c6
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-02-08 14:26:10 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(input_str: str) -> None:
+    list_str = input_str.split()
+    if list_str[1] == list_str[2]:
+        return None
+    with open(list_str[1], "r") as file_in, open(list_str[2], "w") as file_out:
+        text_from_file_in = file_in.read()
+        file_out.write(text_from_file_in)
