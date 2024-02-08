38fd8b6503141671f99b26947225b608d9e83024
Vyacheslav Malchyk <k2.mvo98@gmail.com>
2024-02-08 21:04:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
     command_list = command.split()
 
-    if command_list[0] != "cp":
-        raise ValueError("Command must be cp")
+    if command_list[0] != "cp" or len(command_list) != 3:
+        raise ValueError("The command is specified incorrectly")
 
-    if len(command_list) == 3 and command_list[1] != command_list[2]:
+    if command_list[1] != command_list[2]:
         with (open(command_list[1], "r") as file_in,
               open(command_list[2], "w") as file_out):
             for line in file_in:
