9599bb9f62c488124511e79316c9b89d76c04ca4
Vyacheslav Malchyk <k2.mvo98@gmail.com>
2024-02-08 19:41:46 +0200

    Solution 4

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ def copy_file(command: str) -> None:
 
     if len(command_list) == 3 and command_list[1] != command_list[2]:
         with (open(command_list[1], "r") as file_in,
-              open(command_list[2], "a") as file_out):
+              open(command_list[2], "w") as file_out):
             for line in file_in:
                 file_out.write(line)
