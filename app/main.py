34ab3aeb9c791ff0fe692c6fc2994ae27f35f1a3
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-02-11 00:20:59 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split()) == 3 and command.split[0] == "cp":
+
+        source = command.split()[1]
+        destination = command.split()[2]
+
+        if source == destination:
+            return
+
+        with open(source, "r") as s, open(destination, "w") as d:
+            d.write(s.read())
