17225addda04fd68d62b12e9e57bb2ef447106ac
RomanHlodann <roman.hlodann@gmail.com>
2024-02-11 16:58:30 +0200

    function to copy file using linux command

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    arguments = command.split(" ")
+    if arguments[0] != "cp" or arguments[1] == arguments[2]:
+        return
+    with (open(arguments[1], "r") as file_in,
+          open(arguments[2], "w") as file_out):
+        file_out.write(file_in.read())
