7c5bc3b54726134208d1e9fa71db5df988127fc0
Molchanov Ihor <=>
2024-02-11 22:11:24 +0200

    change variable name

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     if command.startswith("cp") and len(command.split()) == 3:
-        command_parts, file_source, new_file = command.split()
+        arg, file_source, new_file = command.split()
         if file_source != new_file:
             with open(file_source, "r") as source, open(new_file, "w") as copy:
                 copy.write(source.read())
