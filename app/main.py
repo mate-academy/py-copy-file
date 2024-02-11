02f8b8135cbeac7a95be5ad7c43b27a8719235c3
Molchanov Ihor <=>
2024-02-11 22:10:29 +0200

    add check correct command

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_file(command: str) -> None:
-    command_parts, file_source, new_file = command.split()
-    if file_source != new_file:
-        with open(file_source, "r") as source, open(new_file, "w") as copy:
-            copy.write(source.read())
+    if command.startswith("cp") and len(command.split()) == 3:
+        command_parts, file_source, new_file = command.split()
+        if file_source != new_file:
+            with open(file_source, "r") as source, open(new_file, "w") as copy:
+                copy.write(source.read())
