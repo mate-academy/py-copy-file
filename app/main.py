d04e0603ddc4a8da7548ae00d009408389c0c2d3
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-02-11 23:34:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    _, input_name, output_name = command.split(" ")
+
+    if input_name == output_name:
+        return
+
+    with (open(input_name, "r") as input_file,
+          open(output_name, "w") as output_file):
+        data = input_file.read()
+
+        output_file.write(data)
