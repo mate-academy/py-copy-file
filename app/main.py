0ef2787b7df4f87018caa1f19f31b3fb27774c0c
Maxim Moshenskiy <osidiv@ukr.net>
2024-02-11 20:50:03 +0200

    added solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    if command[0] == "cp" and command[1] != command[2]:
+        with open(command[1], "r") as file_input:
+            with open(command[2], "w") as file_output:
+                for line in file_input:
+                    file_output.write(line)
+
+
+if __name__ == "__main__":
+    copy_file("cp file.txt file1.txt")
