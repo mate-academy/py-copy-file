9d43b009bb9fa23cdf816f1bd12a73b4b0764210
RomanHlodann <roman.hlodann@gmail.com>
2024-02-11 18:18:13 +0200

    added new statement to first if

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
     arguments = command.split(" ")
-    if arguments[0] != "cp" or arguments[1] == arguments[2]:
+    if (len(arguments) != 3
+            or arguments[0] != "cp"
+            or arguments[1] == arguments[2]):
         return
     with (open(arguments[1], "r") as file_in,
           open(arguments[2], "w") as file_out):
