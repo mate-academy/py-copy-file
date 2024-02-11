a327be73c3030001ded50b03dfbc9e5d991efede
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-02-11 17:38:12 +0200

    made changes

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@ def copy_file(command: str) -> None:
     args = command.split()
     if len(args) == 3 and args[0] == "cp":
 
-        source = args()[1]
-        destination = args()[2]
+        _, source, destination = args
 
         if source == destination:
             return
 
-        with open(source, "r") as source_file, \
-             open(destination, "w") as out_file:
+        with (
+            open(source, "r") as source_file,
+            open(destination, "w") as out_file
+        ):
             out_file.write(source_file.read())
