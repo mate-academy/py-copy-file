7745fe19767be655354876dad89218105016de93
Molchanov Ihor <=>
2024-02-11 20:20:26 +0200

    solved task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_parts = command.split(" ")
+    file_source = command_parts[1]
+    new_file = command_parts[2]
+    if file_source == new_file:
+        return
+    with open(file_source, "r") as source, open(new_file, "w") as copy:
+        copy.write(source.read())
