cd388e50572b7ec601bd5b84fcccb182c99b18a1
Molchanov Ihor <=>
2024-02-11 22:05:49 +0200

    fix code

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 def copy_file(command: str) -> None:
-    command_parts = command.split(" ")
-    file_source = command_parts[1]
-    new_file = command_parts[2]
-    if file_source == new_file:
-        return
-    with open(file_source, "r") as source, open(new_file, "w") as copy:
-        copy.write(source.read())
+    command_parts, file_source, new_file = command.split()
+    if file_source != new_file:
+        with open(file_source, "r") as source, open(new_file, "w") as copy:
+            copy.write(source.read())
