a067a8d583812de40a82740650e64c65699d1550
Molchanov Ihor <=>
2024-02-12 13:19:27 +0200

    add check for parameter

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy_file(command: str) -> None:
-    if command.startswith("cp") and len(command.split()) == 3:
-        arg, file_source, new_file = command.split()
-        if file_source != new_file:
-            with open(file_source, "r") as source, open(new_file, "w") as copy:
-                copy.write(source.read())
+    param, file_source, new_file = command.split()
+    if param == "cp" and file_source != new_file:
+        with open(file_source, "r") as source, open(new_file, "w") as copy:
+            copy.write(source.read())
