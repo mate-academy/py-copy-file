1943c0dd28e8dbce7dc764e27a15f5754af62a0a
RomanHlodann <roman.hlodann@gmail.com>
2024-02-12 12:47:58 +0200

    some changes

--- app/main.py
+++ app/main.py
@@ -3,9 +3,8 @@ def copy_file(command: str) -> None:
     if len(arguments) != 3:
         raise ValueError("Wrong count of arguments")
     com, original_filename, copy_filename = arguments
-    if com != "cp":
+    if com != "cp" or original_filename == copy_filename:
         raise ValueError("Wrong command")
-    with (open(arguments[1], "r") as file_in,
-          open(arguments[2], "w") as file_out):
-        if original_filename != copy_filename:
-            file_out.write(file_in.read())
+    with (open(original_filename, "r") as file_in,
+          open(copy_filename, "w") as file_out):
+        file_out.write(file_in.read())
