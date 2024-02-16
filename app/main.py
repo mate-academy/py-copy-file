cd0f53457df982c7037f09e591547623d09e645b
Eugen <zond342@gmail.com>
2024-02-16 14:23:45 +0200

    removed print

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    print(len(command))
     if len(command) == 3:
         if command[0] == "cp" and command[1] != command[2]:
             with (open(command[1], "r") as file_in,
