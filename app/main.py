0ebe3ac11a47461ab506c5dd9162c6340d77af41
Eugen <zond342@gmail.com>
2024-02-16 14:21:10 +0200

    removed .txt files, added 3 arguments check

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    if command[0] == "cp" and command[1] != command[2]:
-        with (open(command[1], "r") as file_in,
-              open(command[2], "w") as file_out):
-            file_out.write(file_in.read())
-
-
-copy_file("cp file.txt file2.txt")
+    print(len(command))
+    if len(command) == 3:
+        if command[0] == "cp" and command[1] != command[2]:
+            with (open(command[1], "r") as file_in,
+                  open(command[2], "w") as file_out):
+                file_out.write(file_in.read())
