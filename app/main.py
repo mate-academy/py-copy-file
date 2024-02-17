1d1006cc47a72e7f49222254248c5a3ed6e78f65
Anna Zadorozhna <annalepiska@gmail.com>
2024-02-18 00:50:03 +0200

    Implement py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split()
+    if not split_command[1] == split_command[2] and split_command[0] == "cp":
+        with (open(split_command[1], "r") as origin,
+              open(split_command[2], "w") as copy):
+            copy.write(origin.read())
