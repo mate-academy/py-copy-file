66f128c96a8efb72f44c103b33df0db708cc32b9
Anna Zadorozhna <annalepiska@gmail.com>
2024-02-18 19:15:08 +0200

    Add a length check

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
     split_command = command.split()
-    if not split_command[1] == split_command[2] and split_command[0] == "cp":
+    if (len(split_command) == 3
+            and split_command[1] != split_command[2]
+            and split_command[0] == "cp"):
         with (open(split_command[1], "r") as origin,
               open(split_command[2], "w") as copy):
             copy.write(origin.read())
