2a5a5756ccc1f171c775958a9a43690f334af7d7
Mykola Medved <kradu2001@gmail.com>
2024-02-19 08:00:21 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None | str:
+    command_list = command.split()
+
+    if command_list[0] != "cp":
+        return (
+            f"Wrong command has been entered '{command_list[0]}'"
+            f" (only the 'cp' command is accepted)"
+        )
+
+    if command_list[1] == command_list[2]:
+        return
+
+    with (open(command_list[1], "r") as original,
+          open(command_list[2], "w") as copy):
+        copy.write(original.read())
