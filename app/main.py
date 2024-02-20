eeeac7d7a9c468a06e5d8056ee29b8b066fe4cf3
Yevhen Tkachuk <yevhen23tk@gmail.com>
2024-02-20 14:05:08 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    split_com = command.split()
+
+    cp = split_com[0]
+    source_file = split_com[1]
+    destination_file = split_com[2]
+
+    if (len(split_com) == 3
+            and source_file != destination_file
+            and cp == "cp"):
+
+        try:
+            with (open(source_file, "r") as old_file,
+                  open(destination_file, "w") as new_file):
+                new_file.write(old_file.read())
+        except Exception as e:
+            print(e)
