4f51f8cf46ef60505540ec77c03f7f2a5e717f98
Kolya Ostrovka <130283941+kolya2731@users.noreply.github.com>
2024-02-20 14:50:10 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
-
 def copy_file(command: str) -> None:
     command_parts = command.split()
     com, source_file, target_file = (command_parts[0],
                                      command_parts[1],
                                      command_parts[2])
+    
     if (
             com == "cp"
             and source_file != target_file
             and len(command_parts) == 3
     ):
-            with (open(source_file, "r") as file,
-                  open(target_file, "w") as new_file):
-                new_file.write(file.read())
+        
+        with (open(source_file, "r") as file,
+                open(target_file, "w") as new_file):
+            new_file.write(file.read())
