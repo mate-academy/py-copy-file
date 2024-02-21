c918b00a157be0110a5db911be780c4eb10755f7
Mykola Kokoiev <veokoknik@gmail.com>
2024-02-21 18:15:08 +0100

    SOL

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 def copy_file(command: str) -> None:
-    if command.split()[0] == "cp":
+    element = command.split()
 
-        with open(f"{command.split()[1]}", "r") as file,\
-                open(f"{command.split()[2]}", "a") as file_copy:
+    if element[0] == "cp":
+        with open(
+                f"{element[1]}", "r"
+        ) as file, open(
+            f"{element[2]}", "a"
+        ) as file_copy:
 
             if file.name != file_copy.name:
                 file_copy.write(f"{file.read()}")
