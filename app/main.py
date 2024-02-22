80b29837ccdeac8a2b1525d94db746d2e017538f
Alexander <sasacihun@gmail.com>
2024-02-22 13:04:14 +0200

    Removed .txt file. Refactor (removed unnecessary function calls, added command validation and tracking unpacking)

--- app/main.py
+++ app/main.py
@@ -1,15 +1,13 @@
-def copy_file(command: str) -> None:
-    input_filename = command.split()[1]
-    output_filename = command.split()[2]
+def copy_file(command_name: str) -> None:
+    try:
+        command, input_filename, output_filename, *_ = command_name.split()
+    except Exception as e:
+        print(e, "Wrong number of arguments!")
+        return
 
-    if input_filename != output_filename:
+    if command == "cp" and input_filename != output_filename:
         with (
             open(input_filename, "r") as inp_f,
             open(output_filename, "w") as out_f
         ):
             out_f.write(inp_f.read())
-
-
-if __name__ == "__main__":
-    copy_file("cp file.txt file.txt")
-    copy_file("cp file.txt new_file.txt")
