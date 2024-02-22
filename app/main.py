23369a258cd66e6d0de608f224a8e75916dec7c7
donbas777 <renary34@gmail.com>
2024-02-22 19:13:37 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 def copy_file(command: str) -> None:
     sort_command = command.split()
-    with open(sort_command[1], "r") as file_in, open(sort_command[2], "w") as file_out:
+    with (open(sort_command[1], "r") as file_in,
+          open(sort_command[2], "w") as file_out):
         file_out.write(f"{file_in}")
