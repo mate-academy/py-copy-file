010ceb31ebe882a3c28b001efa369b93dacdc5d1
CreepyMo <blashchukstas@gmail.com>
2024-02-23 19:13:41 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_words = command.split()
+    if command_words[1] == command_words[2]:
+        return
+
+    with open(command_words[1], "r") as file_in, open(command_words[2], "w") as file_out:
+        file_out.writelines(file_in.readlines())
