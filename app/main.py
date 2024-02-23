f2003ef00c42f0ed3f759d9551c4d100e80b2d27
CreepyMo <blashchukstas@gmail.com>
2024-02-23 19:15:52 +0700

    Fixed too long line

--- app/main.py
+++ app/main.py
@@ -3,5 +3,8 @@ def copy_file(command: str) -> None:
     if command_words[1] == command_words[2]:
         return
 
-    with open(command_words[1], "r") as file_in, open(command_words[2], "w") as file_out:
+    with (
+        open(command_words[1], "r") as file_in,
+        open(command_words[2], "w") as file_out
+    ):
         file_out.writelines(file_in.readlines())
