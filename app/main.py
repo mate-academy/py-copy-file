ce5ba870f7ddaeddaa06867728ce24857eaf95c9
donbas777 <renary34@gmail.com>
2024-02-24 12:14:33 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_file(command: str) -> None:
     sort_command = command.split()
-    with (open(sort_command[1], "r") as file_in,
-          open(sort_command[2], "w") as file_out):
-        file_out.write(f"{file_in}")
+    if sort_command[0] == "cp" and sort_command[1] != sort_command[2]:
+        with (open(sort_command[1], "r") as file_in,
+              open(sort_command[2], "w") as file_out):
+            file_out.write(file_in.read())
