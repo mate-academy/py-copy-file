25c5d8510f5139c2be8e1d0bc88a0d61a6fbf700
Dmytro Kaliuta <d.kaliuta.work@gmail.com>
2024-02-25 12:36:16 +0200

    Implement py-copy-file solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> any:
+    (
+        command,
+        first_filename,
+        second_filename,
+    ) = command.split(" ")
+    if command != "cp" or first_filename == second_filename:
+        return
+    try:
+        with (open(first_filename, "r") as source,
+              open(second_filename, "w") as copy):
+            copy.write(source.read())
+    except FileNotFoundError:
+        print(f"File not found: {first_filename}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
