84529ae2e905c17dda17081b6d7295c98f7d789b
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-02-25 19:58:32 +0100

    Added check params

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 def copy_file(command: str) -> None:
     params = command.split(" ")
-    command_name = params[0]
-    file_name = params[1]
-    file_name_copy = params[2]
+    if len(params) == 3:
+        command_name, file_name, file_name_copy = params
 
-    if file_name != file_name_copy and command_name == "cp":
-        with (
-            open(file_name, "r") as file,
-            open(file_name_copy, "w") as file_copy
-        ):
-            file_copy.write(file.read())
+        if file_name != file_name_copy and command_name == "cp":
+            with (
+                open(file_name, "r") as file,
+                open(file_name_copy, "w") as file_copy
+            ):
+                file_copy.write(file.read())
