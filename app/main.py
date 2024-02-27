f73ba71d0f26a4575cd65a8a4a6c0383e244a3ca
rtsadm <98526243+rtsadm@users.noreply.github.com>
2024-02-27 14:07:27 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_in = command.split()[1]
+    file_out = command.split()[2]
+    if file_in != file_out:
+        with open(file_in, "r") as to_copy, open(file_out, "w") as copied:
+            copied.write(to_copy.read())
