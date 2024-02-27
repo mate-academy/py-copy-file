d0871f79b65ad9db435ee4cd52d35ad2b66fbd24
rtsadm <98526243+rtsadm@users.noreply.github.com>
2024-02-27 15:07:40 +0200

    fix2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
     split = command.split()
-    if split[1] != split[2] and split[0] == "cp":
+    if split[1] != split[2] and split[0] == "cp" and len(split) == 3:
         with open(split[1], "r") as file_in, open(split[2], "w") as file_out:
             file_out.write(file_in.read())
