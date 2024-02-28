1fff21a2afc6e89770bf74f646c0bfb6adb2ae6f
Ivan Kozerovskiy <vanko6226@gmail.com>
2024-02-28 20:39:40 +0200

    solution 2

--- app/main.py
+++ app/main.py
@@ -2,5 +2,6 @@ def copy_file(command: str) -> None:
     c_arr = command.split()
     if len(c_arr) == 3:
         if c_arr[0] == "cp" and (c_arr[1] != c_arr[2]):
-            with open(c_arr[1], "r") as file_in, open(c_arr[2], "w") as file_out:
+            with (open(c_arr[1], "r") as file_in,
+                  open(c_arr[2], "w") as file_out):
                 file_out.write(file_in.read())
