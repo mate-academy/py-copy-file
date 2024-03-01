434a5280df401307251da88991ab51c216ab057b
sudakolha <sudakolha@outlook.com>
2024-03-01 19:39:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> bool:
+    cp_file = command.split(" ")[1]
+    new_file = command.split(" ")[2]
+
+    if cp_file != new_file:
+        with (open(cp_file, "r") as file_in,
+              open(new_file, "w") as file_out):
+            file_out.write(file_in.read())
+            return open("file.txt").read() == open("new_file.txt").read()
+
+
+copy_file("cp file.txt file.txt")
+copy_file("cp file.txt new_file.txt")
+print(open("file.txt").read() == open("new_file.txt").read())
