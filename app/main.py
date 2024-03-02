20e0bcb6498d1476274364c0dcef56d1b684c6e5
sudakolha <sudakolha@outlook.com>
2024-03-02 19:38:28 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,10 @@
 def copy_file(command: str) -> bool:
-    cp_file = command.split(" ")[1]
-    new_file = command.split(" ")[2]
+    commands = command.split(" ")
+    if len(commands) == 3 and commands[0] == "cp":
+        cp_file = commands[1]
+        new_file = commands[2]
 
-    if cp_file != new_file:
-        with (open(cp_file, "r") as file_in,
-              open(new_file, "w") as file_out):
-            file_out.write(file_in.read())
-            return open("file.txt").read() == open("new_file.txt").read()
-
-
-copy_file("cp file.txt file.txt")
-copy_file("cp file.txt new_file.txt")
-print(open("file.txt").read() == open("new_file.txt").read())
+        if cp_file != new_file:
+            with (open(cp_file, "r") as file_in,
+                  open(new_file, "w") as file_out):
+                file_out.write(file_in.read())
