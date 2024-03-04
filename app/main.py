f17584041b483f56f4d850989344b80fbf15cd93
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-03-04 09:37:08 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import shutil
+import os
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3:
+        print("Invalid command format. "
+              "Please provide command in the format: "
+              "cp source_file target_file")
+        return
+
+    source_file, target_file = parts[1], parts[2]
+
+    if source_file == target_file:
+        print("Source file and target file are the same. No action taken.")
+        return
+
+    if os.path.exists(target_file):
+        print("Target file already exists. No action taken.")
+        return
+
+    shutil.copyfile(source_file, target_file)
+    print(f"File '{source_file}' copied to '{target_file}' successfully.")
