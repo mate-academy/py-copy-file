9412c4686a5f935012a95cf698c5eacb343f2365
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-03-05 08:43:20 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
-    c_arr = command.split()
-    if len(c_arr) == 3:
-        if c_arr[0] == "cp" and (c_arr[1] != c_arr[2]):
-            with (open(c_arr[1], "r") as file_in,
-                  open(c_arr[2], "w") as file_out):
+    cmd_split = command.split()
+    if len(cmd_split) == 3:
+        if cmd_split[0] == "cp" and (cmd_split[1] != cmd_split[2]):
+            with (open(cmd_split[1], "r") as file_in,
+                  open(cmd_split[2], "w") as file_out):
                 file_out.write(file_in.read())
