0f439c4321901de7a8ed9e0a59d5732cc9e4b44f
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-03-06 10:13:35 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
-    cmd, source, destination = command.split()
-    if len(cmd) == 3:
-        if cmd == "cp" and (source != destination):
-            with (open(source, "r") as file_in,
-                  open(destination, "w") as file_out):
+    split_command = command.split()
+    if len(split_command) == 3:
+        cmd, source, destination = split_command
+        if cmd == "cp" and source != destination:
+            with open(source, "r") as file_in, open(destination, "w") as file_out:
                 file_out.write(file_in.read())
