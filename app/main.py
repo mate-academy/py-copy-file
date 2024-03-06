6d24083910214e93ed05252f7084355c13f10ee5
Serhii Dannyk <serhii.dannyk@gmail.com>
2024-03-06 11:25:24 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    old_file = parts[1]
+    new_file = parts[2]
+    try:
+        with open(old_file, "r") as file_in:
+            if old_file != new_file:
+                with open(new_file, "w") as file_out:
+                    file_out.write(file_in.read())
+    except FileNotFoundError:
+        raise
