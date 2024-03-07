2b8c7bfa6369929bab78f2a352388f3df735dc29
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-03-07 17:42:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(commdand: str) -> None:
+    splitted_file = commdand.split()
+    if len(splitted_file) != 3:
+        return
+    command, file_from, file_to = splitted_file
+    if command != "cp":
+        return
+    if file_from == file_to:
+        return
+
+    with open(file_from, "r") as file_from, open(file_to, "w") as file_to:
+        file_to.write(file_from.read())
