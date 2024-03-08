801534e0a80a41c17792676cdf057db29c8af3fc
Kyrylo <kbuchinskiy1@gmail.com>
2024-03-08 18:22:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[1] == parts[2]:
+        return
+
+    with (open(parts[1], "r") as file_in,
+            open(parts[2], "w") as file_out):
+        for line in file_in:
+            file_out.write(line)
+
+
+copy_file("cp test.txt test.txt")
