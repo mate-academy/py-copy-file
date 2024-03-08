a8c2a0f6eeccd27f979a8a3d93a557c65e362e2c
Maksim <sma4no.7@gmail.com>
2024-03-08 14:45:19 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -5,11 +5,10 @@ def copy_file(command: str) -> None:
         return
 
     source_file, destination_file = list_of_words[1:]
-
+    if list_of_words[0] != "mv":
+        return
     if source_file == destination_file:
         return
-    else:
-        with (open(source_file, "r") as file_in,
-              open(destination_file, "w") as file_out):
-            content = file_in.read()
-            file_out.write(content)
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
+        file_out.write(file_in.read())
