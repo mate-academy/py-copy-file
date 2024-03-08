b919510145b5428268aa7b314a623c73a1c03967
meier <Meyertal2@gmail.com>
2024-03-08 12:55:38 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def copy_file(command: str) -> None:
     command = command.split()
+    if command != 3:
+        return
     if command[1] == command[2]:
         return
     if command[0] != "cp":
