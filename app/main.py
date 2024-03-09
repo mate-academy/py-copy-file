8b12f85ecd70e21dd19dec766586f744a27c6eaf
Kyrylo <kbuchinskiy1@gmail.com>
2024-03-09 19:55:31 +0200

    fix solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        return
     action, source_file, destination_file = command.split()
-
-    if len(command.split()) == 3 and source_file != destination_file:
+    if source_file != destination_file:
         with (open(source_file, "r") as file_in,
               open(destination_file, "w") as file_out):
             file_out.writelines(file_in)
