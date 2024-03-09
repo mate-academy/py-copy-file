e005ffe57cefcf6b93e8b1d929b7dce642afa808
mvsydoruk <150350026+mvsydoruk@users.noreply.github.com>
2024-03-09 19:51:19 +0200

    Update app/main.py
    Co-authored-by: Vasyl Smutok <vasyl.smutok@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(cp_command: str) -> None:
     splitted_cp_command = cp_command.split()
     if (
-        len(splitted_cp_command)
+        len(splitted_cp_command) == 3
         and splitted_cp_command[0] == "cp"
         and splitted_cp_command[1] != splitted_cp_command[2]
     ):
