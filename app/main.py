883b10e105b8781b1d2c4e62c0f28fa32b5d769c
Oles Shulzhenko <simplemanua@gmail.com>
2024-03-10 18:10:38 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_ls = command.split()
+    if command_ls[0] is not "cp":
+        raise ValueError
+
+    if command_ls[1] != command_ls[2]:
+        with (open(command_ls[1], "r") as file_in,
+              open(command_ls[2], "w") as file_out):
+            data = file_in.read()
+            file_out.write(data)
+
+    else:
+        pass
