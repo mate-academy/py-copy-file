4cb589e5bf875a654f24345d60de37e545c8b639
skeletronys <mykytcey@gmail.com>
2024-03-10 19:04:53 +0100

    use unpacking, split default

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
-    names = command.split(" ")
-    first_file = names[1]
-    copys_file = names[2]
-    if len(names) == 3 and names[0] == "cd" and not first_file == copys_file:
+    names = command.split()
+    cd_type, first_file, copys_file = names
+    if len(names) == 3 and cd_type == "cd" and not first_file == copys_file:
         with (open(first_file, "r") as file_in,
               open(copys_file, "w") as file_out):
             file_out.write(file_in.read())
