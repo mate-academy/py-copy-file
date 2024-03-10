b090e37c99e7140ee6b7096dacf4f766255843a7
Kyrylo <kbuchinskiy1@gmail.com>
2024-03-10 13:39:55 +0200

    Add cp check

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str) -> None:
     if len(command.split()) != 3:
         return
     action, source_file, destination_file = command.split()
-    if source_file != destination_file:
+    if source_file != destination_file and action == "cp":
         with (open(source_file, "r") as file_in,
               open(destination_file, "w") as file_out):
             file_out.writelines(file_in)
