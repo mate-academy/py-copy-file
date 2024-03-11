3dcb7eaaa347cbf8c39557b4849bc863818821db
Oles Shulzhenko <simplemanua@gmail.com>
2024-03-11 08:42:53 +0200

    'Corected'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     command_ls = command.split()
-    if command_ls[0] != "cp":
+    if len(command_ls) != 3 or command_ls[0] != "cp":
         raise ValueError
 
     if command_ls[1] != command_ls[2]:
@@ -8,6 +8,3 @@ def copy_file(command: str) -> None:
               open(command_ls[2], "w") as file_out):
             data = file_in.read()
             file_out.write(data)
-
-    else:
-        pass
