0348a2d6e3b7cf09ae8a85a0db912605a530b5fc
skeletronys <mykytcey@gmail.com>
2024-03-11 16:07:20 +0100

    use if before unpacking and rename "cd" for "cp"

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 def copy_file(command: str) -> None:
     names = command.split()
-    cd_type, first_file, copys_file = names
-    if len(names) == 3 and cd_type == "cd" and not first_file == copys_file:
+    if len(names) == 3:
+        cd_type, first_file, copys_file = names
+    else:
+        return
+    if cd_type == "cp" and not first_file == copys_file:
         with (open(first_file, "r") as file_in,
               open(copys_file, "w") as file_out):
             file_out.write(file_in.read())
