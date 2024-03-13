b6e910533ad0bf18545c080d7d63e4deca32d8a1
Anton <antonrozegan1@gmail.com>
2024-03-13 13:57:12 +0200

    Solutin

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-def cope_file(command: str) -> any:
+def cope_file(command: str) -> None:
     new_command = command.split()
-    if new_command[1] == new_command[2]:
-        return
-    with open(new_command[1], "r") as file_in, open(new_command[2], "w"
-                                                    ) as file_out:
-        file_out.write(file_in.read())
+    if (
+        len(new_command) == 3
+        and new_command[0] == "cp"
+        and new_command[1] != new_command[2]
+    ):
+         with (
+            open(new_command[1], "r") as file_in,
+            open(new_command[2], "w") as file_out
+        ):
+            file_out.write(file_in.read())
