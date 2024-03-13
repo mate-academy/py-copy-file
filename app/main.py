68e5bf5b0d5632a12083cbda7304d669c94a802c
antonver <anton.verbovyi@gmail.com>
2024-03-13 19:20:20 +0100

    Done

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 # write your code here
 def copy_file(command: str) -> None:
     command = command.split()
-    if command[1] == command[2]:
+    if len(command) == 3 and command[0] == "cp":
+        old_file, new_file = command[1:]
+    if old_file == new_file:
         return
-    with open(command[1], "r") as old_file, open(command[2], "w") as new_file:
+    with open(old_file, "r") as old_file, open(new_file, "w") as new_file:
         new_file.write(old_file.read())
