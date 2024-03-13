f15988dd908fc88442d1b5d13adde7038a8706ba
Viesich <v.s.vesich@gmail.com>
2024-03-13 23:50:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,14 +4,3 @@ def copy_file(command: str) -> None:
     if file_origin != cp_file:
         with open(file_origin, "r") as file_in, open(cp_file, "w") as file_out:
             file_out.write(file_in.read())
-
-
-file = open("file.txt", "w")
-file.write("Hallo world!!!")
-file.close()
-
-
-copy_file("cp file.txt new_file.txt")
-print(open("file.txt").read() == open("new_file.txt").read())
-print(open("file.txt").read())
-print(open("new_file.txt").read())
