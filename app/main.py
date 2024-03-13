af95079255f78bf2ec8925f345562a546411d509
Anton <antonrozegan1@gmail.com>
2024-03-13 13:05:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def cope_file(command: str) -> any:
+    new_command = command.split()
+    if new_command[1] == new_command[2]:
+        return
+    with open(new_command[1], "r") as file_in, open(new_command[2], "w"
+                                                    ) as file_out:
+        file_out.write(file_in.read())
