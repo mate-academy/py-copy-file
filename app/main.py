afcfb3db1a7edb224f5a38cb5a802e2d561bde9e
Viesich <v.s.vesich@gmail.com>
2024-03-13 23:47:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_origin = command.split()[1]
+    cp_file = command.split()[2]
+    if file_origin != cp_file:
+        with open(file_origin, "r") as file_in, open(cp_file, "w") as file_out:
+            file_out.write(file_in.read())
+
+
+file = open("file.txt", "w")
+file.write("Hallo world!!!")
+file.close()
+
+
+copy_file("cp file.txt new_file.txt")
+print(open("file.txt").read() == open("new_file.txt").read())
+print(open("file.txt").read())
+print(open("new_file.txt").read())
