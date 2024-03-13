442d973b1a893287f53e889cebbf8e0a871a3590
antonver <anton.verbovyi@gmail.com>
2024-03-13 16:43:43 +0100

    Done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
 # write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    if command[1] == command[2]:
+        return
+    with open(command[1], "r") as old_file, open(command[2], "w") as new_file:
+        new_file.write(old_file.read())
