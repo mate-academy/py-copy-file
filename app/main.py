6fff6f2d8424c216c841f42e12583b3ce90c5784
SHCHERBANV <shcherbanvasyl@gmail.com>
2024-03-14 15:35:37 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    commands = command.split(" ")
-    if commands[0] == "cp" and commands[1] != commands[2]:
+    commands = command.split() # refactor split to split(" ")
+    if commands[0] == "cp" and commands[1] != commands[2]:# merge conditions
         with open(commands[1], "r") as input_file, \
                 open(commands[2], "w") as output_file:
             file_in = input_file.readlines()
