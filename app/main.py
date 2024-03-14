454417643a2545e74977a048206515fe14ba3f23
SHCHERBANV <shcherbanvasyl@gmail.com>
2024-03-14 11:46:30 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
     commands = command.split(" ")
-    if commands[0] == "cp":
-        if commands[1] != commands[2]:
-            with open(commands[1], "r") as input_file, \
-                    open(commands[2], "w") as output_file:
-                file_in = input_file.readlines()
-                output_file.writelines(file_in)
+    if commands[0] == "cp" and commands[1] != commands[2]:
+        with open(commands[1], "r") as input_file, \
+                open(commands[2], "w") as output_file:
+            file_in = input_file.readlines()
+            output_file.writelines(file_in)
