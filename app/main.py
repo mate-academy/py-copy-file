922f327e72618c9afa85f2aecc2110e6c8f1f0cb
Oleh Kolodka <oleh1928@gmail.com>
2024-03-14 23:18:36 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+class CommandError(Exception):
+    def __str__(self) -> str:
+        return "Use: cp <file> <file_copy>"
+
+
+def copy_file(command: str) -> None:
+    if not command:
+        raise CommandError
+
+    command = command.split()
+    if len(command) != 3 or command[0] != "cp":
+        raise CommandError
+    if not command[1] == command[2]:
+        with (
+            open(command[1], "r") as file_to_copy,
+            open(command[2], "w") as file_to_paste
+        ):
+            file_to_paste.write(file_to_copy.read())
