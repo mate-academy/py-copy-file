fb6ebfa13e69a14af0856af60111cf471dd9092a
Oleh Kolodka <oleh1928@gmail.com>
2024-03-14 23:19:48 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def copy_file(command: str) -> None:
     command = command.split()
     if len(command) != 3 or command[0] != "cp":
         raise CommandError
-    if not command[1] == command[2]:
+    if command[1] != command[2]:
         with (
             open(command[1], "r") as file_to_copy,
             open(command[2], "w") as file_to_paste
