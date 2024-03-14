3d796f7323c2d3d7f8e9f3713c6fc1fafb360f8f
Viesich <v.s.vesich@gmail.com>
2024-03-14 14:21:22 +0200

    refactor

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ def copy_file(command: str) -> None:
     parts = command.split()
     if len(parts) == 3:
         com, file_origin, cp_file = parts
-        if com == "cp" and file_origin != cp_file:
-            with open(file_origin, "r") as file_in, open(cp_file, "w") as file_out:
+        if file_origin != cp_file and com == "cp":
+            with (open(file_origin, "r") as file_in,
+                  open(cp_file, "w") as file_out):
                 file_out.write(file_in.read())
 
 
