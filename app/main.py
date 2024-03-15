3fcff41161c23cfc647a5fe2b1024034900154a6
Oleh Kolodka <oleh1928@gmail.com>
2024-03-15 22:47:35 +0200

    Unpack list to variables

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ def copy_file(command: str) -> None:
     command = command.split()
     if len(command) != 3 or command[0] != "cp":
         raise ValueError("Use: cp <file> <file_copy>")
-    if command[1] != command[2]:
+    file_name, copy_name = command[1:]
+    if command[file_name] != command[copy_name]:
         with (
-            open(command[1], "r") as file_to_copy,
-            open(command[2], "w") as file_to_paste
+            open(command[file_name], "r") as file_to_copy,
+            open(command[copy_name], "w") as file_to_paste
         ):
             file_to_paste.write(file_to_copy.read())
