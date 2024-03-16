3175f349889d4f3a6c9a98388d3992761fba8e24
yes <yes@yess-MacBook-Pro.local>
2024-03-16 05:12:41 +0200

    refactor

--- app/main.py
+++ app/main.py
@@ -1,18 +1,9 @@
-from os import path, listdir, chdir
+from os import path, chdir
 from sys import argv
 
 
 def copy_file(command: str) -> None:
     chdir(path.dirname(argv[0]))
-    if command \
-        and len(command.split(" ")) == 3 \
-        and command[1] != command[2] \
-        and command[2] not in listdir():
-        ref, copy = command[1], command[2]
-        with open(command[2]) as new:
-            with open(command[1]) as ref:
-                new.write(ref.read())
-                ref.close()
-            new.close()
-    else:
-        return
+    cm = command.split(" ")
+    return None if (len(cm) == 1) or (cm[1] == cm[2]) \
+        else open(cm[2], "w").write(open(cm[1], "r").read())
