dbe79929b51d26e7ed1db91e3e079f8533f9cfad
Lumideus <d.bilan2004@gmail.com>
2024-03-17 22:34:18 +0200

    Implement copy

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    text = command.split()
+    if len(text) == 3:
+        if text[0] == "cp" and text[1] != text[2]:
+            with (open(text[1], "r") as source,
+                  open(text[2], "r") as destination):
+                destination.write(source.read())
