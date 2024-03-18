e555d7be1f77b6ed1de9ded811e1de2efc17c79b
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-03-18 20:29:47 +0200

    i don't understand that how should i test this

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split(" ")
+    initial_file = split_command[1]
+    final_file = split_command[2]
+    with (open(initial_file, "r") as file_old,
+          open(final_file, "w") as file_new):
+        copy_value = file_old.readline()
+        file_new.write(copy_value)
