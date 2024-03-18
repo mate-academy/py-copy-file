59a885cb20b655910e93c387aade3660c10e1898
Rost Bonchuk <rostbonchuk@gmail.com>
2024-03-18 15:51:38 +0200

    Correction

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ def copy_file(command: str) -> None:
         key, f_in, f_out = command
         if key == "cp" and f_in != f_out:
             with (
-                open(command[1], "r") as file_in,
-                open(command[2], "w") as file_out
+                open(f_in, "r") as file_in,
+                open(f_out, "w") as file_out
             ):
                 file_out.write(file_in.read())
