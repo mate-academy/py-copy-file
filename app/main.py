5434ef93bc0e9d4b3a20cf31c420c53de9017808
Rost Bonchuk <rostbonchuk@gmail.com>
2024-03-18 15:50:14 +0200

    Correction

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 def copy_file(command: str) -> None:
     command = command.split()
-
     if len(command) == 3:
-        with (
-            open(command[1], "r") as file_in,
-            open(command[2], "w") as file_out
-        ):
-            file_out.write(file_in.read())
+        key, f_in, f_out = command
+        if key == "cp" and f_in != f_out:
+            with (
+                open(command[1], "r") as file_in,
+                open(command[2], "w") as file_out
+            ):
+                file_out.write(file_in.read())
