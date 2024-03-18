264476a4b0df309f41ac821bac65173502d178e0
Rost Bonchuk <rostbonchuk@gmail.com>
2024-03-18 14:58:01 +0200

    Correction

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 def copy_file(command: str) -> None:
-    read_file = command.split()[1]
-    write_file = command.split()[2]
-    if read_file != write_file and command.split()[0] == "cp":
+    key, read_file, write_file, *_ = command.split()
+
+    if read_file != write_file and key == "cp":
         with (
             open(read_file, "r") as file_in,
             open(write_file, "w") as file_out
         ):
-            content = file_in.read()
-            file_out.write(content)
+            file_out.write(file_in.read())
