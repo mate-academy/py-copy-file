5d5142ef66870997690fcf61e52e1d8f5bc19bb4
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-03-18 23:29:00 +0200

    i use unpacking

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
-    split_command = command.split(" ")
-    initial_file = split_command[1]
-    final_file = split_command[2]
+    _, initial_file, final_file = command.split(" ")
     with (open(initial_file, "r") as file_old,
           open(final_file, "w") as file_new):
-        copy_value = file_old.readline()
-        file_new.write(copy_value)
+        if command.startswith("cp"):
+            copy_value = file_old.readline()
+            file_new.write(copy_value)
