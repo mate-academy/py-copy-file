98ba961a907f0e458712ac61431b0f5d14eb2741
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-03-19 22:52:47 +0200

    Sorry for that i didn't test this code

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        return "Command must be exactly 3 characters"
     command, initial_file, final_file = command.split(" ")
-    if len(command) >= 3 and command == "cp":
+    if initial_file != final_file and command == "cp":
         with (open(initial_file, "r") as file_old,
               open(final_file, "w") as file_new):
-            copy_value = file_old.readline()
+            copy_value = file_old.read()
             file_new.write(copy_value)
+
+
+copy_file("cp baba.txt apple.txt")
