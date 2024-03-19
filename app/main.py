f7aa35dca5205693304c80ac285f4a049f3020f2
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-03-19 20:01:52 +0200

    'noprint'

--- app/main.py
+++ app/main.py
@@ -5,21 +5,20 @@ def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) != 3:
-        print("Invalid command format. "
-              "Please provide the command in the format:"
-              " cp <source_file> <destination_file>")
-        return
+        raise ValueError("Invalid command format. "
+                         "Please provide the command in the format:"
+                         " cp <source_file> <destination_file>")
 
     _, source_file, test_file = parts
 
     if source_file == test_file:
-        print("Source and destination files have the same name."
-              " No action taken.")
-        return
+        raise ValueError("Source and destination files have the same name. "
+                         "No action taken.")
 
     try:
         shutil.copyfile(source_file, test_file)
         print(f"File '{source_file}' copied to '{test_file}' successfully.")
     except FileNotFoundError:
-        print(f"Error: One or both of the files"
-              f" '{source_file}' and '{test_file}' not found.")
+        raise FileNotFoundError(f"Error: One or both of the files"
+                                f" '{source_file}' and '{test_file}' "
+                                f"not found.")
