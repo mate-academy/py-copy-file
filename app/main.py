4372f2f6bbc0d260d40df3866d9853e4b9c29b4b
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-03-19 18:08:54 +0200

    add if command and add if length

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
-    _, initial_file, final_file = command.split(" ")
-    with (open(initial_file, "r") as file_old,
-          open(final_file, "w") as file_new):
-        if command.startswith("cp"):
-            copy_value = file_old.readline()
-            file_new.write(copy_value)
+    if len(command) >= 3:
+        _, initial_file, final_file = command.split(" ")
+        if "cp" in _:
+            with (open(initial_file, "r") as file_old,
+                  open(final_file, "w") as file_new):
+                copy_value = file_old.readline()
+                file_new.write(copy_value)
