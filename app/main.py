65c36c965bb5214ec7003ed23026b4d122959d26
Rost Bonchuk <rostbonchuk@gmail.com>
2024-03-19 13:55:16 +0200

    Last correction

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 def copy_file(command: str) -> None:
-    command = command.split()
-    if len(command) == 3:
-        key, f_in, f_out = command
-        if key == "cp" and f_in != f_out:
-            with (
-                open(f_in, "r") as file_in,
-                open(f_out, "w") as file_out
-            ):
-                file_out.write(file_in.read())
+    key_word, read_file, write_file, *_ = command.split()
+    """# After use "*_" instruction no need "len == 3" verif,
+    because all the extra parts end up in a variable _ """
+
+    if key_word == "cp" and read_file != write_file:
+        with (
+            open(read_file, "r") as file_in,
+            open(write_file, "w") as file_out
+        ):
+            file_out.write(file_in.read())
