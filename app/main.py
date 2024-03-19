151777675d9369ac5fda926752c33ee22f9c0627
pochtalon <pochtalon1010@gmail.com>
2024-03-19 16:14:56 +0200

    Solved

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     file_names = command.split(" ")
-    if file_names[1] == file_names[2]:
+    if file_names[1] == file_names[2] or file_names[0] != "cp":
         return
     with (open(file_names[1], "r") as file_in,
           open(file_names[2], "w") as file_out):
