da03a8b27cf57aae9a077f527caa43bf9dcc333a
bujhm <Bujhm99@gmail.com>
2024-03-19 07:50:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    name_massive = command.split(" ")
+    if name_massive[1] != name_massive[2]:
+        with (open(name_massive[1], "r") as file_in,
+              open(name_massive[2], "w") as file_out):
+            file_out.write(file_in.read())
