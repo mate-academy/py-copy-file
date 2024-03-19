be5e9aad25f370be4279d4fb466924aa48d36bd3
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-03-19 19:59:50 +0200

    кÑrename _ to command and add if command == cp

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
+    command, initial_file, final_file = command.split(" ")
     if len(command) >= 3:
-        _, initial_file, final_file = command.split(" ")
-        if "cp" in _:
+        if command == "cp":
             with (open(initial_file, "r") as file_old,
                   open(final_file, "w") as file_new):
                 copy_value = file_old.readline()
