96515875cdf7eacdd60bf99467f00ecb484f98e6
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-03-19 21:34:19 +0200

    ÐI Ñadded AND

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
     command, initial_file, final_file = command.split(" ")
-    if len(command) >= 3:
-        if command == "cp":
-            with (open(initial_file, "r") as file_old,
-                  open(final_file, "w") as file_new):
-                copy_value = file_old.readline()
-                file_new.write(copy_value)
+    if len(command) >= 3 and command == "cp":
+        with (open(initial_file, "r") as file_old,
+              open(final_file, "w") as file_new):
+            copy_value = file_old.readline()
+            file_new.write(copy_value)
