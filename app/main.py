d08fca742a3293a0e0282bf978e9352bdc1f1df8
Vitalii Haponiuk <doninpotato@gmail.com>
2024-03-19 14:09:11 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    old_file, new_file = (command[2:].split())
+    if old_file != new_file:
+        with open(old_file, "r") as file:
+            info = file.read()
+        with open(new_file, "w") as my_file:
+            my_file.write(info)
