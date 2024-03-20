3ff03b7eba68917a26ba0c6f532ec6f757690ac5
Olevsky Ivan <134000226+Olevsky-Ivan@users.noreply.github.com>
2024-03-20 18:22:02 +0000

    flake8

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     command_split = command.split()
     if len(command_split) != 3:
-	    return "Command must be exactly 3 characters"
+        return "Command must be exactly 3 characters"
     command_name, initial_file, final_file = command_split
     if initial_file != final_file and command == "cp":
         with (open(initial_file, "r") as file_old,
