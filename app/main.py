6fb7b68c9cf5868d95749042cf5014716a6f96b7
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-03-20 18:42:54 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     parts = command.split()
 
-    if len(parts) != 3 or parts[0] != 'cp':
+    if len(parts) != 3 or parts[0] != "cp":
         raise ValueError("Invalid command format. "
                          "Please provide the command in the format:"
                          " cp <source_file> <destination_file>")
@@ -14,7 +14,8 @@ def copy_file(command: str) -> None:
 
     try:
 
-        with open(source_file, "r") as file_in, open(test_file, "w") as file_out:
+        with (open(source_file, "r") as file_in,
+              open(test_file, "w") as file_out):
             file_out.write(file_in.read())
 
         print(f"File '{source_file}' copied to '{test_file}' successfully.")
@@ -23,4 +24,4 @@ def copy_file(command: str) -> None:
               f"'{source_file}' and '{test_file}' not found.")
 
 
-copy_file("cp file.txt file-copy.txt")
+copy_file("cpf file.txt file-copy.txt")
