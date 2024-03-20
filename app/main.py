2d66b96af12a0a787673f221823b87bc28b2d4c8
alexpanzhar <alex.panzhar1@gmail.com>
2024-03-20 21:55:55 +0100

    solution1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    command_list = command.split()
+    file_orig_name = command_list[1]
+    file_copy_name = command_list[2]
+
+    if file_orig_name != file_copy_name:
+        with (open(file_orig_name, "r") as orig_f,
+              open(file_copy_name, "w") as copy_f):
+            copy_f.write(orig_f.read())
