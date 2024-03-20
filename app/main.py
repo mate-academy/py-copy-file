f70f05dfc65452d9b0ab485b74af189e16239c00
alexpanzhar <alex.panzhar1@gmail.com>
2024-03-20 22:08:48 +0100

    solution1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 def copy_file(command: str) -> None:
 
     command_list = command.split()
+
+    if len(command_list) != 3:
+        raise ValueError(f"Expected 3 arguements, {len(command_list)} "
+                         f"were given!")
+    if command_list[0] != "cp":
+        raise ValueError(f"Expected 'cp' as the first argument, "
+                         f"{command_list[0]} was given!")
+
     file_orig_name = command_list[1]
     file_copy_name = command_list[2]
 
