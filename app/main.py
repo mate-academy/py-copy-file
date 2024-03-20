bd918f423d6fabe388040553d49b5326941c7340
Kate Arzhevikina <arzhevikina96@gmail.com>
2024-03-21 00:34:26 +0200

    added condition checks

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 def copy_file(command: str) -> None:
+    if len(command.split()) != 3 or command.split()[0] != "cp":
+        return
+
     input_file, output_file = command.split()[1], command.split()[2]
     if input_file != output_file:
         with (
@@ -6,8 +9,9 @@ def copy_file(command: str) -> None:
             open(output_file, "w") as copied
         ):
             copied.write(to_copy.read())
-    return
 
 
 copy_file("cp file.txt file.txt")
+copy_file("cp file.txt file1.txt file2.txt")
+copy_file("file.txt file.txt")
 copy_file("cp file.txt new_file.txt")
