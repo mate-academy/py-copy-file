69b00e9d48dabf90682995f36cf2bb40cafd2b3d
alexk <alexkochubey13@gmail.com>
2024-03-20 14:50:18 +0200

    Solution?

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_split = command.split(" ")
+    if command_split[0] != "cp":
+        return
+    file_name = command_split[1]
+    file_copy = command_split[2]
+    with (open(file_name, "r") as original,
+            open(file_copy, "w") as copy):
+        copy.write(original.read())
+
