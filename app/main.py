09745c228037e13b30eaa7a54bb3afa2da734bc0
OKochubeii <155968394+OKochubeii@users.noreply.github.com>
2024-03-21 13:57:10 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 def copy_file(command: str) -> None:
-    command_split = command.split(" ")
-    if command_split[0] != "cp":
+
+    command_name, file_name, file_copy = command.split()
+
+    if command_name != "cp":
         return
-    file_name = command_split[1]
-    file_copy = command_split[2]
+
     with (open(file_name, "r") as original,
             open(file_copy, "w") as copy):
         copy.write(original.read())
