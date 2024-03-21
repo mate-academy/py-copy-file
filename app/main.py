d8f1d63a029ff5c0e58b07055832b08d7a1121dd
vova-stoliar <vova.stoliar@overdose.digital>
2024-03-21 20:16:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_to_copy_path, destination_file_to_copy_path = command.split(" ")[1:]
+
+    with (open(file_to_copy_path, "r") as file_in,
+          open(destination_file_to_copy_path, "w") as file_out):
+        file_out.write(file_in.read())
