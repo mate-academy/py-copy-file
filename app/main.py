c6a5ed89a4e73dbc52833d7d2b5965731c7e131e
Olha <olgazinevych@gmail.com>
2024-03-22 18:38:03 +0200

    Added conditions to check the command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
-    file_names = command.split(" ")
-    if file_names[1] != file_names[2]:
+    file_names = command.split()
+    if (len(file_names) == 3 and file_names[0] == "cp"
+            and file_names[1] != file_names[2]):
         with (open(file_names[1], "r") as file_in,
               open(file_names[2], "w") as file_out):
             lines = file_in.read()
