3fd79725ca4651f42647e39f8c628307c643fb36
Khrystenko_Denys <deonis062002@gmail.com>
2024-03-25 22:00:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 def copy_file(text: str) -> None:
-    command, file_name_1, file_name_2 = text.split()
-    print(command, file_name_1, file_name_2)
-    if command == "cd" and file_name_1 != file_name_2:
-        with open(file_name_1, "r") as source, open(file_name_2, "w") as copy:
-            copy.write(source.read())
-
-
-copy_file("cd ark.txt beke.txt")
+    if text.split() == 3:
+        command, file_name_1, file_name_2 = text.split()
+        if command == "cd" and file_name_1 != file_name_2:
+            with open(file_name_1, "r") as source, open(file_name_2, "w") as copy:
+                copy.write(source.read())
