da98b74c08588b5893cdb3ddd5e6c8ae40c9ca64
Khrystenko_Denys <deonis062002@gmail.com>
2024-03-25 22:08:40 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,6 @@ def copy_file(text: str) -> None:
     if text.split() == 3:
         command, file_name_1, file_name_2 = text.split()
         if command == "cd" and file_name_1 != file_name_2:
-            with open(file_name_1, "r") as source, open(file_name_2, "w") as copy:
+            with open(file_name_1, "r") as source, \
+                    open(file_name_2, "w") as copy:
                 copy.write(source.read())
