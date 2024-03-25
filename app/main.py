ca777282302b4e02f012cf76b7acc4a5f8efdb8e
Vitalii <warcied1@gmail.com>
2024-03-25 11:58:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 def copy_file(command: str) -> None:
 
-    command_content = command.split(" ")
+    command_content = command.split()
 
     if len(command_content) == 3:
-        command, filename_in, filename_out = command_content
+        command_name, filename_in, filename_out = command_content
 
-        if command == "cp" and filename_in != filename_out:
+        if command_name == "cp" and filename_in != filename_out:
             with (open(filename_in, "r") as file_in,
                   open(filename_out, "w") as file_out):
                 file_out.write(file_in.read())
