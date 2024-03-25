37c3281ab89fccf024969a864c09887536429580
Yevheniia Sosnovska <sosnovskaevgeniya.2504@gmail.com>
2024-03-25 19:36:00 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_parts = command.split()
+    if command_parts[0] != "cp":
+        return
+
+    source_file = command_parts[1]
+    destination_file = command_parts[2]
+
+    if source_file == destination_file:
+        return
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
+        file_out.write(file_in.read())
