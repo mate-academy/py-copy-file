692f995380bb55b4915af8fe79f9dc43fff9d1ca
Sribniak Taras <sribniak.terry@protonmail.com>
2024-03-25 15:08:48 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+
+
+def copy_file(file_name: str, new_file: str) -> None:
+    if file_name == new_file:
+        return
+    with open(file_name, "r") as file_in, open(new_file, "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
