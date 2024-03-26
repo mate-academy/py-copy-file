56c302e67110bd0fd33d1327422a42f28a53ef6c
Mykola Setrakov <nikset1@gmail.com>
2024-03-26 10:13:24 +0200

    rewrite in one line

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 def copy_file(command: str) -> None:
-    command = command.split()
-    if len(command) == 3:
-        command_name = command[0]
-        file_copy = command[1]
-        file_new = command[2]
+
+    if len(command.split()) == 3:
+        command_name, file_copy, file_new = command.split()
 
         if command_name == "cp" and file_new != file_copy:
 
@@ -11,5 +9,4 @@ def copy_file(command: str) -> None:
                 open(file_copy, "r") as file_in,
                 open(file_new, "w") as file_out
             ):
-                data = file_in.read()
-                file_out.write(data)
+                file_out.write(file_in.read())
