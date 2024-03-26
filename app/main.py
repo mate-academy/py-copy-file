dbe122134e93b0b67399a0c9fd243198f93b63c9
Andrii <andreboiko1@gmail.com>
2024-03-26 10:19:19 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
-    command, file, copied_file = command.split(" ")
+    command, file, copied_file = command.split("")
+    if command not in "cp":
+        raise ValueError(f"Not correct command: {command}")
     if file != copied_file:
         with (open(file, "r") as reading_file,
               open(copied_file, "w") as writing_file):
-            reading_file_data = reading_file.read()
-            writing_file.write(reading_file_data)
+            writing_file.write(reading_file.read())
