b72c2b603cf0468413409cc0e6a5532437d93fe3
bujhm <Bujhm99@gmail.com>
2024-03-26 23:51:52 +0200

    No index, no space, check not cp

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
-    name_massive = command.split(" ")
-    if name_massive[1] != name_massive[2]:
-        with (open(name_massive[1], "r") as file_in,
-              open(name_massive[2], "w") as file_out):
+    command, name_in, name_out = command.split()
+    if command != "cp":
+        print("I don't know this command")
+    if name_in != name_out:
+        with (open(name_in, "r") as file_in,
+              open(name_out, "w") as file_out):
             file_out.write(file_in.read())
