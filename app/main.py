d7bbf3393b5ef9ea1c787b9d3755e06c27ac58e4
Sergiy Yurov <urovsergej10@gmail.com>
2024-03-27 10:51:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def coopy_file(command: str) -> None:
+    elements = command.split(" ")
+
+    if len(elements) < 3 and elements[1] != "cp":
+        print("Invalid command. Usage: cp source.file destination.file")
+
+    source_file, destination_file = elements[1], elements[2]
+
+    if source_file != destination_file:
+        with open(source_file, "r") as file_in, open(destination_file,
+                                                     "w") as file_out:
+            file_out.write(file_in.read())
+            print(f"File {file_in} copied to {file_out}."
+                  f" Successfully")
+    else:
+        pass
