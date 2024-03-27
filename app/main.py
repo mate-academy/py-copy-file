99b5d58a26ac7616b1fba3aad9c3c600c994b3f6
Tymur Shmorhun <shmorguntimur@gmail.com>
2024-03-27 17:00:15 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
-    ls = command.split(" ")
-    if ls[1] == ls[2]:
-        return
-    with open(ls[1], "r") as file_in, open(ls[2], "w") as file_out:
-        file_out.write(file_in.read())
+    files = command.split()
+    if len(files) == 3:
+        if files[1] == files[2]:
+            return
+        with open(files[1], "r") as file_in, open(files[2], "w") as file_out:
+            file_out.write(file_in.read())
+    return
