7d53e8f49c7b12bc2927562ba8cfc146fa968c05
= <=>
2024-03-29 16:12:10 +0200

    fixed for flake8

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_file(command: str) -> None:
     command_list = [file for file in command.split(" ")]
     if command_list[1] != command_list[2]:
-        with open(command_list[1], "r") as file_in, open(command_list[2], "w") as file_out:
+        with open(command_list[1], "r") as \
+                file_in, open(command_list[2], "w") as file_out:
             file_out.write(file_in.read())
