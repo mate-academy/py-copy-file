0dedb58723077aa6b5a3bd20b5606732afc10bc0
= <=>
2024-03-29 16:42:50 +0200

    fixed warnings

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    command_list = [file for file in command.split(" ")]
-    if command_list[1] != command_list[2]:
+    command_list = command.split()
+    if command_list[1] != command_list[2] and command_list[0] == "cp":
         with open(command_list[1], "r") as \
                 file_in, open(command_list[2], "w") as file_out:
             file_out.write(file_in.read())
