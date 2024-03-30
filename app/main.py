9033d51747085b6e0c73ef1944551f345ea7efa2
Shershen Alexandra <hornet240204@gmail.com>
2024-03-30 15:39:40 +0200

    Solution for task copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    commands = command.split(" ")
+
+    source_file, destination_file = commands[1], commands[2]
+
+    if source_file == destination_file:
+        return
+
+    file_in = open(source_file, "r")
+    file_out = open(destination_file, "w")
+    file_out.write(file_in.read())
