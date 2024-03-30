11ea27800402e7d2eb5c24dabca69d254e5b8679
Maksym Hekalo <maximgekalo@gmail.com>
2024-03-30 17:29:19 +0200

    validate of length commands parts

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 
 def copy_file(command: str) -> None:
     command_parts = command.split(" ")
-    main_file_name = command_parts[1]
-    new_file_name = command_parts[2]
-    if (new_file_name != main_file_name
-            and command_parts[0] == "cp"):
-        with (
-            open(main_file_name, "r") as main_file,
-            open(new_file_name, "w") as new_file
-        ):
-            content_of_general_file = main_file.read()
-            new_file.write(content_of_general_file)
+    if len(command_parts) == 3:
+        main_file_name = command_parts[1]
+        new_file_name = command_parts[2]
+        if (new_file_name != main_file_name
+                and command_parts[0] == "cp"):
+            with (
+                open(main_file_name, "r") as main_file,
+                open(new_file_name, "w") as new_file
+            ):
+                content_of_general_file = main_file.read()
+                new_file.write(content_of_general_file)
     return
