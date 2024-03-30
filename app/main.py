e96ca6fa37dc0945d039970960109425155135a0
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-03-30 12:37:10 +0100

    reconstruction

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
     commands = command.split()
-    if len(commands) != 3 or commands[0] != "cp" or commands[1] == commands[2]:
-        return
-
-    _, copied_file, new_file = commands
-    with open(copied_file, "r") as c_file, open(new_file, "w") as n_file:
-        n_file.write(c_file.read())
+    if len(commands) == 3:
+        cp, copied_file, new_file = commands
+        if cp == "cp" and copied_file != new_file:
+            with (open(copied_file, "r") as c_file,
+                  open(new_file, "w") as n_file):
+                n_file.write(c_file.read())
