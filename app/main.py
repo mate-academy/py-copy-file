1843db3e64b9274a9921d8cf73d07911d839cecf
Mikhail Yemets <mikhailyemets.pl@gmail.com>
2024-03-30 17:54:07 +0100

    fix structure

--- app/main.py
+++ app/main.py
@@ -2,5 +2,4 @@ def copy_file(command: str) -> None:
     files = command.split(" ")
     if files[1] != files[2]:
         with open(files[1], "r") as file_in, open(files[2], "w") as file_out:
-            for_copy = file_in.read()
-            file_out.write(for_copy)
+            file_out.write(file_in.read())
