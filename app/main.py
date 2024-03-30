99512c620538121b763aabbb1058030396186907
Shershen Alexandra <hornet240204@gmail.com>
2024-03-30 15:51:51 +0200

    Remove function call

--- app/main.py
+++ app/main.py
@@ -6,10 +6,6 @@ def copy_file(command: str) -> None:
     if source_file == destination_file:
         return
 
-    with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
         file_out.write(file_in.read())
-
-
-if __name__ == "__main__":
-    copy_file("cp file.txt new_file.txt")
-    print(open("file.txt").read() == open("new_file.txt").read())  # True
