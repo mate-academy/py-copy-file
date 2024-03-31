a3b6605e65b3f0ecf7ad26d99b0f6f0d2435c5f8
Mikhail Yemets <mikhailyemets.pl@gmail.com>
2024-03-31 11:42:28 +0200

    add one more command format check

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 def copy_file(command: str) -> None:
     if len(command.split(" ")) < 3:
         raise TypeError("Incorrect command format")
+    if (not command.split(" ")[1].endswith(".txt")
+            or not command.split(" ")[2].endswith(".txt")):
+        raise TypeError("Incorrect command format")
 
     cp_command, input_file_name, output_file_name = command.split(" ")
+
     if input_file_name != output_file_name:
         with (open(input_file_name, "r") as file_in,
               open(output_file_name, "w") as file_out):
             file_out.write(file_in.read())
-
