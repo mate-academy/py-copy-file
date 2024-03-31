54b841fe82c181b3eadff59ad6e5072f9c7d5bc4
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-03-31 10:55:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_comps = command.split()
+    if command_comps[0] == "cp" and command_comps[1] != command_comps[2]:
+        with (open(command_comps[1], "r") as file_obj,
+              open(command_comps[2], "w") as copy_file_obj):
+            copy_file_obj.write(file_obj.read())
+            file_obj.close()
+            copy_file_obj.close()
