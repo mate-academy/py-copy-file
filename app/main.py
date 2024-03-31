276f787aeeb67f0e29e87042b994f165ebd5f52e
Vladyslav Prokopiv <prokopiv.vladyslav@gmail.com>
2024-03-31 12:38:10 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        copy, old_file, new_file = command.split()
+
+        if old_file != new_file or copy == "copy":
+            with (
+                open(old_file, "r") as file_int,
+                open(new_file, "w") as file_out
+            ):
+                file_out.write(file_int.read())
+    except Exception:
+        print(
+            "Invalid command, must be like:"
+            "cp file_name.txt new_file_name.txt"
+        )
