612fbbd00df72616792d1dfcee4c9566cfb206b4
NioviIki <charmniovi@tutanota.com>
2024-03-31 21:48:25 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+"""
+I thought that such decisions might not be fair
+
+import subprocess
+def copy_file(command: str):
+    subprocess.run(command, shell=True)
+
+import os
+def copy_file(command: str):
+    os.system(command)
+"""
+
+
+def copy_file(command: str) -> None:
+    if command[:3] == "cp":
+        raise ValueError(
+            "Expected 'cp' as the first argument"
+        )
+    command = command[3:]
+    source_parts = command.split(".")
+
+    source_file = source_parts[0] + "." + source_parts[1].split()[0]
+    destination_file = command[len(source_file) + 1:]
+
+    if not destination_file or len(destination_file.split(".")) > 2:
+        raise ValueError(
+            "Expected 3 arguements"
+        )
+
+    if source_file != destination_file:
+        with (
+            open(source_file, "r") as source,
+            open(destination_file, "w") as destination
+        ):
+            destination.write(source.read())
