7335ac06a67ebc5fc26642eff5453342afb7a46c
bwaah1 <chykivan@gmail.com>
2024-04-01 02:42:17 +0300

    Implement Copy file task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split()
+
+    if command_list[0] != "cp":
+        return
+
+    source_file, destination_file = command_list[1], command_list[2]
+
+    if source_file == destination_file:
+        return
+
+    with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
+        file_out.write(file_in.read())
