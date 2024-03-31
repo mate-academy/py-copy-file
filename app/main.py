f359b9db7b07843bc5589087ee6c9f54da037b0c
Sofiia <sonagm9@gmail.com>
2024-04-01 00:26:24 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3:
+        print("Error: Invalid command format")
+        return
+    _, source_file, dest_file = parts
+
+    if source_file == dest_file:
+        return
+
+    with open(source_file, "r") as file_in, open(dest_file, "w") as file_out:
+        file_out.write(file_in.read())
