ebaa7a9d8b677c159832720ef9c6edeb40c18041
kekW <panicua12@gmail.com>
2024-04-01 13:58:01 +0300

    fixed returns>raises, removed None type annotation

--- app/main.py
+++ app/main.py
@@ -1,20 +1,17 @@
 import shutil
 
 
-def copy_file(command: str) -> None | str:
+def copy_file(command: str) -> str:
     command_list = command.split()
     if command_list[0] == "cp" and len(command_list) == 3:
         _, first_file, second_file = command_list
     else:
-        return f"Wrong command line please double check it: {command}"
+        raise f"Wrong command line please double check it: {command}"
 
     try:
         shutil.copy(first_file, second_file)
         return f"File '{second_file}' was created"
     except FileNotFoundError:
-        return f"File '{first_file}' doesn't exist"
+        raise f"File '{first_file}' doesn't exist"
     except OSError:
-        return f"Invalid file name '{second_file}'"
-    except Exception:
-        """Incase some unknown error still exist that I don't know of"""
-        raise Exception
+        raise f"Invalid file name '{second_file}'"
