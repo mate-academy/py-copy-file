a504e16a36058e0f5d2bfbf273c6b174ebb002bb
kekW <panicua12@gmail.com>
2024-04-01 09:57:47 +0300

    fixes, changed os module to shutil, added try/except, added condition

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
-def copy_file(command: str) -> None:
-    command_list = command.split(" ")
-    first_file, second_file = command_list[1], command_list[2]
+import shutil
 
-    if first_file != second_file:
-        with (open(first_file, "r") as file_from,
-              open(second_file, "w") as file_to):
-            file_to.write(file_from.read())
+
+def copy_file(command: str) -> None | str:
+    command_list = command.split()
+    if command_list[0] == "cp" and len(command_list) == 3:
+        _, first_file, second_file = command_list
+    else:
+        return f"Wrong command line please double check it: {command}"
+
+    try:
+        shutil.copy(first_file, second_file)
+        return f"File '{second_file}' was created"
+    except FileNotFoundError:
+        return f"File '{first_file}' doesn't exist"
+    except OSError:
+        return f"Invalid file name '{second_file}'"
+    except Exception:
+        """Incase some unknown error still exist that I don't know of"""
+        raise Exception
