5d4b32e74ec9e48da41972a1f7bb3f5ae0ee2840
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-04-01 08:35:55 +0200

    Added an exceptions

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
+import shutil
+
+
 def copy_file(command: str) -> None:
-    commands = command.split()
-    if len(commands) == 3:
-        cp, copied_file, new_file = commands
+    copied_file = new_file = None
+    try:
+        cp, copied_file, new_file = command.split()
+
         if cp == "cp" and copied_file != new_file:
-            with (open(copied_file, "r") as c_file,
-                  open(new_file, "w") as n_file):
-                n_file.write(c_file.read())
+            shutil.copyfile(copied_file, new_file)
+    except FileNotFoundError:
+        print(f"Error: One of the files '{copied_file}' or '{new_file}' does not exist.")
+    except ValueError:
+        print("Error: Command format is incorrect. Expected format: 'cp <source> <destination>'.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
