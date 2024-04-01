2d0ce797299406d54a763d3f41e5ca601164e527
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-04-01 08:36:53 +0200

    fixed flake

--- app/main.py
+++ app/main.py
@@ -9,8 +9,10 @@ def copy_file(command: str) -> None:
         if cp == "cp" and copied_file != new_file:
             shutil.copyfile(copied_file, new_file)
     except FileNotFoundError:
-        print(f"Error: One of the files '{copied_file}' or '{new_file}' does not exist.")
+        print(f"Error: One of the files '{copied_file}' "
+              f"or '{new_file}' does not exist.")
     except ValueError:
-        print("Error: Command format is incorrect. Expected format: 'cp <source> <destination>'.")
+        print("Error: Command format is incorrect. "
+              "Expected format: 'cp <source> <destination>'.")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
