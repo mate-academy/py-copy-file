696a2591f1a89023416d443b0f7280784f1840a4
Oleksandr Moroz <molecxander@gmail.com>
2024-04-01 17:26:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 def copy_file(command: str) -> None:
-    parts = command.split()
-    _, old_file, new_file = parts
-    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-        file_out.write(file_in.read())
+    if len(command.split()) == 3:
+        linux_command, old_file, new_file = command.split()
+    else:
+        raise "Need write command, copied file, new file"
+    if linux_command == "cp":
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
+    else:
+        raise "Command not found"
