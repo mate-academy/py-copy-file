0653ef77b8fddc625ff1d1a9acc1ce268a779b31
siaserb <assiya.serb@gmail.com>
2024-04-02 02:49:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    function, file_in_name, file_out_name = command.split()
+    if (function == "cp"
+            and file_in_name.endswith(".txt")
+            and file_out_name.endswith(".txt")
+            and file_in_name != file_out_name):
+
+        with (open(file_in_name, "r") as file_in,
+              open(file_out_name, "w") as file_out):
+            file_out.write(file_in.read())
