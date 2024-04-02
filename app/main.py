26aea5a8cced8c52639a78f8427dc901a39fcb8d
Ivan Chyk <119738911+bwaah1@users.noreply.github.com>
2024-04-02 09:59:29 +0300

    Fix copy_file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     cp, source_file, destination_file = command.split()
 
-    if cp == "cp" or source_file != destination_file:
+    if cp == "cp" and source_file != destination_file:
         with (open(source_file, "r") as file_in,
               open(destination_file, "w") as file_out):
             file_out.write(file_in.read())
