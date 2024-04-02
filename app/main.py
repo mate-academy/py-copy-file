b063b283015b387a8c3621830b22c7f35f403abc
Liudmyla <4888814@gmail.com>
2024-04-02 15:41:37 +0300

    'solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    files_name = command.split(" ")
+    if files_name[1] == files_name[2]:
+        return
+
+    with (open(files_name[1], "r") as file_in,
+          open(files_name[2], "w") as files_name[2]):
+        text = file_in.read()
+        files_name[2].write(text)
