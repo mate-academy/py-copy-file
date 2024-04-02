d01ca4da8a6de9d2177495f95852c3835ad35acf
Vladyslav Prymas <prymas.vladyslav@gmail.com>
2024-04-02 12:40:37 +0300

    Made code simplier

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 def copy_file(command: str) -> None:
-    command = command.split()
-    file_name1 = command[1]
-    file_name2 = command[2]
-    if file_name1 == file_name2 and command[0] == "cp":
+    command_name, file_name1, file_name2 = command.split()
+    if file_name1 == file_name2 and command_name == "cp":
         return
     with open(file_name1, "r") as file_in, open(file_name2, "w") as file_out:
-        for line in file_in.readlines():
-            file_out.write(line)
+        file_out.write(file_in.read())
