6d94e31cd298e0fc69efba7c77441017f0348acd
semermaks <semermaks@mail.com>
2024-04-02 09:45:01 +0300

    Fix parameters names

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def copy_file(files_name: str) -> None:
-    files_name_arr = files_name.split(" ")
+    cp_parameter, old_file_name, new_file_name = files_name.split()
 
-    if len(files_name_arr) != 3:
+    if cp_parameter != "cp":
         print("Invalid command format. "
               "Please use 'cp old_file new_file'")
         return
 
-    if files_name_arr[1] != files_name_arr[2]:
-        with (open(files_name_arr[1], "r") as first_file,
-              open(files_name_arr[2], "w") as second_file):
-            second_file.write(first_file.read())
+    if old_file_name != new_file_name:
+        with (open(old_file_name, "r") as old_file,
+              open(new_file_name, "w") as new_file):
+            new_file.write(old_file.read())
