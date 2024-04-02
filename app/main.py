5c56f860b79a11e2de193d695e26ad7b9d7ba005
Vladyslav Prokopiv <prokopiv.vladyslav@gmail.com>
2024-04-01 23:27:37 -0600

    учexpand exeptions inside  function

--- app/main.py
+++ app/main.py
@@ -8,8 +8,10 @@ def copy_file(command: str) -> None:
                 open(new_file, "w") as file_out
             ):
                 file_out.write(file_int.read())
-    except Exception:
+    except ValueError:
         print(
             "Invalid command, must be like:"
             "cp file_name.txt new_file_name.txt"
         )
+    except FileNotFoundError:
+        print("Your file that needs to copied can not be found")
