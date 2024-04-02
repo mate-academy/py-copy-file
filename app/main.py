0dc65111c523068a3ef25b264d31afd67b5b74c1
Alona Sorochynska <alona.sorochynska@gmail.com>
2024-04-02 10:45:00 +0100

    add 1 condition, change variations assignment

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def copy_file(command: str) -> None:
-    command_separate = command.split()
-    file_name1 = command_separate[1]
-    file_name2 = command_separate[2]
-    if file_name1 != file_name2:
+    action, file_name1, file_name2 = command.split()
+    if action == "cp" and file_name1 != file_name2:
         with open(file_name1, "r") as file1, open(file_name2, "w") as file2:
             file2.write(file1.read())
