6f31eb9a8cb7b20a21d7f7c02b167c70082c4d7c
Anton Kudriavtsev <bryanfinchnzt@gmail.com>
2024-04-03 08:05:45 -0700

    Solution for py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    orig_file, new_file = command[3:].split(" ")
+    if orig_file == new_file:
+        return
+    with open(orig_file, "r") as f1, open(new_file, "w") as f2:
+        for line in f1.readlines():
+            f2.write(line)
