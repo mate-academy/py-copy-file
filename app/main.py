4a6e273bbb2fcb1fbab3340763d6ec4c4482468c
Githi54 <chinter.games1@gmail.com>
2024-04-03 22:44:42 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command) < 3:
+        return
+    operation, file_name_to_copy, new_file_name = command.split(" ")
+
+    if file_name_to_copy == new_file_name or operation != "cp":
+        return
+
+    with (open(file_name_to_copy, "r") as file_in,
+          open(new_file_name, "w") as file_out):
+        file_out.write(file_in.read())
