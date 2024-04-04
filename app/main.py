0625d8e84389fb8bc37d7a226274e9dd1505bebe
siaserb <assiya.serb@gmail.com>
2024-04-05 01:13:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
-def copy_file(command: str) -> None:
-    function, file_in_name, file_out_name = command.split()
-    if (function == "cp"
-            and file_in_name.endswith(".txt")
-            and file_out_name.endswith(".txt")
-            and file_in_name != file_out_name):
+import shutil
 
-        with (open(file_in_name, "r") as file_in,
-              open(file_out_name, "w") as file_out):
-            file_out.write(file_in.read())
+
+def copy_file(command: str) -> None:
+    command_list = command.split()
+    if len(command_list) == 3:
+        function, file_in_name, file_out_name = command_list
+        if (function == "cp"
+                and file_in_name.endswith(".txt")
+                and file_out_name.endswith(".txt")
+                and file_in_name != file_out_name):
+            try:
+                shutil.copy(file_in_name, file_out_name)
+            except FileNotFoundError:
+                print(f"File '{file_in_name}' not found.")
+            except PermissionError:
+                print(f"Permission denied to copy '{file_in_name}' to '{file_out_name}'.")
+            except Exception as e:
+                print(f"An error occurred while copying the file: {e}")
