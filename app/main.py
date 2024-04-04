5bcfcdbd6dfe7537026facca6d4040ba7bf66e64
Vladyslav Prokopiv <prokopiv.vladyslav@gmail.com>
2024-04-03 21:33:18 -0600

    Fix copy statement and add statement that check command len

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 def copy_file(command: str) -> None:
-    try:
-        copy, old_file, new_file = command.split()
+    if len(command.split()) == 3:
+        try:
+            copy, old_file, new_file = command.split()
 
-        if old_file != new_file or copy == "copy":
-            with (
-                open(old_file, "r") as file_int,
-                open(new_file, "w") as file_out
-            ):
-                file_out.write(file_int.read())
-    except ValueError:
-        print(
-            "Invalid command, must be like:"
-            "cp file_name.txt new_file_name.txt"
-        )
-    except FileNotFoundError:
-        print("Your file that needs to copied can not be found")
+            if old_file != new_file or copy == "cp":
+                with (
+                    open(old_file, "r") as file_int,
+                    open(new_file, "w") as file_out
+                ):
+                    file_out.write(file_int.read())
+        except ValueError:
+            print(
+                "Invalid command, must be like:"
+                "cp file_name.txt new_file_name.txt"
+            )
+        except FileNotFoundError:
+            print("Your file that needs to copied can not be found")
