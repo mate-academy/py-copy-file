3893d3b231053892fdcee4efabf2d1eb36d11988
onyevyerov <onyevyerov@gmail.com>
2024-04-05 20:23:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,5 +5,6 @@ def copy_file(command: str) -> None:
         return
 
     if file_for_copy != new_file:
-        with open(file_for_copy, "r") as file_in, open(new_file, "w") as file_out:
+        with (open(file_for_copy, "r") as file_in,
+              open(new_file, "w") as file_out):
             file_out.write(file_in.read())
