c801a3dff39382c844777bcec4ae436e9b65e8f4
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-04-05 20:45:48 +0300

    edited the split method

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    list_of_str = command.split(" ")
+    list_of_str = command.split()
     old_file_n = list_of_str[0]
     new_file_n = list_of_str[1]
     if old_file_n == new_file_n:
