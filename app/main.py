7afac6255ee240fdcefe42c7dd139e2e0ea539e7
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-04-05 20:52:38 +0300

    renamed the variables

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
-    list_of_str = command.split()
-    old_file_n = list_of_str[0]
-    new_file_n = list_of_str[1]
-    if old_file_n == new_file_n:
+    list_of_names = command.split()
+    old_file = list_of_names[0]
+    new_file = list_of_names[1]
+    if old_file == new_file:
         return
-    with open(old_file_n, "r") as file_in, open(new_file_n, "w") as file_out:
+    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
         content = file_in.read()
         file_out.write(content)
