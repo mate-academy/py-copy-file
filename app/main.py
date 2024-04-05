7caa9517c79c94767ecfc99438b2ca476a3fa53d
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-04-05 21:20:25 +0300

    fixed unpacking

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     list_of_names = command.split()
     if list_of_names[0] == "cp":
-        old_file, new_file = list_of_names[1], list_of_names[2]
+        old_file, new_file = list_of_names[1:]
         if old_file == new_file:
             return
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
