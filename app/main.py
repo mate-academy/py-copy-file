531476d70efe229eabe27e842bb563c3eb871d50
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-04-05 21:15:00 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
     list_of_names = command.split()
-    old_file = list_of_names[0]
-    new_file = list_of_names[1]
-    if old_file == new_file:
-        return
-    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-        content = file_in.read()
-        file_out.write(content)
+    if list_of_names[0] == "cp":
+        old_file, new_file = list_of_names[1], list_of_names[2]
+        if old_file == new_file:
+            return
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            content = file_in.read()
+            file_out.write(content)
