61081cc95381872874e05dfb1ae697c3916e12b6
Vitalii <vitalikbashkiser@gmail.com>
2024-04-06 18:30:27 +0300

    Solution#6 (refactored logic and removed unnecessary comments and print statements)

--- app/main.py
+++ app/main.py
@@ -2,32 +2,25 @@ import shutil
 
 
 def copy_file(command: str) -> None:
-    # Split the command string into its components
     parts = command.split()
 
-    # Validate if the command is correct and the number of arguments is correct
-    if len(parts) != 3 or parts[0] != "cp":
+    if len(parts) == 3 and parts[0] == "cp":
+        _, old_file, new_file = parts
+
+        if old_file == new_file:
+            print(
+                "The source and destination filenames are the same. "
+                "Nothing to do."
+            )
+        else:
+            try:
+                shutil.copy(old_file, new_file)
+            except FileNotFoundError:
+                print(f"Error: File '{old_file}' not found.")
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+    else:
         print(
             "Invalid command format or command. "
             "Please provide command in the format: cp old_file new_file"
         )
-        return
-
-    _, old_file, new_file = parts
-
-    # Validate if the source and destination filenames are the same
-    if old_file == new_file:
-        print(
-            "The source and destination filenames are the same. "
-            "Nothing to do."
-        )
-        return
-
-    try:
-        # Copy the file using shutil
-        shutil.copy(old_file, new_file)
-        print(f"File '{old_file}' copied to '{new_file}' successfully.")
-    except FileNotFoundError:
-        print(f"Error: File '{old_file}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
