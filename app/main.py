4083c50a00d6fca647727ab41600b4a5c40feaea
Liubomyr Midzhak <lmidzhak@gmail.com>
2024-04-06 12:03:33 +0300

    срcheck 3 argument condition

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ def copy_file(command: str) -> None:
     file_command = command.split()
     first_file = file_command[1]
     second_file = file_command[2]
-    if file_command[0] == "cp" and first_file != second_file:
-        with (open(first_file, "r") as file1,
-              open(second_file, "w") as file2):
-            file2.write(file1.read())
+    if len(file_command) == 3:
+        if file_command[0] == "cp" and first_file != second_file:
+            with (open(first_file, "r") as file1,
+                  open(second_file, "w") as file2):
+                file2.write(file1.read())
