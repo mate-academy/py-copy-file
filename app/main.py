117c19e374ddd23e72545950e9309bb2368892ee
serhii_chaban <boetz1995@gmail.com>
2024-04-07 19:27:38 +0300

    fix2

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 def copy_file(command: str) -> None:
     parts_of_command = command.split()
-    if "cp" in parts_of_command:
-        parts_of_command.remove("cp")
-    if len(parts_of_command) == 2:
-        file_source, file_new = parts_of_command
+    if len(parts_of_command) == 3 and "cp" in parts_of_command:
+        _, file_source, file_new = parts_of_command
     if file_source != file_new:
         with open(file_source, "r") as source, open(file_new, "w") as copied:
             copied.write(source.read())
