9b93d7b52de6347b0a8bcd993b239e828c74ba6a
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-04-07 21:20:28 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 def copy_file(command: str) -> None:
     command = command.split()
+
+    if len(command) != 3:
+        return
+
+    if command[0] != "cp":
+        return
+    if command[1] == command[2]:
+        return
+
     with (
-        open(command[1], "r") as f1,
-        open(command[2], "w") as f2
+        open(command[1], "r") as file1,
+        open(command[2], "w") as file2
     ):
-        f2.write(f1.read())
+        file2.write(file1.read())
