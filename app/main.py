5532a0c77128ee78f1ef23dac2fec67f5a53df1b
Sasha <binsasha97@gmail.com>
2024-04-09 13:48:08 +0300

    first try

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(str_files: str) -> None:
+
+    first_file = str_files.split(" ")[1]
+    another_file = str_files.split(" ")[-1]
+    if first_file == another_file:
+        return None
+
+    with open(first_file, "r") as file_in, open(another_file, "w") as file_out:
+        file_out.write(file_in.read())
