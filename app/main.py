62b4018f132f09a5bd5a2f2e0d2c3a6a578c5a97
Yaroslav Ivanov <165368790+nrjss67@users.noreply.github.com>
2024-04-09 20:37:34 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
     if len(command.split()) == 3:
-        old_file, new_file = command.split()[1:]
+        comm, old_file, new_file = command.split()
 
-    if old_file != new_file:
+    if old_file != new_file and comm = "cp":
         with (open(old_file, "r") as from_file,
               open(new_file, "w") as to_file):
             to_file.write(from_file.read())
