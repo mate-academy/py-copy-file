074e94e2fe8a362c15f484be14b25e3bbd157a51
Sasha <binsasha97@gmail.com>
2024-04-09 15:13:22 +0300

    last

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 def copy_file(command: str) -> None:
-    if len(command.split()) == 3 and command.split()[0] == 'cp':
+    if len(command.split()) == 3 and command.split()[0] == "cp":
         first_file = command.split(" ")[1]
         another_file = command.split(" ")[-1]
         if first_file == another_file:
             return None
 
-        with open(first_file, "r") as file_in, open(another_file, "w") as file_out:
+        with (open(first_file, "r") as file_in,
+              open(another_file, "w") as file_out):
+
             file_out.write(file_in.read())
 
 
