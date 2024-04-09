6daa1c39ecbcb4432be2814f2b04a2e74a17fac5
Sasha <binsasha97@gmail.com>
2024-04-09 15:11:47 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-def copy_file(str_files: str) -> None:
+def copy_file(command: str) -> None:
+    if len(command.split()) == 3 and command.split()[0] == 'cp':
+        first_file = command.split(" ")[1]
+        another_file = command.split(" ")[-1]
+        if first_file == another_file:
+            return None
 
-    first_file = str_files.split(" ")[1]
-    another_file = str_files.split(" ")[-1]
-    if first_file == another_file:
-        return None
+        with open(first_file, "r") as file_in, open(another_file, "w") as file_out:
+            file_out.write(file_in.read())
 
-    with open(first_file, "r") as file_in, open(another_file, "w") as file_out:
-        file_out.write(file_in.read())
+
+copy_file("cp text.txt new_text.txt")
