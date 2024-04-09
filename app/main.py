567cfba6de3e95916215f7c325a3844cbb157c72
Pavlo <pavlo@MacBook-Pro-de-Pavlo.local>
2024-04-09 16:05:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cmd, file1, file2 = command.split(" ")
+
+    if file1 == file2:
+        return
+
+    if cmd == "cp":
+        with open(file1, "r") as file_in, open(file2, "w") as file_out:
+            file_out.write(file_in.read())
+
+
+if __name__ == "__main__":
+    copy_file("cp filename.txt filename_new.txt")
