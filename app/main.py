9334e67257ca11aa57748fc1a4f60e4da28e3786
Heorhii Zashchuk <ggeorgiyy@gmail.com>
2024-04-09 14:29:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    spl_com = command.split(" ")
+    with open(spl_com[1], "r") as file_in, open(spl_com[2], "w") as file_out:
+        if file_in == file_out:
+            pass
+        else:
+            shutil.copyfile(file_in, file_out)
+    file_out.close()
