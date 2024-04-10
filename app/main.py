a9b93854b976cc60de8c8149a4d51ffdbc927b55
Heorhii Zashchuk <ggeorgiyy@gmail.com>
2024-04-10 22:13:40 +0300

    New solution

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ import shutil
 
 
 def copy_file(command: str) -> None:
-    spl_com = command.split(" ")
-    with open(spl_com[1], "r") as file_in, open(spl_com[2], "w") as file_out:
-        if file_in == file_out:
-            pass
-        else:
-            shutil.copyfile(file_in, file_out)
-    file_out.close()
+    path = command.split(" ")
+    if len(path) == 3:
+        with open(path[1], "r") as file_in, open(path[2], "w") as file_out:
+            if file_in == file_out:
+                pass
+            else:
+                shutil.copyfile(file_in, file_out)
+        file_out.close()
