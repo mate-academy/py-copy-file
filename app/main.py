0262a288c89086f27a261bad72460abdd34472e5
Volkov <volkovyegorandriiovich@gmail.com>
2024-04-11 04:24:53 +0300

    copy_function_v1.0

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    """
+    this function designed for copying two files
+    :param command: "cp file_to_copy.format new_file.format"
+    """
+    file_1 = command.split(" ")[1]
+    file_2 = command.split(" ")[2]
+    with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
+        file_out.write(file_in.read())
