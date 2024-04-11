6ddbdc8a2a95518a2c7c1946a2059f9edeaebc0d
Tymur Shmorhun <shmorguntimur@gmail.com>
2024-04-11 12:55:56 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
     files = command.split()
     if len(files) != 3 or files[1] == files[2] or files[0] != "cp":
+        print("Error: command is invalid")
         return
     with open(files[1], "r") as file_in, open(files[2], "w") as file_out:
         file_out.write(file_in.read())
