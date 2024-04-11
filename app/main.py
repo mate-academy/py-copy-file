79db472b0d222c5ae01fcaa98ff21f2119b36aed
Mikola Rozhansky <bymikola@gmail.com>
2024-04-11 23:02:07 +0300

    Done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        cmd = command.split()
+        command_name = cmd[0]
+        source_name = cmd[1]
+        copy_name = cmd[2]
+    except Exception:
+        print("Error in command argument!")
+        return None
+
+    if command_name == "cp":
+        if source_name != copy_name:
+            with open(source_name) as source, open(copy_name, "w") as copy:
+                data = source.read()
+                copy.write(data)
+
+
