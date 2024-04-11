533bbea237b3b2af0e28cd3a2d413e08561feb8b
Mikola Rozhansky <bymikola@gmail.com>
2024-04-11 23:37:18 +0300

    Done new unpack

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 def copy_file(command: str) -> None:
     try:
         cmd = command.split()
-        command_name = cmd[0]
-        source_name = cmd[1]
-        copy_name = cmd[2]
+        command_name, source_name, copy_name = cmd
     except Exception:
         print("Error in command argument!")
-        return None
+        return
 
-    if command_name == "cp":
-        if source_name != copy_name:
-            with open(source_name) as source, open(copy_name, "w") as copy:
-                data = source.read()
-                copy.write(data)
+    if (command_name == "cp") and (source_name != copy_name):
+        with open(source_name) as source, open(copy_name, "w") as copy:
+            data = source.read()
+            copy.write(data)
