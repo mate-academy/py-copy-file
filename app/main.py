c60a39e36df76f0a77540688abd7bc2ea4dd1340
MRGL <Sonemongoo@gmail.com>
2024-04-14 17:33:23 +0300

    'Fix_2'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
     if command.count(" ") != 2:
         return
-    _, file1, file2 = command.split(" ")
-    if file1 == file2:
+    command, file1, file2 = command.split(" ")
+    if file1 == file2 or command != "cp":
         return
     with open(file1, "r") as f1, open(file2, "w") as f2:
         f2.write(f1.read())
