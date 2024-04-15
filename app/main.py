14402797bf5bf93e656f981ffd9c5b9d52d42092
Heorhii Zashchuk <ggeorgiyy@gmail.com>
2024-04-15 20:25:35 +0300

    Solution with message

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ import shutil
 
 
 def copy_file(command: str) -> None:
-    path = command.split(" ")
+    path = command.split()
     if len(path) == 3:
         with open(path[1], "r") as file_in, open(path[2], "w") as file_out:
             if file_in == file_out:
-                pass
+                print("Your command don't allow to copy file")
             else:
                 shutil.copyfile(file_in, file_out)
         file_out.close()
