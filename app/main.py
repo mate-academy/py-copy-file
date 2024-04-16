3cdb2dfc91d5cf60159f9322e6add1373cec2c3e
yes <yes@yess-MacBook-Pro.local>
2024-04-16 22:09:21 +0300

    commit #...

--- app/main.py
+++ app/main.py
@@ -3,7 +3,15 @@ from sys import argv
 
 
 def copy_file(command: str) -> None:
-    chdir(path.dirname(argv[0]))
-    cm = command.split(" ")
-    return None if (len(cm) == 1) or (cm[1] == cm[2]) \
-        else open(cm[2], "w").write(open(cm[1], "r").read())
+    parts = command.split(" ")
+    if len(parts) < 3:
+        print("invalid command format")
+    else:
+        source = parts[1]
+        new = parts[2]
+        if source == new:
+            print(f"source file name:{source} is same to new file name: {new}")
+        else:
+            with open(source, "r") as src, open(new, "w") as new:
+                content = src.read()
+                new.write(content)
