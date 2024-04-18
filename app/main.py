7d8cb6b3d2ffcf14d552883c97db98232df7d163
Viktor Zhdanovych <cryingtm27@gmail.com>
2024-04-18 15:26:24 +0300

    added check

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 def copi_file(command: str) -> None:
     command_parts = command.split()
-    if command_parts[0] == "cp" and command_parts[1] != command_parts[2]:
+    if len(command_parts) == 3\
+            and command_parts[0] == "cp"\
+            and command_parts[1] != command_parts[2]:
+
         with (open(command_parts[1], "r") as file_in,
               open(command_parts[2], "w") as file_out):
             file_out.write(file_in.read())
