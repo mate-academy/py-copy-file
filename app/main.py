5083c5bf1d564df79460c761845dc317629b1ba6
Oleh <musiychuk09@gmail.com>
2024-04-19 22:47:33 +0300

    add check the command have 3 parameteres

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(text: str) -> None:
     text = text.split()
-    if "cp" in text[0] and text[1] == text[2]:
+    if len(text) == 3 and text[1] == text[2]:
         print("Does nothing")
-    if "cp" in text[0] and text[1] != text[2]:
+    if len(text) == 3 and text[1] != text[2]:
         open(text[2], "w").write(open(text[1], "r").read())
