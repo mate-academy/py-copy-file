04e3a9c6332a6e42f3d03e9b1fa512a3da5ef7ab
Oleh <musiychuk09@gmail.com>
2024-04-19 20:40:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(text: str):
+    text = text.split()
+    if "cp" in text[0] and text[1] == text[2]:
+        print("Does nothing")
+    if "cp" in text[0] and text[1] != text[2]:
+        open(text[2], "w").write(open(text[1], "r").read())
+
+
