f7cd7e91fa4e3fee487db2606080878d80857aef
Oleh <musiychuk09@gmail.com>
2024-04-19 20:42:29 +0300

    flake8

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
-def copy_file(text: str):
+def copy_file(text: str) -> None:
     text = text.split()
     if "cp" in text[0] and text[1] == text[2]:
         print("Does nothing")
     if "cp" in text[0] and text[1] != text[2]:
         open(text[2], "w").write(open(text[1], "r").read())
-
-
