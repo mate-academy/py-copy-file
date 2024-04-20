b2b938edfecada02fb222935eeb874153027375d
Oleh <musiychuk09@gmail.com>
2024-04-20 14:09:20 +0300

    Add raise

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(text: str) -> None:
     text = text.split()
     if len(text) != 3 or text[0] != "cp" or text[1] == text[2]:
-        print("Wrong command")
-        return
+        raise Exception("Wrong command")
     with open(text[2], "r") as file_in, open(text[1], "w") as file_out:
         file_out.write(file_in.read())
