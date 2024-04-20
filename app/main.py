15a14f5493dd53bac0cd0e1ed8c01c6ae9a47129
Maryna Barannik <sober2night@gmail.com>
2024-04-20 23:08:58 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    source_file = parts[1]
+    destination_file = parts[2]
+    if (len(parts) == 3 and parts[0] == "cp"
+            and source_file != destination_file):
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
+            file_out.write(file_in.read())
+    return
