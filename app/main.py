092c5664f4ec823ad8816c84c9ab301a4363b436
Roman Zaliznyi <romanzaliznyi.python@gmail.com>
2024-04-25 22:31:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,9 +2,8 @@ def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) == 3:
-        source_file = parts[1]
-        destination_file = parts[2]
-        if parts[0] == "cp" and source_file != destination_file:
+        cmd, source_file, destination_file = parts
+        if cmd == "cp" and source_file != destination_file:
 
             with (
                 open(source_file, "r") as file_in,
