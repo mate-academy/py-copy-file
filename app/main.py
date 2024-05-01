22ebf57ae3049acd6df308e02ffeebe0daa29e00
Nazar <legoman1858@gmail.com>
2024-05-02 00:01:12 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    splitted_input = command.split()
+    if splitted_input[1] != splitted_input[2]:
+        with (open(splitted_input[1], "r") as file_in,
+              open(splitted_input[2], "w") as file_out):
+            file_out.write(file_in.read())
