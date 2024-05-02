d049f31db273b08a6a5c5e0df3003872b64547fc
Nazar <legoman1858@gmail.com>
2024-05-02 10:31:52 +0300

    solution fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
     splitted_input = command.split()
-    if splitted_input[1] != splitted_input[2]:
+    if (len(splitted_input) == 3
+            and splitted_input[0] == "cp"
+            and splitted_input[1] != splitted_input[2]):
         with (open(splitted_input[1], "r") as file_in,
               open(splitted_input[2], "w") as file_out):
             file_out.write(file_in.read())
