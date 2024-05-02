d823686a9ac3f76435f9b66568cc12526e30a014
daikj <tyutyunniksolomia@gmail.com>
2024-05-02 17:38:07 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(cmd_name: str) -> None:
+    cp_part, old_file, new_file = cmd_name.split()
+
+    if cp_part != "cp":
+        return
+
+    if old_file == new_file:
+        return
+
+    try:
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError as e:
+        print(f"Error: Source file not found: {e}")
