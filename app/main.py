c7e091d224c985b891fd6b9da66a92ab3af8523b
Volodymyr Shutov <forshutov@gmail.com>
2024-05-02 17:35:52 +0400

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    split_command = [word for word in command.split(" ")]
+    command = split_command[0]
+    file_name_in = split_command[1]
+    file_name_out = split_command[2]
+
+    if (
+            len(split_command) == 3
+            and command == "cp"
+            and file_name_in != file_name_out
+    ):
+
+        with (
+            open(file_name_in, "r") as file_in,
+            open(file_name_out, "w") as file_out
+        ):
+            file_out.write(file_in.read())
