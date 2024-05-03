ac39b55844f2f8143d0585af28010e392e29f927
Viktor Shcherbak <dev.psychologist@gmail.com>
2024-05-03 21:24:46 +0300

    Copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command_line: str) -> None:
+
+    command_list = command_line.split()
+    command = command_list[0]
+    first_file = command_list[1]
+    second_file = command_list[2]
+
+    if command == "cp":
+        if first_file != second_file:
+            with (open(first_file, "r") as file_in,
+                  open(second_file, "w") as file_out):
+                content = file_in.read()
+                file_out.write(content)
