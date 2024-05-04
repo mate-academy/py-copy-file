7082b45d8dabee5a3e75909d94997564450ecbe5
Triukhan <tryukhan.d@gmail.com>
2024-05-04 21:53:50 +0300

    triukhan solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    splitted_command = command.split(" ")
+    if splitted_command[0] != "cp":
+        print("Undefined command")
+        return
+    if splitted_command[1] == splitted_command[2]:
+        return
+    with open(splitted_command[1], "r") as file_in, \
+         open(splitted_command[2], "w") as file_out:
+        text = file_in.read()
+        file_out.write(text)
