a7401ad24b3b84b7d950f5ac5d72fd1a3d9d5b9d
Anna Shevtsova <aniashevtsova@gmail.com>
2024-05-07 15:52:58 +0200

    completed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    new_list = command.split(" ")
+    old_name = new_list[1]
+    new_name = new_list[2]
+
+    if old_name == new_name:
+        return
+
+    with (open(f"{old_name}", "r") as file_in,
+          open(f"{new_name}", "w") as file_out):
+        content = file_in.read()
+        file_out.write(content)
