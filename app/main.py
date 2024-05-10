950e19dbd82e7b1f2ee19e9751a2c95bdc08b299
Your_Name Your_Surname <your@email>
2024-05-10 23:37:30 +0300

    fix if statement

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 def copy_file(command: str) -> None:
     arguments = command.split()
-    if len(arguments) == 3 and not (arguments[1] == arguments[2]):
+
+    if (len(arguments) == 3
+            and arguments[0] == "cp"
+            and not (arguments[1] == arguments[2])):
         source_file, destination_file = arguments[1:]
+
         with (open(source_file, "r") as source,
               open(destination_file, "w") as destination):
             destination.write(source.read())
