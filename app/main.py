436d9067cacc7c7aef2716542aacd90eabcdf6e7
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-05-10 21:31:58 +0300

    Solution+

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 def copy_file(command: str) -> None:
     parts = command.split()
+    source_file, destination_file = parts[1], parts[2]
+    if len(parts) != 3 or parts[0] != "cp" or source_file == destination_file:
+        return
 
-    with open(parts[1], "r") as file_in, open(parts[2], "w") as file_out:
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
         file_out.write(file_in.read())
--- app/test_copy_file.py
+++ app/test_copy_file.py
@@ -2,4 +2,4 @@ from main import copy_file
 
 
 def test_copy_file() -> None:
-    copy_file("cp requirements.txt d.txt")
+    copy_file("cp requirements.txt a.txt")
