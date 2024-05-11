ea4bba2d8db1568a9bd196550e0ab69c147a34ae
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-05-11 14:12:26 +0300

    final Solution maybe

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
 def copy_file(command: str) -> None:
     parts = command.split()
     source_file, destination_file = parts[1:]
-    if len(parts) != 3 or parts[0] != "cp":
-        print("Please use the format: cp copy_file.txt new_file.txt")
-        return
-    if source_file == destination_file:
-        print("Source and destination files are the same. Nothing to copy.")
-        return
-
-    with (open(source_file, "r") as file_in,
-          open(destination_file, "w") as file_out):
-        file_out.write(file_in.read())
+    if (len(parts) == 3 and parts[0] == "cp"
+            and source_file != destination_file):
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
+            file_out.write(file_in.read())
