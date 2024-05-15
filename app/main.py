6e29fa9cd42c65f3001366be14abea75f88a31b3
Olena Zaritska <zaritskaos@gmail.com>
2024-05-15 13:26:04 +0300

    py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    action, file_to_copy, new_file = command.split()
+    if file_to_copy == new_file:
+        pass
+    else:
+        with open(file_to_copy, "r") as f1, open(new_file, "w") as f2:
+            f2.write("".join(f1.readlines()))
