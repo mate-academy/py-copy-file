75b15d7cd1860b0bdb34de77eb23c9c1eb7c1c11
Olena Zaritska <zaritskaos@gmail.com>
2024-05-16 10:52:05 +0300

    add checking command

--- app/main.py
+++ app/main.py
@@ -1,7 +1,31 @@
+class WrongCommandError(Exception):
+    pass
+
+
 def copy_file(command: str) -> None:
-    action, file_to_copy, new_file = command.split()
+    try:
+        action, file_to_copy, new_file = command.split()
+        if action != "cp":
+            raise WrongCommandError
+    except ValueError:
+        print(
+            "The command should contain cp "
+            "and names of two files separated by spaces"
+        )
+        raise
+    except WrongCommandError:
+        print("The command should start with cp")
+        raise
+
     if file_to_copy == new_file:
         pass
     else:
-        with open(file_to_copy, "r") as f1, open(new_file, "w") as f2:
-            f2.write("".join(f1.readlines()))
+        try:
+            with open(file_to_copy, "r") as f1, open(new_file, "w") as f2:
+                f2.write("".join(f1.readlines()))
+        except FileNotFoundError:
+            print(
+                "The files names should be correct "
+                "and the files should exist in the current directory"
+            )
+            raise
