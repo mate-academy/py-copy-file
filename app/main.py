fec5f14428f7683c8fca7cebdee0a5d76ad44dba
Olena Zaritska <zaritskaos@gmail.com>
2024-05-16 13:32:51 +0300

    implement changes

--- app/main.py
+++ app/main.py
@@ -18,14 +18,16 @@ def copy_file(command: str) -> None:
         raise
 
     if file_to_copy == new_file:
-        pass
-    else:
-        try:
-            with open(file_to_copy, "r") as f1, open(new_file, "w") as f2:
-                f2.write("".join(f1.readlines()))
-        except FileNotFoundError:
-            print(
-                "The files names should be correct "
-                "and the files should exist in the current directory"
-            )
-            raise
+        return
+    try:
+        with (
+            open(file_to_copy, "r") as file_in,
+            open(new_file, "w") as file_out
+        ):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        print(
+            "The files names should be correct "
+            "and the files should exist in the current directory"
+        )
+        raise
