3232ddf7ec5f30497d7e1539f39ef5fc9748613d
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-05-19 22:42:04 +0300

    Hell yeah solution. Refactoring mith custom logic 4

--- app/main.py
+++ app/main.py
@@ -5,8 +5,6 @@ def copy_file(command: str) -> None:
             open(command[1], "r") as read_file,
             open(command[2], "w") as write_file
         ):
-            for line in read_file.readlines():
-                write_file.write(line)
+            write_file.write(read_file.read())
         return
     print("Invalid command")
-    return
