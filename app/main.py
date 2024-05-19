cd40980773e9cc3f9c73a74d8cfa55ff69cc8fa9
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-05-19 19:41:48 +0300

    Hell yeah solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+    if len(command) == 3 and command[0] == "cp":
+        shutil.copy(command[1], command[2])
+        return
+    print("Invalid command")
+    return
