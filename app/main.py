85bd3f2111e3888e6272ca3839fdae10d61ab765
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-05-19 20:56:50 +0300

    Hell yeah solution. Refactoring mith custom logic

--- app/main.py
+++ app/main.py
@@ -4,7 +4,14 @@ import shutil
 def copy_file(command: str) -> None:
     command = command.split(" ")
     if len(command) == 3 and command[0] == "cp":
-        shutil.copy(command[1], command[2])
+        with (
+            open(command[1], "r") as read_file,
+            open(command[2], "w") as write_file
+        ):
+            for line in read_file.readlines():
+                write_file.write(line)
         return
     print("Invalid command")
     return
+
+copy_file("cp 1.txt 2.txt")
