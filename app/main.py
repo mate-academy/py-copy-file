4d92e7e9d3c26c5fb21c6ccd5c84d3db1953bf15
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-05-20 23:23:55 +0300

    update

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
-    old_file_name = command.split(" ")[1]
-    new_file_name = command.split(" ")[2]
+    command_split = command.split(" ")
+    old_file_name = command_split[1]
+    new_file_name = command_split[2]
     if old_file_name == new_file_name:
         return
     with (open(old_file_name, "r") as old_file,
