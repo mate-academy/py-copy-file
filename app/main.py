026a107926c2034c486191b5dd755cb4f90bd1c2
Dima Drozdenko <anonimchik920@gmail.com>
2024-05-21 18:26:01 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 def copy_file(command: str) -> None:
-    command_split = command.split(' ')
+    command_split = command.split(" ")
     command_old = command_split[1]
     command_new = command_split[2]
 
     if command_new == command_old:
         exit()
 
-    with open(command_old, 'r') as old_file, open(command_new, 'w') as new_file:
+    with (open(command_old, "r") as old_file,
+          open(command_new, "w") as new_file):
         content = old_file.read()
         new_file.write(content)
