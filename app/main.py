e2c4ee2f284ac6fcd9130c7b38258afab372f67e
Dima Drozdenko <anonimchik920@gmail.com>
2024-05-21 18:32:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 def copy_file(command: str) -> None:
+    if len(command) > 4:
+        exit()
     command_split = command.split(" ")
     command_old = command_split[1]
     command_new = command_split[2]
