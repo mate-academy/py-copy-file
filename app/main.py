0e491c383cac6c7c17cdefa1790f47452510a17e
Dima Drozdenko <anonimchik920@gmail.com>
2024-05-22 11:45:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,10 +4,12 @@ import os
 def copy_file(command: str) -> None:
     command_split = command.split()
     if len(command_split) != 3 or command[0] != "copy":
+        print("Error: You need to provide exactly 2 arguments")
         return
     _, old_file_name, new_file_name = command_split
 
     if old_file_name == new_file_name:
+        print("Error: The name of the new and old file is the same")
         return
     if not os.path.exists(old_file_name):
         print(f"Source file '{old_file_name}' does not exist.")
