ec452164e0c323e9ac25acb3ba4594c519c9900a
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-05-22 18:27:37 +0300

    added more checks and refactored

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 def copy_file(command: str) -> None:
-    prefix = command.split()[0]
-    file_name = command.split()[1]
-    copy_file_name = command.split()[2]
-    if file_name != copy_file_name and prefix == "cp":
-        with (
-            open(file_name, "r") as file_in,
-            open(copy_file_name, "w") as file_out
-        ):
-            file_out.write(file_in.read())
+    command_ls = command.split()
+    if len(command_ls) == 3:
+        prefix = command.split()[0]
+        file_name = command.split()[1]
+        copy_file_name = command.split()[2]
+        if prefix == "cp" and file_name != copy_file_name:
+            with (
+                open(file_name, "r") as file_in,
+                open(copy_file_name, "w") as file_out
+            ):
+                file_out.write(file_in.read())
