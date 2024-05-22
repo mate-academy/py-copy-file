31d0e01fac96fac41a9486b59e3d38df06da5788
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-05-22 18:19:26 +0300

    added solution to copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    prefix = command.split()[0]
+    file_name = command.split()[1]
+    copy_file_name = command.split()[2]
+    if file_name != copy_file_name and prefix == "cp":
+        with (
+            open(file_name, "r") as file_in,
+            open(copy_file_name, "w") as file_out
+        ):
+            file_out.write(file_in.read())
