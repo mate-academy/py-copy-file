d37a24b9aa28eaea2739a45a261837f572a02c8c
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-05-23 15:56:29 +0300

    fixed after review

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 def copy_file(command: str) -> None:
     command_ls = command.split()
     if len(command_ls) == 3:
-        prefix = command.split()[0]
-        file_name = command.split()[1]
-        copy_file_name = command.split()[2]
-        if prefix == "cp" and file_name != copy_file_name:
+        command, old_file, new_file = command.split()
+        if command == "cp" and old_file != new_file:
             with (
-                open(file_name, "r") as file_in,
-                open(copy_file_name, "w") as file_out
+                open(old_file, "r") as file_in,
+                open(new_file, "w") as file_out
             ):
                 file_out.write(file_in.read())
