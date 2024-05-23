3a82bd4343f1301c41ef1ace2ca7ec9093c3fe50
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-05-23 16:47:22 +0300

    fixed after review

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
-    command_ls = command.split()
-    if len(command_ls) == 3:
-        command, old_file, new_file = command.split()
+    command_list = command.split()
+    if len(command_list) == 3:
+        command, old_file, new_file = command_list
         if command == "cp" and old_file != new_file:
             with (
                 open(old_file, "r") as file_in,
