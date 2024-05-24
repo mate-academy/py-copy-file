6debf38185b54cc9fa9cba7463ec191e032f3957
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-05-24 23:34:09 +0200

    Message for errors

--- app/main.py
+++ app/main.py
@@ -4,9 +4,11 @@ import os
 def copy_file(command: str) -> None:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "cp":
+        print("Error: The command format is incorrect")
         return
     _, source, destination = parts
     if source == destination:
+        print("Error: Source and destination files cannot be the same.")
         return
     if not os.path.exists(source):
         print(f"Source file '{source}' does not exist.")
@@ -15,3 +17,5 @@ def copy_file(command: str) -> None:
           open(destination, "w") as destination_file):
         content = source_file.read()
         destination_file.write(content)
+
+    print(f"File '{source}' has been copied to '{destination}'.")
