3f5e270a670c3106174d77a75f53c4a2233003d3
AndrewwSSS <clash2clans1one@gmail.com>
2024-05-26 14:01:00 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    args = command.split(" ")
+    if args[1] == args[2] or args[0] != "cp":
+        return
+    with (open(args[1], "r") as file_read,
+          open(args[2], "w") as file_write):
+        buffer = file_read.read(256)
+        while buffer:
+            file_write.write(buffer)
+            buffer = file_read.read(256)
+
+
+copy_file("cp test.txt aboba.txt")
