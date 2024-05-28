2f924e1824ea1fc5cec4708b7ec506d4987e7f9b
AndrewwSS <clash2clans1one@gmail.com>
2024-05-28 16:12:21 +0300

    develop

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
+BUFFER_SIZE = 256
+
+
 def copy_file(command: str) -> None:
-    args = command.split(" ")
-    if args[1] == args[2] or args[0] != "cp":
+    command, first_path, second_path = command.split(" ")
+    if first_path == second_path or command != "cp":
         return
-    with (open(args[1], "r") as file_read,
-          open(args[2], "w") as file_write):
-        buffer = file_read.read(256)
+    with (open(first_path, "r") as file_read,
+          open(second_path, "w") as file_write):
+        buffer = file_read.read(BUFFER_SIZE)
         while buffer:
             file_write.write(buffer)
-            buffer = file_read.read(256)
-
-
-copy_file("cp test.txt aboba.txt")
+            buffer = file_read.read(BUFFER_SIZE)
