1d14f7a6c205551fb8b04da7cc2b8e5729af3718
Igor Kolisnyk <moonbosszzz@gmail.com>
2024-05-28 19:43:01 +0200

    The solution of the task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 # write your code here
 def copy_file(command: str) -> None:
-    command, original_file, copy_file = command.split(" ")
+    command, original_file, copied_file = command.split(" ")
     if command == "cd" or original_file != copy_file:
-        with open(original_file, "r") as original, open(copy_file, "w") as copy:
+        with open(original_file, "r") as original, open(copied_file, "w") as copy:
             copy.write(original.read())
