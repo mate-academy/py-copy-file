8735c60ad725117a57dfdf65cf6877893460e3d4
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-05-30 12:36:08 +0300

    py-copy-file v3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def copy_file(command: str) -> None:
-    split_command = command.split()
-    with (open(split_command[1], "r") as file_in,
-          open(split_command[2], "w") as file_out):
+    with (open(command.split()[1], "r") as file_in,
+          open(command.split()[2], "w") as file_out):
         file_out.write(file_in.read())
