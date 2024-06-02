aa13f1083475f01ce719f2079d55c2e81ae0ce70
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-06-02 14:12:01 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None | bool:
+    command = command.split(" ")[1:]
+    if command[0] != command[1]:
+        with (open(command[0], "r") as file_in,
+              open(command[1], "w") as file_out):
+            file_out.write(file_in.read())
+        if open("file.txt").read() == open("new_file.txt").read():
+            return True
+    return
