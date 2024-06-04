7371fdd24460a7393887e41deaa08840a4546bf5
Yurii Yurchenko <fuceqwertyt@gmail.com>
2024-06-04 08:45:47 +0300

    new line fix

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    command, first_file, second_file = command.split(" ")
+    command, first_file, second_file = command.split()
     if first_file == second_file or command != "cp":
         return
     with open(first_file, "r") as file_in, open(second_file, "w") as file_out:
