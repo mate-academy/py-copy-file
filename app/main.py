cc6bf2cd9a0104317064958363420de0bd629127
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-06-04 18:47:44 +0300

    fix some issue

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(command: str) -> None:
-    commands_list = command.split()
-    if commands_list[0] == "cp":
-        if commands_list[1] != commands_list[2]:
-            with (open(commands_list[1], "r") as source_file,
-                  open(commands_list[2], "w") as output_file):
-                output_file.write(source_file.read())
+    cp, source, output = command.split()
+    if cp == "cp" and source != output:
+        with (open(source, "r") as source_file,
+              open(output, "w") as output_file):
+            output_file.write(source_file.read())
