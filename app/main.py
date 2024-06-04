a58a91a27f35ea61812c4b660f90f70b44289ab8
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-06-04 14:56:32 +0200

    some fix v2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 def copy_file(command: str) -> None | bool:
-    command = command.split()[1:]
-    input_file, output_file = command[0], command[1]
+    input_file, output_file = command.split()[1:3]
     if input_file != output_file:
         with (open(input_file, "r") as file_in,
               open(output_file, "w") as file_out):
             file_out.write(file_in.read())
-        if open("file.txt").read() == open("new_file.txt").read():
-            return True
     return
