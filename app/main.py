378e4d614c1fd43edf4c1dbff9acc2cfc2979817
Vlad <vlad@example.com>
2024-06-04 20:06:58 +0300

    fixed code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def copy_file(command: str) -> None:
     command, file1, file2 = command.split()
-    if file1 == file2:
-        return None
-    with open(file1) as file1, open(file2, "w") as file2:
-        file2.write(file1.read())
+    if file1 != file2 and command == "cp":
+        with open(file1) as file1, open(file2, "w") as file2:
+            file2.write(file1.read())
