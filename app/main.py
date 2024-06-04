09f0472c6c2f5a5110a804574d3ec5dd9f593d4d
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-06-04 08:56:20 +0300

    add solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    commands_list = command.split()
+    if commands_list[0] == "cp":
+        if commands_list[1] != commands_list[2]:
+            with (open(commands_list[1], "r") as source_file,
+                  open(commands_list[2], "w") as output_file):
+                output_file.write(source_file.read())
