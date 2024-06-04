8038fccd07c2f1587cd431c62870f91d9b32c2c4
Alex Bul <tarasov57228@gmail.com>
2024-06-04 18:57:22 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split(" ")
+    if "cp" in command_list or command_list[1] != command_list[2] or len(command_list) != 3:
+        return
+    with open(f"{command_list[1]}", "r") as file, open(f"{command_list[2]}", "w") as new_file:
+        new_file.write(f"{file.read()}")
