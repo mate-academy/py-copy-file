f1620931e07b180235680e76e9a12f6200577b76
Oleh Deshko <kochaminny@gmail.com>
2024-06-04 19:15:02 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    com_from = command[1]
-    com_to = command[2]
-    if com_from[1] == com_to[2]:
-        pass
-    with open(com_from, "r") as in_, open(com_to, "a") as out_:
-        out_.write(in_.read())
+    from_file = command[1]
+    to_file = command[2]
+    if from_file != to_file:
+        with open(from_file, "r") as file_in, open(to_file, "a") as file_out:
+            file_out.write(file_in.read())
