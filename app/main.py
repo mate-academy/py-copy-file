bc1ff457758c3a2992b6eea614fee568ae704b61
unlie9 <unlie.devops9@gmail.com>
2024-06-04 18:48:21 +0300

    sol2

--- app/main.py
+++ app/main.py
@@ -3,14 +3,10 @@ def copy_file(command: str) -> None:
     copy_file = command[1]
     new_file = command[2]
     if copy_file == new_file:
-        new_file = "new_" + command[2]
+        return
 
-    content = []
-    with open(copy_file) as f:
-        lines = f.readlines()
-
-        for line in lines:
-            content.append(line)
-
-    with open(new_file, "w") as f:
-        f.writelines(content)
+    with (
+        open(copy_file) as file_in,
+        open(new_file, "w") as file_out
+    ):
+        file_out.writelines(file_in.readlines())
