8673809e6fd0cb8c3dda498f9d4d60c239cee2ca
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-06-04 14:56:38 +0300

    Solution V2

--- app/main.py
+++ app/main.py
@@ -4,5 +4,6 @@ def copy_file(command: str) -> bool:
         old_file, new_file = parts[1], parts[2]
 
         if old_file != new_file:
-            with open(old_file, "r") as file_old, open(new_file, "w") as file_new:
+            with (open(old_file, "r") as file_old,
+                  open(new_file, "w") as file_new):
                 file_new.write(file_old.read())
