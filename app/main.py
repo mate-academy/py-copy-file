514328b2e090fab48d49c84355c93ef7708477c4
Alex Bul <tarasov57228@gmail.com>
2024-06-04 19:12:35 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,5 +4,6 @@ def copy_file(command: str) -> None:
     to_file = command_list[2]
     if "cp" in command_list or from_file != to_file or len(command_list) != 3:
         return
-    with open(f"{from_file}", "r") as file, open(f"{to_file}", "w") as new_file:
+    with (open(f"{from_file}", "r") as file,
+          open(f"{to_file}", "w") as new_file):
         new_file.write(f"{file.read()}")
