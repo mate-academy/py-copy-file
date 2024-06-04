49b23f851bd4f13bce6560197fef8f75079c611d
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-06-04 12:46:53 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str) -> None:
     command = command.split(" ")
     name_from = command[1]
     name_to = command[2]
-    if command[1] == command[2]:
+    if name_from == name_to:
         pass
     with open(name_from, "r") as file_in, open(name_to, "w") as file_out:
         file_out.write(file_in.read())
