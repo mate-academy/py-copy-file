ca57442780eae26db2162d742d3174b1e9357074
unlie9 <unlie.devops9@gmail.com>
2024-06-04 16:52:25 +0300

    sol1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+    copy_file = command[1]
+    new_file = command[2]
+    if copy_file == new_file:
+        new_file = "new_" + command[2]
+
+    content = []
+    with open(copy_file) as f:
+        lines = f.readlines()
+
+        for line in lines:
+            content.append(line)
+
+    with open(new_file, "w") as f:
+        f.writelines(content)
