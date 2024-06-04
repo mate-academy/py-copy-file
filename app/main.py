af4c0ca0a82ac76f0d3c2b042e6c9a7f25a8d144
Oleh Deshko <kochaminny@gmail.com>
2024-06-04 16:30:09 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -4,5 +4,5 @@ def copy_file(command: str) -> None:
     com_to = command[2]
     if com_from[1] == com_to[2]:
         pass
-    with open(com_from, "r") as f_in, open(com_to, "a") as f_out:
-        f_out.write(f_in.read())
+    with open(com_from, "r") as in_, open(com_to, "a") as out_:
+        out_.write(in_.read())
