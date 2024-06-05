2b3f85b790dfc503f75696c3b6fdf05e7d002678
TkachukTara <zaz42988@gmail.com>
2024-06-05 10:41:35 +0300

    Done Task!

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 def copy_file(command: str) -> None:
     commands = command.split()
-    if commands[1] != commands[2] and commands[0] == "cp":
-        with (open(commands[1], "r") as file_in,
-              open(commands[2], "w") as file_out):
+    source_file = commands[1]
+    destination_file = commands[2]
+
+    if source_file != destination_file and commands[0] == "cp":
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
             file_out.write(file_in.read())
