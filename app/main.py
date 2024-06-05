ed10f04c84d05f7e9f84027eb2fff787d06fdc9b
Alex Bul <tarasov57228@gmail.com>
2024-06-05 19:59:16 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def copy_file(command: str) -> None:
     command_name, file_in, file_out, *_ = command.split()
-    if ("cp" in command_name
-            or file_in != file_out
-            or len(command_name) != 3):
+    if command_name == "cp" and file_in != file_out:
         return
     with (open(f"{file_in}", "r") as file,
           open(f"{file_out}", "w") as new_file):
