65a05defdfe9c805f04125f105648e74fd769185
Alex Bul <tarasov57228@gmail.com>
2024-06-06 17:10:04 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
-def copy_file(command: str) -> None:
-    command_name, file_in, file_out, *_ = command.split()
-    if command_name == "cp" and file_in != file_out:
-        return
+def copy_file(command: str) -> str:
+    command = command.split()
+    if command[0] != "cp":
+        return "the file does not contain 'cp'"
+    elif len(command) != 3:
+        return "must have three arguments"
+    file_in, file_out, *_ = command
+
+    if file_in == file_out:
+        return "cannot be copied to the same file"
+
     with (open(f"{file_in}", "r") as file,
           open(f"{file_out}", "w") as new_file):
         new_file.write(file.read())
