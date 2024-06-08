206b72f6cd50620713d5e41a52ba74045aa8de37
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-08 21:19:45 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cmd_list = command.split()
+    if cmd_list[1] != cmd_list[2]:
+        with (open(cmd_list[1], "r") as old_f,
+              open(cmd_list[2], "w") as new_f):
+            new_f.write(old_f.read())
