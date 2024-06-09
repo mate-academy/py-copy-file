4312398d4264b8872ad1e3f7b0f0e179f4aa430d
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-09 14:56:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ def copy_file(command: str) -> None:
     cmd_list = command.split()
 
     if (
-        (cmd_list[0] == "cp")
-        and (len(cmd_list) == 3)
+        (len(cmd_list) == 3)
+        and (cmd_list[0] == "cp")
         and (cmd_list[1] != cmd_list[2])
     ):
         with (open(cmd_list[1], "r") as old_f,
