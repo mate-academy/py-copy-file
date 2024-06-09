bcb19e35da820f576960ae2f48dac64bf6ac7e5b
mykyta <nikita.fedotov222@gmail.com>
2024-06-09 16:08:26 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split()
+    if len(split_command) < 3:
+        print("Invalid value")
+        return
+    if split_command[1] != split_command[2]:
+        with open(split_command[1], 'r') as r, open(split_command[2], "a") as w:
+            w.write(r.read())
