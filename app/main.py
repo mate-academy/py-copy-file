50802a9ef10d103012205c87ac78c00d9d72d4d5
Kolya <nikomail2023@gmail.com>
2024-06-17 00:06:52 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) == 3 and parts[0] == "cp":
-        source_file, destination_file = parts[1], parts[2]
+        source_file, destination_file = parts[1:]
 
         if source_file != destination_file:
             with (
@@ -10,10 +10,3 @@ def copy_file(command: str) -> None:
                 open(destination_file, "w") as file_out
             ):
                 file_out.write(file_in.read())
-            print(f"File {source_file} "
-                  f"copied to {destination_file}")
-
-
-copy_file("cp file.txt file.txt")  # Does nothing
-
-copy_file("cp file.txt new_file.txt")
