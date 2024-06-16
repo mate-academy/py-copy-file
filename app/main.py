24e096505a6d28558416848fc38d63bdb34798b4
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-16 21:23:32 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    first_file, second_file = command[3:].split(" ")
+    if first_file == second_file:
+        pass
+    else:
+        content = []
+        with open(first_file, "r") as file_data:
+            content = file_data.read()
+        with open(second_file, "w") as file_copy:
+            file_copy.write(content)
