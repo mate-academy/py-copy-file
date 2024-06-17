e4e420e2d50a1778900ff1e38ca3dced4b63e5bf
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-17 18:26:14 +0200

    fix line

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 def copy_file(command: str) -> None:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "cp" or parts[1] == parts[2]:
-        print("Data isn't correct, please enter other name command or other names files")
+        print("Data isn't correct,"
+              " please enter other"
+              " name command or other names files")
     else:
         content = []
         with open(parts[1], "r") as file_data:
