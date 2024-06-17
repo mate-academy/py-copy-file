2d08f5b67c001f7db15c206672342f5d7bf800c4
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-06-17 16:22:23 +0300

    Fix solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
+class CopyCommandError(Exception):
+    pass
+
+
+class InvalidCommandError(CopyCommandError):
+    pass
+
+
+class SameFileError(CopyCommandError):
+    pass
+
+
 def copy_file(command: str) -> None:
-    file_names = command.split(" ")
+    file_names = command.split()
+    if len(file_names) != 3 or file_names[0] != "cp":
+        raise InvalidCommandError("The command is incorrect!")
     if file_names[1] == file_names[2]:
-        return None
+        raise SameFileError("It is the same file!")
     with (open(file_names[1], "r") as file_in,
           open(file_names[2], "w") as file_out):
         file_out.write(file_in.read())
