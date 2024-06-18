70728d353e025196463f84e9c377499d817db0c5
Romanskyy Andrey <andrey.romanskyy@icloud.com>
2024-06-19 00:40:15 +0300

    Solution.
    Signed-off-by: Romanskyy Andrey <andrey.romanskyy@icloud.com>

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, incoming_file_path, outgoing_file_path = command.split(" ")
+    if incoming_file_path == outgoing_file_path or command != "cp":
+        return
+
+    with (
+        open(incoming_file_path, "r") as incoming_file,
+        open(outgoing_file_path, "w") as outgoing_file
+    ):
+        outgoing_file.write(incoming_file.read())
