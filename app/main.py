ce8f4937d3a6014a8ab2bccdae9bc9c19d21cdac
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2024-06-19 18:49:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,22 +1,20 @@
+import os
+
+
 def copy_file(command: str) -> None:
-    elements = command.split(" ")
-    if elements[1] != elements[2]:
-        with open(elements[1], "r") as file_out, open(
-            elements[2], "w"
-        ) as file_in:
-            text = file_out.read()
-            file_in.write(text)
-
-
-print("Command format: cp [Name of the source file] [Name of the final file]")
-while True:
-    command_text = input("Type the command: ")
-    if not command_text.startswith("cp"):
-        print("Incorrect command name. Try again")
-    else:
-        try:
-            copy_file(command_text)
-            break
-        except Exception:
-            print("Error in command")
-            continue
+    elements = command.split()
+    try:
+        if not command.startswith("cp"):
+            raise ValueError
+        os.path.exists(elements[1])
+        if elements[1] != elements[2]:
+            with open(elements[1], "r") as file_out, open(
+                elements[2], "w"
+            ) as file_in:
+                text = file_out.read()
+                file_in.write(text)
+                print(f"File {elements[1]} copied to file {elements[2]}")
+    except FileNotFoundError:
+        print(f"File {elements[1]} does not exist")
+    except ValueError:
+        print(f"Incorrect command name: {elements[0]}")
