f0b69a7745864b80f5e742c2067cfdaf0c1cea74
Andriy Zhuk <andri195595@gmail.com>
2024-06-24 21:10:22 +0300

    +

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    command = command.split(" ")
+    command = command.split()
     if command[1] == command[2]:
         return
     with open(command[1], "r") as file_def, open(command[2], "w") as file_def2:
