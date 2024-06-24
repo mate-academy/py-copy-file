e948d723568b3160aabf0aaee6dcbf99aa140900
Ppllupold <kotnazar2005@gmail.com>
2024-06-24 20:34:03 +0300

    done file-copy

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split(" ")
+    if parts[1] == parts[2]:
+        return
+    if len(parts) != 3 or parts[0] != "cp":
+        raise ValueError("Command must be in the format: cp file_from file_to")
+    with open(parts[1], "r") as file_from, open(parts[2], "r") as file_to:
+        file_to.write(file_from.read())
