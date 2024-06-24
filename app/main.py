092717387e316bbcdd7ce93e2765120114be0724
Yevheniy Dolinskiy <zkorvoz02@gmail.com>
2024-06-24 12:27:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str):
+    splitted_command = command.split("")
+    with (open(splitted_command[1], "r") as f_original,
+          open(splitted_command[2], "w") as f_copy):
+        if splitted_command[1] == splitted_command[2]:
+            pass
+        else:
+            f_copy.write(f_original.read())
