31bccf2581c9403501a3630da696619437a2f107
KovbasinskyV <kovbasinskyvv@gmail.com>
2024-06-24 11:24:56 +0300

    solution1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    action, file, new_file = command.split(" ")
+
+    if action == "cp":
+        if file != new_file:
+            with open(file) as file_in, open(new_file, "w") as file_out:
+                file_out.write(file_in.read())
