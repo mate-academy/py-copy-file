df99bda985c88d280859cf4407d740fc1f94650e
Andriy Zhuk <andri195595@gmail.com>
2024-06-24 21:47:18 +0300

    +

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
-    command = command.split()
-    if command[1] == command[2]:
+    сopy_command, file1, file2 = command.split()
+    if file1 == file2:
         return
-    with open(command[1], "r") as file_def, open(command[2], "w") as file_def2:
+    with open(file1, "r") as file_def, open(file2, "w") as file_def2:
         text_file = file_def.read()
         file_def2.write(text_file)
