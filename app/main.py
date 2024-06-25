edf8a8e08276fc33c470631db3c001273f08cba1
silchenko-arsen <silchenkoarsen@gmail.com>
2024-06-25 18:25:53 +0300

    fix after review

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(commands_str: str) -> None:
-    commands_list = commands_str.split()
-    file_out = commands_list[1]
-    file_in = commands_list[2]
-    with open(file_out, "r") as file_out:
-        with open(file_in, "w") as file_in:
+    commands_list, file_out, file_in = commands_str.split()
+    if (len(commands_list) == 3 and commands_list[0] == "cp"
+            and file_out != file_in):
+        with open(file_out, "r") as file_out, open(file_in, "w") as file_in:
             file_in.write(file_out.read())
