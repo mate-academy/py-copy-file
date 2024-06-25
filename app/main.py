b410a3de448a80ab9c631f64e6bed5dd6d044100
Danulo <danulo1337@gmail.com>
2024-06-25 15:35:58 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    files = command.split(" ")
+    if len(files) != 3 or files[0] != "cp":
+        return
+
+    file1 = files[1]
+    file2 = files[2]
+
+    if file1 == file2:
+        return
+    else:
+        with open(file1, "r") as f, open(file2, "w") as f2:
+            content = f.read()
+            f2.write(content)
