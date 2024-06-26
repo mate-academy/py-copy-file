b220424708ba15d8b6f24ba15c3c0717b06a4474
hhorhe <horhehorda@gmail.com>
2024-06-26 19:09:08 +0200

    'Solution_2'

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) != 3 or parts[0] != "cp":
+        print("Invalid data provided!")
         return
 
     source_file = parts[1]
     dest_file = parts[2]
 
     if source_file == dest_file:
+        print("Invalid data provided!")
         return
 
     with open(source_file, "r") as file_in, open(dest_file, "w") as file_out:
