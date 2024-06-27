ac236919bde4942216e1503eacf56f10509ae5d2
Vova <v.kampanchenko@gmail.com>
2024-06-27 14:12:16 +0300

    Add command test

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 def copy_file(command: str) -> None:
     cmd_ls = command.split(" ")
+    if len(cmd_ls) != 3 or cmd_ls[1] != "cp":
+        raise ValueError(
+            "Invalid command. "
+            "Command must look like \'cp file.txt file-copy.txt\'"
+        )
     if cmd_ls[1] == cmd_ls[2]:
         return None
     with open(cmd_ls[1], "r") as file_in, open(cmd_ls[2], "w") as file_out:
