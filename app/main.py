9876cd9dc0a636dbe399f4f918d41d9040f5ef1c
Vova <v.kampanchenko@gmail.com>
2024-06-27 23:50:57 +0300

    Rename cmd_ls

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 def copy_file(command: str) -> None:
-    cmd_ls = command.split(" ")
-    if len(cmd_ls) != 3 or cmd_ls[1] != "cp":
+    command_list = command.split(" ")
+    if len(command_list) != 3 or command_list[1] != "cp":
         raise ValueError(
             "Invalid command. "
             "Command must look like \'cp file.txt file-copy.txt\'"
         )
-    if cmd_ls[1] == cmd_ls[2]:
+    if command_list[1] == command_list[2]:
         return None
-    with open(cmd_ls[1], "r") as file_in, open(cmd_ls[2], "w") as file_out:
+    with (open(command_list[1], "r") as file_in,
+          open(command_list[2], "w") as file_out):
         file_out.write(file_in.read())
