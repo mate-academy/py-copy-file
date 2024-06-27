47e41cfc93268142e5e6ef12967b6f0ae784451b
Artem <mikhailetc.artem@gmail.com>
2024-06-27 11:53:07 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 def copy_file(command: str) -> None:
     command_list = command.split()
+    if len(command_list) != 3:
+        print("length your command must be 3")
+        return None
     command, file_name_input, file_name_output = command_list
     if command == "cp" and file_name_input != file_name_output:
         with (open(file_name_input, "r") as file_in,
