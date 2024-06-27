f88fedf8819f727d5339754313f1b18d60561d18
Valeriy Morkvych <morkvych@gmail.com>
2024-06-27 20:49:37 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 def copy_file(command: str) -> None:
     command_list = command.split(" ")
+    if len(command_list) != 3:
+        raise RuntimeError("Command is not correct!")
+    if command_list[0] != "cp":
+        raise NameError("Expected 'cp' command")
+
     file_name_in = command_list[1]
     file_name_out = command_list[2]
     if file_name_in == file_name_out:
