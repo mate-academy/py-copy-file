a9d21145d7b94bb488055f6842f379905c8429b9
Maksimens <Muskam.dan@gmail.com>
2024-06-27 17:01:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_split = command.split()
+
+    reports_copy = command_split[1]
+    where_copy = command_split[2]
+
+    if reports_copy == where_copy:
+        return
+
+    with open(reports_copy, "r") as file_in, open(where_copy, "w") as file_out:
+        file_out.write(file_in.read())
+
