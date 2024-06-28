cecb527a50e1149c8308aecae6e616df87528a22
Buchak Tamara <Buchak.Tamara@gmail.com>
2024-06-28 13:07:33 +0300

    add solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_name = command.split(" ")
+
+    if (
+        file_name[0] != "cp"
+        or len(file_name) != 3
+        or file_name[1] == file_name[2]
+    ):
+        return
+
+    with (
+        open(file_name[1], "r") as file_in,
+        open(file_name[2], "w") as file_out
+    ):
+        file_out.write(file_in.read())
+
+
+if __name__ == "__main__":
+    copy_file("cp file.txt new_file.txt")
+    print(open("file.txt").read() == open("new_file.txt").read())
