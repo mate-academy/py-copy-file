1a5dbf8eee9b468e31c5105156d19be5c1c4b051
Buchak Tamara <Buchak.Tamara@gmail.com>
2024-06-28 14:51:32 +0300

    add message

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def copy_file(command: str) -> None:
-    file_name = command.split(" ")
+    file_name = command.split()
 
     if (
         file_name[0] != "cp"
         or len(file_name) != 3
         or file_name[1] == file_name[2]
     ):
-        return
+        print(f"Command '{command}' is not correct.")
 
     with (
         open(file_name[1], "r") as file_in,
