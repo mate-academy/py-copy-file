1f6ea7b20f9cf17721c60b54cfb421c8c9c0a035
zakhar <zaharsavchen@gmail.com>
2024-06-30 21:39:33 +0300

    fix suggestion

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
-    file1 = command.split(" ")[1]
-    file2 = command.split(" ")[2]
-    if file1 != file2:
-        with (open(file1, "r") as file_in,
-              open(file2, "w") as file_out):
-            content = file_in.read()
-            file_out.write(content)
+    command, file_out, file_in = command.split()
+    if file_out != file_in:
+        with (open(file_out, "r") as f_in,
+              open(file_in, "w") as f_out):
+            content = f_in.read()
+            f_out.write(content)
