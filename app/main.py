3edb2e3ba2df0549d9847d7b8f4cdbc232bc3800
Ruxxell <furman06228@gmail.com>
2024-06-30 21:13:38 +0300

    Solution 2.0.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
-import shutil
-
-
 def copy_file(command: str) -> None:
-    if command.startswith("cp "):
-        _, source_file, destination_file = command.split()
-        if source_file != destination_file:
-            try:
-                shutil.copy(source_file, destination_file)
-            except FileNotFoundError:
-                print("Source file not found.")
-            except IOError as e:
-                print(f"Error copying file: {e}")
+    command_parts = command.split()
+    source_file, destination_file = command_parts[1], command_parts[2]
+
+    if source_file != destination_file:
+        with (
+            open(source_file, "r") as source,
+            open(destination_file, "w") as destination
+        ):
+            destination.write(source.read())
