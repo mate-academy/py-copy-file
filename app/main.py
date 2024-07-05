88a41dbf416e8f587b2533eaa3cf4b6c3f41766a
Anatoliy <an.kaminskij@gmail.com>
2024-07-05 17:12:11 +0300

    additional check

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-def copy_file(command: str) -> None | str:
+def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        print("Invalid command. "
+              "Use the format: cp <source_file> <destination_file>")
+        return
+
     command_name, file_in, file_out = command.split()
 
     if command_name != "cp" or file_in == file_out:
         print("Does nothing")
-        return "Does nothing"
+        return
 
     with open(file_in, "r") as fi, open(file_out, "w") as fo:
         fo.write(fi.read())
