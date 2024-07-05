f23444d714e48d62ea52275b496dfab6c646b6db
Daniil <daniilrizhenko@gmail.com>
2024-07-05 16:41:00 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+    source_file = parts[1]
+    destination_file = parts[2]
+
+    if source_file != destination_file:
+        with (open(source_file, "rb") as fin,
+              open(destination_file, "wb") as fout):
+            shutil.copyfileobj(fin, fout)
