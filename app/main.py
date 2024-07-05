a3d90b9676fce969aa3732dfe8a65bc119934d6e
Anatoliy <an.kaminskij@gmail.com>
2024-07-05 12:02:31 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None | str:
+    command_name, file_in, file_out = command.split()
+
+    if command_name != "cp" or file_in == file_out:
+        print("Does nothing")
+        return "Does nothing"
+
+    with open(file_in, "r") as fi, open(file_out, "w") as fo:
+        fo.write(fi.read())
