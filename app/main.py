e823ccf083a81a761c88fd3cb10489ebcaa08a3f
Daniil <daniilrizhenko@gmail.com>
2024-07-05 16:50:27 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ import shutil
 
 def copy_file(command: str) -> None:
     parts = command.split()
-    source_file = parts[1]
-    destination_file = parts[2]
 
-    if source_file != destination_file:
-        with (open(source_file, "rb") as fin,
-              open(destination_file, "wb") as fout):
-            shutil.copyfileobj(fin, fout)
+    if len(parts) == 3:
+        source_file = parts[1]
+        destination_file = parts[2]
+
+        if source_file != destination_file:
+            with (open(source_file, "rb") as fin,
+                  open(destination_file, "wb") as fout):
+                shutil.copyfileobj(fin, fout)
