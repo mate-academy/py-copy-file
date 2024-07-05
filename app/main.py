a53da40b0c4f421219b3017dc0f691c36758ba59
Daniil <daniilrizhenko@gmail.com>
2024-07-05 17:10:16 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,9 +4,10 @@ import shutil
 def copy_file(command: str) -> None:
     parts = command.split()
 
-    if len(parts) == 3:
-        source_file = parts[1]
-        destination_file = parts[2]
+    if len(parts) != 3:
+        print("Invalid command format.")
+    else:
+        source_file, destination_file = parts[1:]
 
         if source_file != destination_file:
             with (open(source_file, "rb") as fin,
