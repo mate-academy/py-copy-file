2effcea4124de3eaa5d924d01ce303e9ae0356ff
Vasyl <Widbmak@gmail.com>
2024-07-06 18:29:54 +0300

    test

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
-def copy_file(command: str) -> None:
-    if len(command.split()) != 3:
-        return
-    operation, current_file, new_file = command.split()
+def copy_file(command: str) -> str:
+    command = command.split()
+    if len(command) != 3:
+        return (
+            "Please enter a command in "
+            "format: 'cp filename.txt new_filename.txt'"
+        )
+    operation, current_file, new_file = command
     if current_file == new_file:
-        return
+        return "The name of copy should differ"
     else:
         with (
             open(current_file) as input_file,
