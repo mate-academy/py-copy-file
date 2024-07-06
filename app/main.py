1d4652564454d43e910c6466a02f9a0929e3bf2b
Vasyl <Widbmak@gmail.com>
2024-07-06 14:17:42 +0300

    test

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        return
+    operation, current_file, new_file = command.split()
+    if current_file == new_file:
+        return
+    else:
+        with (
+            open(current_file) as input_file,
+            open(new_file, "w") as output_file
+        ):
+            input_file.write(output_file.read())
