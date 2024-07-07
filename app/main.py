903e6301321b6c278ea8d09dbc1c403353bcd647
bythewaters <bythewaters800@gmail.com>
2024-07-07 21:05:41 +0200

    copy

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    if command[1] != command[2]:
+        with open(command[1], "r") as file, open(command[2], "w") as file_copy:
+            content = file.read()
+            file_copy.write(content)
+
+
+if __name__ == "__main__":
+    copy_file("cp file.txt new_file.txt")
