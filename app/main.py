ffcf50228b65d9db3b58f03b1da3a6ddb4d91f8a
bythewaters <bythewaters800@gmail.com>
2024-07-08 16:53:22 +0200

    copy

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     command, first_file, second_file = command.split()
-    if first_file != second_file:
+    if command == "cp" and first_file != second_file:
         with (open(first_file, "r") as file,
               open(second_file, "w") as file_copy):
             content = file.read()
