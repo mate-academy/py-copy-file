ec15a04ba5d512e7252b60110b451d019255ae16
Vadym Piddubnyi <leegrad@ukr.net>
2024-07-08 14:24:32 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> str:
+    parts = command.split()
+
+    if len(parts) == 3 and parts[0] == "cp":
+        source_code, destination_code = parts[1], parts[2]
+
+        if source_code != destination_code:
+            with open(source_code) as f, open(destination_code, "wt") as m:
+                m.write(f.read())
+
+            return f"File {source_code} copied to {destination_code}"
+
+
+print(copy_file("cp test.txt test_copy.txt"))
