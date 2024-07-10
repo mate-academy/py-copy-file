dc10a911b61ff68bed8682bbf55b84aadfeb5394
Mykola <jacktyler2391@gmail.com>
2024-07-10 20:00:58 +0300

    fixes according to mentor comments

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 import shutil
 
-source = "/Users/jacktyler/py-copy-file/app/file.txt"
-destination = "/Users/jacktyler/py-copy-file/app/new_file.txt"
-
 
 def copy_file(source: str, destination: str) -> None:
     try:
@@ -11,6 +8,3 @@ def copy_file(source: str, destination: str) -> None:
         print("Cannot copy the same file!")
     else:
         print("File has been copied successfully")
-
-
-print(copy_file(source, destination))
