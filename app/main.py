44516c7376fbea48d74b2b3b1d38fde47998143c
Mykola <jacktyler2391@gmail.com>
2024-07-10 18:14:45 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import shutil
+
+source = "/Users/jacktyler/py-copy-file/app/file.txt"
+destination = "/Users/jacktyler/py-copy-file/app/new_file.txt"
+
+
+def copy_file(source, destination) -> None:
+    try:
+        shutil.copy(source, destination)
+    except shutil.SameFileError:
+        print("Cannot copy the same file!")
+    else:
+        print("File has been copied successfully")
+
+
+print(copy_file(source, destination))
