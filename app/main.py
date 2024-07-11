9e3e275c765c7cf3acd15123931553266be6b1b0
Mykola <jacktyler2391@gmail.com>
2024-07-11 12:02:15 +0300

    Rewrite code using custom logic

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
-import shutil
-
-
 def copy_file(source: str, destination: str) -> None:
-    try:
-        shutil.copy(source, destination)
-    except shutil.SameFileError:
-        print("Cannot copy the same file!")
-    else:
-        print("File has been copied successfully")
+    with open(source, "r") as first, open(destination, "w") as second:
+        if first.name != second.name:
+            for line in first:
+                second.write(line)
+            print("Successfully copied file!")
+        else:
+            raise Exception("Cannot copy to identical file")
