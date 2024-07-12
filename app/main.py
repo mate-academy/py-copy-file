58e17ab4d7ac88f8e31d10031b89b4568f049ced
Maksym Rozdoba <rozdobamax@gmail.com>
2024-07-12 10:52:46 +0300

    implement improvements

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
+from app.errors import CommandLineError
+
+
 def copy_file(command: str) -> None:
+
+    if len(command.split(" ")) != 3 or command.split(" ")[0] != "cp":
+        raise CommandLineError("Command line is incorrect.")
+
     command_line, original_file, new_file = command.split(" ")
-    if command_line == "cp":
-        if original_file != new_file:
-            with (open(original_file) as file_in,
-                  open(new_file, "w") as file_out):
-                file_out.write(file_in.read())
+
+    if original_file != new_file:
+        with (open(original_file) as file_in,
+              open(new_file, "w") as file_out):
+            file_out.write(file_in.read())
+            print("The file successfully copied")
