c8916c7f9b69272b0125acc4fdb897e88ff74dff
Hennadii <bzpg545256@gmail.com>
2024-07-15 18:34:34 +0100

    add list check ot func

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 def copy_file(command: str) -> None:
     file_name_list = command.split()
+    if len(file_name_list) != 3 or file_name_list[0] != "cp":
+        print("Invalid command")
+        return
     source_file = file_name_list[1]
     destination_file = file_name_list[2]
     if source_file == destination_file:
