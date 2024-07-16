90be921fa6feecec329d1dbad425341776294f93
maksym_korniev <maksymkorniev88@gmail.com>
2024-07-16 15:33:19 +0300

    remove unnecessary lines

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def copy_file(command: str) -> None:
     file_names = command.split(" ")
     if file_names[0] != "cp" or file_names[1] == file_names[2]:
-        print("GG")
         return
 
     with open(file_names[1], "r") as read_file, \
