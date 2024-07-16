40f5eb949aedd040ef8b72ed0ddda7bb32bf1927
Olga-Gynguliak <olga.gynguliak@gmail.com>
2024-07-16 14:03:18 +0300

    "Solution py-copy-file"

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split(" ")
+
+    if len(parts) < 3 or parts[0] != "cp":
+        return
+
+    src, dest = parts[1], parts[2]
+
+    if src == dest:
+        return
+
+    with open(src, "r") as file_in, open(dest, "w") as file_out:
+        file_out.writelines(file_in.readlines())
+
+
+if __name__ == "__main__":
+    copy_file("cp file.txt new_file.txt")
