8b6dcbb1584b9fde213d576e097bed8b849377e3
maksym_korniev <maksymkorniev88@gmail.com>
2024-07-16 15:31:01 +0300

    created copy file realisation

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_names = command.split(" ")
+    if file_names[0] != "cp" or file_names[1] == file_names[2]:
+        print("GG")
+        return
+
+    with open(file_names[1], "r") as read_file, \
+            open(file_names[2], "x") as write_file:
+        write_file.writelines(read_file.readlines())
