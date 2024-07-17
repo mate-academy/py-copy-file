6ebe7d391d19b3e091a44a411b29c601a45093fa
viktor-77 <vumanskiy0664041341@gmail.com>
2024-07-18 00:00:20 +0300

    Resolved Copy file task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    _, copy_from, copy_to = command.split()
+    if copy_from == copy_to:
+        return
+
+    with (
+        open(copy_from, "r") as starting_file,
+        open(copy_to, "w") as destination_file
+    ):
+        destination_file.write(
+            starting_file.read()
+        )
