08ac6c22e7b4438187fb73bd7298da4992fc0ede
viktor-77 <vumanskiy0664041341@gmail.com>
2024-07-18 14:30:25 +0300

    Added split length check

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 def copy_file(command: str) -> None:
-    command_type, copy_from, copy_to, *_ = command.split()
-    if copy_from == copy_to:
-        return
+    command_split = command.split()
+    if len(command_split) == 3:
+        command_type, copy_from, copy_to = command_split
 
-    if command_type == "cp":
-        with (
-            open(copy_from, "r") as starting_file,
-            open(copy_to, "w") as destination_file
-        ):
-            destination_file.write(
-                starting_file.read()
-            )
+        if copy_from == copy_to:
+            return
+
+        if command_type == "cp":
+            with (
+                open(copy_from, "r") as starting_file,
+                open(copy_to, "w") as destination_file
+            ):
+                destination_file.write(
+                    starting_file.read()
+                )
