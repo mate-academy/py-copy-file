541d636e854c3680aead4cb3168ee52c28b1bb83
Nazarii Khalimonov <nazar131188@gmail.com>
2024-07-18 16:02:28 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+from typing import Any
+
+
+def copy_file(command: str) -> Any:
+    cp, old_file, new_file = command.split()
+
+    if old_file == new_file:
+        return
+    elif old_file != new_file:
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            lines = file_in.read()
+            file_out.write(lines)
+
