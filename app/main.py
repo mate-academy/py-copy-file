c30d8f7011320400110a5c8d28c84801162de07b
Nazarii Khalimonov <nazar131188@gmail.com>
2024-07-18 17:34:15 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from typing import Any
 
 
 def copy_file(command: str) -> Any:
+    if len(command.split()) != 3 or command.split()[0] != "cp":
+        return
+
     cp, old_file, new_file = command.split()
 
     if old_file == new_file:
@@ -10,4 +13,3 @@ def copy_file(command: str) -> Any:
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             lines = file_in.read()
             file_out.write(lines)
-
