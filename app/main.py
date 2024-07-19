67592240107524308df2557e962c35904d93e48a
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-07-19 22:00:53 +0300

    check changes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 def copy_file(command: str) -> None:
-    file, new_file = command.split()[1:3]
-    if file == new_file:
+    split_command = command.split()
+    if len(split_command) != 3 or split_command[0] != "cp":
         return
 
-    with open(file, "r") as file_in, open(new_file, "w") as file_out:
-        for line in file_in:
-            file_out.write(line)
+    current_file = split_command[1]
+    new_file = split_command[2]
+
+    if current_file == new_file:
+        return
+
+    with open(current_file, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
