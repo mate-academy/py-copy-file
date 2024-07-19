8b5a9af8bc22693bf5930daf8410e613a6771277
Anton Bilous <anton.bilosus@gmail.com>
2024-07-19 17:37:32 +0300

    Implement copy_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import shlex
+
+
+def copy_file(command: str) -> None:
+    try:
+        command, source_name, destination_name = shlex.split(command)
+        if command != "cp":
+            raise ValueError("The command is not 'cp'")
+    except ValueError as e:
+        raise ValueError("The command is not valid") from e
+
+    if source_name == destination_name:
+        return
+
+    with open(source_name, "rb") as source, open(
+        destination_name, "wb"
+    ) as destination:
+        destination.write(source.read())
+
+
+if __name__ == "__main__":
+    copy_file("cp README.md 'Some file.md'")
