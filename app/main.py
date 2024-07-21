c0e0f4f2d89c4074d9cd99707d3765644a8ca7eb
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-07-21 16:15:09 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -3,20 +3,27 @@ import shutil
 
 def copy_file(command: str) -> None:
     splited = command.split()
+
+    if len(splited) != 3:
+        print("Command must have 3 arguments: 'cp <source> <destination>'")
+        return
+
     cp = splited[0]
     if cp != "cp":
         print("Command must be started from 'cp'")
         return
+
     file_name_to_copy = splited[1]
     new_file_name = splited[2]
+
     if file_name_to_copy == new_file_name:
-        print("files can't have the same titles")
+        print("Files can't have the same titles")
         return
 
     try:
         shutil.copy(file_name_to_copy, new_file_name)
-        print(f"file '{file_name_to_copy}' copied into '{new_file_name}'")
+        print(f"File '{file_name_to_copy}' copied into '{new_file_name}'")
     except FileNotFoundError:
-        print(f"file '{file_name_to_copy}' is not found")
+        print(f"File '{file_name_to_copy}' is not found")
     except Exception as e:
         print(f"Error: {e}")
