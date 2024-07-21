6d89ef1cb88d74c09857e069ea64d25d0150dc5e
Maksim <golevmaksim52@gmail.com>
2024-07-21 20:17:08 +0300

    I wrote the code as I expected, but could not test it...

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    source_filename = parts[1]
+    destination_filename = parts[2]
+    if source_filename == destination_filename:
+        return
+    with (open(source_filename, "r") as file_in,
+          open(destination_filename, "w") as file_out):
+        content = file_in.read()
+        file_out.write(content)
