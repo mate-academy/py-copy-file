f05728545cb6418bfa7c6eaba9935f97550c71ce
Nazarii Khalimonov <nazar131188@gmail.com>
2024-07-21 10:35:29 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,13 +4,15 @@ from typing import Any
 def copy_file(command: str) -> Any:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "cp":
-        print("Invalid command format. Use: cp <source_file> <destination_file>")
+        print("Invalid command format. Use:"
+              " cp <source_file> <destination_file>")
         return
 
     _, old_file, new_file = parts
 
     if old_file == new_file:
-        print("Source and destination files are the same. No action taken.")
+        print("Source and destination files"
+              " are the same. No action taken.")
         return
     elif old_file != new_file:
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
