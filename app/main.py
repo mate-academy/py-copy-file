59a81f502556d00ac53685b82a3390b18954eaea
Stepan <oleksiukstepann@gmail.com>
2024-07-21 15:09:32 +0300

    Implemented main

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    sep_command = command.split()
+
+    with open(sep_command[1], "r") as f, open(sep_command[-1], "w") as w:
+        w.write(f.read())
