3b3594c3bfa4274c3ba10d023e212d46e62e372e
andreykolomiec <akolomiec982@gmail.com>
2024-07-22 11:29:30 +0300

    informative message

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 def copy_file(command: str) -> None:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "cp":
-        raise ValueError
+        raise ValueError(
+            "Invalid command format."
+            "Expected format: cp source_file destination_file"
+        )
     source_file, destination_file = parts[1], parts[2]
     if source_file == destination_file:
+        print("The source file and the destination file have the same names.")
         return
     with open(source_file, "r") as file_in, \
             open(destination_file, "w") as file_out:
