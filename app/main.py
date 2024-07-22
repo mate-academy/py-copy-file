59867643f9399081f0c8983bdcff275693a09a52
andreykolomiec <akolomiec982@gmail.com>
2024-07-22 15:18:29 +0300

    use unpacking

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 def copy_file(command: str) -> None:
-    parts = command.split()
-    if len(parts) != 3 or parts[0] != "cp":
+    command_name, source_file, destination_file, *_ = command.split()
+    if command_name != "cp":
         raise ValueError(
             "Invalid command format."
             "Expected format: cp source_file destination_file"
         )
-    source_file, destination_file = parts[1], parts[2]
     if source_file == destination_file:
         print("The source file and the destination file have the same names.")
         return
-    with open(source_file, "r") as file_in, \
-            open(destination_file, "w") as file_out:
-        file_out.write(file_in.read())
+    with open(source_file, "r") as file_in:
+        with open(destination_file, "w") as file_out:
+            file_out.write(file_in.read())
