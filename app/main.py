e859fcbf7854886bdab15a1796eea47b6d7eebeb
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-07-22 19:21:56 +0300

    Implemented solution (added try except block)

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ def copy_file(command: str) -> None:
     garbage, copy_name, new_name = command.split()
     if copy_name == new_name:
         return
-    with open(copy_name, "r") as file_in, open(new_name, "w") as file_out:
-        data = file_in.read()
-        file_out.write(data)
+    try:
+        with open(copy_name, "r") as file_in, open(new_name, "w") as file_out:
+            data = file_in.read()
+            file_out.write(data)
+    except FileNotFoundError:
+        print("No such file in the directory")
