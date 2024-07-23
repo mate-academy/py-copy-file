fe3bced332b7f6960b3d9eaa3f282543b00c9dfb
Maksim <golevmaksim52@gmail.com>
2024-07-23 17:36:25 +0300

    add changes by splitting the command by spaces

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def copy_file(command: str) -> None:
-    parts = command.split()
-    source_filename = parts[1]
-    destination_filename = parts[2]
+    cmd, source_filename, destination_filename = command.split()
     if source_filename == destination_filename:
         return
     with (open(source_filename, "r") as file_in,
