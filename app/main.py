3488353a4eb7a7e6993f14a1c8b60e25c27e8007
Igor Chapy <misha@mate.acedemy>
2024-07-24 16:00:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-def copy_file(command: str) -> None:
+import os
+
+
+def move_file(command: str) -> None:
     part = command.split()
-    command_cp = part[0]
-    if command_cp != "cp":
+    command_mv = part[0]
+    if command_mv != "mv":
         return
     if len(part) != 3:
         print("Error: The command requires exactly 3 arguments.")
@@ -14,3 +17,4 @@ def copy_file(command: str) -> None:
     with open(file_copy_from, "r") as source:
         with open(file_copy_to, "w") as destination:
             destination.write(source.read())
+        os.remove(file_copy_to)
