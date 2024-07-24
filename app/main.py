944ca3dee262618de1f5da537f6417fbf7334c3a
Viktor Panasiuk <Viktor66462@gmail.com>
2024-07-24 22:35:30 +0300

    'Solution_fixed'

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ def copy_file(command: str) -> None:
     if len(args) < 3:
         raise FileNotFoundError("No file name specified.")
 
-    file1 = command.split()[1]
-    file2 = command.split()[2]
+    file1, file2 = args[1:]
 
-    if not file1 == file2:
+    if file1 == file2:
+        raise ValueError("Source and destination filenames are the same.")
+
+    if file1 != file2:
         with open(file1, "r") as file_in, open(file2, "w") as file_out:
             file_out.write(file_in.read())
 
