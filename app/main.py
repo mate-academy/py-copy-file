737b90936a92f7d0f8373a39bf7bc7d6b89bb037
Yuliia Yedynak <eeddiinnaakk@gmail.com>
2024-07-26 06:51:19 +0000

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 def copy_file(command: str) -> None:
-    commands = command.split(" ")
-    if commands[1] != commands[2]:
+    commands = command.split()
+    if (
+            len(commands) == 3
+            and commands[0] == "cp"
+            and commands[1] != commands[2]
+    ):
         with (open(commands[1], "r") as file_for_read,
               open(commands[2], "w") as file_for_write):
             file_for_write.write(file_for_read.read())
