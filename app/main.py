00b6eae00fa449e6054b70fe6c922fc9bb5e6713
DSahalatyi <dsahalatyi@gmail.com>
2024-07-26 14:27:58 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    command, file_name, copy_name = command.split()
+
+    if file_name == copy_name:
+        return
+
+    with open(copy_name, "w") as copy, open(file_name, "r") as file:
+        copy.writelines(file.readlines())
