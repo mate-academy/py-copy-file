c6ad2afaf64adcde7c9167c4087ba38911f1946b
Olexandr Bastov <iamlucky1990@gmail.com>
2024-07-31 00:23:45 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split(" ")
+    if len(parts) != 3 or parts[0] != "cp" or parts[1] == parts[2]:
+        return
+    with open(parts[1]) as source, open(parts[2], "w") as copy:
+        copy.write(source.read())
