b72faef764f26aa845923cbd10584fced573bed0
Olexandr Bastov <iamlucky1990@gmail.com>
2024-07-31 16:00:52 +0300

    The unnecessary "return" statement has been removed now split() method returns everything right into variables unnecessary " " argument has been removed from split() method variables names are more descriptive now

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
-    parts = command.split(" ")
-    if len(parts) != 3 or parts[0] != "cp" or parts[1] == parts[2]:
-        return
-    with open(parts[1]) as source, open(parts[2], "w") as copy:
-        copy.write(source.read())
+    command, source, destination = command.split()
+
+    if not (command != "cp" or source == destination):
+
+        with open(source) as source, open(destination, "w") as copy:
+            copy.write(source.read())
