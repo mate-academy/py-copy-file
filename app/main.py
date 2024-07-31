0f914d6e0a069c19895483e8a73de1e73b53486f
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-07-31 15:58:47 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ def copy_file(command: str) -> None:
     garbage, copy_name, new_name = command.split()
     if copy_name != new_name:
         try:
-            with open(copy_name, "r") as file_in, open(new_name, "w") as file_out:
+            with (
+                open(copy_name, "r") as file_in,
+                open(new_name, "w") as file_out
+            ):
                 file_out.write(file_in.read())
         except FileNotFoundError:
             print("No such file in the directory")
