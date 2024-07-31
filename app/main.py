48ff6c401dfe56c03c7df6a8034a6daae0b9966f
Stepan <oleksiukstepann@gmail.com>
2024-07-31 17:36:03 +0300

    fixed mistake2

--- app/main.py
+++ app/main.py
@@ -1,9 +1,5 @@
 def copy_file(command: str) -> None:
-    file_copy = command[1]
-    new_file = command[2]
-
-    if file_copy == new_file:
-        return
-
-    with open(file_copy, "r") as copy, open(new_file, "w") as file:
-        file.write(copy.read())
+    command, old_file, new_file = command.split()
+    if old_file != new_file:
+        with open(old_file, "r") as copy, open(new_file, "w") as file:
+            file.write(copy.read())
