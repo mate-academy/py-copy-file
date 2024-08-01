237515ce52e24082b73717639fbad5ada33ecb7d
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2024-08-01 22:20:45 +0300

    Solution 2.0

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
     source_file, new_file = command.split(" ")[1:]
-    if source_file != new_file:
+    if source_file != new_file and len(command.split(" ")) == 3:
         with open(source_file) as file_out, open(new_file, "w") as file_in:
             file_in.writelines(file_out.readlines())
