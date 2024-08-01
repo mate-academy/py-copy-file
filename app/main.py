515b404aee3858033fb163d52e122cf0bbc5acdb
maksym_korniev <maksymkorniev88@gmail.com>
2024-08-01 12:53:22 +0300

    refactored command unpacking

--- app/main.py
+++ app/main.py
@@ -1,12 +1,7 @@
-import shlex
-
-
 def copy_file(command: str) -> None:
-    commands = shlex.split(command)
-    source_file = commands[1]
-    destination_file = commands[2]
+    key_word, source_file, destination_file = command.split()
 
-    if commands[0] == "cp" or source_file != destination_file:
+    if key_word == "cp" or source_file != destination_file:
         with open(source_file, "r") as read_file, \
                 open(destination_file, "x") as write_file:
             write_file.writelines(read_file.readlines())
