f458a6acfba3a5d362a31ce2180d3f8d007579c4
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2024-08-01 22:47:02 +0300

    Solution 4.0

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 def copy_file(command: str) -> None:
     command_args = command.split(" ")
-    if len(command_args) == 3 and command_args[1] != command_args[2]:
+
+    if len(command_args) != 3:
+        print("Wrong command")
+
+    if command_args[1] != command_args[2]:
         _, source_file, new_file = command_args
         with open(source_file) as file_out, open(new_file, "w") as file_in:
             file_in.writelines(file_out.readlines())
