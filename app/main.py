db6bb3d1ef5b2110b8cb5e67e79f440ccacda997
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-08-02 22:59:35 +0300

    implement copy-file solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_key_word, file_in_name, file_out_name = command.split()
+    if command_key_word != "cp" or file_in_name == file_out_name:
+        return
+
+    with (
+        open(file_in_name, "r") as file_in,
+        open(file_out_name, "w") as file_out
+    ):
+        content_to_copy = file_in.read()
+        file_out.write(content_to_copy)
