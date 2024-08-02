d151bc0de3272dbf4a275d98db94300b5f43ef96
Danyil <dartomov@gmail.com>
2024-08-02 15:25:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    files = command.split(" ")
+
+    if "cp" not in files and files.index("cp") != 0:
+        print("Incorrect command to copy file")
+        return
+
+    if files[1] == files[2]:
+        return None
+
+    with open(files[1], "r") as file, open(files[2], "w") as copying_file:
+        copying_file.write(file.read())
