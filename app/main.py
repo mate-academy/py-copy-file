969e73412601d184647f2a5495d97f60d0b4046b
MaksimGolev <98954167+MaksimGolev@users.noreply.github.com>
2024-08-03 23:53:54 +0300

    Update main.py
    update code style

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ def copy_file(command: str) -> None:
     cmd, source_filename, destination_filename = command.split()
     if source_filename == destination_filename:
         return
-    with (open(source_filename, "r") as file_in,
-          open(destination_filename, "w") as file_out):
-        content = file_in.read()
-        file_out.write(content)
+    with (open(source_filename, "r") as file,
+          open(destination_filename, "w") as file_copy):
+        file_out.write(file_in.read())
