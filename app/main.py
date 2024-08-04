86f0b73edbee1a0ab2adc3cc4dc17b9ba1f56d2b
Yakiv Lobach <lobachyakiv@gmail.com>
2024-08-04 20:05:05 +0300

    first try

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        raise ValueError("Invalid command format")
+
+    source_file = parts[1]
+    destination_file = parts[2]
+
+    if source_file == destination_file:
+        return
+
+    try:
+        with (
+            open(source_file, "r") as file_in,
+            open(destination_file, "w") as file_out
+        ):
+            shutil.copyfileobj(file_in, file_out)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Source file '{source_file}' not found")
+    except Exception as e:
+        raise RuntimeError(f"An error occurred: {e}")
