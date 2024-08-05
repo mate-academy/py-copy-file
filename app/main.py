fa642d000b8b7f500721034175a3953cb66795bd
Halyna Rohatska <rohatskahr@gmail.com>
2024-08-05 15:07:07 +0300

    added changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 def copy_file(command: str) -> None:
-    command_split = command.split(" ")
-    with (
-        open(command_split[1], "r") as file_in,
-        open(command_split[2], "w") as file_out
-    ):
-        file_out.write(file_in.read())
+    command_split = command.split()
+    command_to_read, command_to_write = command_split
+    if command_to_read != command_to_write:
+        with (
+            open(command_to_read, "r") as file_in,
+            open(command_to_write, "w") as file_out
+        ):
+            file_out.write(file_in.read())
