9c0c1ed996d9df70cdd375da70854047bfb467a6
Danyil <dartomov@gmail.com>
2024-08-05 13:12:43 +0300

    Fixed_1

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 def copy_file(command: str) -> None:
-    files = command.split(" ")
+    files = command.split()
 
     if "cp" not in files and files.index("cp") != 0:
         print("Incorrect command to copy file")
         return
 
-    if files[1] == files[2]:
-        return None
+    copy_from = files[1]
+    copy_to = files[2]
 
-    with open(files[1], "r") as file, open(files[2], "w") as copying_file:
-        copying_file.write(file.read())
+    if copy_from != copy_to:
+        with open(copy_from, "r") as copying_file, open(copy_to, "w") as copied_file:
+            copied_file.write(copying_file.read())
