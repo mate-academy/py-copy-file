d9ffd506b15b4195d8193deab817ef9449dc23e6
Andrii <andrii.khamaza0@gmail.com>
2024-08-05 18:02:29 +0300

    Solution of py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    words = command.split()
+
+    if words[1] == words[2]:
+        return
+
+    with open(words[1]) as file_in, open(words[2], "w") as file_out:
+        content = file_in.read()
+
+        file_out.write(content)
