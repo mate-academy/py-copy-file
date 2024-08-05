a4d1e5dbb81452ed0b759bd3be83ba92bd555968
Alexandr Politov <apolitov83@gmail.com>
2024-08-05 16:17:07 +0300

    Politov Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    command_list = command.split()
+
+    if command_list[1] == command_list[2] or command_list[0] != "cd":
+        print("do nothing")
+        return None
+
+    with open(command_list[1], "r") as file_out, open(command_list[2], "w") as file_to:
+        print("copy file")
+        file_to.write(file_out.read())
