4545424fc1b321c97d3d8e170612e6c6d121e845
Vlada <vlada.belayy@mail.com>
2024-08-05 21:21:13 +0300

    first

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_cp = command.split()
+    if len(command_cp) != 3 or command_cp[0] != "cp":
+        return
+
+    cp, source, destination = command_cp
+
+    if source == destination:
+        return
+
+    try:
+        with open(source, "r") as file_in, open(destination, "w") as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        print(f"The file '{source}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
