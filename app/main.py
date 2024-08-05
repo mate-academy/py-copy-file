ec1a8f51f5bfd694e51c4d3708184429ded11f79
Olena Cherneha <166736208+olena4reunited@users.noreply.github.com>
2024-08-05 23:46:22 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+class UnidentifiedCommandError(Exception):
+    pass
+
+
+def copy_file(command: str) -> None:
+    cm, copy_filename, new_filename = command.split()
+    if cm != "cp":
+        try:
+            raise UnidentifiedCommandError(f"{cm} command doesn't exist. ")
+        except UnidentifiedCommandError as error:
+            print(error, "You need to type correct command.")
+
+    if copy_filename != new_filename:
+        try:
+            with (
+                open(copy_filename, "r") as copy,
+                open(new_filename, "w") as new
+            ):
+                new.write(copy.read())
+        except FileNotFoundError:
+            print("No such file in the directory")
