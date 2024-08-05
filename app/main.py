513edf0e5458e0c2df08e6a0547493dceca1d506
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-08-05 10:57:04 +0300

    fix copy_file function

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
 def copy_file(command: str) -> None:
     command_key_word, file_in_name, file_out_name = command.split()
-    if command_key_word != "cp" or file_in_name == file_out_name:
-        return
-
-    with (
-        open(file_in_name, "r") as file_in,
-        open(file_out_name, "w") as file_out
-    ):
-        content_to_copy = file_in.read()
-        file_out.write(content_to_copy)
+    if command_key_word == "cp" and file_in_name != file_out_name:
+        with (
+            open(file_in_name, "r") as file_in,
+            open(file_out_name, "w") as file_out
+        ):
+            file_out.write(file_in.read())
