9321174d09a0c42339d71eec9a46f4d9dd667dee
achalaidiuk <sasha.treyser90@gmail.com>
2024-08-05 13:57:22 +0300

    Task Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+class FileNotFound(Exception):
+    pass
+
+
+def copy_file(command: str) -> None:
+    operation, file_name, destination = command.split()
+
+    if operation != "cp" or file_name == destination:
+        print("File error")
+
+    try:
+        with (open(file_name, "r") as file_in,
+              open(destination, "w") as file_out):
+            file_out.write(file_in.read())
+
+    except FileNotFound:
+        raise "File not found"
