8afa6dd4de7052349f7f92270c7c24d47c673013
Bogdan <bogdan.rylov422@gmail.com>
2024-08-05 23:06:54 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split()
+    if (
+            len(command_list) != 3
+            or command_list[0] != "cp"
+            or command_list[1] == command_list[2]
+    ):
+        return
+
+    file1_name = command_list[1]
+    file2_name = command_list[2]
+    with open(file1_name) as file1, open(file2_name, "w") as file2:
+        for line in file1:
+            file2.write(line)
