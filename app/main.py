d2e655538ad9602f3c3ab331b845b95ec0ac3075
Misha Havryliuk <misharob31415@gmail.com>
2024-08-05 15:12:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    com = command.split()
+    source_file = com[1]
+    destination_file = com[2]
+    if len(com) != 3 or com[0] != "cp":
+        raise ValueError("Command should be in wrong format")
+    if source_file != destination_file:
+        try:
+            with (
+                open(source_file, "r") as file_in,
+                open(destination_file, "w") as file_out
+            ):
+                file_out.write(file_in.read())
+        except FileNotFoundError:
+            print(f"Error: The file {source_file} does not exist.")
+        except Exception as e:
+            print(f"Error occurred: {e}")
