b8f60b100c705a30208545a3f756386846f99d44
Pasha <pavik.varganov@gmail.com>
2024-08-06 14:23:11 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+class IncorrectCommand(Exception):
+    pass
+
+
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+    if len(command) != 3 or "cp" not in command:
+        raise IncorrectCommand("Incorrectly written command")
+    if command[1] == command[2]:
+        return
+
+    with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+        file_out.write(file_in.read())
