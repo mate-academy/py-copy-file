248808bfa1f36c4c5f26a633a12b0ae7977e73c7
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-08-06 16:52:40 +0300

    Task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, first_name, second_name = command.split()
+    if first_name != second_name and command == "cp":
+        with (open(first_name, "r") as file_in,
+              open(second_name, "w") as file_out):
+            file_out.write(file_in.read())
