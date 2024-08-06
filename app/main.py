29bc4f6f7078038369adca48b528d6489fba9132
achalaidiuk <sasha.treyser90@gmail.com>
2024-08-06 12:53:08 +0300

    Fixing review changes

--- app/main.py
+++ app/main.py
@@ -1,17 +1,12 @@
-class FileNotFound(Exception):
-    pass
-
-
 def copy_file(command: str) -> None:
     operation, file_name, destination = command.split()
 
-    if operation != "cp" or file_name == destination:
-        print("File error")
-
     try:
-        with (open(file_name, "r") as file_in,
-              open(destination, "w") as file_out):
-            file_out.write(file_in.read())
-
-    except FileNotFound:
-        raise "File not found"
+        if operation != "cp" or file_name == destination:
+            print("File error")
+        else:
+            with (open(file_name, "r") as file_in,
+                  open(destination, "w") as file_out):
+                file_out.write(file_in.read())
+    except FileNotFoundError:
+        print("File not found")
