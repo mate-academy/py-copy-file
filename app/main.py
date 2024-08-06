bc2e67c6ee9fb6690197a65b6ccc81e847603e06
Bogdan <bogdan.rylov422@gmail.com>
2024-08-06 18:35:55 +0300

    Reformat code according to mentor's requirements

--- app/main.py
+++ app/main.py
@@ -1,14 +1,9 @@
 def copy_file(command: str) -> None:
-    command_list = command.split()
-    if (
-            len(command_list) != 3
-            or command_list[0] != "cp"
-            or command_list[1] == command_list[2]
-    ):
-        return
-
-    file1_name = command_list[1]
-    file2_name = command_list[2]
-    with open(file1_name) as file1, open(file2_name, "w") as file2:
-        for line in file1:
-            file2.write(line)
+    command, source_file_name, destination_file_name = command.split()
+    if command == "cp" and source_file_name != destination_file_name:
+        with (
+            open(source_file_name) as source_file,
+            open(destination_file_name, "w") as destination_file
+        ):
+            for line in source_file:
+                destination_file.write(line)
