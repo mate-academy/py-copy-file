aac32090999cf4cfe3b7b14ab9f0eb7885dcc003
Andrii <andrii.khamaza0@gmail.com>
2024-08-06 15:11:20 +0300

    fixed mistakes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
-    _, first_name, second_name = command.split()
+    command, first_name, second_name = command.split()
 
-    if first_name != second_name:
+    if first_name != second_name and command == "cp":
         with open(first_name) as file_in, open(second_name, "w") as file_out:
             file_out.write(file_in.read())
