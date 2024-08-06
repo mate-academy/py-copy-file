a7a59ecb21f38be8a488016a320c4e96a62a636d
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-08-06 08:56:23 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None | ValueError:
+    if command.split()[0] != "cp" or len(command.split()) != 3:
+        raise ValueError("Invalid command format")
+
+    original, copy = command.split()[1], command.split()[2]
+    if original == copy:
+        return
+
+    with open(original, "r") as file_in, open(copy, "w") as file_out:
+        file_out.write(file_in.read())
