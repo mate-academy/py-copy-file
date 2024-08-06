5cce16ef136f041795243265e1fc7044ce54be20
achalaidiuk <sasha.treyser90@gmail.com>
2024-08-06 13:06:45 +0300

    Fixing review changes

--- app/main.py
+++ app/main.py
@@ -2,9 +2,7 @@ def copy_file(command: str) -> None:
     operation, file_name, destination = command.split()
 
     try:
-        if operation != "cp" or file_name == destination:
-            print("File error")
-        else:
+        if file_name != destination and operation == "cp":
             with (open(file_name, "r") as file_in,
                   open(destination, "w") as file_out):
                 file_out.write(file_in.read())
