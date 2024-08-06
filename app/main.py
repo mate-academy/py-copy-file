4971df4ba3bb1a8785301abdc7b5468b094c809d
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-06 17:43:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+
+def copy_file(command: str) -> str | None:
+    new_str = command.split()
+    if len(new_str) < 3:
+        return "argument is not correct"
+    file_first = new_str[1]
+    file_second = new_str[2]
+    cmd = new_str[0]
+    if cmd != "cp":
+        return "Command is not correct"
+
+    if file_first == file_second:
+        pass
+    else:
+        try:
+            with (
+                open(file_first, "r") as first,
+                open(file_second, "w") as second
+            ):
+                for line in first:
+                    second.write(f"{line}")
+        except OSError as e:
+            print(f"Error {e}")
+
+
+copy_file("cp file.txt new_file.txt")
