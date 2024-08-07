141c6b2bc8c5739b8938ba0b66a96d705ded2474
Andrii <andrii.khamaza0@gmail.com>
2024-08-07 13:03:27 +0300

    fixed mistakes

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@ def copy_file(command: str) -> None:
         command, first_name, second_name = command.split()
 
         if first_name != second_name and command == "cp":
-            with open(first_name) as file_in, open(second_name, "w") as file_out:
+            with (
+                open(first_name) as file_in,
+                open(second_name, "w") as file_out
+            ):
                 file_out.write(file_in.read())
     except Exception as er:
         print(er)
