c61781ef18a5d35b4efc781be8655f7d84cecc2d
Halyna Rohatska <rohatskahr@gmail.com>
2024-08-07 11:04:57 +0300

    added changes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
+class FileError(Exception):
+    pass
+
 def copy_file(command: str) -> None:
     is_cp, command_to_read, command_to_write = command.split()
-    if is_cp != "cp":
-        raise ValueError("The command must start with the command 'cp'")
 
-    if command_to_read != command_to_write:
-        with (
-            open(command_to_read, "r") as file_in,
-            open(command_to_write, "w") as file_out
-        ):
-            file_out.write(file_in.read())
+    if is_cp == "cp" and command_to_read != command_to_write:
+        try:
+            with (
+                open(command_to_read, "r") as file_in,
+                open(command_to_write, "w") as file_out
+            ):
+                file_out.write(file_in.read())
+        except FileError:
+            print("The file is corrupted")
