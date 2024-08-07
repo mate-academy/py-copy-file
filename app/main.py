385339d263c99abe2e5290ba4d41e7a8edbeec4f
Denys Piuro <denys.pyuro@gmail.com>
2024-08-07 15:14:25 +0300

    Updated according to Oleksandr's remarks

--- app/main.py
+++ app/main.py
@@ -1,38 +1,22 @@
-class CopyFileError(Exception):
-    pass
-
-
-class InvalidCommandError(CopyFileError):
-    pass
-
-
-class SameFileError(CopyFileError):
-    pass
-
-
-class CustomFileNotFoundError(CopyFileError):
-    pass
-
-
-class FileCopyError(CopyFileError):
-    pass
-
-
-def copy_file(copy_command: str) -> None:
-    command, source_file, destination_file = copy_command.split()
-
-    if command != "cp" or len(copy_command.split()) != 3:
-        raise InvalidCommandError("Invalid command format")
-    if source_file == destination_file:
-        raise SameFileError(f"{source_file} "
-                            f"and {destination_file} files are the same.")
+def copy_file(command: str) -> None:
     try:
+        parts = command.split()
+
+        if (
+                len(parts) != 3
+                or parts[0] != "cp"
+                or parts[1] == parts[2]
+        ):
+            return
+
+        source_file = parts[1]
+        destination_file = parts[2]
+
         with (
             open(source_file, "r") as file_in,
             open(destination_file, "w") as file_out
         ):
             file_out.write(file_in.read())
-    except FileNotFoundError:
-        raise CustomFileNotFoundError(f"File {source_file} not found.")
+
     except Exception as e:
-        raise FileCopyError(f"An error occurred: {e}")
+        raise OSError("An error occurred") from e
