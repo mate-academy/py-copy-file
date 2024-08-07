d30a0e9871bd4a5221caa71f12ec1fbd69ca6c5e
Ivan Shakhman <shakhmanivan@gmail.com>
2024-08-07 11:49:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def copy_file(command: str) -> None:
     comm, file_name, copy_name = command.split()
-    if len(command.split()) != 3 or comm != "cp" or file_name == copy_name:
-        return
-    try:
-        with open(file_name, "r") as file_in, open(copy_name, "w") as file_out:
-            file_out.write(file_in.read())
-    except FileNotFoundError:
-        print(f"File {file_name} not found")
+    if len(command.split()) == 3 and comm == "cp" and file_name != copy_name:
+        try:
+            with open(file_name, "r") as file_in, open(copy_name, "w") as file_out:
+                file_out.write(file_in.read())
+        except FileNotFoundError:
+            print(f"File {file_name} not found")
