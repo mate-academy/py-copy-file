db29c79824540652dd0b68e7b0b3e8059770ce0e
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-08-07 16:53:21 +0300

    fix 2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 def copy_file(command: str) -> None:
     command, first_name, second_name = command.split()
-    if first_name != second_name and command == "cp":
-        with (open(first_name, "r") as file_in,
-              open(second_name, "w") as file_out):
-            file_out.write(file_in.read())
+    try:
+        if first_name != second_name and command == "cp":
+            with (open(first_name, "r") as file_in,
+                    open(second_name, "w") as file_out):
+                file_out.write(file_in.read())
+    except FileNotFoundError:
+        print("File not found")
