4f24a1bbc12bf70db71dcbd46dbf92c3456aa760
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-08-07 13:15:21 +0200

    implement py-copy-file with changes 5

--- app/main.py
+++ app/main.py
@@ -1,30 +1,12 @@
-class SameNamesError(Exception):
-    pass
-
-
-class NotRightCommand(Exception):
-    pass
-
-
 def copy_file(command: str) -> None:
-    if len(command.split()) == 3:
-        try:
-            command, source_file, destination_file = command.split()
-            if command != "cp":
-                raise NotRightCommand("Incorrect command")
-            if source_file == destination_file:
-                raise SameNamesError("File names are the same!")
-
-            with (
-                open(
-                    f"{source_file}", "r"
-                ) as first_file,
-                open(
-                    f"{destination_file}", "w"
-                ) as copy_file
-            ):
-                copy_file.write(first_file.read())
-        except SameNamesError as e:
-            print(e)
-        except NotRightCommand as e:
-            print(e)
+    commands, source_file, destination_file = command.split()
+    if len(command.split()) == 3 and commands == "cp" and source_file != destination_file:
+        with (
+            open(
+                f"{source_file}", "r"
+            ) as first_file,
+            open(
+                f"{destination_file}", "w"
+            ) as copy_file
+        ):
+            copy_file.write(first_file.read())
