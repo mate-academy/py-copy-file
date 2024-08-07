1bd44eb611ee6ca874ba35a852c158b9cd752b1a
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-07 13:21:59 +0300

    first commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    com, file_name, copy_name = command.split()
+    if len(command.split()) == 3 and com == "cp" and file_name != copy_name:
+        try:
+            with(
+                open(file_name, "r") as file_input,
+                open(copy_name, "w") as file_output
+            ):
+                file_output.write(file_input.read())
+        except FileNotFoundError:
+            print(f"File {file_name} not found.")
