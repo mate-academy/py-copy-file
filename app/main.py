b0851f7df817b70fa5909e612855946d9438d182
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-08-07 13:26:46 +0200

    implement py-copy-file with changes 7

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
 def copy_file(command: str) -> None:
-    commands, source_file, destination_file = command.split()
-    if (
-        len(command.split()) == 3
-        and commands == "cp"
-        and source_file != destination_file
-    ):
-        with (
-            open(
-                f"{source_file}", "r"
-            ) as first_file,
-            open(
-                f"{destination_file}", "w"
-            ) as copy_file
-        ):
-            copy_file.write(first_file.read())
+    if len(command.split()) == 3:
+        commands, source_file, destination_file = command.split()
+        if commands == "cp" and source_file != destination_file:
+            with (
+                open(
+                    f"{source_file}", "r"
+                ) as first_file,
+                open(
+                    f"{destination_file}", "w"
+                ) as copy_file
+            ):
+                copy_file.write(first_file.read())
