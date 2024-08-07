e610930616ab60b8d0319fd78371d6f099df0af4
Ivan Shakhman <shakhmanivan@gmail.com>
2024-08-07 11:38:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
 def copy_file(command: str) -> None:
-    temp_lst = command.split()
-    if len(temp_lst) != 3 or any(value for value in temp_lst) is None:
-        raise ValueError("incorrect data. You must specify 'command' \
-        'file name' and 'name of the copy to create'")
-    if temp_lst[0] != "cp":
-        raise ValueError(f"Unknown command {temp_lst[0]}")
-    if temp_lst[1] == temp_lst[2]:
-        raise ValueError("You can not create new file with same name")
-    with open(temp_lst[1], "r") as file_in, open(temp_lst[2], "w") as file_out:
-        file_out.write(file_in.read())
-    print(f"File {temp_lst[1]} was successfully copied to {temp_lst[2]}")
+    comm, file_name, copy_name = command.split()
+    if len(command.split()) != 3 or comm != "cp" or file_name == copy_name:
+        return
+    try:
+        with open(file_name, "r") as file_in, open(copy_name, "w") as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        print(f"File {file_name} not found")
