c6181965cc37efed5d1fbf41412b1255e5fa0a13
Pasha <pavik.varganov@gmail.com>
2024-08-07 12:15:41 +0300

    change "with"

--- app/main.py
+++ app/main.py
@@ -3,8 +3,11 @@ def copy_file(command: str) -> None:
 
     if filename1 != filename2 and command == "cp":
         try:
-            with open(filename1, "r") as file_in, \
-                    open(filename2, "w") as file_out:
-                file_out.write(file_in.read())
+            with open(filename1, "r") as file_in:
+                with open(filename2, "w") as file_out:
+                    file_out.write(file_in.read())
         except FileNotFoundError as e:
             print(e)
+
+
+copy_file("cp abc bca.txt")
