fccec07507cbff622e37d67adbb39f150427f0ed
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-07 14:33:32 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 
 def copy_file(command: str) -> None:
     new_str = command.split()
-
-    file_first = new_str[1]
-    file_second = new_str[2]
-    cmd = new_str[0]
+    cmd, file_first, file_second = new_str
 
     if file_first != file_second and cmd == "cp" and len(new_str) == 3:
 
@@ -13,9 +10,6 @@ def copy_file(command: str) -> None:
                 open(file_first, "r") as first,
                 open(file_second, "w") as second
             ):
-                for line in first:
-                    second.write(f"{line}")
+                second.write(first.read())
         except OSError as e:
             print(f"Error {e}")
-
-
