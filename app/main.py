80cc7d5ff37561716fae430cc39436338340c81f
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-07 13:23:13 +0300

    first commit

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str) -> None:
     com, file_name, copy_name = command.split()
     if len(command.split()) == 3 and com == "cp" and file_name != copy_name:
         try:
-            with(
+            with (
                 open(file_name, "r") as file_input,
                 open(copy_name, "w") as file_output
             ):
