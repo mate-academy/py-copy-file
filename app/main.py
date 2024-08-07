ddd3fcaf1fdfb799cd3d72814ab972e7ca129b9a
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-07 17:53:10 +0300

    implement py-core-file(fixed)

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 def copy_file(command: str) -> None:
     parts = command.split()
 
-    if len(parts) != 3 or parts[0] != "cp":
-        raise ValueError("Invalid command format")
+    if len(parts) == 3 and parts[0] == "cp":
+        src_file, dest_file = parts[1], parts[2]
 
-    src_file, dest_file = parts[1], parts[2]
-
-    if src_file == dest_file:
-        return
-
-    with open(src_file, "r") as file_in, open(dest_file, "w") as file_out:
-        file_out.write(file_in.read())
+        if src_file != dest_file:
+            try:
+                with (
+                    open(src_file, "r") as file_in,
+                    open(dest_file, "w") as file_out
+                ):
+                    file_out.write(file_in.read())
+            except (FileNotFoundError, IOError) as e:
+                print(f"Error copying file: {e}")
