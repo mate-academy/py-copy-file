12beef6e1e8cfdac8c0b57eb65a120a8bf48aedd
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-08-08 12:04:31 +0300

    Solution 1.5

--- app/main.py
+++ app/main.py
@@ -1,16 +1,10 @@
 def copy_file(command: str) -> None:
     parts = command.split()
-    if len(parts) != 3 or parts[0] != "cp":
-        raise ValueError(
-            "Invalid command format. "
-            'Expected format: "cp <source_file> <destination_file>"')
-    source_file = parts[1]
-    destination_file = parts[2]
-
-    if source_file != destination_file:
+    if len(parts) == 3 and parts[0] == "cp" and parts[1] != parts[2]:
+        source_file, destination_file = parts[1], parts[2]
         try:
             with (open(source_file, "r") as file_in,
                   open(destination_file, "w") as file_out):
                 file_out.write(file_in.read())
-        except OSError as err:
-            print(f"Error while copying file {err}")
+        except OSError as e:
+            print(f"An error occurred: {e}")
