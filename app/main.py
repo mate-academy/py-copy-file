b5b06d848adcf4278b1a26d70f81e17d44c92f5e
Bogdan <bogdan.rylov422@gmail.com>
2024-08-08 21:29:49 +0300

    Reformat code according to mentor's requirements

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 def copy_file(command: str) -> None:
-    command, source_file_name, destination_file_name = command.split()
-    if command == "cp" and source_file_name != destination_file_name:
-        with (
-            open(source_file_name) as source_file,
-            open(destination_file_name, "w") as destination_file
-        ):
-            for line in source_file:
-                destination_file.write(line)
+    try:
+        command, source_file_name, destination_file_name = command.split()
+    except ValueError:
+        print("Wrong command format")
+    else:
+        if command == "cp" and source_file_name != destination_file_name:
+            with (
+                open(source_file_name) as source_file,
+                open(destination_file_name, "w") as destination_file
+            ):
+                destination_file.write(source_file.read())
