aa6ae4c68d87a9a1890c1fbf063630d989afa476
pavel_t <{89582095}+{PavelTym}@users.noreply.github.com>
2024-08-09 15:45:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    copy_info = command.split()
+
+    source_file = copy_info[1]
+    target_file = copy_info[2]
+
+    if source_file == target_file:
+        print("The source and target files are identical. Does nothing.")
+        return
+
+    with open(source_file, "r") as file_in, open(target_file, "w") as file_out:
+        file_out.write(file_in.read())
+        print("Copying completed!")
