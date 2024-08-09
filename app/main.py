3cbb5f715eda947594aeea40451c151333939649
dkn3r <n.diachuk333@gmail.com>
2024-08-09 11:25:12 +0300

    implement py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_file(command: str) -> None:
     data = command.split()
-    file1, file2 = data[1], data[2]
-    with open(file1, "r") as file_in, open(file2, "w") as file_out:
-        file_out.write(file_in.read())
+    if len(data) == 3:
+        file1, file2 = data[1], data[2]
+        with open(file1, "r") as file_in, open(file2, "w") as file_out:
+            file_out.write(file_in.read())
