9bd1494a3612957ea90c6a7f909e0c32abeba6d6
serhii <ockar2018@gmail.com>
2024-08-09 16:23:40 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,22 +1,10 @@
 def copy_file(command: str) -> None:
     command_parts = command.split()
-    if len(command_parts) != 3:
-        print(f"Expected 3 arguments, got {len(command_parts)}")
-        return
-
     command, old_file, new_file = command_parts
-
-    if command != "cp":
-        print(f"Expected 'cp' command, got {command}")
-        return
-
-    if old_file == new_file:
-        print(f"File {new_file} already exists")
-        return
-
-    try:
-        with open(old_file, "r") as file_in:
-            with open(new_file, "w") as file_out:
-                file_out.write(file_in.read())
-    except OSError:
-        raise OSError(f"File {old_file} does not exist")
+    if len(command_parts) == 3 and command == "cp" and old_file != new_file:
+        try:
+            with open(old_file, "r") as file_in:
+                with open(new_file, "w") as file_out:
+                    file_out.write(file_in.read())
+        except OSError:
+            raise OSError(f"File {old_file} does not exist")
