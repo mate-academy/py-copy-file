76a119d5c6c836a5a1240236b00984d3469c3b25
Denys Piuro <denys.pyuro@gmail.com>
2024-08-09 18:24:16 +0300

    Fixed according to Oleksandr`s remarks

--- app/main.py
+++ app/main.py
@@ -1,22 +1,20 @@
 def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if (
+        len(parts) == 3
+        and parts[0] == "cp"
+        and parts[1] == parts[2]
+    ):
+        _, source_file, destination_file = parts
+
     try:
-        parts = command.split()
-
-        if (
-                len(parts) != 3
-                or parts[0] != "cp"
-                or parts[1] == parts[2]
-        ):
-            return
-
-        source_file = parts[1]
-        destination_file = parts[2]
-
         with (
             open(source_file, "r") as file_in,
             open(destination_file, "w") as file_out
         ):
+
             file_out.write(file_in.read())
 
-    except Exception as e:
-        raise OSError("An error occurred") from e
+    except OSError:
+        print("An error occured")
