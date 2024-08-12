2d9b35a177d7fa7d95a8699c3a50a69607757dd8
Marharyta Zazulia <zazmargo@gmail.com>
2024-08-12 18:39:12 -0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 def copy_file(command: str) -> None:
     command_content = command.split()
-    if len(command_content) == 3:
-        if command_content[0] == "cp":
-            if command_content[1] != command_content[2]:
-                with (open(command_content[1], "r") as file_in,
-                      open(command_content[2], "w") as file_out):
-                    file_out.write(file_in.read())
+    if (
+        len(command_content) == 3
+        and command_content[0] == "cp"
+        and command_content[1] != command_content[2]
+    ):
+        with (open(command_content[1], "r") as file_in,
+              open(command_content[2], "w") as file_out):
+            file_out.write(file_in.read())
