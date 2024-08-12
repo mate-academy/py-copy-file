6393f5448551e6cb13db6fd26dd232acf8d82653
Oleksandr Vovk <165489096+VovkOL@users.noreply.github.com>
2024-08-12 21:04:35 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 def copy_file(command: str) -> None:
     file_names = command.split()
+
+    if len(file_names) != 3:
+        print(
+            "Error: The command must include exactly three arguments."
+        )
+        return
+
     if file_names[1] == file_names[2]:
         return
+
     with (
         open(file_names[1], "r") as file_in,
         open(file_names[2], "w") as file_out
