7d5ee18c0c8372d3fc7335f3b1c0072e97ef0109
FalseCoreCentury <simonov0101k@gmail.com>
2024-08-12 19:39:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 def copy_file(command: str) -> None:
-    command = command.split()
-    if command[1] == command[2]:
-        pass
-    else:
-        with open(command[1], "r") as f, open(command[2], "w") as f_copy:
-            f_copy.write(f.read())
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        print("Invalid command!")
+        return
+
+    if parts[1] == parts[2]:
+        return
+
+    with open(parts[1], "r") as f, open(parts[2], "w") as f_copy:
+        f_copy.write(f.read())
+
+
+copy_file("cp file.txt file_copy.txt")
