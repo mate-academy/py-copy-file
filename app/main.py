8bface759f6225c5674ce05f79dfd241473c4209
Marharyta Zazulia <zazmargo@gmail.com>
2024-08-12 12:59:55 -0300

    add check len command_content

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
     command_content = command.split()
-    if command_content[0] == "cp":
-        if command_content[1] != command_content[2]:
-            with (open(command_content[1], "r") as file_in,
-                  open(command_content[2], "w") as file_out):
-                file_out.write(file_in.read())
+    if len(command_content) == 3:
+        if command_content[0] == "cp":
+            if command_content[1] != command_content[2]:
+                with (open(command_content[1], "r") as file_in,
+                      open(command_content[2], "w") as file_out):
+                    file_out.write(file_in.read())
