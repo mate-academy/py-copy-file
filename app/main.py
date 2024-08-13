e744a712db8640978c3b617bab21aa7bc5cd883d
Oleksandr Vovk <165489096+VovkOL@users.noreply.github.com>
2024-08-13 17:15:51 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     file_names = command.split()
 
-    if len(file_names) != 3:
+    if len(file_names) != 3 or file_names[0] != "cp":
         print(
             "Error: The command must include exactly three arguments."
         )
