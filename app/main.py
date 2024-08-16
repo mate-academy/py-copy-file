0db33ab4f51c6ace5069a8511c3462bf727222cd
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2024-08-17 00:03:33 +0300

    add my solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if not (command.startswith("cp ")):
+        return
+
+    cmd, file1, file2 = command.split()
+    if (file1 == file2
+            or not (file1.endswith(".txt"))
+            or not (file2.endswith(".txt"))):
+        return
+
+    with open(f"{file1}", "r") as file_in, open(f"{file2}", "w") as file_out:
+        file_out.write(file_in.read())
