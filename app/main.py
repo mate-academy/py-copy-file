c40b56abf93ab8523a67c9addf47c802159149c3
user <graban2253@gmail.com>
2024-08-16 13:26:00 +0300

    Finished

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     parts = command.split()
 
-    if len(parts) == 3:
+    if len(parts) == 3 and parts[0] == "cp":
         file1, file2 = parts[1], parts[2]
         if file1 != file2:
             with open(file1, "r") as file_in, open(file2, "w") as file_out:
