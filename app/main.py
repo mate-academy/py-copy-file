241a8002f5d3c1972a45cd32a5de4d7c09731cb5
AntonBessarab <antonbessarab55@gmail.com>
2024-08-19 12:10:11 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(cp_line: str) -> None:
     parts = cp_line.split()
-    source_file, destination_file = parts[1], parts[2]
     if len(parts) != 3 or parts[0] != "cp":
         raise ValueError("Invalid command format."
                          " Use: cp <source> <destination>")
+    source_file, destination_file = parts[1], parts[2]
     if source_file == destination_file:
         return
     with (open(source_file, "r") as file_in,
