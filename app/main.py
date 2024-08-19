806633dd441acf68dd063ee37c5a664b8c24ae0c
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2024-08-19 16:16:57 +0300

    solution mistake was fixed

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 def copy_file(command: str) -> None:
-    if not (command.startswith("cp ")):
+    if (not (command.startswith("cp "))
+            and len(command.split()) == 3):
         return
 
     cmd, file1, file2 = command.split()
     if (file1 == file2
-            or not (file1.endswith(".txt"))
-            or not (file2.endswith(".txt"))):
+            or not file1.endswith(".txt")
+            or not file2.endswith(".txt")):
         return
 
     with open(f"{file1}", "r") as file_in, open(f"{file2}", "w") as file_out:
