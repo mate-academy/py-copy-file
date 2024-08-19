d304d6889032cea3577901a2006ff7803e4bd530
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2024-08-19 21:16:57 +0300

    add some checkstyle update

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 def copy_file(command: str) -> None:
-    if (not (command.startswith("cp "))
-            and len(command.split()) == 3):
+    if (
+            not (command.startswith("cp "))
+            and len(command.split()) == 3
+    ):
         return
 
     cmd, file1, file2 = command.split()
-    if (file1 == file2
+    if (
+            file1 == file2
             or not file1.endswith(".txt")
-            or not file2.endswith(".txt")):
+            or not file2.endswith(".txt")
+    ):
         return
 
     with open(f"{file1}", "r") as file_in, open(f"{file2}", "w") as file_out:
