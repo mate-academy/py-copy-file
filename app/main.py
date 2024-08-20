1bf9b3ffdac9950c3dbaaeb9b54cbce4e5a2a4a5
Felix Poslushnyi <felixp4@gmail.com>
2024-08-20 05:51:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    command_list = command.split()
+
+    cmd = command_list[0]
+    file_in = command_list[1]
+    file_out = command_list[2]
+
+    path_in = os.path.join(os.path.dirname(__file__), file_in)
+    path_out = os.path.join(os.path.dirname(__file__), file_out)
+
+    if cmd != "cp":
+        print("Enter correct command, please ...")
+        return
+
+    if file_in == file_out:
+        print("Doing nothing ...")
+        return
+
+    with open(path_in, "r") as src, open(path_out, "w") as dst:
+        dst.write(src.read())
