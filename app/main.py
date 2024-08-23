a72165ee18dcf71c24bf85d9389f5570544e70e4
Andrii Uvarov <aaanyyy@gmail.com>
2024-08-23 18:55:21 +0200

    complete code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp, file_to_copy, new_file = command.split(" ")
+    if cp != "cp":
+        print("Not cp command")
+        return
+    if file_to_copy == new_file:
+        return
+    with open(file_to_copy, "r") as f_from, open(new_file, "w") as f_to:
+        content = f_from.read()
+        f_to.write(content)
+
+
+copy_file("cp main.py main.bak")
