c68b1b884d71bbb1a220df9bc8173f8ba2d61609
Rudge0 <maksim.pariychuk07@gmail.com>
2024-08-25 22:48:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,5 +4,6 @@ def copy_file(command: str) -> None:
     if all_files[0] is "cp":
 
         if all_files[1] != all_files[2]:
-            with open(all_files[1], "r") as first_file, open(all_files[2], "w") as second_file:
+            with (open(all_files[1], "r") as first_file,
+                  open(all_files[2], "w") as second_file):
                 second_file.write(first_file.read())
