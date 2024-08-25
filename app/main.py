89e6f42a8c0c44aef35848b68c90a6f2b5fe179b
Rudge0 <maksim.pariychuk07@gmail.com>
2024-08-25 22:49:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     all_files = command.split(" ")
 
-    if all_files[0] is "cp":
+    if all_files[0] == "cp":
 
         if all_files[1] != all_files[2]:
             with (open(all_files[1], "r") as first_file,
