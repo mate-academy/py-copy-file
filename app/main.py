f54832d52885226ca90d906226733c61aac0c895
Yaroslav Ivanov <extazzzyprofit@gmail.com>
2024-08-26 20:51:10 +0300

    updated

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_dile(command: str) -> None:
-    cp, old, new = command.split()
+    if len(command.split()) == 3:
+        cp, old, new = command.split()
     if cp == "cp" and old != new:
         with open(old, "r") as old, open(new, "w") as new:
-            for line in old.readlines():
-                new.write(line)
+            new.write(old.read())
