c3d5cdde0feb4627dcda63d726aef38173212398
Yaroslav Ivanov <extazzzyprofit@gmail.com>
2024-08-26 18:31:05 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_dile(command: str) -> None:
+    cp, old, new = command.split()
+    if cp == "cp" and old != new:
+        with open(old, "r") as old, open(new, "w") as new:
+            for line in old.readlines():
+                new.write(line)
