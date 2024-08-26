4829058fa328d9791f671feb0c34c5f9aaa20a8f
Illia Khomutov <ilyakhomutov613@gmail.com>
2024-08-26 14:04:14 +0300

    Solution 5

--- app/main.py
+++ app/main.py
@@ -3,12 +3,11 @@ def copy_file(command: str) -> None:
         command_name, source_file_name, destination_file_name = command.split()
 
         if (
-            (source_file_name != destination_file_name)
-            and (command_name == "cp")
+            source_file_name != destination_file_name
+            and command_name == "cp"
         ):
             with (
                 open(source_file_name, "r") as source,
                 open(destination_file_name, "w") as destination
             ):
-
                 destination.write(source.read())
