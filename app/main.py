7a90bd660d727794bf02e774852ac7ce6053895a
Alex <54539kalashnikov@gmail.com>
2024-08-27 21:52:33 +0300

    Check if command has 3 arguments

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     name_file = command.split()
-    if name_file[0] == "cp":
+    if len(name_file) == 3 and name_file[0] == "cp":
         if name_file[1] != name_file[2]:
             with (
                 open(name_file[1], "r") as file_in,
