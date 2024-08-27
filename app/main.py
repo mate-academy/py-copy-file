dcde9936ae95efeb8329adc0aa7e38c5bd821653
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-27 17:50:31 +0300

    add condition

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
-    list_of_files = command[3:].split(" ")
-    if list_of_files[0] != list_of_files[1]:
-        with (open(list_of_files[0], "r") as file_in,
-              open(list_of_files[1], "a") as file_out):
-            for line in file_in.readlines():
-                file_out.write(line)
+    if command.startswith("cp ") and len(command.split(" ")) == 3:
+        list_of_files = command[3:].split(" ")
+        if list_of_files[0] != list_of_files[1]:
+            with (open(list_of_files[0], "r") as file_in,
+                  open(list_of_files[1], "a") as file_out):
+                for line in file_in.readlines():
+                    file_out.write(line)
