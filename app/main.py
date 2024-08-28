921db2dd2675616980b20c14f7f5f5d487b9e03e
Maksym Lupekha <2627number2@gmail,com>
2024-08-28 18:37:44 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def copy_file(command: list) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    origin_file, copy_file = parts[1], parts[2]
+
+    if origin_file == copy_file:
+        return
+
+    try:
+        with open(origin_file, "r") as file_in, \
+             open(copy_file, "w") as file_out:
+            file_out.write(file_in.read())
+
+    except Exception:
+        pass
