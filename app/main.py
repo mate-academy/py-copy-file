5e4bf9d7ab6080c9561f0dbdd8e14d833cd8b8a0
Illia Dubina <armahanz.music@gmail.com>
2024-09-02 17:21:26 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     command_elements = command.split()
-    if len(command_elements) == 3 and command_elements[0] == "cd":
+    if len(command_elements) == 3 and command_elements[0] == "cp":
         f_1, f_2 = command_elements[1], command_elements[2]
         if f_1 != f_2:
             with open(f_1, "r") as file_in, open(f_2, "w") as file_out:
