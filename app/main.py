ce987e2efb5ba9c98cdcf2f4ad9489c8c9e6b45c
Illia Dubina <armahanz.music@gmail.com>
2024-09-02 17:12:58 +0300

    checking if cp is not missed and there are two arguments after that and fix doing indexing twice

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def copy_file(command: str) -> None:
-    files = command.strip("cp ").split()
-    if files[0] != files[1]:
-        with open(files[0], "r") as file_in, open(files[1], "w") as file_out:
-            file_out.write(file_in.read())
+    command_elements = command.split()
+    if len(command_elements) == 3 and command_elements[0] == "cd":
+        f_1, f_2 = command_elements[1], command_elements[2]
+        if f_1 != f_2:
+            with open(f_1, "r") as file_in, open(f_2, "w") as file_out:
+                file_out.write(file_in.read())
