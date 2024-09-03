aeba147407e0c6c1e8e0a28bc76deca556900fcc
Dmitry <dimon032006@gmail.com>
2024-09-03 18:14:44 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_names = command.split(" ")
+    if (file_names[1] != file_names[2]
+            and file_names[0] == "cp"
+            and len(file_names) == 3):
+        with (open(file_names[1], "r") as file_in,
+              open(file_names[2], "w") as file_out):
+            for line in file_in:
+                file_out.write(line)
