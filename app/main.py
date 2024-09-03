7ad66e9879d5bff0d934d42b62098506496fc081
Ivan <vanya.bidnenko@gmail.com>
2024-09-03 16:08:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file, file_copy = command.split(" ")[1:3]
+
+    if file == file_copy:
+        return
+
+    with open(file, "r") as f, open(file_copy, "w") as fc:
+        content = f.read()
+        fc.write(content)
+
+
+command = "cd andrii Suprun.txt"
+
+copy_file(command)
+
