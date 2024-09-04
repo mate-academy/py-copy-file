10b99775f036743f841be294e10a4df776b73799
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-09-04 11:01:20 +0300

    add solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+
+    if "cp" not in command:
+        print("Unknow command")
+        return
+
+    if command[1] == command[2]:
+        print("Files name the same")
+        return
+
+    with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+        file_out.write(file_in.read())
