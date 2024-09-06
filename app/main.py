227dda1233c4acff7916f4a3161672d745fe0ec7
Gariman <garimanblack1@gmail.com>
2024-09-06 15:22:56 +0300

    Copy File

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    nod = command.strip().split()
+
+    if nod[0] != "cp":
+        return
+    elif nod[1] == nod[2]:
+        return
+    try:
+        with open(nod[1], "r") as file_in:
+            content = file_in.read()
+        with open(nod[2], "w") as file_out:
+            file_out.write(content)
+    except FileNotFoundError:
+        print(f"File {nod[1]} not found.")
