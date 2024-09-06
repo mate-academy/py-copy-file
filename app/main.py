de257d3516270d82fe00250a04a32433b1d45649
Tetiana Sobko <sobkotiana@gmail.com>
2024-09-07 01:43:37 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    if len(command) != 3:
+        raise Exception("Invalid command")
+
+    _, source_file_name, file_copy_name = command
+
+    if source_file_name != file_copy_name:
+        with (
+            open(source_file_name, "r") as source_file,
+            open(file_copy_name, "w") as file_copy
+        ):
+            file_copy.write(source_file.read())
