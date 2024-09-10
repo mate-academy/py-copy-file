ac628a82003c999033846f2144972eb35c5a5562
Andrii Levchenko <levchenkoandrii11@gmail.com>
2024-09-10 10:37:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    words = command.split()
+
+    if len(words) == 3 and words[0] == "cp":
+        file_name = words[1]
+        copy_name = words[2]
+
+        if file_name == copy_name:
+            return
+
+        with open(file_name, "r") as file_in, open(copy_name, "w") as file_out:
+            file_out.write(file_in.read())
