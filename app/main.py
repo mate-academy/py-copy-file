bc197a246913c7aec71471cc5dc70ca56dcf6ccd
Artem Mahda <artm.magda@gmail.com>
2024-09-10 21:15:44 +0300

    Added error print if command format is incorrect

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "cp":
+        print("Error: Command format is incorrect.")
         return
-    name_to_copy = parts[1]
-    new_name = parts[2]
+    name_to_copy, new_name = parts[1], parts[2]
     if name_to_copy == new_name:
         return
     with open(name_to_copy, "r") as file_in, open(new_name, "w") as file_out:
