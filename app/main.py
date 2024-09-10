8995e802002d70ecf6b3e00657128b50dd7fadc6
Artem Mahda <artm.magda@gmail.com>
2024-09-10 20:19:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
     parts = command.split()
-    if len(parts) != 3 or parts[0] != 'cp':
+    if len(parts) != 3 or parts[0] != "cp":
         return
     name_to_copy = parts[1]
     new_name = parts[2]
     if name_to_copy == new_name:
         return
-    with open(name_to_copy, 'r') as file_in, open(new_name, 'w') as file_out:
+    with open(name_to_copy, "r") as file_in, open(new_name, "w") as file_out:
         file_out.write(file_in.read())
