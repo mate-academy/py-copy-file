affcd95e09e7133f1d2fb2f6be38043db914f1a7
Ekaterina <sudarkova.ekaterina@gmail.com>
2024-09-11 00:45:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split()
+    if len(command) != 3 or command[0] != "cp":
+        return
+    file_name = command[1]
+    new_file_name = command[2]
+    if file_name != new_file_name:
+        with (open(file_name, "r") as file_in,
+              open(new_file_name, "w") as file_out):
+            file_out.write(file_in.read())
