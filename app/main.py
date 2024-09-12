eb5f00f84bfd4c3871cfcd158939731449c731a1
Alona <alena.tsapenko7@gmail.com>
2024-09-12 12:36:39 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    source_file, dest_file = parts[1], parts[2]
+
+    if source_file == dest_file:
+        return
+
+    with open(source_file, "r") as file_in, open(dest_file, "w") as file_out:
+        file_out.write(file_in.read())
