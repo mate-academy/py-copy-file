3fb5af6b5b94186ade4787f281d0d14baec82155
Ekaterina <sudarkova.ekaterina@gmail.com>
2024-09-12 10:30:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
     command = command.split()
     if len(command) != 3 or command[0] != "cp":
+        print("You need to include 'cp' ")
         return
-    file_name = command[1]
-    new_file_name = command[2]
+    _, file_name, new_file_name = command
     if file_name != new_file_name:
         with (open(file_name, "r") as file_in,
               open(new_file_name, "w") as file_out):
