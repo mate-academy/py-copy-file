c211d87f58ec27dff6f409c3663204771c487474
Andrii Uvarov <aaanyyy@gmail.com>
2024-09-16 11:25:08 +0200

    minor changes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 def copy_file(command: str) -> None:
     cp, file_to_copy, new_file = command.split(" ")
-    if cp != "cp":
+    if cp != "cp" or file_to_copy == new_file:
         print("Not cp command")
         return
-    if file_to_copy == new_file:
-        return
+
     with open(file_to_copy, "r") as f_from, open(new_file, "w") as f_to:
         content = f_from.read()
         f_to.write(content)
