ce56b9744f5c842f5406e9a36735a1975fbbec93
Nastya Kalinina <kalininan.kalinina@gmail.com>
2024-09-18 19:36:30 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-import shutil
-
-
 def copy_file(command: str) -> None:
-    file, new_file = command.split()[1], command.split()[2]
-    if file != new_file:
-        shutil.copy(file, new_file)
+    cp_command, file, new_file = command.split()
+    if cp_command != "cp":
+        raise ValueError(f"The first argument must be 'cp'! "
+                         f"Instead '{cp_command}' provided.")
+    print(cp_command, file, new_file)
+
+    with open(file, "r") as file_in, open(new_file, "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
