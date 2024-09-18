d48f9e8848b0f88819b3057791b0ce2a58b63e7a
Nastya Kalinina <kalininan.kalinina@gmail.com>
2024-09-18 20:38:03 +0200

    Solution4

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        raise ValueError("We need 3 elements!")
     cp_command, file, new_file = command.split()
     if cp_command != "cp":
         raise ValueError(f"The first argument must be 'cp'! "
                          f"Instead '{cp_command}' provided.")
 
-    with open(file, "r") as file_in, open(new_file, "w") as file_out:
-        content = file_in.read()
-        file_out.write(content)
+    if file != new_file:
+        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
