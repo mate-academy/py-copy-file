6231f0c6c27de51f04ed8a79e395da6ce713d30b
Nazar Makivchuk <nazar.makivchuk@gmail.com>
2024-09-18 18:19:09 +0300

    copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, source_file, destination_file = command.split()
+
+    if len(command) != 3 or command != "cp":
+        raise ValueError("Invalid command format. "
+                         "Use: 'cp source_file destination_file'")
+
+    if source_file == destination_file:
+        return
+
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
+        file_out.write(file_in.read())
