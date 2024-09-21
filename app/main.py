b4b59a4db369fa92c59bca119abbd06e6fb6bc99
Ruslan Toister <rtoister.cv@gmail.com>
2024-09-21 23:26:48 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command, file, new_file = command.split()
+
+    if command != "cp" or file == new_file:
+        raise ValueError()
+
+    with (open(file, "r") as file_in,
+          open(new_file, "w") as file_out):
+        file_out.write(file_in.read())
+
+
