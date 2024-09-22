d04d61e748b9ac02b66194d880a4f16a48f02534
Viacheslav417865 <sfinks417865@gmail.com>
2024-09-22 16:17:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    args = command.split()
+    if len(args) != 3 or args[0] != "cp":
+        raise (ValueError
+               ("Invalid command format. Expected 'cp <src_file> <dst_file>'"))
+    src_file, dst_file = args[1], args[2]
+    if src_file == dst_file:
+        print("Source and destination files are the same. Doing nothing.")
+        return
+    with open(src_file, "r") as file_in, open(dst_file, "w") as file_out:
+        file_out.write(file_in.read())
