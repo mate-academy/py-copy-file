76a7f1f5fb0bba7140faae38c03901c6a2af1a85
Ivan Bilobrov <ivan.b@team.voodoo-digital.com>
2024-09-22 17:33:02 +0300

    done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_parts = command.split(" ")
+
+    file_to_copy = command_parts[1]
+    file_copy_name = command_parts[2]
+
+    if file_to_copy == file_copy_name:
+        return
+
+    with open(file_to_copy, "r") as file_in, \
+         open(file_copy_name, "w") as file_out:
+        file_out.write(file_in.read())
+
+
+copy_file("cp file.txt file-new.txt")
