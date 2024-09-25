58a611bc8a8a381872d8825d8f8b12ea0da36fce
yura <pollo7964132@gmail.com>
2024-09-25 20:07:29 +0300

    fixed flake8 error

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ def copy_file(cp: str) -> None:
     file_names = cp.split()
     if file_names[1] != file_names[2] and file_names[0] == "cp":
         try:
-            with open(file_names[1]) as file_in, open(file_names[2], "w") as file_out:
+            with (
+                open(file_names[1]) as file_in,
+                open(file_names[2], "w") as file_out
+            ):
                 file_out.write(file_in.read())
         except FileNotFoundError:
             print(f"File {file_names[1]} does not exist")
