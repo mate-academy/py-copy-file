c835286eea4da68f018938656f1d0b542d1c7bcc
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2024-09-26 12:45:28 +0300

    copy_file function has been implemented

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    source_file = parts[1]
+    dest_file = parts[2]
+
+    if source_file == dest_file:
+        return
+
+    with open(source_file, "r") as file_in, open(dest_file, "w") as file_out:
+        content = file_in.read()
+        file_out.write(f"{content}")
