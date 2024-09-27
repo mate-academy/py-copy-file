276aff36f84364a03ecb248bdf497f87e9345c90
Lev <lev2099@gmail.com>
2024-09-27 19:54:10 +0300

    copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    source_file, destination_file = parts[1], parts[2]
+
+    if source_file == destination_file:
+        return
+
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
+        file_out.write(file_in.read())
+
+
+copy_file("cp file.txt file.txt")  # Does nothing
+
+copy_file("cp file.txt new_file.txt")
+open("file.txt").read() == open("new_file.txt").read()  # True
