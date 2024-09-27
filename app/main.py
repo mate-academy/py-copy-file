ca30dbbaf7087dbffde63c8a2bc1124cf34d9f07
Lev <lev2099@gmail.com>
2024-09-28 00:31:16 +0300

    add print("some informative message")

--- app/main.py
+++ app/main.py
@@ -2,19 +2,15 @@ def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) != 3 or parts[0] != "cp":
+        print("some informative message")
         return
 
     source_file, destination_file = parts[1], parts[2]
 
     if source_file == destination_file:
+        print("some informative message")
         return
 
     with (open(source_file, "r") as file_in,
           open(destination_file, "w") as file_out):
         file_out.write(file_in.read())
-
-
-copy_file("cp file.txt file.txt")  # Does nothing
-
-copy_file("cp file.txt new_file.txt")
-open("file.txt").read() == open("new_file.txt").read()  # True
