e4c6dd546aa3e9824dde7e2f43630bca599189b6
dmytro <dmysamo@gmail.com>
2024-09-28 12:55:09 +0300

    copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command_line: str) -> None:
+    (command,
+     file_name,
+     new_file_name) = command_line.split(" ")
+    # we are cheking on correct data input
+    if command != "cp" or file_name == new_file_name:
+        return
+    # we are opening current file and creat new file
+    # using mode "w" and write down there data from
+    # current_file
+    with (open(file_name, "r") as file_in,
+          open(new_file_name, "w") as file_out):
+        for line in file_in.readlines():
+            file_out.write(line)
