412af95daa14ef348e818d520b0fa35286e6d6d7
Lev <lev2099@gmail.com>
2024-09-28 19:44:28 +0300

    add information

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) != 3 or parts[0] != "cp":
-        print("some informative message")
+        print("Invalid command or missing arguments")
         return
 
     source_file, destination_file = parts[1], parts[2]
 
     if source_file == destination_file:
-        print("some informative message")
+        print(f"Cannot copy the file to itself! "
+              f"Source file {source_file} is the same"
+              f" as the destination file {destination_file}.")
+
         return
 
     with (open(source_file, "r") as file_in,
