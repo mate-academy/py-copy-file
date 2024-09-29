0f6b031991d5bfcac263e81a1a63258e1643d213
Nikita Ponomarov <wertyciti29@gmail.com>
2024-09-29 17:50:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split(" ")
+    if split_command[1] != split_command[2]:
+        with (
+                open(split_command[1], "r") as in_file,
+                open(split_command[2], "w") as out_file
+        ):
+            out_file.write(in_file.read())
