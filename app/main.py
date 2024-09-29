633b99c020ea744144cee1208fe92aa5148013ed
Nikita Ponomarov <wertyciti29@gmail.com>
2024-09-29 18:17:06 +0300

    added checks to clarify the correctness of the command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 def copy_file(command: str) -> None:
-    split_command = command.split(" ")
-    if split_command[1] != split_command[2]:
+    split_command = command.split()
+    first_check = len(split_command) == 3
+    second_check = split_command[0] == "cp"
+    third_check = split_command[1] != split_command[2]
+    if first_check and second_check and third_check:
         with (
                 open(split_command[1], "r") as in_file,
                 open(split_command[2], "w") as out_file
