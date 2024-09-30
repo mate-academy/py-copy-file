4f52a4fc8337cc9598dc4111cf68fff419245eed
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-09-30 16:34:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    cm, file, new_file = command.split(" ")
+    if file == new_file:
+        return
+    if cm == "cp":
+        with open(file, "r"), open(new_file, "w"):
+            shutil.copyfile(file, new_file)
