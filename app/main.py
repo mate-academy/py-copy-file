c57b9407f8f63d54d386e958c61857186e79c302
Nikita Ponomarov <wertyciti29@gmail.com>
2024-09-30 17:21:01 +0300

    unnecessary variables removed

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 def copy_file(command: str) -> None:
     split_command = command.split()
-    first_check = len(split_command) == 3
-    second_check = split_command[0] == "cp"
-    third_check = split_command[1] != split_command[2]
-    if first_check and second_check and third_check:
+    if (
+            len(split_command) == 3
+            and split_command[0] == "cp"
+            and split_command[1] != split_command[2]
+    ):
         with (
                 open(split_command[1], "r") as in_file,
                 open(split_command[2], "w") as out_file
