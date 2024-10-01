7d596b585b666758947cb7c00c7a910465c76e30
Obusker <mr.kudryachenko@gmail.com>
2024-10-01 17:33:33 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_files(file_names: str) -> None:
+    command, file, new_file = file_names.split()
+    if command == "cp" and file != new_file:
+        with open(file, "r") as origin, open(new_file, "w") as copy:
+            copy.write(origin.read())
