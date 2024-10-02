0dd47a308d9c7bcfd6c4e736993168f990f15c3b
Obusker <mr.kudryachenko@gmail.com>
2024-10-02 11:18:38 +0300

    commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def copy_files(file_names: str) -> None:
-    command, file, new_file = file_names.split()
-    if len(file_names.split()) == 3 and command == "cp" and file != new_file:
-        with open(file, "r") as origin, open(new_file, "w") as copy:
-            copy.write(origin.read())
+    if len(file_names.split()) == 3:
+        command, file, new_file = file_names.split()
+        if command == "cp" and file != new_file:
+            with open(file, "r") as origin, open(new_file, "w") as copy:
+                copy.write(origin.read())
