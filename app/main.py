039b3d44ced8a2416cdb5506bfdf238916ec651a
Max <mxbely98@gmail.com>
2024-10-03 19:09:47 +0300

    Deleted context manager and fix if condition

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 
 def copy_file(command: str) -> None:
     command = command.split()
-    if command[0] != "cp":
-        return
-    if command[1] == command[2]:
+    if command[0] != "cp" and command[1] == command[2]:
         return
     with (
         open(command[1], "r") as file_in,
