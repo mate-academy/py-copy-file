58e44abf836bb65cc06ebe0aae2821879ea62550
Liftoff <vladislav70707@gmail.com>
2024-10-03 18:28:13 +0200

    test_solution copy_file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    data = command.split()
+    source_file, target_file = data[1], data[2]
+    if source_file == target_file:
+        return print("Does nothing")
+    with open(source_file, "r") as file_in, open(target_file, "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
