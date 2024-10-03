697879a7b193025a86273e7223588de70bd0b5ed
Max <mxbely98@gmail.com>
2024-10-03 19:08:51 +0300

    Deleted context manager

--- app/main.py
+++ app/main.py
@@ -1,14 +1,3 @@
-from contextlib import contextmanager
-
-
-@contextmanager
-def open_file(name: str, method: str) -> None:
-    func = open(name, method)
-    try:
-        yield func
-    finally:
-        func.close()
-
 
 def copy_file(command: str) -> None:
     command = command.split()
@@ -17,7 +6,12 @@ def copy_file(command: str) -> None:
     if command[1] == command[2]:
         return
     with (
-        open_file(command[1], "r") as file_in,
-        open_file(command[2], "x") as file_out,
+        open(command[1], "r") as file_in,
+        open(command[2], "x") as file_out,
     ):
         file_out.write(file_in.read())
+
+
+if __name__ == "__main__":
+    comm = "cp test.txt new_test.txt"
+    copy_file(comm)
