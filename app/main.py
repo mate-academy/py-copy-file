e6fb625d71b7dbaa68b4ce687679a9a7addc8247
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-10-04 13:32:41 +0300

    Some fix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
     command_name, first_file, second_file, *_ = command.split()
 
-    if first_file == second_file or command.split(" ")[0] == "cp":
+    if first_file == second_file or command.split(" ")[0] != "cp":
         return
 
     with open(first_file, "r") as first, open(second_file, "w") as second:
