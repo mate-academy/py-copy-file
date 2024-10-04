75071a4757a1f0b07fff6a60ef18608dde5b6d36
Ivan Hutsuliak <ivan.guculyak@gmail.com>
2024-10-04 23:02:30 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 def copy_file(command: str) -> None:
     cp, name, new_name = command.split()
+    if len(command.split()) != 3:
+        print("Wrong command!")
     if cp != "cp":
+        print('Command should be "cp".')
         return
     if name == new_name:
+        print("This file exists.")
         return
     with (
         open(name, "r") as source_file,
