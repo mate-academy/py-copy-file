ca30fd30f0d7bb1318d46fb1c95d6ccf1eb17fa5
Liftoff <vladislav70707@gmail.com>
2024-10-05 21:24:57 +0200

    solution copy_file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def copy_file(command: str) -> None:
     data = command.split()
+    if len(data) != 3 and data[0].lower() == "cp":
+        raise "Invalid command format. Correct: cp <source_file> <target_file>"
     source_file, target_file = data[1], data[2]
     if source_file == target_file:
         return print("Does nothing")
