1dfdaf3617613ce315274bc6ad8c6fc6f572f67d
Daria Ganchukova <umi194@gmail.com>
2024-10-07 17:44:50 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 def copy_file(command: str) -> None:
-    command_name, source_filename, target_filename, *_ = command.split(" ")
+    command_split = command.split()
+    if len(command_split) == 3:
+        command_name, source_filename, target_filename = command_split
 
-    if command_name == "cp" and source_filename != target_filename:
-        with (open(source_filename, "r") as file_in,
-              open(target_filename, "w") as file_out):
-            file_out.write(file_in.read())
+        if command_name == "cp" and source_filename != target_filename:
+            with (open(source_filename, "r") as file_in,
+                  open(target_filename, "w") as file_out):
+                file_out.write(file_in.read())
