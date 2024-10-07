7f6942bce3fa11b4f56bd634487aab66b97d2186
Aleksandr Mazur <a9p9m9@gmail.com>
2024-10-07 09:49:52 +0300

    Solution_2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def copy_file(command: str) -> None:
     com_split = command.split()
-    with (open(com_split[1], "r") as file_in,
-          open(com_split[2], "w") as file_out):
+    with (
+        open(com_split[1], "r") as file_in,
+        open(com_split[2], "w") as file_out
+    ):
         file_out.write(file_in.read())
