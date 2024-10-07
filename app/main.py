ccf5b935051eab77a0061e4015d1bbd825163bae
Aleksandr Mazur <a9p9m9@gmail.com>
2024-10-07 16:39:23 +0300

    Solution_9

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     split_result = command.split()
-    if len(command.split()) == 3:
+    if len(split_result) == 3:
         cp, file, new_file = split_result
         if cp == "cp" and file != new_file:
             with open(file, "r") as file_in, open(new_file, "w") as file_out:
