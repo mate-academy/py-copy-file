8f5900007b986482038bfb625c3d85c793ff189b
Oleh <oleh.shcherbyna0@gmail.com>
2024-10-08 16:50:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    if command[:2] == "cp":
+    if len(command.split()) == 3 and command.split()[0] == "cp":
         if command.split()[1] == command.split()[2]:
             return
         with (open(command.split()[1], "r") as file_in,
