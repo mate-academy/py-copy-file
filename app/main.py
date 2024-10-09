7ddea14b43fb6d8d1a3a3a88317f11c38b44f058
Nazar <nazargorodyko@gmail.com>
2024-10-09 16:06:01 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str):
+    ls = command.split(" ")
+    if ls[0] == "cp":
+        if ls[1] == ls[2]:
+            print("Does nothing")
+        with open(ls[1], "r") as file_in, open(ls[2], "w") as file_out:
+            file_out.write(file_in.read())
