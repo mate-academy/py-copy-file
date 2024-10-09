b512eaf42ae37f7bb550a6e3bd7185504cf9c9d4
Dima Havrylenko <94nj111@gmail.com>
2024-10-09 14:19:19 +0300

    copy-file solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+    cp = command[0]
+    file_from = command[1]
+    file_to = command[2]
+    if cp == "cp" and file_from != file_to:
+        with open(file_from, "r") as f:
+            content = f.read()
+        with open(file_to, "w") as f:
+            f.write(content)
