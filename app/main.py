214754f46278fc72bbda97964dba0e9dbfd3473b
Nazar <nazargorodyko@gmail.com>
2024-10-09 18:13:07 +0300

    Fix error

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
     ls = command.split(" ")
-    if ls[0] == "cp":
-        if ls[1] == ls[2]:
-            print("Does nothing")
-        with open(ls[1], "r") as file_in, open(ls[2], "w") as file_out:
-            file_out.write(file_in.read())
+    if len(ls) == 3:
+        if ls[0] == "cp":
+            if ls[1] == ls[2]:
+                print("Does nothing")
+            with open(ls[1], "r") as file_in, open(ls[2], "w") as file_out:
+                file_out.write(file_in.read())
