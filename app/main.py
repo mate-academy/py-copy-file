2adbd789a08a6186dd56783b338580a628489e8b
Nazar <nazargorodyko@gmail.com>
2024-10-10 10:03:02 +0300

    ÐFix format

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 def copy_file(command: str) -> None:
     ls = command.split()
-    if len(ls) == 3:
-        if ls[0] == "cp":
-            if ls[1] == ls[2]:
-                print("Does nothing")
-            with open(ls[1], "r") as file_in, open(ls[2], "w") as file_out:
-                file_out.write(file_in.read())
+    if len(ls) == 3 and ls[0] == "cp" and ls[1] != ls[2]:
+        with open(ls[1], "r") as file_in, open(ls[2], "w") as file_out:
+            file_out.write(file_in.read())
+    else:
+        print("command should be in this format <cp file.txt new_file.txt>")
