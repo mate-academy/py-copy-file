f651ae3ff538cb4d052da008ef5bb53a7e2c0b8b
Dima Havrylenko <94nj111@gmail.com>
2024-10-10 17:38:07 +0300

    copy-file fix

--- app/main.py
+++ app/main.py
@@ -4,7 +4,5 @@ def copy_file(command: str) -> None:
     file_from = command[1]
     file_to = command[2]
     if cp == "cp" and file_from != file_to:
-        with open(file_from, "r") as f:
-            content = f.read()
-        with open(file_to, "w") as f:
-            f.write(content)
+        with open(file_from, "r") as file_from, open(file_to, "w") as file_to:
+            file_to.write(file_from.read())
