27d8190ce7c3483e8c79f7e368f9a380b8adf069
vavarvariuk <vasya.varvariuk.27@gmail.com>
2024-10-11 12:33:32 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    list_file = command.split()
+    if list_file == 3 and list_file[1] != list_file[2]:
+        with open(list_file[1], "r") as f, open(list_file[2], "w") as g:
+            g.write(f.read())
