3cc8c71cd2e9a4767876582d8a29a2a98996455b
vavarvariuk <vasya.varvariuk.27@gmail.com>
2024-10-11 12:41:22 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
     command_list = command.split()
-    if len(command_list) == 3 and command_list[1] != command_list[2]:
+    if len(command_list) == 3 and command_list[1] != command_list[2]\
+            and command_list[0] == "cp":
         with (open(command_list[1], "r") as main_file,
               open(command_list[2], "w") as file_copy):
             file_copy.write(main_file.read())
