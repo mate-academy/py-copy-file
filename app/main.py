4ad75e491f419c0757e070f67eacb27808743d4b
roman <skyfoxwork@gmail.com>
2024-10-12 16:06:02 +0300

    Solution-py-copy-file v1.0

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split()
+    if split_command[1] == split_command[2]:
+        return
+
+    if split_command[0] == "cp":
+        with (
+            open(split_command[1], "r") as file_input,
+            open(split_command[2], "w") as file_output
+        ):
+            file_output.write(file_input.read())
