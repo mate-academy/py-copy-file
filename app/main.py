ad7720d707a5ed396b7b47ee2bff3c63b46a0306
roman <skyfoxwork@gmail.com>
2024-10-12 18:22:02 +0300

    Solution-py-copy-file v2.0 AI

--- app/main.py
+++ app/main.py
@@ -1,7 +1,29 @@
+class CommandNameError(Exception):
+    """CommandNameError"""
+
+
+class DoubleNameError(Exception):
+    """DoubleNameError"""
+
+
+class LanCommandError(Exception):
+    """LaneCommandError"""
+
+
 def copy_file(command: str) -> None:
     split_command = command.split()
+
+    if len(split_command) < 3:
+        raise LanCommandError(
+            "The command must be"
+            " 'cp input_file_name.txt output_file_name.txt'"
+        )
+
     if split_command[1] == split_command[2]:
-        return
+        raise DoubleNameError(
+            f"intput: {split_command[1]} output: {split_command[1]}"
+            f" must not have the same names"
+        )
 
     if split_command[0] == "cp":
         with (
@@ -9,3 +31,5 @@ def copy_file(command: str) -> None:
             open(split_command[2], "w") as file_output
         ):
             file_output.write(file_input.read())
+    else:
+        raise CommandNameError(f"'{split_command[0]}' is not 'cp' command")
