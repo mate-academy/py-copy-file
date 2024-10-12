e70dc7556ddb6fb0dfdb521f1a72f01c79e0765b
Andrey Dementyev <dementyev.andreyy@gmail.com>
2024-10-12 16:37:33 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
 def copy_file(command: str) -> None:
-    parts = command.split()
+    command_parts = command.split()
 
-    if len(parts) != 3 or parts[0] != "cp":
+    if len(command_parts) != 3 or command_parts[0] != "cp":
         raise ValueError("Invalid command format. "
                          "Use 'cp <source_file> <destination_file>'.")
 
-    source_file, destination_file = parts[1], parts[2]
+    source_file, destination_file = command_parts[1], command_parts[2]
 
     if source_file == destination_file:
         return
 
     try:
-        with (open(source_file, "r") as file_in,
-              open(destination_file, "w") as file_out):
-            file_out.write(file_in.read())
+        with open(source_file, "r") as file_in:
+            with open(destination_file, "w") as file_out:
+                file_out.write(file_in.read())
     except FileNotFoundError:
-        print(f"Error: {source_file} does not exist.")
+        raise FileNotFoundError(f"Error: {source_file} does not exist.")
