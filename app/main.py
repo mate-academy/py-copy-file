bccf76e0112d7c03ffb9ed10942263f7e68e20b7
Dmytro Stepanenko <dmytrostepanenkoua@gmail.com>
2024-10-13 23:10:42 +0300

    Solution fix 2

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 
-def copy_file_from_command_cp(command: str) -> None:
+def copy_file_using_cp_command(command: str) -> None:
     command_parsed = command.split(" ")
-    source_file_name = command_parsed[1]
-    destination_file_name = command_parsed[2]
+    src_file_name = command_parsed[1]
+    dst_file_name = command_parsed[2]
 
-    if source_file_name == destination_file_name:
-        print(f"File wasn't copied: source file '{source_file_name}' "
+    if src_file_name == dst_file_name:
+        print(f"File wasn't copied: source file '{src_file_name}' "
               f"has the same name as the destination file.")
     else:
-        with open(source_file_name, "r") as source_file, \
-             open(destination_file_name, "w") as destination_file:
+        with open(src_file_name, "r") as src_file, \
+             open(dst_file_name, "w") as dst_file:
 
-            file_in_data = source_file.readlines()
-            destination_file.writelines(file_in_data)
+            file_in_data = src_file.readlines()
+            dst_file.writelines(file_in_data)
