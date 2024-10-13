a2a4769351b510f1102c7e5ea2c6567ea4ab460f
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-10-14 00:01:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp, old_file, new_file = command.split()
+
+    if cp != "cp":
+        raise ValueError("The command need to start with 'cp'")
+
+    if old_file != new_file:
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
