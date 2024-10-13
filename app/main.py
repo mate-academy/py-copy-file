000e5a76a39e78ec5b20283d7cc3111833a41286
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-10-13 20:51:42 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str) -> None:
     done_lst_path = command.split()
     if done_lst_path[1] == done_lst_path[2]:
         return
-    with open(done_lst_path[1], "r") as file_in, open(one_lst_path[2], "w") as file_out:
+    with (open(done_lst_path[1], "r") as file_in,
+          open(done_lst_path[2], "w") as file_out):
         read_all = file_in.read()
         file_out.write(read_all)
-
