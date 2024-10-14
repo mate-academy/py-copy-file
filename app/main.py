c09ef8f35a136b34cb61d99fa3526dce585a8b2a
Serhiy Dema <sergeydema@gmail.com>
2024-10-14 18:31:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if (not isinstance(command, str) or len(command) < 3
+            or command[:3] != "cp "):
+        return
+
+    cmd_list = command.strip().split(" ")
+    if len(cmd_list) < 3 or cmd_list[1] == cmd_list[2]:
+        return
+
+    with open(cmd_list[1], "r") as file_in, open(cmd_list[2], "w") as file_out:
+        file_out.write(file_in.read())
