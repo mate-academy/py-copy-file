7aa05b04697f95eedea45fd315398a862ab73009
Serhii <serhijkrasa64uk5@gmail.com>
2024-10-14 20:52:30 +0300

    solution 2

--- app/main.py
+++ app/main.py
@@ -4,5 +4,6 @@ def copy_file(command: str) -> None:
     if command[1] == command[2]:
         pass
     elif ".txt" in (command[1] and command[2]):
-        with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+        with (open(command[1], "r") as file_in,
+              open(command[2], "w") as file_out):
             file_out.write(file_in.read())
