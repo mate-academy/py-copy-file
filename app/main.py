42f7f6ff802ee48423cb2f6cabbebeb6f4ab2a11
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-10-14 12:08:18 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
+import os
+
+
 def copy_file(command: str) -> None:
     done_lst_path = command.split()
+
     if done_lst_path[1] == done_lst_path[2]:
         return
-    with (open(done_lst_path[1], "r") as file_in,
-          open(done_lst_path[2], "w") as file_out):
-        read_all = file_in.read()
-        file_out.write(read_all)
+
+    if done_lst_path[0] != "cp":
+        print("Wrong command use 'cp'.")
+        return
+
+    if not os.path.exists(done_lst_path[1]):
+        print(f"File {done_lst_path[1]} not exist.")
+        return
+
+    try:
+        with (open(done_lst_path[1], "r") as source_file,
+              open(done_lst_path[2], "w") as destination_file):
+            read_all = source_file.read()
+            destination_file.write(read_all)
+    except IOError as e:
+        print(f"Error during copy file {e}")
