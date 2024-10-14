f5c4ec0cd3d4e838e302f5125d933ff643b365b0
Mike Shmygel <m.shmygel@gmail.com>
2024-10-14 23:14:22 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str = "cp") -> None:
+    with open(f"{command} file.txt", "r") as r_file:
+        with open("new_file.txt", "w") as w_file:
+            for line in r_file:
+                w_file.write(line)
+    r_file.close()
+    w_file.close()
