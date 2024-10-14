25eb78857dcd2d70c3e9ec3bfacdaf83f7cb030f
Serhiy Dema <sergeydema@gmail.com>
2024-10-14 19:07:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ def copy_file(command: str) -> None:
         return
 
     cmd_list = command.strip().split(" ")
-    if len(cmd_list) < 3 or cmd_list[1] == cmd_list[2]:
+    if len(cmd_list) < 3 or cmd_list[-2] == cmd_list[-1]:
         return
 
-    with open(cmd_list[1], "r") as file_in, open(cmd_list[2], "w") as file_out:
+    with open(cmd_list[-2], "r") as file_in,\
+            open(cmd_list[-1], "w") as file_out:
         file_out.write(file_in.read())
