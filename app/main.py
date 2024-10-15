fae415b164e5b308e9cfab539e28df86e97fd1a2
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-10-15 17:17:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,10 +2,7 @@ from shutil import copy
 
 
 def copy_file(command: str) -> None:
-    split_command = command.split(" ")
-
-    source_file = split_command[1]
-    destination_file = split_command[2]
+    _, source_file, destination_file = command.split(" ")
 
     if source_file == destination_file:
         return
