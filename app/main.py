5d65644bb87f1fdc2395ae66ef2511bbebf09d70
Max Savenya <savenya2013@gmail.com>
2024-10-15 17:17:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cmd, origin, dupe = command.split()
+
+    if cmd != "cp" or origin == dupe:
+        return
+    try:
+        with open(origin, "r") as source, open(dupe, "w") as copy:
+            copy.write(source.read())
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
