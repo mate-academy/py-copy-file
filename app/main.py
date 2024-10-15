38edfd21d92eb373225c6337ecf1f281b92da6a2
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-10-15 14:17:13 +0300

    my_solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+
+def copy_file(command: str) -> None:
+    date = command.split()
+    if date[0] != "cp":
+        return
+    first_txt, second_txt = date[1], date[2]
+    if first_txt == second_txt:
+        return
+    else:
+        with (open(first_txt, "r") as file_in,
+              open(second_txt, "w") as file_out):
+            file_out.write(file_in.read())
