870897a07d788d96abc38a5219d90b05fb05f36c
Artem_Shatylo <alukard32244@gmail.com>
2024-10-15 14:30:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parsed_command = command.split()
+    if (
+        len(parsed_command) != 3
+        or parsed_command[1] == parsed_command[2]
+        or parsed_command[0] != "cp"
+    ):
+        return
+
+    with open(parsed_command[1], "r") as current_file, open(
+        parsed_command[2], "w"
+    ) as new_file:
+        new_file.write(current_file.read())
+
+
+if __name__ == "__main__":
+    copy_file(input("Input command: "))
