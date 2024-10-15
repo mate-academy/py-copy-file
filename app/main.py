6243a8a9263c98bc628b58ab4cebcbc7ae1dc018
Vadym Sulim <ohnooq1@gmail.com>
2024-10-15 16:44:26 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp_command, source_file, copied_file = command.split()
+
+    if cp_command == "cp" and source_file == copied_file:
+        return
+
+    with open(source_file, "r") as source, open(copied_file, "w") as copied:
+        copied.write(source.read())
