818634f9462e2946d403410f4487b1bc7db0cad6
Volodymyr <uuforreasonsunknownuu@gmail.com>
2024-10-15 17:27:20 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    list_ = command.split()
+    first_name = str(list_[1])
+    second_name = str(list_[2])
+    if first_name == second_name:
+        return
+    with open(first_name, "r") as file_in, open(second_name, "w") as file_out:
+        file_out.write(file_in.read())
