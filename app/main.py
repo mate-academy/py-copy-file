2e5eec32fb9f089638587af7b7c9c04b730002c1
Serhii <serhijkrasa64uk5@gmail.com>
2024-10-15 09:09:38 +0300

    solution 4

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    print(command)
+    if command[0] != "cp":
+        raise ValueError("You line should start with cp")
+    if len(command) != 3:
+        raise ValueError("Write down cp than file name "
+                         "where the info is and than "
+                         "file name where you w ant to safe that info")
     if command[1] == command[2]:
-        pass
+        raise ValueError("There should be two different names of files")
     elif ".txt" in (command[1] and command[2]):
         with (open(command[1], "r") as file_in,
               open(command[2], "w") as file_out):
