d2f70f948cf600a1abec64c03ec8eac4762a6456
mregorlite <egor.infinity@gmail.com>
2024-10-15 16:02:52 +0300

    Solution v4

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
 def copy_file(command: str) -> None:
     commands = command.split()
-    if len(commands) < 3:
+    if (
+        len(commands) < 3 or
+        commands[0] != "cp" or
+        commands[1] == commands[2]
+    ):
         return
-    if commands[0] != "cp":
-        return
-    if commands[1] == commands[2]:
-        return
-
-    with open(commands[1], "r") as source, open(commands[2], "w") as target:
-        source.write(target.read())
-
+    try:
+        with open(commands[1], "r") as source, open(commands[2], "a") as target:
+            for line in source:
+                target.write(line)
+    except UnicodeError:
+        print("Error while copy file")
 
 if __name__ == "__main__":
     copy_file(input("Input command: "))
