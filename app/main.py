6da6e6a8d54fff3fd183b9f5abf1f46d8f82bf25
Bondzik-S <bondziks@gmail.com>
2024-10-15 17:00:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ def copy_file(command: str) -> None:
     if cmd != "cp" or source_file == target_file:
         return
     try:
-        with open(source_file, "r") as source, open(target_file, "w") as target:
+        with (open(source_file, "r") as source,
+              open(target_file, "w") as target):
             target.write(source.read())
     except UnicodeError:
         print("Error while copying file")
