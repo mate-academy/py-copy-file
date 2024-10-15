e58f03a2580122e7f60c4a014d3ca4ec3e69e908
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-10-15 17:44:04 +0300

    CorrectedSolution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from shutil import copy
 
 
-def copy_file(command: str) -> None:
-    _, source_file, destination_file = command.split(" ")
+def copy_file(command: str) -> str:
+    split_command = command.split()
+
+    if len(split_command) != 3:
+        return "Invalid command"
+
+    source_file, destination_file = split_command[1:]
 
     if source_file == destination_file:
-        return
+        return "Source and destination files are the same."
 
-    copy(source_file, destination_file)
+    try:
+        copy(source_file, destination_file)
+        return f"File copied successfully"
+    except FileNotFoundError:
+        return f"The source file: {source_file} does not exist"
+    except PermissionError:
+        return f"Permission denied for {destination_file}"
