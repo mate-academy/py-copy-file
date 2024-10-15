04a2ae5be33109dc4071edfb687981ae8f51fd0b
Volodymyr <uuforreasonsunknownuu@gmail.com>
2024-10-15 18:43:42 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -9,5 +9,6 @@ def copy_file(command: str) -> None:
         return
     if source_file == destination_file:
         return
-    with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
         file_out.write(file_in.read())
