81ac90049328cdf0d4035f3e3d216362d0095655
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-10-15 15:33:58 +0300

    my_solution

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ def copy_file(command: str) -> None:
     if command_parts[0] != "cp":
         print("Invalid command format.")
         return
-    first_txt, second_txt = command_parts[1], command_parts[2]
-    if first_txt == second_txt:
+    source_file, destination_file = command_parts[1], command_parts[2]
+    if source_file == destination_file:
         print("Source and destination files are the same.")
         return
     else:
-        with (open(first_txt, "r") as file_in,
-              open(second_txt, "w") as file_out):
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
             file_out.write(file_in.read())
