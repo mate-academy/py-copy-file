e8c23d220e55f38835d7ed3ca0a36e3082d95ce5
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2024-10-15 23:41:59 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ def copy_file(command: str) -> None:
         raise ValueError("Command can take only 2 arguments")
     cp_command, file, new_file = command.split()
     if cp_command != "cp":
-        raise ValueError(f"Command not found")
+        raise ValueError("Command not found")
 
     if file != new_file:
         with open(file, "r") as file_in, open(new_file, "w") as file_out:
