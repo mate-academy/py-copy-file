ce55936b4c57252bfbd69e9ce8c7a23ae8429f9d
Mike Shmygel <m.shmygel@gmail.com>
2024-10-15 16:14:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-def copy_file(command: str = "cp") -> None:
-    with open(f"{command} file.txt", "r") as r_file:
-        with open("new_file.txt", "w") as w_file:
-            for line in r_file:
-                w_file.write(line)
-    r_file.close()
-    w_file.close()
+def copy_file(command: str) -> None:
+    command_ls = command.split()
+    if len(command_ls) == 3 and command_ls[0] == "cp" and command_ls[1] != command_ls[2]:
+        with open(command_ls[1], "r") as r_file, open(command_ls[2], "w") as w_file:
+            w_file.write(r_file.read())
+
+
