21fc35600ed925c87b72ab6282cdb5f6ec4d8bd7
Serhiy Dema <sergeydema@gmail.com>
2024-10-15 14:03:05 +0300

    Solution 04

--- app/main.py
+++ app/main.py
@@ -1,12 +1,33 @@
 def copy_file(command: str) -> None:
-    if (not isinstance(command, str) or len(command) < 3
-            or command[:3] != "cp "):
+    if not isinstance(command, str) or len(command) <= 1:
+        print("Command is incorrect")
+        return
+
+    if command[:2] != "cp":
+        print("Command should start with 'cp'")
         return
 
     cmd_list = command.strip().split(" ")
-    if len(cmd_list) < 3 or cmd_list[-2] == cmd_list[-1]:
+    if len(cmd_list) <= 2:
+        print("Command 'cp' should contains at least two arguments")
         return
+
+    if cmd_list[-2] == cmd_list[-1]:
+        print("Warning: arguments are the same")
+        return
+
+    try:
+        open(cmd_list[-1], "r")
+    except FileNotFoundError:
+        pass
+    else:
+        confirm = input("Destination file exists, confirm override (y/n):")
+        while confirm not in ["y", "Y", "n", "N"]:
+            confirm = input("Destination file exists, confirm override (y/n):")
+        if confirm in ["n", "N"]:
+            return
 
     with open(cmd_list[-2], "r") as file_in,\
             open(cmd_list[-1], "w") as file_out:
-        file_out.write(file_in.read())
+        for line in file_in.readlines():
+            file_out.write(line)
