91f0855ce5f25efe68ff6cb6321f2994ebec10fe
srx <srx92@protonmail.com>
2024-10-23 16:30:21 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if command.split() != 3:
+        raise ValueError("Wrong copy command input")
+    cmd = command.split()[0]
+    data_file = command.split()[1]
+    data_file_copy = command.split()[2]
+    if cmd == "cp" and data_file_copy != data_file_copy:
+        with (open(data_file, "r") as dfile,
+              open(data_file_copy, "w") as cpfile):
+            cpfile.write(dfile.read())
