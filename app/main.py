b1838b078fc3ed6a770a7476aeb9098e97e4efc2
Anatolii <shtyrka.anatoliy@gmail.com>
2024-10-23 16:13:35 -0400

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split()) == 3:
+        command_to_execute, file, new_file = command.split()
+        if file == new_file:
+            return
+        if command_to_execute.lower() == "cp":
+            try:
+                with open(file, "r") as source, open(new_file, "w") as new:
+                    new.write(source.read())
+            except (FileNotFoundError, IOError) as err:
+                print("Error: ", err)
