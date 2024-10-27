76dbc0ba51ca52f846dc87d31a847c3147a106b1
vadym <vadym@ohmanalytics.com>
2024-10-27 15:52:25 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    file_names = command.replace("cp ", "").split(" ")
+    if file_names[0] == file_names[1]:
+        return
+    shutil.copy(file_names[0], file_names[1])
