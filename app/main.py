d20fd0f395fe8aa88f15edc121ddad5aed068fbc
vadym <vadym@ohmanalytics.com>
2024-10-27 21:21:03 +0200

    added checking command

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ import shutil
 
 def copy_file(command: str) -> None:
     file_names = command.replace("cp ", "").split(" ")
-    if file_names[0] == file_names[1]:
+    if file_names[0] == file_names[1] or not command.startswith("cp"):
         return
     shutil.copy(file_names[0], file_names[1])
