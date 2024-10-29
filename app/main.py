56117afd1d1f0106928ac32708b52fc9332cec4a
Eldar <eldardemchenko19@mail.com>
2024-10-29 21:25:22 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
 # write your code here
+import os
+
+
+def copy_file(command: str):
+
+    command_parts = command.split()
+
+    if len(command_parts) != 3 or command_parts[0] != "cp":
+        print("Invalid command format")
+        return
+
+    source, direction = command_parts[1], command_parts[2]
+
+    if source == direction:
+        return
+
+    if not os.path.isfile(source):
+        return
+
+    try:
+        with open(source, "r") as source, open(direction, "w") as direction:
+            direction.write(source.read())
+    except (FileNotFoundError, IOError) as err:
+        print("Error: ", err)
