4192d5b545125e6936318055d57240a3eb62fc01
Ihor Chaikovskyy <Altairerko@gmail.com>
2024-10-29 15:30:17 +0200

    copy file solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        print("Invaild command format")
+        return
+    _, source, destination = parts
+    if source == destination:
+        return
+    with open(source, "r") as file_in, open(destination, "w") as file_out:
+        file_out.write(file_in.read())
