fb5a9f633cd4d6027aa41ab737d80e3cd91af4c2
konstantinou77 <kostiakryvulia@gmail.com>
2024-11-01 16:43:36 +0200

    py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if parts[1] == parts[2]:
+        return
+    with open(parts[1], "r") as file_in, open(parts[2], "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
