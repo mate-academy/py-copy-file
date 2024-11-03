2ee43eb6efeaec8d92869ac911fc97db062cef18
Dmytro <labtech.kh@gmail.com>
2024-11-03 08:49:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
 # write your code here
+def copy_file(command: str) -> None:
+
+    cp, source_file_name, copy_file_name = command.split()
+    if cp != "cp" or source_file_name == copy_file_name:
+        return
+
+    with (open(source_file_name, "r") as source,
+          open("copy_file_name.txt", "w") as backup):
+        backup.write(source.read())
+        source.close()
+        backup.close()
