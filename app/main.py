f73222b88ca505b24ecf5300fca61b19a93984c2
vadym <vadym@ohmanalytics.com>
2024-11-03 18:17:15 +0200

    fixed flake

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ import shutil
 
 def copy_file(command: str) -> None:
     commands = command.split(" ")
-    if len(commands) != 3 or not commands[0] == "cp" or commands[1] == commands[2]:
+    if (
+            len(commands) != 3
+            or not commands[0] == "cp"
+            or commands[1] == commands[2]
+    ):
         return
     shutil.copy(commands[1], commands[2])
