ed2dfd31546bf514cb71762b956013dec28916b7
Yuriy Komar <omnigun85@gmail.com>
2024-11-04 19:13:46 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    check_list = command.split(" ")
+    if check_list[0] != "cp" or check_list[1] == check_list[2]:
+        return None
+
+    with (open(check_list[1], "r")
+          as file_in, open(check_list[2], "w") as file_out):
+        file_out.write(file_in.read())
