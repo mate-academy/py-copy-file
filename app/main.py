26cac02be3f14e499cf5a303c992d86f1b87e7a3
fanni <fmalevych@zoolatech.com>
2024-11-07 13:29:51 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    data = command.split()
+    file_to_copy = data[1]
+    copy = data[2]
+
+    if len(data) != 3 or data[0] != "cp" or file_to_copy == copy:
+        return
+
+    with open(file_to_copy, "r") as file_in, open(copy, "a") as file_out:
+        content = file_in.read()
+        file_out.write(content)
