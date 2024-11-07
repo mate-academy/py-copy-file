9b5a6cbca7db3eeefdd4220dd623c7b8d51477ac
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2024-11-07 15:54:23 +0200

    solution 02

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
+class CommandError(Exception):
+    def __str__(self) -> str:
+        return "Your command is not complete"
+
+
 def copy_file(command: str) -> None:
-    copy_command, old_file, new_file = command.split(" ")
-    if copy_command == "cp" and old_file != new_file:
-        with (open(old_file, "r") as file_in,
-              open(new_file, "w") as file_out):
-            content = file_in.read()
-            file_out.write(content)
+    try:
+        copy_command, old_file, new_file = command.split(" ")
+    except CommandError:
+        raise CommandError
+    else:
+        if copy_command == "cp" and old_file != new_file:
+            with (open(old_file, "r") as file_in,
+                  open(new_file, "w") as file_out):
+                content = file_in.read()
+                file_out.write(content)
