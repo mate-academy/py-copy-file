dff2939b1e66df7ae03b1b9c8e1e2d16fc795711
88victory88 <victory.otrok@gmail.com>
2024-11-09 11:00:48 +0800

    check cp was added

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 
 def copy_file(command: str) -> None:
     splited_command = command.split(" ")
-    if splited_command[1] != splited_command[2]:
-        with (open(splited_command[1], "r") as file_in,
-              open(splited_command[2], "w") as file_out):
-            file_in_text = file_in.read()
-            file_out.write(file_in_text)
+    if splited_command[0].lower() == "cp":
+        if splited_command[1] != splited_command[2]:
+            with (open(splited_command[1], "r") as file_in,
+                open(splited_command[2], "w") as file_out):
+                file_in_text = file_in.read()
+                file_out.write(file_in_text)
+
