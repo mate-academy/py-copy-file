2c9cd60f6b5dfb39d18b9098f533b4631fad77b8
Radeon <egorka22e40x@gmail.com>
2024-11-10 15:48:40 +0300

    add check

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 def copy_file(command: str) -> None:
-    operator, existing_file, new_file = command.split(" ")
-    if operator == "cp" and new_file != existing_file:
-        with (open(existing_file, "r") as file_in,
-              open(new_file, "w") as file_out):
-            for line in file_in:
-                file_out.write(line + "\n")
+    command_lst = command.split(" ")
+    if len(command_lst) == 3:
+        operator, existing_file, new_file = command_lst
+        if operator == "cp" and new_file != existing_file:
+            with (open(existing_file, "r") as file_in,
+                  open(new_file, "w") as file_out):
+                for line in file_in:
+                    file_out.write(line)
