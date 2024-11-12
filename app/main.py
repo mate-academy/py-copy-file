825bfaacf1afddf6064228529ef8577db9ae7657
Alex Plugin <plugin0785@gmail.com>
2024-11-12 17:31:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def copy_file(command: str) -> None:
     command_listed = command.split(" ")
-    if command_listed[1] == command_listed[2]:
+    if len(command_listed) != 3 or command_listed[0] != "cp":
+        return
+    elif command_listed[1] == command_listed[2]:
         return
     with open(command_listed[1], "r") as file_in, \
             open(command_listed[2], "w") as file_out:
