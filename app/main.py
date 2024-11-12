34f614dd5e3464875292b33ffc6bcc1afbe13f35
Alex Plugin <plugin0785@gmail.com>
2024-11-12 17:05:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_listed = command.split(" ")
+    if command_listed[1] == command_listed[2]:
+        return
+    with open(command_listed[1], "r") as file_in, \
+            open(command_listed[2], "w") as file_out:
+        file_out.write(file_in.read())
