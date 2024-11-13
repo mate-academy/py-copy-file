49fe95f2caa6497819fee0a58e2042a510222a4a
bashyrov <bashyroov@gmail.com>
2024-11-13 22:52:30 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(files_to_copy_path: str) -> None:
+    files = files_to_copy_path.split()
+    if files[1] == files[2]:
+        raise ValueError("Provide another file name.")
+    try:
+        with (open(files[1], "r") as file_in,
+              open(files[2], "w") as file_out):
+            for line in file_in:
+                file_out.write(line)
+    except (FileNotFoundError, PermissionError):
+        raise ValueError("Provide another file name.")
