104f215d979eb9b036b843d3510409bfb9ca6dff
Valeriy Semenik <valeriy.semenik@gmail.com>
2024-11-14 13:34:00 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    com_list = command.split()
+    if len(com_list) != 3 or com_list[0] != "cp" or com_list[1] == com_list[2]:
+        return
+    with open(com_list[1], "r") as file_in, open(com_list[2], "w") as file_out:
+        file_out.write(file_in.read())
