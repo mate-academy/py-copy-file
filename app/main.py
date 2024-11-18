42231c9cf03d019d20bb052579cdb6f09aceaeb5
AndreyShevchenkoDevil <ashevche20@gmail.com>
2024-11-18 15:50:13 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+    filename_in = parts[1]
+    filename_out = parts[2]
+
+    if filename_in == filename_out:
+        return
+
+    with (open(filename_in, "r") as file_in,
+          open(filename_out, "w") as file_out):
+        file_out.write(file_in.read())
