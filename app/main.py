485bfa75ff65480b0bc51dccd76fec4c543c5b8f
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2024-11-19 22:13:34 +0100

    solution2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 def copy_file(command: str) -> None:
-    file_out = command.split(" ")[1]
-    file_in = command.split(" ")[2]
+    names = command.split(" ")
+    file_in = names[1]
+    file_out = names[2]
     if file_in == file_out:
         pass
     else:
-        with open(file_out, "r") as file_out, open(file_in, "w") as file_in:
-            file_in.writelines(file_out.readlines())
+        with open(file_in, "r") as file_in, open(file_out, "w") as file_out:
+            file_out.writelines(file_in.readlines())
