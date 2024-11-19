fc68d1223154ace7215b12d71cf255d5b3b9802e
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2024-11-19 22:08:07 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_out = command.split(" ")[1]
+    file_in = command.split(" ")[2]
+    if file_in == file_out:
+        pass
+    else:
+        with open(file_out, "r") as file_out, open(file_in, "w") as file_in:
+            file_in.writelines(file_out.readlines())
