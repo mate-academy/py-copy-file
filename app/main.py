e660d453bcb45161e1b03c8366ab7aca0bd091eb
Serhiy <sergeynechaenko@ukr.net>
2024-11-19 23:21:34 +0200

    add check to command

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 def copy_file(command: str) -> None:
-    name = command.split()
 
-    with open(name[1], "r") as file_in, open(name[2], "w") as file_out:
-        if name[1] == name[2]:
+    if (len(command.split()) != 3 and command.split()[0] != "cp"
+            and command.count(" ") != 2):
+        return
+    command_parts = command.split()
+
+    with (open(command_parts[1], "r") as file_in,
+          open(command_parts[2], "w") as file_out):
+        if command_parts[1] == command_parts[2]:
             return
         for line in file_in:
             file_out.write(line)
