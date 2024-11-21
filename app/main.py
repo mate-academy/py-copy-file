d353095da8d4001a0109d473a5e292777b169cee
Ярослав Шевченко <yaroslav_0205@MacBook-Pro-Aroslav.local>
2024-11-21 20:59:53 +0200

    copy_file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        raise ValueError("Invalid command format. "
+                         "Use 'cp source_file destination_file'")
+
+    source_file = parts[1]
+    destination_file = parts[2]
+
+    if source_file == destination_file:
+        return
+
+    try:
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        print(f"Errors: {source_file} does not exist.")
+    except Exception as e:
+        print(f"An errors occurred: {e}")
