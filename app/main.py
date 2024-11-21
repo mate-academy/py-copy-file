416f895891939dc744abb233a87dbac1590a2d6d
whatmode <31383686+whatmode@users.noreply.github.com>
2024-11-21 12:27:24 -0700

    Updated logic to match the requirements

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
-    command_attributes = [command_param
-                          for command_param in command.split()
-                          if command_param not in ["cp", ""]]
-    file_source = command_attributes[0]
-    file_target = command_attributes[1]
-    if file_source != file_target:
-        with (open(file_source, "r") as file_source_obj,
-              open(file_target, "w") as file_target_obj):
-            file_target_obj.write(file_source_obj.read())
+    command = [command_param for command_param in command.split()]
+    if len(command) == 3:
+        command_cp = command_attributes[0]
+        file_source = command_attributes[1]
+        file_target = command_attributes[2]
+        if file_source != file_target and command_cp == "cp":
+            with (open(file_source, "r") as file_source_obj,
+                  open(file_target, "w") as file_target_obj):
+                file_target_obj.write(file_source_obj.read())
