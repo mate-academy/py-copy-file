8fab161cb602ea021c3d4921ce70f7d0b5d7cf0f
whatmode <31383686+whatmode@users.noreply.github.com>
2024-11-21 12:31:21 -0700

    Renamed the variable

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def copy_file(command: str) -> None:
     command = [command_param for command_param in command.split()]
     if len(command) == 3:
-        command_cp = command_attributes[0]
-        file_source = command_attributes[1]
-        file_target = command_attributes[2]
+        command_cp = command[0]
+        file_source = command[1]
+        file_target = command[2]
         if file_source != file_target and command_cp == "cp":
             with (open(file_source, "r") as file_source_obj,
                   open(file_target, "w") as file_target_obj):
