f682bd771b8f2325025fcbe6472aab19fb59edc6
Viacheslav Kaliak <trinidaa@gmial.com>
2024-11-22 14:15:08 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def copy_file(argument: str) -> None:
+    file_source = argument.split(" ")[1]
+    file_destination = argument.split(" ")[2]
+    if not os.path.isfile(file_source):
+        print(f"File '{file_source}' is not exist.")
+        return
+    elif os.path.isfile(file_destination):
+        print(f"File '{file_destination}' is exist.")
+        return
+    with open(file_source, "r") as f1, open(file_destination, "w") as f2:
+        f2.write(f1.read())
