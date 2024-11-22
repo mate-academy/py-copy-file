c4ec5a8f6b68738c7a2644e597e64d52c6f40062
Beliar24 <blackdid7@gmail.com>
2024-11-22 23:01:46 +0200

    copy function impemented

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ def copy_file(command: str) -> None:
 
     if files_name[1] == files_name[2]:
         return
-
-    with (open(files_name[1], "r") as file_in,
-          open(files_name[2], "w") as file_out):
-        for line in file_in:
-            file_out.write(line)
+    if len(command) > 1 and command.startswith("cp"):
+        with (open(files_name[1], "r") as file_in,
+              open(files_name[2], "w") as file_out):
+            for line in file_in:
+                file_out.write(line)
+    else:
+        return
