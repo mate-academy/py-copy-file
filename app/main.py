b78b91fe020944a86cff0c488c32db3111365546
Oleh <sholeg2005@gmail.com>
2024-11-23 19:28:20 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    com, f_name, s_name = command.split(" ")
+
+    if f_name != s_name:
+        with open(f_name, "r") as first, open(s_name, "w") as second:
+            for line in first:
+                second.write(line)
