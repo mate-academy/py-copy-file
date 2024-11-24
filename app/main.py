888eae619bdb8dee008c27e5c50d2b14fc2be177
Maksym Kryvsha <KryivshaMaxim@gmail.com>
2024-11-24 13:33:59 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(input_str: str) -> None:
-    old_name, new_name = input_str.strip("cp ").split()
-    if old_name != new_name:
-        with (open(old_name, "r") as input_file,
-              open(new_name, "a") as output_file):
-            for line in input_file:
-                output_file.write(line)
+    validator = input_str.split()
+    if len(validator) == 3 and validator[0] == "cp":
+        key_name, old_name, new_name = validator
+        if old_name != new_name:
+            with (open(old_name, "r") as input_file,
+                  open(new_name, "w") as output_file):
+                output_file.write(input_file.read())
