89a979af03d274721173d7fabd6dece6f78f914a
Maksym Kryvsha <KryivshaMaxim@gmail.com>
2024-11-24 13:18:00 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(input_str: str) -> None:
+    old_name, new_name = input_str.strip("cp ").split()
+    if old_name != new_name:
+        with open(old_name, "r") as input_file, open(new_name, "a") as output_file:
+            for line in input_file:
+                output_file.write(line)
