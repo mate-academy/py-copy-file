39c53adbbf5ea9d1cec29b6f0729d9ef970829a1
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-11-29 22:53:31 +0200

    Solution_v1.0

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split(" ")
+
+    if (len(split_command) == 3
+            and split_command[0] == "cp"
+            and split_command[1] != split_command[2]):
+
+        with (open(f"{split_command[1]}", "r") as file_in,
+              open(f"{split_command[2]}", "w+") as file_out):
+            file_out.write(file_in.read())
