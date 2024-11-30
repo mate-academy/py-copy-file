78dc40de58c60b5332fa42e7158232e8927597ca
Valdyslav <magnetto54@gmail.com>
2024-11-30 12:37:45 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    for part in command.split(" "):
+        if part[1] != part[2]:
+            with open(part[1], "r") as file_in, open(part[2], "w") as file_out:
+                file_out.write(file_in)
