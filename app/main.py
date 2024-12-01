fdb790e6179874601d4a42da5788fb78219c03f1
Глеб Дубинин <gleb1395@gmail.com>
2024-12-01 17:02:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    check_command = command.split()
+    if (len(check_command) != 3 or
+            check_command[0] != "cp" or
+            check_command[1] == check_command[2]):
+        return
+    with (open(check_command[1], "r") as file_in,
+          open(check_command[2], "w") as file_out):
+        for line in file_in:
+            file_out.write(line + "\n")
