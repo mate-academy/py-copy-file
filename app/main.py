916d00904686c5b6483a4435cd03afe3cd2be241
Глеб Дубинин <gleb1395@gmail.com>
2024-12-01 17:04:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def copy_file(command: str) -> None:
     check_command = command.split()
-    if (len(check_command) != 3 or
-            check_command[0] != "cp" or
-            check_command[1] == check_command[2]):
+    if (len(check_command) != 3
+            or check_command[0] != "cp"
+            or check_command[1] == check_command[2]):
         return
     with (open(check_command[1], "r") as file_in,
           open(check_command[2], "w") as file_out):
