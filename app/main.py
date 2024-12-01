9613d3edfdb9764231d853626287524271d80275
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2024-12-02 00:46:11 +0100

    fixing py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 def copy_file(command: str) -> None:
     line = command.split(" ")
+
+    if len(line) != 3 or line[0] != "cp":
+        raise ValueError
+
     file1 = line[1]
     file2 = line[2]
+
     if file1 == file2:
+        print("Source and destination files are the same. No action taken.")
         return
     with open(file1, "r") as file_in, open(file2, "w") as file_out:
-        for _ in file1:
-            file_out.write(file_in.readline())
+        for line in file_in:
+            file_out.write(line)
