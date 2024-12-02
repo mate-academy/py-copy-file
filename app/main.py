b71957de1266484794751f0d98bc4adf1a450f7d
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-12-02 15:41:07 +0200

    Solution_v1.1_updated due to suggestion

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 def copy_file(command: str) -> None:
     split_command = command.split(" ")
+    action, source, destination = split_command
+    if (len(split_command) != 3
+            and action != "cp"
+            and source == destination):
+        raise ValueError("Wrong command was entered!")
 
-    if (len(split_command) == 3
-            and split_command[0] == "cp"
-            and split_command[1] != split_command[2]):
-
-        with (open(f"{split_command[1]}", "r") as file_in,
-              open(f"{split_command[2]}", "w+") as file_out):
-            file_out.write(file_in.read())
+    with (open(f"{source}", "r") as file_in,
+          open(f"{destination}", "w+") as file_out):
+        file_out.write(file_in.read())
