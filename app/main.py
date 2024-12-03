b87f20ec11a32de431aa10ae70d6f5571d2b22c0
Psychox1k <success01001011@gmail.com>
2024-12-03 17:53:33 +0100

    updated solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,16 @@
 def copy_file(command: str) -> None:
-    file1, file2 = command.split(" ")[-2:]
 
-    with open(file1, "r") as file_in, open(file2, "w") as file_out:
-        file_out.write(file_in.read())
+    command_user = command.split(" ")
+
+    if len(command_user) < 3:
+        raise ValueError("Command must have at least 3 arguments")
+
+    if command_user[0] != "cp":
+        raise ValueError("Command must start with 'cp' to copy files")
+
+    file_src = command_user[1]
+    file_copy = command_user[2]
+
+    if not file_src == file_copy:
+        with open(file_src, "r") as file_in, open(file_copy, "w") as file_out:
+            file_out.write(file_in.read())
