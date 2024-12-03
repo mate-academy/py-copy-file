8bba62b8b7431163ae555c7b20548a3076ffbb2a
Volodymyr Kolomiiets <valdi6014@gmail.com>
2024-12-03 12:35:08 +0200

    Added FileNotFound, IOError exceptions

--- app/main.py
+++ app/main.py
@@ -5,6 +5,13 @@ def copy_file(command: str) -> None:
         file_source = command[1]
         file_target = command[2]
         if file_source != file_target and command_cp == "cp":
-            with (open(file_source, "r") as file_source_obj,
-                  open(file_target, "w") as file_target_obj):
-                file_target_obj.write(file_source_obj.read())
+            try:
+                with (open(file_source, "r") as file_source_obj,
+                      open(file_target, "w") as file_target_obj):
+                    file_target_obj.write(file_source_obj.read())
+            except FileNotFoundError as e:
+                print(f"Error: {e}")
+                print("Desired file was not found")
+            except IOError as e:
+                print(f"Error: {e}")
+                print("Something happened during read/write")
