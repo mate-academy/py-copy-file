69d88dcafa601f45cc491026a44d4f293f328af1
Ihor <andreiunderwhat@mail.com>
2024-12-08 19:37:12 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+
+def copy_file(names: str):
+    command, old_file, new_file = names.split(" ")
+    if command == "cp":
+        if not old_file == new_file:
+            with open(old_file, "r") as file_in, open(new_file, "w+") as file_out:
+                file_out.write(file_in.read())
+
+
