03fb3eb88c8fc587b4bcc6f78ae36df13f257b30
Artem Ig <artem.ig1990@gmail.com>
2024-12-09 18:11:16 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    f_name = command.split(" ")[1]
+    d_file = command.split(" ")[2]
+    if f_name == d_file:
+        return None
+    with open(f_name, "r") as fro, open(d_file, "w") as dest:
+        dest.write(fro.read())
