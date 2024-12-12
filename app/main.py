06e34331673fb32389e41fd4ee414b352cad6026
shafran <shafran.andriy@gmail.com>
2024-12-12 20:02:26 +0200

    Created function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split(" ")
+    if command_list[-1] == command_list[-2]:
+        pass
+    else:
+        with open(command_list[-2],
+                  "r") as file_in, open(command_list[-1],
+                                        "w") as file_out:
+            file_out.write(file_in.read())
+
+
+copy_file("cp file.txt new_file.txt")
