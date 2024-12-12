4f7c817b51a1972e9f3d5cb2dcdb16a710a233f8
Dolgov Dmytro <dimakherson74@gmail.com>
2024-12-12 22:08:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    element = command.split(" ")
+    if element[1] != element[2]:
+        with (open(element[1], "r") as source_file,
+              open(element[2], "w") as destination_file):
+            for line in source_file:
+                destination_file.write(line)
