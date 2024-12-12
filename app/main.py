1707977d45ea7e0f94ac729b5b00764663e788e3
Dolgov Dmytro <dimakherson74@gmail.com>
2024-12-12 22:24:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,12 @@ def copy_file(command: str) -> None:
     command_parts = command.split(" ")
     if len(command_parts) == 3 and command_parts[0] == "cp":
         if command_parts[1] != command_parts[2]:
-            with (open(command_parts[1], "r") as source_file,
-                  open(command_parts[2], "w") as destination_file):
-                for line in source_file:
-                    destination_file.write(line)
+            try:
+                with (open(command_parts[1], "r") as source_file,
+                      open(command_parts[2], "w") as destination_file):
+                    for line in source_file:
+                        destination_file.write(line)
+            except FileNotFoundError:
+                print("File not found!")
+            except PermissionError:
+                print("No permissions to access the file!")
