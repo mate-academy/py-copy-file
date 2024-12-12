77e4b816f4bcfe450871e2293641d5f463024490
shafran <shafran.andriy@gmail.com>
2024-12-12 22:40:43 +0200

    Created function

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str) -> None:
     command_list = command.split()
     if (len(command_list) == 3
             and command_list[0] == "cp"
-            and command_list[-1] == command_list[-2]):
+            and command_list[-1] != command_list[-2]):
         with open(command_list[-2],
                   "r") as file_in, open(command_list[-1],
                                         "w") as file_out:
