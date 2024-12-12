34d3ff2b87293fa361b54a2499c80d0c71d63ae8
shafran <shafran.andriy@gmail.com>
2024-12-12 22:38:41 +0200

    Created function

--- app/main.py
+++ app/main.py
@@ -1,13 +1,9 @@
 def copy_file(command: str) -> None:
     command_list = command.split()
-    if len(command_list) == 3 and command_list[0] == "cp":
-        if command_list[-1] == command_list[-2]:
-            pass
-        else:
-            with open(command_list[-2],
-                      "r") as file_in, open(command_list[-1],
-                                            "w") as file_out:
-                file_out.write(file_in.read())
-
-
-copy_file("cp file.txt new_file.txt")
+    if (len(command_list) == 3
+            and command_list[0] == "cp"
+            and command_list[-1] == command_list[-2]):
+        with open(command_list[-2],
+                  "r") as file_in, open(command_list[-1],
+                                        "w") as file_out:
+            file_out.write(file_in.read())
