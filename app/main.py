0760bfb8d8b2fd6c2b0aa8c3e13f74dd18e810e1
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2024-12-12 13:08:14 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split(" ")
+
+    if len(split_command) != 3 or split_command[0] != "cp":
+        print("Invalid command")
+        return
+
+    file_to_copy = split_command[1]
+    copied_file = split_command[2]
+
+    if file_to_copy == copied_file:
+        return
+
+    with open(file_to_copy, "r") as file, open(copied_file, "w") as copy:
+        copy.write(file.read())
