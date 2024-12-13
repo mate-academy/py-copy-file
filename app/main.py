6fc4e2b8f0d08cb72b0f0b152c5e710fdbfac44a
Valerii Kashpur <kashpur.v.f@gmail.com>
2024-12-13 17:59:26 +0200

    Custom copy file cmd

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def copy_file(command: str) -> None:
+    list_from_command = command.split(" ")
+    command_name, file_name, copy_file_name = list_from_command
+    is_name_valid = file_name != copy_file_name
+    is_command_valid = command_name == "cp"
+    is_all_params = len(list_from_command) == 3
+
+    print(is_name_valid, is_command_valid, is_all_params)
+
+    if is_name_valid and is_command_valid and is_all_params:
+        with (
+            open(file_name, "rb") as original,
+            open(copy_file_name, "wb") as copy
+        ):
+            copy.write(original.read())
+
+
+copy_file("cp mango.txt file-copy.txt")
