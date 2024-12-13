aefd88f32b9dce8ade79646bbbe488837b4745fe
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2024-12-13 06:03:59 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(cp: str) -> None:
+    command, file_in, file_out = cp.split()
+    if command == "cp":
+        with open(
+            "app/"
+            + file_in, "r"
+        ) as original, open("app/" + file_out, "w") as copy:
+            copy.write(original.read())
