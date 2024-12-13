6818ad72d3f9fb3d57af87b04846e4115b295a00
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2024-12-13 06:22:07 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 def copy_file(cp: str) -> None:
     list_input = cp.split()
-    if command == "cp" and len(list_input) = 3:
-        command, file_in, file_out = list_input
-        with open(
-            "app/"
-            + file_in, "r"
-        ) as original, open("app/" + file_out, "w") as copy:
-            copy.write(original.read())
+    command, file_in, file_out = list_input
+    if command == "cp" and len(list_input) == 3:
+        if file_in != file_out:
+            with open(
+                "app/"
+                + file_in, "r"
+            ) as original, open("app/" + file_out, "w") as copy:
+                copy.write(original.read())
