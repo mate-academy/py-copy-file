ac293c0b479a3bfcb8f7fb3ee473baaf538c3a01
Dell <gyrkalomas@gmail.com>
2024-12-14 17:59:42 +0300

    Fix flake8

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ class CpError(Exception):
 
 
 def copy_file(command: str) -> None:
-    command_parts = command.split()
-    if command_parts[1] == command_parts[2]:
+    files = command.split()
+    if files[1] == files[2]:
         pass
-    if command_parts[0] != "cp":
+    if files[0] != "cp":
         raise CpError
     else:
-        with open(command_parts[1], "r") as file_in, open(command_parts[2], "w") as file_out:
+        with open(files[1], "r") as file_in, open(files[2], "w") as file_out:
             file_out.write(file_in.read())
