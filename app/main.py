f85280366481f00f77a66782e0627fc9f7f3d513
Dell <gyrkalomas@gmail.com>
2024-12-14 17:55:52 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
+class CpError(Exception):
+    pass
+
+
 def copy_file(command: str) -> None:
-    ls = command.split()
-    if ls[1] == ls[2]:
+    command_parts = command.split()
+    if command_parts[1] == command_parts[2]:
         pass
+    if command_parts[0] != "cp":
+        raise CpError
     else:
-        with open(ls[1], "r") as file_in, open(ls[2], "w") as file_out:
+        with open(command_parts[1], "r") as file_in, open(command_parts[2], "w") as file_out:
             file_out.write(file_in.read())
