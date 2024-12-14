2ce14947821aaf73360b07c013c80b88b4544a77
Julia Alexeeva <julia4406@gmail.com>
2024-12-14 12:46:04 +0200

    Added verifications

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ def copy_file(command: str) -> None:
         try:
             cp, file_name, new_name = command.split()
             if file_name != new_name:
-                with open(file_name, "r") as file_in, open(new_name, "x") as file_out:
+                with (open(file_name, "r") as file_in,
+                      open(new_name, "x") as file_out):
                     file_out.write(file_in.read())
         except FileExistsError:
             pass
