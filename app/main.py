1ad93ad37f2746b513d0e96f0e5b9e6a9e2693a4
Koziakov Hlib <hlibs.work@gmail.com>
2024-12-15 17:06:52 +0200

    solution for cope

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split(" ")
+
+    if command_list[0] == "sp" and command_list[1] != command_list[2]:
+        with open(command_list[1], "r") as file_in,\
+                open(command_list[2], "w") as file_out:
+            cope_text = file_in.read()
+            file_out.write(cope_text)
