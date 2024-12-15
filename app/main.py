aee37c673f5c8682b880e9aa63cfe4b6fc854dee
AntonKrinichnyi <a.krinichnyi@gmail.com>
2024-12-15 12:20:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split()
+    file_name = command_list[1]
+    new_file_name = command_list[-1]
+    if file_name == new_file_name:
+        return
+    if command_list[0] == "cp":
+        with open(file_name, "r") as f_in, open(new_file_name, "w") as f_out:
+            for line in f_in:
+                f_out.write(line)
