2db4ae85f5f178b7dc03b10c4cf32c18dbebca26
taraskhom <khomenkotaras1305@gmail.com>
2024-12-15 21:02:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if command.startswith("cm"):
+        command = command.replace("cm ", "")
+    else:
+        print("Unknown command")
+    list_of_files_name = command.split(" ")
+    if list_of_files_name[0] != list_of_files_name[1]:
+        with (
+            open(list_of_files_name[0], "r")
+            as first_file,
+            open(list_of_files_name[1], "w")
+            as second_file
+        ):
+            second_file.write(first_file.read())
