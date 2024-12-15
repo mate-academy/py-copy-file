a9018bfefca1c34bb48c6e913fb3b1777244e90b
IvankaKuzin <kuziniwanka@gmail.com>
2024-12-16 00:03:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+class CommandError(Exception):
+    pass
+
+
+def copy_file(command: str) -> None:
+    command_list = command.split(" ")
+
+    if command_list[0] == "cp":
+        with (
+            open(command_list[1], "r") as file_in,
+            open(command_list[2], "w") as file_out,
+        ):
+            reader = file_in.read()
+            file_out.write(reader)
+    raise CommandError
