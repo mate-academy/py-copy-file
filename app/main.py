fbeba293abdc6496715d7fd19ba085f5ea73774e
4ikkamony <compbanderloga@gmail.com>
2024-12-16 22:35:46 +0200

    Added a lil' exception

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def copy_file(command: str) -> None:
     command_words = command.split(" ")
 
-    if len(command_words) != 3:
-        return
+    if len(command_words) != 3 or command_words[0] != "cp":
+        raise ValueError(f"{command} is invalid. Use 'cp source destination'")
 
     command_name, file_in_path, file_out_path = command_words
 
-    if command_name == "cp" and (file_in_path != file_out_path):
+    if file_in_path != file_out_path:
         with (
             open(file_in_path, "r") as file_in,
             open(file_out_path, "w") as file_out
