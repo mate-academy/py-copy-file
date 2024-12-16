8a62a128975db52c6cd9338eb04254b00229f09d
taraskhom <khomenkotaras1305@gmail.com>
2024-12-16 04:26:48 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,25 @@
 def copy_file(command: str) -> None:
-    if command.startswith("cm"):
-        command = command.replace("cm ", "")
+    if command.startswith("cp"):
+        command = command.replace("cp ", "")
     else:
         print("Unknown command")
+        return
     list_of_files_name = command.split(" ")
-    if list_of_files_name[0] != list_of_files_name[1]:
-        with (
-            open(list_of_files_name[0], "r")
-            as first_file,
-            open(list_of_files_name[1], "w")
-            as second_file
-        ):
-            second_file.write(first_file.read())
+    if (list_of_files_name[0] != list_of_files_name[1]
+            and len(list_of_files_name) == 2):
+        try:
+            with (
+                open(list_of_files_name[0], "r")
+                as first_file,
+                open(list_of_files_name[1], "w")
+                as second_file
+            ):
+                second_file.write(first_file.read())
+        except FileNotFoundError:
+            raise FileNotFoundError(
+                f"The file '{first_file}' does not exist."
+            )
+        except Exception as error:
+            raise RuntimeError(
+                f"An error occurred while copying the file: {error}"
+            )
