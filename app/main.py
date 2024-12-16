0929b3248de2c9152ad5a8e946553620758a5571
Ruslan <ruslanyemets@gmail.com>
2024-12-16 20:09:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -15,11 +15,9 @@ def copy_file(command: str) -> None:
         )
     if command_list[0] != "cp":
         raise WrongCommand("Command must start with 'cp'")
-    if command_list[1] == command_list[2]:
-        return
-
-    with (
-        open(command_list[1], "r") as file_in,
-        open(command_list[2], "w") as file_out
-    ):
-        file_out.write(file_in.read())
+    if command_list[1] != command_list[2]:
+        with (
+            open(command_list[1], "r") as file_in,
+            open(command_list[2], "w") as file_out
+        ):
+            file_out.write(file_in.read())
