114db152fca0203c3ba89fee65685eedd2c8bd5d
Nazar Slavych <slavuch232355@gmail.com>
2024-12-16 16:09:43 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        raise ValueError
+
+    source_file, destination_file = parts[1], parts[2]
+
+    if source_file == destination_file:
+        return
+
+    if not os.path.exists(source_file):
+        raise FileNotFoundError
+
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
+        file_out.write(file_in.read())
