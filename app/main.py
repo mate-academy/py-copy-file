7ebe11c24823800bf6cbbb03c93ab914af29c633
Anastasiia <skubakovaa@gmail.com>
2024-12-16 22:06:42 +0100

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 def copy_file(command: str) -> None:
     command = command.split(" ")
-    file_name = command[1]
-    new_file_name = command[2]
-    if file_name != new_file_name:
-        with (open(file_name, "r") as file,
-              open(new_file_name, "w") as new_file):
+    source_file_name = command[1]
+    destination_file_name = command[2]
+    if len(command) != 3 or command[0] != "cp":
+        return
+    if source_file_name != destination_file_name:
+        with (open(source_file_name, "r") as file,
+              open(destination_file_name, "w") as new_file):
             for line in file:
                 new_file.write(f"{line}")
