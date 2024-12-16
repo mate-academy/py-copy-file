a0691575cee7031a596e4ef1016361eefd0c9bf5
Maksym <chukhno.maksym@gmail.com>
2024-12-16 16:29:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    original_file = parts[1]
+    copy_file = parts[2]
+
+    if original_file == copy_file:
+        return True
+
+    with open(original_file, "r") as file_in, open(copy_file, "w") as file_out:
+        file_out.write(file_in.read())
