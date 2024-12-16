a0c36038df17377e1a2c553eac6b971417d70d75
Oleksandr Barabanov <imtence@icloud.com>
2024-12-16 21:19:00 +0200

    Updated

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "cp":
-        raise ValueError("Invalid command format. Use: 'cp source_file target_file'")
+        raise ValueError("Invalid command format. "
+                         "Use: 'cp source_file target_file'")
     source_file, target_file = parts[1], parts[2]
     if source_file == target_file:
         return
