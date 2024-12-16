94e80ca0857ea7fd28535d227fcdca5e652035f2
Maksym <chukhno.maksym@gmail.com>
2024-12-16 20:48:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 def copy_file(command: str) -> None:
     parts = command.split()
 
+    if len(parts) != 3 or parts[0] != "cp":
+        raise ValueError
+
     original_file = parts[1]
     copy_file = parts[2]
 
     if original_file == copy_file:
-        return True
+        return
 
     with open(original_file, "r") as file_in, open(copy_file, "w") as file_out:
         file_out.write(file_in.read())
