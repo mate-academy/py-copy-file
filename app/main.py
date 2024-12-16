54cff7c7348b1d58a58011b539c9d8b8fde73162
Ruslan <ruslanyemets@gmail.com>
2024-12-16 17:11:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+class CommandNotFound(Exception):
+    pass
+
+
+class IncorrectCommand(Exception):
+    pass
+
+
+def copy_file(command: str) -> None:
+    command_list = command.split()
+
+    if len(command_list) != 3:
+        raise IncorrectCommand("Incorrect command")
+    if command_list[0] != "cp":
+        raise CommandNotFound("Command 'cp' not found")
+    if command_list[1] == command_list[2]:
+        return
+
+    with (
+        open(command_list[1], "r") as file_in,
+        open(command_list[2], "w") as file_out
+    ):
+        file_out.write(file_in.read())
+
+
+copy_file("cp 124.txt 124_copy.txt")
