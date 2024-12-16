805edede378332d6434ffbec6cec024156e82118
IvankaKuzin <kuziniwanka@gmail.com>
2024-12-16 14:50:39 +0200

    Added check if command string has only 3 words and added block else for raised

--- app/main.py
+++ app/main.py
@@ -5,11 +5,18 @@ class CommandError(Exception):
 def copy_file(command: str) -> None:
     command_list = command.split(" ")
 
-    if command_list[0] == "cp":
+    if len(command_list) != 3:
+        return
+
+    if (
+        command_list[0] == "cp" and
+        command_list[1] != command_list[2]
+    ):
         with (
             open(command_list[1], "r") as file_in,
             open(command_list[2], "w") as file_out,
         ):
             reader = file_in.read()
             file_out.write(reader)
-    raise CommandError
+    else:
+        raise CommandError
