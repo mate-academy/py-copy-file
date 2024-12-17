d51c7951b7614e56c6ed6ca473a52ece061e728d
Oksana Dzeryn <oks.dzeryn@gmail.com>
2024-12-17 12:51:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 def copy_file(command: str) -> None:
-    f_in = command.split(" ")[1]
-    f_out = command.split(" ")[2]
-    if f_in == f_out:
-        return None
-    with open(f_in, "r") as file_in, open(f_out, "w") as file_out:
-        info = file_in.read()
-        file_out.write(info)
+    command_list = command.split(" ")
+    cp = command_list[0]
+    f_in = command_list[1]
+    f_out = command_list[2]
+    if len(command_list) == 3 and cp == "cp":
+        try:
+            if f_in == f_out:
+                return None
+            with open(f_in, "r") as file_in, open(f_out, "w") as file_out:
+                info = file_in.read()
+                file_out.write(info)
+        except FileNotFoundError:
+            print(f"Error: File '{f_in}' not found.")
