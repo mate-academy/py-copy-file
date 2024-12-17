4a8b153e3cfc87297c4190e992b3ce5bc2a7a040
git_practic <dogoberrr@gmail.com>
2024-12-17 23:34:44 +0200

    fix-flake-error

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ def copy_file(command: str) -> None:
 
         if file_to_copy_name != new_file_name:
             with (open(file_to_copy_name, "r") as file_in,
-                open(new_file_name, "w") as file_out):
+                  open(new_file_name, "w") as file_out):
                 for line in file_in:
                     file_out.write(f"{line}")
