120daaeba3e5f7a6d125ed3cb982b12c8e48d612
Kateryna <katerina.a.biruk@gmail.com>
2024-12-17 21:09:26 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split = command.split(" ")
+    command = split[0]
+
+    if command != "cp":
+        return
+
+    file1 = split[1]
+    file2 = split[2]
+
+    if file1 == file2:
+        return
+
+    with open(file1, "r") as file_in, open(file2, "w") as file_out:
+        file_out.writelines(file_in.readlines())
