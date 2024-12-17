5a9389a6ccbbf6e0647ac4bfec817511ad09a38f
Lisa <zhyganiuk.elizaveta@gmail.com>
2024-12-18 01:19:55 +0200

    solution_v_1.0

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_name(command: str) -> None:
+    parts = command.split(" ")
+
+    source_file, new_file = parts[1], parts[2]
+
+    if source_file == new_file:
+        return
+
+    with open(source_file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
