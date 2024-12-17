0d6386a5651e3dcc7f93a3a5ca34c43abc2b6401
git_practic <dogoberrr@gmail.com>
2024-12-17 23:32:34 +0200

    fixed-cp-command-condition

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 def copy_file(command: str) -> None:
     copy_data = command.split()
 
-    if len(copy_data) < 3 or copy_data[0] != "cp":
-        return
+    if len(copy_data) == 3 and copy_data[0] == "cp":
+        cp_command, file_to_copy_name, new_file_name = copy_data
 
-    command, file_to_copy_name, new_file_name = copy_data
-
-    if file_to_copy_name != new_file_name:
-        with (open(file_to_copy_name, "r") as file_in,
-              open(new_file_name, "w") as file_out):
-            for line in file_in:
-                file_out.write(f"{line}")
+        if file_to_copy_name != new_file_name:
+            with (open(file_to_copy_name, "r") as file_in,
+                open(new_file_name, "w") as file_out):
+                for line in file_in:
+                    file_out.write(f"{line}")
