247af937a982f40762328494e9c83b04cd1bc2ae
Dmytro <dima.sichkar2003@gmail.com>
2024-12-17 17:25:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    file_names = command.split(" ")[1:]
+    file_to_copy_name = file_names[0]
+    new_file_name = file_names[1]
+
+    if file_to_copy_name != new_file_name:
+        with (open(file_to_copy_name, "r") as file_in,
+              open(new_file_name, "w") as file_out):
+            for line in file_in:
+                file_out.write(f"{line}")
