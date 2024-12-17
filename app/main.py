ce154d71273fffacc1c4c8a44ed03f830e71ae82
Dmytro <dima.sichkar2003@gmail.com>
2024-12-17 18:00:47 +0200

    New solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 def copy_file(command: str) -> None:
-    file_names = command.split(" ")[1:]
-    file_to_copy_name = file_names[0]
-    new_file_name = file_names[1]
-
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        raise ValueError
+    file_to_copy_name, new_file_name = parts[1], parts[2]
     if file_to_copy_name != new_file_name:
-        with (open(file_to_copy_name, "r") as file_in,
-              open(new_file_name, "w") as file_out):
+         with open(file_to_copy_name, "r") as file_in, open(new_file_name, "w") as file_out:
             for line in file_in:
-                file_out.write(f"{line}")
+                file_out.write(line)
