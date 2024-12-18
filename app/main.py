ed099fffc9b380e8b95564b56078b001d3147d12
Maks Vakulenko <maksimvakulenkooo@gmail.com>
2024-12-18 22:09:44 +0100

    solute 3

--- app/main.py
+++ app/main.py
@@ -2,11 +2,10 @@ def copy_file(cp: str) -> None:
     parts = cp.split(" ")
     if len(parts) != 3 or parts[0] != "cp":
         raise Exception
-    source  = parts[1]
+    source = parts[1]
     destination = parts[2]
-    if source  == destination:
+    if source == destination:
         return
-
     with (open(source, "r") as file_in,
           open(destination, "w") as file_out):
         file_out.write(file_in.read())
