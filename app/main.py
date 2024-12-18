02ee5b20f56dab7976e19852ba383c458166b27c
Maks Vakulenko <maksimvakulenkooo@gmail.com>
2024-12-18 01:51:40 +0100

    solute

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(cp: str) -> None:
+    parts = cp.split(" ")
+    old = parts[1]
+    new = parts[2]
+    if old == new:
+        return
+    else:
+        with open(old, "r") as file_in:
+            with open(new, "w") as file_out:
+                file_out.write(file_in.read())
