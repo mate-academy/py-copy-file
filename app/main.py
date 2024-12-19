a9acafaeb26939f0f650c506d36f76e5cbe527de
Maks Vakulenko <maksimvakulenkooo@gmail.com>
2024-12-19 12:08:02 +0100

    solute 4

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
-def copy_file(cp: str) -> None:
-    parts = cp.split(" ")
-    if len(parts) != 3 or parts[0] != "cp":
-        raise Exception
-    source = parts[1]
-    destination = parts[2]
-    if source == destination:
+def copy_file(command : str) -> None:
+    command, source, destination = command.split()
+    if source == destination or command != "cp":
         return
     with (open(source, "r") as file_in,
           open(destination, "w") as file_out):
