9ed6758e24be93efc8ec5ad7b55a82c2ee72d9e8
neeeksa <avgust.k31@gmail.com>
2024-12-20 19:11:44 +0200

    First_solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> str | None:
+    command_info = command.split(" ")
+
+    if "cp" in command_info[0]:
+        if command_info[1] == command_info[2]:
+            return None
+        else:
+            with (open(command_info[1], "r") as original_file,
+                  open(command_info[2], "w") as new_file):
+                for line in original_file.readline():
+                    new_file.write(line)
