057da7fad0346ded887b628cd744383c4e67a793
MarianaAB19 <mariana.antoniak.babish@gmail.com>
2024-12-21 23:39:47 +0200

    Update app/main.py
    Co-authored-by: Vasyl Smutok <vasyl.smutok@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def copy_file(command: str) -> None:
-    text = command.split(" ")
+    text = command.split()
     if len(text) == 3 and text[0] == "cp" and text[1] != text[2]:
         with (open(text[1], "r") as file_in,
               open(text[2], "w") as file_copy):
