5dd448d771b936b0fafc76c6765faa53762905a8
Olexander Lypovenko <fantomasdeft@gmail.com>
2024-12-21 18:41:11 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ def copy_file(command: str) -> None:
     if len(parts) != 3 or parts[0] != "cp":
         raise ValueError("Command must be in the "
                          "format 'cp <file.txt> <file-copy.txt>'")
-    source_file, destanation_file = parts[1], parts[2]
-    if source_file == destanation_file:
+    source_file, destination_file = parts[1], parts[2]
+    if source_file == destination_file:
         return
 
     try:
         with (open(source_file, "r") as file_in,
-              open(destanation_file, "w") as file_out):
+              open(destination_file, "w") as file_out):
             file_out.write(file_in.read())
     except FileNotFoundError:
-        raise FileNotFoundError(f"The file '{source_file}' does not exits.")
+        raise FileNotFoundError(f"The file '{source_file}' does not exists.")
     except IOError as e:
         raise IOError(f"An I/O error occurred: {e}")
