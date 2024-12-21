86ace17ee9d805895e72bc8a5cd621d1843e0a97
Iryna Mishchenko <djrobentab@gmail.com>
2024-12-21 21:57:05 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     command_parts = command.split()
-    if command_parts[0] == "cp":
+    if len(command_parts) != 3 and command_parts[0] != "cp":
         raise ValueError("Bad command")
     if command_parts[1] != command_parts[2]:
         with open(command_parts[1], "r") as file_in, open(
