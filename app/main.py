4aa8afc80a1c1fc450beee8be3dc97d0b67c86ee
Dmytro.Poberezhnyi <foxymood@gmail.com>
2024-12-23 00:19:34 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    splitted = command.split(" ")
+    cm = splitted[0]
+    initial_file_name = splitted[1]
+    new_file_name = splitted[2]
+    if initial_file_name == new_file_name:
+        return None
+    elif cm != "cp":
+        return None
+    with (open(initial_file_name, "r") as file_in,
+          open(new_file_name, "w") as file_out):
+        initial = file_in.readlines()
+        for line in initial:
+            file_out.write(line)
