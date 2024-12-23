3697c86080158cb2472b99ed25a1710f8412ac25
Fallen Hero <failent.hero@gmail.com>
2024-12-23 15:53:20 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    _, file_name, copy_name = command.split(" ")
+    if file_name == copy_name:
+        return
+    with open(file_name, "r") as file_in, open(copy_name, "w") as file_out:
+        for line in file_in:
+            file_out.write(f"{line}")
