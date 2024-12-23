6552868efa5c8463f8b185e51449ed971792db61
Fallen Hero <failent.hero@gmail.com>
2024-12-23 16:03:26 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 def copy_file(command: str) -> None:
-    _, file_name, copy_name = command.split(" ")
-    if file_name == copy_name:
+    if command[0:2] != "cp" or command.count(" ") != 2:
         return
-    with open(file_name, "r") as file_in, open(copy_name, "w") as file_out:
+    _, file_name, copy_file = command.split(" ")
+    if file_name == copy_file:
+        return
+    with open(file_name, "r") as file_in, open(copy_file, "w") as file_out:
         for line in file_in:
             file_out.write(f"{line}")
