bfac2ced863f97cb42dee0eba39247bf7d219e16
Taras <t.kudinov90@gmail.com>
2024-12-23 16:05:44 +0200

    Solved task for review, after flake8 test fail remaking.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ def copy_file(command: str) -> None:
     c_list = command.split()
     if len(c_list) == 3 and c_list[0] == "cp":
         if c_list[1] != c_list[2]:
-            with open(c_list[1], "r") as file_in, open(c_list[2], "w") as file_out:
+            with (open(c_list[1], "r") as file_in,
+                  open(c_list[2], "w") as file_out):
                 file_out.write(file_in.read())
             file_in.close()
             file_out.close()
