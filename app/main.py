3dab9a7068bd04c69bd832587399a188f944cfff
nagafochfv <ohinskiy96@gmail.com>
2024-12-25 22:13:25 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def copy_file(command: str) -> None:
-    list_files = command.split()
-    if list_files[1] != list_files[2]:
-        with open(list_files[1], "r") as f1, open(list_files[2], "w") as f2:
-            f2.write(f1.read())
+    command_parts = command.split()
+    if len(command_parts) == 3:
+        if command_parts[0] == "cp":
+            if command_parts[1] != command_parts[2]:
+                with open(command_parts[1], "r") as f1, open(command_parts[2], "w") as f2:
+                    f2.write(f1.read())
