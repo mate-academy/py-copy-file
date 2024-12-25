4e425799c36c479d51f4894f10b2282b3baa15ab
Dmytro <dima.sichkar2003@gmail.com>
2024-12-25 15:06:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 def copy_file(command: str) -> None:
-    parts = command.split()
-    if len(parts) != 3 or parts[0] != "cp":
-        raise ValueError
-    file_to_copy_name, new_file_name = parts[1], parts[2]
-    if file_to_copy_name != new_file_name:
-         with open(file_to_copy_name, "r") as file_in, open(new_file_name, "w") as file_out:
-            for line in file_in:
-                file_out.write(line)
+    separated_command = command.split()
+    if len(separated_command) != 3:
+        raise ValueError("Your command is not valid")
+    if separated_command[0] != "cp":
+        raise ValueError("Use 'cp' at the beginning of command")
+    file_from = separated_command[1]
+    file_to = separated_command[2]
+    if file_from != file_to:
+        with open(file_from, "r") as file_from, open(file_to, "w") as file_to:
+            data = file_from.read()
+            file_to.write(data)
