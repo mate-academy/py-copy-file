9dfeed02f8175e0fba80bae78e4b63d453a1a197
Liza Shulika <liza.shulika2009@gmail.com>
2024-12-25 14:42:49 +0200

    copy

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str):
+    cd = command.split(" ")
+    if cd[1] == cd[2]:
+        return None
+    else:
+        with open(cd[1], "r") as file_in, open(cd[2], "w") as file_out:
+            for line in file_in:
+                file_out.write(line)
