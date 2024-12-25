c5d6ace018748f9fe13c5c454286894090867bbc
nagafochfv <ohinskiy96@gmail.com>
2024-12-25 22:08:21 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    list_files = command.split()
+    if list_files[1] != list_files[2]:
+        with open(list_files[1], "r") as f1, open(list_files[2], "w") as f2:
+            f2.write(f1.read())
