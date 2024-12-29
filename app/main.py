ab714a4e84a1d650293ab4e481e2f4a17d78489a
Misha Bigun <bigun.mykhaylo@gmail.com>
2024-12-29 21:21:54 +0200

    fix some problems

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 def copy_file(command: str) -> None:
     command = command.split()
-    if command[1] == command[2]:
-        with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+    if (command[0] == "cp"
+            and len(command) == 3
+            and command[1] != command[2]):
+        with (open(command[1], "r") as file_in,
+              open(command[2], "w") as file_out):
             file_out.write(file_in.read())
 
 
