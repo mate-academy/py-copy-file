d82ec2b140570d7b1892574fb05815f2a0c889bc
JulianskyMe <juliakamynskaya@gmail.com>
2025-01-01 19:52:12 +0200

    Solution
    Program takes in a command in string format, takes out the info about files out of it and creates the new file if the names are not repeating

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp_command, origin_file_name, new_file_name = command.split()
+
+    if origin_file_name == new_file_name:
+        return
+    elif not cp_command == "cp":
+        return
+    else:
+        with open(origin_file_name, "r") as file_in, \
+            open(new_file_name, "w") \
+                as file_out:
+            content = file_in.read()
+            file_out.write(content)
