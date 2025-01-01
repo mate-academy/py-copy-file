069c05794fcea0a52a882aa83655ea6f181c0c11
JulianskyMe <juliakamynskaya@gmail.com>
2025-01-01 20:06:29 +0200

    upd
    Small update to insure that program is checking the passed command for all of the required parameters

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 def copy_file(command: str) -> None:
-    cp_command, origin_file_name, new_file_name = command.split()
+    try:
+        cp_command, origin_file_name, new_file_name = command.split()
+    except ValueError:
+        return
 
     if origin_file_name == new_file_name:
         return
-    elif not cp_command == "cp":
+    elif cp_command != "cp":
         return
-    else:
-        with open(origin_file_name, "r") as file_in, \
-            open(new_file_name, "w") \
-                as file_out:
-            content = file_in.read()
-            file_out.write(content)
+
+    with open(origin_file_name, "r") as file_in, \
+        open(new_file_name, "w") \
+            as file_out:
+        content = file_in.read()
+        file_out.write(content)
