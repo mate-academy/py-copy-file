294383664566d671b79258d3fcc44304b2f269bd
Your NameMykola <mykola.osolinskyi@gmail.com>
2025-01-02 18:28:09 +0200

    task done. fixed according to the mentors comments

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    cp = parts[0]
+    old_file = parts[1]
+    new_file = parts[2]
+
+    if len(parts) != 3 or cp != "cp":
+        raise ValueError("Invalid command format")
+
+    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+        if file_in == file_out:
+            return
+        input_data = file_in.read()
+        file_out.write(input_data)
