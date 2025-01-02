3dc34a275468f9f8d93df21fdc84a7e32840b597
Anastasiia Mylnikova <87014295+amylnikova@users.noreply.github.com>
2025-01-02 22:08:24 +0200

    copy file task solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def copy_file(command: str) -> None:
+    inputs = command.split()
+
+    if len(inputs) > 3 or inputs[0] != "cp":
+        raise ValueError("Invalid command format.")
+
+    src_filename = inputs[1]
+    dst_filename = inputs[2]
+
+    if src_filename == dst_filename:
+        return
+
+    with (
+        open(f"./{src_filename}", "r")as file_in,
+        open(f"./{dst_filename}", "w") as file_out
+    ):
+        content = file_in.read()
+        file_out.write(content)
