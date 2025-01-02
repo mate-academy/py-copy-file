9bd70f579bb51e03ff8abf4dd3d56ef3fb3b66d1
Greshch <greshchook@gmail.com>
2025-01-02 15:32:36 +0200

    fix ai warns-1

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 def copy_file(command_line: str) -> None:
-    command, src, dest = command_line.split(" ")
+    arguments = command_line.split(" ")
+    if not len(arguments) == 2:
+        return
+    command, src, dest = arguments
     if src == dest:
         return
     quantum = 8
-    with open(src, "r") as read_src:
-        with open(dest, "a") as write_dst:
-            while True:
-                current_read = read_src.read(quantum)
-                if not current_read:
-                    break
-                write_dst.write(current_read)
+    with open(src, "r") as read_src, open(dest, "w") as write_dst:
+        while True:
+            current_read = read_src.read(quantum)
+            if not current_read:
+                break
+            write_dst.write(current_read)
