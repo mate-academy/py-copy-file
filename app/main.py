0e3b654427ee5bb52fa3fdd0323c1099949d4e73
Ivan Khomych <ivan.khomychh@gmail.com>
2025-01-04 01:24:05 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def copy_file(command: str) -> None:
     r_command = command.split(" ")
     if len(r_command) != 3 or r_command[0] != "cp":
-        print(f"Wrong command: {r_command[0]}")
+        print(f"Wrong command: {r_command[0]}. Need: cp")
         return
 
     if r_command[1] == r_command[2]:
         return
 
-    with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+    with open(r_command[1], "r") as file_in, open(r_command[2], "w") as file_out:
         for line in file_in.readlines():
             file_out.write(line)
