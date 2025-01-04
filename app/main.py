72f4e319ebd1806ca8bc7f5f01762a40b26b8132
Darya-Savchenko <savchenkodarya277@gmail.com>
2025-01-04 17:15:47 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3:
+        print("Invalid command format")
+        return
+
+    cmd, src, dest = parts
+
+    if src == dest:
+        print("Source and destination file names are the same. "
+              "Operation aborted.")
+        return
+
+    # Copy the file if names are different
+    with open(src, "r") as file_in, open(dest, "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
