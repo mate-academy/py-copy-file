50bc156ad8b49fdd7344d6c8a3f797c0676e73af
yevpy <eugene2piskun@gmail.com>
2025-01-06 01:14:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        cmd, source_file, second_file = command.split(maxsplit=3)
+    except ValueError:
+        print("Command must contain keyword 'cp' and 2 arguments")
+
+    if command == "cp" and source_file != second_file:
+        with (
+            open(source_file, "r") as source,
+            open(second_file, "w") as second,
+        ):
+            second.write(source.read())
