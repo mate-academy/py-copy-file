40b1d887283fefcace1beb69d5792894aaa429c9
yevpy <eugene2piskun@gmail.com>
2025-01-06 01:17:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
     try:
-        cmd, source_file, second_file = command.split(maxsplit=3)
+        cmd, source_file, second_file = command.split(maxsplit=2)
     except ValueError:
         print("Command must contain keyword 'cp' and 2 arguments")
 
-    if command == "cp" and source_file != second_file:
+    if cmd == "cp" and source_file != second_file:
         with (
             open(source_file, "r") as source,
             open(second_file, "w") as second,
