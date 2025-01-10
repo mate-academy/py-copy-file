db7be6468d4a628d627410f680f9dbfafbaec602
Olha Yukhnenko <pyolh36@gmail.com>
2025-01-10 18:08:29 +0200

    solved_task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import shutil
+import os
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if parts[0] != "cp" or len(parts) != 3:
+        raise ValueError("Invalid format. Expected: 'cp file.txt file-copy.txt'")
+
+    source_file = parts[1]
+    destination_file = parts[2]
+
+    if source_file == destination_file:
+        return
+
+    if not os.path.exists(source_file):
+        print(f"Error: '{source_file}' not found!")
+        return
+
+    try:
+        shutil.copy(source_file, destination_file)
+        print(f"File '{source_file}' copied to '{destination_file}'")
+    except Exception as e:
+        print(f"Error: {e}")
