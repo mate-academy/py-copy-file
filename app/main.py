e3ccd2d5271fc26d38595881846314392495ec8e
Leo <leonsiypopov@gmail.com>
2025-01-12 22:09:15 +0200

    done4

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ import shutil
 def copy_file(command: str) -> None:
     if command.startswith("cp "):
         cmd = command.split()
-        old_path = cmd[1]
-        new_path = cmd[2]
-        if old_path != new_path:
-            shutil.copy(old_path, new_path)
+        if len(cmd) == 3:
+            old_path = cmd[1]
+            new_path = cmd[2]
+            if old_path != new_path:
+                shutil.copy(old_path, new_path)
