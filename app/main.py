109422fff1568038fb96bb7d26c10bddaa819ac5
Leo <leonsiypopov@gmail.com>
2025-01-12 22:03:40 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
 # write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    old_path = command.split()[1]
+    new_path = command.split()[2]
+    if old_path != new_path:
+        shutil.copy(old_path, new_path)
