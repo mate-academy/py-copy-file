a3110c9bb9ba59e9323f516b68610584fc989594
Dmytro <dimkanividimka4@gmail.com>
2025-01-15 17:52:40 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split()
+    if command_list[1] != command_list[2]:
+        with (open(command_list[1], "r") as file_read,
+              open(command_list[2], "a") as file_write):
+            file_write.write(file_read.read())
