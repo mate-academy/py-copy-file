1c3117331fb7e66685f635cc0ef336676114b850
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2025-01-17 00:58:58 +0000

    completed task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    separate = command.split()
+    with open(separate[1], "r") as copy, open(separate[2], "w") as new_file:
+        if separate[1] == separate[2]:
+            pass
+        else:
+            for item in copy:
+                new_file.write(item)
