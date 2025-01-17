541f6fe9dfc14d694736f2b89f6746ef6021d15b
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2025-01-17 01:07:41 +0000

    resolved mistakes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 def copy_file(command: str) -> None:
     separate = command.split()
-    with open(separate[1], "r") as copy, open(separate[2], "w") as new_file:
+    if len(separate) != 3 or separate[0] != "cp":
+        raise StopIteration
+    with (open(separate[1], "r") as source_file,
+          open(separate[2], "w") as destination_file):
         if separate[1] == separate[2]:
             pass
         else:
-            for item in copy:
-                new_file.write(item)
+            for item in source_file:
+                destination_file.write(item)
