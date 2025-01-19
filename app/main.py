743bbd8111e752b5129dfce4bb36dd95ed1c0ca6
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-01-19 17:43:31 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    files_list = command.split(" ")
+    input_file = files_list[1]
+    output_file = files_list[2]
+
+    if str(input_file) != str(output_file):
+        with (open(input_file, "r") as input_object,
+              open(output_file, "w") as output_object):
+            content = input_object.read()
+            output_object.write(content)
