edd7d56c6e8d2432a0a0a0e40e1e32d498346d51
Andrii <andrijgordienko67@gmail.com>
2025-01-19 11:36:03 +0200

    Fix2

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ def copy_file(command: str) -> None:
     if "cp" in command:
         command_files = command.split()[1:]
         if len(set(command_files)) != 1:
-            with open(command_files[0], "r") as file, open(command_files[1], "w") as f:
+            with (open(command_files[0], "r") as file,
+                  open(command_files[1], "w") as f):
                 text_to_copy = file.read()
                 f.write(text_to_copy)
