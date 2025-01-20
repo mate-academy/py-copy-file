054c52d597209396c5aef8b63c4113bbd6c00acd
Vladik <vasylyshynvladik@gmail.com>
2025-01-20 14:02:33 +0200

    Fixed minor issues

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 def copy_file(command: str) -> None:
+    if not command.startswith("cp"):
+        return
+
     command_split = command.split()
     if len(command_split) != 3:
         return
 
-    command_name, from_file, to_file = command_split
+    _, from_file, to_file = command_split
 
-    if command_name != "cp" or from_file == to_file:
+    if from_file == to_file:
         return
 
     with open(from_file, "r") as file_in, open(to_file, "w") as file_out:
