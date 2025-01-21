36e84eb7617a2158c29a2b2118a1ec468ddb0d89
Bartolomeu <bartoszokroj@wp.pl>
2025-01-21 20:08:50 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
 def copy_file(command: str) -> None:
     command_split = command.split(" ")
-    while True:
-        if command_split[0] != "cp" or len(command_split) != 3:
-            break
-        if command_split[1] == command_split[2]:
-            break
-
+    if (command_split[0] == "cp"
+            and len(command_split) == 3
+            and command_split[1] != command_split[2]):
         with (open(command_split[1], "r") as file_in,
               open(command_split[2], "w") as file_out):
             file_out.write(file_in.read())
-        break
 
 
 if __name__ == "__main__":
-    copy_file()
+    command = str(input())
+    copy_file(command)
