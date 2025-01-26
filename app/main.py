b322b44a7656e09ac30584ba4e8050dc1dbe1249
Ron Phillips <mcspidy@gmail.com>
2025-01-26 11:55:58 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(values: str) -> None:
+    value = values.split(" ")
+    if value[0] != "cp":
+        return None
+    if value[1] == value[2]:
+        return None
+    if open(value[1], "r").read() == open(value[2], "r").read():
+        return None
+    else:
+        with open(value[1], "r") as file, open(value[2], "w") as file:
+            data = file.read()
+            file.write(data)
+        return None
