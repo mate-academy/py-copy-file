eab16da833cacdbb39b79b03d219db13dde098b6
Svitlana <svetapruta@gmail.com>
2025-01-27 14:06:56 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import shutil
 
 def copy_file(command: str) -> None:
     command_list = command.split(" ")
-    if command_list[0] == "cp" and len(command_list) == 3:
+    if command_list[0] == "cp" and len(command_list) == 3 and command_list[1] != command_list[2]:
         src_path = f"{command_list[1]}"
         dst_path = f"{command_list[2]}"
         shutil.copy(src_path, dst_path)
