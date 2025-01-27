fb0e289ae6d3aa5761a9a5aef19ec10ce81543b3
Svitlana <svetapruta@gmail.com>
2025-01-27 14:01:56 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    command_list = command.split(" ")
+    if command_list[0] == "cp" and len(command_list) == 3:
+        src_path = f"{command_list[1]}"
+        dst_path = f"{command_list[2]}"
+        shutil.copy(src_path, dst_path)
