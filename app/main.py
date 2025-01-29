13ab0da20310c7f9242afc957d13dcab73bd54e3
rediklayer <www.reddy12@gmail.com>
2025-01-29 10:32:42 +0200

    test solution v2 fixed

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 def copy_file(command: str) -> None:
     parts = command.split()
+    if len(parts) != 3 or parts[0] != 'cp':
+        return
     src_file = parts[1]
     dest_file = parts[2]
     if src_file == dest_file:
         return
     with (
         open(src_file, mode="r") as file_in,
-        open(dest_file, mode="r") as file_out
+        open(dest_file, mode="w") as file_out
     ):
         content = file_in.read()
         file_out.write(content)
