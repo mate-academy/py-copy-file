df8dcf273c087a3970c9ece7fa161de804158017
rediklayer <www.reddy12@gmail.com>
2025-01-29 10:28:13 +0200

    test solution?

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    src_file = parts[1]
+    dest_file = parts[2]
+    if src_file == dest_file:
+        return
+    with (
+        open(src_file, mode="r") as file_in,
+        open(dest_file, mode="r") as file_out
+    ):
+        content = file_in.read()
+        file_out.write(content)
