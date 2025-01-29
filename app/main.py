99f82488285c8b918e54ae2a3e8cbaead91166dd
oh <wish.master.mvp@gmail.com>
2025-01-29 23:57:05 +0200

    Fix parentheses instead backslashes

--- app/main.py
+++ app/main.py
@@ -4,12 +4,10 @@ def copy_file(command: str) -> None:
     if len(command_attr) != 3 or command_attr[0] != "cp":
         return
 
-    file_name = command_attr[1] \
-        if command_attr[1].endswith(".txt") \
-        else f"{command_attr[1]}.txt"
-    file_name_new = command_attr[2] \
-        if command_attr[2].endswith(".txt") \
-        else f"{command_attr[2]}.txt"
+    file_name = (command_attr[1] if command_attr[1].endswith(".txt")
+                 else f"{command_attr[1]}.txt")
+    file_name_new = (command_attr[2] if command_attr[2].endswith(".txt")
+                     else f"{command_attr[2]}.txt")
 
     if file_name == file_name_new:
         return
