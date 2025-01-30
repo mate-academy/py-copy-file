868febabead04ab0dffa36826ea3ea722bf3ffb5
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-01-30 18:55:22 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
 # write your code here
+def copy_file(command: str) -> None:
+    command_word = command.split(" ")
+    with (open(command_word[1], "r") as file_in,
+          open(command_word[2], "w") as file_out):
+        file_out.write(file_in.read())
