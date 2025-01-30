15d821d69375320d4c07101b65458e64626852f5
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-01-30 19:00:01 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 # write your code here
 def copy_file(command: str) -> None:
     command_word = command.split(" ")
+    if command_word[0] != "cp":
+        raise ValueError("Invalid command")
+    if len(command_word) != 3:
+        raise ValueError("Command should has 3 words")
     with (open(command_word[1], "r") as file_in,
           open(command_word[2], "w") as file_out):
         file_out.write(file_in.read())
