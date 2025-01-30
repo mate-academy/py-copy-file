790d82590e4e4487681bbebdf6dcb224ac4a3617
Yaroslav <yaroslav.liubarets@icloud.com>
2025-01-30 17:51:05 +0000

    Solution 1.1

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 def copy_file(command: str) -> None:
-    parts = command.split()
-    source_file = parts[1]
-    dest_file = parts[2]
-    if source_file == dest_file:
+    command_attr = command.split()
+
+    if len(command_attr) != 3 or command_attr[0] != "cp":
         return
 
-    with open(source_file, "r") as file_in, open(dest_file, "w") as file_out:
+    file_name = (command_attr[1] if command_attr[1].endswith(".txt")
+                 else f"{command_attr[1]}.txt")
+    file_name_new = (command_attr[2] if command_attr[2].endswith(".txt")
+                     else f"{command_attr[2]}.txt")
+
+    if file_name == file_name_new:
+        return
+
+    with open(file_name, "r") as file_in, open(file_name_new, "w") as file_out:
         file_out.write(file_in.read())
