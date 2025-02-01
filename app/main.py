22ef7c455070c9e060d9c53be4f10d58e3768fa9
bc2024-4 <bzabrodskij20@gmail.com>
2025-02-01 17:43:58 +0200

    add program

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    linux = command.split()
+    if linux != "cp":
+        raise ValueError
+    if linux[1] == linux[-1]:
+        pass
+    with open("source.txt", "r") as src, open("destination.txt", "w") as dst:
+        dst.write(src.read())
