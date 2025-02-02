43bb576bbad542a21577686105a3aafaa2e10c4f
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-02-02 13:06:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_str = command.split()
+
+    if (
+        len(command_str) == 3
+        and command_str[0] == "cp"
+        and command_str[1] != command_str[2]
+    ):
+        source_file_name = command_str[1]
+        new_file_name = command_str[2]
+
+        with (
+            open(source_file_name) as source_file,
+            open(new_file_name, "w") as new_file
+        ):
+            new_file.write(
+                source_file.read()
+            )
