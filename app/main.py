674edc1f777c5bfa84363b4ca7a1ceee928160c2
Oleksii Churbanov <lyoshatop@gmail.com>
2025-02-02 21:32:07 +0200

    implement py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    splitted_command = command.split(" ")
+    if splitted_command[1] == splitted_command[2]:
+        return
+    with (open(f"{splitted_command[1]}", "r") as file1,
+          open(f"{splitted_command[2]}", "w") as file2):
+        for line in file1.readlines():
+            file2.write(line)
