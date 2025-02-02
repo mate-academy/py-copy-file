5302baba3df0a9f75303a1776af2445bbaeb7c00
Vinohradov Volodymyr <eXpertt13@gmail.com>
2025-02-02 23:04:05 +0200

    Complete function copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_data = command.split()
+    file_in = split_data[1]
+    file_out = split_data[2]
+    if file_in == file_out:
+        pass
+    else:
+        with (open(file_in, "r", encoding="utf-8") as file_in,
+              open(file_out, "w", encoding="utf-8") as file_out):
+            file_out.write(file_in.read())
