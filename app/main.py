3ff6e0401bc99ff3b899e889929d86012133658f
Vinohradov Volodymyr <eXpertt13@gmail.com>
2025-02-03 12:41:38 +0200

    refactored code. Complete function copy file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 def copy_file(command: str) -> None:
-    split_data = command.split()
-    file_in = split_data[1]
-    file_out = split_data[2]
-    if file_in == file_out:
-        pass
-    else:
-        with (open(file_in, "r", encoding="utf-8") as file_in,
-              open(file_out, "w", encoding="utf-8") as file_out):
+    command_parts = command.split()
+    if len(command_parts) != 3:
+        raise ValueError("Must be [cmd, file1, file2]")
+    cmd, src_file, dst_file = command_parts
+
+    if src_file == dst_file:
+        return
+    if cmd != "cp":
+        raise ValueError("wrong command accept only cp - Copy")
+    try:
+        with (open(src_file, "r", encoding="utf-8") as file_in,
+              open(dst_file, "w", encoding="utf-8") as file_out):
             file_out.write(file_in.read())
+    except (ValueError, FileNotFoundError) as e_info:
+        print(e_info)
+
+
+def main() -> None:
+    copy_file("cp file.txt new_file.txt")
+
+
+if __name__ == "__main__":
+    main()
