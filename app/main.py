9582246dc2d3d4d760959c1de551e9c45d3b135c
RuslanTopal <baranotik15@gmail.com>
2025-02-03 07:32:01 +0000

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    data = command.split()
+
+    if data[1] == data[2]:
+        return
+
+    try:
+        with open(data[1], "r") as f_in, open(data[2], "w") as f_out:
+            file_data = f_in.read()
+            f_out.write(file_data)
+    except FileNotFoundError:
+        print(f"Error: {data[1]} not found.")
+    except PermissionError:
+        print("Error: Permission denied to read/write files.")
