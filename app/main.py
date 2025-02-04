d62cebe03d1d20b21dd8c6fdf9d126c6ebb8b606
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-02-04 17:47:38 +0100

    verify cp command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 def copy_file(command: str) -> None:
+    words = command.split(" ")
+    if len(words) != 3:
+        return
     cp_command, file_to_copy, new_file = command.split(" ")
     if file_to_copy == new_file:
         return
-    with open(file_to_copy, "r") as file_in, open(new_file, "w") as file_out:
-        file_out.write(file_in.read())
+    if cp_command == "cp":
+        with (open(file_to_copy, "r") as file_in,
+              open(new_file, "w") as file_out):
+            file_out.write(file_in.read())
