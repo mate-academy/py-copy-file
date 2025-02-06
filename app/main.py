39064fffd780fe3490965aeed296964568cfc41f
Vitaliy Tetianichenko <vitaliy2work@icloud.com>
2025-02-06 16:21:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+def copy_file(command: str) -> None:
+    ls = command.split()
+    first = ls[1]
+    second = ls[2]
+    if not first == second:
+        with open(first, "r") as one, open(second, "w") as two:
+            two.write(one.read())
