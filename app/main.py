8feefaf10b9f82c7a8fc46b23500e0e38dcd5cc7
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-02-07 20:57:40 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+    file_in = command[1]
+    file_out = command[2]
+    if file_in != file_out:
+        content = None
+        with open(file_in, "r") as f:
+            content = f.read()
+
+        with open(file_out, "w") as f:
+            f.write(content)
