533f44a8ff639d5fa2767df5fbdb45ff2b7f289d
Marek Smolana <msmolana@gmail.com>
2025-02-07 10:25:24 +0100

    draft

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_parts = command.split(" ")
+    command = command_parts[0]
+    source_file_name = command_parts[1]
+    destination_file_name = command_parts[2]
+    if command != "cp" or source_file_name == destination_file_name:
+        return
+
+    with (open(source_file_name, "r") as source,
+         open(destination_file_name, "w") as destination):
+        destination.write(source.read())
