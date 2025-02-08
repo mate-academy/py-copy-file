8827eb9704980415a1f6297d6f1fd05385472262
MTCH <dimamitich2@gmail.com>
2025-02-09 01:55:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    list_command = command.split()
+    if len(list_command) != 3 and list_command[0] != "cp":
+        return None
+    if list_command[1] == list_command[2]:
+        return None
+
+    with (open(list_command[1]) as first_file,
+          open(list_command[2], "w") as second_file):
+        second_file.write(first_file.read())
