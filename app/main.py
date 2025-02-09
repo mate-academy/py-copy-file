689dbce9a19856a1d5b96c620c387d3013916e7f
MTCH <dimamitich2@gmail.com>
2025-02-09 02:00:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def copy_file(command: str) -> None:
 
     list_command = command.split()
-    if len(list_command) != 3 and list_command[0] != "cp":
+    if len(list_command) != 3 or list_command[0] != "cp":
         return None
     if list_command[1] == list_command[2]:
         return None
