7d2d17911f62976e1255626926923daa2000eff7
Yaroslav Zhuravlov <zyzy92187@gmail.com>
2025-02-10 16:11:00 +0200

    implement copy_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(user_input: str) -> None:
+    user_input_args = user_input.split(" ")
+
+    assert len(user_input_args) == 3
+
+    cmd, source_filename, destination_filename = user_input_args
+
+    assert cmd == "cp"
+
+    if source_filename == destination_filename:
+        return
+
+    with open(source_filename, "r") as source_file, open(
+        destination_filename, "w"
+    ) as destination_file:
+        destination_file.write(source_file.read())
