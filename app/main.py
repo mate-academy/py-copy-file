13d758bcfab832d7ddd5cd67513eef9a4fcaf486
Yaroslav Zhuravlov <zyzy92187@gmail.com>
2025-02-11 20:24:28 +0200

    fix: simplify copy_file implementation by argparse and shlex

--- app/main.py
+++ app/main.py
@@ -1,16 +1,28 @@
+import argparse
+import shlex
+
+parser = argparse.ArgumentParser(
+    prog="cp",
+    description="Copy file from source to destination",
+)
+
+parser.add_argument("source")
+parser.add_argument("destination")
+
+
 def copy_file(user_input: str) -> None:
-    user_input_args = user_input.split(" ")
+    cmd, *args = shlex.split(user_input)
 
-    assert len(user_input_args) == 3
-
-    cmd, source_filename, destination_filename = user_input_args
-
-    assert cmd == "cp"
-
-    if source_filename == destination_filename:
+    if cmd != "cp":
+        parser.print_usage()
         return
 
-    with open(source_filename, "r") as source_file, open(
-        destination_filename, "w"
+    parsed_args = parser.parse_args(args)
+
+    if parsed_args.source == parsed_args.destination:
+        return
+
+    with open(parsed_args.source, "r") as source_file, open(
+        parsed_args.destination, "w"
     ) as destination_file:
         destination_file.write(source_file.read())
