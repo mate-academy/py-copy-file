212538fd69d477813046e2638a2512b294bfac7e
Volodymyrdv <vovakryt123453@gmail.com>
2025-02-13 02:36:12 +0200

    Commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+def copy_file(command: str) -> None:
+    intro, outro = command.split(" ")[1:]
+    if intro != outro:
+        with open(intro, "r") as file_in, open(outro, "w") as file_out:
+            file_out.write(file_in.read())
