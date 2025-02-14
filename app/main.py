9ab30100060aacf71c1e4e22773b870456eb6eab
Ivan Revko <revko7692@gmail.com>
2025-02-14 12:46:01 +0200

    Add one more check

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def copy_file(command: str) -> None:
     split_command = command.split()
-    if split_command[1] != split_command[2]:
-        or_file, cp_file = split_command[1], split_command[2]
-        with open(or_file, "r") as file_in, open(cp_file, "w") as file_out:
-            file_out.write(file_in.read())
+    if len(split_command) == 3 and split_command[0] == "cp":
+        if split_command[1] != split_command[2]:
+            or_file, cp_file = split_command[1], split_command[2]
+            with open(or_file, "r") as file_in, open(cp_file, "w") as file_out:
+                file_out.write(file_in.read())
