dc570b6bb5d212534ee1078080834acfd7004a0d
Johnny <ivensmk@gmail.com>
2025-02-14 17:00:14 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 def copy_file(command: str) -> None:
-    new_file = command.split()
-    test1 = new_file[1]
-    test2 = new_file[2]
-
-    if len(new_file) != 3 or "cp" != new_file[0] or test1 == test2:
+    name_in, name_out = command.split()[1:]
+    if name_in == name_out:
         return
-    with open(test1, "r") as file_in, open(test2, "w") as file_out:
+    with open(name_in, "r") as file_in, open(name_out, "w") as file_out:
         file_out.write(file_in.read())
