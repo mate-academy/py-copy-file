08472b47d78e2fea2878764aff2fd0f9e883d5e4
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-02-17 17:27:51 +0200

    Solution. After fixing issues.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
+class UnknownCommand(Exception):
+    pass
+
+
 def copy_file(command: str) -> None:
     cmd = command.split(" ")
     if cmd[0] == "cp":
         if cmd[1] != cmd[2]:
-            with open(cmd[1], "r") as source, open(cmd[2], "w") as target:
-                lines = source.readlines()
-                for line in lines:
-                    target.write(line)
+            try:
+                with open(cmd[1], "r") as source, open(cmd[2], "w") as target:
+                    lines = source.readlines()
+                    for line in lines:
+                        target.write(line)
+            except FileNotFoundError:
+                print(f"File {cmd[1]} does not exist!")
+            except IOError:
+                print(f"File {cmd[1]} cannot be read!")
+    else:
+        raise UnknownCommand("Use format: 'cp source_file destination_file'.")
