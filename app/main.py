fd72fb098adb261d2261a432317aaca6abbef032
CryptoQwest <morokhovgleb@gmail.com>
2025-02-20 17:47:50 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from pathlib import Path
+
+
+def copy_file(command: str) -> None:
+    if isinstance(command, str):
+        list_of_files = command.split(" ")
+        if "cp" in list_of_files:
+            if len(list_of_files) == 3:
+                filee = list_of_files[1]
+                new_file = list_of_files[2]
+                if filee == new_file:
+                    return
+                elif Path(filee).is_file():
+                    with open(f"{filee}", "r") as file_in:
+                        reader = file_in.read()
+
+                    with open(f"{new_file}", "w") as file_out:
+                        file_out.write(f"{reader}")
