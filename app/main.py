ee925c08e37a648567e93263de7f265b55c24434
Andrii <yurkivandriy02@gmail.com>
2025-02-26 19:40:14 +0200

    Completed task copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def copy_file(command: str) -> None:
+    part_of_command = command.split()
+    if len(part_of_command) != 3 or part_of_command[0] != "cp":
+        print("Incorrect file format")
+        return
+
+    file_1, file_2 = part_of_command[1], part_of_command[2]
+
+    if file_1 == file_2:
+        print("Incorrect file name")
+        return
+
+    try:
+        with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
+            file_out.write(file_in.read())
+            print(f"File '{file_1}' successfully copy to '{file_2}'!")
+    except FileNotFoundError:
+        print(f"File {file_1} not found")
