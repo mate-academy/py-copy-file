07bd19840e2502ef4a0a059dec69cb7ca1c162b5
Nikita Korenevskyi <korenevsky37@gmail.com>
2025-02-26 16:56:59 +0200

    Done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split(" ")
+    if (len(split_command) < 3
+            or split_command[0] != "cp"
+            or split_command[1] == split_command[2]):
+        return
+    try:
+        with (open(split_command[1], "r") as file_in,
+              open(split_command[2], "w") as file_out):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        print(f"Error: The file '{split_command[1]}' does not exist.")
