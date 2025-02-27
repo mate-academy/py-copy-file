e3993e9697110e9d8bc13ad64359753e2fbd12c4
Windings-Lab <janton20001@gmail.com>
2025-02-27 18:15:17 +0200

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
-from itertools import zip_longest
-
-
 def copy_file(command: str) -> None:
-    command_dict = {"cp": None, "src_name": None, "dst_name": None}
-    command_dict = dict(zip_longest(command_dict, command.split()))
-    if (command_dict["cp"] != "cp"
-            or command_dict["src_name"] is None
-            or command_dict["dst_name"] is None):
+    if not command.startswith("cp"):
+        return
+
+    args = command.split()[1:]
+    if len(args) != 2 or args[0] is None or args[1] is None:
         return
 
     try:
-        with (open(command_dict["src_name"], "r") as src_file,
-              open(command_dict["dst_name"], "w") as dst_file):
+        with (open(args[0], "r") as src_file,
+              open(args[1], "w") as dst_file):
             dst_file.write(src_file.read())
     except FileNotFoundError:
         return
