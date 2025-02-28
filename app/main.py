df2f68bc5465b36448791fc4137743557e223cb7
Andrii <yurkivandriy02@gmail.com>
2025-02-28 12:19:59 +0200

    fix flake8 problem

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ def copy_file(command: str) -> None:
     command_parts = command.split()
 
     if len(command_parts) != 3:
-        print("Invalid command format. Expected format: 'cp <source_file> <destination_file>'")
+        print(
+            "Invalid command format. Expected format:"
+            " 'cp <source_file> <destination_file>'"
+        )
         return
 
     operation, source_file, destination_file = command_parts
@@ -16,8 +19,13 @@ def copy_file(command: str) -> None:
         return
 
     try:
-        with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
+        with open(source_file, "r") as file_in, open(
+            destination_file, "w"
+        ) as file_out:
             file_out.write(file_in.read())
-            print(f"File '{source_file}' successfully copied to '{destination_file}'!")
+            print(
+                f"File '{source_file}' "
+                f"successfully copied to '{destination_file}'!"
+            )
     except FileNotFoundError:
         print(f"File '{source_file}' not found.")
