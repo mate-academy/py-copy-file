e11f9a7b73e125da0a8dd76863855b2381bf9a93
vadym <vadym@ohmanalytics.com>
2025-03-01 18:15:10 +0200

    make some changes

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ import shutil
 
 
 def copy_file(command: str) -> None:
-    commands = command.split(" ")
+    commands = command.split()
     if (
             len(commands) != 3
-            or not commands[0] == "cp"
+            or commands[0] != "cp"
             or commands[1] == commands[2]
     ):
+        print(f"Please check you command {command}. Something went wrong.")
         return
     shutil.copy(commands[1], commands[2])
