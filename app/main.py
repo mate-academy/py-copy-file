6c1fda16a6c4b8d68f9ae039bdf16eeb3c39a509
Oleh Kozdryn <kozdryn.oleg@gmail.com>
2025-03-01 15:40:58 +0200

    Solution v2

--- app/main.py
+++ app/main.py
@@ -10,7 +10,8 @@ def copy_file(command: str) -> None:
         print("File name is the same, copying aborted.")
         return
     try:
-        with open(f"{file}.txt", "r") as file_in, open(f"{destination_file}.txt", "w") as file_out:
+        with (open(f"{file}.txt", "r") as file_in,
+              open(f"{destination_file}.txt", "w") as file_out):
             file_out.write(file_in.read())
     except FileNotFoundError:
         print("No such file")
