37fa2f0ec3527e6887aecd08547d8e567d1ebea7
PrimeGlorious <TheComboPunk@gmail.com>
2025-03-02 16:05:21 +0200

    Implement copy_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split()
+
+    try:
+        if split_command[0] != "cp" or split_command[1] == split_command[2]:
+            return None
+
+        with (open(split_command[1], "r") as current_file,
+              open(split_command[2], "w") as new_file):
+            current_file_content = current_file.read()
+
+            new_file.write(current_file_content)
+    except (FileNotFoundError, IndexError):
+        return
