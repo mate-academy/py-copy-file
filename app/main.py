d3beecaa39556318fd85ee2989d19adeb73be6da
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-03-03 20:02:07 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> bool:
+    list_command = command.split()
+    if len(list_command) == 3 and list_command[0] == "cp":
+        if list_command[1] != list_command[2]:
+            try:
+                with (open(list_command[1], "r") as file_in,
+                      open(list_command[2], "w") as file_out):
+                    for row in file_in:
+                        file_out.write(row)
+            except FileNotFoundError:
+                return False
