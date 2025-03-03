16d13b0ad10c339cbd84b1cea6a84ddf4c1eb81c
Бронислав <bronya.po4ta@gmail.com>
2025-03-03 17:12:27 +0200

    well done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    source_file = parts[1]
+    destination_file = parts[2]
+
+    if source_file == destination_file:
+        return
+
+    try:
+        with open(source_file, "r") as file_in:
+            content = file_in.read()
+    except FileNotFoundError:
+        return
+
+    with open(destination_file, "w") as file_out:
+        file_out.write(content)
