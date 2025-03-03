d580bd644fe78bcee66d35f2f80794c5d3c3e141
Mykyta Kpych <kopicnikita@gmail.com>
2025-03-03 14:34:08 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        parts_of_command = command.split()
+        if len(parts_of_command) == 3 and parts_of_command[0] == "cp":
+            original_file = parts_of_command[1]
+            copied_file = parts_of_command[2]
+
+            if original_file != copied_file:
+                with (open(original_file, "r") as file_out,
+                      open(copied_file, "w") as file_in):
+                    file_in.write(file_out.read())
+    except FileNotFoundError:
+        pass
