05e4e2dd737f07e5dcf06277d7558610c519fcc5
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-03-03 18:12:05 +0100

    Copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    files = command.split()
+    if len(files) != 3 or files[0] != "cp":
+        return
+    _, file, file_copy = files
+    if file == file_copy:
+        return
+    try:
+        with open(file, "r") as file1, open(file_copy, "w") as file2:
+            file2.write(file1.read())
+    except FileNotFoundError:
+        return
