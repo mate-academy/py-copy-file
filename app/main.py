af61e1dab411a577dc0cbb1daa3fbbb94006bfa7
Khrystyna Sen <anollina@ukr.net>
2025-03-03 22:13:24 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+from typing import Any
+
+
+def copy_file(command: str) -> Any:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        print("Not correct command format. Use 'cp <file> <new_file>'")
+        return
+    file, new_file = parts[1], parts[2]
+
+    if file == new_file:
+        return
+
+    try:
+        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
+        print(f"File '{file}' copied to '{new_file}'")
+
+    except FileNotFoundError:
+        print(f"Error: '{file}' not found.")
