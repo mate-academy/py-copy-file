46f29c9edcf0697d11064fee536da63818d4b3e7
Maksym Omelkov <omelkov85@gmail.com>
2025-03-04 17:26:12 +0200

    Copy file (1.0)

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    command_words = command.split()
+    if (len(command_words) == 3
+        and command_words[1] != command_words[2]
+        and os.path.exists(command_words[1])
+        and command_words[0] == "cp"): # noqa: E129 E261
+        with (open(command_words[1], "r") as file_read,
+              open(command_words[2], "w") as file_write):
+            file_write.write(file_read.read())
+    pass
