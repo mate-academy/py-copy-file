a9193976b87fffb345c8bb56b366fa584724f925
Olexand <yarmus.olexandr@gmail.com>
2025-03-04 15:51:29 +0200

    task solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        operation, file_name, destination = command.split()
+
+        if file_name == destination or operation != "cp":
+            return
+
+        try:
+            with (open(file_name, "r") as file_in,
+                  open(destination, "w") as file_out):
+                file_out.write(file_in.read())
+        except FileNotFoundError:
+            print(f"File '{file_name}' not found!")
+    except ValueError:
+        print("Invalid command format!")
